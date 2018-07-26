69fb7585ddba05df5be10b98da199d16787fe513
Michał Wieczorek <wieczorek-michal@wp.pl>
2018-07-26 14:26:48 +0200

    Fix script for running kyma in minikube in CI (#33)

--- installation/scripts/installer-ci-local.sh
+++ installation/scripts/installer-ci-local.sh
@@ -7,6 +7,8 @@ CR_PATH="$TMPDIR/installer-cr-local.yaml"
 MINIKUBE_IP=$(minikube ip)
 MINIKUBE_CA=$(cat ${HOME}/.minikube/ca.crt | base64 | tr -d '\n')
 
+bash $CURRENT_DIR/generate-local-config.sh
+
 bash $CURRENT_DIR/create-cr.sh --output ${CR_PATH} --domain "kyma.local" --k8s-apiserver-url "https://${MINIKUBE_IP}:8443" --k8s-apiserver-ca "${MINIKUBE_CA}"
 
 $CURRENT_DIR/installer.sh --local --cr ${CR_PATH}
