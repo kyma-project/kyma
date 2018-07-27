4c1bc0da71277f517fa4db62ee1c8951b7b9729d
Krystian Cieślik <crabtree@users.noreply.github.com>
2018-07-27 11:01:00 +0200

    Support for local run on linux (#41)
    * setting vm-driver accordingly to the platform
    
    * parameter to set vm-driver on mac/linux added

--- installation/cmd/run.sh
+++ installation/cmd/run.sh
@@ -6,6 +6,11 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DOMAIN="kyma.local"
 
+VM_DRIVER="virtualbox"
+if [ `uname -s` = "Darwin" ]; then
+    VM_DRIVER="hyperkit"
+fi
+
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -21,6 +26,11 @@ do
             shift # past argument
             shift # past value
         ;;
+        --vm-driver)
+            VM_DRIVER="$2"
+            shift
+            shift
+        ;;
         *)    # unknown option
             POSITIONAL+=("$1") # save it in an array for later
             shift # past argument
@@ -30,20 +40,20 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 if [[ ! ${SKIP_MINIKUBE_START} ]]; then
-    bash $CURRENT_DIR/../scripts/minikube.sh --domain ${DOMAIN}
+    bash ${CURRENT_DIR}/../scripts/minikube.sh --domain "${DOMAIN}" --vm-driver "${VM_DRIVER}"
 fi
 
 bash $CURRENT_DIR/../scripts/generate-local-config.sh
 
 if [ -z "$CR_PATH" ]; then
 
-    TMPDIR=`mktemp -d "$CURRENT_DIR/../../temp-XXXXXXXXXX"`
-    CR_PATH="$TMPDIR/installer-cr-local.yaml"
+    TMPDIR=`mktemp -d "${CURRENT_DIR}/../../temp-XXXXXXXXXX"`
+    CR_PATH="${TMPDIR}/installer-cr-local.yaml"
 
-    bash $CURRENT_DIR/../scripts/create-cr.sh --output ${CR_PATH} --domain ${DOMAIN}
-    bash $CURRENT_DIR/../scripts/installer.sh --local --cr "$CR_PATH"
+    bash ${CURRENT_DIR}/../scripts/create-cr.sh --output "${CR_PATH}" --domain "${DOMAIN}"
+    bash ${CURRENT_DIR}/../scripts/installer.sh --local --cr "${CR_PATH}"
 
     rm -rf $TMPDIR
 else
-    bash $CURRENT_DIR/../scripts/installer.sh --cr "$CR_PATH"
+    bash ${CURRENT_DIR}/../scripts/installer.sh --cr "${CR_PATH}"
 fi
