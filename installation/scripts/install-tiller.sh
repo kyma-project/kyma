605ac243ff889651cc8264bbb29172e86ebf5b03
Magda Strek <magdalena.strek@sap.com>
2018-08-01 11:52:52 +0200

    Moving tiller installation from installer component to bash scripts

--- installation/scripts/install-tiller.sh
+++ installation/scripts/install-tiller.sh
@@ -2,10 +2,6 @@
 
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo "- Creating service account for tiller..."
-kubectl -n kube-system create sa tiller
-kubectl create clusterrolebinding tiller-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-
 echo "- Installing Tiller..."
 kubectl create -f ${CURRENT_DIR}/../resources/tiller.yaml
 bash ${CURRENT_DIR}/is-ready.sh kube-system name tiller
