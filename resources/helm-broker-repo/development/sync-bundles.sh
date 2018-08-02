de3934cd90e039cd6290fe713c13a2912d90e7e6
KarolGrzyb <grzybk@gmail.com>
2018-08-02 10:49:23 +0200

    Fixing issues from Code Review

--- resources/helm-broker-repo/development/sync-bundles.sh
+++ resources/helm-broker-repo/development/sync-bundles.sh
@@ -2,11 +2,11 @@
 
 NAMESPACE=kyma-system
 
-BASE_DIR="$(dirname $0)/.."
+readonly CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly PROVISIONING_DIR="${CURRENT_DIR}/../provisioning"
+readonly BUNDLES_DIR="${CURRENT_DIR}/../bundles"
 
-PROVISIONING_DIR="${BASE_DIR}/provisioning"
-
-bash ${PROVISIONING_DIR}/provision-bundles.sh ${BASE_DIR}/bundles
+bash ${PROVISIONING_DIR}/provision-bundles.sh ${BUNDLES_DIR}
 
 HELM_BROKER_POD_NAME=$(kubectl get po -n ${NAMESPACE} -l app=core-helm-broker -o jsonpath="{.items[0].metadata.name}")
 
