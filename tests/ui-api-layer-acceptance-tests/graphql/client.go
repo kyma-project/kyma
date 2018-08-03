04b0591c9b8f3a0cb822c3eafacd91394342deb3
Damian Pacierpnik <17701137+damianpacierpnikatsap@users.noreply.github.com>
2018-08-03 13:49:11 +0200

    Authentication and authorization for GraphQL - acceptance tests (#80)

--- tests/ui-api-layer-acceptance-tests/graphql/client.go
+++ tests/ui-api-layer-acceptance-tests/graphql/client.go
@@ -4,16 +4,21 @@ import (
 	"context"
 	"crypto/tls"
 	"net/http"
-	"os"
 	"time"
 
+	"fmt"
+	"net"
+	"os/exec"
+	"strings"
+
+	"github.com/golang/glog"
 	"github.com/machinebox/graphql"
+	"github.com/pkg/errors"
 )
 
 const (
-	endpointEnvName = "ENDPOINT"
-	defaultEndpoint = "http://127.0.0.1:3000/graphql"
-	timeout         = 3 * time.Second
+	ingressGatewayControllerServiceURL = "istio-ingressgateway.istio-system.svc.cluster.local"
+	timeout                            = 3 * time.Second
 )
 
 type Client struct {
@@ -21,19 +26,17 @@ type Client struct {
 }
 
 func New() (*Client, error) {
-	endpoint := os.Getenv(endpointEnvName)
-	if endpoint == "" {
-		endpoint = defaultEndpoint
-	}
 
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
-	}
-	gqlClient := graphql.NewClient(endpoint, graphql.WithHTTPClient(httpClient))
+	config := loadConfig()
+
+	dexHttpClient := newHttpClient(config.localClusterHosts, nil)
+
+	idTokenProvider := newDexIdTokenProvider(dexHttpClient, config.idProviderConfig)
+
+	gqlHttpClient := newHttpClient(config.localClusterHosts, idTokenProvider)
+
+	gqlClient := graphql.NewClient(config.graphQlEndpoint, graphql.WithHTTPClient(gqlHttpClient))
+
 	return &Client{gqlClient}, nil
 }
 
@@ -47,4 +50,84 @@ func (c *Client) Do(req *Request, res interface{}) error {
 	defer cancel()
 
 	return c.gqlClient.Run(ctx, req.req, res)
+}
+
+type transportWithIdTokensSupport struct {
+	http.Transport
+	idTokenProvider idTokenProvider
+	currentIdToken  string
+}
+
+func (t *transportWithIdTokensSupport) RoundTrip(req *http.Request) (*http.Response, error) {
+
+	if t.idTokenProvider != nil {
+
+		if t.currentIdToken == "" {
+			if idToken, err := t.idTokenProvider.fetchIdToken(); err != nil {
+				return nil, errors.Wrap(err, "Request canceled.")
+			} else {
+				t.currentIdToken = idToken
+			}
+		}
+
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", t.currentIdToken))
+	}
+	return t.Transport.RoundTrip(req)
+}
+
+func newHttpClient(localClusterHosts []string, idTokenProvider idTokenProvider) *http.Client {
+
+	tr := &transportWithIdTokensSupport{
+		Transport: http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		idTokenProvider: idTokenProvider,
+	}
+
+	ingressGatewayControllerAddr, err := net.LookupHost(ingressGatewayControllerServiceURL)
+	if err != nil {
+
+		glog.Warningf("Unable to resolve host '%s' (if you are running this test from outside of Kyma ignore this log). Root cause: %v", ingressGatewayControllerServiceURL, err)
+
+		if minikubeIp := tryToGetMinikubeIp(); minikubeIp != "" {
+			ingressGatewayControllerAddr = []string{minikubeIp}
+		}
+	}
+
+	if len(ingressGatewayControllerAddr) > 0 {
+
+		glog.Infof("Ingress controller address: '%s'", ingressGatewayControllerAddr[0])
+
+		tr.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+
+			glog.Infof("Resolving: %s", addr)
+			for _, host := range localClusterHosts {
+				if strings.HasPrefix(addr, host) {
+					addr = strings.Replace(addr, host, ingressGatewayControllerAddr[0], 1)
+					break
+				}
+			}
+			glog.Infof("Resolved: %s", addr)
+
+			dialer := net.Dialer{}
+			return dialer.DialContext(ctx, network, addr)
+		}
+	}
+
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   time.Second * 10,
+	}
+
+	return client
+}
+
+func tryToGetMinikubeIp() string {
+	mipCmd := exec.Command("minikube", "ip")
+	if mipOut, err := mipCmd.Output(); err != nil {
+		glog.Warningf("Error while getting minikube IP (ignore this message if you are running this test inside Kyma). Root cause: %s", err)
+		return ""
+	} else {
+		return strings.Trim(string(mipOut), "\n")
+	}
 }
