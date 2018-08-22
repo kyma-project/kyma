df962334af2144903ec231f8debe0321f11f6ea0
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2018-08-22 14:18:29 +0200

    Code review #1

--- components/metadata-service/internal/externalapi/staticfilehandler_test.go
+++ components/metadata-service/internal/externalapi/staticfilehandler_test.go
@@ -1,14 +1,3 @@
-/*
- * [y] hybris Platform
- *
- * Copyright (c) 2000-2018 hybris AG
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of hybris
- * ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with hybris.
- */
 package externalapi
 
 import (
