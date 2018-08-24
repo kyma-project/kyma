a465aa04998f7e80b061e6e1e32a95d6af775d93
Krystian Cieslik <crabtree@users.noreply.github.com>
2018-08-24 12:30:41 +0200

    bash scripts aligned for providing local sources into installer

--- installation/scripts/copy-resources.sh
+++ installation/scripts/copy-resources.sh
@@ -2,7 +2,7 @@
 
 INSTALLER_NS_NAME=kyma-installer
 INSTALLER_POD_PATTERN=kyma-installer
-REMOTE_DIRECTORY=/kyma
+REMOTE_DIRECTORY=/kyma/injected
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOCAL_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
@@ -13,7 +13,7 @@ POD_NAME=$(kubectl -n ${INSTALLER_NS_NAME} get pods -l name=${INSTALLER_POD_PATT
 echo "Copying kyma sources from ${LOCAL_DIRECTORY} into ${POD_NAME}:${REMOTE_DIRECTORY} ..."
 
 kubectl -n ${INSTALLER_NS_NAME} exec ${POD_NAME} -- "/bin/rm" "-rf" "${REMOTE_DIRECTORY}"
-kubectl -n ${INSTALLER_NS_NAME} exec ${POD_NAME} -- "/bin/mkdir" "${REMOTE_DIRECTORY}"
+kubectl -n ${INSTALLER_NS_NAME} exec ${POD_NAME} -- "/bin/mkdir" "-p" "${REMOTE_DIRECTORY}"
 
 kubectl cp "${LOCAL_DIRECTORY}/resources" "${INSTALLER_NS_NAME}/${POD_NAME}:${REMOTE_DIRECTORY}/resources"
 kubectl cp "${LOCAL_DIRECTORY}/installation" "${INSTALLER_NS_NAME}/${POD_NAME}:${REMOTE_DIRECTORY}/installation"
