3a4ce945d30be7cf0d3cebd3699531e951217dea
Marco Bebway | SAP Hybris <marco.bebway@sap.com>
2018-09-06 15:21:52 +0200

    Extract the Event service outside of the Gateway: * add the event-service helm charts * remove event handling from the gateway component code base * remove event related configurations from the gateway helm charts * update kyma acceptance-tests to use the new event-service

--- components/gateway/cmd/gateway/gateway.go
+++ components/gateway/cmd/gateway/gateway.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/kyma-project/kyma/components/gateway/internal/apperrors"
-	"github.com/kyma-project/kyma/components/gateway/internal/events/bus"
 	"github.com/kyma-project/kyma/components/gateway/internal/externalapi"
 	"github.com/kyma-project/kyma/components/gateway/internal/httptools"
 	"github.com/kyma-project/kyma/components/gateway/internal/k8sconsts"
@@ -33,8 +32,6 @@ func main() {
 
 	options := parseArgs()
 	log.Infof("Options: %s", options)
-
-	bus.Init(&options.sourceNamespace, &options.sourceType, &options.sourceEnvironment, &options.eventsTargetURL)
 
 	proxyCache := proxycache.NewProxyCache(options.skipVerify, options.proxyCacheTTL)
 
--- components/gateway/internal/externalapi/externalapi.go
+++ components/gateway/internal/externalapi/externalapi.go
@@ -9,8 +9,6 @@ import (
 func NewHandler() http.Handler {
 	router := mux.NewRouter()
 
-	router.PathPrefix("/{re}/v1/events").Handler(NewEventsHandler()).Methods(http.MethodPost)
-
 	router.Path("/v1/health").Handler(NewHealthCheckHandler()).Methods(http.MethodGet)
 
 	router.NotFoundHandler = NewErrorHandler(404, "Requested resource could not be found.")
