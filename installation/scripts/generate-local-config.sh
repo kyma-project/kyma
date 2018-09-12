2a4af3f48e8b5875107fb7f8769540ebe2a6359e
Krystian Cieslik <crabtree@users.noreply.github.com>
2018-09-12 10:30:43 +0200

    move cluster config to separate file

--- installation/scripts/generate-local-config.sh
+++ installation/scripts/generate-local-config.sh
@@ -3,44 +3,11 @@ set -e
 
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-CONFIG_TPL_PATH="${ROOT_PATH}/../resources/installer-config.yaml.tpl"
+CONFIG_TPL_PATH="${ROOT_PATH}/../resources/installer-config-local.yaml.tpl"
 CONFIG_OUTPUT_PATH=$(mktemp)
 
 cp $CONFIG_TPL_PATH $CONFIG_OUTPUT_PATH
 
-##########
-
-echo -e "\nGenerating configmap for Cluster certificate"
-
-TLS_FILE=${ROOT_PATH}/../resources/local-tls-certs.yaml
-TLS_CERT=$(cat ${TLS_FILE} | grep 'tls.crt' | sed 's/^.*: //' | tr -d '\n')
-TLS_KEY=$(cat ${TLS_FILE} | grep 'tls.key' | sed 's/^.*: //' | tr -d '\n')
-
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__TLS_CERT__" --value "${TLS_CERT}"
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__TLS_KEY__" --value "${TLS_KEY}"
-
-##########
-
-echo -e "\nGenerating secret for Remote Environemnts"
-
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__REMOTE_ENV_CA__" --value ""
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__REMOTE_ENV_CA_KEY__" --value ""
-
-##########
-
-echo -e "\nGenerating config map for installation"
-
-MINIKUBE_IP=$(minikube ip)
-MINIKUBE_CA=$(cat ${HOME}/.minikube/ca.crt | base64 | tr -d '\n')
-
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__IS_LOCAL_INSTALLATION__" --value "true"
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__DOMAIN__" --value "kyma.local"
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__EXTERNAL_PUBLIC_IP__" --value ""
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__REMOTE_ENV_IP__" --value ""
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__ADMIN_GROUP__" --value ""
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__ENABLE_ETCD_BACKUP__" --value "false"
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__ETCD_BACKUP_ABS_CONTAINER_NAME__" --value ""
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${CONFIG_OUTPUT_PATH} --placeholder "__INGRESSGATEWAY_SERVICE_TYPE__" --value "NodePort"
 ##########
 
 echo -e "\nApplying configuration"
@@ -56,26 +23,6 @@ rm ${CONFIG_OUTPUT_PATH}
 echo -e "\nConfiguring sub-components"
 
 bash ${ROOT_PATH}/configure-components.sh
-
-##########
-
-echo -e "\nGenerating secret for UI Test"
-
-UI_TEST_TPL_PATH="${ROOT_PATH}/../resources/ui-test-secret.yaml.tpl"
-UI_TEST_OUTPUT_PATH=$(mktemp)
-
-cp $UI_TEST_TPL_PATH $UI_TEST_OUTPUT_PATH
-
-UI_TEST_USER=$(echo -n "admin@kyma.cx" | base64 | tr -d '\n')
-UI_TEST_PASSWORD=$(echo -n "nimda123" | base64 | tr -d '\n')
-
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${UI_TEST_OUTPUT_PATH} --placeholder "__UI_TEST_USER__" --value "${UI_TEST_USER}"
-bash ${ROOT_PATH}/replace-placeholder.sh --path ${UI_TEST_OUTPUT_PATH} --placeholder "__UI_TEST_PASSWORD__" --value "${UI_TEST_PASSWORD}"
-
-echo -e "\nApplying asecret for UI Test"
-kubectl apply -f "${UI_TEST_OUTPUT_PATH}"
-
-rm ${UI_TEST_OUTPUT_PATH}
 
 ##########
 
