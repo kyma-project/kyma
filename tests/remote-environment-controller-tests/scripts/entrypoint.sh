37f3632650fc0ae86947ddb1fec1e89e88913eda
Szymon Gibala <szymon.gibala@sap.com>
2018-09-13 13:15:52 +0200

    Fix dependencies

--- tests/remote-environment-controller-tests/scripts/entrypoint.sh
+++ tests/remote-environment-controller-tests/scripts/entrypoint.sh
@@ -3,12 +3,8 @@ res=0
 
 echo "Remote Environment Controller tests"
 
-echo "Run api tests"
-./apitests.test
-res=$((res+$?))
-
-echo "Run kubernetes tests"
-./k8stests.test
+echo "Run controller tests"
+./controller.test
 res=$((res+$?))
 
 exit ${res}
