b131ab6d3779c8c2ae2347059fc28ec0ca387a5d
Sayan Hazra <sayan.mnit@gmail.com>
2018-09-14 10:04:57 +0200

    Increased fs.inotify.max_user_instances for minikube

--- installation/scripts/minikube.sh
+++ installation/scripts/minikube.sh
@@ -131,6 +131,13 @@ function addDevDomainsToEtcHosts() {
 	log "Domain added to /etc/hosts" green
 }
 
+function increaseFsInotifyMaxUserInstances() {
+    if [[ "$VM_DRIVER" != "none" ]]; then
+        minikube ssh -- "sudo sysctl -w fs.inotify.max_user_instances=524288"
+        log "fs.inotify.max_user_instances is increased" green
+    fi
+}
+
 function start() {
     checkMinikubeVersion
 
@@ -159,6 +166,8 @@ function start() {
 
     # Adding domains to /etc/hosts files
     addDevDomainsToEtcHosts "apiserver.${MINIKUBE_DOMAIN} console.${MINIKUBE_DOMAIN} catalog.${MINIKUBE_DOMAIN} instances.${MINIKUBE_DOMAIN} dex.${MINIKUBE_DOMAIN} docs.${MINIKUBE_DOMAIN} lambdas-ui.${MINIKUBE_DOMAIN} ui-api.${MINIKUBE_DOMAIN} minio.${MINIKUBE_DOMAIN} jaeger.${MINIKUBE_DOMAIN} grafana.${MINIKUBE_DOMAIN}  configurations-generator.${MINIKUBE_DOMAIN} gateway.${MINIKUBE_DOMAIN} connector-service.${MINIKUBE_DOMAIN}"
+
+    increaseFsInotifyMaxUserInstances
 }
 
 start
