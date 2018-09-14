52f7610bef8c9831e22ea76e67f60d47e32e71b4
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2018-09-14 12:48:17 +0200

    Switched minikube bootstrapper back to localkube

--- installation/scripts/minikube.sh
+++ installation/scripts/minikube.sh
@@ -145,13 +145,15 @@ function start() {
     minikube start \
     --memory 8192 \
     --cpus 4 \
-    --extra-config=apiserver.authorization-mode=RBAC \
-    --extra-config=apiserver.cors-allowed-origins="." \
-    --extra-config=apiserver.admission-control="LimitRanger,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota" \
+    --extra-config=apiserver.Authorization.Mode=RBAC \
+    --extra-config=apiserver.GenericServerRunOptions.CorsAllowedOriginList=".*" \
+    --extra-config=controller-manager.ClusterSigningCertFile="/var/lib/localkube/certs/ca.crt" \
+	--extra-config=controller-manager.ClusterSigningKeyFile="/var/lib/localkube/certs/ca.key" \
+    --extra-config=apiserver.Admission.PluginNames="LimitRanger,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota" \
     --kubernetes-version=v$KUBERNETES_VERSION \
     --vm-driver=$VM_DRIVER \
     --feature-gates="MountPropagation=false" \
-    -b=kubeadm
+    -b=localkube
 
     waitForMinikubeToBeUp
 
