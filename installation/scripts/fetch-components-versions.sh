bab18aa9819eafdcb26135013920fdd47ecdf35e
Krystian Cieslik <crabtree@users.noreply.github.com>
2018-09-14 09:18:40 +0200

    curl output param

--- installation/scripts/fetch-components-versions.sh
+++ installation/scripts/fetch-components-versions.sh
@@ -7,7 +7,7 @@ VERSIONS_FILE_PATH="$( cd "$( dirname "${CURRENT_DIR}" )" && pwd )/versions.env"
 
 if [ ! -f ${VERSIONS_FILE_PATH} ]; then
     echo -e "\nDownloading versions.env file."
-    wget -o "${VERSIONS_FILE_PATH}" "${VERSIONS_FILE_URL}" 2>/dev/null || curl -o "${VERSIONS_FILE_PATH}" "${VERSIONS_FILE_URL}"
+    wget -O "${VERSIONS_FILE_PATH}" "${VERSIONS_FILE_URL}" 2>/dev/null || curl -o "${VERSIONS_FILE_PATH}" "${VERSIONS_FILE_URL}"
 else
     echo -e "\nFile ${VERSIONS_FILE_PATH} exists, reusing."
 fi
