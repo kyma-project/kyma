f181e503378c54ba070dd2b27c5006f9b70b10db
Karol Furgol <furgolkarol@gmail.com>
2018-09-19 13:16:18 +0200

    changed echo to more precise one, replaced git log command with simpler one

--- components/installer/scripts/extract-commit-hash.sh
+++ components/installer/scripts/extract-commit-hash.sh
@@ -4,11 +4,11 @@ NUMBER_OF_PARENTS=$(git show -q --format=%P | wc -w)
 
 if [ $NUMBER_OF_PARENTS -eq 1 ]
 then
-    echo $(git log -1 --format=%H)
+    echo $(git rev-parse HEAD)
 elif [ $NUMBER_OF_PARENTS -eq 2 ]
 then
     echo $(git rev-parse HEAD^2)
 else
-    echo There\'s more than two parents of commit $NUMBER_OF_PARENTS.
+    echo Can\'t resolve valid commit hash, there are $NUMBER_OF_PARENTS parents of HEAD \(should be 1 or 2\).
     exit 1
 fi
