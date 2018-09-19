59c2bd6c0ddd360d696e0590530ce54af01d7a07
Magda Strek <magdalena.strek@sap.com>
2018-09-19 07:46:57 +0200

    Removing unknown PROVISIONING_DIR variable

--- resources/helm-broker-repo/install.sh
+++ resources/helm-broker-repo/install.sh
@@ -3,5 +3,7 @@
 NAMESPACE="istio-system"
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-bash ${PROVISIONING_DIR}/../../installation/scripts/is-ready.sh ${NAMESPACE} istio sidecar-injector
+set -o errexit
+
+bash ${CURRENT_DIR}/../../installation/scripts/is-ready.sh ${NAMESPACE} istio sidecar-injector
 bash ${CURRENT_DIR}/provisioning/provision-bundles.sh ${CURRENT_DIR}/bundles
