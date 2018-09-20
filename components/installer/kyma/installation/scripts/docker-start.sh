bf86f2d8db628ff014628781d4f20d57c68b27f2
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2018-09-20 15:55:35 +0200

    going forward...

--- components/installer/kyma/installation/scripts/docker-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-service docker start
--- components/installer/kyma/resources/helm-broker-repo/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-NAMESPACE="istio-system"
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-set -o errexit
-
-bash ${CURRENT_DIR}/../../installation/scripts/is-ready.sh ${NAMESPACE} istio sidecar-injector
-bash ${CURRENT_DIR}/provisioning/provision-bundles.sh ${CURRENT_DIR}/bundles
