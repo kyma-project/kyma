4fddbc2ad358341e5fd9341321528ac47181bc3f
Jan Medrek <jan.medrek@sap.com>
2018-09-20 17:51:00 +0200

    Review fixes for integration tests

--- tests/metadata-service-tests/test/apitests/metadata_test.go
+++ tests/metadata-service-tests/test/apitests/metadata_test.go
@@ -91,6 +91,47 @@ func TestApiMetadata(t *testing.T) {
 			require.Equal(t, http.StatusNoContent, statusCode)
 		})
 
+		t.Run("should register service (overriding connected-app label)", func(t *testing.T) {
+			// when
+			initialServiceDefinition.Labels = map[string]string{"connected-app": "different-re"}
+			statusCode, postResponseData, err := metadataServiceClient.CreateService(t, initialServiceDefinition)
+			require.NoError(t, err)
+
+			// then
+			require.Equal(t, http.StatusOK, statusCode)
+			require.NotEmpty(t, postResponseData.ID)
+
+			// when
+			statusCode, receivedServiceDefinition, err := metadataServiceClient.GetService(t, postResponseData.ID)
+			require.NoError(t, err)
+
+			// then
+			require.Equal(t, http.StatusOK, statusCode)
+			require.EqualValues(t, hideClientCredentials(initialServiceDefinition), *receivedServiceDefinition)
+
+			// when
+			statusCode, existingServices, err := metadataServiceClient.GetAllServices(t)
+			require.NoError(t, err)
+
+			postedService := findPostedService(existingServices, postResponseData.ID)
+
+			// then
+			require.Equal(t, http.StatusOK, statusCode)
+			require.NotNil(t, postedService)
+			require.Equal(t, "test service", postedService.Name)
+			require.Equal(t, "service provider", postedService.Provider)
+			require.Equal(t, "service description", postedService.Description)
+			require.Equal(t, "service identifier", postedService.Identifier)
+			require.Equal(t, map[string]string{"connected-app": "dummy-re"}, postedService.Labels)
+
+			// clean up
+			statusCode, err = metadataServiceClient.DeleteService(t, postResponseData.ID)
+			require.NoError(t, err)
+			initialServiceDefinition.Labels = map[string]string{}
+
+			require.Equal(t, http.StatusNoContent, statusCode)
+		})
+
 		t.Run("should update service (with API, Events catalog, Documentation)", func(t *testing.T) {
 			// given
 			postStatusCode, postResponseData, err := metadataServiceClient.CreateService(t, initialServiceDefinition)
