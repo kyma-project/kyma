965e9ffad6530717fbc9a0319ce0099e0b2469fd
Szymon Gibala <szymon.gibala@sap.com>
2018-09-21 07:41:27 +0200

    Updated RE client

--- tests/remote-environment-controller-tests/test/testkit/k8sresources_client.go
+++ tests/remote-environment-controller-tests/test/testkit/k8sresources_client.go
@@ -1,7 +1,7 @@
 package testkit
 
 import (
-	"github.com/kyma-project/kyma/components/remote-environment-broker/pkg/apis/remoteenvironment/v1alpha1"
+	"github.com/kyma-project/kyma/components/remote-environment-broker/pkg/apis/applicationconnector/v1alpha1"
 	"github.com/kyma-project/kyma/components/remote-environment-broker/pkg/client/clientset/versioned"
 	v1apps "k8s.io/api/apps/v1"
 	v1core "k8s.io/api/core/v1"
@@ -80,14 +80,13 @@ func (c *k8sResourcesClient) CreateDummyRemoteEnvironment(name string) (*v1alpha
 		TypeMeta:   v1.TypeMeta{Kind: "RemoteEnvironment", APIVersion: v1alpha1.SchemeGroupVersion.String()},
 		ObjectMeta: v1.ObjectMeta{Name: name, Namespace: c.namespace},
 		Spec: v1alpha1.RemoteEnvironmentSpec{
-			Source:   v1alpha1.Source{Environment: "test", Type: "test", Namespace: c.namespace},
 			Services: []v1alpha1.Service{},
 		},
 	}
 
-	return c.remoteEnvironmentClient.RemoteenvironmentV1alpha1().RemoteEnvironments().Create(dummyRe)
+	return c.remoteEnvironmentClient.ApplicationconnectorV1alpha1().RemoteEnvironments().Create(dummyRe)
 }
 
 func (c *k8sResourcesClient) DeleteRemoteEnvironment(name string, options *v1.DeleteOptions) error {
-	return c.remoteEnvironmentClient.RemoteenvironmentV1alpha1().RemoteEnvironments().Delete(name, options)
+	return c.remoteEnvironmentClient.ApplicationconnectorV1alpha1().RemoteEnvironments().Delete(name, options)
 }
