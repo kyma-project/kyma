c8e79413972d23683ab17e58a470db59b18dec47
Szymon Gibała <szymon.gibala@sap.com>
2018-09-21 07:52:36 +0200

    Updated RE client in Gateway (#787)

--- components/gateway/internal/metadata/remoteenv/repository.go
+++ components/gateway/internal/metadata/remoteenv/repository.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"github.com/kyma-project/kyma/components/gateway/internal/apperrors"
-	"github.com/kyma-project/kyma/components/remote-environment-broker/pkg/apis/remoteenvironment/v1alpha1"
+	"github.com/kyma-project/kyma/components/remote-environment-broker/pkg/apis/applicationconnector/v1alpha1"
 	log "github.com/sirupsen/logrus"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
