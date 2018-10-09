37da9fef22b355f8938cfe6c6d009c4084e516e0
Szymon Gibala <szymon.gibala@sap.com>
2018-10-09 11:18:58 +0200

    Setting Access Label in RE Controller

--- components/remote-environment-controller/pkg/controller/reconciler.go
+++ components/remote-environment-controller/pkg/controller/reconciler.go
@@ -12,11 +12,16 @@ import (
 )
 
 const (
-	reChartDirectory = "remote-environments"
+	reChartDirectory   = "remote-environments"
+	updateRetriesCount = 5
 )
 
 type ManagerClient interface {
 	Get(ctx context.Context, key client.ObjectKey, obj runtime.Object) error
+}
+
+type RemoteEnvironmentClient interface {
+	Update(*v1alpha1.RemoteEnvironment) (*v1alpha1.RemoteEnvironment, error)
 }
 
 type RemoteEnvironmentReconciler interface {
@@ -28,12 +33,14 @@ type remoteEnvironmentReconciler struct {
 	helmClient kymahelm.HelmClient
 	overrides  string
 	namespace  string
+	reClient   RemoteEnvironmentClient
 }
 
-func NewReconciler(mgrClient ManagerClient, helmClient kymahelm.HelmClient, overrides string, namespace string) RemoteEnvironmentReconciler {
+func NewReconciler(mgrClient ManagerClient, helmClient kymahelm.HelmClient, reClient RemoteEnvironmentClient, overrides string, namespace string) RemoteEnvironmentReconciler {
 	return &remoteEnvironmentReconciler{
 		mgrClient:  mgrClient,
 		helmClient: helmClient,
+		reClient:   reClient,
 		overrides:  overrides,
 		namespace:  namespace,
 	}
@@ -50,6 +57,12 @@ func (r *remoteEnvironmentReconciler) Reconcile(request reconcile.Request) (reco
 			return reconcile.Result{}, nil
 		}
 		log.Errorf("Error getting %s Remote Environment: %s", request.Name, err.Error())
+		return reconcile.Result{}, err
+	}
+
+	err = r.ensureAccessLabel(instance)
+	if err != nil {
+		log.Errorf("Error while updating RE %s with access-label: %s", instance.Name, err.Error())
 		return reconcile.Result{}, err
 	}
 
@@ -82,7 +95,7 @@ func (r *remoteEnvironmentReconciler) installNewREChart(name string) {
 func (r *remoteEnvironmentReconciler) deleteREChart(name string) {
 	_, err := r.helmClient.DeleteRelease(name)
 	if err != nil {
-		log.Error("Error while deleting release for %s RE: %s", name, err.Error())
+		log.Errorf("Error while deleting release for %s RE: %s", name, err.Error())
 	} else {
 		log.Infof("Release %s successfully deleted", name)
 	}
@@ -103,4 +116,20 @@ func (r *remoteEnvironmentReconciler) checkReleaseExistence(name string) (bool, 
 	}
 
 	return false, nil
+}
+
+func (r *remoteEnvironmentReconciler) ensureAccessLabel(remoteEnv *v1alpha1.RemoteEnvironment) error {
+	if remoteEnv.Spec.AccessLabel != remoteEnv.Name {
+		log.Infof("Invalid access-label, setting access-label to %s", remoteEnv.Name)
+
+		remoteEnv.Spec.AccessLabel = remoteEnv.Name
+		if remoteEnv.Spec.Services == nil {
+			remoteEnv.Spec.Services = []v1alpha1.Service{}
+		}
+
+		_, err := r.reClient.Update(remoteEnv)
+		return err
+	}
+
+	return nil
 }
