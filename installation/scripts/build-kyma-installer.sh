fabb725a2fda6d13182bb0a10150e25aa15c10a2
Magda Strek <magdalena.strek@sap.com>
2018-10-09 11:19:07 +0200

    Local installation based on kyma-installer

--- installation/scripts/build-kyma-installer.sh
+++ installation/scripts/build-kyma-installer.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-IMAGE_NAME=eu.gcr.io/kyma-project/develop/kyma-installer:63f27f76
+IMAGE_NAME="$(${CURRENT_DIR}/extract-kyma-installer-image.sh)"
 
 eval $(minikube docker-env --shell bash)
 docker build -t ${IMAGE_NAME} -f ${CURRENT_DIR}/../../kyma-installer/kyma.Dockerfile .
