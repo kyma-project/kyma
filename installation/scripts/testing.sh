fc61ce35b819f3240a0e9de9572129b6f04208d3
Jan Medrek <jan.medrek@sap.com>
2018-10-09 13:05:58 +0200

    Remove RE tests from installator

--- installation/scripts/testing.sh
+++ installation/scripts/testing.sh
@@ -213,19 +213,13 @@ istioTestErr=$?
 checkAndCleanupTest istio-system
 testCheckIstio=$?
 
-echo "- Testing Remote Environments"
-helm test ec-default
-ecTestErr=$?
-helm test hmc-default
-hmcTestErr=$?
-
 checkAndCleanupTest kyma-integration
 testCheckGateway=$?
 
 printImagesWithLatestTag
 latestTagsErr=$?
 
-if [ ${latestTagsErr} -ne 0 ] || [ ${coreTestErr} -ne 0 ]  || [ ${istioTestErr} -ne 0 ] || [ ${ecTestErr} -ne 0 ] || [ ${hmcTestErr} -ne 0 ]
+if [ ${latestTagsErr} -ne 0 ] || [ ${coreTestErr} -ne 0 ]  || [ ${istioTestErr} -ne 0 ]
 then
     exit 1
 else
