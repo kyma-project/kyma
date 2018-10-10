6fdc858f4abfdcb93f83dd3d8b5d20c83643df6e
Maladie <mariusz.maks.szymanski@gmail.com>
2018-10-10 10:30:21 +0200

    Error message adjusted to better fit to other messages in the file

--- components/metadata-service/cmd/metadataservice/metadataservice.go
+++ components/metadata-service/cmd/metadataservice/metadataservice.go
@@ -61,7 +61,7 @@ func main() {
 
 	middlewares, err := monitoring.SetupMonitoringMiddleware()
 	if err != nil {
-		log.Errorf("Middleware monitoring setup failed, %s", err.Error())
+		log.Errorf("Failed to setup middleware monitoring, %s", err.Error())
 	}
 
 	externalHandler := newExternalHandler(serviceDefinitionService, middlewares, options.detailedErrorResponse)
