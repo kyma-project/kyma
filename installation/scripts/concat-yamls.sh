22fe96ec0973d40cae9f55e01b7dac63e255a53d
Jakub Kabza <jakub.kabza@sap.com>
2018-10-15 15:46:09 +0200

    fix

--- installation/scripts/concat-yamls.sh
+++ installation/scripts/concat-yamls.sh
@@ -3,45 +3,29 @@
 set -o errexit
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-COMBO_FILE=$(mktemp ${CURRENT_DIR}/../../temp.XXXXXX)
-TMP_FILE=$(mktemp ${CURRENT_DIR}/../../temp.XXXXXX)
-
-sedPattern () {
-
-    case `uname -s` in
-        Darwin)
-            sed -i '' "$1" "$2"
-            ;;
-        *)
-            sed -i "$1" "$2"
-            ;;
-    esac
-}
 
 for file in "$@"
 do
+
+    TMP=""
+
     if [[ ! -f "${file}" ]]; then
-        echo "File '${file}' not found"
+        echo "File ${file} not found"
         continue
     fi
 
-    cat "${file}" > "${TMP_FILE}"
-    sedPattern '/^\s*$/d' "${TMP_FILE}"
+    TMP=$(cat "${file}" | sed '/^\s*$/d')
 
-    FIRST_LINE=$(head -n 1 "${TMP_FILE}")
+    FIRST_LINE=$(head -n1 <<< "${TMP}")
     if [[ "$FIRST_LINE" == "---" ]]; then
-        sedPattern '1d' ${TMP_FILE}
+        TMP=$(sed '1d' <<< "${TMP}")
     fi
 
-    LAST_LINE=$(tail -n 1 "${TMP_FILE}")
+    echo "${TMP}"
+
+    LAST_LINE=$(tail -n1 <<<"${TMP}")
     if [[ "$LAST_LINE" != "---" ]]; then
-        echo '---' >> "${TMP_FILE}"
+        echo '---'
     fi
 
-    cat "${TMP_FILE}" >> "${COMBO_FILE}"
-    
 done
-
-cat "${COMBO_FILE}"
-rm "${COMBO_FILE}"
-rm "${TMP_FILE}"
