97d327fbf601334d403acf0f25107c53161bacce
Jakub Kabza <jakub.kabza@sap.com>
2018-10-15 07:24:13 +0200

    fix

--- installation/scripts/concat-yamls.sh
+++ installation/scripts/concat-yamls.sh
@@ -10,35 +10,35 @@ sedPattern () {
 
     case `uname -s` in
         Darwin)
-            sed -i '' $1 $2
+            sed -i '' "$1" "$2"
             ;;
         *)
-            sed -i $1 $2
+            sed -i "$1" "$2"
             ;;
     esac
 }
 
 for file in "$@"
 do
-    if [[ ! -f ${file} ]]; then
-        echo "File ${file} not found"
+    if [[ ! -f "${file}" ]]; then
+        echo "File '${file}' not found"
         continue
     fi
 
-    cat ${file} > ${TMP_FILE}
-    sedPattern '/^\s*$/d' ${TMP_FILE}
+    cat "${file}" > "${TMP_FILE}"
+    sedPattern '/^\s*$/d' "${TMP_FILE}"
 
     FIRST_LINE=$(head -n 1 "${TMP_FILE}")
-    if [[ $FIRST_LINE == "---" ]]; then
+    if [[ "$FIRST_LINE" == "---" ]]; then
         sedPattern '1d' ${TMP_FILE}
     fi
 
     LAST_LINE=$(tail -n 1 "${TMP_FILE}")
-    if [[ $LAST_LINE != "---" ]]; then
-        echo '---' >> ${TMP_FILE}
+    if [[ "$LAST_LINE" != "---" ]]; then
+        echo '---' >> "${TMP_FILE}"
     fi
 
-    cat ${TMP_FILE} >> ${COMBO_FILE}
+    cat "${TMP_FILE}" >> "${COMBO_FILE}"
     
 done
 
