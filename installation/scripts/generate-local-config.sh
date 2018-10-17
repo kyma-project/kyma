1695f7b3414e6e47553ad06b39ce7b5893fe723a
Jakub Kabza <jakub.kabza@sap.com>
2018-10-17 12:11:08 +0200

    Enable Azure-Broker bug fixed

--- installation/scripts/generate-local-config.sh
+++ installation/scripts/generate-local-config.sh
@@ -3,14 +3,6 @@ set -e
 
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-##########
-
-echo -e "\nConfiguring sub-components"
-
-bash ${ROOT_PATH}/configure-components.sh
-
-##########
-
 # The following variables are optional and must be exported manually in base64 encoded format
 # before running local installation (you need them only to enable Azure Broker):
 # AZURE_BROKER_TENANT_ID,
@@ -19,7 +11,14 @@ bash ${ROOT_PATH}/configure-components.sh
 # AZURE_BROKER_CLIENT_SECRET
 
 if [ -n "${AZURE_BROKER_SUBSCRIPTION_ID}" ]; then
-  echo -e "\nGenerating secret for Azure Broker"
+
+  echo -e "\nAzure-Broker subscription ID found in environment variables. Enabling component..."
+
+  bash ${ROOT_PATH}/manage-component.sh "azure-broker" true
+
+  ##########
+
+  echo -e "\nGenerating the secret for Azure Broker..."
 
   AZURE_BROKER_TPL_PATH="${ROOT_PATH}/../resources/azure-broker-secret.yaml.tpl"
   AZURE_BROKER_OUTPUT_PATH=$(mktemp)
@@ -30,8 +29,14 @@ if [ -n "${AZURE_BROKER_SUBSCRIPTION_ID}" ]; then
   bash ${ROOT_PATH}/replace-placeholder.sh --path ${AZURE_BROKER_OUTPUT_PATH} --placeholder "__AZURE_BROKER_CLIENT_ID__" --value "${AZURE_BROKER_CLIENT_ID}"
   bash ${ROOT_PATH}/replace-placeholder.sh --path ${AZURE_BROKER_OUTPUT_PATH} --placeholder "__AZURE_BROKER_CLIENT_SECRET__" --value "${AZURE_BROKER_CLIENT_SECRET}"
 
-  echo -e "\nApplying asecret for Azure Broker"
+  echo -e "\nApplying the secret for Azure Broker..."
   kubectl apply -f "${AZURE_BROKER_OUTPUT_PATH}"
 
   rm ${AZURE_BROKER_OUTPUT_PATH}
 fi
+
+##########
+
+echo -e "\nConfiguring sub-components..."
+
+bash ${ROOT_PATH}/configure-components.sh
