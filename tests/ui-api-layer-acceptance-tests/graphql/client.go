52f8d4137ba123180f98407524233f47057c9fc5
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2018-10-31 09:38:13 +0100

    Stop using Tiller in ui-api-acceptance tests (#1434)

--- tests/ui-api-layer-acceptance-tests/graphql/client.go
+++ tests/ui-api-layer-acceptance-tests/graphql/client.go
@@ -3,13 +3,12 @@ package graphql
 import (
 	"context"
 	"crypto/tls"
-	"net"
 	"net/http"
-	"os/exec"
 	"strings"
 	"time"
 
-	"github.com/golang/glog"
+	"log"
+
 	"github.com/machinebox/graphql"
 	"github.com/pkg/errors"
 )
@@ -19,10 +18,10 @@ const (
 )
 
 type Client struct {
-	gqlClient      *graphql.Client
-	token          string
-	endpoint       string
-	clusterContext func(ctx context.Context, network, addr string) (net.Conn, error)
+	gqlClient *graphql.Client
+	token     string
+	endpoint  string
+	logs      []string
 }
 
 func New() (*Client, error) {
@@ -31,21 +30,23 @@ func New() (*Client, error) {
 		return nil, errors.Wrap(err, "while loading config")
 	}
 
-	clusterContext := dialContext(config.LocalClusterHosts)
-	token, err := authenticate(config.IdProviderConfig, clusterContext)
+	token, err := authenticate(config.IdProviderConfig)
 	if err != nil {
 		return nil, err
 	}
 
-	httpClient := newAuthorizedClient(token, clusterContext)
-	gqlClient := graphql.NewClient(config.GraphQlEndpoint, graphql.WithHTTPClient(httpClient))
+	httpClient := newAuthorizedClient(token)
+	gqlClient := graphql.NewClient(config.GraphQLEndpoint, graphql.WithHTTPClient(httpClient))
 
-	return &Client{
-		gqlClient:      gqlClient,
-		token:          token,
-		clusterContext: clusterContext,
-		endpoint:       config.GraphQlEndpoint,
-	}, nil
+	client := &Client{
+		gqlClient: gqlClient,
+		token:     token,
+		endpoint:  config.GraphQLEndpoint,
+		logs:      []string{},
+	}
+	client.gqlClient.Log = client.addLog
+
+	return client, nil
 }
 
 func (c *Client) DoQuery(q string, res interface{}) error {
@@ -57,7 +58,16 @@ func (c *Client) Do(req *Request, res interface{}) error {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	return c.gqlClient.Run(ctx, req.req, res)
+	c.clearLogs()
+	err := c.gqlClient.Run(ctx, req.req, res)
+	if err != nil {
+		for _, l := range c.logs {
+			if l != "" {
+				log.Println(l)
+			}
+		}
+	}
+	return err
 }
 
 func (c *Client) Subscribe(req *Request) *Subscription {
@@ -68,7 +78,7 @@ func (c *Client) Subscribe(req *Request) *Subscription {
 	url := strings.Replace(c.endpoint, "http://", "ws://", -1)
 	url = strings.Replace(url, "https://", "wss://", -1)
 
-	connection, err := newWebsocket(url, c.token, req.req.Header, c.clusterContext)
+	connection, err := newWebsocket(url, c.token, req.req.Header)
 	if err != nil {
 		return errorSubscription(err)
 	}
@@ -91,53 +101,22 @@ func (c *Client) Subscribe(req *Request) *Subscription {
 	return newSubscription(connection)
 }
 
-func authenticate(config idProviderConfig, dialContext func(ctx context.Context, network, addr string) (net.Conn, error)) (string, error) {
+func (c *Client) addLog(log string) {
+	c.logs = append(c.logs, log)
+}
+
+func (c *Client) clearLogs() {
+	c.logs = []string{}
+}
+
+func authenticate(config idProviderConfig) (string, error) {
 	httpClient := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			DialContext:     dialContext,
 		},
 	}
 
 	idTokenProvider := newDexIdTokenProvider(httpClient, config)
 	token, err := idTokenProvider.fetchIdToken()
 	return token, err
-}
-
-func dialContext(localClusterHosts []string) func(ctx context.Context, network, addr string) (net.Conn, error) {
-	ingressGatewayControllerAddr, err := net.LookupHost(ingressGatewayControllerServiceURL)
-	if err != nil {
-
-		glog.Warningf("Unable to resolve host '%s' (if you are running this test from outside of Kyma ignore this log). Root cause: %v", ingressGatewayControllerServiceURL, err)
-
-		if minikubeIp := tryToGetMinikubeIp(); minikubeIp != "" {
-			ingressGatewayControllerAddr = []string{minikubeIp}
-		}
-	}
-
-	return func(ctx context.Context, network, addr string) (net.Conn, error) {
-		if len(ingressGatewayControllerAddr) > 0 {
-			glog.Infof("Resolving: %s", addr)
-			for _, host := range localClusterHosts {
-				if strings.HasPrefix(addr, host) {
-					addr = strings.Replace(addr, host, ingressGatewayControllerAddr[0], 1)
-					break
-				}
-			}
-			glog.Infof("Resolved: %s", addr)
-		}
-
-		dialer := net.Dialer{}
-		return dialer.DialContext(ctx, network, addr)
-	}
-}
-
-func tryToGetMinikubeIp() string {
-	mipCmd := exec.Command("minikube", "ip")
-	if mipOut, err := mipCmd.Output(); err != nil {
-		glog.Warningf("Error while getting minikube IP (ignore this message if you are running this test inside Kyma). Root cause: %s", err)
-		return ""
-	} else {
-		return strings.Trim(string(mipOut), "\n")
-	}
 }
