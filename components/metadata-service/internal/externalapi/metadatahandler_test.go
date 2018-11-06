25d8363ebf88c8a404f12159386c4a850e9445ec
Jan Medrek <jan.medrek@sap.com>
2018-11-06 16:37:22 +0100

    Complete tests for metadata handler

--- components/metadata-service/internal/externalapi/metadatahandler_test.go
+++ components/metadata-service/internal/externalapi/metadatahandler_test.go
@@ -32,8 +32,7 @@ var (
 )
 
 func TestMetadataHandler_CreateService(t *testing.T) {
-	t.Run("should create a service", func(t *testing.T) {
-
+	t.Run("should create a service with OAuth credentials", func(t *testing.T) {
 		// given
 		serviceDetails := ServiceDetails{
 			Name:             "service name",
@@ -78,6 +77,91 @@ func TestMetadataHandler_CreateService(t *testing.T) {
 						URL:          "http://oauth.com",
 						ClientID:     "clientId",
 						ClientSecret: "clientSecret",
+					},
+				},
+				Spec: apiRawSpec,
+			},
+			Events: &metadata.Events{
+				Spec: eventsRawSpec,
+			},
+			Documentation: documentationRaw,
+		}
+
+		validator := ServiceDetailsValidatorFunc(func(sd ServiceDetails) apperrors.AppError {
+			return nil
+		})
+		serviceDefinitionService := &metadataMock.ServiceDefinitionService{}
+		serviceDefinitionService.On("Create", "re", serviceDefinition).Return("1", nil)
+
+		metadataHandler := NewMetadataHandler(validator, serviceDefinitionService, false)
+
+		serviceDetailsData, err := json.Marshal(serviceDetails)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodPost, "/re/v1/metadata/services", bytes.NewReader(serviceDetailsData))
+		require.NoError(t, err)
+
+		req = mux.SetURLVars(req, map[string]string{"remoteEnvironment": "re"})
+		rr := httptest.NewRecorder()
+
+		// when
+		metadataHandler.CreateService(rr, req)
+
+		// then
+		responseBody, err := ioutil.ReadAll(rr.Body)
+		require.NoError(t, err)
+
+		var postResponse CreateServiceResponse
+		err = json.Unmarshal(responseBody, &postResponse)
+
+		require.NoError(t, err)
+		assert.Equal(t, "1", postResponse.ID)
+		assert.Equal(t, http.StatusOK, rr.Code)
+	})
+
+	t.Run("should create a service with Basic Auth credentials", func(t *testing.T) {
+		// given
+		serviceDetails := ServiceDetails{
+			Name:             "service name",
+			Provider:         "service provider",
+			Description:      "service description",
+			ShortDescription: "service short description",
+			Identifier:       "service identifier",
+			Labels:           &map[string]string{"showcase": "true"},
+			Api: &API{
+				TargetUrl: "http://service.com",
+				Credentials: &Credentials{
+					Basic: &BasicAuth{
+						Username: "username",
+						Password: "password",
+					},
+				},
+				Spec: apiRawSpec,
+			},
+			Events: &Events{
+				Spec: eventsRawSpec,
+			},
+			Documentation: &Documentation{
+				DisplayName: "documentation name",
+				Description: "documentation description",
+				Type:        "documentation type",
+				Docs:        []DocsObject{{Title: "doc title", Type: "doc type", Source: "doc source"}},
+			},
+		}
+
+		serviceDefinition := &metadata.ServiceDefinition{
+			Name:             "service name",
+			Provider:         "service provider",
+			Description:      "service description",
+			ShortDescription: "service short description",
+			Identifier:       "service identifier",
+			Labels:           &map[string]string{"showcase": "true"},
+			Api: &serviceapi.API{
+				TargetUrl: "http://service.com",
+				Credentials: &serviceapi.Credentials{
+					Basic: &serviceapi.Basic{
+						Username: "username",
+						Password: "password",
 					},
 				},
 				Spec: apiRawSpec,
@@ -260,7 +344,7 @@ func TestMetadataHandler_CreateService(t *testing.T) {
 }
 
 func TestMetadataHandler_GetService(t *testing.T) {
-	t.Run("should return requested service", func(t *testing.T) {
+	t.Run("should return requested service with OAuth credentials", func(t *testing.T) {
 		// given
 		serviceDefinition := metadata.ServiceDefinition{
 			Name:        "service name",
@@ -314,6 +398,69 @@ func TestMetadataHandler_GetService(t *testing.T) {
 		assert.Equal(t, "http://oauth.com", serviceDetails.Api.Credentials.Oauth.URL)
 		assert.Equal(t, stars, serviceDetails.Api.Credentials.Oauth.ClientID)
 		assert.Equal(t, stars, serviceDetails.Api.Credentials.Oauth.ClientSecret)
+		assert.Equal(t, apiSpec, raw2Json(t, serviceDetails.Api.Spec))
+		assert.Equal(t, eventsSpec, raw2Json(t, serviceDetails.Events.Spec))
+		assert.Equal(t, "documentation name", serviceDetails.Documentation.DisplayName)
+		assert.Equal(t, "documentation description", serviceDetails.Documentation.Description)
+		assert.Equal(t, "documentation type", serviceDetails.Documentation.Type)
+		assert.Len(t, serviceDetails.Documentation.Docs, 1)
+		assert.Equal(t, DocsObject{Title: "doc title", Type: "doc type", Source: "doc source"}, serviceDetails.Documentation.Docs[0])
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+	})
+
+	t.Run("should return requested service with Basic Auth credentials", func(t *testing.T) {
+		// given
+		serviceDefinition := metadata.ServiceDefinition{
+			Name:        "service name",
+			Provider:    "service provider",
+			Description: "service description",
+			Api: &serviceapi.API{
+				TargetUrl: "http://service.com",
+				Credentials: &serviceapi.Credentials{
+					Basic: &serviceapi.Basic{
+						Username: "username",
+						Password: "password",
+					},
+				},
+				Spec: apiRawSpec,
+			},
+			Events: &metadata.Events{
+				Spec: eventsRawSpec,
+			},
+			Documentation: documentationRaw,
+		}
+
+		serviceDefinitionService := &metadataMock.ServiceDefinitionService{}
+		serviceDefinitionService.On("GetByID", "re", "123456").Return(serviceDefinition, nil)
+		detailedErrorResponse := false
+
+		metadataHandler := NewMetadataHandler(nil, serviceDefinitionService, detailedErrorResponse)
+
+		req, err := http.NewRequest(http.MethodGet, "/re/v1/metadata/services/123456", nil)
+		require.NoError(t, err)
+
+		req = mux.SetURLVars(req, map[string]string{"remoteEnvironment": "re", "serviceId": "123456"})
+		rr := httptest.NewRecorder()
+
+		// when
+		metadataHandler.GetService(rr, req)
+
+		// then
+		responseBody, err := ioutil.ReadAll(rr.Body)
+		require.NoError(t, err)
+
+		var serviceDetails ServiceDetails
+		err = json.Unmarshal(responseBody, &serviceDetails)
+
+		require.NoError(t, err)
+		serviceDefinitionService.AssertCalled(t, "GetByID", "re", "123456")
+		assert.Equal(t, "service name", serviceDetails.Name)
+		assert.Equal(t, "service provider", serviceDetails.Provider)
+		assert.Equal(t, "service description", serviceDetails.Description)
+		assert.Equal(t, "http://service.com", serviceDetails.Api.TargetUrl)
+		assert.Equal(t, stars, serviceDetails.Api.Credentials.Basic.Username)
+		assert.Equal(t, stars, serviceDetails.Api.Credentials.Basic.Password)
 		assert.Equal(t, apiSpec, raw2Json(t, serviceDetails.Api.Spec))
 		assert.Equal(t, eventsSpec, raw2Json(t, serviceDetails.Events.Spec))
 		assert.Equal(t, "documentation name", serviceDetails.Documentation.DisplayName)
@@ -552,7 +699,7 @@ func TestMetadataHandler_GetServices(t *testing.T) {
 }
 
 func TestMetadataHandler_UpdateService(t *testing.T) {
-	t.Run("should update a service", func(t *testing.T) {
+	t.Run("should update a service with Oauth credentials", func(t *testing.T) {
 		// given
 		serviceDetails := ServiceDetails{
 			Name:        "service name",
@@ -637,6 +784,99 @@ func TestMetadataHandler_UpdateService(t *testing.T) {
 		assert.Equal(t, "http://oauth.com", serviceDetailsResponse.Api.Credentials.Oauth.URL)
 		assert.Equal(t, stars, serviceDetailsResponse.Api.Credentials.Oauth.ClientID)
 		assert.Equal(t, stars, serviceDetailsResponse.Api.Credentials.Oauth.ClientSecret)
+		assert.Equal(t, apiSpec, raw2Json(t, serviceDetailsResponse.Api.Spec))
+		assert.Equal(t, eventsSpec, raw2Json(t, serviceDetailsResponse.Events.Spec))
+		assert.Equal(t, "documentation name", serviceDetailsResponse.Documentation.DisplayName)
+		assert.Equal(t, "documentation description", serviceDetailsResponse.Documentation.Description)
+		assert.Equal(t, "documentation type", serviceDetailsResponse.Documentation.Type)
+		assert.Len(t, serviceDetailsResponse.Documentation.Docs, 1)
+		assert.Equal(t, DocsObject{Title: "doc title", Type: "doc type", Source: "doc source"}, serviceDetailsResponse.Documentation.Docs[0])
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+	})
+
+	t.Run("should update a service with Basic Auth credentials", func(t *testing.T) {
+		// given
+		serviceDetails := ServiceDetails{
+			Name:        "service name",
+			Provider:    "service provider",
+			Description: "service description",
+			Api: &API{
+				TargetUrl: "http://service.com",
+				Credentials: &Credentials{
+					Basic: &BasicAuth{
+						Username: "username",
+						Password: "password",
+					},
+				},
+				Spec: apiRawSpec,
+			},
+			Events: &Events{
+				Spec: eventsRawSpec,
+			},
+			Documentation: &Documentation{
+				DisplayName: "documentation name",
+				Description: "documentation description",
+				Type:        "documentation type",
+				Docs:        []DocsObject{{Title: "doc title", Type: "doc type", Source: "doc source"}},
+			},
+		}
+
+		serviceDefinition := &metadata.ServiceDefinition{
+			Name:        "service name",
+			Provider:    "service provider",
+			Description: "service description",
+			Api: &serviceapi.API{
+				TargetUrl: "http://service.com",
+				Credentials: &serviceapi.Credentials{
+					Basic: &serviceapi.Basic{
+						Username: "username",
+						Password: "password",
+					},
+				},
+				Spec: apiRawSpec,
+			},
+			Events: &metadata.Events{
+				Spec: eventsRawSpec,
+			},
+			Documentation: documentationRaw,
+		}
+
+		validator := ServiceDetailsValidatorFunc(func(sd ServiceDetails) apperrors.AppError {
+			return nil
+		})
+		serviceDefinitionService := &metadataMock.ServiceDefinitionService{}
+		serviceDefinitionService.On("Update", "re", "1234", serviceDefinition).Return(*serviceDefinition, nil)
+		detailedErrorResponse := false
+
+		metadataHandler := NewMetadataHandler(validator, serviceDefinitionService, detailedErrorResponse)
+
+		serviceDetailsData, err := json.Marshal(serviceDetails)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodPut, "/re/v1/metadata/services/1234", bytes.NewReader(serviceDetailsData))
+		require.NoError(t, err)
+
+		req = mux.SetURLVars(req, map[string]string{"remoteEnvironment": "re", "serviceId": "1234"})
+		rr := httptest.NewRecorder()
+
+		// when
+		metadataHandler.UpdateService(rr, req)
+
+		// then
+		responseBody, err := ioutil.ReadAll(rr.Body)
+		require.NoError(t, err)
+
+		var serviceDetailsResponse ServiceDetails
+		err = json.Unmarshal(responseBody, &serviceDetailsResponse)
+
+		require.NoError(t, err)
+		assert.Equal(t, "service name", serviceDetailsResponse.Name)
+		assert.Equal(t, "service provider", serviceDetailsResponse.Provider)
+		assert.Equal(t, "service description", serviceDetailsResponse.Description)
+		assert.Equal(t, "http://service.com", serviceDetailsResponse.Api.TargetUrl)
+		assert.Equal(t, stars, serviceDetailsResponse.Api.Credentials.Basic.Username)
+		assert.Equal(t, stars, serviceDetailsResponse.Api.Credentials.Basic.Password)
 		assert.Equal(t, apiSpec, raw2Json(t, serviceDetailsResponse.Api.Spec))
 		assert.Equal(t, eventsSpec, raw2Json(t, serviceDetailsResponse.Events.Spec))
 		assert.Equal(t, "documentation name", serviceDetailsResponse.Documentation.DisplayName)
