208f3d949fb2102d742a830c04c26ec7bc8417bd
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2018-11-08 16:51:02 +0100

    Code review #1

--- components/proxy-service/internal/metadata/servicedefservice.go
+++ components/proxy-service/internal/metadata/servicedefservice.go
@@ -39,7 +39,7 @@ func (sds *serviceDefinitionService) GetAPI(serviceId string) (*serviceapi.API, 
 	}
 
 	if service.API == nil {
-		return nil, apperrors.WrongInput("service with ID '%s' has no API")
+		return nil, apperrors.WrongInput("service with ID '%s' has no API", serviceId)
 	}
 
 	api, err := sds.serviceAPIService.Read(service.API)
