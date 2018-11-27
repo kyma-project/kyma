7b676096266cdfef2a45a5279b55c3085f84c3ac
Michal Kempski <PolskiKiel@gmail.com>
2018-11-27 13:29:41 +0100

    Migrate acceptance tests to prow (#1759)

--- tests/acceptance/build.sh
+++ tests/acceptance/build.sh
@@ -26,6 +26,40 @@ if [ $(echo ${#fmtResult}) != 0 ]
 	else echo -e "${GREEN}√ go fmt${NC}"
 fi
 
+goFilesToCheck=$(find . -type f -name "*.go" | egrep -v "/vendor")
+##
+# GO IMPORTS & FMT
+##
+go build -o goimports-vendored ./vendor/golang.org/x/tools/cmd/goimports
+buildGoImportResult=$?
+if [ ${buildGoImportResult} != 0 ]; then
+	echo -e "${RED}✗ go build goimports${NC}\n$buildGoImportResult${NC}"
+	exit 1
+fi
+
+goImportsResult=$(echo "${goFilesToCheck}" | xargs -L1 ./goimports-vendored -w -l)
+rm goimports-vendored
+
+if [ $(echo ${#goImportsResult}) != 0 ]; then
+	echo -e "${RED}✗ goimports and fmt${NC}\n$goImportsResult${NC}"
+	exit 1
+else echo -e "${GREEN}√ goimports and fmt${NC}"
+fi
+
+##
+# GO VET
+##
+packagesToVet=$(find . -type d -maxdepth 1 | egrep -v "/vendor|/pkg" | egrep "./")
+packagesArray=(${packagesToVet//\\n/})
+for vPackage in "${packagesArray[@]}"; do
+	vetResult=$(go vet "${vPackage}/...")
+	if [ $(echo ${#vetResult}) != 0 ]; then
+		echo -e "${RED}✗ go vet ${vPackage}/... ${NC}\n$vetResult${NC}"
+		exit 1
+	else echo -e "${GREEN}√ go vet ${vPackage}/... ${NC}"
+	fi
+done
+
 # goBuild compiles the Go projects
 #
 # params:
