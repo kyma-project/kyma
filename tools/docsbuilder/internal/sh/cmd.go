d19933d683e736c35f8d18c6e63de94f031a3c8d
Paweł Kosiec <pawel.kosiec@gmail.com>
2018-12-05 16:22:13 +0100

    Add first implementation of docsbuilder

---
+++ tools/docsbuilder/internal/sh/cmd.go
@@ -0,0 +1,27 @@
+package sh
+
+import "os/exec"
+
+func RunInDir(command, dir string) (string, error) {
+	cmd := execCommand(command)
+	cmd.Dir = dir
+	return strOutput(cmd)
+}
+
+func Run(command string) (string, error) {
+	cmd := execCommand(command)
+	return strOutput(cmd)
+}
+
+func execCommand(command string) *exec.Cmd {
+	return exec.Command("/bin/sh", "-c", command)
+}
+
+func strOutput(cmd *exec.Cmd) (string, error) {
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
--- tools/docsbuilder/main.go
+++ tools/docsbuilder/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/kyma-project/kyma/tools/docsbuilder/internal/sh"
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
@@ -16,22 +17,55 @@ type Doc struct {
 func main() {
 
 	//TODO: ENVs
-	const docsDir = "docs"
-	const dockerPushRoot = "test"
+	const dockerPushRoot = "test-"
 	const dockerImageTag = "latest"
-	const dockerImageLabel = "component=docs"
-	const dockerImageSuffix = docsDir
-	const path = "../../docs/docs-build.yaml"
+	const dockerImageSuffix = "docs"
+	const docsDir = "../../docs"
 
+	docsBuildPath := fmt.Sprintf("%s/docs-build.yaml", docsDir)
+	dockerfilePath := fmt.Sprintf("%s/Dockerfile", docsDir)
 
-
-	docs, err := readDocs(path)
+	docs, err := readDocs(docsBuildPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v", docs)
+	dockerImageComponentLabel := "component=docs"
+	dockerImageVersionLabel := fmt.Sprintf("version=%s", dockerImageTag)
+	imageLabels := fmt.Sprintf("--label %s --label %s", dockerImageVersionLabel, dockerImageComponentLabel)
 
+	for _, doc := range docs {
+		imageName := fmt.Sprintf("%s%s-%s:%s", dockerPushRoot, doc.Name, dockerImageSuffix, dockerImageTag)
+		buildCommand := fmt.Sprintf("cat %s | docker build -f - . -t %s %s", dockerfilePath, imageName, imageLabels)
+
+		// build image
+		log.Printf("> Building image %s...\n\n", imageName)
+
+		docDir := doc.Name
+		if doc.Directory != "" {
+			docDir = doc.Directory
+		}
+
+		dir := fmt.Sprintf("%s/%s", docsDir, docDir)
+
+		out, err := sh.RunInDir(buildCommand, dir)
+		fmt.Print(out)
+		if err != nil {
+			log.Fatal(errors.Wrapf(err, "while executing %s", buildCommand))
+		}
+
+
+		// push image
+
+		log.Printf("> Pushing image %s...\n\n", imageName)
+		pushCommand := fmt.Sprintf("docker push %s", imageName)
+		pushOut, err := sh.Run(pushCommand)
+		fmt.Print(pushOut)
+
+		if err != nil {
+			log.Fatal(errors.Wrapf(err, "while executing %s", buildCommand))
+		}
+	}
 }
 
 func readDocs(path string) ([]Doc, error) {
