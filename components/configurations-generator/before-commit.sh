9e3d5ba942878bf547c6ddb7625223efb72fdd5d
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2018-12-12 15:51:01 +0100

    Implement suggestions

--- components/configurations-generator/before-commit.sh
+++ components/configurations-generator/before-commit.sh
@@ -33,7 +33,7 @@ if [ "$1" == "$CI_FLAG" ]; then
 	buildEnv="env CGO_ENABLED=0"
 fi
 
-${buildEnv} go build -o bin/app ./cmd/controller/
+${buildEnv} go build -o generator ./cmd/generator/
 
 goBuildResult=$?
 if [ ${goBuildResult} != 0 ]; then
@@ -57,7 +57,7 @@ fi
 # GO TEST
 ##
 echo "? go test"
-go test -short ./...
+go test ./...
 # Check if tests passed
 if [ $? != 0 ]; then
 	echo -e "${RED}✗ go test\n${NC}"
@@ -65,31 +65,23 @@ if [ $? != 0 ]; then
 else echo -e "${GREEN}√ go test${NC}"
 fi
 
+goFilesToCheck=$(find . -type f -name "*.go" | egrep -v "\/vendor\/|_*/automock/|_*/testdata/|/pkg\/|_*export_test.go")
 
-##
-# GO IMPORTS & FMT
-##
-go build -o goimports-vendored ./vendor/golang.org/x/tools/cmd/goimports
-buildGoImportResult=$?
-if [ ${buildGoImportResult} != 0 ]; then
-	echo -e "${RED}✗ go build goimports${NC}\n$buildGoImportResult${NC}"
-	exit 1
-fi
-
-goFilesToCheck=$(find . -type f -name "*.go" | egrep -v "/vendor")
-goImportsResult=$(echo "${goFilesToCheck}" | xargs -L1 ./goimports-vendored -w -l)
-rm goimports-vendored
-
-if [ $(echo ${#goImportsResult}) != 0 ]; then
-	echo -e "${RED}✗ goimports and fmt${NC}\n$goImportsResult${NC}"
-	exit 1
-else echo -e "${GREEN}√ goimports and fmt${NC}"
+#
+# GO FMT
+#
+goFmtResult=$(echo "${filesToCheck}" | xargs -L1 go fmt)
+if [ $(echo ${#goFmtResult}) != 0 ]
+	then
+    	echo -e "${RED}✗ go fmt${NC}\n$goFmtResult${NC}"
+    	exit 1;
+	else echo -e "${GREEN}√ go fmt${NC}"
 fi
 
 ##
 # GO VET
 ##
-packagesToVet=($(go list ./... | grep -v /vendor/ | grep -v api-controller/pkg/clients ))
+packagesToVet=("./cmd/..." "./pkg/...")
 
 for vPackage in "${packagesToVet[@]}"; do
 	vetResult=$(go vet ${vPackage})
@@ -99,18 +91,3 @@ for vPackage in "${packagesToVet[@]}"; do
 	else echo -e "${GREEN}√ go vet ${vPackage} ${NC}"
 	fi
 done
-
-##
-# INFO.JSON
-##
-author=$(git show -s --pretty=%an)
-# the branch calcualtion looks as follow, because the git checkout process works differently on jenkins and our local machine
-branch=${GIT_BRANCH}
-if [$branch == ""]; then
-	branch=$(git rev-parse --abbrev-ref HEAD)
-fi
-commit=$(git rev-parse --verify HEAD)
-commitMsg=$(git show -s --pretty=%s)
-commitDate=$(git log -1 --format=%cd)
-printf "{\n\t\"author\": \"""$author""\",\n\t\"commit\": \""$commit"\",\n\t\"branch\": \""$branch"\",\n\t\"commitDate\":\"""$commitDate""\",\n\t\"commitMessage\":\"""$commitMsg""\",\n\t\"deployDate\": \"""$(date)""\"\n}" >info.json
-echo -e "${GREEN}√ info.json ${NC}" $(cat info.json)
