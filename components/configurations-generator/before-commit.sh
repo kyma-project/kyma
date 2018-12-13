714aadc9f5310e9c8da020dc307dfbf2ab09782a
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2018-12-13 11:55:20 +0100

    Fix binary name

--- components/configurations-generator/before-commit.sh
+++ components/configurations-generator/before-commit.sh
@@ -33,7 +33,7 @@ if [ "$1" == "$CI_FLAG" ]; then
 	buildEnv="env CGO_ENABLED=0"
 fi
 
-${buildEnv} go build -o generator ./cmd/generator/
+${buildEnv} go build -o bin/app ./cmd/generator/
 
 goBuildResult=$?
 if [ ${goBuildResult} != 0 ]; then
