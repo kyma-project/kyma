2cbbbb66237cb359c20e982ba70daafcd70556a2
Jakub Kabza <jakub.kabza@sap.com>
2018-12-20 11:09:22 +0100

    init

--- installation/scripts/configure-components.sh
+++ installation/scripts/configure-components.sh
@@ -3,8 +3,9 @@
 set -o errexit
 
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-FILE_NAME="components.env"
-FILE_PATH=${ROOT_PATH}/../${FILE_NAME}
+
+readonly FILE_NAME="components.env"
+readonly FILE_PATH=${ROOT_PATH}/../${FILE_NAME}
 
 # Do nothing if the components.env is empty or does not exist at all
 if [[ ! -f "${FILE_PATH}" || ! -s "${FILE_PATH}" ]]; then
@@ -13,3 +14,5 @@ fi
 
 kubectl create configmap "kyma-sub-components" --from-env-file=${FILE_PATH} -n kyma-installer
 kubectl label configmap "kyma-sub-components" -n kyma-installer "installer=overrides"
+
+rm -rf "$FILE_PATH"
