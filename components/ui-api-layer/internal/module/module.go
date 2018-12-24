3981652e93e76892974b208de291424c916d9af6
Paweł Kosiec <pawel.kosiec@gmail.com>
2018-12-24 12:33:57 +0100

    Write logic for BackendModule controller

--- components/ui-api-layer/internal/module/module.go
+++ components/ui-api-layer/internal/module/module.go
@@ -1,13 +1,8 @@
 package module
 
 import (
-	"github.com/pkg/errors"
-	"log"
-	"time"
-
 	"github.com/golang/glog"
-	"github.com/kyma-project/kyma/components/ui-api-layer/pkg/apis/ui/v1alpha1"
-	"k8s.io/client-go/rest"
+	"github.com/pkg/errors"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -20,74 +15,21 @@ type PluggableModule interface {
 
 func MakePluggableFunc(informer cache.SharedIndexInformer, pluggabilityEnabled bool) func(PluggableModule) {
 	return func(module PluggableModule) {
-
 		if !pluggabilityEnabled {
 			err := module.Enable()
-			glog.Error(errors.Wrapf(err, "while enabling module %s", module.Name()))
+			printModuleErrorIfShould(err, module, "enabling")
 			return
 		}
 
-		glog.Infof("Enable module pluggability: %s", module.Name())
-		store := informer.GetStore()
-		informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				resource, ok := obj.(*v1alpha1.BackendModule)
-				if !ok {
-					glog.Error("Incorrect item type: %T, should be: *BackendModule", resource)
-					return
-				}
-
-				moduleName := module.Name()
-				if resource.Name != moduleName {
-					return
-				}
-
-				isEnabled := module.IsEnabled()
-				if isEnabled {
-					return
-				}
-
-				if resource.Name == name && !isEnabled {
-					err := module.Enable()
-
-				}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				glog.Infof("UpdateFunc \n %+v \n\n %+v", oldObj, newObj)
-				//TODO: Is there a need to handle update event?
-			},
-			DeleteFunc: func(obj interface{}) {
-				glog.Infof("DeleteFunc %+v", obj)
-				//TODO: Is the deleted object is already deleted from store, or it needs to be filtered?
-				req, err := isModuleRequired(store, module.Name())
-				if err != nil {
-					log.Fatal("Update Module Informer", err)
-				}
-				if req {
-					module.Enable()
-				} else {
-					module.Disable()
-				}
-			},
-		})
+		glog.Infof("Enabling module pluggability for %s", module.Name())
+		eventHandler := newEventHandler(module)
+		informer.AddEventHandler(eventHandler)
 	}
 }
 
-func isModuleRequired(store cache.Store, moduleName string) (bool, error) {
-	_, exists, err := store.GetByKey(moduleName)
-	if err != nil {
-		return false, err
+func printModuleErrorIfShould(err error, module PluggableModule, operationType string) {
+	if err == nil {
+		return
 	}
-
-	return exists, nil
-}
-
-func NewInformerFactory(restConfig *rest.Config, informerResyncPeriod time.Duration) (externalversions.SharedInformerFactory, error) {
-	clientset, err := versioned.NewForConfig(restConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	factory := externalversions.NewSharedInformerFactory(clientset, informerResyncPeriod)
-	return factory, nil
+	glog.Error(errors.Wrapf(err, "while %s module %s", operationType, module.Name()))
 }
