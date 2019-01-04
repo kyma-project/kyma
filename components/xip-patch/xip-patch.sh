67ad56727f61699fd5c93ed098d29b377f7f01b2
Karol Furgol <furgolkarol@gmail.com>
2019-01-04 13:41:12 +0100

    application-connector fix for xip-io

--- components/xip-patch/xip-patch.sh
+++ components/xip-patch/xip-patch.sh
@@ -2,6 +2,11 @@
 
 set -o errexit
 
+if [ -z "${CONNECTOR_IP}" ]; then
+  echo "Connector IP address not provided"
+  exit 1
+fi
+
 if [ -z "${EXTERNAL_PUBLIC_IP}" ]; then
 
   SECONDS=0
@@ -28,26 +33,30 @@ fi
 
 XIP_PATCH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DOMAIN="${EXTERNAL_PUBLIC_IP}.xip.io"
+PUBLIC_DOMAIN="${EXTERNAL_PUBLIC_IP}.xip.io"
+CONNECTOR_DOMAIN="${CONNECTOR_IP}.xip.io"
 
 CERT_PATH="${XIP_PATCH_DIR}/cert.pem"
 KEY_PATH="${XIP_PATCH_DIR}/key.pem"
 
 openssl req -x509 -nodes -days 5 -newkey rsa:4069 \
-                 -subj "/CN=${DOMAIN}" \
+                 -subj "/CN=${PUBLIC_DOMAIN}" \
                  -reqexts SAN -extensions SAN \
                  -config <(cat /etc/ssl/openssl.cnf \
-        <(printf "\\n[SAN]\\nsubjectAltName=DNS:*.%s" "${DOMAIN}")) \
+        <(printf "\\n[SAN]\\nsubjectAltName=DNS:*.%s" "${PUBLIC_DOMAIN}")) \
                  -keyout "${KEY_PATH}" \
                  -out "${CERT_PATH}"
 
 TLS_CERT=$(base64 "${CERT_PATH}" | tr -d '\n')
 TLS_KEY=$(base64 "${KEY_PATH}" | tr -d '\n')
 
-DOMAIN_YAML=$(cat << EOF
+rm "${CERT_PATH}"
+rm "${KEY_PATH}"
+
+PUBLIC_DOMAIN_YAML=$(cat << EOF
 ---
 data:
-  global.domainName: "${DOMAIN}"
+  global.domainName: "${PUBLIC_DOMAIN}"
 EOF
 )
 
@@ -66,9 +75,24 @@ data:
 EOF
 )
 
-kubectl patch configmap installation-config-overrides --patch "${DOMAIN_YAML}" -n kyma-installer
+kubectl patch configmap installation-config-overrides --patch "${PUBLIC_DOMAIN_YAML}" -n kyma-installer
 kubectl patch configmap cluster-certificate-overrides --patch "${TLS_CERT_AND_KEY_YAML}" -n kyma-installer
 kubectl patch secret ingress-tls-cert --patch "${TLS_CERT_YAML}" -n kyma-system
 
-rm "${CERT_PATH}"
-rm "${KEY_PATH}"
+CONFIGMAP=$(cat << EOF
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: connector-service-xip-tls-overrides
+  namespace: kyma-installer
+  labels:
+    installer: overrides
+    component: application-connector
+data:
+  "global.connectorDomainName": "${CONNECTOR_DOMAIN}"
+  "connector-service.tests.skipSslVerify": "true"
+EOF
+)
+
+echo "${CONFIGMAP}" | kubectl create -f -
