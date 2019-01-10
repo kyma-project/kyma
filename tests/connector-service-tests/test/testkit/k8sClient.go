5328762a8c1908c3fdffe0dbead18d90661435be
Franciszek Pogodziński <32485355+franpog859@users.noreply.github.com>
2019-01-10 11:59:17 +0100

    Skip Application charts installation in acceptance tests if not needed (#2154)
    * Rename dummyRe to dummyApp
    
    * Skip app chart installation in connector-service and application-registry
    
    * Add random postfix for connector test pods names
    
    * Dep ensure connector-tests
    
    * Dep ensure with dep 0-4-1
    
    * Add random postfix to app registry test apps
    
    * Go fmt changed files
    
    * Add skip chart installation for apps in acceptance tests
    
    * Add skip all charts installation for apps in acceptance tests
    
    * Rename RE to Application

--- tests/connector-service-tests/test/testkit/k8sClient.go
+++ tests/connector-service-tests/test/testkit/k8sClient.go
@@ -1,14 +1,16 @@
 package testkit
 
 import (
+	"fmt"
 	"github.com/kyma-project/kyma/components/application-operator/pkg/apis/applicationconnector/v1alpha1"
 	"github.com/kyma-project/kyma/components/application-operator/pkg/client/clientset/versioned"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
 	restclient "k8s.io/client-go/rest"
 )
 
 type K8sResourcesClient interface {
-	CreateDummyApplication(name string, accessLabel string) (*v1alpha1.Application, error)
+	CreateDummyApplication(namePrefix string, accessLabel string, skipInstallation bool) (*v1alpha1.Application, error)
 	DeleteApplication(name string, options *v1.DeleteOptions) error
 }
 type k8sResourcesClient struct {
@@ -22,6 +24,7 @@ func NewK8sResourcesClient() (K8sResourcesClient, error) {
 	}
 	return initClient(k8sConfig)
 }
+
 func initClient(k8sConfig *restclient.Config) (K8sResourcesClient, error) {
 	applicationsClientSet, err := versioned.NewForConfig(k8sConfig)
 	if err != nil {
@@ -31,17 +34,29 @@ func initClient(k8sConfig *restclient.Config) (K8sResourcesClient, error) {
 		applicationsClient: applicationsClientSet,
 	}, nil
 }
-func (c *k8sResourcesClient) CreateDummyApplication(name string, accessLabel string) (*v1alpha1.Application, error) {
+
+func (c *k8sResourcesClient) CreateDummyApplication(namePrefix string, accessLabel string, skipInstallation bool) (*v1alpha1.Application, error) {
+	spec := v1alpha1.ApplicationSpec{
+		Services:         []v1alpha1.Service{},
+		AccessLabel:      accessLabel,
+		SkipInstallation: skipInstallation,
+	}
+
+	dummyAppName := addRandomPostfix(namePrefix)
+
 	dummyApp := &v1alpha1.Application{
 		TypeMeta:   v1.TypeMeta{Kind: "Application", APIVersion: v1alpha1.SchemeGroupVersion.String()},
-		ObjectMeta: v1.ObjectMeta{Name: name},
-		Spec: v1alpha1.ApplicationSpec{
-			Services:    []v1alpha1.Service{},
-			AccessLabel: accessLabel,
-		},
+		ObjectMeta: v1.ObjectMeta{Name: dummyAppName},
+		Spec:       spec,
 	}
+
 	return c.applicationsClient.ApplicationconnectorV1alpha1().Applications().Create(dummyApp)
 }
+
+func addRandomPostfix(s string) string {
+	return fmt.Sprintf(s+"-%s", rand.String(5))
+}
+
 func (c *k8sResourcesClient) DeleteApplication(name string, options *v1.DeleteOptions) error {
 	return c.applicationsClient.ApplicationconnectorV1alpha1().Applications().Delete(name, options)
 }
