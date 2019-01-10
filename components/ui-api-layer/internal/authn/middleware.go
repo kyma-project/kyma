2086198add804511db8c281b2424bf2e58fe3785
Szymon Janota <szymon.janota@sap.com>
2019-01-10 08:30:51 +0100

    Read token from sec-websocket-protocol

--- components/ui-api-layer/internal/authn/middleware.go
+++ components/ui-api-layer/internal/authn/middleware.go
@@ -19,6 +19,11 @@ func AuthMiddleware(a authenticator.Request) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+			wsToken := r.Header.Get("sec-websocket-protocol")
+			if wsToken != "" {
+				r.Header.Set("authorization", "Bearer "+wsToken)
+			}
+
 			u, ok, err := a.AuthenticateRequest(r)
 			if err != nil {
 				glog.Errorf("Unable to authenticate the request due to an error: %v", err)
