2fbfd56f69f6759d1505c1b94921e0eaa9a7ff1e
Szymon Gibala <szymon.gibala@sap.com>
2019-01-18 11:12:29 +0100

    Added unit tests to handler

--- components/application-proxy/internal/authorization/certificategenstrategy.go
+++ components/application-proxy/internal/authorization/certificategenstrategy.go
@@ -1,10 +1,7 @@
 package authorization
 
 import (
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
 	"net/http"
 	"net/http/httputil"
 
@@ -26,7 +23,7 @@ func newCertificateGenStrategy(certificate, privateKey []byte) certificateGenStr
 func (b certificateGenStrategy) AddAuthorization(r *http.Request, proxy *httputil.ReverseProxy) apperrors.AppError {
 	cert, err := b.prepareCertificate()
 	if err != nil {
-		return err
+		return apperrors.Internal("Failed to prepare certificate, %s", err.Error())
 	}
 
 	transport := &http.Transport{
@@ -43,28 +40,6 @@ func (b certificateGenStrategy) AddAuthorization(r *http.Request, proxy *httputi
 func (b certificateGenStrategy) Invalidate() {
 }
 
-func (b certificateGenStrategy) prepareCertificate() (tls.Certificate, apperrors.AppError) {
-	key, err := parseKey(b.privateKey)
-	if err != nil {
-		return tls.Certificate{}, apperrors.Internal("Failed to parse private key, %s", err.Error())
-	}
-
-	certificate, _ := pem.Decode(b.certificate)
-	if certificate == nil {
-		return tls.Certificate{}, apperrors.Internal("Empty certificate pem block")
-	}
-
-	return tls.Certificate{
-		PrivateKey:  key,
-		Certificate: [][]byte{certificate.Bytes},
-	}, nil
-}
-
-func parseKey(encodedData []byte) (*rsa.PrivateKey, error) {
-	pemBlock, _ := pem.Decode(encodedData)
-	if pemBlock == nil {
-		return nil, apperrors.Internal("Empty key pem block")
-	}
-
-	return x509.ParsePKCS1PrivateKey(pemBlock.Bytes)
+func (b certificateGenStrategy) prepareCertificate() (tls.Certificate, error) {
+	return tls.X509KeyPair(b.certificate, b.privateKey)
 }
--- components/application-registry/internal/metadata/certificates/model.go
+++ components/application-registry/internal/metadata/certificates/model.go
@@ -1,29 +1,6 @@
 package certificates
 
-import "crypto/x509/pkix"
-
 type KeyCertPair struct {
 	PrivateKey  []byte
 	Certificate []byte
-}
-
-// TODO - probably can be removed
-type Subject struct {
-	CommonName         string
-	Country            string
-	Organization       string
-	OrganizationalUnit string
-	Locality           string
-	Province           string
-}
-
-func (s Subject) asCertificateSubject() pkix.Name {
-	return pkix.Name{
-		CommonName:         s.CommonName,
-		Country:            []string{s.Country},
-		Organization:       []string{s.Organization},
-		OrganizationalUnit: []string{s.OrganizationalUnit},
-		Locality:           []string{s.Locality},
-		Province:           []string{s.Province},
-	}
 }
