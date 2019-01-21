34b1716ef199a8b836e17953f3e5fb3f554bedc2
Szymon Gibala <szymon.gibala@sap.com>
2019-01-21 15:26:32 +0100

    Added flags

--- components/connector-service/cmd/connectorservice/options.go
+++ components/connector-service/cmd/connectorservice/options.go
@@ -15,6 +15,8 @@ type options struct {
 	tokenExpirationMinutes int
 	domainName             string
 	connectorServiceHost   string
+	group                  string
+	tenant                 string
 }
 
 type environment struct {
@@ -34,6 +36,8 @@ func parseArgs() *options {
 	tokenExpirationMinutes := flag.Int("tokenExpirationMinutes", 5, "Time to Live of tokens expressed in minutes.")
 	domainName := flag.String("domainName", ".wormhole.cluster.kyma.cx", "Domain name used for URL generation.")
 	connectorServiceHost := flag.String("connectorServiceHost", "cert-service.wormhole.cluster.kyma.cx", "Host at which this service is accessible.")
+	group := flag.String("group", "", "Default group")
+	tenant := flag.String("tenant", "", "Default tenant")
 
 	flag.Parse()
 
@@ -51,8 +55,8 @@ func parseArgs() *options {
 
 func (o *options) String() string {
 	return fmt.Sprintf("--appName=%s --externalAPIPort=%d --internalAPIPort=%d --namespace=%s --tokenLength=%d "+
-		"--tokenExpirationMinutes=%d --domainName=%s --connectorServiceHost=%s", o.appName, o.externalAPIPort,
-		o.internalAPIPort, o.namespace, o.tokenLength, o.tokenExpirationMinutes, o.domainName, o.connectorServiceHost)
+		"--tokenExpirationMinutes=%d --domainName=%s --connectorServiceHost=%s --group=%s --tenant=%s", o.appName, o.externalAPIPort,
+		o.internalAPIPort, o.namespace, o.tokenLength, o.tokenExpirationMinutes, o.domainName, o.connectorServiceHost, o.group, o.tenant)
 }
 
 func parseEnv() *environment {
