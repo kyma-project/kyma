528d9959962941277d5f3031ef720532cd464379
Jan Medrek <jan.medrek@sap.com>
2019-01-23 12:26:47 +0100

    Initial commit

---
+++ tests/end-to-end/external-solution-integration/testkit/k8s_resources_check.go
@@ -0,0 +1,66 @@
+package testkit
+
+import (
+	"fmt"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	ingressNameFormat                 = "%s-application"
+	applicationProxyDeploymentFormat  = "%s-application-proxy"
+	applicationProxyRoleFormat        = "%s-application-proxy-role"
+	applicationProxyRoleBindingFormat = "%s-application-proxy-rolebinding"
+	applicationProxySvcFormat         = "%s-application-proxy-external-api"
+	eventServiceDeploymentFormat      = "%s-event-service"
+	eventServiceSvcFormat             = "%s-event-service-external-api"
+)
+
+type k8sResource struct {
+	name        string
+	kind        string
+	getFunction func(string, v1.GetOptions) (interface{}, error)
+}
+
+func newResource(name string, kind string, getFunc func(string, v1.GetOptions) (interface{}, error)) k8sResource {
+	return k8sResource{
+		name:        name,
+		kind:        kind,
+		getFunction: getFunc,
+	}
+}
+
+type K8sResourceChecker struct {
+	k8sClient K8sResourcesClient
+	appName   string
+
+	resources []k8sResource
+}
+
+func NewK8sChecker(client K8sResourcesClient, appName string) *K8sResourceChecker {
+	resources := []k8sResource{
+		newResource(fmt.Sprintf(ingressNameFormat, appName), "ingress", client.GetIngress),
+		newResource(fmt.Sprintf(applicationProxyDeploymentFormat, appName), "deployment", client.GetDeployment),
+		newResource(fmt.Sprintf(applicationProxyRoleFormat, appName), "role", client.GetRole),
+		newResource(fmt.Sprintf(applicationProxyRoleBindingFormat, appName), "ingress", client.GetRoleBinding),
+		newResource(fmt.Sprintf(applicationProxySvcFormat, appName), "ingress", client.GetService),
+		newResource(fmt.Sprintf(eventServiceDeploymentFormat, appName), "ingress", client.GetDeployment),
+		newResource(fmt.Sprintf(eventServiceSvcFormat, appName), "ingress", client.GetService),
+	}
+
+	return &K8sResourceChecker{
+		k8sClient: client,
+		appName:   appName,
+		resources: resources,
+	}
+}
+
+func (c *K8sResourceChecker) CheckK8sResources() error {
+	for _, r := range c.resources {
+		_, err := r.getFunction(r.name, v1.GetOptions{})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
