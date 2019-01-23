0c2dd7dcca86bc3d3583c6f0a41d29789c013d3b
Krystian Cieslik <crabtree@users.noreply.github.com>
2019-01-23 13:44:15 +0100

    TokenResolverMiddleware test

--- components/connector-service/internal/middlewares/tokenresolver_test.go
+++ components/connector-service/internal/middlewares/tokenresolver_test.go
@@ -16,6 +16,7 @@ import (
 const (
 	dummyKey   = "dummyKey"
 	dummyValue = "dummyValue"
+	token      = "token"
 )
 
 type dummyContextExtender struct {
@@ -29,10 +30,17 @@ func dummyExtender(token string, tokenResolver tokens.Resolver) (httpcontext.Con
 	return dummyContextExtender{}, nil
 }
 
+func notFoundExtender(token string, tokenResolver tokens.Resolver) (httpcontext.ContextExtender, apperrors.AppError) {
+	return dummyContextExtender{}, apperrors.NotFound("Not found")
+}
+
+func internalErrorExtender(token string, tokenResolver tokens.Resolver) (httpcontext.ContextExtender, apperrors.AppError) {
+	return dummyContextExtender{}, apperrors.Internal("Error")
+}
+
 func TestTokenResolver_Middleware(t *testing.T) {
 	t.Run("should resolve token and extend context", func(t *testing.T) {
-		token := "token"
-
+		// given
 		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
 			ctxValue := ctx.Value(dummyKey)
@@ -54,5 +62,68 @@ func TestTokenResolver_Middleware(t *testing.T) {
 
 		// then
 		assert.Equal(t, http.StatusOK, rr.Code)
+	})
+
+	t.Run("should return 403 when there is no token sent", func(t *testing.T) {
+		// given
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		req, err := http.NewRequest("GET", "/", nil)
+		require.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+
+		middleware := NewTokenResolverMiddleware(nil, nil)
+
+		// when
+		resultHandler := middleware.Middleware(handler)
+		resultHandler.ServeHTTP(rr, req)
+
+		// then
+		assert.Equal(t, http.StatusForbidden, rr.Code)
+	})
+
+	t.Run("should return 403 when token is not found", func(t *testing.T) {
+		// given
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		req, err := http.NewRequest("GET", "/", nil)
+		require.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+
+		middleware := NewTokenResolverMiddleware(nil, notFoundExtender)
+
+		// when
+		resultHandler := middleware.Middleware(handler)
+		resultHandler.ServeHTTP(rr, req)
+
+		// then
+		assert.Equal(t, http.StatusForbidden, rr.Code)
+	})
+
+	t.Run("should return 500 when internal error occured", func(t *testing.T) {
+		// given
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		req, err := http.NewRequest("GET", "/?token="+token, nil)
+		require.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+
+		middleware := NewTokenResolverMiddleware(nil, internalErrorExtender)
+
+		// when
+		resultHandler := middleware.Middleware(handler)
+		resultHandler.ServeHTTP(rr, req)
+
+		// then
+		assert.Equal(t, http.StatusInternalServerError, rr.Code)
 	})
 }
