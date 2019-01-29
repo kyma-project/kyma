c04780e410b3a800dc690000b3120f443bc70d64
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-01-29 16:14:36 +0100

    Introduce Bucket controller for Asset Store (#2450)
    * Initialize project
    
    * Initialize controller with Kubebuilder
    
    * Create Bucket type
    
    * Generate API for Bucket resource
    
    * Remove Minio bucket on Bucket CR delete
    
    * Define BucketHandler
    
    * Add unit tests for BucketHandler
    
    * Add policy definition to Bucket CR
    
    * Make partial implementation of reconcile
    
    * Move logic to asset-controller
    
    * Rename directory once again
    
    * Add bucketFinalizer
    
    * Refactor finalizer part for Reconcile method
    
    * Refactor Reconcile logic
    
    * Load config from env variables
    
    * Add LastHeartbeatTime
    
    * Make spec fields optional
    
    * Improve test coverage for BucketHandler
    
    * Fix test for Bucket type
    
    * Requeue if bucket is failed because of "BucketNotFound" reason
    
    * Add basic Readme
    
    * Don't requeue when error occurs, just return error
    
    * Add tests
    
    * Set bucket policy type to string
    
    * Fix bucket controller delete failed test
    
    * Separate subtests of Reconcile method
    
    * Fix BucketController along with integration tests
    
    * Add tests for Add function
    
    * Update CODEOWNERS
    
    * Make enum for bucket policy
    
    * Update sample CR
    
    * Update project name
    
    * Improve language for the Readme
    
    * Fix comparing policies

---
+++ components/assetstore-controller-manager/pkg/apis/assetstore/v1alpha1/register.go
@@ -0,0 +1,30 @@
+// NOTE: Boilerplate only.  Ignore this file.
+
+// Package v1alpha1 contains API Schema definitions for the assetstore v1alpha1 API group
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=github.com/kyma-project/kyma/components/assetstore-controller-manager/pkg/apis/assetstore
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=assetstore.kyma-project.io
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/scheme"
+)
+
+var (
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: "assetstore.kyma-project.io", Version: "v1alpha1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+
+	// AddToScheme is required by pkg/client/...
+	AddToScheme = SchemeBuilder.AddToScheme
+)
+
+// Resource is required by pkg/client/listers/...
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
