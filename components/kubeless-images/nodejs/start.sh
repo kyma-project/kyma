d15c7abc88da683a6378e1b6588ea2f23e2bd86e
Rakesh Garimella <rakesh.garimella@sap.com>
2019-02-19 16:23:54 +0100

    Updated Docker file for node8 and also start.sh

--- components/kubeless-images/nodejs/start.sh
+++ components/kubeless-images/nodejs/start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-cat /kubeless/handler.js && node kubeless.js
+awk '{gsub(/\\/,"\\\\\\\\", $0); print $0}' /kubeless/handler.js | awk '{printf "%s\\r\\n", $0}' | rev | cut -c 5- | rev
+
+if [ -e  /kubeless/package.json ]; then
+  awk '{gsub(/\\/,"\\\\\\\\", $0); print $0}' /kubeless/package.json | awk '{printf "%s\\r\\n", $0}'| rev | cut -c 5- | rev
+fi
+
+node kubeless.js
