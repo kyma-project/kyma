4b12b7bafa991e04031ad2e40c2610179671156a
Rakesh Garimella <rakesh.garimella@sap.com>
2019-02-20 11:22:13 +0100

    Some log messages to start file

--- components/kubeless-images/nodejs/start.sh
+++ components/kubeless-images/nodejs/start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+echo "Lambda Code"
 awk '{gsub(/\\/,"\\\\\\\\", $0); print $0}' /kubeless/handler.js | awk '{printf "%s\\r\\n", $0}' | rev | cut -c 5- | rev
 
+echo "\n"
 if [ -e  /kubeless/package.json ]; then
+  echo "Lambda Code dependencies"
   awk '{gsub(/\\/,"\\\\\\\\", $0); print $0}' /kubeless/package.json | awk '{printf "%s\\r\\n", $0}'| rev | cut -c 5- | rev
 fi
 
