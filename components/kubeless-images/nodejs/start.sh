4e3f54712c6ada34fb04c5423146851b0f1e79e3
Rakesh Garimella <rakesh.garimella@sap.com>
2019-02-20 13:36:35 +0100

    Updated docker files

--- components/kubeless-images/nodejs/start.sh
+++ components/kubeless-images/nodejs/start.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 echo "Lambda Code"
-awk '{gsub(/\\/,"\\\\\\\\", $0); print $0}' /kubeless/handler.js | awk '{printf "%s\\r\\n", $0}' | rev | cut -c 5- | rev
-
-echo "\n"
+awk '{gsub(/\\/,"\\\\\\\\", $0); printf "%s\\n", $0}' /kubeless/handler.js | rev | cut -c 3- | rev
+echo
 if [ -e  /kubeless/package.json ]; then
   echo "Lambda Code dependencies"
-  awk '{gsub(/\\/,"\\\\\\\\", $0); print $0}' /kubeless/package.json | awk '{printf "%s\\r\\n", $0}'| rev | cut -c 5- | rev
+  awk '{gsub(/\\/,"\\\\\\\\", $0); printf "%s\\n", $0}' /kubeless/package.json | rev | cut -c 3- | rev
 fi
 
 node kubeless.js
