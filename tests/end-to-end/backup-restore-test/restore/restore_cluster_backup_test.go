4cc00a7853a5b80338a4969ab799555a18795e01
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2019-02-20 08:38:48 +0100

    update version for modular backup tests

--- tests/end-to-end/backup-restore-test/restore/restore_cluster_backup_test.go
+++ tests/end-to-end/backup-restore-test/restore/restore_cluster_backup_test.go
@@ -1,97 +1,88 @@
 package restore
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
+	. "github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/restore/backupe2e"
+	"github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/restore/utils"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-const functionYaml = "function.yaml"
-const functionName = "hello"
-
 var testUUID = uuid.New()
-var namespace = "restore-test-" + testUUID.String()
 var backupName = "test-" + testUUID.String()
 
+type e2eTest struct {
+	backupTest BackupTest
+	namespace  string
+	testUUID   string
+}
+
 func TestBackupAndRestoreCluster(t *testing.T) {
-	Convey("Setup", t, func() {
-		Convey("Create namespace", func() {
-			So(createNamespace(namespace), ShouldContainSubstring, "created")
-			Convey("Create function", func() {
-				createFunction(functionYaml, namespace)
-				So(func() string {
-					return getFunctionState(namespace, functionName)
-				}, ShouldReturnSubstringEventually, "Running", 60*time.Second, 1*time.Second, func() string { return printLogsFunctionPodContainers(namespace, functionName) })
-				So(func() string {
-					host := fmt.Sprintf("http://%s.%s:8080", functionName, namespace)
-					value, err := getFunctionOutput(host, namespace, functionName, testUUID.String())
-					if err != nil {
-						return "Host not reachable. Retrying"
-					}
-					return value
-				}, ShouldReturnSubstringEventually, testUUID.String(), 60*time.Second, 1*time.Second)
-			})
-		})
+	myFunctionTest, err := NewFunctionTest()
+
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	backupTests := []BackupTest{myFunctionTest}
+
+	e2eTests := make([]e2eTest, len(backupTests))
+
+	for idx, backupTest := range backupTests {
+		testUUID := uuid.New()
+		e2eTests[idx] = e2eTest{
+			backupTest: backupTest,
+			namespace:  "test-" + testUUID.String(),
+			testUUID:   testUUID.String(),
+		}
+	}
+
+	myBackupClient, err := utils.NewBackupClient()
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	Convey("Create resources", t, func() {
+		for _, e2eTest := range e2eTests {
+
+			err := myBackupClient.CreateNamespace(e2eTest.namespace)
+			So(err, ShouldBeNil)
+			e2eTest.backupTest.CreateResources(e2eTest.namespace)
+		}
+		for _, e2eTest := range e2eTests {
+			e2eTest.backupTest.TestResources()
+		}
 	})
 
 	Convey("Backup Cluster", t, func() {
-		So(func() string {
-			stdout, stderr, err := runCommand("ark", []string{"backup", "create", backupName, "--include-namespaces", namespace})
-			if err != nil {
-				t.Fatalf("Error was: %v \n %v", err, stderr.String())
-			}
-			return stdout.String()
-		}(), ShouldContainSubstring, "submitted successfully")
+		excludedNamespaces := []string{"default", "kube-public", "kube-system", "heptio-ark", "kyma-system", "kyma-installer", "kyma-integration", "istio-system", "natts"}
+		excludedResources := []string{}
+		myBackupClient.CreateBackup(backupName, []string{}, excludedNamespaces, []string{}, excludedResources)
 
 		Convey("Check backup status", func() {
-			So(func() string {
-				stdout, stderr, err := runCommand("ark", []string{"backup", "get", backupName, "-oyaml"})
-				if err != nil {
-					t.Fatalf("Error was: %v \n %v", err, stderr.String())
-				}
-				return stdout.String()
-			}, ShouldReturnSubstringEventually, "phase: Completed", 60*time.Second, 1*time.Second)
-			Convey("Delete resources from cluster", func() {
-				_, stderr, _ := runCommand("kubectl", []string{"delete", "ns", namespace, "--grace-period=0", "--force", "--ignore-not-found"})
-				So(stderr.String(), ShouldNotContainSubstring, "Error from server (Conflict): Operation cannot be fulfilled on namespaces")
-				So(stderr.String(), ShouldNotContainSubstring, "No resources found")
-				So(func() string {
-					_, stderr, _ := runCommand("kubectl", []string{"get", "ns", namespace, "-oyaml"})
-					return stderr.String()
-				}, ShouldReturnSubstringEventually, "NotFound", 1*time.Minute, 1*time.Second)
-			})
-
+			err := myBackupClient.WaitForBackupToBeCreated(backupName, 120*time.Second)
+			So(err, ShouldBeNil)
+			for _, e2eTest := range e2eTests {
+				Convey("Delete resources from cluster", func() {
+					err := myBackupClient.DeleteNamespace(e2eTest.namespace)
+					So(err, ShouldBeNil)
+					err = myBackupClient.WaitForNamespaceToBeDeleted(e2eTest.namespace, 120*time.Second)
+					So(err, ShouldBeNil)
+				})
+			}
 		})
-
 	})
 
 	Convey("Restore Cluster", t, func() {
-		So(func() string {
-			stdout, stderr, err := runCommand("ark", []string{"restore", "create", "--from-backup", backupName, backupName})
-			if err != nil {
-				t.Fatalf("Error was: %v \n %v", err, stderr.String())
-			}
-			return stdout.String()
-		}(), ShouldContainSubstring, "submitted successfully.")
-		So(func() string {
-			stdout, _, _ := runCommand("ark", []string{"restore", "get", backupName})
-			return stdout.String()
-		}, ShouldReturnSubstringEventually, "Completed", 5*time.Minute, 1*time.Second)
+		err := myBackupClient.RestoreBackup(backupName)
+		So(err, ShouldBeNil)
+		err = myBackupClient.WaitForBackupToBeRestored(backupName, 120*time.Second)
+		So(err, ShouldBeNil)
 		Convey("Test restored resources", func() {
-			So(func() string {
-				return getFunctionState(namespace, functionName)
-			}, ShouldReturnSubstringEventually, "Running", 60*time.Second, 1*time.Second, func() string { return printLogsFunctionPodContainers(namespace, functionName) })
-			So(func() string {
-				host := fmt.Sprintf("http://%s.%s:8080", functionName, namespace)
-				value, err := getFunctionOutput(host, namespace, functionName, testUUID.String())
-				if err != nil {
-					return "Host not reachable. Retrying"
-				}
-				return value
-			}, ShouldReturnSubstringEventually, testUUID.String(), 60*time.Second, 1*time.Second)
+			for _, backupTest := range backupTests {
+				backupTest.TestResources()
+			}
 		})
 	})
 }
