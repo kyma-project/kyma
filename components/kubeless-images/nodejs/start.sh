7afb3c01d3afc7aba3960499c737d9ed5f60fc6c
Rakesh Garimella <rakesh.garimella@sap.com>
2019-02-22 14:10:45 +0100

    fixes

--- components/kubeless-images/nodejs/start.sh
+++ components/kubeless-images/nodejs/start.sh
@@ -3,7 +3,7 @@ echo "----------------------------------------------"
 echo "Lambda Code"
 awk '{gsub(/\\/,"\\\\\\\\", $0); printf "%s\\n", $0}' /kubeless/handler.js | rev | cut -c 3- | rev
 echo
-if [ ! -s  /kubeless/package.json ]; then
+if [ -e  /kubeless/package.json ]; then
   echo "Lambda Code dependencies are"
   awk '{gsub(/\\/,"\\\\\\\\", $0); printf "%s\\n", $0}' /kubeless/package.json | rev | cut -c 3- | rev
   echo "--------------------------------------------"
