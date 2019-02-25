40413fb2dd1f504892d7a5e3fb2776bf34cf8fb4
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-02-25 16:14:34 +0100

    Delete ClusterBucket

--- tests/asset-store/internal/testsuite/suite.go
+++ tests/asset-store/internal/testsuite/suite.go
@@ -3,15 +3,14 @@ package testsuite
 import (
 	"github.com/kyma-project/kyma/components/assetstore-controller-manager/pkg/apis/assetstore/v1alpha2"
 	"github.com/pkg/errors"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"strings"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type Config struct {
@@ -51,6 +50,8 @@ func (t *TestSuite) UploadTestData() error {
 	return nil
 }
 
+// TODO: Split
+
 func (t *TestSuite) CreateNamespace() error {
 	_, err := t.coreCli.Namespaces().Create(&v1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
@@ -65,7 +66,7 @@ func (t *TestSuite) CreateNamespace() error {
 	return nil
 }
 
-func (t *TestSuite) Cleanup() error {
+func (t *TestSuite) DeleteNamespace() error {
 	err := t.coreCli.Namespaces().Delete(t.cfg.Namespace, &metav1.DeleteOptions{})
 	if err != nil {
 		return errors.Wrapf(err, "while deleting namespace %s", t.cfg.Namespace)
@@ -75,22 +76,6 @@ func (t *TestSuite) Cleanup() error {
 }
 
 func (t *TestSuite) CreateBucket() error {
-	err := t.createBucket()
-	if err != nil {
-		return errors.Wrapf(err, "while creating Bucket")
-	}
-}
-
-func (t *TestSuite) CreateAsset() error {
-	return nil
-}
-
-func (t *TestSuite) ValidateAssetUpload() error {
-	return nil
-}
-
-
-func (t *TestSuite) createBucket() error {
 	bucket := &v1alpha2.Bucket{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: t.cfg.BucketName,
@@ -105,7 +90,7 @@ func (t *TestSuite) createBucket() error {
 
 	u, err := runtime.DefaultUnstructuredConverter.ToUnstructured(bucket)
 	if err != nil {
-		return errors.Wrap(err, "while converting bucket to unstructured")
+		return errors.Wrap(err, "while converting Bucket to unstructured")
 	}
 
 	unstructuredBucket := &unstructured.Unstructured{
@@ -113,13 +98,67 @@ func (t *TestSuite) createBucket() error {
 	}
 
 	_, err = t.dynamicCli.Resource(schema.GroupVersionResource{
-		Version: bucket.ResourceVersion,
-		Group: bucket.APIVersion,
-		Resource: strings.ToLower(bucket.Kind) + "s",
+		Version: v1alpha2.SchemeGroupVersion.Version,
+		Group: v1alpha2.SchemeGroupVersion.Group,
+		Resource: "buckets",
 	}).Namespace(t.cfg.Namespace).Create(unstructuredBucket, metav1.CreateOptions{})
 	if err != nil {
-		return errors.Wrapf(err, "while creating bucket %s", bucket.Name)
+		return errors.Wrapf(err, "while creating Bucket %s", bucket.Name)
 	}
 
+	return nil
+}
+
+func (t *TestSuite) CreateClusterBucket() error {
+	bucket := &v1alpha2.ClusterBucket{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: t.cfg.ClusterBucketName,
+		},
+		Spec:v1alpha2.ClusterBucketSpec{
+			CommonBucketSpec: v1alpha2.CommonBucketSpec{
+				Policy:v1alpha2.BucketPolicyReadOnly,
+			},
+		},
+	}
+
+	u, err := runtime.DefaultUnstructuredConverter.ToUnstructured(bucket)
+	if err != nil {
+		return errors.Wrap(err, "while converting ClusterBucket to unstructured")
+	}
+
+	unstructuredBucket := &unstructured.Unstructured{
+		Object:u,
+	}
+
+	_, err = t.dynamicCli.Resource(schema.GroupVersionResource{
+		Version: v1alpha2.SchemeGroupVersion.Version,
+		Group: v1alpha2.SchemeGroupVersion.Group,
+		Resource: "clusterbuckets",
+	}).Namespace(t.cfg.Namespace).Create(unstructuredBucket, metav1.CreateOptions{})
+	if err != nil {
+		return errors.Wrapf(err, "while creating ClusterBucket %s", bucket.Name)
+	}
+
+	return nil
+}
+
+func (t *TestSuite) DeleteClusterBucket() error {
+	err := t.dynamicCli.Resource(schema.GroupVersionResource{
+		Version: v1alpha2.SchemeGroupVersion.Version,
+		Group: v1alpha2.SchemeGroupVersion.Group,
+		Resource: "clusterbuckets",
+	}).Namespace(t.cfg.Namespace).Delete(t.cfg.ClusterBucketName, &metav1.DeleteOptions{})
+	if err != nil {
+		return errors.Wrapf(err, "while deleting ClusterBucket %s", t.cfg.ClusterBucketName)
+	}
+
+	return nil
+}
+
+func (t *TestSuite) CreateAsset() error {
+	return nil
+}
+
+func (t *TestSuite) ValidateAssetUpload() error {
 	return nil
 }
