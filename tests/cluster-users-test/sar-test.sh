30849a3513562b6874f449540679233203ddbc4c
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-02-26 13:57:43 +0100

    Fix scripts

--- tests/cluster-users-test/sar-test.sh
+++ tests/cluster-users-test/sar-test.sh
@@ -70,17 +70,13 @@ function runTests() {
 
 function init() {
 	readonly REGISTRATION_REQUEST=$(curl -s -X GET -H 'Content-Type: application/x-www-form-urlencoded' "${DEX_SERVICE_SERVICE_HOST}:${DEX_SERVICE_SERVICE_PORT_HTTP}/auth?response_type=id_token%20token&client_id=kyma-client&redirect_uri=http://127.0.0.1:5555/callback&scope=openid%20profile%20email%20groups&nonce=vF7FAQlqq41CObeUFYY0ggv1qEELvfHaXQ0ER4XM")
-	readonly REQUEST_ID=$(echo ${REGISTRATION_REQUEST} | cut -d '"' -f 2 | cut -d '?' -f 2)
-	readonly EMAIL=$(cat ${EMAIL_FILE})
-	readonly PASSWORD=$(cat ${PASSWORD_FILE})
+	readonly REQUEST_ID=$(echo "${REGISTRATION_REQUEST}" | cut -d '"' -f 2 | cut -d '?' -f 2)
+	readonly EMAIL=$(cat "${EMAIL_FILE}")
+	readonly PASSWORD=$(cat "${PASSWORD_FILE}")
 	curl -X POST -F "login=${EMAIL}" -F "password=${PASSWORD}" "${DEX_SERVICE_SERVICE_HOST}:${DEX_SERVICE_SERVICE_PORT_HTTP}/auth/local?${REQUEST_ID}"
 	readonly RESPONSE=$(curl -X GET "${DEX_SERVICE_SERVICE_HOST}:${DEX_SERVICE_SERVICE_PORT_HTTP}/approval?${REQUEST_ID}")
-	export AUTH_TOKEN=$(echo "${RESPONSE}" | grep -o -P '(?<=id_token=).*(?=&amp;state)')
-	curl -s -H "Authorization: Bearer ${AUTH_TOKEN}" ${CONFIGURATIONS_GENERATOR_SERVICE_HOST}:${CONFIGURATIONS_GENERATOR_SERVICE_PORT_HTTP}/kube-config -o "${PWD}/kubeconfig"
-	export KUBECONFIG="${PWD}/kubeconfig"
-
-	echo "---> Create testing RoleBinding"
-	kubectl create -f ./kyma-developer-binding.yaml -n "${NAMESPACE}"
+	readonly AUTH_TOKEN=$(echo "${RESPONSE}" | grep -o -P '(?<=id_token=).*(?=&amp;state)')
+	curl -s -H "Authorization: Bearer ${AUTH_TOKEN}" "${CONFIGURATIONS_GENERATOR_SERVICE_HOST}:${CONFIGURATIONS_GENERATOR_SERVICE_PORT_HTTP}/kube-config" -o "${PWD}/kubeconfig"
 }
 
 function cleanup() {
@@ -114,6 +110,9 @@ trap cleanup EXIT
 ERROR_LOGGING_GUARD="true"
 
 init
+export KUBECONFIG="${PWD}/kubeconfig"
+echo "---> Create testing RoleBinding"
+kubectl create -f ./kyma-developer-binding.yaml -n "${NAMESPACE}"
 runTests
 
 echo "ALL TESTS PASSED"
