185f7a811c22a370a02f415d38c9becd49d361b4
franpog859 <franciszek.pog@gmail.com>
2019-02-27 17:01:06 +0100

    Add revcationListRepository

--- components/connector-service/internal/certificates/revocationlist/repository.go
+++ components/connector-service/internal/certificates/revocationlist/repository.go
@@ -5,18 +5,18 @@ type RevocationListRepository interface {
 	Contains(hash string) (bool, error)
 }
 
-type revocationListRepository struct {
-
-}
+type revocationListRepository map[string]string
 
 func NewRepository() RevocationListRepository {
 	return &revocationListRepository{}
 }
 
 func (r revocationListRepository) Insert(hash string) error {
+	r[hash] = hash
 	return nil
 }
 
 func (r revocationListRepository) Contains(hash string) (bool, error) {
-	return false, nil
+	_, ok := r[hash]
+	return ok, nil
 }
---
+++ components/connector-service/internal/certificates/revocationlist/repository_test.go
@@ -0,0 +1,61 @@
+package revocationlist
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestRevocationListRepository_Insert(t *testing.T) {
+
+	t.Run("should insert value to the list", func(t *testing.T) {
+		// given
+		repository := NewRepository()
+		someHash := "someHash"
+
+		// when
+		err := repository.Insert(someHash)
+		require.NoError(t, err)
+
+		isPresent, err := repository.Contains(someHash)
+		require.NoError(t, err)
+
+		// then
+		assert.Equal(t, isPresent, true)
+	})
+
+	t.Run("should be idempotent", func(t *testing.T) {
+		// given
+		repository := NewRepository()
+		someHash := "someHash"
+
+		// when
+		err := repository.Insert(someHash)
+		require.NoError(t, err)
+
+		err = repository.Insert(someHash)
+		require.NoError(t, err)
+
+		isPresent, err := repository.Contains(someHash)
+		require.NoError(t, err)
+
+		// then
+		assert.Equal(t, isPresent, true)
+	})
+}
+
+func TestRevocationListRepository_Contains(t *testing.T) {
+
+	t.Run("should return false if value is not present", func(t *testing.T) {
+		// given
+		repository := NewRepository()
+		someHash := "someHash"
+
+		// when
+		isPresent, err := repository.Contains(someHash)
+		require.NoError(t, err)
+
+		// then
+		assert.Equal(t, isPresent, false)
+	})
+}
