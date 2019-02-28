140335c4bb8021d2fbee9eee8e8ca090f6a0346e
Mariusz Szymanski <mariusz.szymanski@sap.com>
2019-02-28 12:19:43 +0100

    Revocation check middleware

---
+++ components/connector-service/internal/certificates/middlewares/revocationcheck.go
@@ -0,0 +1,42 @@
+package middlewares
+
+import (
+	"github.com/kyma-project/kyma/components/connector-service/internal/apperrors"
+	"github.com/kyma-project/kyma/components/connector-service/internal/certificates"
+	"github.com/kyma-project/kyma/components/connector-service/internal/certificates/revocationlist"
+	"github.com/kyma-project/kyma/components/connector-service/internal/externalapi"
+	"github.com/kyma-project/kyma/components/connector-service/internal/httphelpers"
+	"net/http"
+)
+
+type revocationCheckMiddleware struct {
+	revocationList revocationlist.RevocationListRepository
+}
+
+func NewRevocationCheckMiddleware(revocationList revocationlist.RevocationListRepository) *revocationCheckMiddleware {
+	return &revocationCheckMiddleware{
+		revocationList: revocationList,
+	}
+}
+
+func (rcm revocationCheckMiddleware) Middleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		certificate := r.Header.Get(externalapi.CertificateHeader)
+
+		hash := certificates.CalculateHash(certificate)
+
+		contains, err := rcm.revocationList.Contains(hash)
+
+		if err != nil {
+			httphelpers.RespondWithErrorAndLog(w, apperrors.Internal("Failed to read revocation list"))
+			return
+		}
+
+		if contains {
+			httphelpers.RespondWithErrorAndLog(w, apperrors.Forbidden("Certificate has been revoked"))
+			return
+		}
+
+		handler.ServeHTTP(w, r)
+	})
+}
---
+++ components/connector-service/internal/certificates/middlewares/revocationcheck_test.go
@@ -0,0 +1,96 @@
+package middlewares
+
+import (
+	"github.com/kyma-project/kyma/components/connector-service/internal/certificates/revocationlist/mocks"
+	"github.com/kyma-project/kyma/components/connector-service/internal/externalapi"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRevocationCheckMiddleware(t *testing.T) {
+
+	t.Run("should return http code 403 when certificate hash on revocation list", func(t *testing.T) {
+		// given
+		testCert := "testCert"
+		hashedTestCert := "f21139ef2b82d02ee73a56c5c73c053fbafa3480a0b35459cba276b0667c57fc"
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		req, err := http.NewRequest("POST", "/", nil)
+		req.Header.Set(externalapi.CertificateHeader, testCert)
+		require.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+
+		repository := &mocks.RevocationListRepository{}
+		repository.On("Contains", hashedTestCert).Return(true, nil)
+
+		middleware := NewRevocationCheckMiddleware(repository)
+
+		// when
+		resultHandler := middleware.Middleware(handler)
+		resultHandler.ServeHTTP(rr, req)
+
+		// then
+		assert.Equal(t, http.StatusForbidden, rr.Code)
+	})
+
+	t.Run("should allow certificate when hash not on revocation list", func(t *testing.T) {
+		// given
+		testCert := "testCert"
+		hashedTestCert := "f21139ef2b82d02ee73a56c5c73c053fbafa3480a0b35459cba276b0667c57fc"
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		req, err := http.NewRequest("POST", "/", nil)
+		req.Header.Set(externalapi.CertificateHeader, testCert)
+		require.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+
+		repository := &mocks.RevocationListRepository{}
+		repository.On("Contains", hashedTestCert).Return(false, nil)
+
+		middleware := NewRevocationCheckMiddleware(repository)
+
+		// when
+		resultHandler := middleware.Middleware(handler)
+		resultHandler.ServeHTTP(rr, req)
+
+		// then
+		assert.Equal(t, http.StatusOK, rr.Code)
+	})
+
+	t.Run("should return http code 500 when error occured on contains method call", func(t *testing.T) {
+		// given
+		testCert := "testCert"
+		hashedTestCert := "f21139ef2b82d02ee73a56c5c73c053fbafa3480a0b35459cba276b0667c57fc"
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+
+		req, err := http.NewRequest("POST", "/", nil)
+		req.Header.Set(externalapi.CertificateHeader, testCert)
+		require.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+
+		repository := &mocks.RevocationListRepository{}
+		repository.On("Contains", hashedTestCert).Return(false, errors.New("Some error"))
+
+		middleware := NewRevocationCheckMiddleware(repository)
+
+		// when
+		resultHandler := middleware.Middleware(handler)
+		resultHandler.ServeHTTP(rr, req)
+
+		// then
+		assert.Equal(t, http.StatusInternalServerError, rr.Code)
+	})
+}
