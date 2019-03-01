85f13a815d53e208355a364075bedadb3c571122
franpog859 <franciszek.pog@gmail.com>
2019-03-01 09:40:52 +0100

    Avoid map races with mutex

--- components/connector-service/internal/certificates/revocationlist/repository.go
+++ components/connector-service/internal/certificates/revocationlist/repository.go
@@ -1,22 +1,36 @@
 package revocationlist
 
+import "sync"
+
 type RevocationListRepository interface {
 	Insert(hash string) error
 	Contains(hash string) (bool, error)
 }
 
-type revocationListRepository map[string]string
-
-func NewRepository() RevocationListRepository {
-	return &revocationListRepository{}
+type revocationListRepository struct {
+	revocationList map[string]string
+	mutex sync.RWMutex
 }
 
-func (r revocationListRepository) Insert(hash string) error {
-	r[hash] = hash
+func NewRepository() RevocationListRepository {
+	return &revocationListRepository{
+		make(map[string]string),
+		sync.RWMutex{},
+	}
+}
+
+func (r *revocationListRepository) Insert(hash string) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	r.revocationList[hash] = hash
 	return nil
 }
 
-func (r revocationListRepository) Contains(hash string) (bool, error) {
-	_, ok := r[hash]
+func (r *revocationListRepository) Contains(hash string) (bool, error) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
+	_, ok := r.revocationList[hash]
 	return ok, nil
 }
