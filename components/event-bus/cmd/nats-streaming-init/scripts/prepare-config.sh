6d7aaf3b5b43357126b7601ad6bd0d091f88b6ba
Pavel Pavlov <pavel.pavlov@sap.com>
2019-03-04 15:36:32 +0100

    updating init container and deployments

--- components/event-bus/cmd/nats-streaming-init/scripts/prepare-config.sh
+++ components/event-bus/cmd/nats-streaming-init/scripts/prepare-config.sh
@@ -13,17 +13,19 @@ fi
 if [ -d ${out_path} ]; then
     cd ${out_path}
     cp -Lv ${configmap_path}/*.conf .
-
-    GCFG="gnatsd.conf"
-    echo "authorization {" >> ${GCFG}
-    echo "  users = [" >> ${GCFG}
-    echo "    {user: ${STAN_USERNAME}, password: ${STAN_PASSWD}}" >> ${GCFG}
-    echo "    {user: ${EB_USERNAME}, password: ${EB_PASSWD}}" >> ${GCFG}
-    echo "    {user: ${KN_USERNAME}, password: ${KN_PASSWD}}" >> ${GCFG}
-    echo "  ]" >> ${GCFG}
-    echo "}" >> ${GCFG}
-    
-    cat ${GCFG}
+    if [ ${ENABLE_BA} eq "true" ]; then
+        GCFG="gnatsd.conf"
+        echo "authorization {" >> ${GCFG}
+        echo "  users = [" >> ${GCFG}
+        echo "    {user: ${STAN_USERNAME}, password: ${STAN_PASSWD}}" >> ${GCFG}
+        echo "    {user: ${EB_USERNAME}, password: ${EB_PASSWD}}" >> ${GCFG}
+        echo "    {user: ${KN_USERNAME}, password: ${KN_PASSWD}}" >> ${GCFG}
+        echo "  ]" >> ${GCFG}
+        echo "}" >> ${GCFG}
+        echo "authorization configured for users ${STAN_USERNAME}, ${EB_USERNAME}, ${KN_USERNAME}."
+    else 
+        echo "authorization not configured."
+    fi
     echo "${out_path} prepared. done."
 else 
     echo "${out_path} not found. done."
