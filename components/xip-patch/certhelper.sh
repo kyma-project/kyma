24dffc58ff47d2c5aa2258e85fe508e5e554ebd1
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-03-04 09:55:53 +0100

    Create new getLB function, complete helper script

--- components/xip-patch/certhelper.sh
+++ components/xip-patch/certhelper.sh
@@ -3,15 +3,14 @@
 # - DOMAIN (optional) - Static domain for which to generate certs
 # - TLS_CERT (optinal) - Current TLS certificate
 # - TLS_KEY (optional) - Current TLS cert key
-# - LB_SERVICE_NAME (required) - Name of LoadBalancer Service
-# - LB_SERVICE_NS (required) - Namespace of LoadBalancer Service
+# - LB_LABEL (required) - Selector label for the LoadBalancer service
 
 readonly CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $CURRENT_DIR/utils.sh
 
 discoverUnsetVar=false
 
-for var in LB_SERVICE_NAME LB_SERVICE_NS; do
+for var in LB_LABEL; do
     if [ -z "${!var}" ] ; then
         echo "ERROR: $var is not set"
         discoverUnsetVar=true
@@ -24,11 +23,19 @@ fi
 
 if [[ -z "${DOMAIN}" ]]; then
 	echo "---> DOMAIN not SET. Creating..."
-	INGRESS_IP=$(getLoadBalancerIP "${LB_SERVICE_NAME}" "${LB_SERVICE_NS}")
+	INGRESS_IP=$(getLoadBalancerIPFromLabel "${LB_LABEL}")
 	DOMAIN="${INGRESS_IP}.xip.io"
+	DOMAIN_YAML=$(cat << EOF
+---
+data:
+  global.applicationConnectorDomainName: "${DOMAIN}"
+EOF
+)
+	kubectl patch configmap installation-config-overrides --patch "${DOMAIN_YAML}" -n kyma-installer
 fi
 
 if [[ ! -z "${TLS_CERT}" ]] && [[ ! -z "${TLS_KEY}" ]]; then
 	echo "---> Generating Certs for ${DOMAIN}"
 	generateCertificatesForDomain "${DOMAIN}" /root/key.pem /root/cert.pem
+	kubectl create secret tls application-connector-ingress-tls-cert --cert=/root/cert.pem --key=/root/key.pem --namespace=kyma-integration
 fi
--- components/xip-patch/utils.sh
+++ components/xip-patch/utils.sh
@@ -33,6 +33,38 @@ getLoadBalancerIP() {
     echo "${LOAD_BALANCER_IP}"
 }
 
+getLoadBalancerIPFromLabel() {
+
+    if [ "$#" -ne 1 ]; then
+        echo "usage: getLoadBalancerIP <label>"
+        exit 1
+    fi
+
+    LABEL="$1"
+    LOAD_BALANCER_IP=""
+
+    SECONDS=0
+    END_TIME=$((SECONDS+60))
+
+    while [ ${SECONDS} -lt ${END_TIME} ];do
+
+        LOAD_BALANCER_IP=$(kubectl get service --field-selector "${LABEL}" -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+
+        if [ -n "${LOAD_BALANCER_IP}" ]; then
+            break
+        fi
+
+        sleep 10
+
+    done
+
+    if [ -z "${LOAD_BALANCER_IP}" ]; then
+        exit 1
+    fi
+
+    echo "${LOAD_BALANCER_IP}"
+}
+
 generateCertificatesForDomain() {
 
     if [ "$#" -ne 3 ]; then
