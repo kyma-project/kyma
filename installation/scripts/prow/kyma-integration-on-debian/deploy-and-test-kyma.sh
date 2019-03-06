521272581ebf7aecd7471784bc3ea71e75f8853d
Suleyman Akbas <suleymanakbas91@gmail.com>
2019-03-06 20:37:14 +0100

    Use custom image in kyma integration tests job (#3009)

--- installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
+++ installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
@@ -9,7 +9,6 @@ set -o errexit
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INSTALLATION_DIR=${CURRENT_DIR}/../../../
 
-sudo ${CURRENT_DIR}/install-deps-debian.sh
 sudo ${INSTALLATION_DIR}/cmd/run.sh --vm-driver "none"
 sudo ${INSTALLATION_DIR}/scripts/is-installed.sh --timeout 30m
 sudo ${INSTALLATION_DIR}/scripts/watch-pods.sh
