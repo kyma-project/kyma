bab13336cd2a018156d839e1367ea4b37cd847cd
Szymon Gibala <szymon.gibala@sap.com>
2019-03-08 11:55:44 +0100

    Improve before-commit.sh

--- components/application-gateway/before-commit.sh
+++ components/application-gateway/before-commit.sh
@@ -67,15 +67,23 @@ fi
 
 goFilesToCheck=$(find . -type f -name "*.go" | egrep -v "\/vendor\/|_*/automock/|_*/testdata/|/pkg\/|_*export_test.go")
 
-#
-# GO FMT
-#
-goFmtResult=$(echo "${goFilesToCheck}" | xargs -L1 go fmt)
-if [ $(echo ${#goFmtResult}) != 0 ]
-	then
-    	echo -e "${RED}✗ go fmt${NC}\n$goFmtResult${NC}"
-    	exit 1;
-	else echo -e "${GREEN}√ go fmt${NC}"
+##
+# GO IMPORTS & FMT
+##
+go build -o goimports-vendored ./vendor/golang.org/x/tools/cmd/goimports
+buildGoImportResult=$?
+if [ ${buildGoImportResult} != 0 ]; then
+	echo -e "${RED}✗ go build goimports${NC}\n$buildGoImportResult${NC}"
+	exit 1
+fi
+
+goImportsResult=$(echo "${goFilesToCheck}" | xargs -L1 ./goimports-vendored -w -l)
+rm goimports-vendored
+
+if [ $(echo ${#goImportsResult}) != 0 ]; then
+	echo -e "${RED}✗ goimports and fmt${NC}\n$goImportsResult${NC}"
+	exit 1
+else echo -e "${GREEN}√ goimports and fmt${NC}"
 fi
 
 ##
