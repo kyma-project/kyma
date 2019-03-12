a8be51e527cd2c610cd6bdd62a4fb977ae3b48d8
Jan Medrek <jan.medrek@sap.com>
2019-03-12 13:38:45 +0100

    Differentiate certificate validity time for apps and runtimes

--- components/connector-service/internal/externalapi/managementinfohandler.go
+++ components/connector-service/internal/externalapi/managementinfohandler.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	RenewCertURLFormat = "%s/certificates/renewals"
+	RenewCertURLFormat      = "%s/certificates/renewals"
 	RevocationCertURLFormat = "%s/certificates/revocations"
 )
 
@@ -39,8 +39,8 @@ func (ih *managementInfoHandler) GetManagementInfo(w http.ResponseWriter, r *htt
 
 func (ih *managementInfoHandler) buildURLs(clientContextService clientcontext.ClientContextService) mgmtURLs {
 	return mgmtURLs{
-		RuntimeURLs:  clientContextService.GetRuntimeUrls(),
-		RenewCertURL: fmt.Sprintf(RenewCertURLFormat, ih.certificateProtectedBaseURL),
+		RuntimeURLs:       clientContextService.GetRuntimeUrls(),
+		RenewCertURL:      fmt.Sprintf(RenewCertURLFormat, ih.certificateProtectedBaseURL),
 		RevocationCertURL: fmt.Sprintf(RevocationCertURLFormat, ih.certificateProtectedBaseURL),
 	}
 }
