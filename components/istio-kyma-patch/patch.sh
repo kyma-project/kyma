0c2394e8e1700d4de3a21d11390d95479b52cfbb
kubadz <jakub.dziechciewicz@sap.com>
2019-03-13 14:30:10 +0100

    Revert changes in istio-kyma-patch script

--- components/istio-kyma-patch/patch.sh
+++ components/istio-kyma-patch/patch.sh
@@ -27,8 +27,8 @@ function require_istio_system() {
 }
 
 function require_mtls_disabled() {
-    local mTLS=$(kubectl get meshpolicy default -o jsonpath='{.spec.peers[0].mtls.mode}' --ignore-not-found=true )
-    if [[ ${mTLS} != "PERMISSIVE" ]]; then
+    local mTLS=$(kubectl get meshpolicy default -o jsonpath='{.spec.peers}' --ignore-not-found=true | grep "mtls")
+    if [[ ${mTLS} != "" ]]; then
         echo "mTLS must be disabled"
         exit 1
     fi
@@ -88,7 +88,7 @@ function configure_sidecar_injector() {
     configmap=$(sed 's/policy: disabled/policy: enabled/' <<< "$configmap")
   fi
 
-  configmap=$(sed 's/\[\[ \.ProxyConfig\.GetTracing\.GetZipkin\.GetAddress \]\]/zipkin.kyma-system:9411/g' <<< "$configmap")
+  configmap=$(sed 's/\[\[ \.ProxyConfig\.ZipkinAddress \]\]/zipkin.kyma-system:9411/g' <<< "$configmap")
 
   # Set limits for sidecar. Our namespaces have resource quota set thus every container needs to have limits defined.
   # In case there is no limits section add one at the beginning of container definition. It serves as default.
