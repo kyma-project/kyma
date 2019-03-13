0bcf18a471eecbd77cab030b38bc174d5134ff56
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-03-13 13:03:40 +0100

    Swap satori for gofrs (#3141)

--- components/api-controller/pkg/controller/commons/errors.go
+++ components/api-controller/pkg/controller/commons/errors.go
@@ -3,13 +3,16 @@ package commons
 import (
 	"fmt"
 
-	"github.com/satori/go.uuid"
+	"github.com/gofrs/uuid"
 	log "github.com/sirupsen/logrus"
 )
 
-// Logs root cause and returns new error to hide implementation details
+//HandleError Logs root cause and returns new error to hide implementation details
 func HandleError(rootCause error, msg string) error {
-	errId := uuid.NewV4()
-	log.Errorf("[Error '%s']: %v", errId, rootCause)
-	return fmt.Errorf("%s (error code = '%s')", msg, errId)
+	errID, err := uuid.NewV4()
+	if err != nil {
+		log.Fatalf("failed to generate UUID: %v", err)
+	}
+	log.Errorf("[Error '%s']: %v", errID, rootCause)
+	return fmt.Errorf("%s (error code = '%s')", msg, errID)
 }
