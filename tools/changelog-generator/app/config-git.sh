b1a57feac1a41cdc8ab653898e5e0f9f18e3b593
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-03-14 12:51:06 +0100

    Move changelog-generator to test-infra repository

--- tools/changelog-generator/app/config-git.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Configuring git..."
-git config --global user.email "kyma.bot@sap.com"
-git config --global user.name "Kyma Bot"
-git config --global core.sshCommand 'ssh -i '$SSH_FILE''
