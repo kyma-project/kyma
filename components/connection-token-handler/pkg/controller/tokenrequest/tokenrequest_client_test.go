c8c3c33aad6a75326fe682492568d6f815308cd5
Mariusz Szymanski <mariusz.szymanski@sap.com>
2019-03-14 13:44:49 +0100

    Support for token fetching in central connector service

--- components/connection-token-handler/pkg/controller/tokenrequest/tokenrequest_client_test.go
+++ components/connection-token-handler/pkg/controller/tokenrequest/tokenrequest_client_test.go
@@ -9,11 +9,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func getServerMock(t *testing.T, appName, token string) *httptest.Server {
+func getServerMock(t *testing.T, appName, tenant, group, token string) *httptest.Server {
 	return httptest.NewServer(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.URL.Path == "/v1/applications/tokens" {
-				assert.Equal(t, appName, r.Header.Get(applicationHeader))
+				checkHeaders(t, appName, r, tenant, group)
 
 				w.Header().Add("Content-Type", "application/json")
 				w.Write([]byte(fmt.Sprintf(
@@ -22,29 +22,55 @@ func getServerMock(t *testing.T, appName, token string) *httptest.Server {
 		}),
 	)
 }
+
+func checkHeaders(t *testing.T, appName string, r *http.Request, tenant string, group string) {
+	assert.Equal(t, appName, r.Header.Get(applicationHeader))
+	if tenant != "" && group != "" {
+		assert.Equal(t, tenant, r.Header.Get(tenantHeader))
+		assert.Equal(t, group, r.Header.Get(groupHeader))
+	}
+}
+
 func TestTokenRequestClient_FetchToken(t *testing.T) {
 	appName := "some-app"
 	token := "some-long-token-value"
 
 	t.Run("should return TokenDto with valid token", func(t *testing.T) {
-		srvMock := getServerMock(t, appName, token)
+		//given
+		srvMock := getServerMock(t, appName, "", "", token)
 		defer srvMock.Close()
-
+		//when
 		svcClient := NewConnectorServiceClient(srvMock.URL)
-		tokenDto, err := svcClient.FetchToken(appName)
+		tokenDto, err := svcClient.FetchToken(appName, "", "")
+		//then
+		assert.NoError(t, err)
+		assert.NotNil(t, tokenDto)
+		assert.Equal(t, token, tokenDto.Token)
+	})
 
+	t.Run("should return TokenDto with valid token when tenant and group provided", func(t *testing.T) {
+		//given
+		tenant := "some-tenant"
+		group := "some-group"
+		srvMock := getServerMock(t, appName, tenant, group, token)
+		defer srvMock.Close()
+		//when
+		svcClient := NewConnectorServiceClient(srvMock.URL)
+		tokenDto, err := svcClient.FetchToken(appName, tenant, group)
+		//then
 		assert.NoError(t, err)
 		assert.NotNil(t, tokenDto)
 		assert.Equal(t, token, tokenDto.Token)
 	})
 
 	t.Run("should return error when calling invalid URL", func(t *testing.T) {
-		srvMock := getServerMock(t, appName, token)
+		//given
+		srvMock := getServerMock(t, appName, "", "", token)
 		defer srvMock.Close()
-
+		//when
 		svcClient := NewConnectorServiceClient(srvMock.URL + "/some-text")
-		_, err := svcClient.FetchToken(appName)
-
+		_, err := svcClient.FetchToken(appName, "", "")
+		//then
 		assert.Error(t, err)
 	})
 }
