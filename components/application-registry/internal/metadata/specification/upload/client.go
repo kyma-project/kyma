2c29c22f73d76ca58a2dfd7db7bbe4e56bc18c81
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2019-03-15 15:28:00 +0100

    First verion of Upload Service client working.

--- components/application-registry/internal/metadata/specification/upload/client.go
+++ components/application-registry/internal/metadata/specification/upload/client.go
@@ -1,9 +1,18 @@
 package upload
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/kyma-project/kyma/components/application-registry/internal/apperrors"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"net/url"
+)
+
+
+const (
+	AssetFieldName = "assetFile"
 )
 
 type InputFile struct {
@@ -24,13 +33,90 @@ type UploadClient interface {
 }
 
 type uploadClient struct {
-
+	httpClient *http.Client
+	uploadServiceUrl *url.URL
 }
 
-func NewUploadClient(uploadServiceUrl *url.URL, httpclient http.Client) UploadClient {
-	return uploadClient{}
+func NewUploadClient(uploadServiceUrl *url.URL, httpclient *http.Client) UploadClient {
+	return uploadClient{
+		uploadServiceUrl: uploadServiceUrl,
+		httpClient: httpclient,
+	}
 }
 
 func (uc uploadClient) Upload(file InputFile) (OutputFile, apperrors.AppError){
-	return OutputFile{},nil
+
+	req, err := uc.prepareRequest(file)
+	if err != nil {
+		return OutputFile{}, err
+	}
+
+	res, err := uc.executeRequest(req)
+	if err != nil {
+		return OutputFile{}, err
+	}
+
+	return uc.unmarshal(res)
+}
+
+func (uc uploadClient) prepareRequest(file InputFile) (*http.Request, apperrors.AppError){
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	part, err := writer.CreateFormFile(AssetFieldName, file.Name)
+	if err != nil {
+		return nil, apperrors.Internal("Failed to create multipart content.")
+	}
+
+	_, err = part.Write(file.Contents)
+	if err != nil {
+		return nil, apperrors.Internal("Failed to write file contents.")
+	}
+
+	writer.Close()
+
+	req, err := http.NewRequest(http.MethodPost, uc.uploadServiceUrl.String(), body)
+	if err != nil {
+		return nil, apperrors.Internal("Failed to create request.")
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	return req, nil
+}
+
+func (uc uploadClient) executeRequest(r *http.Request) (*http.Response, apperrors.AppError) {
+	res, err := uc.httpClient.Do(r)
+	if err != nil {
+		return nil, apperrors.Internal("Failed to execute request.")
+	}
+
+	switch res.StatusCode {
+	case http.StatusOK:
+		return res, nil
+	case http.StatusNotFound:
+		return nil, apperrors.NotFound("Upload service call failed.")
+	default:
+		return nil, apperrors.Internal("Failed to call Upload Service.")
+	}
+}
+
+func (uc uploadClient) unmarshal(r *http.Response) (OutputFile, apperrors.AppError) {
+
+	b, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		return OutputFile{}, apperrors.Internal("Failed to read request body.")
+	}
+
+	defer r.Body.Close()
+
+	var outputFile OutputFile
+	err = json.Unmarshal(b, &outputFile)
+
+	if err != nil {
+		return OutputFile{}, apperrors.Internal("Failed to unmarshal body.")
+	}
+
+	return outputFile, nil
 }
--- components/application-registry/internal/metadata/specification/upload/client_test.go
+++ components/application-registry/internal/metadata/specification/upload/client_test.go
@@ -1,6 +1,10 @@
 package upload
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/kyma-project/kyma/components/application-registry/internal/httpconsts"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net/http"
@@ -16,7 +20,7 @@ func TestUploadClient(t *testing.T){
 	t.Run("Should upload single file", func (t *testing.T){
 		// given
 		testServer := getTestServer(t)
-		client := http.Client{}
+		client := &http.Client{}
 		u, err := url.Parse(testServer.URL)
 		require.NoError(t, err)
 		uploadClient := NewUploadClient(u, client)
@@ -24,17 +28,17 @@ func TestUploadClient(t *testing.T){
 		// when
 		input := InputFile{
 			Directory: "testDir",
-			Name: "filename",
+			Name: "testfile",
 			Contents: testContent,
 		}
 		output, err := uploadClient.Upload(input)
-
-		assert.Equal(t, output.Bucket, "test bucket")
-		assert.Equal(t, output.FileName, "testFile")
-		assert.Equal(t, output.RemotePath, "testPath")
+		require.NoError(t, err)
 
 		// then
-
+		assert.Equal(t, output.Bucket, "testBucket")
+		assert.Equal(t, output.FileName, "testFile")
+		assert.Equal(t, output.RemotePath, "testDir/testPath")
+		assert.Equal(t, output.Size, 10)
 	})
 
 	t.Run("Should fail when uploading file failed", func (t *testing.T){
@@ -51,20 +55,45 @@ func getTestServer(t *testing.T) *httptest.Server{
 	return httptest.NewServer(http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			err := r.ParseMultipartForm(32 << 20)
+			defer func() {
+				err := r.MultipartForm.RemoveAll()
+				if err != nil {
+					logrus.Error("Cleaning up multipart form data failed.")
+				}
+			}()
+
+			defer r.Body.Close()
+
 			require.NoError(t, err)
 			require.NotNil(t, r.MultipartForm)
 
 			files := r.MultipartForm.File
 			require.Equal(t, len(files), 1)
 
-			filesList, found := files["uploadedfile"]
+			filesList, found := files[AssetFieldName]
 			require.True(t, found)
 			require.Equal(t, len(filesList), 1)
 
 			fileHeader := filesList[0]
-			assert.Equal(t, fileHeader.Filename, "test.json")
+			assert.Equal(t, fileHeader.Filename, "testfile")
 			assert.NotZero(t, fileHeader.Size)
 
+			outputFile := OutputFile{
+				FileName: "testFile",
+				RemotePath: "testDir/testPath",
+				Bucket: "testBucket",
+				Size: 10,
+			}
+
+			var b bytes.Buffer
+			err = json.NewEncoder(&b).Encode(outputFile)
+			require.NoError(t, err)
+
+			w.Header().Set(httpconsts.HeaderContentType, httpconsts.ContentTypeApplicationJson)
 			w.WriteHeader(http.StatusOK)
+
+			w.Write(b.Bytes())
+
 	}))
+
 }
