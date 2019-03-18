290b48e7ffd2539621b3273e9ae7af4c79327b28
Szymon Janota <szymon.janota@sap.com>
2019-03-18 08:36:56 +0100

    Fix kubeless-integration error reporting

--- tests/end-to-end/kubeless-integration/test-kubeless.go
+++ tests/end-to-end/kubeless-integration/test-kubeless.go
@@ -388,7 +388,7 @@ func publishEvent(testID string) {
 	cmd := exec.Command("curl", "-s", "http://event-bus-publish:8080/v1/events", "-H", "Content-Type: application/json", "-d", `{"source-id": "dummy", "event-type": "test", "event-type-version": "v1", "event-time": "0001-01-01T00:00:00+00:00", "data": "`+testID+`"}`)
 	stdoutStderr, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatal("Unable to publish event:\n", string(stdoutStderr))
+		log.Fatalf("Unable to publish event: %s\n", string(stdoutStderr))
 	}
 }
 
