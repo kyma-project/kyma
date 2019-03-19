b7e1766355d506f069ea94443344906fd705bcef
Szymon Gibala <szymon.gibala@sap.com>
2019-03-19 15:42:53 +0100

    Verbose test output

--- tests/application-gateway-tests/scripts/executor-entrypoint.sh
+++ tests/application-gateway-tests/scripts/executor-entrypoint.sh
@@ -4,7 +4,7 @@ res=0
 echo "Application Gateway tests"
 
 echo "Run proxy tests"
-./proxytestsexecutor.test
+./proxytestsexecutor.test -test.v
 res=$((res+$?))
 
 exit ${res}
--- tests/application-gateway-tests/scripts/helm-test-entrypoint.sh
+++ tests/application-gateway-tests/scripts/helm-test-entrypoint.sh
@@ -4,7 +4,7 @@ res=0
 echo "Application Gateway tests"
 
 echo "Run proxy tests"
-./proxyhelmtests.test
+./proxyhelmtests.test -test.v
 res=$((res+$?))
 
 exit ${res}
