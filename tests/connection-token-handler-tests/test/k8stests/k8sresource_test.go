2c2d0a2ab3f2ba86523fbffa279d601cfca5554d
Mariusz Szymanski <mariusz.szymanski@sap.com>
2019-03-20 09:11:29 +0100

    minor changes

--- tests/connection-token-handler-tests/test/k8stests/k8sresource_test.go
+++ tests/connection-token-handler-tests/test/k8stests/k8sresource_test.go
@@ -31,7 +31,7 @@ func TestTokenRequests(t *testing.T) {
 		require.NoError(t, e)
 
 		//then
-		tokenRequest = waitUntilTokenRequestIsProperlyCreated(t, client, tokenRequest)
+		tokenRequest = waitForToken(t, client, tokenRequest)
 
 		assert.NotEmpty(t, tokenRequest.Status.Token)
 	})
@@ -46,19 +46,20 @@ func TestTokenRequests(t *testing.T) {
 		require.NoError(t, e)
 
 		//then
-		tokenRequest = waitUntilTokenRequestIsProperlyCreated(t, client, tokenRequest)
+		tokenRequest = waitForToken(t, client, tokenRequest)
 
 		assert.NotEmpty(t, tokenRequest.Status.Token)
 		assert.Equal(t, tokenRequest.Context.Tenant, tenant)
 		assert.Equal(t, tokenRequest.Context.Group, group)
 	})
 }
-func waitUntilTokenRequestIsProperlyCreated(t *testing.T, client testkit.K8sResourcesClient, tokenRequest *v1alpha1.TokenRequest) *v1alpha1.TokenRequest {
+
+func waitForToken(t *testing.T, client testkit.K8sResourcesClient, tokenRequest *v1alpha1.TokenRequest) *v1alpha1.TokenRequest {
 	isReady := &tokenRequest.Status != nil
 
 	var e error
 
-	for i := 0; isReady == false && i < retries; i++ {
+	for i := 0; !isReady && i < retries; i++ {
 		time.Sleep(waitTime)
 		tokenRequest, e = client.GetTokenRequest(tokenRequest.Name, v1.GetOptions{})
 		require.NoError(t, e)
