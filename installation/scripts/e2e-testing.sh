b1485330ba5d7ab487564bfeafe722205006e46f
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2019-03-20 11:55:53 +0100

    fix shellcheck errors

--- installation/scripts/e2e-testing.sh
+++ installation/scripts/e2e-testing.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source ${ROOT_PATH}/utils.sh
-source ${ROOT_PATH}/testing-common.sh
+source "${ROOT_PATH}/utils.sh"
+source "${ROOT_PATH}/testing-common.sh"
 
 
 cleanupHelmE2ERelease () {
     local release=$1
     log 'Running cleanup'
-    helm del --purge  $release
-    while helm list --deleting 2>/dev/null | grep $release ; do
+    helm del --purge "${release}"
+    local deleteErr=$?
+    if [ ${deleteErr} -ne 0 ]
+    then
+      log "FAILED cleaning release.\n" red
+      return 1
+    fi
+    while helm list --deleting 2>/dev/null | grep "${release}" ; do
         sleep 1
         echo .
     done
@@ -22,7 +28,10 @@ echo "-------------------------------"
 cleanupHelmTestPods end-to-end
 cleanupE2EErr=$?
 
-
+if [ ${cleanupE2EErr} -ne 0 ]
+then
+    exit 1
+fi
 
 echo "----------------------------"
 echo "- E2E Testing Kyma..."
@@ -30,26 +39,20 @@ echo "----------------------------"
 
 exitCode=0
 
-for testcase in $(ls -d ${ROOT_PATH}/../../tests/end-to-end/*/deploy/chart/*)
-do
-    release=$(basename $testcase)
-    cleanupHelmE2ERelease $release
 
-    if [ ${cleanupE2EErr} -ne 0 ]
-    then
-        exit 1
-    fi
+testcase="${ROOT_PATH}"/../../tests/end-to-end/backup-restore-test/deploy/chart/backup-test
+release=$(basename "$testcase")
 
-    helm install $testcase --name $release --namespace end-to-end
-    helm test $release --timeout 10000
-    testResult=$?
-    if [ $testResult -eq 0 ]
-    then
-        releasesToClean="$releasesToClean $release"
-    else
-        exitCode=$testResult
-    fi
-done
+cleanupHelmE2ERelease "${release}"
+helm install "$testcase" --name "${release}" --namespace end-to-end
+helm test "${release}" --timeout 10000
+testResult=$?
+if [ $testResult -eq 0 ]
+then
+    releasesToClean="$releasesToClean ${release}"
+else
+    exitCode=$testResult
+fi
 
 checkAndCleanupTest end-to-end
 cleanupResult=$?
@@ -60,7 +63,7 @@ fi
 
 
 for release in $releasesToClean; do
-    cleanupHelmE2ERelease $release
+    cleanupHelmE2ERelease "${release}"
     cleanupResult=$?
     if [ $cleanupResult -ne 0 ]
     then
