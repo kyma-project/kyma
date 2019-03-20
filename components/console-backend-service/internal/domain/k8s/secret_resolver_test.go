0bd6a2bac75b9b4a0587eb65cf9b8c262f9b9cad
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-03-20 09:35:54 +0100

    Fix existing test

--- components/console-backend-service/internal/domain/k8s/secret_resolver_test.go
+++ components/console-backend-service/internal/domain/k8s/secret_resolver_test.go
@@ -4,13 +4,12 @@ import (
 	"errors"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/k8s"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/k8s/automock"
+	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlschema"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"k8s.io/api/core/v1"
-	k8sTesting "k8s.io/client-go/testing"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	k8sTesting "k8s.io/client-go/testing"
 	"testing"
 	"time"
 )
@@ -19,35 +18,37 @@ func failingReactor(action k8sTesting.Action) (handled bool, ret runtime.Object,
 	return true, nil, errors.New("custom error")
 }
 
-func TestSecretResolver(t *testing.T) {
+func TestSecretResolver_SecretQuery(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		t1 := time.Unix(1, 0)
+		expected := &gqlschema.Secret{
+			Name:         "Test",
+			Namespace:    "TestNS",
+			CreationTime: t1,
+			Annotations:  gqlschema.JSON{"second-annot": "content"},
+		}
+
+		name := "name"
+		namespace := "namespace"
+		resource := &v1.Secret{}
+		resourceGetter := automock.NewSecretSvc()
+		resourceGetter.On("Find", name, namespace).Return(resource, nil).Once()
+		defer resourceGetter.AssertExpectations(t)
+
+		converter := automock.NewGQLSecretConverter()
+		converter.On("ToGQL", resource).Return(expected, nil).Once()
+		defer converter.AssertExpectations(t)
+
+		resolver := k8s.NewSecretResolver(resourceGetter)
+		resolver.SetSecretConverter(converter)
+
+		result, err := resolver.SecretQuery(nil, name, namespace)
+
+		require.NoError(t, err)
+		assert.Equal(t, expected, result)
+	})
 	// GIVEN
-	t1 := time.Unix(1, 0)
-	expected := &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:              "my-secret",
-			Namespace:         "production",
-			CreationTimestamp: metav1.NewTime(t1),
-			Annotations:       map[string]string{"second-annot": "content"},
-		},
-	}
-	resource := &v1.Secret{}
-	resourceGetter := automock.NewSecretSvc()
-	name := "name"
-	namespace := "namespace"
-	resourceGetter.On("Find", name, namespace).Return(resource, nil).Once()
-	defer resourceGetter.AssertExpectations(t)
 
-	converter := automock.NewGQLSecretConverter()
-	converter.On("ToGQL", resource).Return(expected, nil).Once()
-	defer converter.AssertExpectations(t)
-
-	resolver := k8s.NewSecretResolver(resourceGetter)
-	resolver.SetSecretConverter(converter)
-
-	result, err := resolver.SecretQuery(nil, name, namespace)
-
-	require.NoError(t, err)
-	assert.Equal(t, expected, result)
 	//resolver := k8s.NewSecretResolver(fakeClientSet.CoreV1())
 	// WHEN
 	//actualSecret, err := resolver.SecretQuery(context.Background(), "my-secret", "production")
