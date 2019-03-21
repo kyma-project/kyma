08798968b0d475dbb89e04d05f2cc4071bca2bb9
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-03-21 16:18:35 +0100

    Change TLS installation method

--- installation/scripts/install-tiller.sh
+++ installation/scripts/install-tiller.sh
@@ -2,9 +2,6 @@
 
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo "- Enabling SSL for Tiller"
-bash "${CURRENT_DIR}/tiller-ssl.sh"
-
 echo "- Installing Tiller..."
 kubectl apply -f ${CURRENT_DIR}/../resources/tiller.yaml
 bash ${CURRENT_DIR}/is-ready.sh kube-system name tiller
