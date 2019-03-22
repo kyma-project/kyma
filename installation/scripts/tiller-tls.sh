78088f08eb6adefb73d4ec1e7421ac130bc983da
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-03-22 11:09:30 +0100

    Fix secret handling

--- installation/scripts/tiller-tls.sh
+++ installation/scripts/tiller-tls.sh
@@ -15,6 +15,6 @@ do
 done
 
 echo "---> Get Helm secrets and put then into $(helm home)"
-kubectl get -n kyma-installer secret helm-secret -o json | jq '.data."global.helm.ca.crt"' > "$(helm home)/ca.pem"
-kubectl get -n kyma-installer secret helm-secret -o json | jq '.data."global.helm.tls.crt"' > "$(helm home)/cert.pem"
-kubectl get -n kyma-installer secret helm-secret -o json | jq '.data."global.helm.tls.key"' > "$(helm home)/key.pem"
+kubectl get -n kyma-installer secret helm-secret -o json | jq '.data."global.helm.ca.crt"' | tr -d '"' | base64 -D > "$(helm home)/ca.pem"
+kubectl get -n kyma-installer secret helm-secret -o json | jq '.data."global.helm.tls.crt"' | tr -d '"' | base64 -D > "$(helm home)/cert.pem"
+kubectl get -n kyma-installer secret helm-secret -o json | jq '.data."global.helm.tls.key"' | tr -d '"' | base64 -D > "$(helm home)/key.pem"
