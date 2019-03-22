fa7246e6a132eb622655008007d5f5ce727fc504
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-03-22 10:45:56 +0100

    Add waiting loop

--- installation/scripts/tiller-tls.sh
+++ installation/scripts/tiller-tls.sh
@@ -2,6 +2,18 @@
 
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+while :
+do
+  if [[ $(kubectl get -n kyma-installer secret helm-secret) ]]
+    then
+      echo "---> Secrets have been created"
+      break
+    else
+      echo "---> Secrets not present. Waiting 5s..."
+      sleep 5
+    fi
+done
+
 echo "---> Get Helm secrets and put then into $(helm home)"
 kubectl get -n kyma-installer secret helm-secret -o json | jq '.data."global.helm.ca.crt"' > "$(helm home)/ca.pem"
 kubectl get -n kyma-installer secret helm-secret -o json | jq '.data."global.helm.tls.crt"' > "$(helm home)/cert.pem"
