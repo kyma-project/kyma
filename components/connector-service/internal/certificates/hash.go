4dc27d60f3e9872d5497c963ebb39d2002371d31
Szymon Gibala <szymon.gibala@sap.com>
2019-03-25 11:47:06 +0100

    Adjust acceptance tests

--- components/connector-service/internal/certificates/hash.go
+++ components/connector-service/internal/certificates/hash.go
@@ -2,25 +2,20 @@ package certificates
 
 import (
 	"crypto/sha256"
-	"crypto/x509"
 	"encoding/hex"
 	"encoding/pem"
 
 	"github.com/kyma-project/kyma/components/connector-service/internal/apperrors"
 )
 
-func FingerprintSHA256(rawPem []byte) (string, error) {
+// FingerprintSHA256 decodes pem block and returns fingerprint generated using SHA 256 algorithm
+func FingerprintSHA256(rawPem []byte) (string, apperrors.AppError) {
 	block, _ := pem.Decode(rawPem)
 	if block == nil {
-		return "", apperrors.Internal("Faield to decode pem block")
+		return "", apperrors.Internal("Failed to decode pem block.")
 	}
 
-	certificate, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		return "", apperrors.Internal("Failed to parse certificate: %s", err.Error())
-	}
-
-	sha := sha256.Sum256(certificate.Raw)
+	sha := sha256.Sum256(block.Bytes)
 
 	return hex.EncodeToString(sha[:]), nil
 }
