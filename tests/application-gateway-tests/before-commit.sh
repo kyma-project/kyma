cc51f73ae0ccd100e39313295764accf482d36d6
Szymon Gibała <szymongib@gmail.com>
2019-03-27 15:11:48 +0100

    Acceptance tests for Application Gateway (#3159)

--- tests/application-gateway-tests/before-commit.sh
+++ tests/application-gateway-tests/before-commit.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-readonly CI_FLAG=ci
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 INVERTED='\033[7m'
@@ -13,6 +11,8 @@ echo "PATH: " + $PATH
 echo "GOPATH:" + $GOPATH
 echo -e "${NC}"
 
+goFilesToCheck=$(find . -type f -name "*.go" | egrep -v "\/vendor\/|_*/automock/|_*/testdata/|/pkg\/|_*export_test.go")
+
 ##
 # DEP ENSURE
 ##
@@ -22,24 +22,6 @@ if [ ${ensureResult} != 0 ]; then
 	echo -e "${RED}✗ dep ensure -v --vendor-only${NC}\n$ensureResult${NC}"
 	exit 1
 else echo -e "${GREEN}√ dep ensure -v --vendor-only${NC}"
-fi
-
-##
-# GO BUILD
-##
-buildEnv=""
-if [ "$1" == "$CI_FLAG" ]; then
-	# build binary statically
-	buildEnv="env CGO_ENABLED=0"
-fi
-
-${buildEnv} go build -o manager ./cmd/manager/
-
-goBuildResult=$?
-if [ ${goBuildResult} != 0 ]; then
-	echo -e "${RED}✗ go build${NC}\n$goBuildResult${NC}"
-	exit 1
-else echo -e "${GREEN}√ go build${NC}"
 fi
 
 ##
@@ -54,34 +36,28 @@ else echo -e "${GREEN}√ dep status${NC}"
 fi
 
 ##
-# GO TEST
+# GO IMPORTS
 ##
-echo "? go test"
-go test ./...
-# Check if tests passed
-if [ $? != 0 ]; then
-	echo -e "${RED}✗ go test\n${NC}"
+go build -o goimports-vendored ./vendor/golang.org/x/tools/cmd/goimports
+buildGoImportResult=$?
+if [ ${buildGoImportResult} != 0 ]; then
+	echo -e "${RED}✗ go build goimports${NC}\n$buildGoImportResult${NC}"
 	exit 1
-else echo -e "${GREEN}√ go test${NC}"
 fi
 
-goFilesToCheck=$(find . -type f -name "*.go" | egrep -v "\/vendor\/|_*/automock/|_*/testdata/|_*export_test.go")
+goImportsResult=$(echo "${goFilesToCheck}" | xargs -L1 ./goimports-vendored -w -l)
+rm goimports-vendored
 
-#
-# GO FMT
-#
-goFmtResult=$(echo "${goFilesToCheck}" | xargs -L1 go fmt)
-if [ $(echo ${#goFmtResult}) != 0 ]
-	then
-    	echo -e "${RED}✗ go fmt${NC}\n$goFmtResult${NC}"
-    	exit 1;
-	else echo -e "${GREEN}√ go fmt${NC}"
+if [ $(echo ${#goImportsResult}) != 0 ]; then
+	echo -e "${RED}✗ goimports and fmt${NC}\n$goImportsResult${NC}"
+	exit 1
+else echo -e "${GREEN}√ goimports and fmt${NC}"
 fi
 
 ##
 # GO VET
 ##
-packagesToVet=("./cmd/..." "./pkg/...")
+packagesToVet=("./test/...")
 
 for vPackage in "${packagesToVet[@]}"; do
 	vetResult=$(go vet ${vPackage})
