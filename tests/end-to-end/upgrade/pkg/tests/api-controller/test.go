6d569192e5d3e3307d4b219475037d259cccdbf9
Szymon Janota <szymon.janota@sap.com>
2019-04-01 14:01:58 +0200

    Lint

--- tests/end-to-end/upgrade/pkg/tests/api-controller/test.go
+++ tests/end-to-end/upgrade/pkg/tests/api-controller/test.go
@@ -9,19 +9,23 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+// Test tests the API controller business logic after Kyma upgrade phase
 type Test struct {
 	upstream backupe2e.ApiControllerTest
 }
 
+// New creates new instance of Test
 func New(gatewayInterface gateway.Interface, coreInterface kubernetes.Interface, kubelessInterface kubeless.Interface, domainName string) Test {
 	upstream := backupe2e.NewApiControllerTest(gatewayInterface, coreInterface, kubelessInterface, domainName)
 	return Test{upstream}
 }
 
+// CreateResources creates resources for tests
 func (t Test) CreateResources(stop <-chan struct{}, log logrus.FieldLogger, namespace string) error {
 	return t.upstream.CreateResourcesError(namespace)
 }
 
+// TestResources tests if resources are working properly after upgrade
 func (t Test) TestResources(stop <-chan struct{}, log logrus.FieldLogger, namespace string) error {
 	return t.upstream.TestResourcesError(namespace)
 }
