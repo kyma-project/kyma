2520b5c387821a1401123dd7d3c2adedf08d5244
adamwalach <adam.walach@sap.com>
2019-04-01 14:24:32 +0200

    Add TLS support

--- components/helm-broker/internal/helm/client_test.go
+++ components/helm-broker/internal/helm/client_test.go
@@ -2,10 +2,15 @@ package helm_test
 
 import (
 	"context"
+	"encoding/pem"
+	"io/ioutil"
+	"log"
 	"net"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/SpectoLabs/hoverfly/core/certs"
 	"github.com/ghodss/yaml"
 	"github.com/kyma-project/kyma/components/helm-broker/internal"
 	"github.com/kyma-project/kyma/components/helm-broker/internal/helm"
@@ -13,14 +18,55 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"k8s.io/helm/pkg/proto/hapi/chart"
 	hapi_release5 "k8s.io/helm/pkg/proto/hapi/release"
 	"k8s.io/helm/pkg/proto/hapi/services"
 )
 
+var (
+	keyFile  string
+	certFile string
+)
+
+func TestMain(m *testing.M) {
+	setupCerts()
+	code := m.Run()
+	cleanupCerts()
+	os.Exit(code)
+}
+
+func setupCerts() {
+	certOut, err := ioutil.TempFile("/tmp/", "certFile")
+	if err != nil {
+		log.Fatal(err)
+	}
+	keyOut, err := ioutil.TempFile("/tmp/", "keyFile")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = createDummyKeyPair(keyOut, certOut)
+	keyOut.Close()
+	certOut.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	keyFile = keyOut.Name()
+	certFile = certOut.Name()
+
+}
+func cleanupCerts() {
+	os.Remove(keyFile)
+	os.Remove(certFile)
+}
+
 func TestClientInstallSuccess(t *testing.T) {
 	// given
-	fakeTiller := &fakeTillerSvc{}
+	fakeTiller := &fakeTillerSvc{
+		certFile: certFile,
+		keyFile:  keyFile,
+	}
 	fakeTiller.SetUp(t)
 
 	cVals := internal.ChartValues{
@@ -30,6 +76,9 @@ func TestClientInstallSuccess(t *testing.T) {
 	hClient := helm.NewClient(helm.Config{
 		TillerHost:              fakeTiller.Host,
 		TillerConnectionTimeout: time.Second,
+		TillerTLSCrt:            certFile,
+		TillerTLSKey:            keyFile,
+		TillerTLSInsecure:       true,
 	}, spy.NewLogDummy())
 
 	// when
@@ -52,18 +101,22 @@ func TestClientInstallSuccess(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, fakeTiller.GotInstReleaseReq.Values, &chart.Config{Raw: string(b)})
 
-	// Clean-up
+	// clean-up
 	fakeTiller.TearDown(t)
 }
 
 func TestClientDeleteSuccess(t *testing.T) {
-	// given
-	fakeTiller := &fakeTillerSvc{}
+	fakeTiller := &fakeTillerSvc{
+		certFile: certFile,
+		keyFile:  keyFile,
+	}
 	fakeTiller.SetUp(t)
-
 	hClient := helm.NewClient(helm.Config{
 		TillerHost:              fakeTiller.Host,
 		TillerConnectionTimeout: time.Second,
+		TillerTLSCrt:            certFile,
+		TillerTLSKey:            keyFile,
+		TillerTLSInsecure:       true,
 	}, spy.NewLogDummy())
 
 	// when
@@ -75,7 +128,7 @@ func TestClientDeleteSuccess(t *testing.T) {
 	assert.NotNil(t, fakeTiller.GotDelReleaseReq)
 	assert.Equal(t, fakeTiller.GotDelReleaseReq.Name, "r-name")
 
-	// Clean-up
+	// clean-up
 	fakeTiller.TearDown(t)
 }
 
@@ -84,8 +137,11 @@ type fakeTillerSvc struct {
 	GotInstReleaseReq *services.InstallReleaseRequest
 	GotDelReleaseReq  *services.UninstallReleaseRequest
 
-	grpcSvc      *grpc.Server
-	Host         string
+	grpcSvc  *grpc.Server
+	Host     string
+	keyFile  string
+	certFile string
+
 	serverErr    error
 	serverClosed chan struct{}
 }
@@ -96,8 +152,10 @@ func (s *fakeTillerSvc) SetUp(t *testing.T) {
 	require.NoError(t, err)
 
 	s.Host = lis.Addr().String()
+	credentials, err := credentials.NewServerTLSFromFile(s.certFile, s.keyFile)
+	require.NoError(t, err)
 
-	s.grpcSvc = grpc.NewServer()
+	s.grpcSvc = grpc.NewServer(grpc.Creds(credentials))
 	services.RegisterReleaseServiceServer(s.grpcSvc, s)
 
 	go func() {
@@ -141,4 +199,23 @@ func fixChart() *chart.Chart {
 			Version: "1.0.0",
 		},
 	}
+}
+
+func createDummyKeyPair(keyOut, certOut *os.File) error {
+	cert, priv, err := certs.NewCertificatePair("Kyma", "SAP", 12*time.Hour)
+	if err != nil {
+		return err
+	}
+
+	err = pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw})
+	if err != nil {
+		return err
+	}
+
+	err = pem.Encode(keyOut, certs.PemBlockForKey(priv))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
