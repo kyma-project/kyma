5d2ae2b06386c83af2e826e99e89076e5c4c99f5
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2019-04-04 09:20:09 +0200

    remove disabled tests

--- tests/end-to-end/upgrade/main.go
+++ tests/end-to-end/upgrade/main.go
@@ -18,11 +18,8 @@ import (
 	"github.com/kyma-project/kyma/tests/end-to-end/upgrade/internal/platform/logger"
 	"github.com/kyma-project/kyma/tests/end-to-end/upgrade/internal/platform/signal"
 	"github.com/kyma-project/kyma/tests/end-to-end/upgrade/internal/runner"
-	assetstore "github.com/kyma-project/kyma/tests/end-to-end/upgrade/pkg/tests/asset-store"
-	cms "github.com/kyma-project/kyma/tests/end-to-end/upgrade/pkg/tests/cms"
 	monitoring "github.com/kyma-project/kyma/tests/end-to-end/upgrade/pkg/tests/monitoring"
 	servicecatalog "github.com/kyma-project/kyma/tests/end-to-end/upgrade/pkg/tests/service-catalog"
-	"k8s.io/client-go/dynamic"
 )
 
 // Config holds application configuration
@@ -74,9 +71,6 @@ func main() {
 	appBrokerCli, err := ab.NewForConfig(k8sConfig)
 	fatalOnError(err, "while creating Application Broker clientset")
 
-	dynamicCli, err := dynamic.NewForConfig(k8sConfig)
-	fatalOnError(err, "while creating K8s dynamic clientset")
-
 	// Register tests. Convention:
 	// <test-name> : <test-instance>
 
@@ -94,8 +88,6 @@ func main() {
 		"ApplicationBrokerUpgradeTest": servicecatalog.NewAppBrokerUpgradeTest(scCli, k8sCli, buCli, appBrokerCli, appConnectorCli),
 		"GrafanaUpgradeTest":           grafanaUpgradeTest,
 		"MetricsUpgradeTest":           metricUpgradeTest,
-		"AssetStoreUpgradeTest":        assetstore.NewAssetStoreUpgradeTest(dynamicCli),
-		"CmsUpgradeTest":               cms.NewHeadlessCmsUpgradeTest(dynamicCli),
 	}
 
 	// Execute requested action
