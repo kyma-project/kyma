6ecbf54f09b10aa2aafe2196535df660e822ec57
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-04-05 10:37:35 +0200

    Fix e2e testing script

--- installation/scripts/e2e-testing.sh
+++ installation/scripts/e2e-testing.sh
@@ -52,8 +52,12 @@ cleanupHelmE2ERelease "${release}"
 ADMIN_EMAIL=$(kubectl get secret admin-user -n kyma-system -o jsonpath="{.data.email}" | base64 --decode)
 ADMIN_PASSWORD=$(kubectl get secret admin-user -n kyma-system -o jsonpath="{.data.password}" | base64 --decode)
 
-helm install "$testcase" --name "${release}" --namespace end-to-end --set global.ingress.domainName="${DOMAIN}" --set-file global.adminEmail=<(echo -n "${ADMIN_EMAIL}") --set-file global.adminPassword=<(echo -n "${ADMIN_PASSWORD}")
-helm test "${release}" --timeout 10000
+if [  -f "$(helm home)/ca.pem" ]; then
+    local HELM_ARGS="--tls"
+fi
+
+helm install "$testcase" --name "${release}" --namespace end-to-end --set global.ingress.domainName="${DOMAIN}" --set-file global.adminEmail=<(echo -n "${ADMIN_EMAIL}") --set-file global.adminPassword=<(echo -n "${ADMIN_PASSWORD}") $HELM_ARGS
+helm test "${release}" --timeout 10000 $HELM_ARGS
 testResult=$?
 if [ $testResult -eq 0 ]
 then
