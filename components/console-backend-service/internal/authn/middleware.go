3f2059a9704e03801f201ff76b5236eb0d06f589
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-04-05 15:49:42 +0200

    Add directives to subsribe events

--- components/console-backend-service/internal/authn/middleware.go
+++ components/console-backend-service/internal/authn/middleware.go
@@ -22,7 +22,6 @@ type contextKey struct {
 func AuthMiddleware(a authenticator.Request) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 			wsProtocolHeader := r.Header.Get("sec-websocket-protocol")
 			if wsProtocolHeader != "" {
 				wsProtocolParts := strings.Split(wsProtocolHeader, ",")
@@ -34,9 +33,6 @@ func AuthMiddleware(a authenticator.Request) func(http.Handler) http.Handler {
 				r.Header.Set("Authorization", "Bearer "+wsToken)
 				r.Header.Set("sec-websocket-protocol", wsProtocol)
 			}
-
-			//r.Header.Set("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjIwOTUxZDAyNTFjMjE0NzY1MmQ0MDQ0Y2E3ODBiZjM5MWFhODFhNjMifQ.eyJpc3MiOiJodHRwczovL2RleC5reW1hLmxvY2FsIiwic3ViIjoiQ2lCcU5tWnFZWHBpTUdNMGEzSm9hSEU1WlhsbmJqZGhlWFZvTW1VNGVXUjBZaElGYkc5allXdyIsImF1ZCI6WyJreW1hLWNsaWVudCIsImNvbnNvbGUiXSwiZXhwIjoxNTUzNzk2NjU4LCJpYXQiOjE1NTM3Njc4NTgsImF6cCI6ImNvbnNvbGUiLCJub25jZSI6ImVlNWRlOWY4ZmE4NTQ4MmNiMjg3ZWU1YTAyNWU4NWJmIiwiYXRfaGFzaCI6InZraU1hdHhjeGp5OEFKUGltYjVNd1EiLCJlbWFpbCI6ImFkbWluQGt5bWEuY3giLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6ImFkbWluIn0.kSsFn_9TYb3HK49YJqNRUR8cJY3zBJmKIcLFJxmgTjIeylhsgvhlNRGP3_xm6n39x5GzcDm94PM1QnKdMbdAsgxY6yfXMcJBKNXPpSN7497ocicwoXKPXR6P8H8d-34PumQ6iuWBoYdupkxlnlVbXXbBD7EvJ8KhBRvud7QtTv9W7j0wNtxfBOtz672SikvdTbsGVpFunXs3qyTzVfWaD3-I0539uCGR43XVoSkqoubMNxxXFxe95IzkccDH1URzSdxufDR75wnW7DOONM6FPZcF0XyM7V4L8pf6svOj_VHSZnIPBh68cHGrHw9JHEVM3FgwGaUQhswR49yVbNzm4A")
-			//r.Header.Set("sec-websocket-protocol", "graphql-ws")
 
 			u, ok, err := a.AuthenticateRequest(r)
 			if err != nil {
