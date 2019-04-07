31ccaccb4668527d7a95bde18bf4019ae824b8e6
Michał Hudy <26445970+michal-hudy@users.noreply.github.com>
2019-04-07 21:26:54 +0200

    Update dependencies

--- tests/asset-store/testsuite/testdata.go
+++ tests/asset-store/testsuite/testdata.go
@@ -137,10 +137,10 @@ type uploadedFile struct {
 func uploadedFiles(ref v1alpha2.AssetStatusRef, ownerName, ownerKind string) []uploadedFile {
 	var files []uploadedFile
 
-	for _, singleAsset := range ref.Files {
-		url := fmt.Sprintf("%s/%s", ref.BaseURL, singleAsset.Name)
+	for _, file := range ref.Files {
+		url := fmt.Sprintf("%s/%s", ref.BaseURL, file.Name)
 		files = append(files, uploadedFile{
-			AssetPath: singleAsset.Name,
+			AssetPath: file.Name,
 			URL:       url,
 			Owner: uploadedFileOwner{
 				Name: ownerName,
