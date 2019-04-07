016e87eff45473a35086314c99cf01bdbd6b4efa
Marco Bebway <marco.bebway@sap.com>
2019-04-07 23:02:15 +0200

    Prefix Knative channel name with a lowercase alphabet character to fix the istio virtual service name issue (#3619)

--- components/event-bus/internal/knative/util/util.go
+++ components/event-bus/internal/knative/util/util.go
@@ -33,9 +33,11 @@ func encodeChannelName(sourceID, eventType, eventTypeVersion *string) string {
 }
 
 // GetChannelName function returns a unique hash for the knative channel name from the given event components
+// because of a limitation in the current knative version, if the channel name starts with a number, the corresponding
+// istio-virtualservice will not be created, in order to mitigate that, we prefix the channel name with the letter 'k'
 func GetChannelName(sourceID, eventType, eventTypeVersion *string) string {
 	channelName := encodeChannelName(sourceID, eventType, eventTypeVersion)
-	return hash.ComputeHash(&channelName)
+	return fmt.Sprintf("k%s", hash.ComputeHash(&channelName))
 }
 
 // GetKnSubscriptionName joins the kySubscriptionName and kySubscriptionNamespace
