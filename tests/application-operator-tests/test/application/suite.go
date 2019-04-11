56a16fd5efc1de85453e8dafb971464a093fd5d0
Karol Furgol <furgolkarol@gmail.com>
2019-04-11 13:01:32 +0200

    fix

--- tests/application-operator-tests/test/application/suite.go
+++ tests/application-operator-tests/test/application/suite.go
@@ -148,8 +148,6 @@ func (ts *TestSuite) getLogsAndCleanup(t *testing.T) {
 
 func (ts *TestSuite) getPodLogs(t *testing.T, pod v1.Pod) {
 
-	t.Log("abcd", ts.application)
-	t.Log("def", testAppName)
 	var testContainer string
 	for _, c := range pod.Spec.Containers {
 		if strings.HasPrefix(c.Name, ts.application) {
