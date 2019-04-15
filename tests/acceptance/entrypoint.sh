14df246247e3591f289df0a2bd422a67e22fa2ea
Szecowka, Adam <adam.szecowka@gmail.com>
2019-04-15 09:21:09 +0200

    to revert

--- tests/acceptance/entrypoint.sh
+++ tests/acceptance/entrypoint.sh
@@ -2,16 +2,16 @@
 
 res=0
 
-echo "Run dex tests"
-./dex.test -test.v
-res=$((res+$?))
+#echo "Run dex tests"
+#./dex.test -test.v
+#res=$((res+$?))
 
 echo "Run servicecatalog tests"
 ./servicecatalog.test -test.v
 res=$((res+$?))
 
-echo "Run application tests"
-./application.test -test.v
-res=$((res+$?))
+#echo "Run application tests"
+#./application.test -test.v
+#res=$((res+$?))
 
 exit ${res}
--- tests/acceptance/servicecatalog/servicecatalog_test.go
+++ tests/acceptance/servicecatalog/servicecatalog_test.go
@@ -29,6 +29,7 @@ const (
 )
 
 func TestServiceCatalogContainsClusterServiceClasses(t *testing.T) {
+	t.SkipNow()
 	for testName, brokerURL := range map[string]string{
 		"Helm Broker": os.Getenv(helmBrokerURLEnvName),
 		// "<next broker>": os.Getenv("<broker url env name>"),
@@ -54,6 +55,7 @@ func TestServiceCatalogContainsClusterServiceClasses(t *testing.T) {
 }
 
 func TestServiceCatalogContainsABServiceClasses(t *testing.T) {
+	t.SkipNow()
 	// given
 	k8sConfig, err := restclient.InClusterConfig()
 	require.NoError(t, err)
