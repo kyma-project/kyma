2f8c941d6ab1aea22323b8bf6c3c4348e3fb8a13
Szymon Gibała <szymongib@gmail.com>
2019-04-18 13:25:04 +0200

    Modify subject format of certificates generated by Connector Service (#3753)

--- components/connector-service/internal/certificates/service_test.go
+++ components/connector-service/internal/certificates/service_test.go
@@ -73,10 +73,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 		certUtils.On("AddCertificateHeaderAndFooter", caCrt.Raw).Return(caCRTBytes)
 		certUtils.On("AddCertificateHeaderAndFooter", clientCRT).Return(clientCRTBytes)
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "", subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "")
 
 		// when
-		encodedCertChain, apperr := certificatesService.SignCSR(rawCSR, appName)
+		encodedCertChain, apperr := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.NoError(t, apperr)
@@ -113,10 +113,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 			On("AddCertificateHeaderAndFooter", rootCACrt.Raw).Return(rootCACrtBytes)
 		certUtils.On("AddCertificateHeaderAndFooter", clientCRT).Return(clientCRTBytes)
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, rootCASecretName, subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, rootCASecretName)
 
 		// when
-		encodedCertChain, apperr := certificatesService.SignCSR(rawCSR, appName)
+		encodedCertChain, apperr := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.NoError(t, apperr)
@@ -143,10 +143,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 		certUtils.On("LoadCSR", rawCSR).Return(csr, nil)
 		certUtils.On("CheckCSRValues", csr, subjectValues).Return(nil)
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "", subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "")
 
 		// when
-		encodedChain, err := certificatesService.SignCSR(rawCSR, appName)
+		encodedChain, err := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.Error(t, err)
@@ -171,10 +171,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 		certUtils.On("AddCertificateHeaderAndFooter", caCrt.Raw).Return(caCRTBytes)
 		certUtils.On("AddCertificateHeaderAndFooter", clientCRT).Return(clientCRTBytes)
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, rootCASecretName, subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, rootCASecretName)
 
 		// when
-		encodedChain, err := certificatesService.SignCSR(rawCSR, appName)
+		encodedChain, err := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.Error(t, err)
@@ -191,10 +191,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 		certUtils := &mocks.CertificateUtility{}
 		certUtils.On("LoadCSR", rawCSR).Return(nil, apperrors.Internal("error"))
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "", subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "")
 
 		// when
-		encodedChain, err := certificatesService.SignCSR(rawCSR, appName)
+		encodedChain, err := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.Error(t, err)
@@ -212,10 +212,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 		certUtils.On("LoadCSR", rawCSR).Return(csr, nil)
 		certUtils.On("CheckCSRValues", csr, subjectValues).Return(apperrors.Forbidden("error"))
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "", subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "")
 
 		// when
-		encodedChain, err := certificatesService.SignCSR(rawCSR, appName)
+		encodedChain, err := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.Error(t, err)
@@ -235,10 +235,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 		certUtils.On("CheckCSRValues", csr, subjectValues).Return(nil)
 		certUtils.On("LoadCert", caCrtEncoded).Return(nil, apperrors.Internal("error"))
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "", subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "")
 
 		// when
-		encodedChain, err := certificatesService.SignCSR(rawCSR, appName)
+		encodedChain, err := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.Error(t, err)
@@ -259,10 +259,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 		certUtils.On("LoadCert", caCrtEncoded).Return(caCrt, nil)
 		certUtils.On("LoadKey", caKeyEncoded).Return(nil, apperrors.Internal("error"))
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "", subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "")
 
 		// when
-		encodedChain, err := certificatesService.SignCSR(rawCSR, appName)
+		encodedChain, err := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.Error(t, err)
@@ -284,10 +284,10 @@ func TestCertificateService_SignCSR(t *testing.T) {
 		certUtils.On("CheckCSRValues", csr, subjectValues).Return(nil)
 		certUtils.On("SignCSR", caCrt, csr, caKey).Return(nil, apperrors.Internal("error"))
 
-		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "", subjectValues)
+		certificatesService := certificates.NewCertificateService(secretsRepository, certUtils, authSecretName, "")
 
 		// when
-		encodedChain, err := certificatesService.SignCSR(rawCSR, appName)
+		encodedChain, err := certificatesService.SignCSR(rawCSR, subjectValues)
 
 		// then
 		require.Error(t, err)
