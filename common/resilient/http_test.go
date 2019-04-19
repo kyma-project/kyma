4d916fd906bab2c2b579019dd4478e678601b9b8
Szymon Janota <szymon.janota@sap.com>
2019-04-19 13:56:00 +0200

    Move tests to separate package

--- common/resilient/http_test.go
+++ common/resilient/http_test.go
@@ -2,6 +2,7 @@ package resilient_test
 
 import (
 	"errors"
+	"github.com/kyma-project/kyma/common/resilient"
 	"net/http"
 	"testing"
 	"time"
@@ -18,7 +19,7 @@ func (mockHttpClientFirstSuccess) Do(req *http.Request) (*http.Response, error) 
 
 func TestHttpClientFirstSuccess(t *testing.T) {
 	mock := &mockHttpClientFirstSuccess{}
-	wrapped := WrapHttpClient(mock, retry.Delay(time.Millisecond), retry.Attempts(5))
+	wrapped := resilient.WrapHttpClient(mock, retry.Delay(time.Millisecond), retry.Attempts(5))
 	resp, err := wrapped.Get("http://example.com/")
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusTeapot, resp.StatusCode)
@@ -39,7 +40,7 @@ func (c *mockHttpClientLateSuccess) Do(req *http.Request) (*http.Response, error
 
 func TestHttpClientLateSuccess(t *testing.T) {
 	mock := &mockHttpClientLateSuccess{successAfter: 3}
-	wrapped := WrapHttpClient(mock, retry.Delay(time.Millisecond), retry.Attempts(5))
+	wrapped := resilient.WrapHttpClient(mock, retry.Delay(time.Millisecond), retry.Attempts(5))
 	resp, err := wrapped.Get("http://example.com/")
 	assert.Nil(t, err)
 	assert.Equal(t, 3, mock.calls)
@@ -57,7 +58,7 @@ func (c *mockHttpClientError) Do(req *http.Request) (*http.Response, error) {
 
 func TestHttpClientError(t *testing.T) {
 	mock := &mockHttpClientError{}
-	wrapped := WrapHttpClient(mock, retry.Delay(time.Millisecond), retry.Attempts(5))
+	wrapped := resilient.WrapHttpClient(mock, retry.Delay(time.Millisecond), retry.Attempts(5))
 	_, err := wrapped.Get("http://example.com/")
 	assert.NotNil(t, err)
 	assert.Equal(t, 5, mock.calls)
