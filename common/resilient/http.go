0b9425d3787c461926fe15027819e433b6429686
Szymon Janota <szymon.janota@sap.com>
2019-04-19 14:52:53 +0200

    Add docs

--- common/resilient/http.go
+++ common/resilient/http.go
@@ -9,19 +9,25 @@ import (
 	retry "github.com/avast/retry-go"
 )
 
+// WrappedHttpClient is a wrapper around HttpClient which retries requests in case of errors. It implements all of public
+// methods available in http.Client
 type WrappedHttpClient struct {
 	underlying HttpClient
 	opts       []retry.Option
 }
 
+// HttpClient is a simplified version of http.Client interface
 type HttpClient interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
+// NewHttpClient returns new WrappedHttpClient with default http.Client as upstream. If you want to configure it in
+// anyway use WrapHttpClient instead.
 func NewHttpClient(opts ...retry.Option) *WrappedHttpClient {
 	return WrapHttpClient(&http.Client{}, opts...)
 }
 
+// WrapHttpClient returns new WrappedHttpClient with given HttpClient as upstream
 func WrapHttpClient(client HttpClient, opts ...retry.Option) *WrappedHttpClient {
 	return &WrappedHttpClient{
 		underlying: client,
@@ -29,9 +35,8 @@ func WrapHttpClient(client HttpClient, opts ...retry.Option) *WrappedHttpClient 
 	}
 }
 
-func (c *WrappedHttpClient) Do(req *http.Request) (*http.Response, error) {
-	var resp *http.Response
-	var err error
+// Do calls Do method of underlying HttpClient and retries according to given options when an error occurs
+func (c *WrappedHttpClient) Do(req *http.Request) (resp *http.Response, err error) {
 	err = retry.Do(func() error {
 		resp, err = c.underlying.Do(req)
 		if err != nil {
@@ -42,7 +47,8 @@ func (c *WrappedHttpClient) Do(req *http.Request) (*http.Response, error) {
 	return resp, err
 }
 
-func (c *WrappedHttpClient) Get(url string) (resp *http.Response, err error) {
+// Get is copied from http.Client to be compliant with its interface. For more documentation see http.Client.Get
+func (c *WrappedHttpClient) Get(url string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -50,7 +56,8 @@ func (c *WrappedHttpClient) Get(url string) (resp *http.Response, err error) {
 	return c.Do(req)
 }
 
-func (c *WrappedHttpClient) Post(url, contentType string, body io.Reader) (resp *http.Response, err error) {
+// Post is copied from http.Client to be compliant with its interface. For more documentation see http.Client.Post
+func (c *WrappedHttpClient) Post(url, contentType string, body io.Reader) (*http.Response, error) {
 	req, err := http.NewRequest("POST", url, body)
 	if err != nil {
 		return nil, err
@@ -59,11 +66,13 @@ func (c *WrappedHttpClient) Post(url, contentType string, body io.Reader) (resp 
 	return c.Do(req)
 }
 
-func (c *WrappedHttpClient) PostForm(url string, data url.Values) (resp *http.Response, err error) {
+// PostForm is copied from http.Client to be compliant with its interface. For more documentation see http.Client.PostForm
+func (c *WrappedHttpClient) PostForm(url string, data url.Values) (*http.Response, error) {
 	return c.Post(url, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
 }
 
-func (c *WrappedHttpClient) Head(url string) (resp *http.Response, err error) {
+// Head is copied from http.Client to be compliant with its interface. For more documentation see http.Client.Head
+func (c *WrappedHttpClient) Head(url string) (*http.Response, error) {
 	req, err := http.NewRequest("HEAD", url, nil)
 	if err != nil {
 		return nil, err
