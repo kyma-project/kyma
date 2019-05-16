e78dc50763a54b838f5357ab163aae5890df909a
Piotr Jasiak <piotr.jasiak@sap.com>
2019-05-16 11:50:06 +0200

    New logs and liveness probe (#4004)
    * Add logs to ServiceBindingUsage controller
    
    * Add liveness probe for binding usage controller

--- components/service-binding-usage-controller/cmd/controller/main.go
+++ components/service-binding-usage-controller/cmd/controller/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/kyma-project/kyma/components/service-binding-usage-controller/internal/controller"
 	"github.com/kyma-project/kyma/components/service-binding-usage-controller/internal/controller/usagekind"
 	"github.com/kyma-project/kyma/components/service-binding-usage-controller/internal/platform/logger"
+	"github.com/kyma-project/kyma/components/service-binding-usage-controller/pkg/apis/servicecatalog/v1alpha1"
 	bindingUsageClientset "github.com/kyma-project/kyma/components/service-binding-usage-controller/pkg/client/clientset/versioned"
 	bindingUsageInformers "github.com/kyma-project/kyma/components/service-binding-usage-controller/pkg/client/informers/externalversions"
 	"github.com/kyma-project/kyma/components/service-binding-usage-controller/pkg/signal"
@@ -18,6 +19,9 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 	"github.com/vrischmann/envconfig"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/dynamic"
 	k8sClientset "k8s.io/client-go/kubernetes"
 	restclient "k8s.io/client-go/rest"
@@ -27,6 +31,13 @@ import (
 // informerResyncPeriod defines how often informer will execute relist action. Setting to zero disable resync.
 // BEWARE: too short period time will increase the CPU load.
 const informerResyncPeriod = 30 * time.Minute
+
+// liveness probe is run in some period of time (e.g. 10s), one of the liveness functionality
+// is create ServiceBindingUsage sample, check its state and remove it.
+// It is not necessary to run this check too often, that is the reason of livenessInhibitor value,
+// it slows down the process, is a time multiplier
+// (period of ServiceBindingUsage sample checker = liveness period * livenessInhibitor)
+const livenessInhibitor = 10
 
 // Config holds application configuration
 type Config struct {
@@ -115,16 +126,27 @@ func main() {
 	bindingUsageInformerFactory.Start(stopCh)
 	serviceCatalogInformerFactory.Start(stopCh)
 
-	go runHTTPServer(stopCh, fmt.Sprintf(":%d", cfg.Port), log)
+	go runHTTPServer(stopCh, fmt.Sprintf(":%d", cfg.Port), bindingUsageCli, cfg.AppliedSBUConfigMapNamespace, log)
 	go kindController.Run(stopCh)
 	go ukProtectionController.Run(stopCh)
 
 	ctr.Run(stopCh)
 }
 
-func runHTTPServer(stop <-chan struct{}, addr string, log logrus.FieldLogger) {
+func runHTTPServer(stop <-chan struct{}, addr string, sbuClient *bindingUsageClientset.Clientset, ns string, log logrus.FieldLogger) {
+	counter := 0
 	mux := http.NewServeMux()
 	mux.HandleFunc("/statusz", func(w http.ResponseWriter, req *http.Request) {
+		if counter >= livenessInhibitor {
+			if err := informerAvailability(sbuClient, log, ns); err != nil {
+				log.Errorf("Cannot apply ServiceBindingUsage sample: %v ", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			counter = 0
+		}
+		counter++
+
 		w.WriteHeader(http.StatusOK)
 		if _, err := fmt.Fprint(w, "OK"); err != nil {
 			log.Errorf("Cannot write response body, got err: %v ", err)
@@ -144,6 +166,56 @@ func runHTTPServer(stop <-chan struct{}, addr string, log logrus.FieldLogger) {
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		log.Errorf("HTTP server ListenAndServe: %v", err)
 	}
+}
+
+func informerAvailability(sbuClient *bindingUsageClientset.Clientset, log logrus.FieldLogger, namespace string) error {
+	deleteSample := func() {
+		err := sbuClient.ServicecatalogV1alpha1().ServiceBindingUsages(namespace).Delete(
+			controller.LivenessBUCSample,
+			&metav1.DeleteOptions{})
+		if err != nil {
+			log.Errorf("while deleteing ServiceBindingUsage sample", err)
+		}
+	}
+	_, err := sbuClient.ServicecatalogV1alpha1().ServiceBindingUsages(namespace).Create(
+		&v1alpha1.ServiceBindingUsage{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "servicecatalog.kyma-project.io/v1alpha1",
+				Kind:       "ServiceBindingUsage",
+			},
+			ObjectMeta: metav1.ObjectMeta{Name: controller.LivenessBUCSample},
+		})
+
+	switch {
+	case k8sErrors.IsAlreadyExists(err):
+		deleteSample()
+		return nil
+	case err != nil:
+		return errors.Wrap(err, "while creating ServiceBindingUsage")
+	}
+
+	defer func() {
+		deleteSample()
+	}()
+	err = wait.Poll(1*time.Second, 20*time.Second, func() (done bool, err error) {
+		sbuSample, err := sbuClient.ServicecatalogV1alpha1().ServiceBindingUsages(namespace).Get(
+			controller.LivenessBUCSample,
+			metav1.GetOptions{})
+		if err != nil {
+			return false, errors.Wrap(err, "while fetching ServiceBindingUsage")
+		}
+		for _, condition := range sbuSample.Status.Conditions {
+			if condition.Status == v1alpha1.ConditionTrue {
+				return true, nil
+			}
+		}
+		return false, nil
+	})
+	if err != nil {
+		return errors.Wrap(err, "while checking ServiceBindingUsage status conditions")
+	}
+
+	return nil
 }
 
 func fatalOnError(err error) {
