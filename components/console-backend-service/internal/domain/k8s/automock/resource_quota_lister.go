46558f3e2ceddfcfe67f5a74d0e9e7852b3522a4
Agata Kucharska <agata.kucharska@sap.com>
2019-05-17 15:19:02 +0200

    Tests

--- components/console-backend-service/internal/domain/k8s/automock/resource_quota_lister.go
+++ components/console-backend-service/internal/domain/k8s/automock/resource_quota_lister.go
@@ -10,6 +10,29 @@ type resourceQuotaLister struct {
 	mock.Mock
 }
 
+// CreateResourceQuota provides a mock function with given fields: namespace, name, memoryLimits, memoryRequests
+func (_m *resourceQuotaLister) CreateResourceQuota(namespace string, name string, memoryLimits string, memoryRequests string) (*v1.ResourceQuota, error) {
+	ret := _m.Called(namespace, name, memoryLimits, memoryRequests)
+
+	var r0 *v1.ResourceQuota
+	if rf, ok := ret.Get(0).(func(string, string, string, string) *v1.ResourceQuota); ok {
+		r0 = rf(namespace, name, memoryLimits, memoryRequests)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*v1.ResourceQuota)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, string, string, string) error); ok {
+		r1 = rf(namespace, name, memoryLimits, memoryRequests)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // ListResourceQuotas provides a mock function with given fields: namespace
 func (_m *resourceQuotaLister) ListResourceQuotas(namespace string) ([]*v1.ResourceQuota, error) {
 	ret := _m.Called(namespace)
--- components/console-backend-service/internal/domain/k8s/resourcequota_resolver_test.go
+++ components/console-backend-service/internal/domain/k8s/resourcequota_resolver_test.go
@@ -2,6 +2,10 @@ package k8s
 
 import (
 	"context"
+	"errors"
+	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlerror"
+
+	//	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlerror"
 	"testing"
 
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/k8s/automock"
@@ -9,6 +13,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -35,4 +40,73 @@ func TestResourceQuotaResolver_ResourceQuotasQuery(t *testing.T) {
 	require.NoError(t, err)
 	assert.Len(t, result, 1)
 	assert.Equal(t, gqlschema.ResourceQuota{Name: "mem-default"}, result[0])
+}
+
+func TestResourceQuotaResolver_CreateResourceQuota(t *testing.T) {
+	const (
+		name                   = "mem-default"
+		namespace              = "production"
+		resourceLimitsMemory   = "1Gi"
+		resourceRequestsMemory = "512Mi"
+	)
+
+	var (
+		resourceQuotaGQL = gqlschema.ResourceQuota{
+			Name: name,
+			Limits: gqlschema.ResourceValues{
+				Memory: ptrStr(resourceLimitsMemory),
+			},
+			Requests: gqlschema.ResourceValues{
+				Memory: ptrStr(resourceRequestsMemory),
+			},
+		}
+		resourceQuota = v1.ResourceQuota{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
+			Spec: v1.ResourceQuotaSpec{
+				Hard: v1.ResourceList{
+					v1.ResourceLimitsMemory:   resource.MustParse(resourceLimitsMemory),
+					v1.ResourceRequestsMemory: resource.MustParse(resourceRequestsMemory),
+				},
+			},
+		}
+	)
+
+	t.Run("Success", func(t *testing.T) {
+		createdResourceQuota := resourceQuota
+
+		lister := automock.NewResourceQuotaLister()
+		lister.On("CreateResourceQuota", namespace, name, resourceLimitsMemory, resourceRequestsMemory).Return(&createdResourceQuota, nil).Once()
+		defer lister.AssertExpectations(t)
+
+		converter := automock.NewGQLResourceQuotaConverter()
+		converter.On("ToGQL", &resourceQuota).Return(&resourceQuotaGQL, nil).Once()
+		defer converter.AssertExpectations(t)
+
+		resolver := newResourceQuotaResolver(lister)
+		resolver.SetResourceQuotaConverter(converter)
+
+		result, err := resolver.CreateResourceQuota(nil, namespace, name, resourceLimitsMemory, resourceRequestsMemory)
+
+		require.NoError(t, err)
+		assert.Equal(t, &resourceQuotaGQL, result)
+	})
+
+	t.Run("ErrorCreating", func(t *testing.T) {
+		expected := errors.New("fix")
+
+		lister := automock.NewResourceQuotaLister()
+		lister.On("CreateResourceQuota", namespace, name, resourceLimitsMemory, resourceRequestsMemory).Return(nil, expected).Once()
+		defer lister.AssertExpectations(t)
+
+		resolver := newResourceQuotaResolver(lister)
+
+		result, err := resolver.CreateResourceQuota(nil, namespace, name, resourceLimitsMemory, resourceRequestsMemory)
+
+		require.Error(t, err)
+		assert.True(t, gqlerror.IsInternal(err))
+		assert.Nil(t, result)
+	})
 }
