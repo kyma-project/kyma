bab39fa4dcf1e8e1ef051c0d948941017cb6a9f3
Sayan Hazra <sayan.mnit@gmail.com>
2019-05-21 21:44:34 +0200

    Refactored Channel name to be public

--- components/event-bus/internal/knative/util/util.go
+++ components/event-bus/internal/knative/util/util.go
@@ -16,7 +16,7 @@ var (
 
 // GetKnSubscriptionName joins the kySubscriptionName and kySubscriptionNamespace
 func GetKnSubscriptionName(kySubscriptionName, kySubscriptionNamespace *string) string {
-	return fmt.Sprintf("%s%s%s", *kySubscriptionName, delimiter, EscapeHyphensAndPeriods(kySubscriptionNamespace))
+	return fmt.Sprintf("%s%s%s", *kySubscriptionName, delimiter, escapeHyphensAndPeriods(kySubscriptionNamespace))
 }
 
 // GetDefaultChannelNamespace() returns the default namespace of Knative/Eventing channels and subscriptions
@@ -28,14 +28,14 @@ func GetDefaultChannelNamespace() string {
 //  * In case there was a '-' or more in any of the argument values, each occurrence of the '-' will be escaped by '-d'.
 //  * In case there was a '.' or more in any of the argument values, each occurrence of the '.' will be replaced by the '-p' character sequence,
 //    because of a limitation in the current knative version, if the channel name has a '.', the corresponding istio-virtualservice will not be created.
-func EscapeHyphensAndPeriods(str *string) string {
+func escapeHyphensAndPeriods(str *string) string {
 	return replacer.Replace(*str)
 }
 
 // encodeChannelName function encodes and joins the given event components to construct the encoded channel name
 func EncodeChannelName(sourceID, eventType, eventTypeVersion *string) string {
 	return fmt.Sprintf("%s%s%s%s%s",
-		EscapeHyphensAndPeriods(sourceID), delimiter,
-		EscapeHyphensAndPeriods(eventType), delimiter,
-		EscapeHyphensAndPeriods(eventTypeVersion))
+		escapeHyphensAndPeriods(sourceID), delimiter,
+		escapeHyphensAndPeriods(eventType), delimiter,
+		escapeHyphensAndPeriods(eventTypeVersion))
 }
