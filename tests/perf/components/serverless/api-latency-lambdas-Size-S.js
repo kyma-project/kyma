a05e85d784cd7cb6c27dcf6b0cbf3bfba01a1712
Nils Schmidt <nils.schmidt02@sap.com>
2019-05-22 15:21:26 +0200

    Use combined k6 run file

--- tests/perf/components/serverless/api-latency-lambdas-Size-S.js
+++ tests/perf/components/serverless/api-latency-lambdas-Size-S.js
@@ -3,24 +3,63 @@ import { check, sleep } from "k6";
 
 export let options = {
   vus: 10,
-  duration: "1m",
   rps: 1000,
   tags: {
-    "testName": "size-s",
     "component": "serverless",
     "revision": `${__ENV.REVISION}`
-  }
+  },
+  // ramp up #virtual users (VU) over time to get maximum througput
+  stages: [
+    { duration: "90s", target: 10 },
+    { duration: "90s", target: 100 },
+    { duration: "90s", target: 1000 },
+    // { duration: "30s", target: 10000 },
+  ],
 }
 
-export let configuration = {
-  url: `https://size-s.${__ENV.CLUSTER_DOMAIN_NAME}`,
-}
+export let configuration =
+  [
+    {
+      url: `https://size-s.${__ENV.CLUSTER_DOMAIN_NAME}`,
+      tags: {
+        tags: {
+          'testName': 'size-sc'
+        }
+      }
+    },
+    {
+      url: `https://size-m.${__ENV.CLUSTER_DOMAIN_NAME}`,
+      tags: {
+        tags: {
+          'testName': 'size-mc'
+        }
+      }
+    },
+    {
+      url: `https://size-l.${__ENV.CLUSTER_DOMAIN_NAME}`,
+      tags: {
+        tags: {
+          'testName': 'size-lc'
+        }
+      }
+    },
+    // {
+    //   url: `https://size-xl.${__ENV.CLUSTER_DOMAIN_NAME}`,
+    //   tags: {
+    //     tags: {
+    //       'testName': 'size-xlc'
+    //     }
+    //   }
+    // }
+  ]
 
 export default function () {
-  let res = http.get(configuration.url);
+  configuration.forEach(function (element) {
+    let res = http.get(element.url, element.tags);
 
-  check(res, {
-    "status was 200": (r) => r.status == 200,
-    "transaction time OK": (r) => r.timings.duration < 200
+    check(res, {
+      "status was 200": (r) => r.status == 200,
+      "transaction time OK": (r) => r.timings.duration < 200
+    }, element.tags);
   });
 };
