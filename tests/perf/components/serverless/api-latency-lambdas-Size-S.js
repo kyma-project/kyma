42b2fa0d2472144f266735d189046f7da6f2ff79
Nils Schmidt <nils.schmidt02@sap.com>
2019-05-22 16:41:10 +0200

    Polishing

--- tests/perf/components/serverless/api-latency-lambdas-Size-S.js
+++ tests/perf/components/serverless/api-latency-lambdas-Size-S.js
@@ -2,7 +2,6 @@ import http from "k6/http"
 import { check, sleep } from "k6";
 
 export let options = {
-  vus: 10,
   rps: 1000,
   tags: {
     "component": "serverless",
@@ -13,7 +12,6 @@ export let options = {
     { duration: "90s", target: 10 },
     { duration: "90s", target: 100 },
     { duration: "90s", target: 1000 },
-    // { duration: "30s", target: 10000 },
   ],
 }
 
@@ -23,7 +21,7 @@ export let configuration =
       url: `https://size-s.${__ENV.CLUSTER_DOMAIN_NAME}`,
       tags: {
         tags: {
-          'testName': 'size-sc'
+          'testName': 'size-s'
         }
       }
     },
@@ -31,7 +29,7 @@ export let configuration =
       url: `https://size-m.${__ENV.CLUSTER_DOMAIN_NAME}`,
       tags: {
         tags: {
-          'testName': 'size-mc'
+          'testName': 'size-m'
         }
       }
     },
@@ -39,7 +37,7 @@ export let configuration =
       url: `https://size-l.${__ENV.CLUSTER_DOMAIN_NAME}`,
       tags: {
         tags: {
-          'testName': 'size-lc'
+          'testName': 'size-l'
         }
       }
     },
@@ -59,7 +57,6 @@ export default function () {
 
     check(res, {
       "status was 200": (r) => r.status == 200,
-      "transaction time OK": (r) => r.timings.duration < 200
     }, element.tags);
   });
 };
