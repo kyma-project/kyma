149c6bf1e4e839cb5ff986a3ab8a3f7765349166
Sayan Hazra <sayan.mnit@gmail.com>
2019-05-28 23:42:23 +0200

    Enable scaddmons test

--- tests/end-to-end/backup-restore-test/restore_cluster_backup_test.go
+++ tests/end-to-end/backup-restore-test/restore_cluster_backup_test.go
@@ -12,7 +12,7 @@ import (
 	// . "github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/backupe2e/asset-store"
 	// . "github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/backupe2e/cms"
 
-	// . "github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/backupe2e/service-catalog"
+	. "github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/backupe2e/service-catalog"
 	backupClient "github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/utils/backup"
 	"github.com/sirupsen/logrus"
 	. "github.com/smartystreets/goconvey/convey"
@@ -38,20 +38,20 @@ func TestBackupAndRestoreCluster(t *testing.T) {
 	cfg, err := loadConfig()
 	fatalOnError(t, err, "while reading configuration from environment variables")
 
-	myFunctionTest, err := NewFunctionTest()
-	fatalOnError(t, err, "while creating structure for Function test")
+	// myFunctionTest, err := NewFunctionTest()
+	// fatalOnError(t, err, "while creating structure for Function test")
 
-	myStatefulSetTest, err := NewStatefulSetTest()
-	fatalOnError(t, err, "while creating structure for StatefulSet test")
+	// myStatefulSetTest, err := NewStatefulSetTest()
+	// fatalOnError(t, err, "while creating structure for StatefulSet test")
 
-	myDeploymentTest, err := NewDeploymentTest()
-	fatalOnError(t, err, "while creating structure for Deployment test")
+	// myDeploymentTest, err := NewDeploymentTest()
+	// fatalOnError(t, err, "while creating structure for Deployment test")
 
-	myPrometheusTest, err := NewPrometheusTest()
-	fatalOnError(t, err, "while creating structure for Prometheus test")
+	// myPrometheusTest, err := NewPrometheusTest()
+	// fatalOnError(t, err, "while creating structure for Prometheus test")
 
-	myGrafanaTest, err := NewGrafanaTest()
-	fatalOnError(t, err, "while creating structure for Grafana test")
+	// myGrafanaTest, err := NewGrafanaTest()
+	// fatalOnError(t, err, "while creating structure for Grafana test")
 
 	// appBrokerTest, err := NewAppBrokerTest()
 	// fatalOnError(t, err, "while creating structure for AppBroker test")
@@ -59,8 +59,8 @@ func TestBackupAndRestoreCluster(t *testing.T) {
 	// helmBrokerTest, err := NewHelmBrokerTest()
 	// fatalOnError(t, err, "while creating structure for HelmBroker test")
 
-	// scAddonsTest, err := NewServiceCatalogAddonsTest()
-	// fatalOnError(t, err, "while creating structure for ScAddons test")
+	scAddonsTest, err := NewServiceCatalogAddonsTest()
+	fatalOnError(t, err, "while creating structure for ScAddons test")
 
 	// myNamespaceControllerTest, err := NewNamespaceControllerTestFromEnv()
 	// fatalOnError(t, err, "while creating structure for NamespaceController test")
@@ -78,14 +78,14 @@ func TestBackupAndRestoreCluster(t *testing.T) {
 	// fatalOnError(t, err, "while creating structure for Cms test")
 
 	backupTests := []BackupTest{
-		myPrometheusTest,
-		myFunctionTest,
-		myDeploymentTest,
-		myStatefulSetTest,
-		myGrafanaTest,
+		// myPrometheusTest,
+		// myFunctionTest,
+		// myDeploymentTest,
+		// myStatefulSetTest,
+		// myGrafanaTest,
 		// myAssetStoreTest,
 		// appBrokerTest,
-		// scAddonsTest,
+		scAddonsTest,
 		// helmBrokerTest,
 		// myNamespaceControllerTest,
 		// apiControllerTest,
