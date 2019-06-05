cf1b745cbcee412c574f80abc7e68889e742308d
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2019-06-05 15:19:33 +0200

    Update service_test.go

--- components/application-registry/internal/metadata/specification/service_test.go
+++ components/application-registry/internal/metadata/specification/service_test.go
@@ -14,8 +14,8 @@ import (
 )
 
 const (
-	serviceId                 = "1234"
-	gatewayUrl                = "http://app-1234.io"
+	serviceId = "1234"
+	gatewayUrl = "http://app-1234.io"
 	defaultSpecRequestTimeout = 5
 )
 
