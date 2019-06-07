52034ebae59e9248c7e0d71cb0e3d84bfb9eb166
Magda Strek <magdalena.strek@sap.com>
2019-06-07 10:45:36 +0200

    Review fixes

---
+++ components/api-controller/cmd/controller/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestValidateApi(t *testing.T) {
+
+	Convey("If readBlacklistedServices", t, func() {
+
+		Convey("is fed with an empty string", func() {
+			//given
+			list := ""
+
+			Convey("it should return an empty array", func() {
+
+				//when
+				blacklistedSvc := readBlacklistedServices(list)
+
+				//then
+				So(blacklistedSvc, ShouldBeEmpty)
+				So(len(blacklistedSvc), ShouldEqual, 0)
+			})
+		})
+
+		Convey("is fed with string containing service", func() {
+			//given
+			list := "svc-1.ns-1"
+
+			Convey("it should return an array containing one item", func() {
+
+				//when
+				blacklistedSvc := readBlacklistedServices(list)
+
+				//then
+				So(blacklistedSvc, ShouldNotBeEmpty)
+				So(len(blacklistedSvc), ShouldEqual, 1)
+			})
+		})
+
+		Convey("is fed with string containing services", func() {
+			//given
+			list := "svc-1.ns-1, svc-2.ns-1"
+
+			Convey("it should return an array containing two items", func() {
+
+				//when
+				blacklistedSvc := readBlacklistedServices(list)
+
+				//then
+				So(blacklistedSvc, ShouldNotBeEmpty)
+				So(len(blacklistedSvc), ShouldEqual, 2)
+			})
+		})
+	})
+}
+
