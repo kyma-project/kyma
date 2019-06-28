70c59efb5ec8284515215d5ec633ff0d16a99fa2
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-06-28 11:27:16 +0200

    Include compass-gateway.kyma.local /etc/hosts entry on Windows

--- installation/scripts/minikube.ps1
+++ installation/scripts/minikube.ps1
@@ -116,5 +116,5 @@ InitializeMinikubeConfig
 StartMinikube
 WaitForMinikubeToBeUp
 ConfigureMinikubeAddons
-AddDevDomainsToEtcHosts "apiserver", "console", "catalog", "instances", "brokers", "dex", "docs", "add-ons", "lambdas-ui", "console-backend", "minio", "jaeger", "grafana", "configurations-generator", "gateway", "connector-service"
+AddDevDomainsToEtcHosts "apiserver", "console", "catalog", "instances", "brokers", "dex", "docs", "add-ons", "lambdas-ui", "console-backend", "minio", "jaeger", "grafana", "configurations-generator", "gateway", "connector-service", "compass-gateway"
 IncreaseFsInotifyMaxUserInstances
