01f1b3542348ebf48a5f204f3643b98c0c1d68c2
Kucharska <agata.kucharska@sap.com>
2019-07-01 09:47:51 +0200

    Add mutation update namespace

--- components/console-backend-service/internal/domain/k8s/namespace_service.go
+++ components/console-backend-service/internal/domain/k8s/namespace_service.go
@@ -7,6 +7,10 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"github.com/pkg/errors"
+	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/application/pretty"
+	apiErrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -69,6 +73,37 @@ func (svc *namespaceService) Create(name string, labels gqlschema.Labels) (*v1.N
 	}
 
 	return svc.client.Namespaces().Create(&namespace)
+}
+
+func (svc *namespaceService) Update(name string, labels gqlschema.Labels) (*v1.Namespace, error) {
+	maxUpdateRetries  := 5
+	var lastErr error
+	for i := 0; i < maxUpdateRetries; i++ {
+		namespace, err := svc.Find(name)
+
+		if err != nil {
+			return nil, errors.Wrapf(err, "while getting %s [%s]", pretty.Namespace, name)
+		}
+		if namespace == nil {
+			return nil, apiErrors.NewNotFound(schema.GroupResource{
+				Group:    "",
+				Resource: "namespaces",
+			}, name)
+		}
+		namespace.ObjectMeta.Labels = labels
+
+		updated, err := svc.client.Namespaces().Update(namespace)
+		switch {
+		case err == nil:
+			return updated, nil
+		case apiErrors.IsConflict(err):
+			lastErr = err
+			continue
+		default:
+			return nil, errors.Wrapf(err, "while updating %s [%s]", pretty.Namespace, name)
+		}
+	}
+	return nil, errors.Wrapf(lastErr, "couldn't update %s [%s], after %d retries", pretty.Namespace, name, maxUpdateRetries)
 }
 
 func (svc *namespaceService) Delete(name string) error {
