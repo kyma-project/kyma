7e4351f4e86d6658bc73ed4aa3c585ddc55e67c1
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2019-07-04 14:33:36 +0200

    Revert "Code review #3 cleaned up useless fields"

--- components/application-gateway/pkg/authorization/externaltokenstrategy_test.go
+++ components/application-gateway/pkg/authorization/externaltokenstrategy_test.go
@@ -18,7 +18,7 @@ func TestExternalAuthStrategy(t *testing.T) {
 
 		oauthStrategy := newOAuthStrategy(oauthClientMock, "clientId", "clientSecret", "www.example.com/token")
 
-		externalTokenStrategy := newExternalTokenStrategy(oauthStrategy, nil, nil)
+		externalTokenStrategy := newExternalTokenStrategy(oauthStrategy)
 
 		request, err := http.NewRequest("GET", "www.example.com", nil)
 		require.NoError(t, err)
@@ -35,70 +35,6 @@ func TestExternalAuthStrategy(t *testing.T) {
 
 		assert.Equal(t, "Bearer external", authHeader)
 		assert.Equal(t, "", externalTokenHeader)
-		oauthClientMock.AssertNotCalled(t, "GetToken")
-	})
-
-	t.Run("should inject custom headers", func(t *testing.T) {
-		// given
-		oauthClientMock := &mocks.Client{}
-
-		oauthStrategy := newOAuthStrategy(oauthClientMock, "clientId", "clientSecret", "www.example.com/token")
-
-		headers := map[string][]string{
-			"X-Custom1": []string{"custom-value"},
-		}
-
-		externalTokenStrategy := newExternalTokenStrategy(oauthStrategy, &headers, nil)
-
-		request, err := http.NewRequest("GET", "www.example.com", nil)
-		require.NoError(t, err)
-
-		request.Header.Set(httpconsts.HeaderAccessToken, "Bearer external")
-
-		// when
-		err = externalTokenStrategy.AddAuthorization(request, nil)
-
-		// then
-		require.NoError(t, err)
-		authHeader := request.Header.Get(httpconsts.HeaderAuthorization)
-		externalTokenHeader := request.Header.Get(httpconsts.HeaderAccessToken)
-		customHeader := request.Header.Get("X-Custom1")
-
-		assert.Equal(t, "Bearer external", authHeader)
-		assert.Equal(t, "", externalTokenHeader)
-		assert.Equal(t, "custom-value", customHeader)
-		oauthClientMock.AssertNotCalled(t, "GetToken")
-	})
-
-	t.Run("should inject custom query parameters", func(t *testing.T) {
-		// given
-		oauthClientMock := &mocks.Client{}
-
-		oauthStrategy := newOAuthStrategy(oauthClientMock, "clientId", "clientSecret", "www.example.com/token")
-
-		queryParams := map[string][]string{
-			"param1": []string{"custom-value"},
-		}
-
-		externalTokenStrategy := newExternalTokenStrategy(oauthStrategy, nil, &queryParams)
-
-		request, err := http.NewRequest("GET", "www.example.com", nil)
-		require.NoError(t, err)
-
-		request.Header.Set(httpconsts.HeaderAccessToken, "Bearer external")
-
-		// when
-		err = externalTokenStrategy.AddAuthorization(request, nil)
-
-		// then
-		require.NoError(t, err)
-		authHeader := request.Header.Get(httpconsts.HeaderAuthorization)
-		externalTokenHeader := request.Header.Get(httpconsts.HeaderAccessToken)
-		customQueryParam := request.URL.Query().Get("param1")
-
-		assert.Equal(t, "Bearer external", authHeader)
-		assert.Equal(t, "", externalTokenHeader)
-		assert.Equal(t, "custom-value", customQueryParam)
 		oauthClientMock.AssertNotCalled(t, "GetToken")
 	})
 
@@ -109,7 +45,7 @@ func TestExternalAuthStrategy(t *testing.T) {
 
 		oauthStrategy := newOAuthStrategy(oauthClientMock, "clientId", "clientSecret", "www.example.com/token")
 
-		externalTokenStrategy := newExternalTokenStrategy(oauthStrategy, nil, nil)
+		externalTokenStrategy := newExternalTokenStrategy(oauthStrategy)
 
 		request, err := http.NewRequest("GET", "www.example.com", nil)
 		require.NoError(t, err)
@@ -132,7 +68,7 @@ func TestExternalAuthStrategy(t *testing.T) {
 
 		oauthStrategy := newOAuthStrategy(oauthClientMock, "clientId", "clientSecret", "www.example.com/token")
 
-		externalTokenStrategy := newExternalTokenStrategy(oauthStrategy, nil, nil)
+		externalTokenStrategy := newExternalTokenStrategy(oauthStrategy)
 
 		// when
 		externalTokenStrategy.Invalidate()
