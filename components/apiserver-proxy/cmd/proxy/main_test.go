e3513d032869d4099e7d43384107dc80656b91c2
Jakub Kabza <jakub.kabza@sap.com>
2019-07-05 13:47:07 +0200

    add test

---
+++ components/apiserver-proxy/cmd/proxy/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/rest"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+const (
+	allowOrigin = "domain.com"
+	allowMethod = "GET"
+	allowHeader = "Content-Type"
+)
+
+func TestDeleteUstreamCORSHeaders(t *testing.T) {
+
+	//GIVEN
+
+	//Setup backend server
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set(corsAllowOriginHeader, "upstream-cors-allow-origin")
+		w.Header().Set(corsAllowMethodsHeader, "upstream-cors-allow-method")
+		w.Header().Set(corsAllowHeadersHeader, "upstream-cors-allow-header")
+	}))
+
+	defer backend.Close()
+	backendURL, _ := url.Parse(backend.URL)
+
+	//Setup proxy
+	rp, _ := newReverseProxy(backendURL, &rest.Config{}, true)
+
+	//Setup CORS config
+	cfg := corsConfig{
+		allowOrigin:  []string{allowOrigin},
+		allowMethods: []string{allowMethod},
+		allowHeaders: []string{allowHeader},
+	}
+
+	//Setup CORS wrapper
+	corsHandler := getCORSHandler(rp, cfg)
+
+	//Prepare request
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("Origin", allowOrigin)
+
+	//Prepare ResponseWriter
+	res := httptest.NewRecorder()
+
+
+	//WHEN
+	corsHandler.ServeHTTP(res, req)
+
+	//THEN
+	assert.Equal(t, 200, res.Code)
+	assert.Equal(t, 3, len(res.Header())) //expected headers: corsAllowOriginHeader, Content-Length, Date
+	assert.Equal(t, allowOrigin, res.Header().Get(corsAllowOriginHeader))
+}
