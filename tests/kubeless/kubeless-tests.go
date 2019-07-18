fed46a30e0891a0a2d60aeb98f19934132dea1e1
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2019-07-18 16:57:15 +0200

    cleanup logging
    a bit

--- tests/kubeless/kubeless-tests.go
+++ tests/kubeless/kubeless-tests.go
@@ -100,14 +100,14 @@ func ensureFunctionIsRunning(namespace, name string) {
 			stdoutStderr, err := cmd.CombinedOutput()
 
 			if err != nil {
-				log.Infof("Error while fetching the status phase of the function pod when verifying function is running: %v", string(stdoutStderr))
+				log.Info("Error while fetching the status phase of the function pod when verifying function is running: ", string(stdoutStderr))
 			}
 
 			functionPodsCmd := exec.Command("kubectl", "-n", namespace, "get", "pod", "-l", "function="+name, "-ojsonpath={.items[0].metadata.name}")
 
 			functionPodName, err := functionPodsCmd.CombinedOutput()
 			if err != nil {
-				log.Infof("Error is fetch function pod when verifying function is running: %v", string(functionPodName))
+				log.Info("Error fetching function pod when verifying function is running: ", string(functionPodName))
 			}
 			if err == nil && strings.Contains(string(stdoutStderr), "Running") {
 				log.Infof("Pod: %v: is running!", string(functionPodName))
@@ -121,7 +121,7 @@ func printLogsFunctionPodContainers(namespace, name string) {
 	functionPodsCmd := exec.Command("kubectl", "-n", namespace, "get", "pod", "-l", "function="+name, "-ojsonpath={.items[0].metadata.name}")
 	functionPodName, err := functionPodsCmd.CombinedOutput()
 	if err != nil {
-		log.Infof("Error is fetch function pod: %v", string(functionPodName))
+		log.Info("Error fetching function pod: ", string(functionPodName))
 	}
 
 	log.Infof("---------- Logs from all containers for function pod: %s ----------\n", string(functionPodName))
@@ -129,22 +129,22 @@ func printLogsFunctionPodContainers(namespace, name string) {
 	prepareContainerLogCmd := exec.Command("kubectl", "-n", namespace, "logs", string(functionPodName), "prepare")
 
 	prepareContainerLog, _ := prepareContainerLogCmd.CombinedOutput()
-	log.Infof("Logs from prepare container:\n%s\n", string(prepareContainerLog))
+	log.Infof("Logs from prepare container:\n%s", string(prepareContainerLog))
 
 	installContainerLogCmd := exec.Command("kubectl", "-n", namespace, "logs", string(functionPodName), "install")
 
 	installContainerLog, _ := installContainerLogCmd.CombinedOutput()
-	log.Infof("Logs from prepare container:\n%s\n", string(installContainerLog))
+	log.Infof("Logs from prepare container:\n%s", string(installContainerLog))
 
 	functionContainerLogCmd := exec.Command("kubectl", "-n", namespace, "logs", string(functionPodName), name)
 
 	functionContainerLog, _ := functionContainerLogCmd.CombinedOutput()
-	log.Infof("Logs from %s container in pod %s:\n%s\n", name, string(functionPodName), string(functionContainerLog))
+	log.Infof("Logs from %s container in pod %s:\n%s", name, string(functionPodName), string(functionContainerLog))
 
 	envoyLogsCmd := exec.Command("kubectl", "-n", namespace, "log", "-l", string(functionPodName), "-c", "istio-proxy")
 
 	envoyLogsCmdStdErr, _ := envoyLogsCmd.CombinedOutput()
-	log.Infof("Envoy Logs are:\n%s\n", string(envoyLogsCmdStdErr))
+	log.Infof("Envoy Logs are:\n%s", string(envoyLogsCmdStdErr))
 }
 
 func deleteFun(namespace, name string) error {
@@ -262,14 +262,14 @@ func main() {
 		return cleanup(namespace, functionName)
 	}, retry.Attempts(5), retry.Delay(5 * time.Second))
 	if err != nil {
-		log.WithField("error", err).Fatal("Cleanup failed")
+		log.Fatal("Cleanup failed: ", err)
 	}
 
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	log.Info("Starting kubeless unit test")
 
-	log.Infof("Creating namespace: %v\n", namespace)
+	log.Info("Creating namespace: ", namespace)
 	deployK8s("ns.yaml")
 
 	log.Info("Deploying test-hello function")
@@ -277,7 +277,7 @@ func main() {
 
 	log.Info("Verifying correct function output for test-hello")
 	host := fmt.Sprintf("http://%s.%s:8080", functionName, namespace)
-	log.Infof("Endpoint for the function: %v\n", host)
+	log.Info("Endpoint for the function: ", host)
 	expectedOutput := fmt.Sprintf("{\"result\":\"%v\"}", testID)
 	ensureOutputIsCorrect(host, expectedOutput, testID, namespace, functionName)
 
