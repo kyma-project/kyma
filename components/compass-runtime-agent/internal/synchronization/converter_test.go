1f06deed2b54369547c0d6c0567db091ffd3c6f1
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2019-07-25 15:17:11 +0200

    goimports: compilation fix

--- components/compass-runtime-agent/internal/synchronization/converter_test.go
+++ components/compass-runtime-agent/internal/synchronization/converter_test.go
@@ -1,11 +1,12 @@
 package synchronization
 
 import (
+	"testing"
+
 	"github.com/kyma-project/kyma/components/application-operator/pkg/apis/applicationconnector/v1alpha1"
 	k8smocks "github.com/kyma-project/kyma/components/compass-runtime-agent/internal/k8sconsts/mocks"
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
 )
 
 func TestConverter(t *testing.T) {
