c76bcd68368a9b4e37201b7c62fd26d80e8d697b
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2019-07-31 13:59:37 +0200

    Compilation fix

--- components/compass-runtime-agent/internal/kyma/service_test.go
+++ components/compass-runtime-agent/internal/kyma/service_test.go
@@ -1,8 +1,9 @@
 package kyma
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/stretchr/testify/mock"
 	"k8s.io/apimachinery/pkg/types"
