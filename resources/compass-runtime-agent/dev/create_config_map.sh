2771b2b1c97911a3bb54f2cbbf2d69ad49b5b5b3
Szymon Gibala <szymon.gibala@sap.com>
2019-08-01 12:40:07 +0200

    Make config map optional

--- resources/compass-runtime-agent/dev/create_config_map.sh
+++ resources/compass-runtime-agent/dev/create_config_map.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This config map is used by the
+
 DOMAIN=$(kubectl -n compass-system get vs compass-gateway -o 'jsonpath={.spec.hosts[0]}')
 
 cat <<EOF | kubectl -n compass-system apply -f -
@@ -7,7 +9,7 @@ apiVersion: v1
 data:
   DIRECTOR_URL: https://$DOMAIN/director/graphql
   RUNTIME_ID: 854f2e79-3266-47c0-8ccb-8ed7b6b8f48f
-  TENANT: 'demo1'
+  TENANT: "$TENANT"
 kind: ConfigMap
 metadata:
   name: compass-agent-configuration
