e8b7b99516234773405b3548936fbe3d5ef81e3b
montaro <ahmed.rifaey@gmail.com>
2019-08-19 14:07:50 +0200

    updated k8s libs to 1.15.2
    kubernetes-1.12.6
    
    Update Gopkg.lock
    
    fix imports
    
    Delete my-kyma.yaml
    
    Update reconcile_test.go
    
    Update Gopkg.lock
    
    Update publish.go
    
    Update Gopkg.toml
    
    Update Gopkg.lock
    
    Update Gopkg.lock
    
    Update before-commit.sh

--- components/event-bus/api/push/eventing.kyma-project.io/v1alpha1/register.go
+++ components/event-bus/api/push/eventing.kyma-project.io/v1alpha1/register.go
@@ -1,7 +1,7 @@
 package v1alpha1
 
 import (
-	"github.com/kyma-project/kyma/components/event-bus/api/push/eventing.kyma-project.io"
+	eventingkymaio "github.com/kyma-project/kyma/components/event-bus/api/push/eventing.kyma-project.io"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
