34b3c2898356bf2fecce5d1269ab30e5416ce598
Kucharska <agata.kucharska@sap.com>
2019-08-23 11:49:45 +0200

    update helm-broker package address

--- components/console-backend-service/internal/domain/servicecatalogaddons/addonsconfiguration_resolver_test.go
+++ components/console-backend-service/internal/domain/servicecatalogaddons/addonsconfiguration_resolver_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/servicecatalogaddons/automock"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlschema"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/pager"
-	"github.com/kyma-project/kyma/components/helm-broker/pkg/apis/addons/v1alpha1"
+	"github.com/kyma-project/helm-broker/pkg/apis/addons/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
