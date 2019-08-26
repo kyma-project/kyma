26ca90fc94653e5c776e117301c3acdf258db9db
Szymon Janota <szymon.janota@sap.com>
2019-08-26 12:42:15 +0200

    WIP

--- components/console-backend-service/go.mk
+++ components/console-backend-service/go.mk
@@ -3,9 +3,15 @@ TAG = $(DOCKER_TAG)
 LOCAL_DIR = $(shell pwd)
 WORKSPACE_DIR = /workspace/go/src/github.com/kyma-project/kyma/$(APP_PATH)
 
+RED = '\033[0;31m'
+GREEN = '\033[0;32m'
+INVERTED = '\033[7m'
+NC = '\033[0m' # No Color
+
 .PHONY: release resolve build build-image push-image pull-licenses
 
 define buildpack
+	@echo "=> Buildpack: $(1)"
 	@docker run --rm -v $(LOCAL_DIR):$(WORKSPACE_DIR) -w $(WORKSPACE_DIR) $(BUILDPACK) $(1)
 endef
 
@@ -17,15 +23,20 @@ resolve:
 test:
 	$(call buildpack,go test ./...)
 
-build:
-	$(call buildpack,./before-commit.sh)
-
 build-image: pull-licenses
 	docker build -t $(IMG_NAME) .
 
 push-image:
 	docker tag $(IMG_NAME) $(IMG):$(TAG)
 	docker push $(IMG):$(TAG)
+
+before-commit: build dep-status test imports fmt errcheck vet
+
+build:
+	$(call buildpack,env CGO_ENABLED=0 go build -o $(APP_NAME))
+
+dep-status:
+	$(call buildpack, dep status -v)
 
 pull-licenses:
 ifdef LICENSE_PULLER_PATH
