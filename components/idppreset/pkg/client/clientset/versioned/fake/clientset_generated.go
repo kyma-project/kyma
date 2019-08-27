6e82f9b542de7fd06c536998e2b5edf1f8f1fea7
Kucharska <agata.kucharska@sap.com>
2019-08-27 09:17:06 +0200

    Revert "Bump client-go version to kubernetes1.12.5"
    This reverts commit 6c0451b5120e313020a085f80d04c8ef79279188.

--- components/idppreset/pkg/client/clientset/versioned/fake/clientset_generated.go
+++ components/idppreset/pkg/client/clientset/versioned/fake/clientset_generated.go
@@ -25,7 +25,7 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 		}
 	}
 
-	cs := &Clientset{}
+	cs := &Clientset{tracker: o}
 	cs.discovery = &fakediscovery.FakeDiscovery{Fake: &cs.Fake}
 	cs.AddReactor("*", "*", testing.ObjectReaction(o))
 	cs.AddWatchReactor("*", func(action testing.Action) (handled bool, ret watch.Interface, err error) {
@@ -47,20 +47,20 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 type Clientset struct {
 	testing.Fake
 	discovery *fakediscovery.FakeDiscovery
+	tracker   testing.ObjectTracker
 }
 
 func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 	return c.discovery
 }
 
+func (c *Clientset) Tracker() testing.ObjectTracker {
+	return c.tracker
+}
+
 var _ clientset.Interface = &Clientset{}
 
 // AuthenticationV1alpha1 retrieves the AuthenticationV1alpha1Client
 func (c *Clientset) AuthenticationV1alpha1() authenticationv1alpha1.AuthenticationV1alpha1Interface {
-	return &fakeauthenticationv1alpha1.FakeAuthenticationV1alpha1{Fake: &c.Fake}
-}
-
-// Authentication retrieves the AuthenticationV1alpha1Client
-func (c *Clientset) Authentication() authenticationv1alpha1.AuthenticationV1alpha1Interface {
 	return &fakeauthenticationv1alpha1.FakeAuthenticationV1alpha1{Fake: &c.Fake}
 }
