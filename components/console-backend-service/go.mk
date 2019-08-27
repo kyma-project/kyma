29cdc96f8133bca126c22ab78d09aa353a5d92f9
Szymon Janota <szymon.janota@sap.com>
2019-08-27 08:40:07 +0200

    Initial version si working

--- components/console-backend-service/go.mk
+++ components/console-backend-service/go.mk
@@ -1,27 +1,31 @@
 IMG = $(DOCKER_PUSH_REPOSITORY)$(DOCKER_PUSH_DIRECTORY)/$(APP_NAME)
 TAG = $(DOCKER_TAG)
-LOCAL_DIR = $(shell pwd)
-WORKSPACE_DIR = /workspace/go/src/github.com/kyma-project/kyma/$(APP_PATH)
+BASE_PKG = github.com/kyma-project/kyma
+IMG_GOPATH = /workspace/go
+IMG_GOCACHE = /root/.cache/go-build
+PKGS_TO_IGNORE = /vendor\|/automock\|/testdata\|/pkg
 
-RED = '\033[0;31m'
-GREEN = '\033[0;32m'
-INVERTED = '\033[7m'
-NC = '\033[0m' # No Color
+LOCAL_DIR = $(shell pwd)
+WORKSPACE_DIR = $(IMG_GOPATH)/src/$(BASE_PKG)/$(APP_PATH)
+FILES_TO_CHECK = $(find . -type f -name "*.go" | grep -v "$(PKGS_TO_IGNORE)"')
+PKGS_TO_CHECK = $(go list ./... | grep -v "$(PKGS_TO_IGNORE)")
 
 .PHONY: release resolve build build-image push-image pull-licenses
 
-define buildpack
-	@echo "=> Buildpack: $(1)"
-	@docker run --rm -v $(LOCAL_DIR):$(WORKSPACE_DIR) -w $(WORKSPACE_DIR) $(BUILDPACK) $(1)
+DOCKER_CREATE_OPTS = --rm -t -w $(WORKSPACE_DIR) -v $(shell go env GOCACHE):$(IMG_GOCACHE) -v $(shell go env GOPATH)/pkg/dep:$(IMG_GOPATH)/pkg/dep $(BUILDPACK)
+
+define buildpack-mount
+	@docker run -i -v $(LOCAL_DIR):$(WORKSPACE_DIR) $(DOCKER_CREATE_OPTS) make $(1)-local
 endef
 
-release: resolve build build-image push-image
+define buildpack-cp
+	$(eval container = $(shell docker create $(DOCKER_CREATE_OPTS) make $(1)-local))
+	@docker cp $(LOCAL_DIR)/. $(container):$(WORKSPACE_DIR)/
+	@docker start -i $(container)
+endef
 
-resolve:
-	$(call buildpack,dep ensure -v -vendor-only)
-
-test:
-	$(call buildpack,go test ./...)
+verify: dep-status test imports fmt errcheck vet
+release: resolve verify build-image push-image
 
 build-image: pull-licenses
 	docker build -t $(IMG_NAME) .
@@ -30,13 +34,46 @@ push-image:
 	docker tag $(IMG_NAME) $(IMG):$(TAG)
 	docker push $(IMG):$(TAG)
 
-before-commit: build dep-status test imports fmt errcheck vet
-
 build:
-	$(call buildpack,env CGO_ENABLED=0 go build -o $(APP_NAME))
+	$(call buildpack-cp,build)
+build-local:
+	env CGO_ENABLED=0 go build -o $(APP_NAME)
+	rm $(APP_NAME)
+
+resolve:
+	$(call buildpack-cp,resolve)
+resolve-local:
+	dep ensure -v -vendor-only
 
 dep-status:
-	$(call buildpack, dep status -v)
+	$(call buildpack-cp,dep-status)
+dep-status-local:
+	dep status -v
+
+test:
+	$(call buildpack-cp,test)
+test-local:
+	go test ./...
+
+imports:
+	$(call buildpack-mount,imports)
+imports-local:
+	goimports -w -l $(FILES_TO_CHECK))
+
+fmt:
+	$(call buildpack-mount,go fmt $(PKGS_TO_CHECK))
+fmt-local:
+	go fmt $(PKGS_TO_CHECK)
+
+errcheck:
+	$(call buildpack-mount,errcheck)
+errcheck-local:
+	errcheck -blank -asserts -ignoregenerated -exclude automock ./...
+
+vet:
+	$(call buildpack-mount,vet)
+vet-local:
+	go vet $(PKGS_TO_CHECK)
 
 pull-licenses:
 ifdef LICENSE_PULLER_PATH
