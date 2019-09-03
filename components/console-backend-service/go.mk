7664cd151a6c3673f6cefe2cd9c6c79524dc819a
Szymon Janota <szymon.janota@sap.com>
2019-09-03 08:27:11 +0200

    split check and format

--- components/console-backend-service/go.mk
+++ components/console-backend-service/go.mk
@@ -1,3 +1,4 @@
+# Default configuration
 IMG_NAME = $(DOCKER_PUSH_REPOSITORY)$(DOCKER_PUSH_DIRECTORY)/$(APP_NAME)
 TAG = $(DOCKER_TAG)
 BASE_PKG = github.com/kyma-project/kyma
@@ -5,21 +6,29 @@ IMG_GOPATH = /workspace/go
 IMG_GOCACHE = /root/.cache/go-build
 VERIFY_IGNORE = /vendor\|/automock\|/testdata\|/pkg
 
+# Other variables
 LOCAL_DIR = $(shell pwd)
 WORKSPACE_DIR = $(IMG_GOPATH)/src/$(BASE_PKG)/$(APP_PATH)
 FILES_TO_CHECK = find . -type f -name "*.go" | grep -v "$(VERIFY_IGNORE)"
 DIRS_TO_CHECK = go list ./... | grep -v "$(VERIFY_IGNORE)"
 DIRS_TO_IGNORE = go list ./... | grep "$(VERIFY_IGNORE)"
+INTERACTIVE := $(shell [ -t 0 ] && echo 1)
 
-.PHONY: release resolve build build-image push-image pull-licenses
-
+# Docekr configuration
 DOCKER_CREATE_OPTS := --rm -w $(WORKSPACE_DIR) $(BUILDPACK)
-ifndef GOPATH
-DOCKER_CREATE_OPTS := -v $(shell go env GOCACHE):$(IMG_GOCACHE):delegated -v $(shell go env GOPATH)/pkg/dep:$(IMG_GOPATH)/pkg/dep:delegated -t $(DOCKER_CREATE_OPTS)
-DOCKER_INTERACTIVE := -i
+
+ifdef GOPATH
+DOCKER_CREATE_OPTS := -v $(shell go env GOCACHE):$(IMG_GOCACHE):delegated -v $(shell go env GOPATH)/pkg/dep:$(IMG_GOPATH)/pkg/dep:delegated $(DOCKER_CREATE_OPTS)
 endif
 
+ifdef INTERACTIVE
+DOCKER_INTERACTIVE := -i
+DOCKER_CREATE_OPTS := -t $(DOCKER_CREATE_OPTS)
+endif
+
+# Buildpack directives
 define buildpack-mount
+	@echo make $(1)
 	@docker run $(DOCKER_INTERACTIVE) -v $(LOCAL_DIR):$(WORKSPACE_DIR):delegated $(DOCKER_CREATE_OPTS) make $(1)-local
 endef
 
@@ -29,48 +38,68 @@ define buildpack-cp-ro
 	@docker start $(DOCKER_INTERACTIVE) $(container)
 endef
 
-verify: test imports fmt
-verify-local: test-local imports-local fmt-local dep-status-local
+.PHONY: verify format release
+verify: test check-imports check-fmt
+format: imports fmt
 release: resolve dep-status verify build-image push-image
 
+.PHONY: build-image push-image
 build-image: pull-licenses
 	docker build -t $(IMG_NAME) .
-
 push-image:
 	docker tag $(IMG_NAME) $(IMG_NAME):$(TAG)
 	docker push $(IMG_NAME):$(TAG)
 
+.PHONY: build build-local
 build:
 	$(call buildpack-mount,build)
 build-local:
 	env CGO_ENABLED=0 go build -o $(APP_NAME)
 	rm $(APP_NAME)
 
+.PHONY: resolve resolve-local
 resolve:
 	$(call buildpack-mount,resolve)
 resolve-local:
 	dep ensure -vendor-only
 
+.PHONY: ensure ensure-local
 ensure:
 	$(call buildpack-mount,ensure)
 ensure-local:
 	dep ensure
 
+.PHONY: dep-status dep-status-local
 dep-status:
 	$(call buildpack-mount,dep-status)
 dep-status-local:
 	dep status
 
+.PHONY: test test-local
 test:
 	$(call buildpack-cp-ro,test)
 test-local:
 	go test ./...
 
+.PHONY: check-imports check-imports-local
+check-imports:
+	$(call buildpack-mount,check-imports)
+check-imports-local:
+	exit $(shell goimports -l $$($(FILES_TO_CHECK)) | wc -l | xargs)
+
+.PHONY: imports imports-local
 imports:
 	$(call buildpack-mount,imports)
 imports-local:
 	goimports -w -l $$($(FILES_TO_CHECK))
 
+.PHONY: check-fmt check-fmt-local
+check-fmt:
+	$(call buildpack-mount,check-fmt)
+check-fmt-local:
+	exit $(shell gofmt -l $$($(FILES_TO_CHECK)) | wc -l | xargs)
+
+.PHONY: fmt fmt-local
 fmt:
 	$(call buildpack-mount,fmt)
 fmt-local:
