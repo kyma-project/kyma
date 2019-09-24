5a9e4b1dbf4ee945aadbf1c6da144b4b5b3185c5
Antoine Cotten <antoine.cotten@sap.com>
2019-09-24 10:45:31 +0200

    Migrate function-controller build backend to Tekton (#5652)
    * Convert Knative Builds to Tekton TaskRuns.
    * Perform semantic comparison of Knative Services instead of using DeepEqual to avoid unnecessary syncs.

--- components/function-controller/pkg/controller/function/function_controller_suite_test.go
+++ components/function-controller/pkg/controller/function/function_controller_suite_test.go
@@ -18,47 +18,58 @@ package function
 
 import (
 	"fmt"
-	stdlog "log"
 	"os"
 	"path/filepath"
 	"sync"
 	"testing"
 
-	buildv1alpha1 "github.com/knative/build/pkg/apis/build/v1alpha1"
-	"github.com/kyma-project/kyma/components/function-controller/pkg/apis"
-	"github.com/onsi/gomega"
+	gm "github.com/onsi/gomega"
+
+	tektonv1alpha1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
+	servingv1alpha1 "knative.dev/serving/pkg/apis/serving/v1alpha1"
+
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	servingv1alpha1 "knative.dev/serving/pkg/apis/serving/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+
+	"github.com/kyma-project/kyma/components/function-controller/pkg/apis"
 )
 
 var cfg *rest.Config
 
 func TestMain(m *testing.M) {
 	t := &envtest.Environment{
-		Config:            cfg,
-		CRDDirectoryPaths: []string{filepath.Join("..", "..", "..", "config", "crds")},
+		Config: cfg,
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "..", "..", "config", "crds"),
+			filepath.Join("..", "..", "..", "config", "crds-thirdparty"),
+		},
 	}
 
 	logf.SetLogger(logf.ZapLogger(false))
-	apis.AddToScheme(scheme.Scheme)
 
-	if err := servingv1alpha1.SchemeBuilder.AddToScheme(scheme.Scheme); err != nil {
-		log.Error(err, "unable add serving APIs to scheme")
+	if err := apis.AddToScheme(scheme.Scheme); err != nil {
+		log.Error(err, "unable add serverless APIs to scheme")
 		os.Exit(1)
 	}
-	if err := buildv1alpha1.AddToScheme(scheme.Scheme); err != nil {
-		log.Error(err, "unable add Build APIs to scheme")
+
+	if err := servingv1alpha1.SchemeBuilder.AddToScheme(scheme.Scheme); err != nil {
+		log.Error(err, "unable add Knative Serving APIs to scheme")
+		os.Exit(1)
+	}
+
+	if err := tektonv1alpha1.AddToScheme(scheme.Scheme); err != nil {
+		log.Error(err, "unable add Tekton APIs to scheme")
 		os.Exit(1)
 	}
 
 	var err error
 	if cfg, err = t.Start(); err != nil {
-		stdlog.Fatal(err)
+		log.Error(err, "failed to start Kubernetes test environment")
+		os.Exit(1)
 	}
 
 	code := m.Run()
@@ -69,7 +80,6 @@ func TestMain(m *testing.M) {
 // SetupTestReconcile returns a reconcile.Reconcile implementation that delegates to inner and
 // writes the request to requests after Reconcile is finished. If the reconcile function encounters any error, it is written to the errors channel
 func SetupTestReconcile(inner reconcile.Reconciler) (reconcile.Reconciler, chan reconcile.Request, chan error) {
-
 	requests := make(chan reconcile.Request)
 	errors := make(chan error)
 
@@ -86,13 +96,13 @@ func SetupTestReconcile(inner reconcile.Reconciler) (reconcile.Reconciler, chan 
 }
 
 // StartTestManager adds recFn
-func StartTestManager(mgr manager.Manager, g *gomega.GomegaWithT) (chan struct{}, *sync.WaitGroup) {
+func StartTestManager(mgr manager.Manager, g *gm.GomegaWithT) (chan struct{}, *sync.WaitGroup) {
 	stop := make(chan struct{})
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		g.Expect(mgr.Start(stop)).NotTo(gomega.HaveOccurred())
+		g.Expect(mgr.Start(stop)).NotTo(gm.HaveOccurred())
 	}()
 	return stop, wg
 }
