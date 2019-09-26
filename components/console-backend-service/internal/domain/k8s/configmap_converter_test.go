75178642933f3cffb6127861b8f08c6846359186
Szymon Janota <szymon.janota@sap.com>
2019-09-26 09:53:55 +0200

    Break test to check CI behavoiour

--- components/console-backend-service/internal/domain/k8s/configmap_converter_test.go
+++ components/console-backend-service/internal/domain/k8s/configmap_converter_test.go
@@ -21,7 +21,7 @@ func TestConfigMapConverter_ToGQL(t *testing.T) {
 				CreationTimestamp: metav1.Time{},
 				Labels: map[string]string{
 					"exampleKey":  "exampleValue",
-					"exampleKey2": "exampleValue2",
+					"exampleKey2": "exampleValue",
 				},
 			},
 		}
