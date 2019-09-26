3356c777e6569dd4a347090ab7faad1acee85316
adamwalach <adam.walach@sap.com>
2019-09-26 15:22:44 +0200

    Apply review comments

--- components/console-backend-service/hack/update-mocks.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-mockery -case underscore -name DiscoveryInterface -dir ./vendor/k8s.io/client-go/discovery/ -output ./internal/domain/k8s/automock -outpkg automock
