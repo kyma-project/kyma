7d0259adeba59dbdd70654a60933ec0bf6ab378d
Sayan Hazra <sayan.mnit@gmail.com>
2019-09-27 17:29:19 +0200

    Removing unused methods

--- components/event-bus/internal/knative/subscription/controller/subscription/reconcile_test.go
+++ components/event-bus/internal/knative/subscription/controller/subscription/reconcile_test.go
@@ -275,7 +275,6 @@ func makeReadySubscription() *eventingv1alpha1.Subscription {
 	subscription := makeSubscriptionWithFinalizer()
 	subscription.Status.Conditions = []eventingv1alpha1.SubscriptionCondition{
 		{Type: eventingv1alpha1.EventsActivated, Status: eventingv1alpha1.ConditionTrue},
-		{Type: eventingv1alpha1.ChannelReady, Status: eventingv1alpha1.ConditionTrue},
 		{Type: eventingv1alpha1.SubscriptionReady, Status: eventingv1alpha1.ConditionTrue},
 		{Type: eventingv1alpha1.Ready, Status: eventingv1alpha1.ConditionTrue},
 	}
@@ -295,9 +294,6 @@ func makeEventsActivatedSubscription() *eventingv1alpha1.Subscription {
 	subscription := makeSubscriptionWithFinalizer()
 	subscription.Status.Conditions = []eventingv1alpha1.SubscriptionCondition{{
 		Type:   eventingv1alpha1.EventsActivated,
-		Status: eventingv1alpha1.ConditionTrue,
-	}, {
-		Type:   eventingv1alpha1.ChannelReady,
 		Status: eventingv1alpha1.ConditionTrue,
 	}, {
 		Type:   eventingv1alpha1.SubscriptionReady,
