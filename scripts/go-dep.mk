35377d2004bccbc3389e75a0027a99d9cb1795f7
Szymon Janota <szymon.janota@sap.com>
2019-09-30 10:52:53 +0200

    Add target to open a buildpack

--- scripts/go-dep.mk
+++ scripts/go-dep.mk
@@ -138,3 +138,9 @@ endif
 .PHONY: list
 list:
 	@$(MAKE) -pRrq -f $(COMPONENT_DIR)/Makefile : 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | sort | egrep -v -e '^[^[:alnum:]]' -e '^$@$$'
+
+.PHONY: exec
+exec:
+	@docker run $(DOCKER_INTERACTIVE) \
+    		-v $(COMPONENT_DIR):$(WORKSPACE_COMPONENT_DIR):delegated \
+    		$(DOCKER_CREATE_OPTS) bash
