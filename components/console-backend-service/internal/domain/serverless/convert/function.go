584050f542b5481432bc53cfb4ce3d6a61bea5bd
Szymon Janota <szymon.janota@sap.com>
2019-10-03 09:20:22 +0200

    Extract unstructured to resource pakcage

--- components/console-backend-service/internal/domain/serverless/convert/function.go
+++ components/console-backend-service/internal/domain/serverless/convert/function.go
@@ -2,45 +2,21 @@
 package convert
 
 import (
-	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/serverless/pretty"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlschema"
+	"github.com/kyma-project/kyma/components/console-backend-service/internal/resource"
 	"github.com/kyma-project/kyma/components/function-controller/pkg/apis/serverless/v1alpha1"
-	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
-
 func FunctionToUnstructured(obj *v1alpha1.Function) (*unstructured.Unstructured, error) {
-	if obj == nil {
-		return nil, nil
-	}
-
-	u, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
-	if err != nil {
-		return nil, errors.Wrapf(err, "while converting resource %s %T to unstructured", pretty.Function, obj)
-	}
-	if len(u) == 0 {
-		return nil, nil
-	}
-
-	return &unstructured.Unstructured{Object: u}, nil
+	return resource.ToUnstructured(obj)
 }
 
 func UnstructuredToFunction(obj *unstructured.Unstructured) (*v1alpha1.Function, error) {
-	if obj == nil {
-		return nil, nil
-	}
-
-	var function v1alpha1.Function
-	err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, &function)
-	if err != nil {
-		return nil, errors.Wrapf(err, "while converting unstructured to resource %s %s", pretty.Function, obj.Object)
-	}
-
-	return &function, nil
+	var function *v1alpha1.Function
+	err := resource.FromUnstructured(obj, function)
+	return function, err
 }
-
 
 func FunctionToGQL(item *v1alpha1.Function) (*gqlschema.Function, error) {
 	if item == nil {
