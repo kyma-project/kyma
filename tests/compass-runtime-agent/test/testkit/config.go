da21194f7c4cadcfb791bb4e129d98b55c45af89
Szymon Gibala <szymon.gibala@sap.com>
2019-10-03 10:01:58 +0200

    Update chart

--- tests/compass-runtime-agent/test/testkit/config.go
+++ tests/compass-runtime-agent/test/testkit/config.go
@@ -17,8 +17,8 @@ type TestConfig struct {
 	TestPodAppLabel                string        `envconfig:"TEST_POD_APP_LABEL" default:"compass-runtime-agent-tests"`
 	MockServicePort                int32         `envconfig:"MOCK_SERVICE_PORT" default:"8080"`
 	MockServiceName                string        `envconfig:"MOCK_SERVICE_NAME" default:"compass-runtime-agent-tests-mock"`
-	ConfigApplicationWaitTime      int64         `envconfig:"CONFIG_APPLICATION_WAIT_TIME" default:"40"`
-	ProxyInvalidationWaitTime      int64         `envconfig:"PROXY_INVALIDATION_WAIT_TIME" default:"150"`
+	ConfigApplicationWaitTime      time.Duration `envconfig:"CONFIG_APPLICATION_WAIT_TIME" default:"40s"`
+	ProxyInvalidationWaitTime      time.Duration `envconfig:"PROXY_INVALIDATION_WAIT_TIME" default:"150s"`
 	GraphQLLog                     bool          `envconfig:"GRAPHQL_LOG" default:"false"`
 	ScenarioLabel                  string        `envconfig:"SCENARIO_LABEL" default:"COMPASS_RUNTIME_AGENT_TESTS"`
 	HydraPublicURL                 string        `envconfig:"HYDRA_PUBLIC_URL" default:"http://ory-hydra-public.kyma-system:4444"`
