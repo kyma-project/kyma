ed05e3929c8a6c62d2e986e91e988b2f55e72ebf
Miskiewicz, Piotr <piotr.miskiewicz@sap.com>
2019-10-07 11:50:28 +0200

    Unify makefiles for: SBUC, AppBroker, Acc-tests and Upgrade tests

--- tests/acceptance/application/cmd/gateway-client/main.go
+++ tests/acceptance/application/cmd/gateway-client/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/typed/core/v1"
+	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	restclient "k8s.io/client-go/rest"
 )
 
