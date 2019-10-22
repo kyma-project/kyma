ddfad4cc1df48c63ad3738daa010224fa1dea51a
Jakub Kabza <jakub.kabza@sap.com>
2019-10-22 10:56:59 +0200

    retrier

---
+++ tests/integration/api-gateway/gateway-tests/pkg/callretry/callretry.go
@@ -0,0 +1,74 @@
+package callretry
+
+import (
+	"fmt"
+	"github.com/avast/retry-go"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+type securityValidator struct {
+	securedClient, unsecuredClient *http.Client
+	opts                           []retry.Option
+}
+
+func NewSecurityValidator(c *http.Client, opts []retry.Option) *securityValidator {
+	return &securityValidator{
+		securedClient:   c,
+		unsecuredClient: &http.Client{},
+		opts:            opts,
+	}
+}
+
+func (h *securityValidator) ValidateAPISecured(url string) error {
+
+	err := h.withRetries(func() (*http.Response, error) {
+		return h.securedClient.Get(fmt.Sprintf("https://%s", url))
+	}, httpOkPredicate)
+
+	if err != nil {
+		return err
+	}
+
+	return h.withRetries(func() (*http.Response, error) {
+		return h.unsecuredClient.Get(fmt.Sprintf("https://%s", url))
+	}, httpUnauthorizedPredicate)
+}
+
+func (h *securityValidator) ValidateAPINotSecured(url string) error {
+	return h.withRetries(func() (*http.Response, error) {
+		return h.unsecuredClient.Get(fmt.Sprintf("https://%s", url))
+	}, httpOkPredicate)
+}
+
+func (h *securityValidator) withRetries(httpCall func() (*http.Response, error), shouldRetry func(*http.Response) bool) error {
+
+	if err := retry.Do(func() error {
+
+		var callErr error
+		response, callErr := httpCall()
+		if callErr != nil {
+			return callErr
+		}
+
+		if shouldRetry(response) {
+			return errors.Errorf("unexpected response: %s", response.Status)
+		}
+
+		return nil
+	},
+		h.opts...
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func httpOkPredicate(response *http.Response) bool {
+	return response.StatusCode < 200 || response.StatusCode > 299
+}
+
+func httpUnauthorizedPredicate(response *http.Response) bool {
+	return response.StatusCode != 401
+}
