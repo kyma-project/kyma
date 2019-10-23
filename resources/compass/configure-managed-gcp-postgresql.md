2fc67f69c4cc80c6f854de4e22c868cfb504767c
Tomasz Gorgol <45565988+tgorgol@users.noreply.github.com>
2019-10-23 17:18:25 +0200

    Bump compass chart and update runtime agent tests (#6081)
    * Update compass chart
    
    * Update runtime agent tests
    
    * Apply suggestions from code review
    
    Co-Authored-By: Klaudia Grzondziel <35192450+klaudiagrz@users.noreply.github.com>

--- resources/compass/configure-managed-gcp-postgresql.md
+++ resources/compass/configure-managed-gcp-postgresql.md
@@ -10,9 +10,9 @@ To set up the database, create the following:
   - Cloud SQL Editor
   - Cloud SQL Admin
 
-## Install the Compass with managed GCP PostgreSQL database
+## Install Compass with managed GCP PostgreSQL database
 
-To install the Compass with GCP managed Postgres database, set the `database.embedded.enabled` value to `false` inside the `./chart/compass/values.yaml` file, and fill in these values:
+To install Compass with GCP managed Postgres database, set the **database.embedded.enabled** value to `false` inside the `./chart/compass/values.yaml` file, and fill these values:
 
 | Parameter | Description | Values | Default |
 | --- | --- | --- | --- |
--- tests/compass-runtime-agent/test/testkit/applications/app.go
+++ tests/compass-runtime-agent/test/testkit/applications/app.go
@@ -2,12 +2,12 @@ package applications
 
 import "github.com/kyma-incubator/compass/components/director/pkg/graphql"
 
-type ApplicationInput graphql.ApplicationInput
+type ApplicationCreateInput graphql.ApplicationCreateInput
 
-func NewApplication(name, description string, labels map[string]interface{}) *ApplicationInput {
+func NewApplication(name, description string, labels map[string]interface{}) *ApplicationCreateInput {
 	appLabels := graphql.Labels(labels)
 
-	app := ApplicationInput(graphql.ApplicationInput{
+	app := ApplicationCreateInput(graphql.ApplicationCreateInput{
 		Name:        name,
 		Description: &description,
 		Labels:      &appLabels,
@@ -19,11 +19,11 @@ func NewApplication(name, description string, labels map[string]interface{}) *Ap
 	return &app
 }
 
-func (input *ApplicationInput) ToCompassInput() graphql.ApplicationInput {
-	return graphql.ApplicationInput(*input)
+func (input *ApplicationCreateInput) ToCompassInput() graphql.ApplicationCreateInput {
+	return graphql.ApplicationCreateInput(*input)
 }
 
-func (input *ApplicationInput) WithAPIs(apis []*APIDefinitionInput) *ApplicationInput {
+func (input *ApplicationCreateInput) WithAPIs(apis []*APIDefinitionInput) *ApplicationCreateInput {
 	compassAPIs := make([]*graphql.APIDefinitionInput, len(apis))
 	for i, api := range apis {
 		compassAPIs[i] = api.ToCompassInput()
@@ -34,7 +34,7 @@ func (input *ApplicationInput) WithAPIs(apis []*APIDefinitionInput) *Application
 	return input
 }
 
-func (input *ApplicationInput) WithEventAPIs(apis []*EventAPIDefinitionInput) *ApplicationInput {
+func (input *ApplicationCreateInput) WithEventAPIs(apis []*EventAPIDefinitionInput) *ApplicationCreateInput {
 	compassAPIs := make([]*graphql.EventAPIDefinitionInput, len(apis))
 	for i, api := range apis {
 		compassAPIs[i] = api.ToCompassInput()
@@ -43,4 +43,19 @@ func (input *ApplicationInput) WithEventAPIs(apis []*EventAPIDefinitionInput) *A
 	input.EventAPIs = compassAPIs
 
 	return input
+}
+
+type ApplicationUpdateInput graphql.ApplicationUpdateInput
+
+func NewApplicationUpdateInput(name, description string) *ApplicationUpdateInput {
+	appUpdateInput := ApplicationUpdateInput(graphql.ApplicationUpdateInput{
+		Name:        name,
+		Description: &description,
+	})
+
+	return &appUpdateInput
+}
+
+func (input *ApplicationUpdateInput) ToCompassInput() graphql.ApplicationUpdateInput {
+	return graphql.ApplicationUpdateInput(*input)
 }
