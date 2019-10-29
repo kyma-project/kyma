982d75df8b5296c9728d8905792638dd5c6d43ad
Maciej Urbańczyk <maciej.urbanczyk@sap.com>
2019-10-29 14:02:33 +0100

    Fix script for kyma-project.io preview

--- .kyma-project-io/build-preview.sh
+++ .kyma-project-io/build-preview.sh
@@ -33,6 +33,10 @@ step() {
   echo -e "\\n${YELLOW}${message}${NC}"
 }
 
+remove-cache-content() {
+  ( rm -rf "${BUILD_DIR}" ) || true
+}
+
 copy-website-repo() {
   git clone -b "master" --single-branch "${WEBSITE_REPO}" "${WEBSITE_DIR}"
 }
@@ -43,6 +47,10 @@ build-preview() {
 }
 
 main() {
+  step "Remove website cache content"
+  remove-cache-content
+  pass "Removed"
+
   step "Copying kyma/website repo"
   copy-website-repo
   pass "Copied"
