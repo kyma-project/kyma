ab8a90e2bcfcdf90f1a09ae21b67df62d489310a
kubadz <jakub.dziechciewicz@sap.com>
2019-10-29 11:04:20 +0100

    add APIRule resolver tests for create & update

--- components/console-backend-service/internal/domain/apigateway/apirule_resolver_test.go
+++ components/console-backend-service/internal/domain/apigateway/apirule_resolver_test.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"testing"
 
+	"k8s.io/apimachinery/pkg/runtime"
+
 	"github.com/kyma-incubator/api-gateway/api/v1alpha1"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/apigateway/automock"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlerror"
@@ -116,109 +118,76 @@ func TestApiRuleResolver_APIRuleQuery(t *testing.T) {
 	})
 }
 
-//func TestApiResolver_CreateAPI(t *testing.T) {
-//	namespace := "test-1"
-//	name := "test-api"
-//	hostname := "test-hostname"
-//	serviceName := "test-service-name"
-//	servicePort := 8080
-//	jwksUri := "http://test-jwks-uri"
-//	issuer := "test-issuer"
-//
-//	params := paramsToAPICreationInput(hostname, serviceName, jwksUri, issuer, servicePort, nil, nil)
-//
-//	t.Run("Should create an API", func(t *testing.T) {
-//		api := fixTestApi(name, namespace, hostname, serviceName, jwksUri, issuer, servicePort)
-//		expected := testApiToGQL(name, hostname, serviceName, jwksUri, issuer, servicePort)
-//
-//		converter := automock.NewApiConv()
-//		converter.On("ToApi", name, namespace, params).Return(api, nil).Once()
-//
-//		service := automock.NewApiSvc()
-//		service.On("Create", api).Return(api, nil).Once()
-//
-//		resolver, err := newApiResolver(service)
-//		require.NoError(t, err)
-//
-//		result, err := resolver.CreateAPI(nil, name, namespace, params)
-//
-//		service.AssertExpectations(t)
-//		require.NoError(t, err)
-//		assert.Equal(t, expected, result)
-//	})
-//
-//	t.Run("Should return an error", func(t *testing.T) {
-//		api := fixTestApi(name, namespace, hostname, serviceName, jwksUri, issuer, servicePort)
-//
-//		converter := automock.NewApiConv()
-//		converter.On("ToApi", name, namespace, params).Return(api, nil).Once()
-//
-//		service := automock.NewApiSvc()
-//		service.On("Create", api).Return(nil, errors.New("test")).Once()
-//
-//		resolver, err := newApiResolver(service)
-//		require.NoError(t, err)
-//
-//		_, err = resolver.CreateAPI(nil, name, namespace, params)
-//
-//		service.AssertExpectations(t)
-//		require.Error(t, err)
-//		assert.True(t, gqlerror.IsInternal(err))
-//	})
-//
-//}
-//
-//func TestApiResolver_UpdateAPI(t *testing.T) {
-//	namespace := "test-1"
-//	name := "test-api"
-//	hostname := "test-hostname"
-//	serviceName := "test-service-name"
-//	servicePort := 8080
-//	jwksUri := "http://test-jwks-uri"
-//	issuer := "test-issuer"
-//
-//	params := paramsToAPICreationInput(hostname, serviceName, jwksUri, issuer, servicePort, nil, nil)
-//
-//	t.Run("Should update an API", func(t *testing.T) {
-//		api := fixTestApi(name, namespace, hostname, serviceName, jwksUri, issuer, servicePort)
-//		expected := testApiToGQL(name, hostname, serviceName, jwksUri, issuer, servicePort)
-//
-//		converter := automock.NewApiConv()
-//		converter.On("ToApi", name, namespace, params).Return(api, nil).Once()
-//
-//		service := automock.NewApiSvc()
-//		service.On("Update", api).Return(api, nil).Once()
-//
-//		resolver, err := newApiResolver(service)
-//		require.NoError(t, err)
-//
-//		result, err := resolver.UpdateAPI(nil, name, namespace, params)
-//
-//		service.AssertExpectations(t)
-//		require.NoError(t, err)
-//		assert.Equal(t, expected, result)
-//	})
-//
-//	t.Run("Should return an error", func(t *testing.T) {
-//		api := fixTestApi(name, namespace, hostname, serviceName, jwksUri, issuer, servicePort)
-//
-//		converter := automock.NewApiConv()
-//		converter.On("ToApi", name, namespace, params).Return(api, nil).Once()
-//
-//		service := automock.NewApiSvc()
-//		service.On("Update", api).Return(nil, errors.New("test")).Once()
-//
-//		resolver, err := newApiResolver(service)
-//		require.NoError(t, err)
-//
-//		_, err = resolver.UpdateAPI(nil, name, namespace, params)
-//
-//		service.AssertExpectations(t)
-//		require.Error(t, err)
-//		assert.True(t, gqlerror.IsInternal(err))
-//	})
-//
-//}
+func TestApiRuleResolver_CreateAPIRule(t *testing.T) {
+	params := apiRuleInputParams(hostname1, serviceName1, servicePort1, gateway1)
+	t.Run("Should create an APIRule", func(t *testing.T) {
+		apiRule := fixTestApiRule(name, namespace, hostname1, serviceName1, servicePort1, gateway1)
+		expected := testApiRuleToGQL(name, hostname1, serviceName1, servicePort1, gateway1)
+
+		service := automock.NewApiRuleSvc()
+		service.On("Create", apiRule).Return(apiRule, nil).Once()
+
+		resolver, err := newApiRuleResolver(service)
+		require.NoError(t, err)
+
+		result, err := resolver.CreateAPIRule(nil, name, namespace, params)
+
+		service.AssertExpectations(t)
+		require.NoError(t, err)
+		assert.Equal(t, expected, *result)
+	})
+
+	t.Run("Should return an error", func(t *testing.T) {
+		apiRule := fixTestApiRule(name, namespace, hostname1, serviceName1, servicePort1, gateway1)
+
+		service := automock.NewApiRuleSvc()
+		service.On("Create", apiRule).Return(nil, errors.New("test")).Once()
+
+		resolver, err := newApiRuleResolver(service)
+		require.NoError(t, err)
+
+		_, err = resolver.CreateAPIRule(nil, name, namespace, params)
+
+		service.AssertExpectations(t)
+		require.Error(t, err)
+		assert.True(t, gqlerror.IsInternal(err))
+	})
+}
+
+func TestApiRuleResolver_UpdateAPIRule(t *testing.T) {
+	params := apiRuleInputParams(hostname1, serviceName1, servicePort1, gateway1)
+	t.Run("Should update an API", func(t *testing.T) {
+		apiRule := fixTestApiRule(name, namespace, hostname1, serviceName1, servicePort1, gateway1)
+		expected := testApiRuleToGQL(name, hostname1, serviceName1, servicePort1, gateway1)
+
+		service := automock.NewApiRuleSvc()
+		service.On("Update", apiRule).Return(apiRule, nil).Once()
+
+		resolver, err := newApiRuleResolver(service)
+		require.NoError(t, err)
+
+		result, err := resolver.UpdateAPIRule(nil, name, namespace, params)
+
+		service.AssertExpectations(t)
+		require.NoError(t, err)
+		assert.Equal(t, expected, *result)
+	})
+	t.Run("Should return an error", func(t *testing.T) {
+		apiRule := fixTestApiRule(name, namespace, hostname1, serviceName1, servicePort1, gateway1)
+
+		service := automock.NewApiRuleSvc()
+		service.On("Update", apiRule).Return(nil, errors.New("test")).Once()
+
+		resolver, err := newApiRuleResolver(service)
+		require.NoError(t, err)
+
+		_, err = resolver.UpdateAPIRule(nil, name, namespace, params)
+
+		service.AssertExpectations(t)
+		require.Error(t, err)
+		assert.True(t, gqlerror.IsInternal(err))
+	})
+}
 
 func TestApiRuleResolver_DeleteAPIRule(t *testing.T) {
 	t.Run("Should delete an APIRule", func(t *testing.T) {
@@ -276,7 +245,7 @@ func fixTestApiRule(ruleName string, namespace string, hostName string, serviceN
 	return &v1alpha1.APIRule{
 		TypeMeta: v1.TypeMeta{
 			APIVersion: "gateway.kyma-project.io/v1alpha1",
-			Kind:       "ApiRule",
+			Kind:       "APIRule",
 		},
 		ObjectMeta: v1.ObjectMeta{
 			Name:      ruleName,
@@ -297,21 +266,13 @@ func fixTestApiRule(ruleName string, namespace string, hostName string, serviceN
 						{
 							Handler: &rulev1alpha1.Handler{
 								Name: "allow",
+								Config: &runtime.RawExtension{
+									Raw: []byte("{}"),
+								},
 							},
 						},
 					},
 				},
-			},
-		},
-		Status: v1alpha1.APIRuleStatus{
-			APIRuleStatus: &v1alpha1.APIRuleResourceStatus{
-				Code: "OK",
-			},
-			VirtualServiceStatus: &v1alpha1.APIRuleResourceStatus{
-				Code: "OK",
-			},
-			AccessRuleStatus: &v1alpha1.APIRuleResourceStatus{
-				Code: "OK",
 			},
 		},
 	}
@@ -338,15 +299,25 @@ func testApiRuleToGQL(ruleName string, hostName string, serviceName string, serv
 				},
 			},
 		},
-		Status: gqlschema.APIRuleStatuses{
-			APIRuleStatus: &gqlschema.APIRuleStatus{
-				Code: "OK",
-			},
-			VirtualServiceStatus: &gqlschema.APIRuleStatus{
-				Code: "OK",
-			},
-			AccessRuleStatus: &gqlschema.APIRuleStatus{
-				Code: "OK",
+	}
+}
+
+func apiRuleInputParams(hostName string, serviceName string, servicePort uint32, gateway string) gqlschema.APIRuleInput {
+	return gqlschema.APIRuleInput{
+		Host:        hostName,
+		ServiceName: serviceName,
+		ServicePort: int(servicePort),
+		Gateway:     gateway,
+		Rules: []gqlschema.RuleInput{
+			{
+				Path:    "*",
+				Methods: []string{"GET"},
+				AccessStrategies: []gqlschema.APIRuleConfigInput{
+					{
+						Name:   "allow",
+						Config: gqlschema.JSON{},
+					},
+				},
 			},
 		},
 	}
