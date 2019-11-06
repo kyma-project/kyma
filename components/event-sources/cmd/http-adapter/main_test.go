772a4cb2adbbac3efaf4e33a6fc24459a0f05355
Nils Schmidt <nils.schmidt02@sap.com>
2019-11-06 08:43:51 +0100

    Start sink in integration test

--- components/event-sources/cmd/http-adapter/main_test.go
+++ components/event-sources/cmd/http-adapter/main_test.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"github.com/cloudevents/sdk-go/pkg/cloudevents"
 	"knative.dev/eventing/pkg/kncloudevents"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"strconv"
-	"sync"
 	"testing"
 	"time"
 )
@@ -18,12 +19,70 @@ import (
 // - receiving the CE event enriched by application source from adapter using a mocked server in the test
 // - the sinkURI is set to the mocked http server
 func TestAdapter(t *testing.T) {
-	var wg sync.WaitGroup
-	//sinkURI := "https://dummy-kn-channel.default.svc.cluster.local"
-	sinkURI := "http://localhost:55555"
 	port := 54321
 	adapterURI := fmt.Sprintf("http://localhost:%d", port)
 
+	// receive channel for http.Request from sink
+	sinkRequests := make(chan http.Request, 2)
+	// start mock sink
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("received sink request")
+		if _, err := fmt.Fprintln(w, "Hello, cloudevents client"); err != nil {
+			t.Error(err)
+		}
+		sinkRequests <- *r
+	}))
+	defer ts.Close()
+	sinkURI := ts.URL
+
+	setEnvironmentVariable(sinkURI, port, t)
+
+	// start http-adapter
+	go startAdapter()
+
+	// TODO(nachtmaar): remove sleep
+	time.Sleep(5 * time.Second)
+	sendEvent(t, adapterURI)
+	eventResponse := sendEvent(t, adapterURI)
+
+	// TODO(nachtmaar): validate eventResponse
+	t.Log(eventResponse)
+	fmt.Println("starting select")
+
+	receiveFromSink(sinkRequests, t)
+	receiveFromSink(sinkRequests, t)
+}
+
+func receiveFromSink(sinkRequests chan http.Request, t *testing.T) *http.Request {
+	select {
+	case sinkRequest := <-sinkRequests:
+		fmt.Printf("got sink request: %v\n", sinkRequest)
+		return &sinkRequest
+	case <-time.After(60 * time.Second):
+		t.Error("no cloud event received on sink side")
+		return nil
+	}
+}
+
+func sendEvent(t *testing.T, adapterURI string) *cloudevents.Event {
+	client, err := kncloudevents.NewDefaultClient(adapterURI)
+	if err != nil {
+		t.Fatal(err)
+	}
+	event := cloudevents.New(cloudevents.CloudEventsVersionV1)
+	// TODO(nachtmaar): send custom events, e.g. allow malformed events and different content-types
+	event.Context.SetSource("foo")
+	event.Context.SetType("foo")
+	event.Context.SetID("foo")
+	t.Logf("sending event to http adapter: %s", event)
+	_, eventResponse, err := client.Send(context.Background(), event)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return eventResponse
+}
+
+func setEnvironmentVariable(sinkURI string, port int, t *testing.T) {
 	// set required environment variables
 	envs := map[string]string{
 		"SINK_URI":           sinkURI,
@@ -39,34 +98,8 @@ func TestAdapter(t *testing.T) {
 			t.Fatal(err)
 		}
 	}
-
-	wg.Add(1)
-	go fakeMain(&wg)
-
-	client, err := kncloudevents.NewDefaultClient(adapterURI)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// TODO(nachtmaar): remove sleep
-	time.Sleep(5 * time.Second)
-	event := cloudevents.New(cloudevents.CloudEventsVersionV1)
-	// TODO(nachtmaar): send custom events, e.g. allow malformed events and different content-types
-	event.Context.SetSource("foo")
-	event.Context.SetType("foo")
-	event.Context.SetID("foo")
-	t.Logf("sending event to http adapter: %s", event)
-	_, eventResponse, err := client.Send(context.Background(), event)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// TODO(nachtmaar): validate eventResponse
-	t.Log(eventResponse)
-
-	// TODO(nachtmaar): stop process when all tests completed
-	wg.Wait()
 }
 
-func fakeMain(wg *sync.WaitGroup) {
+func startAdapter() {
 	main()
-	wg.Done()
 }
