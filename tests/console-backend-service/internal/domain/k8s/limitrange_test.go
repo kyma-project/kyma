6944ff57f6a84b95abdd83fd77f0d33cd3bc2193
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-11-15 15:36:29 +0100

    Revert all changes to upgrade tests2

--- tests/console-backend-service/internal/domain/k8s/limitrange_test.go
+++ tests/console-backend-service/internal/domain/k8s/limitrange_test.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/kyma-project/kyma/tests/console-backend-service/internal/client"
-	"github.com/kyma-project/kyma/tests/console-backend-service/internal/dex"
 	"github.com/kyma-project/kyma/tests/console-backend-service/internal/domain/shared/auth"
 	"github.com/kyma-project/kyma/tests/console-backend-service/internal/graphql"
 	"github.com/kyma-project/kyma/tests/console-backend-service/pkg/waiter"
@@ -25,8 +24,6 @@ const (
 )
 
 func TestLimitRangeQuery(t *testing.T) {
-	dex.SkipTestIfSCIEnabled(t)
-
 	c, err := graphql.New()
 	require.NoError(t, err)
 
