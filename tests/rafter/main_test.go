e1e8482a7c1af56415135de061bf486b7f558a9f
Mateusz Puczyński <mateusz.puczynski@sap.com>
2019-12-18 17:03:28 +0100

    Migrate Assetstore integration tests to Rafter (#6569)

---
+++ tests/rafter/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/kyma-project/kyma/tests/rafter/testsuite"
+	"github.com/onsi/gomega"
+	"github.com/vrischmann/envconfig"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type config struct {
+	KubeconfigPath string `envconfig:"optional"`
+	Test           testsuite.Config
+}
+
+func TestRafter(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	cfg, err := loadConfig("APP")
+	failOnError(g, err)
+
+	restConfig, err := newRestClientConfig(cfg.KubeconfigPath)
+	failOnError(g, err)
+
+	testSuite, err := testsuite.New(restConfig, cfg.Test, t, g)
+	failOnError(g, err)
+
+	testSuite.Run()
+
+	testSuite.Cleanup()
+}
+
+func newRestClientConfig(kubeconfigPath string) (*restclient.Config, error) {
+	var config *restclient.Config
+	var err error
+	if kubeconfigPath != "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	} else {
+		config, err = restclient.InClusterConfig()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+func loadConfig(prefix string) (config, error) {
+	cfg := config{}
+	err := envconfig.InitWithPrefix(&cfg, prefix)
+	return cfg, err
+}
+
+func failOnError(g *gomega.GomegaWithT, err error) {
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+}
