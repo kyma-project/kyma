410f29a64f4cd9a867d47a6af90ec973b105eff5
Nils Schmidt <nils.schmidt02@sap.com>
2019-12-19 13:06:30 +0100

    Fix too many arguments error

--- components/application-broker/cmd/broker/main.go
+++ components/application-broker/cmd/broker/main.go
@@ -132,7 +132,7 @@ func SetupServerAndRunControllers(cfg *config.Config, log *logrus.Entry, stopCh 
 	// create broker
 	srv := broker.New(sFact.Application(), sFact.Instance(), sFact.InstanceOperation(), accessChecker,
 		mClient.ApplicationconnectorV1alpha1(), siFacade,
-		mInformersGroup.ApplicationMappings().Lister(), brokerService, &appClient, &mClient, k8sClient, knClient, log, livenessCheckStatus)
+		mInformersGroup.ApplicationMappings().Lister(), brokerService, &mClient, knClient, log, livenessCheckStatus)
 
 	// start informers
 	scInformerFactory.Start(stopCh)
