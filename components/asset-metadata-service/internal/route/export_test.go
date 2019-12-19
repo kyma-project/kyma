6d335d0fbed1b23b170dd0e5dce3538559ba6e06
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-12-19 16:11:27 +0100

    squash

--- components/asset-metadata-service/internal/route/export_test.go
+++
@@ -1,7 +0,0 @@
-package route
-
-import "github.com/kyma-project/kyma/components/asset-metadata-service/pkg/extractor"
-
-func (h *ExtractHandler) SetMetadataExtractor(metadataExtractor extractor.Extractor) {
-	h.metadataExtractor = metadataExtractor
-}
--- components/asset-metadata-service/pkg/processor/export_test.go
+++
@@ -1,9 +0,0 @@
-package processor
-
-func (p *Processor) PopulateErrors(errorsCh chan *ResultError) []ResultError {
-	return p.populateErrors(errorsCh)
-}
-
-func (p *Processor) PopulateResults(resultsCh chan *ResultSuccess) []ResultSuccess {
-	return p.populateResults(resultsCh)
-}
