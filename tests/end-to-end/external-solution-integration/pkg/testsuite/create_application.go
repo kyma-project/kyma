440f51b2cc475d6cc71eaba44ba222ae6081ab38
Anish Joshi <anish.joshi@sap.com>
2020-01-15 10:21:26 +0100

    External solution test for the new Knative Eventing Mesh (#6684)
    * External Solution test setup with Event Mesh Alpha

--- tests/end-to-end/external-solution-integration/pkg/testsuite/create_application.go
+++ tests/end-to-end/external-solution-integration/pkg/testsuite/create_application.go
@@ -6,6 +6,7 @@ import (
 	"github.com/avast/retry-go"
 	acApi "github.com/kyma-project/kyma/components/application-operator/pkg/apis/applicationconnector/v1alpha1"
 	acClient "github.com/kyma-project/kyma/components/application-operator/pkg/client/clientset/versioned/typed/applicationconnector/v1alpha1"
+	esClient "github.com/kyma-project/kyma/components/event-sources/client/generated/clientset/internalclientset/typed/sources/v1alpha1"
 	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/pkg/helpers"
 	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/pkg/step"
 	"github.com/pkg/errors"
@@ -15,6 +16,7 @@ import (
 // CreateApplication is a step which creates new Application
 type CreateApplication struct {
 	applications     acClient.ApplicationInterface
+	httpSources      esClient.HTTPSourceInterface
 	skipInstallation bool
 	name             string
 	accessLabel      string
@@ -25,10 +27,12 @@ type CreateApplication struct {
 var _ step.Step = &CreateApplication{}
 
 // NewCreateApplication returns new CreateApplication
-func NewCreateApplication(name, accessLabel string, skipInstallation bool, tenant, group string, applications acClient.ApplicationInterface) *CreateApplication {
+func NewCreateApplication(name, accessLabel string, skipInstallation bool, tenant, group string,
+	applications acClient.ApplicationInterface, httpSourceClient esClient.HTTPSourceInterface) *CreateApplication {
 	return &CreateApplication{
 		name:             name,
 		applications:     applications,
+		httpSources:      httpSourceClient,
 		skipInstallation: skipInstallation,
 		accessLabel:      accessLabel,
 		tenant:           tenant,
@@ -67,6 +71,7 @@ func (s *CreateApplication) Run() error {
 
 func (s *CreateApplication) isApplicationReady() error {
 	application, err := s.applications.Get(s.name, v1.GetOptions{})
+
 	if err != nil {
 		return err
 	}
@@ -75,7 +80,21 @@ func (s *CreateApplication) isApplicationReady() error {
 		return errors.Errorf("unexpected installation status: %s", application.Status.InstallationStatus.Status)
 	}
 
+	return retry.Do(s.isHttpSourceReady)
+}
+
+func (s *CreateApplication) isHttpSourceReady() error {
+	if s.httpSources != nil {
+		httpsource, err := s.httpSources.Get(s.name, v1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		if !httpsource.Status.IsReady() {
+			return errors.Errorf("httpsource %s is not ready. Status of HttpSource: \n %+v", httpsource.Name, httpsource.Status)
+		}
+	}
 	return nil
+
 }
 
 // Cleanup removes all resources that may possibly created by the step
