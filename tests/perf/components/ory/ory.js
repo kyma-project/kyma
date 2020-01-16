347497c6d1a037ca1392bc484bfe87834e58bb15
Julia Iskierka <jul.iskierka@gmail.com>
2020-01-16 10:47:55 +0100

    Write configuration

--- tests/perf/components/ory/ory.js
+++ tests/perf/components/ory/ory.js
@@ -1,28 +1,46 @@
 import http from 'k6/http';
 import { check, sleep } from "k6";
-
-const k8s = require('@kubernetes/client-node');
+import encoding from "k6/encoding";
 
 export let options = {
-    vus: 10,
-    duration: "1m",
-    rps: 1000,
+    vus: 1,
+    duration: "1s",
+    rps: 1,
     tags: {
         "testName": "http_db_service_10vu_60s_1000",
         "component": "http-db-service",
         "revision": `${__ENV.REVISION}`
+    },
+    conf: {
+        clientId: `${encoding.b64decode(__ENV.CLIENT_ID).trim()}`,
+        clientSecret: `${encoding.b64decode(__ENV.CLIENT_SECRET).trim()}`,
+        domain: `${__ENV.CLUSTER_DOMAIN}`
     }
-}
+};
+
+
+// export ENCODED_CREDENTIALS=$(echo -n "$CLIENT_ID:$CLIENT_SECRET" | base64)
+// -H "Authorization: Basic $ENCODED_CREDENTIALS" -F "grant_type=client_credentials" -F "scope=read"
+// curl -ik -X GET https://httpbin.$DOMAIN/headers -H "Authorization: Bearer $ACCESS_TOKEN_READ"
 
 export function setup() {
-    const client = new k8s.KubeConfig();
-    client.loadFromDefault();
+    const credentials = encoding.b64encode(`${options.conf.clientId}:${options.conf.clientSecret}`);
 
-    const api = client.makeApiClient(k8s.CustomObjectsApi)
+    let url = `https://oauth2.${options.conf.domain}/oauth2/token`;
+    let payload = { "grant_type": "client_credentials", "scope": "read", client_id: options.conf.clientId };
+    let params =  { headers: { "Authorization": `Basic ${credentials}` }};
+
+    let accessToken = http.post(url, payload, params);
+    console.log("token: " + accessToken.body);
+
+    return accessToken.body
 }
 
-export default function() {
-    const response = http.get(`https://http-db-service.${__ENV.CLUSTER_DOMAIN_NAME}/`);
+export default function(data) {
+    let url = `https://httpbin.${options.conf.domain}/headers`;
+    let payload = JSON.stringify({ data: `${Math.random() * 30}` });
+    let params =  { headers: { "Authorization": `Bearer ${data}` }};
+    const response = http.post(url, payload, params);
 
     check(response, {
         "status was 200": (r) => r.status == 200,
@@ -30,3 +48,4 @@ export default function() {
     });
     sleep(1);
 }
+
--- tests/perf/prerequisites/ory/setup.sh
+++ tests/perf/prerequisites/ory/setup.sh
@@ -1,18 +1,20 @@
-set -e
-set -o pipefail
+#!/usr/bin/env bash
 
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TIMEOUT=300
 
 export NAMESPACE=ory-perf-test
 export CLUSTER_DOMAIN=$(kubectl get gateways.networking.istio.io kyma-gateway \
-                        -n kyma-system -ojsonpath="{.spec.servers[0].hosts[0]}" | sed 's/*//g' )
+                        -n kyma-system -ojsonpath="{.spec.servers[0].hosts[0]}" | sed 's/*.//g' )
+export CLIENT_ID=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-z' | fold -w 8 | head -n 1 | base64)
+export CLIENT_SECRET=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-z' | fold -w 32 | head -n 1 | base64)
 
 resources=(
+  namespace.yaml
   deployment.yaml
+  oauth2client.yaml
   apirules.yaml
 )
-
 
 # wait for resource with the given name until it exists or there is a timeout
 function waitFor() {
