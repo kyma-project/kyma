daa219e4af7e4c7e805605a91b3d6cabf1e35236
Miskiewicz, Piotr <piotr.miskiewicz@sap.com>
2020-01-21 12:50:35 +0100

    wip

--- tests/end-to-end/upgrade/pkg/tests/service-catalog/base_flow.go
+++ tests/end-to-end/upgrade/pkg/tests/service-catalog/base_flow.go
@@ -271,7 +271,7 @@ func (f *baseFlow) waitForInstanceFail(name string) error {
 			return false, err
 		}
 		for _, cond := range si.Status.Conditions {
-			if cond.Status == v1beta1.ConditionTrue && cond.Type == v1beta1.ServiceInstanceConditionFailed {
+			if cond.Status == v1beta1.ConditionFalse && cond.Type == v1beta1.ServiceInstanceConditionReady && cond.Reason == "ProvisionCallFailed"
 				return true, nil
 			}
 		}
