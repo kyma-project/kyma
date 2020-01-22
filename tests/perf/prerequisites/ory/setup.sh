fa25c229b3f2ff340ab9029bf888f89a874e2ce8
Julia Iskierka <jul.iskierka@gmail.com>
2020-01-22 10:08:50 +0100

    Setup.sh fix

--- tests/perf/prerequisites/ory/setup.sh
+++ tests/perf/prerequisites/ory/setup.sh
@@ -2,7 +2,10 @@
 
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export NAMESPACE=ory-perf-test
+if [[ -z "${NAMESPACE}" ]]; then
+  NAMESPACE=ory-perf-test
+fi
+
 export CLUSTER_DOMAIN=$(kubectl get gateways.networking.istio.io kyma-gateway \
                         -n kyma-system -ojsonpath="{.spec.servers[0].hosts[0]}" | sed 's/*.//g' )
 
@@ -20,5 +23,5 @@ done
 
 sleep 3s
 
-export CLIENT_ID="$(kubectl get secret -n $NAMESPACE perf-tests-secret -o jsonpath='{.data.client_id}' | base64 --decode)"
-export CLIENT_SECRET="$(kubectl get secret -n $NAMESPACE perf-tests-secret -o jsonpath='{.data.client_secret}' | base64 --decode)"
+export CLIENT_ID="$(kubectl get secret -n $NAMESPACE perf-tests-secret -o jsonpath='{.data.client_id}' | base64 -d)"
+export CLIENT_SECRET="$(kubectl get secret -n $NAMESPACE perf-tests-secret -o jsonpath='{.data.client_secret}' | base64 -d)"
