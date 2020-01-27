39704cc63c820b666d728ce76e185bd9e46c6a0b
pPrecel <filip.strozik@outlook.com>
2020-01-27 13:26:56 +0100

    Fix last changes on fun-controller component

--- components/function-controller/hack/test-cluster.sh
+++ components/function-controller/hack/test-cluster.sh
@@ -81,7 +81,15 @@ knative::install_serving(){
 }
 
 helm::init(){
+    kubectl --namespace kube-system create sa tiller
+    kubectl create clusterrolebinding tiller-cluster-rule \
+      --clusterrole=cluster-admin \
+      --serviceaccount=kube-system:tiller
+
     helm init
+      --service-account tiller \
+      --upgrade --wait  \
+      --history-max 200
 }
 
 main(){
