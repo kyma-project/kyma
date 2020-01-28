230727c041f1c57dcff57ed80781ab444ab9917b
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2020-01-28 14:52:42 +0100

    Turn off specification tests for Compass

--- tests/connector-service-tests/test/apitests/apispec_test.go
+++ tests/connector-service-tests/test/apitests/apispec_test.go
@@ -19,37 +19,39 @@ func TestApiSpec(t *testing.T) {
 
 	hc := testkit.NewHttpClient(config.SkipSslVerify)
 
-	t.Run("should receive api spec", func(t *testing.T) {
-		// when
-		response, err := hc.Get(config.ExternalAPIUrl + apiSpecPath)
+	if !config.Compass {
+		t.Run("should receive api spec", func(t *testing.T) {
+			// when
+			response, err := hc.Get(config.ExternalAPIUrl + apiSpecPath)
 
-		// then
-		require.NoError(t, err)
-		require.Equal(t, http.StatusOK, response.StatusCode)
+			// then
+			require.NoError(t, err)
+			require.Equal(t, http.StatusOK, response.StatusCode)
 
-		// when
-		body, err := ioutil.ReadAll(response.Body)
+			// when
+			body, err := ioutil.ReadAll(response.Body)
 
-		// then
-		require.NoError(t, err)
+			// then
+			require.NoError(t, err)
 
-		var apiSpec struct{}
-		err = yaml.Unmarshal(body, &apiSpec)
-		require.NoError(t, err)
-	})
+			var apiSpec struct{}
+			err = yaml.Unmarshal(body, &apiSpec)
+			require.NoError(t, err)
+		})
 
-	t.Run("should receive 301 when accessing base path", func(t *testing.T) {
-		// given
-		hc.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-			require.Equal(t, apiSpecPath, req.URL.Path)
-			return http.ErrUseLastResponse
-		}
+		t.Run("should receive 301 when accessing base path", func(t *testing.T) {
+			// given
+			hc.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+				require.Equal(t, apiSpecPath, req.URL.Path)
+				return http.ErrUseLastResponse
+			}
 
-		// when
-		response, err := hc.Get(config.ExternalAPIUrl + "/v1")
+			// when
+			response, err := hc.Get(config.ExternalAPIUrl + "/v1")
 
-		// then
-		require.NoError(t, err)
-		require.Equal(t, http.StatusMovedPermanently, response.StatusCode)
-	})
+			// then
+			require.NoError(t, err)
+			require.Equal(t, http.StatusMovedPermanently, response.StatusCode)
+		})
+	}
 }
