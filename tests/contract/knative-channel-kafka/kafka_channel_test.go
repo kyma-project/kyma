4d20063e75269ff7058f8b5016dacb5954bb9a1f
Nils Schmidt <github@nilstschmidt.de>
2020-02-05 11:44:55 +0100

    KafkaChannelAcceptanceTest: Send CE to kafka channel  (#7078)
    * Send cloudevent to kafka channel
    
    * Use logging library for info logging
    
    * Log retries when sending to channel
    
    * t.Fatalf => log.Printf;t.FailNow
    
    * Use select to stop sending events in case of signal
    
    * Note about why using log library
    
    * Polish test
    
    * Adapt to test style
    
    * Polish test description
    
    * Update gopkg lock
    
    * RC: Use retry.Unrecoverable
    
    * RC: extract test data to constants & fixed delay & rename test
    
    * Rename test name as well
    
    * Goimports & let ce client generate uuid

--- tests/contract/knative-channel-kafka/kafka_channel_test.go
+++ tests/contract/knative-channel-kafka/kafka_channel_test.go
@@ -1,23 +1,42 @@
 package knative_eventing_kafka_channel
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"testing"
 	"time"
 
+	"knative.dev/pkg/apis"
+
 	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+
 	// allow client authentication against GKE clusters
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 
 	"github.com/avast/retry-go"
+	cloudevents "github.com/cloudevents/sdk-go"
+	"github.com/cloudevents/sdk-go/pkg/cloudevents/client"
 	kafkav1alpha1 "github.com/kyma-incubator/knative-kafka/components/controller/pkg/apis/knativekafka/v1alpha1"
 	kafkaclientset "github.com/kyma-incubator/knative-kafka/components/controller/pkg/client/clientset/versioned/typed/knativekafka/v1alpha1"
+)
+
+const (
+	// cloudevent test data and metadata
+	ceData        = "hello kafka"
+	ceEventType   = "com.example.testing"
+	ceEventSource = "kafka channel test"
+
+	// test meta for the Kafka channel
+	kafkaName      = "test-kafka-channel"
+	kafkaNamespace = "knative-eventing"
 )
 
 var (
@@ -25,46 +44,124 @@ var (
 	interruptSignals = []os.Signal{syscall.SIGTERM, syscall.SIGINT}
 )
 
-// TestKnativeEventingKafkaChannelAcceptance creates a test Kafka channel and asserts its status to be ready.
-func TestKnativeEventingKafkaChannelAcceptance(t *testing.T) {
-	// test meta for the Kafka channel
-	name := "test-kafka-channel"
-	namespace := "knative-eventing"
+// TestKnativeEventingKafkaChannel performs the following steps:
+// * creates a test Kafka channel
+// * asserts its status to be ready
+// * sends a CE to the channel and asserts status code is 2xx
+// NOTE: log library is used here instead of using testing.T for logging, because log flushes more often
+// and enables live logs on the test.
+func TestKnativeEventingKafkaChannel(t *testing.T) {
 
 	// load cluster config
 	config := loadConfigOrDie(t)
 
 	// prepare a Kafka client
-	kafkaClient := kafkaclientset.NewForConfigOrDie(config).KafkaChannels(namespace)
+	kafkaClient := kafkaclientset.NewForConfigOrDie(config).KafkaChannels(kafkaNamespace)
 
 	// cleanup test resources gracefully when an interrupt signal is received
-	interrupted := cleanupOnInterrupt(t, interruptSignals, func() { deleteChannel(t, kafkaClient, name) })
+	interrupted := cleanupOnInterrupt(t, interruptSignals, func() { deleteChannel(t, kafkaClient, kafkaName) })
 	defer close(interrupted)
 
 	// cleanup the Kafka channel when the test is finished
-	defer deleteChannel(t, kafkaClient, name)
+	defer deleteChannel(t, kafkaClient, kafkaName)
 
 	// delete the Kafka channel if existed before to make sure that
 	// the new channel to be created has the correct structure and data
-	if err := deleteChannelIfExistsAndWaitUntilDeleted(t, interrupted, kafkaClient, name, 5*time.Second, 10, retry.FixedDelay); err != nil {
-		t.Fatalf("test failed with error: %s", err)
+	if err := deleteChannelIfExistsAndWaitUntilDeleted(t, interrupted, kafkaClient, kafkaName, 5*time.Second, 10, retry.FixedDelay); err != nil {
+		log.Printf("test failed with error: %s", err)
+		t.FailNow()
 	}
 
 	// create a Kafka channel
-	if _, err := kafkaClient.Create(newKafkaChannel(name, namespace)); err != nil {
-		t.Fatalf("cannot create a Kafka channel: %s: error: %v", name, err)
+	if _, err := kafkaClient.Create(newKafkaChannel(kafkaName, kafkaNamespace)); err != nil {
+		log.Printf("cannot create a Kafka channel: %s: error: %v", kafkaName, err)
+		t.FailNow()
 	} else {
-		t.Logf("created Kafka channel: %s", name)
+		log.Printf("created Kafka channel: %s", kafkaName)
 	}
 
 	// assert the Kafka channel status to be ready
-	if err := checkChannelReadyWithRetry(t, interrupted, kafkaClient, name, 5*time.Second, 10, retry.FixedDelay); err != nil {
-		t.Fatalf("test failed with error: %s", err)
-	} else {
-		t.Logf("test finished successfully")
+	readyKafkaChannel, err := checkChannelReadyWithRetry(t, interrupted, kafkaClient, kafkaName, 5*time.Second, 10, retry.FixedDelay)
+	if err != nil {
+		log.Printf("test failed with error: %s", err)
+		t.FailNow()
 	}
 
-	// TODO(marcobebway) extend the test to assert event delivery also works using the Kafka channel https://github.com/kyma-project/kyma/issues/7015.
+	// send CE to Kafka channel
+	target := readyKafkaChannel.Status.Address.URL
+	ceClient := createCloudEventsClient(t, target)
+	event := createCloudEvent(t)
+	sendEventUntilReceived(t, interrupted, event, target, ceClient)
+
+	log.Printf("test finished successfully")
+}
+
+// sendEventUntilReceived sends a given CE to the target and retry until the event was successfully received (2xx status code).
+func sendEventUntilReceived(t *testing.T, interrupted chan bool, event cloudevents.Event, target *apis.URL, ceClient client.Client) {
+	err := retry.Do(func() error {
+		select {
+		case <-interrupted:
+			log.Printf("cannot continue, test was interrupted")
+			return retry.Unrecoverable(fmt.Errorf("cannot continue, test was interrupted"))
+		default:
+			// send an CE event to Kafka channel
+			log.Printf("sending cloudevent to Kafka channel: %q", target)
+			rctx, _, err := ceClient.Send(context.Background(), event)
+			if err != nil {
+				return err
+			}
+			rtctx := cloudevents.HTTPTransportContextFrom(rctx)
+			log.Printf("received status code: %d", rtctx.StatusCode)
+			if !is2XXStatusCode(rtctx.StatusCode) {
+				return fmt.Errorf("received non 2xx status code: %d", rtctx.StatusCode)
+			}
+			return nil
+		}
+	}, retry.DelayType(retry.FixedDelay),
+		retry.Attempts(24), retry.Delay(time.Second*5), // 120=24*5 seconds
+		retry.OnRetry(func(n uint, err error) { log.Printf("[%v] try failed: %s", n, err) }),
+	)
+
+	if err != nil {
+		log.Printf("could not send cloudevent %+v to %q: %v", event, target, err)
+		t.FailNow()
+	}
+}
+
+// is2XXStatusCode checks whether status code is a 2XX status code.
+func is2XXStatusCode(statusCode int) bool {
+	return statusCode >= http.StatusOK && statusCode < http.StatusMultipleChoices
+}
+
+// createCloudEvent creates a simple CE.
+func createCloudEvent(t *testing.T) cloudevents.Event {
+	event := cloudevents.NewEvent()
+	if err := event.SetData(ceData); err != nil {
+		log.Printf("could not set cloudevent data %q: %v", ceData, err)
+		t.FailNow()
+	}
+	event.SetType(ceEventType)
+	event.SetSource(ceEventSource)
+	return event
+}
+
+// createCloudEventsClient creates a CE client configured to send to given target.
+func createCloudEventsClient(t *testing.T, target *apis.URL) client.Client {
+	transport, err := cloudevents.NewHTTPTransport(
+		cloudevents.WithTarget(target.String()),
+	)
+	if err != nil {
+		log.Printf("could not create cloudevents http transport: %v", err)
+		t.FailNow()
+	}
+	ceClient, err := cloudevents.NewClient(transport,
+		cloudevents.WithUUIDs(),
+	)
+	if err != nil {
+		log.Printf("could not create cloudevents client: %v", err)
+		t.FailNow()
+	}
+	return ceClient
 }
 
 // loadConfigOrDie loads the cluster config or exits the test with failure if it did not succeed.
@@ -74,14 +171,16 @@ func loadConfigOrDie(t *testing.T) *rest.Config {
 	if _, err := os.Stat(clientcmd.RecommendedHomeFile); os.IsNotExist(err) {
 		config, err := rest.InClusterConfig()
 		if err != nil {
-			t.Fatalf("cannot create in-cluster config: %v", err)
+			log.Printf("cannot create in-cluster config: %v", err)
+			t.FailNow()
 		}
 		return config
 	}
 
 	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
 	if err != nil {
-		t.Fatalf("cannot read config: %s", err)
+		log.Printf("cannot read config: %s", err)
+		t.FailNow()
 	}
 	return config
 }
@@ -103,15 +202,15 @@ func deleteChannelIfExistsAndWaitUntilDeleted(t *testing.T, interrupted chan boo
 	t.Helper()
 
 	if _, err := kafkaClient.Get(name, v1.GetOptions{}); err == nil {
-		t.Logf("delete the old Kafka channel: %s", name)
+		log.Printf("delete the old Kafka channel: %s", name)
 		deleteChannel(t, kafkaClient, name)
 
 		// wait for the old Kafka channel to be deleted
 		return retry.Do(func() error {
 			select {
 			case <-interrupted:
-				t.Fatal("cannot continue, test was interrupted")
-				return nil
+				log.Printf("cannot continue, test was interrupted")
+				return retry.Unrecoverable(fmt.Errorf("cannot continue, test was interrupted"))
 			default:
 				if _, err := kafkaClient.Get(name, v1.GetOptions{}); err == nil {
 					return fmt.Errorf("the old Kafka channel is not deleted yet")
@@ -122,7 +221,7 @@ func deleteChannelIfExistsAndWaitUntilDeleted(t *testing.T, interrupted chan boo
 			retry.Delay(duration),
 			retry.Attempts(attempts),
 			retry.DelayType(delayType),
-			retry.OnRetry(func(n uint, err error) { t.Logf("[%v] try failed: %s", n, err) }),
+			retry.OnRetry(func(n uint, err error) { log.Printf("[%v] try failed: %s", n, err) }),
 		)
 	}
 
@@ -132,20 +231,22 @@ func deleteChannelIfExistsAndWaitUntilDeleted(t *testing.T, interrupted chan boo
 // checkChannelReadyWithRetry gets the Kafka channel given its name and checks its status to be ready in a retry loop.
 func checkChannelReadyWithRetry(t *testing.T, interrupted chan bool,
 	kafkaClient kafkaclientset.KafkaChannelInterface, name string,
-	duration time.Duration, attempts uint, delayType retry.DelayTypeFunc) error {
+	duration time.Duration, attempts uint, delayType retry.DelayTypeFunc) (*kafkav1alpha1.KafkaChannel, error) {
 	t.Helper()
+	var kafkaChannel *kafkav1alpha1.KafkaChannel
 
-	return retry.Do(func() error {
+	err := retry.Do(func() error {
+		var err error
 		select {
 		case <-interrupted:
-			t.Fatal("cannot continue, test was interrupted")
-			return nil
+			log.Printf("cannot continue, test was interrupted")
+			return retry.Unrecoverable(fmt.Errorf("cannot continue, test was interrupted"))
 		default:
-			kafkaChannel, err := kafkaClient.Get(name, v1.GetOptions{})
+			kafkaChannel, err = kafkaClient.Get(name, v1.GetOptions{})
 			if err != nil {
 				return err
 			}
-			t.Logf("found Kafka channel: %s with ready status: %v", name, kafkaChannel.Status.IsReady())
+			log.Printf("found Kafka channel: %s with ready status: %v", name, kafkaChannel.Status.IsReady())
 			if !kafkaChannel.Status.IsReady() {
 				return fmt.Errorf("the Kafka channel is not ready")
 			}
@@ -155,8 +256,10 @@ func checkChannelReadyWithRetry(t *testing.T, interrupted chan bool,
 		retry.Delay(duration),
 		retry.Attempts(attempts),
 		retry.DelayType(delayType),
-		retry.OnRetry(func(n uint, err error) { t.Logf("[%v] try failed: %s", n, err) }),
+		retry.OnRetry(func(n uint, err error) { log.Printf("[%v] try failed: %s", n, err) }),
 	)
+
+	return kafkaChannel, err
 }
 
 // deleteChannel deletes the Kafka channel given its name if it was not already deleted.
@@ -167,11 +270,12 @@ func deleteChannel(t *testing.T, kafkaClient kafkaclientset.KafkaChannelInterfac
 	switch {
 	case errors.IsGone(err):
 	case errors.IsNotFound(err):
-		t.Logf("tried to delete Kafka channel: %s but it was already deleted", name)
+		log.Printf("tried to delete Kafka channel: %s but it was already deleted", name)
 	case err != nil:
-		t.Fatalf("cannot delete Kafka channel %v, Error: %v", name, err)
+		log.Printf("cannot delete Kafka channel %v, Error: %v", name, err)
+		t.FailNow()
 	default:
-		t.Logf("deleted Kafka channel: %s", name)
+		log.Printf("deleted Kafka channel: %s", name)
 	}
 }
 
@@ -190,9 +294,9 @@ func cleanupOnInterrupt(t *testing.T, interruptSignals []os.Signal, cleanup func
 		<-ch
 		interrupted <- true
 
-		t.Log("interrupt signal received, cleanup started")
+		log.Println("interrupt signal received, cleanup started")
 		cleanup()
-		t.Log("cleanup finished")
+		log.Println("cleanup finished")
 	}()
 
 	return interrupted
