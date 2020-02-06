fe0ecbae2718641767d86b27a773bb5b0aedb392
adamwalach <adam.walach@sap.com>
2020-02-06 14:25:23 +0100

    Workaround for minikube pvc issue

--- installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
+++ installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
@@ -10,6 +10,12 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INSTALLATION_DIR=${CURRENT_DIR}/../../../
 
 sudo ${INSTALLATION_DIR}/cmd/run.sh --vm-driver "none"
+
+echo "Fix Minikube issue - https://github.com/kubernetes/minikube/issues/6523"
+sudo docker pull gcr.io/k8s-minikube/storage-provisioner@sha256:088daa9fcbccf04c3f415d77d5a6360d2803922190b675cb7fc88a9d2d91985a
+sudo docker tag gcr.io/k8s-minikube/storage-provisioner@sha256:088daa9fcbccf04c3f415d77d5a6360d2803922190b675cb7fc88a9d2d91985a gcr.io/k8s-minikube/storage-provisioner:v1.8.1
+sudo kubectl -n kube-system delete pod storage-provisioner
+
 sudo ${INSTALLATION_DIR}/scripts/is-installed.sh --timeout 30m
 sudo ${INSTALLATION_DIR}/scripts/watch-pods.sh
 sudo ${INSTALLATION_DIR}/scripts/testing.sh
