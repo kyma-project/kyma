a28d5b6f0e8017a5faa9f2c3b88ef2ac0540175b
marcobebway <marco.bebway@sap.com>
2020-02-13 13:41:40 +0100

    Resolve the todos

--- components/event-service/cmd/eventservice/eventgateway.go
+++ components/event-service/cmd/eventservice/eventgateway.go
@@ -9,17 +9,13 @@ import (
 	"syscall"
 	"time"
 
-	"knative.dev/eventing/pkg/client/clientset/versioned"
-	"knative.dev/eventing/pkg/client/informers/externalversions"
-	"knative.dev/eventing/pkg/client/listers/eventing/v1alpha1"
-
 	"github.com/kyma-project/kyma/components/event-service/internal/events/mesh"
 	"github.com/kyma-project/kyma/components/event-service/internal/events/subscribed"
 	"github.com/kyma-project/kyma/components/event-service/internal/externalapi"
 	"github.com/kyma-project/kyma/components/event-service/internal/httptools"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/rest"
-	"knative.dev/pkg/signals"
+	"knative.dev/eventing/pkg/client/clientset/versioned"
 )
 
 const (
@@ -118,25 +114,4 @@ func getKnativeClient() (versioned.Interface, error) {
 	}
 
 	return knEventingClient, nil
-}
-
-// TODO(marcobebway) remove this
-func initKnativeTriggerLister() (v1alpha1.TriggerLister, error) {
-	k8sConfig, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	knEventingClient, err := versioned.NewForConfig(k8sConfig)
-	if err != nil {
-		log.Infof("error in creating knative client: %+v", err)
-		return nil, err
-	}
-
-	informerFactory := externalversions.NewSharedInformerFactory(knEventingClient, 0)
-	ctx := signals.NewContext()
-	go informerFactory.Start(ctx.Done())
-	informerFactory.WaitForCacheSync(ctx.Done())
-
-	return informerFactory.Eventing().V1alpha1().Triggers().Lister(), nil
 }
