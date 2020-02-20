605327d9cea4b26524309d1f09e726fd2531df34
kubadz <jakub.dziechciewicz@sap.com>
2020-02-20 11:55:50 +0100

    add temporary log for debugging

--- components/kyma-operator/cmd/operator/main.go
+++ components/kyma-operator/cmd/operator/main.go
@@ -34,6 +34,7 @@ var gitCommitHash string
 func main() {
 
 	log.Println("Starting operator...")
+	log.Println("test test test") // TODO: remove
 
 	if gitCommitHash != "" {
 		log.Println("Git commit hash:", gitCommitHash)
