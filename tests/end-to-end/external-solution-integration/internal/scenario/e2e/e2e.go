108175e231afa2df421420ed4b57a3976b700a2a
Damian Badura <damian.badura@sap.com>
2020-02-26 08:43:10 +0100

    fix e2e test

--- tests/end-to-end/external-solution-integration/internal/scenario/e2e/e2e.go
+++ tests/end-to-end/external-solution-integration/internal/scenario/e2e/e2e.go
@@ -1,7 +1,9 @@
 package e2e
 
 import (
+	sourcesclientv1alpha1 "github.com/kyma-project/kyma/components/event-sources/client/generated/clientset/internalclientset/typed/sources/v1alpha1"
 	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/pkg/helpers"
+	eventing "knative.dev/eventing/pkg/client/clientset/versioned"
 
 	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/internal"
 
@@ -24,6 +26,10 @@ func (s *E2EScenario) Steps(config *rest.Config) ([]step.Step, error) {
 		internal.NewHTTPClient(s.SkipSSLVerify),
 		log.New(),
 	)
+
+	knativeEventingClientSet := eventing.NewForConfigOrDie(config)
+	httpSourceClientset := sourcesclientv1alpha1.NewForConfigOrDie(config)
+
 	testService := testkit.NewTestService(
 		internal.NewHTTPClient(s.SkipSSLVerify),
 		clients.CoreClientset.AppsV1().Deployments(s.TestID),
@@ -39,8 +45,9 @@ func (s *E2EScenario) Steps(config *rest.Config) ([]step.Step, error) {
 	return []step.Step{
 		step.Parallel(
 			testsuite.NewCreateNamespace(s.TestID, clients.CoreClientset.CoreV1().Namespaces()),
-			testsuite.NewCreateApplication(s.TestID, s.TestID, false, s.ApplicationTenant, s.ApplicationGroup, clients.AppOperatorClientset.ApplicationconnectorV1alpha1().Applications(), nil),
-		),
+			testsuite.NewCreateApplication(s.TestID, s.TestID, false, s.ApplicationTenant, s.ApplicationGroup,
+				clients.AppOperatorClientset.ApplicationconnectorV1alpha1().Applications(),
+				httpSourceClientset.HTTPSources(helpers.KymaIntegrationNamespace))),
 		step.Parallel(
 			testsuite.NewCreateMapping(s.TestID, clients.AppBrokerClientset.ApplicationconnectorV1alpha1().ApplicationMappings(s.TestID)),
 			testsuite.NewDeployLambda(s.TestID, helpers.LambdaPayload, helpers.LambdaPort, clients.KubelessClientset.KubelessV1beta1().Functions(s.TestID), clients.Pods),
@@ -53,9 +60,12 @@ func (s *E2EScenario) Steps(config *rest.Config) ([]step.Step, error) {
 			clients.ServiceCatalogClientset.ServicecatalogV1beta1().ServiceClasses(s.TestID),
 		),
 		testsuite.NewCreateServiceBinding(s.TestID, s.TestID, clients.ServiceCatalogClientset.ServicecatalogV1beta1().ServiceBindings(s.TestID)),
-		testsuite.NewCreateServiceBindingUsage(s.TestID, s.TestID, s.TestID, clients.ServiceBindingUsageClientset.ServicecatalogV1alpha1().ServiceBindingUsages(s.TestID), nil, nil),
-		testsuite.NewCreateSubscription(s.TestID, s.TestID, lambdaEndpoint, clients.EventingClientset.EventingV1alpha1().Subscriptions(s.TestID)),
-		testsuite.NewSendEvent(s.TestID, helpers.LambdaPayload, state),
+		testsuite.NewCreateServiceBindingUsage(s.TestID, s.TestID, s.TestID,
+			clients.ServiceBindingUsageClientset.ServicecatalogV1alpha1().ServiceBindingUsages(s.TestID),
+			knativeEventingClientSet.EventingV1alpha1().Brokers(s.TestID),
+			knativeEventingClientSet.MessagingV1alpha1().Subscriptions(helpers.KymaIntegrationNamespace)),
+		testsuite.NewCreateKnativeTrigger(s.TestID, helpers.DefaultBrokerName, lambdaEndpoint, knativeEventingClientSet.EventingV1alpha1().Triggers(s.TestID)),
+		testsuite.NewSendEventToMesh(s.TestID, helpers.LambdaPayload, state),
 		testsuite.NewCheckCounterPod(testService),
 	}, nil
 }
