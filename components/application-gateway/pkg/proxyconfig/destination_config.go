e866e596a8f3ceef915c779eacead22186de0c2c
Szymon Gibała <szymon.gibala@sap.com>
2020-02-27 13:25:03 +0100

    Extend Gateway to work in namespace only mode (#7246)

---
+++ components/application-gateway/pkg/proxyconfig/destination_config.go
@@ -0,0 +1,96 @@
+package proxyconfig
+
+import (
+	"github.com/kyma-project/kyma/components/application-gateway/pkg/apperrors"
+	"github.com/kyma-project/kyma/components/application-gateway/pkg/authorization"
+)
+
+//go:generate mockery -name=TargetConfigProvider
+
+// TargetConfigProvider allows to fetch ProxyDestinationConfig for specific service
+type TargetConfigProvider interface {
+	GetDestinationConfig(secretName, apiName string) (ProxyDestinationConfig, apperrors.AppError)
+}
+
+// AuthType determines the secret structure
+type AuthType string
+
+const (
+	Undefined   AuthType = ""
+	NoAuth      AuthType = "noauth"
+	Oauth       AuthType = "oauth"
+	Basic       AuthType = "basicauth"
+	Certificate AuthType = "certificate"
+)
+
+// ProxyDestinationConfig is Proxy configuration for specific target
+type ProxyDestinationConfig struct {
+	TargetURL     string        `json:"targetUrl"`
+	Configuration Configuration `json:"credentials"`
+}
+
+type Configuration struct {
+	RequestParameters *authorization.RequestParameters `json:"requestParameters"`
+	CSRFConfig        *CSRFConfig                      `json:"csrfConfig"`
+	Credentials       Credentials                      `json:"credentials"`
+}
+
+type CSRFConfig struct {
+	TokenURL string `json:"tokenUrl"`
+}
+
+type Credentials interface {
+	ToCredentials() *authorization.Credentials
+}
+
+type NoAuthConfig struct{}
+
+func (oc NoAuthConfig) ToCredentials() *authorization.Credentials {
+	return nil
+}
+
+type OauthConfig struct {
+	ClientId          string                          `json:"clientId"`
+	ClientSecret      string                          `json:"clientSecret"`
+	TokenURL          string                          `json:"tokenUrl"`
+	RequestParameters authorization.RequestParameters `json:"requestParameters,omitempty"`
+}
+
+func (oc OauthConfig) ToCredentials() *authorization.Credentials {
+	return &authorization.Credentials{
+		OAuth: &authorization.OAuth{
+			URL:               oc.TokenURL,
+			ClientID:          oc.ClientId,
+			ClientSecret:      oc.ClientSecret,
+			RequestParameters: &oc.RequestParameters,
+		},
+	}
+}
+
+type BasicAuthConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (bc BasicAuthConfig) ToCredentials() *authorization.Credentials {
+	return &authorization.Credentials{
+		BasicAuth: &authorization.BasicAuth{
+			Username: bc.Username,
+			Password: bc.Password,
+		},
+	}
+}
+
+type CertificateConfig struct {
+	Certificate []byte `json:"certificate"`
+	PrivateKey  []byte `json:"privateKey"`
+}
+
+func (cc CertificateConfig) ToCredentials() *authorization.Credentials {
+	return &authorization.Credentials{
+		CertificateGen: &authorization.CertificateGen{
+			PrivateKey:  cc.PrivateKey,
+			Certificate: cc.Certificate,
+		},
+	}
+}
