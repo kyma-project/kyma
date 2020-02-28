5cd70806ff0ebd51d5676b1884d61449981e5d09
Piotr Mścichowski <piotr.mscichowski@sap.com>
2020-02-28 12:06:36 +0100

    Fix gardener tls rotation (#7238)

--- components/xip-patch/xip-patch.sh
+++ components/xip-patch/xip-patch.sh
@@ -13,11 +13,13 @@ set -o errexit
 # INGRESS_DOMAIN                #
 # INGRESS_TLS_CERT              #
 # INGRESS_TLS_KEY               #
+# TLS_SECRET_NAME               #
 # # # # # # # # # # # # # # # # #
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $CURRENT_DIR/utils.sh
 
+TLS_SECRET_NAME="${TLS_SECRET_NAME:-kyma-gateway-certs}"
 generateXipDomain() {
 
     if [ -z "${EXTERNAL_PUBLIC_IP}" ]; then
@@ -56,12 +58,20 @@ generateCerts() {
 }
 
 createOverridesConfigMap() {
-    if [ -z "$(kubectl get configmap -n kyma-installer net-global-overrides --ignore-not-found)" ]; then
-        kubectl create configmap net-global-overrides \
-            --from-literal global.ingress.domainName="$INGRESS_DOMAIN" \
-            --from-literal global.ingress.tlsCrt="$INGRESS_TLS_CERT" \
-            --from-literal global.ingress.tlsKey="$INGRESS_TLS_KEY" \
-            -n kyma-installer
+    COMMON_PARAMS=$(echo --from-literal global.ingress.domainName="$INGRESS_DOMAIN" \
+                         --from-literal global.ingress.tlsCrt="$INGRESS_TLS_CERT" \
+                         --from-literal global.ingress.tlsKey="$INGRESS_TLS_KEY" \
+                         --from-literal global.environment.gardener="$GARDENER_ENVIRONMENT") \
+
+    if [[ "$GARDENER_ENVIRONMENT" == true ]]; then
+        kubectl create configmap net-global-overrides ${COMMON_PARAMS} \
+        --from-literal global.domainName="$INGRESS_DOMAIN" \
+        --from-literal global.tlsCrt="$INGRESS_TLS_CERT" \
+        --from-literal global.tlsKey="$INGRESS_TLS_KEY" \
+        -n kyma-installer -o yaml --dry-run | kubectl apply -f -
+    else
+        kubectl create configmap net-global-overrides ${COMMON_PARAMS} \
+        -n kyma-installer -o yaml --dry-run | kubectl apply -f -
     fi
     kubectl label configmap net-global-overrides --overwrite installer=overrides -n kyma-installer
     kubectl label configmap net-global-overrides --overwrite kyma-project.io/installation="" -n kyma-installer
@@ -87,13 +97,10 @@ EOF
 }
 
 requestGardenerCerts() {
-    local subdomain
     local shoot_domain
-    subdomain="kyma"
 
     echo "Getting Shoot Domain"
-    shoot_domain="$(kubectl -n kube-system get configmap shoot-info -o jsonpath='{.data.domain}')"
-    DOMAIN="${subdomain}.${shoot_domain}"
+    DOMAIN="$(kubectl -n kube-system get configmap shoot-info -o jsonpath='{.data.domain}')"
 
     echo "Requesting certificate for domain ${DOMAIN}"
 cat <<EOF | kubectl apply -f -
@@ -101,17 +108,18 @@ cat <<EOF | kubectl apply -f -
 apiVersion: cert.gardener.cloud/v1alpha1
 kind: Certificate
 metadata:
-  name: kyma-cert
-  namespace: kyma-installer
+  name: kyma-tls-cert
+  namespace: istio-system
 spec:
   commonName: "*.${DOMAIN}"
+  secretName: "$TLS_SECRET_NAME"
 EOF
 
     SECONDS=0
     END_TIME=$((SECONDS+600)) #600 seconds = 10 minutes
     
     while [ ${SECONDS} -lt ${END_TIME} ];do
-        STATUS="$(kubectl get -n kyma-installer certificate.cert.gardener.cloud kyma-cert -o jsonpath='{.status.state}')"
+        STATUS="$(kubectl get -n istio-system certificate.cert.gardener.cloud kyma-tls-cert -o jsonpath='{.status.state}')"
         if [ "${STATUS}" = "Ready" ]; then
             break
         fi
@@ -124,23 +132,25 @@ EOF
         exit 1
     fi
 
-    local secret_name
-    secret_name=$(kubectl get -n kyma-installer certificate kyma-cert -o jsonpath="{.spec.secretRef.name}")
     echo "Getting certificate from secret"
-    TLS_CERT=$(kubectl get -n kyma-installer secret  "${secret_name}" -o jsonpath="{.data['tls\.crt']}" | sed 's/ /\\ /g' | tr -d '\n')
-    TLS_KEY=$(kubectl get -n kyma-installer secret  "${secret_name}" -o jsonpath="{.data['tls\.key']}" | sed 's/ /\\ /g' | tr -d '\n')
+    TLS_CERT=$(kubectl get -n istio-system secret  "${TLS_SECRET_NAME}" -o jsonpath="{.data['tls\.crt']}" | sed 's/ /\\ /g' | tr -d '\n')
+    TLS_KEY=$(kubectl get -n istio-system secret  "${TLS_SECRET_NAME}" -o jsonpath="{.data['tls\.key']}" | sed 's/ /\\ /g' | tr -d '\n')
 
     echo "Annotating Istio Ingress Gateway with Gardener DNS"
     kubectl -n istio-system annotate service istio-ingressgateway dns.gardener.cloud/class='garden' dns.gardener.cloud/dnsnames='*.'"${DOMAIN}"'' --overwrite
 }
 
 echo "Checking if running on Gardener"
+
+GARDENER_ENVIRONMENT=false
+
 if [ -n "$(kubectl -n kube-system get configmap shoot-info --ignore-not-found)" ] &&
    [ -z "$(kubectl get configmap -n kyma-installer net-global-overrides --ignore-not-found)" ]; then
   requestGardenerCerts
   INGRESS_DOMAIN=${DOMAIN}
   INGRESS_TLS_CERT=${TLS_CERT}
   INGRESS_TLS_KEY=${TLS_KEY}
+  GARDENER_ENVIRONMENT=true
 fi
 
 INGRESS_TLS_CERT="${INGRESS_TLS_CERT:-$GLOBAL_TLS_CERT}"
