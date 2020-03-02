eea3e62e6ab452a035868852d8d56f424eb991a6
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-03-02 14:22:54 +0100

    Yeah

--- components/console-backend-service/internal/domain/servicecatalog/disabled/resolver.go
+++ components/console-backend-service/internal/domain/servicecatalog/disabled/resolver.go
@@ -330,6 +330,15 @@ func (_m *Resolver) ServiceInstancesQuery(ctx context.Context, namespace string,
 	return r0, r1
 }
 
+// ServicePlanAssetGroupField provides a failing mock function with given fields: ctx, obj
+func (_m *Resolver) ServicePlanAssetGroupField(ctx context.Context, obj *gqlschema.ServicePlan) (*gqlschema.AssetGroup, error) {
+	var r0 *gqlschema.AssetGroup
+	var r1 error
+	r1 = _m.err
+
+	return r0, r1
+}
+
 // ServicePlanClusterAssetGroupField provides a failing mock function with given fields: ctx, obj
 func (_m *Resolver) ServicePlanClusterAssetGroupField(ctx context.Context, obj *gqlschema.ServicePlan) (*gqlschema.ClusterAssetGroup, error) {
 	var r0 *gqlschema.ClusterAssetGroup
