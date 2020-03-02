3824a51acc3d35ef525a9d70cd86b82fce0edc02
parostatkiem <jan.sudczak@sap.com>
2020-03-02 08:50:40 +0100

    Fix errors

--- components/console-backend-service/internal/gqlschema/clusterserviceplan.go
+++ components/console-backend-service/internal/gqlschema/clusterserviceplan.go
@@ -1,11 +1,11 @@
 package gqlschema
 
 type ClusterServicePlan struct {
-	Name                           string `json:"name"`
-	DisplayName                    string `json:"displayName"`
-	ExternalName                   string `json:"externalName"`
-	Description                    string `json:"description"`
-	RelatedClusterServiceClassName string `json:"relatedClusterServiceClassName"`
-	InstanceCreateParameterSchema  JSON   `json:"instanceCreateParameterSchema"`
-	BindingCreateParameterSchema   JSON   `json:"bindingCreateParameterSchema"`
+	Name                           string  `json:"name"`
+	DisplayName                    *string `json:"displayName"`
+	ExternalName                   string  `json:"externalName"`
+	Description                    string  `json:"description"`
+	RelatedClusterServiceClassName string  `json:"relatedClusterServiceClassName"`
+	InstanceCreateParameterSchema  *JSON   `json:"instanceCreateParameterSchema"`
+	BindingCreateParameterSchema   *JSON   `json:"bindingCreateParameterSchema"`
 }
