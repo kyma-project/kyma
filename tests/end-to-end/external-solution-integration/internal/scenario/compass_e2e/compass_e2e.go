d4716e580d8273af165fe36bf3c32e5e93b5c3d3
Marco Bebway <marco.bebway@sap.com>
2020-03-04 11:49:57 +0100

    Add more stability to the compass E2E test (#7379)

--- tests/end-to-end/external-solution-integration/internal/scenario/compass_e2e/compass_e2e.go
+++ tests/end-to-end/external-solution-integration/internal/scenario/compass_e2e/compass_e2e.go
@@ -3,17 +3,17 @@ package compass_e2e
 import (
 	"fmt"
 
-	serviceBindingUsageClient "github.com/kyma-project/kyma/components/service-binding-usage-controller/pkg/client/clientset/versioned"
+	"k8s.io/client-go/rest"
 
-	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/pkg/helpers"
 	eventing "knative.dev/eventing/pkg/client/clientset/versioned"
 
+	sourcesclientv1alpha1 "github.com/kyma-project/kyma/components/event-sources/client/generated/clientset/internalclientset/typed/sources/v1alpha1"
+	serviceBindingUsageClient "github.com/kyma-project/kyma/components/service-binding-usage-controller/pkg/client/clientset/versioned"
 	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/internal"
-
+	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/pkg/helpers"
 	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/pkg/step"
 	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/pkg/testkit"
 	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/pkg/testsuite"
-	"k8s.io/client-go/rest"
 )
 
 // Steps return scenario steps
@@ -25,8 +25,9 @@ func (s *CompassE2EScenario) Steps(config *rest.Config) ([]step.Step, error) {
 
 	kymaClients := testkit.InitKymaClients(config, s.testID)
 	compassClients := testkit.InitCompassClients(kymaClients, state, s.domain, s.skipSSLVerify)
-	knativeEventingClientSet := eventing.NewForConfigOrDie(config)
+	knativeEventingClientset := eventing.NewForConfigOrDie(config)
 	serviceBindingUsageClientset := serviceBindingUsageClient.NewForConfigOrDie(config)
+	httpSourceClientset := sourcesclientv1alpha1.NewForConfigOrDie(config)
 
 	testService := testkit.NewTestService(
 		internal.NewHTTPClient(s.skipSSLVerify),
@@ -50,6 +51,7 @@ func (s *CompassE2EScenario) Steps(config *rest.Config) ([]step.Step, error) {
 				testService.GetInClusterTestServiceURL(),
 				kymaClients.AppOperatorClientset.ApplicationconnectorV1alpha1().Applications(),
 				compassClients.DirectorClient,
+				httpSourceClientset.HTTPSources(helpers.KymaIntegrationNamespace),
 				state),
 		),
 		step.Parallel(
@@ -63,8 +65,8 @@ func (s *CompassE2EScenario) Steps(config *rest.Config) ([]step.Step, error) {
 		testsuite.NewCreateServiceBinding(s.testID, apiServiceInstanceName, kymaClients.ServiceCatalogClientset.ServicecatalogV1beta1().ServiceBindings(s.testID)),
 		testsuite.NewCreateServiceBindingUsage(s.testID, s.testID, s.testID,
 			serviceBindingUsageClientset.ServicecatalogV1alpha1().ServiceBindingUsages(s.testID),
-			knativeEventingClientSet.EventingV1alpha1().Brokers(s.testID), knativeEventingClientSet.MessagingV1alpha1().Subscriptions(helpers.KymaIntegrationNamespace)),
-		testsuite.NewCreateKnativeTrigger(s.testID, helpers.DefaultBrokerName, lambdaEndpoint, knativeEventingClientSet.EventingV1alpha1().Triggers(s.testID)),
+			knativeEventingClientset.EventingV1alpha1().Brokers(s.testID), knativeEventingClientset.MessagingV1alpha1().Subscriptions(helpers.KymaIntegrationNamespace)),
+		testsuite.NewCreateKnativeTrigger(s.testID, helpers.DefaultBrokerName, lambdaEndpoint, knativeEventingClientset.EventingV1alpha1().Triggers(s.testID)),
 		testsuite.NewSendEvent(s.testID, helpers.LambdaPayload, state),
 		testsuite.NewCheckCounterPod(testService),
 	}, nil
