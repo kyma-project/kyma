344b90b4fc1c94664829e69e1dd158403a30ac83
parostatkiem <jan.sudczak@sap.com>
2020-03-05 08:45:01 +0100

    Use pretty print of types

--- components/console-backend-service/internal/domain/servicecatalog/clusterserviceplan_resolver.go
+++ components/console-backend-service/internal/domain/servicecatalog/clusterserviceplan_resolver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/golang/glog"
+	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/rafter/pretty"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/shared"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlerror"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlschema"
@@ -21,20 +22,20 @@ func newClusterServicePlanResolver(sc *clusterServicePlanService, r shared.Rafte
 
 func (r *clusterServicePlanResolver) ClusterServicePlanClusterAssetGroupField(ctx context.Context, obj *gqlschema.ClusterServicePlan) (*gqlschema.ClusterAssetGroup, error) {
 	if obj == nil {
-		glog.Error(fmt.Errorf("while getting clusterAssetGroup field obj is empty"))
+		glog.Error(fmt.Errorf("while getting %s field obj is empty", pretty.ClusterAssetGroup))
 		return nil, gqlerror.NewInternal()
 	}
 
 	clusterAssetGroup, err := r.rafter.ClusterAssetGroup().Find(obj.Name)
 	if err != nil {
-		glog.Errorf("Couldn't find clusterAssetGroup with name %s", obj.Name)
+		glog.Errorf("Couldn't find %s with name %s", pretty.ClusterAssetGroup, obj.Name)
 		return nil, nil
 	}
 
 	convertedAssetGroup, err := r.rafter.ClusterAssetGroupConverter().ToGQL(clusterAssetGroup)
 
 	if err != nil {
-		glog.Errorf("Couldn't convert clusterAssetGroup with name %s to GQL", obj.Name)
+		glog.Errorf("Couldn't convert %s with name %s to GQL", pretty.ClusterAssetGroup, obj.Name)
 		return nil, nil
 	}
 	return convertedAssetGroup, nil
--- components/console-backend-service/internal/domain/servicecatalog/serviceplan_resolver.go
+++ components/console-backend-service/internal/domain/servicecatalog/serviceplan_resolver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/golang/glog"
+	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/rafter/pretty"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/shared"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlerror"
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlschema"
@@ -21,20 +22,20 @@ func newServicePlanResolver(sc *servicePlanService, r shared.RafterRetriever) *s
 
 func (r *servicePlanResolver) ServicePlanClusterAssetGroupField(ctx context.Context, obj *gqlschema.ServicePlan) (*gqlschema.ClusterAssetGroup, error) {
 	if obj == nil {
-		glog.Error(fmt.Errorf("while getting clusterAssetGroup field obj is empty"))
+		glog.Error(fmt.Errorf("while getting %s field obj is empty", pretty.ClusterAssetGroup))
 		return nil, gqlerror.NewInternal()
 	}
 
 	assetGroup, err := r.rafter.ClusterAssetGroup().Find(obj.Name)
 	if err != nil {
-		glog.Errorf("Couldn't find clusterAssetGroup with name %s", obj.Name)
+		glog.Errorf("Couldn't find %s with name %s", pretty.ClusterAssetGroup, obj.Name)
 		return nil, nil
 	}
 
 	convertedAssetGroup, err := r.rafter.ClusterAssetGroupConverter().ToGQL(assetGroup)
 
 	if err != nil {
-		glog.Errorf("Couldn't convert clusterAssetGroup with name %s to GQL", obj.Name)
+		glog.Errorf("Couldn't convert %s with name %s to GQL", pretty.ClusterAssetGroup, obj.Name)
 		return nil, nil
 	}
 	return convertedAssetGroup, nil
@@ -42,20 +43,20 @@ func (r *servicePlanResolver) ServicePlanClusterAssetGroupField(ctx context.Cont
 
 func (r *servicePlanResolver) ServicePlanAssetGroupField(ctx context.Context, obj *gqlschema.ServicePlan) (*gqlschema.AssetGroup, error) {
 	if obj == nil {
-		glog.Error(fmt.Errorf("while getting assetGroup field obj is empty"))
+		glog.Error(fmt.Errorf("while getting %s field obj is empty", pretty.AssetGroup))
 		return nil, gqlerror.NewInternal()
 	}
 
 	assetGroup, err := r.rafter.AssetGroup().Find(obj.Name, obj.Namespace)
 	if err != nil {
-		glog.Errorf("Couldn't find assetGroup with name %s", obj.Name)
+		glog.Errorf("Couldn't find %s with name %s", pretty.AssetGroup, obj.Name)
 		return nil, nil
 	}
 
 	convertedAssetGroup, err := r.rafter.AssetGroupConverter().ToGQL(assetGroup)
 
 	if err != nil {
-		glog.Errorf("Couldn't convert assetGroup with name %s to GQL", obj.Name)
+		glog.Errorf("Couldn't convert %s with name %s to GQL", pretty.AssetGroup, obj.Name)
 		return nil, nil
 	}
 	return convertedAssetGroup, nil
