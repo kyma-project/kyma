c36f1741eeed8b634788c7bd75b11d4a123778b5
Karol Furgol <furgolkarol@gmail.com>
2020-03-05 09:43:29 +0100

    bump director image
    bump compass-runtime-agent
    
    bump images
    
    bump ui images
    
    bump ui images

--- tests/compass-runtime-agent/test/testkit/graphql/graphqlizer.go
+++ tests/compass-runtime-agent/test/testkit/graphql/graphqlizer.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"regexp"
+	"strconv"
 	"text/template"
 
 	"github.com/Masterminds/sprig"
@@ -158,14 +159,12 @@ func (g *Graphqlizer) AuthInputToGQL(in *graphql.AuthInput) (string, error) {
 }
 
 func (g *Graphqlizer) LabelsToGQL(in graphql.Labels) (string, error) {
-	return g.genericToGQL(in, `{
-		{{- range $k,$v := . }}
-			{{$k}}: [
-				{{- range $i,$j := $v }}
-					{{- if $i}},{{- end}}"{{$j}}"
-				{{- end }} ]
-		{{- end}}
-	}`)
+	out, err := json.Marshal(in)
+	if err != nil {
+		return "", err
+	}
+
+	return strconv.Quote(string(out)), nil
 }
 
 func (g *Graphqlizer) HTTPHeadersToGQL(in graphql.HttpHeaders) (string, error) {
