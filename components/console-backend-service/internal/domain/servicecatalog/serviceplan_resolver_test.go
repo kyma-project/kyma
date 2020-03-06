d18d6b274e14a996b201a45a04c49ecbde45263f
parostatkiem <jan.sudczak@sap.com>
2020-03-06 11:08:02 +0100

    Add clusterserviceplan_resolver test

--- components/console-backend-service/internal/domain/servicecatalog/serviceplan_resolver_test.go
+++ components/console-backend-service/internal/domain/servicecatalog/serviceplan_resolver_test.go
@@ -14,20 +14,28 @@ import (
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/domain/servicecatalog"
 )
 
-//pobieramy serviceplan z clusterAssetGroup i sprawdZamy, czy ona tam jest
-//pobieramy serviceplan z assetGroup i sprawdZamy, czy ona tam jest
-
-// symulacja błedu raftera
-// symulacja błędu konwersji rafter.assetGroup -> gqlschema.assetGroup
-
 func TestServicePlanResolver_ServicePlanClusterAssetGroupField(t *testing.T) {
 	t.Run("Returns nil if Rafter can't find the ClusterAssetGroup", func(t *testing.T) {
 		servicePlan := givenServicePlan()
 		rafter, cagGetter, _ := givenRafterForClusterAssetGroup()
-
 		resolver := givenServicePlanResolver(rafter)
 
 		cagGetter.On("Find", servicePlan.Name).Return(nil, errors.New("ClusterAssetGroup not found"))
+
+		result, err := resolver.ServicePlanClusterAssetGroupField(context.Background(), servicePlan)
+		require.NoError(t, err)
+		assert.Nil(t, result)
+	})
+
+	t.Run("Returns nil if Rafter can't conver the ClusterAssetGroup to GQL", func(t *testing.T) {
+		servicePlan := givenServicePlan()
+		rafter, cagGetter, cagConverter := givenRafterForClusterAssetGroup()
+		resolver := givenServicePlanResolver(rafter)
+
+		assetGroup := givenClusterAssetGroup(servicePlan.Name)
+
+		cagGetter.On("Find", servicePlan.Name).Return(assetGroup, nil)
+		cagConverter.On("ToGQL", assetGroup).Return(nil, errors.New("Can't convert it to GQL"))
 
 		result, err := resolver.ServicePlanClusterAssetGroupField(context.Background(), servicePlan)
 		require.NoError(t, err)
@@ -50,6 +58,9 @@ func TestServicePlanResolver_ServicePlanClusterAssetGroupField(t *testing.T) {
 		assert.Equal(t, servicePlan.Name, result.Name)
 	})
 
+}
+
+func TestServicePlanResolver_ServicePlanAssetGroupField(t *testing.T) {
 	t.Run("Returns assetGroup when it should", func(t *testing.T) {
 		servicePlan := givenServicePlan()
 		rafter, agGetter, agConverter := givenRafterForAssetGroup()
