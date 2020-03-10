8729ce927c3441358c2c4386f4c0d35e26d30aa9
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2020-03-10 15:15:17 +0100

    Imports

--- components/compass-runtime-agent/internal/kyma/model/util.go
+++ components/compass-runtime-agent/internal/kyma/model/util.go
@@ -16,3 +16,22 @@ func APIExists(id string, application Application) bool {
 
 	return false
 }
+
+func APIExistsInPackage(id string, application Application) bool {
+
+	for _, apiPackage := range application.APIPackages {
+		for _, apiDefinition := range apiPackage.APIDefinitions {
+			if apiDefinition.ID == id {
+				return true
+			}
+		}
+
+		for _, eventAPIDefinition := range apiPackage.EventDefinitions {
+			if eventAPIDefinition.ID == id {
+				return true
+			}
+		}
+	}
+
+	return false
+}
