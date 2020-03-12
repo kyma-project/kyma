60826bbf05beb527b43f465c89f3939726f21cfc
Mateusz Szostok <szostok.mateusz@gmail.com>
2020-03-12 14:12:33 +0100

    Add Director proxy in Runtime Agent (#7504)
    - [x] Add Director proxy in Runtime Agent
    - [x] Simplify`main.go` by adding **ExitOnError**
    - [x] Simply the startup of components (this is already implemented no need to do it by custom logic), see logger service changes.
    - [x] Remove redundant comments in main -`Info`log statement is enough
    - [x] Execute `make fmt-local` and `make imports-local`
    - [x] Change channel from `bool` to `struct` which [occupies zero bytes of storage](https://dave.cheney.net/2014/03/25/the-empty-struct) (there was no need to use the bool channel).
    - [x] Change build pack version to use Go 1.13 version (in 1.12 type *http.Transport does not have Clone method) btw. the Dockerfile already uses 1.13 for building binary so test should also be executed against that version

--- components/compass-runtime-agent/cmd/main.go
+++ components/compass-runtime-agent/cmd/main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
+	"kyma-project.io/compass-runtime-agent/internal/certificates"
+	"kyma-project.io/compass-runtime-agent/internal/compass"
+	"kyma-project.io/compass-runtime-agent/internal/compass/director"
+	"kyma-project.io/compass-runtime-agent/internal/compassconnection"
+	confProvider "kyma-project.io/compass-runtime-agent/internal/config"
+	"kyma-project.io/compass-runtime-agent/internal/graphql"
+	"kyma-project.io/compass-runtime-agent/internal/secrets"
+	apis "kyma-project.io/compass-runtime-agent/pkg/apis/compass/v1alpha1"
+
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/vrischmann/envconfig"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	"kyma-project.io/compass-runtime-agent/internal/certificates"
-	"kyma-project.io/compass-runtime-agent/internal/compass"
-	"kyma-project.io/compass-runtime-agent/internal/compassconnection"
-	confProvider "kyma-project.io/compass-runtime-agent/internal/config"
-	"kyma-project.io/compass-runtime-agent/internal/graphql"
-	"kyma-project.io/compass-runtime-agent/internal/metrics"
-	"kyma-project.io/compass-runtime-agent/internal/secrets"
-	apis "kyma-project.io/compass-runtime-agent/pkg/apis/compass/v1alpha1"
-	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
-	"sync"
 )
 
 func main() {
@@ -25,40 +24,28 @@ func main() {
 
 	var options Config
 	err := envconfig.InitWithPrefix(&options, "APP")
-	if err != nil {
-		log.Error("Failed to process environment variables")
-	}
+	exitOnError(err, "Failed to process environment variables")
+
 	log.Infof("Env config: %s", options.String())
 
 	// Get a config to talk to the apiserver
 	log.Info("Setting up client for manager")
 	cfg, err := config.GetConfig()
-	if err != nil {
-		log.Error(err, "unable to set up client config")
-		os.Exit(1)
-	}
+	exitOnError(err, "Failed to set up client config")
 
 	log.Info("Setting up manager")
 	mgr, err := manager.New(cfg, manager.Options{SyncPeriod: &options.ControllerSyncPeriod})
-	if err != nil {
-		log.Error(err, "unable to set up overall controller manager")
-		os.Exit(1)
-	}
+	exitOnError(err, "Failed to set up overall controller manager")
 
 	// Setup Scheme for all resources
 	log.Info("Setting up scheme")
-	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		log.Error(err, "Unable add APIs to scheme")
-		os.Exit(1)
-	}
+	err = apis.AddToScheme(mgr.GetScheme())
+	exitOnError(err, "Failed to add APIs to scheme")
 
 	log.Info("Registering Components.")
 
 	k8sResourceClientSets, err := k8sResourceClients(cfg)
-	if err != nil {
-		log.Errorf("Failed to initialize K8s resource clients: %s", err.Error())
-		os.Exit(1)
-	}
+	exitOnError(err, "Failed to initialize K8s resource clients")
 
 	secretsManagerConstructor := func(namespace string) secrets.Manager {
 		return k8sResourceClientSets.core.CoreV1().Secrets(namespace)
@@ -76,16 +63,18 @@ func main() {
 		options.IntegrationNamespace,
 		options.GatewayPort,
 		options.UploadServiceUrl)
-	if err != nil {
-		log.Errorf("Failed to create synchronization service, %s", err.Error())
-		os.Exit(1)
-	}
+	exitOnError(err, "Failed to create synchronization service")
 
 	configMapNamespacedName := parseNamespacedName(options.ConnectionConfigMap)
 	configMapClient := k8sResourceClientSets.core.CoreV1().ConfigMaps(configMapNamespacedName.Namespace)
 
 	configProvider := confProvider.NewConfigProvider(configMapNamespacedName.Name, configMapClient)
 	clientsProvider := compass.NewClientsProvider(graphql.New, options.InsecureConnectorCommunication, options.InsecureConfigurationFetch, options.QueryLogging)
+
+	log.Infoln("Setting up Director Proxy Service")
+	directorProxy := director.NewProxy(options.DirectorProxy)
+	err = mgr.Add(directorProxy)
+	exitOnError(err, "Failed to create director proxy")
 
 	log.Infoln("Setting up Controller")
 	controllerDependencies := compassconnection.DependencyConfig{
@@ -95,59 +84,31 @@ func main() {
 		CredentialsManager:           certManager,
 		SynchronizationService:       syncService,
 		ConfigProvider:               configProvider,
+		DirectorProxyConfigurator:    directorProxy,
 		RuntimeURLsConfig:            options.Runtime,
 		CertValidityRenewalThreshold: options.CertValidityRenewalThreshold,
 		MinimalCompassSyncTime:       options.MinimalCompassSyncTime,
 	}
 
 	compassConnectionSupervisor, err := controllerDependencies.InitializeController()
-	if err != nil {
-		log.Error(err, "Unable to initialize Controller")
-		os.Exit(1)
-	}
+	exitOnError(err, "Failed to initialize Controller")
 
-	// Initialize Compass Connection CR
 	log.Infoln("Initializing Compass Connection CR")
 	_, err = compassConnectionSupervisor.InitializeCompassConnection()
-	if err != nil {
-		log.Error("Unable to initialize Compass Connection CR")
-	}
+	exitOnError(err, "Failed to initialize Compass Connection CR")
 
-	// Initialize metrics logger
 	log.Infoln("Initializing metrics logger")
 	metricsLogger, err := newMetricsLogger(options.MetricsLoggingTimeInterval)
-	if err != nil {
-		log.Error(errors.Wrap(err, "Unable to create metrics logger"))
-		os.Exit(1)
-	}
+	exitOnError(err, "Failed to create metrics logger")
+	err = mgr.Add(metricsLogger)
 
-	runManagerAndLoggerConcurrently(mgr, metricsLogger)
-	os.Exit(1)
+	log.Info("Starting the Cmd.")
+	err = mgr.Start(signals.SetupSignalHandler())
+	exitOnError(err, "Failed to run the manager")
 }
 
-func runManagerAndLoggerConcurrently(manager manager.Manager, logger metrics.Logger) {
-	// Enable running the manager and logger concurrently
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-	quitChannel := make(chan bool, 1)
-	defer close(quitChannel)
-
-	// Start the Cmd
-	go func() {
-		log.Info("Starting the Cmd.")
-		if err := manager.Start(signals.SetupSignalHandler()); err != nil {
-			log.Error(err, "Unable to run the manager")
-			quitChannel <- true
-		}
-		wg.Done()
-	}()
-
-	// Start metrics logging
-	go func() {
-		log.Info("Starting metrics logging.")
-		logger.Log(quitChannel)
-		wg.Done()
-	}()
-
-	wg.Wait()
+func exitOnError(err error, context string) {
+	if err != nil {
+		log.Fatal(errors.Wrap(err, context))
+	}
 }
