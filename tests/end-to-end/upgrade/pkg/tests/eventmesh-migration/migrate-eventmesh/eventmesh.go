fff49cebb5b767b7653ae2e478ab305cf216bb0b
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2020-03-16 10:32:15 +0100

    organize imports

--- tests/end-to-end/upgrade/pkg/tests/eventmesh-migration/migrate-eventmesh/eventmesh.go
+++ tests/end-to-end/upgrade/pkg/tests/eventmesh-migration/migrate-eventmesh/eventmesh.go
@@ -1,17 +1,21 @@
 package migrateeventmesh
 
 import (
-	appconnector "github.com/kyma-project/kyma/components/application-operator/pkg/client/clientset/versioned"
-	"k8s.io/client-go/kubernetes"
-	serving "knative.dev/serving/pkg/client/clientset/versioned"
+	"github.com/sirupsen/logrus"
 
 	sc "github.com/kubernetes-incubator/service-catalog/pkg/client/clientset_generated/clientset"
-	appBroker "github.com/kyma-project/kyma/components/application-broker/pkg/client/clientset/versioned"
-	ebClientSet "github.com/kyma-project/kyma/components/event-bus/client/generated/clientset/internalclientset"
+
+	"k8s.io/client-go/kubernetes"
+
+	appbroker "github.com/kyma-project/kyma/components/application-broker/pkg/client/clientset/versioned"
+	appconnector "github.com/kyma-project/kyma/components/application-operator/pkg/client/clientset/versioned"
+	ebclientset "github.com/kyma-project/kyma/components/event-bus/client/generated/clientset/internalclientset"
+
 	"github.com/kyma-project/kyma/tests/end-to-end/upgrade/internal/runner"
-	"github.com/sirupsen/logrus"
+
 	eventingclientv1alpha1 "knative.dev/eventing/pkg/client/clientset/versioned/typed/eventing/v1alpha1"
 	messagingclientv1alpha1 "knative.dev/eventing/pkg/client/clientset/versioned/typed/messaging/v1alpha1"
+	serving "knative.dev/serving/pkg/client/clientset/versioned"
 )
 
 type MigrateFromEventMeshUpgradeTest struct {
@@ -20,16 +24,16 @@ type MigrateFromEventMeshUpgradeTest struct {
 	appConnectorInterface appconnector.Interface
 	messagingClient       messagingclientv1alpha1.MessagingV1alpha1Interface
 	servingClient         serving.Interface
-	appBrokerCli          appBroker.Interface
+	appBrokerCli          appbroker.Interface
 	scCli                 sc.Interface
 	eventingCli           eventingclientv1alpha1.EventingV1alpha1Interface
-	ebCli                 ebClientSet.Interface
+	ebCli                 ebclientset.Interface
 }
 
 // compile time assertion
 var _ runner.UpgradeTest = &MigrateFromEventMeshUpgradeTest{}
 
-func NewMigrateFromEventMeshUpgradeTest(appConnectorCli appconnector.Interface, k8sCli kubernetes.Interface, messagingCli messagingclientv1alpha1.MessagingV1alpha1Interface, servingCli serving.Interface, appBrokerCli appBroker.Interface, scCli sc.Interface, eventingCli eventingclientv1alpha1.EventingV1alpha1Interface, ebCli ebClientSet.Interface) runner.UpgradeTest {
+func NewMigrateFromEventMeshUpgradeTest(appConnectorCli appconnector.Interface, k8sCli kubernetes.Interface, messagingCli messagingclientv1alpha1.MessagingV1alpha1Interface, servingCli serving.Interface, appBrokerCli appbroker.Interface, scCli sc.Interface, eventingCli eventingclientv1alpha1.EventingV1alpha1Interface, ebCli ebclientset.Interface) runner.UpgradeTest {
 	return &MigrateFromEventMeshUpgradeTest{
 		k8sInterface:          k8sCli,
 		messagingClient:       messagingCli,
