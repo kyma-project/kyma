4345ce775587b5891470402a309907d721e59fe4
Przemyslaw Golicz <przemyslaw.golicz@sap.com>
2020-03-23 15:07:50 +0100

    remove not used code from k8sconst package

--- components/compass-runtime-agent/internal/k8sconsts/k8sconsts.go
+++ components/compass-runtime-agent/internal/k8sconsts/k8sconsts.go
@@ -1,9 +1,1 @@
 package k8sconsts
-
-const (
-	LabelApplication      = "app"
-	LabelServiceId        = "serviceId"
-	LabelApp              = "app"
-	KindApplication       = "Application"
-	APIVersionApplication = "applicationconnector.kyma-project.io/v1alpha1"
-)
