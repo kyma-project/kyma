eede8516447aedd22fdaf5b0854e746038338dd8
Mostafa Shorim <mostafa.shorim@sap.com>
2020-03-31 17:56:17 +0200

    debugging

--- tests/end-to-end/upgrade/pkg/tests/logging/logging.go
+++ tests/end-to-end/upgrade/pkg/tests/logging/logging.go
@@ -6,9 +6,7 @@ import (
 
 	dex "github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/utils/fetch-dex-token"
 
-	"github.com/kyma-project/kyma/tests/end-to-end/upgrade/pkg/tests/logging/pkg/fluentbit"
 	"github.com/kyma-project/kyma/tests/end-to-end/upgrade/pkg/tests/logging/pkg/logstream"
-	"github.com/kyma-project/kyma/tests/end-to-end/upgrade/pkg/tests/logging/pkg/loki"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
@@ -34,18 +32,18 @@ func NewLoggingTest(coreInterface kubernetes.Interface, domainName string, dexCo
 
 // CreateResources creates resources for logging upgrade test
 func (t LoggingTest) CreateResources(stop <-chan struct{}, log logrus.FieldLogger, namespace string) error {
-	log.Println("LoggingUpgradeTest creating resources")
+	// log.Println("LoggingUpgradeTest creating resources")
 	// logstream.Cleanup(namespace)
-	log.Println("Test if all the Loki/Fluent Bit pods are ready before upgrade")
-	loki.TestPodsAreReady()
-	log.Println("Test if Fluent Bit is able to find Loki before upgrade")
-	fluentbit.Test()
+	// log.Println("Test if all the Loki/Fluent Bit pods are ready before upgrade")
+	// loki.TestPodsAreReady()
+	// log.Println("Test if Fluent Bit is able to find Loki before upgrade")
+	// fluentbit.Test()
 	log.Println("Deploying test-counter-pod")
 	err := t.deployDummyPod(namespace, log)
 	if err != nil {
 		return err
 	}
-	logstream.WaitForDummyPodToRun(namespace)
+	// logstream.WaitForDummyPodToRun(namespace)
 	log.Println("Test if logs from test-counter-pod are streamed by Loki before upgrade")
 	err = t.testLogStream(namespace, log)
 	if err != nil {
@@ -56,16 +54,16 @@ func (t LoggingTest) CreateResources(stop <-chan struct{}, log logrus.FieldLogge
 
 // TestResources checks if resources are working properly after upgrade
 func (t LoggingTest) TestResources(stop <-chan struct{}, log logrus.FieldLogger, namespace string) error {
-	log.Println("Test if all the Loki/Fluent Bit pods are still running after upgrade")
-	loki.TestPodsAreReady()
-	log.Println("Test if Fluent Bit is able to find Loki after upgrade")
-	fluentbit.Test()
+	// log.Println("Test if all the Loki/Fluent Bit pods are still running after upgrade")
+	// loki.TestPodsAreReady()
+	// log.Println("Test if Fluent Bit is able to find Loki after upgrade")
+	// fluentbit.Test()
 	log.Println("Test if logs from test-counter-pod are streamed by Loki after upgrade")
 	err := t.testLogStream(namespace, log)
 	if err != nil {
 		return err
 	}
-	logstream.Cleanup(namespace)
+	// logstream.Cleanup(namespace)
 	return nil
 }
 
