b3cb49f1f2748e50ebadaf59a165f40c513c910b
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-03-31 14:58:47 +0200

    update golangici-lint script

--- components/function-controller/hack/run-linters.sh
+++ components/function-controller/hack/run-linters.sh
@@ -7,15 +7,14 @@ readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly BIN_DIR="${SCRIPT_DIR}/../bin"
 readonly GOLANGCI_LINT_BINARY="${BIN_DIR}/golangci-lint"
 
-readonly GOLANGCI_LINT_VERSION="v1.24.0"
+readonly GOLANGCI_LINT_VERSION="1.24.0"
 
 main(){
-    if [[ ! -x "${GOLANGCI_LINT_BINARY}" ]]; then
+    if [[ ! -x "${GOLANGCI_LINT_BINARY}" || "$(${GOLANGCI_LINT_BINARY} --version | grep "${GOLANGCI_LINT_VERSION}" -c)"==1 ]]; then
         echo "Downloading golangci-lint..."
-        curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${BIN_DIR}" "${GOLANGCI_LINT_VERSION}"
+        curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${BIN_DIR}" "v${GOLANGCI_LINT_VERSION}"
         echo "Done!"
     fi
-    
     "${GOLANGCI_LINT_BINARY}" run --config "${SCRIPT_DIR}/../.golangci.yml"
 }
 
