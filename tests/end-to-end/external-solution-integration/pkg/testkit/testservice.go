5bcfdd453611b68133784a89ce20e7bad6bb291b
kubadz <jakub.dziechciewicz@sap.com>
2020-04-02 08:56:45 +0200

    use apiRules instead of apis in end-to-end external solution integration tests

--- tests/end-to-end/external-solution-integration/pkg/testkit/testservice.go
+++ tests/end-to-end/external-solution-integration/pkg/testkit/testservice.go
@@ -6,17 +6,21 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"k8s.io/client-go/dynamic"
+
 	"github.com/hashicorp/go-multierror"
+	apiRulev1alpha1 "github.com/kyma-incubator/api-gateway/api/v1alpha1"
+	rulev1alpha1 "github.com/ory/oathkeeper-maester/api/v1alpha1"
 	"github.com/pkg/errors"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	appsclient "k8s.io/client-go/kubernetes/typed/apps/v1"
 	coreclient "k8s.io/client-go/kubernetes/typed/core/v1"
-
-	gatewayv1alpha2 "github.com/kyma-project/kyma/components/api-controller/pkg/apis/gateway.kyma-project.io/v1alpha2"
-	gatewayclientset "github.com/kyma-project/kyma/components/api-controller/pkg/clients/gateway.kyma-project.io/clientset/versioned/typed/gateway.kyma-project.io/v1alpha2"
 )
 
 const (
@@ -27,10 +31,11 @@ const (
 	healthEndpointFormat    = "https://%s.%s/health"
 	endpointFormat          = "https://%s.%s"
 	inClusterEndpointFormat = "http://%s.%s.svc.cluster.local:8090"
+	gateway                 = "kyma-gateway.kyma-system.svc.cluster.local"
 )
 
 type TestService struct {
-	apis            gatewayclientset.ApiInterface
+	apiRules        dynamic.ResourceInterface
 	deployments     appsclient.DeploymentInterface
 	services        coreclient.ServiceInterface
 	HttpClient      *http.Client
@@ -39,11 +44,11 @@ type TestService struct {
 	testServiceName string
 }
 
-func NewTestService(httpClient *http.Client, deployments appsclient.DeploymentInterface, services coreclient.ServiceInterface, apis gatewayclientset.ApiInterface, domain, namespace string) *TestService {
+func NewTestService(httpClient *http.Client, deployments appsclient.DeploymentInterface, services coreclient.ServiceInterface, apiRules dynamic.ResourceInterface, domain, namespace string) *TestService {
 	return &TestService{
 		HttpClient:      httpClient,
 		domain:          domain,
-		apis:            apis,
+		apiRules:        apiRules,
 		deployments:     deployments,
 		services:        services,
 		namespace:       namespace,
@@ -126,7 +131,7 @@ func (ts *TestService) WaitForCounterPodToUpdateValue(val int) error {
 func (ts *TestService) DeleteTestService() error {
 	errDeployment := ts.deployments.Delete(ts.testServiceName, &metav1.DeleteOptions{})
 	errService := ts.services.Delete(ts.testServiceName, &metav1.DeleteOptions{})
-	errApi := ts.apis.Delete(ts.testServiceName, &metav1.DeleteOptions{})
+	errApi := ts.apiRules.Delete(ts.testServiceName, &metav1.DeleteOptions{})
 	err := multierror.Append(errDeployment, errService, errApi)
 	return err.ErrorOrNil()
 }
@@ -207,19 +212,41 @@ func (ts *TestService) createService() error {
 }
 
 func (ts *TestService) createAPI() error {
-	api := &gatewayv1alpha2.Api{
+	port := uint32(testServicePort)
+	host := fmt.Sprintf("%s.%s", ts.testServiceName, ts.domain)
+	gateway := gateway
+	apiRule := &apiRulev1alpha1.APIRule{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: ts.testServiceName,
 		},
-		Spec: gatewayv1alpha2.ApiSpec{
-			Service: gatewayv1alpha2.Service{
-				Name: ts.testServiceName,
-				Port: testServicePort,
+		Spec: apiRulev1alpha1.APIRuleSpec{
+			Service: &apiRulev1alpha1.Service{
+				Name: &ts.testServiceName,
+				Port: &port,
+				Host: &host,
 			},
-			Hostname:       fmt.Sprintf("%s.%s", ts.testServiceName, ts.domain),
-			Authentication: []gatewayv1alpha2.AuthenticationRule{},
+			Gateway: &gateway,
+			Rules: []apiRulev1alpha1.Rule{
+				{
+					Path:    "/.*",
+					Methods: []string{"GET"},
+					AccessStrategies: []*rulev1alpha1.Authenticator{
+						{
+							Handler: &rulev1alpha1.Handler{
+								Name: "noop",
+							},
+						},
+					},
+				},
+			},
 		},
 	}
-	_, err := ts.apis.Create(api)
+
+	unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&apiRule)
+	if err != nil {
+		return err
+	}
+
+	_, err = ts.apiRules.Create(&unstructured.Unstructured{Object: unstructuredObj}, metav1.CreateOptions{})
 	return err
 }
