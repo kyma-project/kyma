9bb1901b3708e1aea1e9596d13d4a48436b70fe2
Magda Strek <magdalena.strek@sap.com>
2020-04-02 13:44:42 +0200

    test

--- components/api-gateway-migrator/pkg/migrator/translate_test.go
+++ components/api-gateway-migrator/pkg/migrator/translate_test.go
@@ -86,4 +86,99 @@ spec:
 		Expect(actual.TypeMeta).To(BeEquivalentTo(expected.TypeMeta))
 		Expect(actual.ObjectMeta).To(BeEquivalentTo(expected.ObjectMeta))
 	})
+
+	It("should translate simple api with 2 uthentications", func() {
+
+		var apiToTranslate = `
+apiVersion: gateway.kyma-project.io/v1alpha2
+kind: Api
+metadata:
+  name: httpbin-api
+spec:
+  service:
+    name: httpbin
+    port: 8000
+  hostname: httpbin.kyma.local
+  authentication:
+    - type: JWT
+      jwt:
+        issuer: https://dex.kyma.local
+        jwksUri: http://dex-service.kyma-system.svc.cluster.local:5556/keys
+        triggerRule:
+          excludedPaths:
+            - prefix: /pref/
+            - suffix: /suffix.ico
+            - regex: /anything.*
+            - prefix: /prefixed/
+            - exact: /exact/path/to/resource.jpg
+    - type: JWT
+      jwt:
+        issuer: https://keks.kyma.local
+        jwksUri: http://keks-service.kyma-system.svc.cluster.local:5556/keys`
+
+		var expectedApiRuleYaml = `
+apiVersion: gateway.kyma-project.io/v1alpha1
+kind: APIRule
+metadata:
+  name: httpbin-api
+spec:
+  gateway: kyma-gateway.kyma-system.svc.cluster.local
+  service:
+    name: httpbin
+    port: 8000
+    host: httpbin.kyma.local
+    external: false
+  rules:
+  - accessStrategies:
+    - handler: allow
+    methods: ["GET", "PUT", "POST", "DELETE"]
+    path: /pref/
+  - accessStrategies:
+    - handler: allow
+    methods: ["GET", "PUT", "POST", "DELETE"]
+    path: /suffix.ico
+  - accessStrategies:
+    - handler: allow
+    methods: ["GET", "PUT", "POST", "DELETE"]
+    path: /anything.*
+  - accessStrategies:
+    - handler: allow
+    methods: ["GET", "PUT", "POST", "DELETE"]
+    path: /prefixed/
+  - accessStrategies:
+    - handler: allow
+    methods: ["GET", "PUT", "POST", "DELETE"]
+    path: /exact/path/to/resource.jpg
+  - accessStrategies:
+    - config:
+        jwks_urls:
+        - http://dex-service.kyma-system.svc.cluster.local:5556/keys
+        - http://keks-service.kyma-system.svc.cluster.local:5556/keys
+        trusted_issuers:
+        - https://dex.kyma.local
+        - https://keks.kyma.local
+      handler: jwt
+    methods: ["GET", "PUT", "POST", "DELETE"]
+    path: /.*
+`
+
+		apiReader := strings.NewReader(apiToTranslate)
+		buffSize := 1000
+		apiObj := oldapi.Api{}
+		err := yaml.NewYAMLOrJSONDecoder(apiReader, buffSize).Decode(&apiObj)
+		Expect(err).To(BeNil())
+
+		apiRuleReader := strings.NewReader(expectedApiRuleYaml)
+		expected := newapi.APIRule{}
+		err = yaml.NewYAMLOrJSONDecoder(apiRuleReader, buffSize).Decode(&expected)
+		Expect(err).To(BeNil())
+
+		actual := translateToApiRule(&apiObj)
+
+		//Expect(actual).To(BeEquivalentTo(expected))
+		Expect(actual.Status).To(BeEquivalentTo(expected.Status))
+		Expect(actual.Spec).To(BeEquivalentTo(expected.Spec))
+		Expect(actual.TypeMeta).To(BeEquivalentTo(expected.TypeMeta))
+		Expect(actual.ObjectMeta).To(BeEquivalentTo(expected.ObjectMeta))
+	})
 })
