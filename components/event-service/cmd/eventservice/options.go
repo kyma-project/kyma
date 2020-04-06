530466a541eda5a06865e96d922a04eef56f2a0d
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2020-04-06 19:39:40 +0200

    remove outdated commandline arguments
    cleanup code along the way

--- components/event-service/cmd/eventservice/options.go
+++ components/event-service/cmd/eventservice/options.go
@@ -6,20 +6,16 @@ import (
 )
 
 type options struct {
-	externalAPIPort   int
-	eventsTargetURLV1 string
-	eventsTargetURLV2 string
-	requestTimeout    int
-	sourceID          string
-	requestLogging    bool
-	maxRequestSize    int64
-	eventMeshURL      string
+	externalAPIPort int
+	requestTimeout  int
+	sourceID        string
+	requestLogging  bool
+	maxRequestSize  int64
+	eventMeshURL    string
 }
 
 func parseArgs() *options {
 	externalAPIPort := flag.Int("externalAPIPort", 8081, "External API port.")
-	eventsTargetURLV1 := flag.String("eventsTargetURLV1", "http://localhost:9001/events", "Target URL for events to be sent.")
-	eventsTargetURLV2 := flag.String("eventsTargetURLV2", "http://localhost:9001/events", "Target URL for events to be sent.")
 	requestTimeout := flag.Int("requestTimeout", 1, "Timeout for services.")
 	requestLogging := flag.Bool("requestLogging", false, "Flag for logging incoming requests.")
 	sourceID := flag.String("sourceId", "stage.local.kyma.commerce", "The source id of the events")
@@ -29,18 +25,16 @@ func parseArgs() *options {
 	flag.Parse()
 
 	return &options{
-		externalAPIPort:   *externalAPIPort,
-		eventsTargetURLV1: *eventsTargetURLV1,
-		eventsTargetURLV2: *eventsTargetURLV2,
-		requestTimeout:    *requestTimeout,
-		requestLogging:    *requestLogging,
-		sourceID:          *sourceID,
-		maxRequestSize:    *maxRequestSize,
-		eventMeshURL:      *eventMeshURL,
+		externalAPIPort: *externalAPIPort,
+		requestTimeout:  *requestTimeout,
+		requestLogging:  *requestLogging,
+		sourceID:        *sourceID,
+		maxRequestSize:  *maxRequestSize,
+		eventMeshURL:    *eventMeshURL,
 	}
 }
 
 func (o *options) String() string {
-	return fmt.Sprintf("--externalAPIPort=%d --eventsTargetURLV1=%s --eventsTargetURLV2=%s --eventMeshURL=%s --requestTimeout=%d --sourceId=%s --requestLogging=%t --maxRequestSize=%d",
-		o.externalAPIPort, o.eventsTargetURLV1, o.eventsTargetURLV2, o.eventMeshURL, o.requestTimeout, o.sourceID, o.requestLogging, o.maxRequestSize)
+	return fmt.Sprintf("--externalAPIPort=%d --eventMeshURL=%s --requestTimeout=%d --sourceId=%s --requestLogging=%t --maxRequestSize=%d",
+		o.externalAPIPort, o.eventMeshURL, o.requestTimeout, o.sourceID, o.requestLogging, o.maxRequestSize)
 }
