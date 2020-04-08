2c4a0f9b43911aad50ba668a8a76d435df0e9b50
Mostafa Shorim <mostafa.shorim@sap.com>
2020-04-08 17:25:13 +0200

    enhancing logging upgrade test

--- tests/end-to-end/upgrade/pkg/tests/logging/logging.go
+++ tests/end-to-end/upgrade/pkg/tests/logging/logging.go
@@ -1,8 +1,6 @@
 package logging
 
 import (
-	"time"
-
 	dex "github.com/kyma-project/kyma/tests/end-to-end/backup-restore-test/utils/fetch-dex-token"
 
 	"github.com/kyma-project/kyma/tests/end-to-end/upgrade/pkg/tests/logging/pkg/jwt"
@@ -46,7 +44,7 @@ func (t LoggingTest) CreateResources(stop <-chan struct{}, log logrus.FieldLogge
 		return err
 	}
 	log.Println("Test if logs from test-counter-pod are streamed by Loki before upgrade")
-	err = t.testLogStream(namespace, 0)
+	err = t.testLogStream(namespace)
 	if err != nil {
 		return err
 	}
@@ -56,8 +54,7 @@ func (t LoggingTest) CreateResources(stop <-chan struct{}, log logrus.FieldLogge
 // TestResources checks if resources are working properly after upgrade
 func (t LoggingTest) TestResources(stop <-chan struct{}, log logrus.FieldLogger, namespace string) error {
 	log.Println("Test if new logs from test-counter-pod are streamed by Loki after upgrade")
-	currentTimeUnixNano := time.Now().UnixNano()
-	err := t.testLogStream(namespace, currentTimeUnixNano)
+	err := t.testLogStream(namespace)
 	if err != nil {
 		return err
 	}
@@ -69,21 +66,21 @@ func (t LoggingTest) TestResources(stop <-chan struct{}, log logrus.FieldLogger,
 	return nil
 }
 
-func (t LoggingTest) testLogStream(namespace string, startTimeUnixNano int64) error {
+func (t LoggingTest) testLogStream(namespace string) error {
 	token, err := jwt.GetToken(t.idpConfig)
 	if err != nil {
 		return errors.Wrap(err, "cannot fetch dex token")
 	}
 	authHeader := jwt.SetAuthHeader(token)
-	err = logstream.Test("container", "count", authHeader, startTimeUnixNano)
+	err = logstream.Test("container", "count", authHeader)
 	if err != nil {
 		return err
 	}
-	err = logstream.Test("app", "test-counter-pod", authHeader, startTimeUnixNano)
+	err = logstream.Test("app", "test-counter-pod", authHeader)
 	if err != nil {
 		return err
 	}
-	err = logstream.Test("namespace", namespace, authHeader, startTimeUnixNano)
+	err = logstream.Test("namespace", namespace, authHeader)
 	if err != nil {
 		return err
 	}
