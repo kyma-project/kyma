40acf234afadc51eb1b9561a4efa767770ba34e2
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-04-17 09:03:17 +0200

    Move ingress-dr to istio for minikube-only

--- resources/api-gateway/files/api-gateway-tests.sh
+++ resources/api-gateway/files/api-gateway-tests.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
-
 sleep 10
-
-cat << EOF > istio-dr.yaml
----
-apiVersion: networking.istio.io/v1alpha3 
-kind: DestinationRule
-metadata:
-  name: istio-ingressgateway
-  namespace: istio-system
-spec:
-  host: istio-ingressgateway.istio-system.svc.cluster.local 
-  trafficPolicy:
-    tls:
-      mode: DISABLE
-EOF
-{{- if .Values.global.isLocalEnv }}
-kubectl apply -f istio-dr.yaml -n istio-system
-sleep 5
-{{- end }}
 
 ./app.test
 exit_code=$?
-
-{{- if .Values.global.isLocalEnv }}
-kubectl delete -f istio-dr.yaml -n istio-system
-{{- end }}
 
 curl -XPOST http://127.0.0.1:15020/quitquitquit
 sleep 5
