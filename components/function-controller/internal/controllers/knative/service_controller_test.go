021ff3910a9c5cdbb216a8038658fd41c913549a
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-04-20 22:04:39 +0200

    update those tests

--- components/function-controller/internal/controllers/knative/service_controller_test.go
+++ components/function-controller/internal/controllers/knative/service_controller_test.go
@@ -1,13 +1,26 @@
 package knative
 
 import (
+	"context"
+	"fmt"
+	"strconv"
 	"testing"
+	"time"
 
-	"github.com/onsi/gomega"
+	"github.com/onsi/ginkgo"
+	gm "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/record"
+	duckv1 "knative.dev/pkg/apis/duck/v1"
 	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
+
+const timeout = time.Second * 15
 
 func Test_getNewestGeneration(t *testing.T) {
 	type args struct {
@@ -56,11 +69,11 @@ func Test_getNewestGeneration(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			g := gomega.NewGomegaWithT(t)
+			g := gm.NewGomegaWithT(t)
 
 			got, err := getNewestGeneration(tt.args.revisions)
-			g.Expect(err != nil).To(gomega.Equal(tt.wantErr))
-			g.Expect(got).To(gomega.Equal(tt.want))
+			g.Expect(err != nil).To(gm.Equal(tt.wantErr))
+			g.Expect(got).To(gm.Equal(tt.want))
 		})
 	}
 }
@@ -108,17 +121,113 @@ func TestFunctionReconciler_getOldRevisionSelector(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			g := gomega.NewGomegaWithT(t)
+			g := gm.NewGomegaWithT(t)
 
 			r := &ServiceReconciler{}
 			got, err := r.getOldRevisionSelector(tt.args.parentService, tt.args.revisions)
 
-			g.Expect(err != nil).To(gomega.Equal(tt.wantErr))
+			g.Expect(err != nil).To(gm.Equal(tt.wantErr))
 			if got != nil {
-				g.Expect(got).To(gomega.Equal(tt.want))
+				g.Expect(got).To(gm.Equal(tt.want))
 			} else {
-				g.Expect(got).To(gomega.BeNil())
+				g.Expect(got).To(gm.BeNil())
 			}
 		})
+	}
+}
+
+var _ = ginkgo.Describe("KService controller", func() {
+	var (
+		reconciler *ServiceReconciler
+		request    ctrl.Request
+	)
+
+	ginkgo.BeforeEach(func() {
+		srv := fixKservice("test-service")
+		request = ctrl.Request{NamespacedName: types.NamespacedName{Namespace: srv.GetNamespace(), Name: srv.GetName()}}
+		gm.Expect(k8sClient.Create(context.TODO(), &srv)).To(gm.Succeed())
+
+		revisionList := fixRevisionList("test-service", 4)
+		for _, rev := range revisionList {
+			pinnedRev := rev // pin
+			gm.Expect(k8sClient.Create(context.TODO(), &pinnedRev)).NotTo(gm.HaveOccurred())
+			gm.Expect(k8sClient.Status().Update(context.TODO(), &pinnedRev)).NotTo(gm.HaveOccurred())
+		}
+		reconciler =
+			NewServiceReconciler(k8sClient, ctrl.Log.WithName("controllers").WithName("kservice"),
+				config, scheme.Scheme, record.NewFakeRecorder(100))
+	})
+
+	ginkgo.It("should successfully delete leftover revisions", func() {
+		_, err := reconciler.Reconcile(request)
+		gm.Expect(err).To(gm.BeNil())
+
+		srv := servingv1.Service{}
+		gm.Expect(k8sClient.Get(context.TODO(), request.NamespacedName, &srv)).NotTo(gm.HaveOccurred())
+
+		_, err = reconciler.Reconcile(request)
+		gm.Expect(err).To(gm.BeNil())
+
+		srv.Status.Status.Conditions = duckv1.Conditions{{
+			Type:   servingv1.ServiceConditionReady,
+			Status: corev1.ConditionTrue,
+		}}
+
+		gm.Expect(k8sClient.Update(context.TODO(), &srv)).Should(gm.Succeed())
+		gm.Expect(k8sClient.Status().Update(context.TODO(), &srv)).Should(gm.Succeed())
+
+		_, err = reconciler.Reconcile(request)
+		gm.Expect(err).To(gm.BeNil())
+
+		var revisions servingv1.RevisionList
+		err := reconciler.resourceClient.ListByLabel(context.TODO(), srv.GetNamespace(), map[string]string{
+			serviceLabelKey: srv.GetName(),
+		}, &revisions)
+
+		// look at their deletion timestamp
+	})
+})
+
+func fixRevisionList(parentSvcName string, num int) []servingv1.Revision {
+	revList := []servingv1.Revision{}
+	for i := 0; i < num; i++ {
+		revision := servingv1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      fmt.Sprintf("test-revision-%d", i),
+				Labels: map[string]string{
+					serviceLabelKey:    parentSvcName,
+					cfgGenerationLabel: strconv.Itoa(i),
+				},
+			},
+			Spec: servingv1.RevisionSpec{},
+		}
+
+		revList = append(revList, revision)
+	}
+	return revList
+}
+
+func fixKservice(name string) servingv1.Service {
+	return servingv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+			Labels:    nil,
+		},
+		Spec: servingv1.ServiceSpec{
+			ConfigurationSpec: servingv1.ConfigurationSpec{
+				Template: servingv1.RevisionTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: nil,
+					},
+					Spec: servingv1.RevisionSpec{
+						PodSpec: corev1.PodSpec{
+
+						},
+					},
+				},
+			},
+		},
 	}
 }
---
+++ components/function-controller/internal/controllers/knative/suite_test.go
@@ -0,0 +1,69 @@
+package knative
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	"github.com/vrischmann/envconfig"
+	"k8s.io/client-go/kubernetes/scheme"
+	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+var config ServiceConfig
+var k8sClient client.Client
+var testEnv *envtest.Environment
+
+func TestAPIs(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+
+	ginkgo.RunSpecsWithDefaultAndCustomReporters(t,
+		"Serverless Suite - Knative Service Controller",
+		[]ginkgo.Reporter{printer.NewlineReporter{}})
+}
+
+var _ = ginkgo.BeforeSuite(func(done ginkgo.Done) {
+	logf.SetLogger(zap.New(zap.UseDevMode(true), zap.WriteTo(ginkgo.GinkgoWriter)))
+	ginkgo.By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "..", "..", "config", "crd", "bases"),
+			filepath.Join("..", "..", "..", "config", "crd", "crds-thirdparty"),
+		},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+	cfg, err := testEnv.Start()
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	gomega.Expect(cfg).ToNot(gomega.BeNil())
+
+	err = scheme.AddToScheme(scheme.Scheme)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	err = servingv1.AddToScheme(scheme.Scheme)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	// +kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	gomega.Expect(k8sClient).ToNot(gomega.BeNil())
+
+	err = envconfig.InitWithPrefix(&config, "TEST")
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	close(done)
+}, 60)
+
+var _ = ginkgo.AfterSuite(func() {
+	ginkgo.By("tearing down the test environment")
+	err := testEnv.Stop()
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+})
