583e41c53c29fb4f395dec889f6d9b87d0949f3c
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-04-26 23:08:48 +0200

    actually implement whole webhook

--- components/function-controller/cmd/webhook/main.go
+++ components/function-controller/cmd/webhook/main.go
@@ -3,25 +3,20 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
-	"os"
-	"os/user"
-	"path/filepath"
 
+	"github.com/vrischmann/envconfig"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
 	"knative.dev/pkg/injection"
 	"knative.dev/pkg/injection/sharedmain"
 	"knative.dev/pkg/signals"
-	"knative.dev/pkg/system"
 	"knative.dev/pkg/webhook"
 	"knative.dev/pkg/webhook/certificates"
 	"knative.dev/pkg/webhook/resourcesemantics"
 	"knative.dev/pkg/webhook/resourcesemantics/defaulting"
 	"knative.dev/pkg/webhook/resourcesemantics/validation"
+	ctrl "sigs.k8s.io/controller-runtime"
 
 	serverlessv1alhpa1 "github.com/kyma-project/kyma/components/function-controller/pkg/apis/serverless/v1alpha1"
 )
@@ -30,61 +25,44 @@ var types = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
 	serverlessv1alhpa1.GroupVersion.WithKind("Function"): &serverlessv1alhpa1.Function{},
 }
 
+type config struct {
+	CurrentNamespace   string `envconfig:"default=kyma-system"`
+	WebhookServiceName string `envconfig:"default=serverless-webhook-svc"`
+	SecretName         string `envconfig:"default=serverless-webhook"`
+	Port               int    `envconfig:"default=8433"`
+}
+
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;update
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
+// +kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=mutatingwebhookconfigurations;validatingwebhookconfigurations,verbs=get;list;watch;create;update;patch;delete
+
 func main() {
-	serviceName := os.Getenv("WEBHOOK_SERVICE_NAME")
-	if serviceName == "" {
-		serviceName = "webhook-aerfio"
+	cfg := &config{}
+	if err := envconfig.InitWithPrefix(cfg, "APP_WEBHOOK"); err != nil {
+		panic(errors.New("while reading env variables"))
 	}
 
 	// Scope informers to the webhook's namespace instead of cluster-wide
-	ctx := injection.WithNamespaceScope(signals.NewContext(), system.Namespace())
+	ctx := injection.WithNamespaceScope(signals.NewContext(), cfg.CurrentNamespace)
 
 	// Set up a signal context with our webhook options
 	ctx = webhook.WithOptions(ctx, webhook.Options{
-		ServiceName: serviceName,
-		Port:        8443,
-		SecretName:  "webhook-certs",
+		ServiceName: cfg.WebhookServiceName,
+		Port:        cfg.Port,
+		SecretName:  cfg.SecretName,
 	})
 
-	config, err := getRestConfig("")
-	if err != nil {
-		panic(errors.New("while initializing rest config"))
-	}
+	restConfig := ctrl.GetConfigOrDie()
 
-	sharedmain.WebhookMainWithConfig(ctx, "webhook",
-		config,
+	sharedmain.WebhookMainWithConfig(ctx, "serverless-webhook",
+		restConfig,
 		certificates.NewController,
 		NewDefaultingAdmissionController,
 		NewValidationAdmissionController,
 	)
 }
 
-func getRestConfig(kubeconfig string) (*rest.Config, error) {
-	if kubeconfig == "" {
-		kubeconfig = os.Getenv("KUBECONFIG")
-	}
-	// If we have an explicit indication of where the kubernetes config lives, read that.
-	if kubeconfig != "" {
-		return clientcmd.BuildConfigFromFlags("", kubeconfig)
-	}
-	// If not, try the in-cluster config.
-	if c, err := rest.InClusterConfig(); err == nil {
-		return c, nil
-	}
-	// If no in-cluster config, try the default location in the user's home directory.
-	if usr, err := user.Current(); err == nil {
-		if c, err := clientcmd.BuildConfigFromFlags("", filepath.Join(usr.HomeDir, ".kube", "config")); err == nil {
-			return c, nil
-		}
-	}
-
-	return nil, fmt.Errorf("could not create a valid kubeconfig")
-}
-
-// +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;update
-// +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
-
-func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+func NewDefaultingAdmissionController(ctx context.Context, _ configmap.Watcher) *controller.Impl {
 	return defaulting.NewAdmissionController(ctx,
 
 		// Name of the resource webhook.
@@ -106,7 +84,7 @@ func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher
 	)
 }
 
-func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+func NewValidationAdmissionController(ctx context.Context, _ configmap.Watcher) *controller.Impl {
 	return validation.NewAdmissionController(ctx,
 
 		// Name of the resource webhook.
