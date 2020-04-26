c9585b7200c70b51e94d3740bb291d7c643ddaa2
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-04-26 23:08:48 +0200

    actually implement whole webhook

--- components/function-controller/pkg/apis/serverless/v1alpha1/webhook.go
+++ components/function-controller/pkg/apis/serverless/v1alpha1/webhook.go
@@ -2,6 +2,7 @@ package v1alpha1
 
 import (
 	"context"
+	"fmt"
 
 	"knative.dev/pkg/apis"
 	"knative.dev/pkg/webhook/resourcesemantics"
@@ -9,19 +10,61 @@ import (
 
 var _ resourcesemantics.GenericCRD = (*Function)(nil)
 
-func (in *Function) SetDefaults(ctx context.Context) {
-	one := int32(1)
-	if in.Spec.MinReplicas == nil {
-		in.Spec.MinReplicas = &one
+func (fn *Function) SetDefaults(_ context.Context) {
+	fn.Spec.defaultReplicas()
+}
+
+func (fn *Function) Validate(_ context.Context) *apis.FieldError {
+	var allErr *apis.FieldError
+
+	if err := fn.Spec.validateReplicas(); err != nil {
+		allErr = err.ViaField("spec")
+	}
+
+	if err := fn.Spec.validateResources(); err != nil {
+		allErr = allErr.Also(err.ViaField("spec"))
+	}
+
+	return allErr
+}
+
+func (spec *FunctionSpec) defaultReplicas() {
+	if spec.MinReplicas == nil {
+		one := int32(1)
+		spec.MinReplicas = &one
+	}
+	if spec.MaxReplicas == nil {
+		*spec.MaxReplicas = *spec.MinReplicas
 	}
 }
 
-func (in *Function) Validate(ctx context.Context) *apis.FieldError {
-	maxReplicas := in.Spec.MaxReplicas
-	minReplicas := in.Spec.MinReplicas
-	if maxReplicas != nil && minReplicas != nil && *maxReplicas < *minReplicas {
-		return apis.ErrInvalidValue("maxReplicas is less then minReplicas", "spec.maxReplicas")
+func (spec *FunctionSpec) validateReplicas() *apis.FieldError {
+	maxReplicas := spec.MaxReplicas
+	minReplicas := spec.MinReplicas
+	if maxReplicas != nil && minReplicas != nil && *minReplicas > *maxReplicas {
+		return apis.ErrInvalidValue("maxReplicas is less than minReplicas", "maxReplicas")
+	}
+	return nil
+}
+
+func (spec *FunctionSpec) validateResources() *apis.FieldError {
+	requests := spec.Resources.Requests
+	limits := spec.Resources.Limits
+
+	var errs *apis.FieldError
+
+	if requests.Cpu().Cmp(*limits.Cpu()) == 1 {
+		errs = apis.ErrInvalidValue("limits cpu should be higher than requests cpu", "limits.cpu")
+	}
+	if requests.Memory().Cmp(*limits.Memory()) == 1 {
+		err := apis.ErrInvalidValue("limits memory should be higher than requests memory", "limits.memory")
+		errs = errs.Also(err)
+	}
+	if errs != nil {
+		fmt.Println("----")
+		fmt.Println(errs.Error())
+		fmt.Println("----")
 	}
 
-	return nil
+	return errs
 }
---
+++ components/function-controller/pkg/apis/serverless/v1alpha1/webhook_test.go
@@ -0,0 +1,59 @@
+package v1alpha1
+
+import (
+	"testing"
+
+	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"knative.dev/pkg/apis"
+)
+
+func TestFunctionSpec_validateResources(t *testing.T) {
+	tests := []struct {
+		name      string
+		resources corev1.ResourceRequirements
+		want      *apis.FieldError
+	}{
+		{
+			name: "does not error on requests=limits",
+			want: nil,
+			resources: corev1.ResourceRequirements{
+				Limits: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("50m"),
+					corev1.ResourceMemory: resource.MustParse("50Mi"),
+				},
+				Requests: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("50m"),
+					corev1.ResourceMemory: resource.MustParse("50Mi"),
+				},
+			},
+		},
+		{
+			name: "returns both errors when requests' cpu and memory are higher",
+			want: nil,
+			resources: corev1.ResourceRequirements{
+				Limits: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("50m"),
+					corev1.ResourceMemory: resource.MustParse("50Mi"),
+				},
+				Requests: corev1.ResourceList{
+					corev1.ResourceCPU:    resource.MustParse("51m"),
+					corev1.ResourceMemory: resource.MustParse("51Mi"),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			spec := &FunctionSpec{
+				Resources: tt.resources,
+			}
+			got := spec.validateResources()
+
+			gm := gomega.NewGomegaWithT(t)
+			gm.Expect(got).To(gomega.Equal(tt.want))
+			// TODO check if there's mention of both errors in `got`
+		})
+	}
+}
