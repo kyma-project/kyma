cd30f49a4832d8f2b94da36010e97c87ba4c7d54
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-04-27 12:15:10 +0200

    Ignore kservices not created by serverless-controller-manager in revision-controller

--- components/function-controller/internal/controllers/knative/service_controller.go
+++ components/function-controller/internal/controllers/knative/service_controller.go
@@ -15,6 +15,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/kyma-project/kyma/components/function-controller/internal/resource"
+	serverlessv1alpha1 "github.com/kyma-project/kyma/components/function-controller/pkg/apis/serverless/v1alpha1"
 )
 
 const (
@@ -68,6 +69,11 @@ func (r *ServiceReconciler) Reconcile(request ctrl.Request) (ctrl.Result, error)
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	if !hasCorrectLabels(*instance) {
+		r.Log.WithValues("service", request.NamespacedName).Info("skipping reconcilation for a service without needed labels")
+		return ctrl.Result{RequeueAfter: r.config.RequeueDuration}, nil
+	}
+
 	if !instance.Status.IsReady() {
 		return ctrl.Result{RequeueAfter: r.config.RequeueDuration}, nil
 	}
@@ -86,6 +92,16 @@ func (r *ServiceReconciler) Reconcile(request ctrl.Request) (ctrl.Result, error)
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func hasCorrectLabels(instance servingv1.Service) bool {
+	labels := instance.GetLabels()
+
+	_, managedByLabelOk := labels[serverlessv1alpha1.FunctionManagedByLabel]
+	_, nameLabelOk := labels[serverlessv1alpha1.FunctionNameLabel]
+	_, uuidLabel := labels[serverlessv1alpha1.FunctionUUIDLabel]
+
+	return managedByLabelOk && nameLabelOk && uuidLabel
 }
 
 func (r *ServiceReconciler) deleteRevisions(ctx context.Context, log logr.Logger, service *servingv1.Service, revisions []servingv1.Revision) error {
