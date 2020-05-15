0067db8ab6c85ff26ec015f81953a2625096434e
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2020-05-15 14:52:15 +0200

    Refactoring in pkg/kymahelm/release_test.go

--- components/kyma-operator/pkg/kymahelm/release_test.go
+++ components/kyma-operator/pkg/kymahelm/release_test.go
@@ -4,87 +4,152 @@ import (
 	"testing"
 
 	"github.com/kyma-project/kyma/components/kyma-operator/pkg/kymahelm"
-	"github.com/stretchr/testify/assert"
+	. "github.com/smartystreets/goconvey/convey"
 	helm "k8s.io/helm/pkg/proto/hapi/release"
 )
 
 func TestIsUpgradeStep(t *testing.T) {
 
-	for _, tc := range []struct {
-		status         kymahelm.ReleaseStatus
-		expectedResult bool
-		expectError    bool
-	}{
-		{
-			status: kymahelm.ReleaseStatus{
-				StatusCode:      helm.Status_PENDING_INSTALL,
-				CurrentRevision: 1,
-			},
-			expectedResult: false,
-			expectError:    false,
-		},
-		{
-			status: kymahelm.ReleaseStatus{
-				StatusCode: helm.Status_PENDING_UPGRADE,
-			},
-			expectedResult: true,
-			expectError:    false,
-		},
-		{
-			status: kymahelm.ReleaseStatus{
-				StatusCode: helm.Status_DEPLOYED,
-			},
-			expectedResult: true,
-			expectError:    false,
-		},
-		{
-			status: kymahelm.ReleaseStatus{
-				StatusCode: helm.Status_PENDING_ROLLBACK,
-			},
-			expectedResult: true,
-			expectError:    false,
-		},
-		{
-			status: kymahelm.ReleaseStatus{
-				StatusCode:           helm.Status_FAILED,
-				CurrentRevision:      2,
-				LastDeployedRevision: 1,
-			},
-			expectedResult: true,
-		},
-		{
-			status: kymahelm.ReleaseStatus{
-				StatusCode:           helm.Status_UNKNOWN,
-				CurrentRevision:      1,
-				LastDeployedRevision: 0,
-			},
-			expectedResult: false,
-		},
-		{
-			status: kymahelm.ReleaseStatus{
-				StatusCode:           helm.Status_DELETED,
-				CurrentRevision:      2,
-				LastDeployedRevision: 0,
-			},
-			expectedResult: false,
-			expectError:    true,
-		},
-		{
-			status: kymahelm.ReleaseStatus{
-				StatusCode: helm.Status_SUPERSEDED,
-			},
-			expectedResult: false,
-			expectError:    true,
-		},
-	} {
-		isUpgrade, err := tc.status.IsUpgradeStep()
+	const installation = false
+	const upgrade = true
+	Convey("IsUpgradeStep", t, func() {
+		Convey("should correctly detect upgradeable releases", func() {
+			Convey("in cases depending only on release status", func() {
 
-		if tc.expectError {
-			assert.Error(t, err)
-		} else {
-			assert.NoError(t, err)
-		}
+				testInput := []struct {
+					status         kymahelm.ReleaseStatus
+					expectedResult bool
+				}{
+					{
+						status: kymahelm.ReleaseStatus{
+							StatusCode: helm.Status_PENDING_INSTALL,
+						},
+						expectedResult: installation,
+					},
+					{
+						status: kymahelm.ReleaseStatus{
+							StatusCode: helm.Status_DEPLOYED,
+						},
+						expectedResult: upgrade,
+					},
+					{
+						status: kymahelm.ReleaseStatus{
+							StatusCode: helm.Status_PENDING_UPGRADE,
+						},
+						expectedResult: upgrade,
+					},
+					{
+						status: kymahelm.ReleaseStatus{
+							StatusCode: helm.Status_PENDING_ROLLBACK,
+						},
+						expectedResult: upgrade,
+					},
+				}
 
-		assert.Equal(t, isUpgrade, tc.expectedResult)
-	}
+				for _, testData := range testInput {
+
+					//given
+					statusObj := testData.status
+
+					//when
+					isUpgrade, err := statusObj.IsUpgradeStep()
+
+					//then
+					So(err, ShouldBeNil)
+					So(isUpgrade, ShouldEqual, testData.expectedResult)
+				}
+			})
+
+			Convey("in cases depending on release status and it's past revisions", func() {
+
+				statusCodesImportantPastRevisions := []helm.Status_Code{
+					helm.Status_FAILED,
+					helm.Status_UNKNOWN,
+					helm.Status_DELETED,
+					helm.Status_DELETING,
+				}
+
+				testInput := []struct {
+					currentRevision      int32
+					lastDeployedRevision int32
+					expectedResult       bool
+					expectedErrorMsg     string
+				}{
+					{
+						currentRevision:      0,
+						lastDeployedRevision: 0,
+						expectedResult:       installation,
+					},
+					{
+						currentRevision:      1,
+						lastDeployedRevision: 0,
+						expectedResult:       installation,
+					},
+					{
+						//TODO: Is this correct?
+						currentRevision:      1,
+						lastDeployedRevision: 1,
+						expectedResult:       installation,
+					},
+					{
+						currentRevision:      2,
+						lastDeployedRevision: 1,
+						expectedResult:       upgrade,
+					},
+					{
+						currentRevision:      3,
+						lastDeployedRevision: 2,
+						expectedResult:       upgrade,
+					},
+					{
+						currentRevision:      2,
+						lastDeployedRevision: 0,
+						expectedErrorMsg:     "no deployed revision to rollback to",
+					},
+				}
+
+				for _, testStatus := range statusCodesImportantPastRevisions {
+					for _, testData := range testInput {
+
+						//given
+						statusObj := kymahelm.ReleaseStatus{
+							StatusCode:           testStatus,
+							CurrentRevision:      testData.currentRevision,
+							LastDeployedRevision: testData.lastDeployedRevision,
+						}
+
+						//when
+						isUpgrade, err := statusObj.IsUpgradeStep()
+
+						//then
+						if err != nil {
+							So(err.Error(), ShouldContainSubstring, testData.expectedErrorMsg)
+						} else {
+							So(isUpgrade, ShouldEqual, testData.expectedResult)
+						}
+					}
+				}
+			})
+		})
+		Convey("should report an error for unrecognized status", func() {
+			statusCodesNoProcessing := []helm.Status_Code{
+				helm.Status_SUPERSEDED,
+			}
+
+			for _, testStatus := range statusCodesNoProcessing {
+
+				//given
+				statusObj := kymahelm.ReleaseStatus{
+					StatusCode: testStatus,
+				}
+
+				//when
+				_, err := statusObj.IsUpgradeStep()
+
+				//then
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, "unexpected status code")
+			}
+		})
+	})
 }
