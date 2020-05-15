1f38aa5090caa14b7973f1f9a98b3e963da217e3
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2020-05-15 10:18:43 +0200

    initial support for cloud events

--- tools/event-subscriber/cmd/subscriber.go
+++ tools/event-subscriber/cmd/subscriber.go
@@ -2,24 +2,97 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
-	"log"
+	"fmt"
+	"net/http"
+	"sync"
+	"sync/atomic"
 
-	"github.com/kyma-project/kyma/tests/event-subscriber/pkg/util"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	counter     uint32
+	receivedCEs map[string]bool
+	mu          sync.Mutex
 )
 
 func main() {
 	port := flag.Int("port", 9000, "tcp port on which to listen for http requests")
 	flag.Parse()
 
-	// creates the subscription server
-	stopSubscriber := make(chan bool)
-	log.Println("Creates the subscription server")
-	subscriberServer := util.NewSubscriberServerWithPort(*port, stopSubscriber)
-
-	<-stopSubscriber
-	log.Println("Shutting down server...")
-	if err := subscriberServer.Shutdown(context.Background()); err != nil {
-		panic(err)
+	ctx := context.Background()
+	p, err := cloudevents.NewHTTP()
+	if err != nil {
+		log.Fatalf("failed to create protocol: %s", err.Error())
 	}
+
+	cehandler, err := cloudevents.NewHTTPReceiveHandler(ctx, p, receiveCE)
+	if err != nil {
+		log.Fatalf("failed to create handler: %s", err.Error())
+	}
+
+	// Use a gorilla mux implementation for the overall http handler.
+	router := mux.NewRouter()
+
+	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		// an example API handler
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+
+	router.Handle("/ce", cehandler).Methods("POST")
+	router.HandleFunc("/", increaseCounter).Methods("POST")
+
+	router.HandleFunc("/ce/{uuid}", checkCE).Methods("GET")
+	router.HandleFunc("/", checkCounter).Methods("GET")
+
+	router.HandleFunc("/", reset).Methods("DELETE")
+
+	log.Printf("will listen on :%v\n", *port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%v", *port), router); err != nil {
+		log.Fatalf("unable to start http server, %s", err)
+	}
+}
+
+func receiveCE(_ context.Context, event cloudevents.Event) {
+	mu.Lock()
+	defer mu.Unlock()
+	id := event.Context.GetID()
+	log.Infof("Received CE: %v", id)
+	receivedCEs[id] = true
+
+}
+
+func increaseCounter(_ http.ResponseWriter, _ *http.Request) {
+	atomic.AddUint32(&counter, 1)
+	log.Infof("Received Request: counter = %v", counter)
+}
+
+func checkCE(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+	log.Infof("Checking for uuid: %v. found: %v", uuid, receivedCEs[uuid])
+	if err := json.NewEncoder(w).Encode(receivedCEs[uuid]); err != nil {
+		log.Errorf("Error during checkCE: %v", err)
+	}
+}
+
+func checkCounter(w http.ResponseWriter, r *http.Request) {
+	log.Infof("Checking counter = %v", counter)
+	if err := json.NewEncoder(w).Encode(counter); err != nil {
+		log.Errorf("Error during checkCounter: %v", err)
+	}
+}
+
+func reset(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	log.Info("Reset")
+	receivedCEs = make(map[string]bool)
+	atomic.StoreUint32(&counter, 0)
 }
