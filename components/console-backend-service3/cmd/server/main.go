32ef274d5fa8687adb7ba387e0ba815a32ec4a55
Szymon Janota <szymon.janota@sap.com>
2020-05-25 09:45:52 +0200

    CBS3 init

---
+++ components/console-backend-service3/cmd/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/kyma-project/kyma/components/console-backend-service3/pkg/graph"
+	"github.com/kyma-project/kyma/components/console-backend-service3/pkg/graph/generated"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
---
+++ components/console-backend-service3/go.mod
@@ -0,0 +1,8 @@
+module github.com/kyma-project/kyma/components/console-backend-service3
+
+go 1.13
+
+require (
+	github.com/99designs/gqlgen v0.11.3
+	github.com/vektah/gqlparser/v2 v2.0.1
+)
