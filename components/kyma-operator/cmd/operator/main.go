f7511ae3f6cf9c627910c6dfa2b507e89be32428
Karol Jaksik <jaksikkarol@gmail.com>
2020-06-04 13:13:02 +0200

    Adjust Kyma Operator and Api Gateway tests to work with independent Compass installation (#8691)
    * Adjust kyma-operator
    
    * Adjust api-gateway
    
    * Const package renaming
    
    Co-authored-by: Tomasz Gorgol <t.gorgol@sap.com>

--- components/kyma-operator/cmd/operator/main.go
+++ components/kyma-operator/cmd/operator/main.go
@@ -9,11 +9,13 @@ import (
 	"strings"
 	"time"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/kyma-project/kyma/components/kyma-operator/pkg/actionmanager"
 	"github.com/kyma-project/kyma/components/kyma-operator/pkg/conditionmanager"
-	"github.com/kyma-project/kyma/components/kyma-operator/pkg/consts"
+	"github.com/kyma-project/kyma/components/kyma-operator/pkg/env"
 	"github.com/kyma-project/kyma/components/kyma-operator/pkg/finalizer"
 	"github.com/kyma-project/kyma/components/kyma-operator/pkg/k8s"
 	"github.com/kyma-project/kyma/components/kyma-operator/pkg/kymahelm"
@@ -48,6 +50,7 @@ func main() {
 	}
 
 	stop := make(chan struct{})
+	env.InitConfig()
 
 	kubeconfig := flag.String("kubeconfig", "", "Path to a kubeconfig file")
 	helmHost := flag.String("helmhost", "tiller-deploy.kube-system.svc.cluster.local:44134", "Helm host")
@@ -101,14 +104,16 @@ func main() {
 	kymaCommandExecutor := &toolkit.KymaCommandExecutor{}
 
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
-	internalInformerFactory := informers.NewSharedInformerFactory(internalClient, time.Second*30)
+	internalInformerFactory := informers.NewSharedInformerFactoryWithOptions(internalClient, time.Second*30, informers.WithTweakListOptions(func(listOptions *v1.ListOptions) {
+		listOptions.FieldSelector = fmt.Sprintf(`metadata.name=%s`, env.Config.InstResource)
+	}))
 	installationLister := internalInformerFactory.Installer().V1alpha1().Installations().Lister()
 
 	kymaStatusManager := statusmanager.NewKymaStatusManager(internalClient, installationLister)
 	kymaActionManager := actionmanager.NewKymaActionManager(internalClient, installationLister)
 	conditionManager := conditionmanager.New(internalClient, installationLister)
 
-	installationFinalizerManager := finalizer.NewManager(consts.InstFinalizer)
+	installationFinalizerManager := finalizer.NewManager(env.Config.InstFinalizer)
 
 	//////////////////////////////////////////
 	//SETUP BUSINESS DOMAIN MODULES
