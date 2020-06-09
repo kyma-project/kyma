69154c81c22f2fb1f9f36ddb3eade9daeb85eba6
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2020-06-09 14:06:40 +0200

    Add deployments field to namespace query (#8739)

--- components/console-backend-service/internal/gqlschema/namespace.go
+++ components/console-backend-service/internal/gqlschema/namespace.go
@@ -1,10 +1,11 @@
 package gqlschema
 
 type Namespace struct {
-	Name              string   `json:"name"`
-	Applications      []string `json:"applications"`
-	Labels            Labels   `json:"labels"`
-	Status            string   `json:"status"`
-	IsSystemNamespace bool     `json:"isSystemNamespace"`
-	Pods              []Pod    `json:"pods"`
+	Name              string       `json:"name"`
+	Applications      []string     `json:"applications"`
+	Labels            Labels       `json:"labels"`
+	Status            string       `json:"status"`
+	IsSystemNamespace bool         `json:"isSystemNamespace"`
+	Pods              []Pod        `json:"pods"`
+	Deployments       []Deployment `json:"deployments"`
 }
