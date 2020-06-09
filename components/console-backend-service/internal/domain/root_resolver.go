cd8f8aad6adf0a86e686e94620dd417a6a9506ab
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-06-09 13:54:13 +0200

    Fix after merge

--- components/console-backend-service/internal/domain/root_resolver.go
+++ components/console-backend-service/internal/domain/root_resolver.go
@@ -923,7 +923,7 @@ func (r *namespaceResolver) Pods(ctx context.Context, obj *gqlschema.Namespace) 
 	return r.k8s.PodsQuery(ctx, obj.Name, nil, nil)
 }
 
-func (r *namespaceResolver) Deployments(ctx context.Context, obj *gqlschema.Namespace, excludeFunctions *bool) ([]gqlschema.Deployment, error) {
+func (r *namespaceResolver) Deployments(ctx context.Context, obj *gqlschema.Namespace, excludeFunctions *bool) ([]*gqlschema.Deployment, error) {
 	return r.k8s.DeploymentsQuery(ctx, obj.Name, excludeFunctions)
 }
 
