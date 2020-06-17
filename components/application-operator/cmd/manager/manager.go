390d81f7744a90f93ee3a98f367b8b92b8032107
Katarzyna Kepka <kasia.kepka23@gmail.com>
2020-06-17 12:16:32 +0200

    rel

--- components/application-operator/cmd/manager/manager.go
+++ components/application-operator/cmd/manager/manager.go
@@ -162,4 +162,5 @@ func newApplicationReleaseManager(options *options, cfg *rest.Config, helmClient
 	releaseManager := appRelease.NewApplicationReleaseManager(helmClient, appClient.ApplicationconnectorV1alpha1().Applications(), overridesDefaults, options.namespace)
 
 	return releaseManager, nil
+
 }
