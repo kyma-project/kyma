7dc33f8027e692d1deea451a2ab4f86451712e40
Sayan Hazra <sayan.mnit@gmail.com>
2020-07-01 13:38:01 +0200

    Refactored

--- installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
+++ installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
@@ -8,10 +8,10 @@ set -o errexit
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INSTALLATION_DIR=${CURRENT_DIR}/../../../
-SECONDS=0
+START_TIME=$SECONDS
 sudo ${INSTALLATION_DIR}/cmd/run.sh --vm-driver "none"
 sudo ${INSTALLATION_DIR}/scripts/is-installed.sh --timeout 30m
-duration=$SECONDS
-echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "$(($ELAPSED_TIME / 60)) minutes and $(($ELAPSED_TIME % 60)) seconds elapsed."
 sudo ${INSTALLATION_DIR}/scripts/watch-pods.sh
 sudo ${INSTALLATION_DIR}/scripts/testing.sh
