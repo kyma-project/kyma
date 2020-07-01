877de8c7e93e39a060a0c4d0ca437afdfa67375a
Sayan Hazra <sayan.mnit@gmail.com>
2020-07-01 12:30:27 +0200

    Added duration to minikube installation

--- installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
+++ installation/scripts/prow/kyma-integration-on-debian/deploy-and-test-kyma.sh
@@ -8,8 +8,10 @@ set -o errexit
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INSTALLATION_DIR=${CURRENT_DIR}/../../../
-
+SECONDS=0
 sudo ${INSTALLATION_DIR}/cmd/run.sh --vm-driver "none"
 sudo ${INSTALLATION_DIR}/scripts/is-installed.sh --timeout 30m
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 sudo ${INSTALLATION_DIR}/scripts/watch-pods.sh
 sudo ${INSTALLATION_DIR}/scripts/testing.sh
