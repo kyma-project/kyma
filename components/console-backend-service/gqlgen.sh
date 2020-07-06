1305b19e38158ecee36279e9831e0c8e1c25ea3c
Piotr Miśkiewicz <pmiskiew@wp.pl>
2020-07-06 12:27:11 +0200

    Cbs cleanup szymus (#8900)
    * New, improved service
    
    * Bind APIRules to k8s models
    
    * Get rid of some commonly repeated functions
    
    * Use spec directly as input
    
    * Use indexes for filtering
    
    * remove service
    
    * Simplify pluggable module usage
    
    * Fixed tests
    
    * Fix tests
    
    * Extract apigateway to separate file
    
    * Restore old resource package structs
    
    * Remove unused resource_v2 package
    
    * Fix apigateway tests
    
    * Fix lints
    
    * Fix port marshalling
    
    * Panic! at the schema.resolvers.go
    
    * Remove old gql generator
    
    * WIP on authenticator
    
    * Custom (Un)marshaler for Authenticator
    
    * Minireview fixes, unmarshalling apiruleaccessstrategyinput
    
    * Fix namespaced delete & marshalling port, add generation check
    
    * Make Extension optional
    
    * Fix tests
    
    * Review fix
    
    * Fix subscriptions
    
    * Remove commented code
    
    * Update resolver.go
    
    * Include in64 as valid port type
    
    * Improve README
    
    Co-authored-by: Szymon Janota <szymon.janota@sap.com>
    Co-authored-by: dariadomagala <daria.domagala@sap.com>
    Co-authored-by: Wawrzyn321 <Wawrzyn321@gmail.com>

--- components/console-backend-service/gqlgen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Generating code from GraphQL schema..."
-
-cd "$(dirname "$0")"
-
-cd ./internal/gqlschema
-GO111MODULE=off go run ../../hack/gqlgen.go --verbose --config ./config.yml
