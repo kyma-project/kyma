3e3858243271c61b9d18212bb030fbe56151a3c9
kubadz <jakub.dziechciewicz@sap.com>
2020-07-23 15:26:25 +0200

    fix mtls v1 check in isito-kyma-patch

--- resources/istio-kyma-patch/files/check.sh
+++ resources/istio-kyma-patch/files/check.sh
@@ -56,18 +56,13 @@ function require_istio_system() {
 }
 
 function check_mtls_enabled_v1() {
-  log "--> Check global mTLS"
-  local mTLS=$(kubectl get meshpolicy default -o jsonpath='{.spec.peers[0].mtls.mode}')
-  local status=$?
-  if [[ "$?" != 0 ]]; then
-    log "----> MeshPolicy istio-system/default not found!" red
-    exit 1
-  fi
-  if [[ "${mTLS}" != "STRICT" ]]; then
-    log "----> mTLS must be \"STRICT\"" red
-    exit 1
-  fi
-  log "----> mTLS is enabled" green
+    log "--> Check global mTLS"
+    local mTLS=$(kubectl get meshpolicy default -o jsonpath='{.spec.peers[0].mtls.mode}')
+    if [[ "${mTLS}" != "STRICT" ]] && [[ "${mTLS}" != "" ]]; then
+        log "----> mTLS must be \"STRICT\"" red
+        exit 1
+    fi
+    log "----> mTLS is enabled" green
 }
 
 function check_mtls_enabled_v2() {
