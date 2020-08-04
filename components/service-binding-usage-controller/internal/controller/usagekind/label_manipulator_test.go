380f0f74d0596f5c214d4a19dc811e163e7af588
michal.kempski@sap.com <michal.kempski@sap.com>
2020-08-04 16:41:50 +0200

    Remove outdated statement from SBU controller

--- components/service-binding-usage-controller/internal/controller/usagekind/label_manipulator_test.go
+++ components/service-binding-usage-controller/internal/controller/usagekind/label_manipulator_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/kyma-project/kyma/components/service-binding-usage-controller/internal/controller/usagekind"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -92,70 +91,6 @@ func TestLabelManipulator_EnsureLabelsAreApplied(t *testing.T) {
 			assert.Equal(t, "val-1", obj.GetLabels()["key-1"])
 		})
 	}
-}
-
-func TestLabelManipulator_DetectLabelsConflicts_NotDetected(t *testing.T) {
-	for tn, tc := range map[string]struct {
-		Object map[string]interface{}
-	}{
-		"empty labels": {
-			Object: map[string]interface{}{
-				"metadata": map[string]interface{}{
-					"labels": map[string]interface{}{},
-				},
-			},
-		},
-		"not empty labels": {
-			Object: map[string]interface{}{
-				"metadata": map[string]interface{}{
-					"labels": map[string]interface{}{
-						"john": "smith",
-					},
-				},
-			},
-		},
-		"not existing labels": {
-			Object: map[string]interface{}{
-				"metadata": map[string]interface{}{},
-			},
-		},
-	} {
-		t.Run(tn, func(t *testing.T) {
-			// given
-			svc := usagekind.NewLabelManipulator("metadata.labels")
-			obj := &unstructured.Unstructured{
-				Object: tc.Object,
-			}
-
-			// when
-			result, err := svc.DetectLabelsConflicts(obj, fixLabels())
-			require.NoError(t, err)
-
-			// then
-			assert.Nil(t, result)
-		})
-	}
-}
-
-func TestLabelManipulator_DetectLabelsConflicts_Detected(t *testing.T) {
-	tc := map[string]interface{}{
-		"metadata": map[string]interface{}{
-			"labels": map[string]interface{}{
-				"key-1": "val-1",
-			},
-		},
-	}
-	svc := usagekind.NewLabelManipulator("metadata.labels")
-	obj := &unstructured.Unstructured{
-		Object: tc,
-	}
-
-	// when
-	result, err := svc.DetectLabelsConflicts(obj, fixLabels())
-	require.Error(t, err)
-
-	// then
-	assert.NotEmpty(t, result)
 }
 
 func fixLabels() map[string]string {
