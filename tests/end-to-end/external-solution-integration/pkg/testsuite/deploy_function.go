ca03c63694a55f84fbd7d53f68bbdd249e4f7017
pPrecel <filip.strozik@outlook.com>
2020-08-05 09:29:00 +0200

    Add one more info about function in logs

--- tests/end-to-end/external-solution-integration/pkg/testsuite/deploy_function.go
+++ tests/end-to-end/external-solution-integration/pkg/testsuite/deploy_function.go
@@ -157,7 +157,7 @@ func (s *DeployFunction) isFunctionReady() error {
 	}
 
 	if !s.isReady(function) {
-		return errors.New("function is not ready yet")
+		return fmt.Errorf("function \" %+v \"is not ready yet", function)
 	}
 
 	return nil
