37b020c6bc3a596fc9b0fd356c0c12725d0038a6
Jakub Kabza <jakub.kabza@sap.com>
2020-08-13 10:04:57 +0200

    consent...

--- components/login-consent/internal/endpoints/consent.go
+++ components/login-consent/internal/endpoints/consent.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"github.com/kyma-project/kyma/components/login-consent/internal/helpers"
+	"github.com/ory/hydra-client-go/models"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 )
@@ -15,6 +16,11 @@ func (cfg *Config) Consent(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	if challenge == "" {
+		w.Write([]byte("expected a consent challenge to be set but received none."))
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
 	log.Info("Fetching consent request from Hydra")
 	consentReq, err := cfg.client.GetConsentRequest(challenge)
 	if err != nil {
@@ -23,8 +29,29 @@ func (cfg *Config) Consent(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	var redirectTo string
 	if consentReq.Skip {
+		log.Info("Accepting consent request")
+		completedReq, err := cfg.client.AcceptConsentRequest(challenge, &models.AcceptConsentRequest{
+			GrantScope:               consentReq.RequestedScope,
+			GrantAccessTokenAudience: consentReq.RequestedAccessTokenAudience,
+			Session: &models.ConsentRequestSession{
+				// ???
+			},
+		})
 
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		redirectTo = *completedReq.RedirectTo
+		http.Redirect(w, req, redirectTo, http.StatusFound)
+
+	} else {
+		requestedScope := consentReq.RequestedScope
+		log.Println(requestedScope)
+		//display consent page here - grant permissions
 	}
-
 }
