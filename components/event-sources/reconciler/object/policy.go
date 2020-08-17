a28b2c7795b972be217e224e184303173d528b26
Korbinian Stoemmer <github@k15r.dev>
2020-08-17 08:14:22 +0200

    deploy event-source-adapter as apps/deployment instead of ksvc (#9186)
    * replace ksvc by deployment
    
    * bump image
    
    * update rbac
    
    * dd missing files
    
    * add simple service test
    
    * add more tests
    
    * reorder imports
    
    * add metrics port
    
    * add missing permissions
    
    * fix port name
    
    must be 15chars or shorter
    
    * remove serving from the upgrade test
    
    * bump image
    
    * fix rbac for upgrade test
    
    * add label for podmonitor
    
    * update latency dashboard
    
    * fix delivery dashboard
    
    * fix tracing
    
    * Apply suggestions from code review
    
    Co-authored-by: Nils Schmidt <github@nilstschmidt.de>
    
    * fix comments
    
    * remove tracing env var support
    
    * dep ensure
    
    * fix latency dashboard
    
    * fix resources/knative-eventing/charts/event-mesh-dashboard/templates/event-mesh-delivery.yaml
    
    * RC from @anishj0shi: Remove dead code and unnecessary type check
    
    * Use same cloudevents client in integration test and adapter
    
    * Incorporate review comments from sayanh
    
    * Incorporate review comments round #2 from sayanh
    
    * Remove copy right note in non-autogenerated files
    
    * Remove copy right note in autogenerated files (using codegen)
    
    * Remove copy right note in resources files
    
    * Use Fatal with zap.Error logging
    
    * Fix style
    
    * Add "Connectivity Validator -> HTTP Source" panel
    
    * Incorporate review comments from marcobebway
    
    * Add post-upgrade hook to delete orphaned ksvcs
    
    * Fix dashboard (order and double graphs in validator->http event source)
    
    * Use internal logger
    
    * Introduce app label for http source pod for inclusion in pods dashboard
    
    * Fix style
    
    Co-authored-by: Nils Schmidt <github@nilstschmidt.de>
    Co-authored-by: Nils Schmidt <nils.schmidt02@sap.com>

--- components/event-sources/reconciler/object/policy.go
+++ components/event-sources/reconciler/object/policy.go
@@ -1,26 +1,9 @@
-/*
-Copyright 2020 The Kyma Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package object
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	authenticationv1alpha1api "istio.io/api/authentication/v1alpha1"
 	authenticationv1alpha1 "istio.io/client-go/pkg/apis/authentication/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const targetPort = "http-usermetric"
@@ -50,7 +33,7 @@ func ApplyExistingPolicyAttributes(src, dst *authenticationv1alpha1.Policy) {
 	dst.ResourceVersion = src.ResourceVersion
 }
 
-// WithTarget sets the target name of the Policy for a Knative Service which
+// WithTarget sets the target name of the Policy for a Deployment which
 // has metrics end-point
 func WithTarget(target string) ObjectOption {
 	return func(o metav1.Object) {
