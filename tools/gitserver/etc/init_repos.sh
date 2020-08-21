dedd20095c58053b09019f754fd51aecb6eb97a8
Tomasz Gorgol <t.gorgol@sap.com>
2020-08-21 13:41:18 +0200

    Use Alpine image instead

--- tools/gitserver/etc/init_repos.sh
+++ tools/gitserver/etc/init_repos.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 readonly REPOS_DIR="/var/www/git"
 
 mkdir "$REPOS_DIR"
-
-git config --global user.email "gitserver@kyma-project.io"
-git config --global user.name "Git Server"
 
 for d in /tmp/repos/*/ ; do
   cd "$d" || exit 1
@@ -14,4 +11,4 @@ for d in /tmp/repos/*/ ; do
   git clone --bare "$d"
 done
 
-chown -Rfv www-data:www-data /var/www/git
+chown -Rfv apache:apache /var/www/git
