f0017697efb971c051d870376c4e1d43e0412f5a
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-08-28 12:57:05 +0200

    Merge scripts

--- resources/certificates/charts/gardener/files/ingress-annotate.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-kubectl -n "${ISTIO_GATEWAY_NAMESPACE}" annotate service "${ISTIO_GATEWAY_NAME}" \
-	cert.gardener.cloud/secretname='"${ISTIO_GATEWAY_SECRET}"' \
-	dns.gardener.cloud/class='garden' \
-	dns.gardener.cloud/dnsnames='*.'"${GLOBAL_DOMAIN}"'' \
-	--overwrite
---
+++ resources/certificates/charts/gardener/files/service-annotate.sh
@@ -0,0 +1,7 @@
+set -e
+
+kubectl -n "${SERVICE_NAMESPACE}" annotate service "${SERVICE_NAME}" \
+	cert.gardener.cloud/secretname='"${SERVICE_SECRET_NAME}"' \
+	dns.gardener.cloud/class='garden' \
+	dns.gardener.cloud/dnsnames='*.'"${GLOBAL_DOMAIN}"'' \
+	--overwrite
