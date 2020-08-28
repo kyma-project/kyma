ab3e9a4797fe431bb840e519a60fbf85323c17c0
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-08-28 10:52:09 +0200

    Switch to annotate strategy

--- resources/certificates/charts/gardener/files/ingress-annotate.sh
+++ resources/certificates/charts/gardener/files/ingress-annotate.sh
@@ -1,6 +1,7 @@
 set -e
 
 kubectl -n "${ISTIO_GATEWAY_NAMESPACE}" annotate service "${ISTIO_GATEWAY_NAME}" \
+	cert.gardener.cloud/secretname='"${ISTIO_GATEWAY_SECRET}"' \
 	dns.gardener.cloud/class='garden' \
 	dns.gardener.cloud/dnsnames='*.'"${GLOBAL_DOMAIN}"'' \
 	--overwrite
