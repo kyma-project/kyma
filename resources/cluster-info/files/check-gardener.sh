56bdbf1676f7ef1b1f52aa46f803472ae3ecc9cb
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-09-02 15:51:56 +0200

    Rework cluster-info II

--- resources/cluster-info/files/check-gardener.sh
+++ resources/cluster-info/files/check-gardener.sh
@@ -1,64 +1,52 @@
 set -ex
 
 CERT_TYPE=$(cat ${CONFIG_MAP_DIR}/global.certificates.type)
-echo $CERT_TYPE " --- " $MANAGER_ENABLED
 
-GARDEN_CERTS=false
-if kubectl api-versions | grep -c cert.gardener.cloud ; then
-  echo "--> Gardener Certificate CR present"
-  GARDEN_CERTS=true
+if [[ "$CERT_TYPE" != "detect" ]]; then
+  echo "--> $CERT_TYPE requested, nothing to do here"
+  exit 0
 fi
 
-if [[ $GARDEN_CERTS && ! $MANAGER_ENABLED ]]; then
-  echo "--> Gardener CR found, manager not requested. Use gardener certs and stop"
-  PATCH_YAML=$(cat << EOF
+export GARDEN_CERTS=false
+if kubectl api-versions | grep -c cert.gardener.cloud ; then
+  echo "--> Gardener Certificate CR present"
+else
+  echo "--> No Gardener Certificate CR present, move on"
+  exit 0
+fi
+
+PATCH_YAML=$(cat << EOF
 ---
 data:
   global.certificates.type: "gardener"
 EOF
 )
+
+echo "---> Patching cm ${CLUSTER_INFO_NS}/${CLUSTER_INFO_CM}"
+set +e
+msg=$(kubectl patch cm ${CLUSTER_INFO_CM} --patch "${PATCH_YAML}" -n ${CLUSTER_INFO_NS} 2>&1)
+status=$?
+set -e
+
+if [[ $status -ne 0 ]] && [[ ! "$msg" == *"not patched"* ]]; then
+    echo "$msg"
+    exit $status
 fi
 
-if [[ ! $GARDEN_CERTS && $MANAGER_ENABLED ]]; then
-  echo "--> Gardener certs are not present, manager requested. Enable cert-manager and move one"
+PATCH_YAML=$(cat << EOF
+---
+data:
+  modules.gardener.enabled: "true"
+EOF
+)
+
+echo "---> Patching cm ${CLUSTER_INFO_NS}/certificates-overrides"
+set +e
+msg=$(kubectl patch cm certificates-overrides --patch "${PATCH_YAML}" -n ${CLUSTER_INFO_NS} 2>&1)
+status=$?
+set -e
+
+if [[ $status -ne 0 ]] && [[ ! "$msg" == *"not patched"* ]]; then
+    echo "$msg"
+    exit $status
 fi
-
-# PATCH_YAML=$(cat << EOF
-# ---
-# data:
-#   global.ingress.domainName: "${DOMAIN}"
-#   global.apiserver.domainName: "${DOMAIN}"
-#   global.environment.type: "gardener"
-#   global.environment.gardener: "true"
-# EOF
-# )
-
-# echo "---> Patching cm ${CLUSTER_INFO_NS}/${CLUSTER_INFO_CM}"
-# set +e
-# msg=$(kubectl patch cm ${CLUSTER_INFO_CM} --patch "${PATCH_YAML}" -n ${CLUSTER_INFO_NS} 2>&1)
-# status=$?
-# set -e
-
-# if [[ $status -ne 0 ]] && [[ ! "$msg" == *"not patched"* ]]; then
-#     echo "$msg"
-#     exit $status
-# fi
-
-# PATCH_YAML=$(cat << EOF
-# ---
-# data:
-#   modules.manager.enabled: "false"
-#   modules.gardener.enabled: "true"
-# EOF
-# )
-
-# echo "---> Patching cm kyma-installer/certificates-overrides"
-# set +e
-# msg=$(kubectl patch cm certificates-overrides --patch "${PATCH_YAML}" -n kyma-installer 2>&1)
-# status=$?
-# set -e
-
-# if [[ $status -ne 0 ]] && [[ ! "$msg" == *"not patched"* ]]; then
-#     echo "$msg"
-#     exit $status
-# fi
