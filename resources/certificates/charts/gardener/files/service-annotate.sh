05240a4714da0a1a3fb8cceb454ed1d1c4f82896
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-09-03 11:04:12 +0200

    Update gardener module, fix default values

--- resources/certificates/charts/gardener/files/service-annotate.sh
+++ resources/certificates/charts/gardener/files/service-annotate.sh
@@ -1,5 +1,7 @@
 set -e
 
+GLOBAL_DOMAIN="$(kubectl -n ${SHOOT_CM_NAMESPACE} get cm ${SHOOT_CM_NAME} -o jsonpath='{.data.domain}')"
+
 kubectl -n "${SERVICE_NAMESPACE}" annotate service "${SERVICE_NAME}" \
 	cert.gardener.cloud/secretname="${SERVICE_SECRET_NAME}" \
 	dns.gardener.cloud/class='garden' \
