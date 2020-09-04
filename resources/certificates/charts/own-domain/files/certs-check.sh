3a6ba5f29a236c96666244cbea53755d36190c78
Magda Strek <magdalena.strek@sap.com>
2020-09-04 12:06:22 +0200

    certificates own-domain/own-cert/user-provided module fixes

--- resources/certificates/charts/own-domain/files/certs-check.sh
+++ resources/certificates/charts/own-domain/files/certs-check.sh
@@ -7,14 +7,14 @@ echo "Checking if cert-manager CRDs are installed"
 kubectl get crd certificates.cert-manager.io
 kubectl get crd clusterissuers.cert-manager.io
 
+echo "Checking if cert-manager ClusterIssuer exists"
+
+kubectl get clusterissuers.cert-manager.io ${KYMA_CA_ISSUER} -n istio-system
+
 echo "Checking if cert-manager Certificates exist"
 
 kubectl get certificates.cert-manager.io ${KYMA_TLS_CERT} -n istio-system
 kubectl get certificates.cert-manager.io ${APISERVER_PROXY_TLS_CERT} -n kyma-system
-
-echo "Checking if cert-manager ClusterIssuer exists"
-
-kubectl get clusterissuers.cert-manager.io ${KYMA-CA-ISSUER} -n istio-system
 
 echo "Checking if Secrets exist"
 
