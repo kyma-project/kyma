4dca02142e93f8e8b1e04c69c5af5aa0019eb050
Julia Iskierka <jul.iskierka@gmail.com>
2020-09-07 12:44:12 +0200

    Add rbac to init containers

--- resources/iam-kubeconfig-service/files/init.sh
+++ resources/iam-kubeconfig-service/files/init.sh
@@ -6,11 +6,7 @@ if [[ $TYPE == "xip" ]]; then
   # When running on xip get the IP address from apiserver-proxy-ssl service
   IP_ADDRESS=$(kubectl get service -n kyma-system apiserver-proxy-ssl -o jsonpath='{.spec.clusterIP}')
   DOMAIN="${IP_ADDRESS}.xip.io"
-elif [[ $TYPE == "gardener" ]]; then
-  URL=$(kubectl get service -n kyma-system apiserver-proxy-ssl -o jsonpath='{.metadata.annotations.dns\.gardener\.cloud/dnsnames}')
-  # need to remove the apiserver. prefix from the annotation to get a domain
-  DOMAIN=${URL#"apiserver."}
-elif [[ $TYPE == "legacy" || $TYPE == "user-provided" ]]; then
+else
   DOMAIN="{{ .Values.global.domainName }}"
 fi
 
@@ -25,6 +21,6 @@ echo $DOMAIN > /injected-config/domain
 
 if [[ $TYPE == "legacy" ]]; then
   echo "{{ .Values.global.tlsCrt }}" | base64 --decode > /injected-config/ca-tls-cert.crt
-elif [[ $TYPE == "xip" || $TYPE == "user-provided" ]]; then
+elif [[ $TYPE == "xip" ]]; then
   kubectl get secret -n istio-system kyma-ca-key-pair -o jsonpath='{.data.tls\.crt}' | base64 --decode > /injected-config/ca-tls-cert.crt
 fi
