b00fadd7d6e473f082c907d85cbb179143c1b331
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-09-09 10:48:49 +0200

    Add test for no-cbs-added triggers

--- components/console-backend-service/internal/domain/eventing/service_test.go
+++ components/console-backend-service/internal/domain/eventing/service_test.go
@@ -2,7 +2,10 @@ package eventing
 
 import (
 	"context"
+	"fmt"
 	"testing"
+
+	"knative.dev/pkg/apis"
 
 	"github.com/stretchr/testify/assert"
 	"knative.dev/eventing/pkg/apis/eventing/v1alpha1"
@@ -41,11 +44,50 @@ func TestTriggersService_List(t *testing.T) {
 		assert.Equal(t, 1, len(result))
 		assert.Equal(t, trigger1.Namespace, result[0].Namespace)
 	})
+
+	t.Run("Should filter triggers with either Ref and URI", func(t *testing.T) {
+		const namespace = "default"
+		const serviceName = "serviceName"
+
+		refTrigger := createMockTriggerWithDestination("refTrigger", namespace, duckv1.Destination{
+			Ref: &duckv1.KReference{
+				Kind:       "Service",
+				Namespace:  namespace,
+				Name:       serviceName,
+				APIVersion: "",
+			},
+			URI: nil,
+		})
+
+		urlString := fmt.Sprintf("https://%s.%s.svc.cluster.local:80", serviceName, namespace)
+		url, err := apis.ParseURL(urlString)
+
+		require.NoError(t, err)
+		uriTrigger := createMockTriggerWithDestination("uriTrigger", namespace, duckv1.Destination{
+			Ref: nil,
+			URI: url,
+		})
+
+		serviceFactory, err := resourceFake.NewFakeGenericServiceFactory(v1alpha1.AddToScheme, refTrigger, uriTrigger)
+		require.NoError(t, err)
+
+		service := New(serviceFactory)
+		err = service.Enable()
+		require.NoError(t, err)
+
+		serviceFactory.InformerFactory.WaitForCacheSync(make(chan struct{}))
+
+		result, err := service.TriggersQuery(context.Background(), namespace, serviceName)
+
+		require.NoError(t, err)
+		assert.Equal(t, 2, len(result))
+	})
 }
 
 func TestTriggersService_Create(t *testing.T) {
 	const namespace = "default"
 	const serviceName = "service-name"
+
 	t.Run("Should create", func(t *testing.T) {
 		serviceFactory, err := resourceFake.NewFakeGenericServiceFactory(v1alpha1.AddToScheme)
 		require.NoError(t, err)
@@ -202,6 +244,25 @@ func createMockTrigger(name, namespace, serviceName string) *v1alpha1.Trigger {
 				},
 				URI: nil,
 			},
+		},
+		Status: v1alpha1.TriggerStatus{},
+	}
+}
+
+func createMockTriggerWithDestination(name, namespace string, subscriber duckv1.Destination) *v1alpha1.Trigger {
+	return &v1alpha1.Trigger{
+		TypeMeta: v1.TypeMeta{
+			APIVersion: "eventing.knative.dev/v1alpha1",
+			Kind:       "Trigger",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: v1alpha1.TriggerSpec{
+			Broker:     "default",
+			Filter:     nil,
+			Subscriber: subscriber,
 		},
 		Status: v1alpha1.TriggerStatus{},
 	}
