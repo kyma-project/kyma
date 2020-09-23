328e0c6e6e91221cbbcddd7bf6f28a14aa2264af
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-09-23 13:29:20 +0200

    Reenable ingress-gateway

--- resources/istio/files/istio-upgrade-1-7.sh
+++ resources/istio/files/istio-upgrade-1-7.sh
@@ -18,3 +18,6 @@ kubectl apply -f /etc/manifests
 
 echo "Apply Kyma related checks and patches"
 kubectl patch MutatingWebhookConfiguration istio-sidecar-injector --type 'json' -p '[{"op":"add","path":"/webhooks/0/namespaceSelector/matchExpressions/-","value":{"key":"gardener.cloud/purpose","operator":"NotIn","values":["kube-system"]}}]'
+
+echo "--> Enable ingress-gateway"
+kubectl scale deploy -n istio-system istio-ingressgateway --replicas 1
