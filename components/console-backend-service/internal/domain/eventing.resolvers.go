68f5c5704d788a4729e6eaa15bbf99cf04bd39f1
Agata Kucharska <agata.kucharska@sap.com>
2020-09-23 09:50:45 +0200

    Fix eventing.resolvers.go

--- components/console-backend-service/internal/domain/eventing.resolvers.go
+++ components/console-backend-service/internal/domain/eventing.resolvers.go
@@ -5,51 +5,50 @@ package domain
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlschema"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v11 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/eventing/pkg/apis/eventing/v1alpha1"
 )
 
-func (r *mutationResolver) CreateTrigger(ctx context.Context, namespace string, trigger gqlschema.TriggerCreateInput, ownerRef []*v1.OwnerReference) (*v1alpha1.Trigger, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *mutationResolver) CreateTrigger(ctx context.Context, namespace string, trigger gqlschema.TriggerCreateInput, ownerRef []*v11.OwnerReference) (*v1alpha1.Trigger, error) {
+	return r.eventing.CreateTrigger(ctx, namespace, trigger, ownerRef)
 }
 
-func (r *mutationResolver) CreateManyTriggers(ctx context.Context, namespace string, triggers []*gqlschema.TriggerCreateInput, ownerRef []*v1.OwnerReference) ([]*v1alpha1.Trigger, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *mutationResolver) CreateManyTriggers(ctx context.Context, namespace string, triggers []*gqlschema.TriggerCreateInput, ownerRef []*v11.OwnerReference) ([]*v1alpha1.Trigger, error) {
+	return r.eventing.CreateTriggers(ctx, namespace, triggers, ownerRef)
 }
 
 func (r *mutationResolver) DeleteTrigger(ctx context.Context, namespace string, triggerName string) (*v1alpha1.Trigger, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.eventing.DeleteTrigger(ctx, namespace, triggerName)
 }
 
 func (r *mutationResolver) DeleteManyTriggers(ctx context.Context, namespace string, triggerNames []string) ([]*v1alpha1.Trigger, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.eventing.DeleteTriggers(ctx, namespace, triggerNames)
 }
 
 func (r *queryResolver) Triggers(ctx context.Context, namespace string, serviceName string) ([]*v1alpha1.Trigger, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.eventing.TriggersQuery(ctx, namespace, serviceName)
 }
 
 func (r *subscriptionResolver) TriggerEvent(ctx context.Context, namespace string, serviceName string) (<-chan *gqlschema.TriggerEvent, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.eventing.TriggerEventSubscription(ctx, namespace, serviceName)
 }
 
 func (r *triggerResolver) Status(ctx context.Context, obj *v1alpha1.Trigger) (*gqlschema.TriggerStatus, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.eventing.StatusField(ctx, obj)
 }
 
 func (r *triggerSpecResolver) Filter(ctx context.Context, obj *v1alpha1.TriggerSpec) (gqlschema.JSON, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.eventing.FilterField(ctx, obj)
 }
 
 func (r *triggerSpecResolver) Port(ctx context.Context, obj *v1alpha1.TriggerSpec) (uint32, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.eventing.PortField(ctx, obj)
 }
 
 func (r *triggerSpecResolver) Path(ctx context.Context, obj *v1alpha1.TriggerSpec) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.eventing.PathField(ctx, obj)
 }
 
 // Trigger returns gqlschema.TriggerResolver implementation.
