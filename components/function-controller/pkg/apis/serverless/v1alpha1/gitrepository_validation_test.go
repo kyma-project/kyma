ddc5e35691eceafb1bfc878ec58e56940a314806
Damian Badura <damian.badura@sap.com>
2020-09-24 12:43:18 +0200

    add git ssh regex test use cases

--- components/function-controller/pkg/apis/serverless/v1alpha1/gitrepository_validation_test.go
+++ components/function-controller/pkg/apis/serverless/v1alpha1/gitrepository_validation_test.go
@@ -126,3 +126,48 @@ func TestGitRepositoryValidation(t *testing.T) {
 		})
 	}
 }
+
+func TestSSHRegex(t *testing.T) {
+	for testName, testData := range map[string]struct {
+		givenURL string
+		isSSH    bool
+	}{
+		"should success": {
+			givenURL: "git@github.com:kyma-project/kyma.git",
+			isSSH:    true,
+		},
+		"should success with protocol": {
+			givenURL: "ssh://ssh@github.com/test.git",
+			isSSH:    true,
+		},
+		"should success with ~": {
+			givenURL: "ssh://user@host.xz/~user/path/to/repo.git/",
+			isSSH:    true,
+		},
+		"should success with git protocol": {
+			givenURL: "git://host.xz/path/to/repo.git/",
+			isSSH:    true,
+		},
+		"should success with port": {
+			givenURL: "ssh://ssh@github.com:2500/test.git",
+			isSSH:    true,
+		},
+		"should not success": {
+			givenURL: "https://github.com/kyma-project/kyma.git",
+			isSSH:    false,
+		},
+		"should not success with @": {
+			givenURL: "https://fix@me.plz.git",
+			isSSH:    false,
+		},
+	} {
+		t.Run(testName, func(t *testing.T) {
+			g := gomega.NewGomegaWithT(t)
+
+			output := isRepoURLIsSSH(testData.givenURL)
+
+			//THEN
+			g.Expect(output).Should(gomega.Equal(testData.isSSH))
+		})
+	}
+}
