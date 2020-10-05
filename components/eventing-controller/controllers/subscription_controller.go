4968ba43ced138fb0373be72912c2927bbc92881
Nils Schmidt <nils.schmidt02@sap.com>
2020-10-05 18:28:59 +0200

    Add very rough reconciliation code

--- components/eventing-controller/controllers/subscription_controller.go
+++ components/eventing-controller/controllers/subscription_controller.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	// TODO: use different package
+	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -18,6 +20,11 @@ type SubscriptionReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+const (
+	// TODO:
+	finalizerName = "todo"
+)
+
 // +kubebuilder:rbac:groups=eventing.kyma-project.io,resources=subscriptions,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=eventing.kyma-project.io,resources=subscriptions/status,verbs=get;update;patch
 
@@ -25,13 +32,114 @@ func (r *SubscriptionReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error
 	_ = context.Background()
 	_ = r.Log.WithValues("subscription", req.NamespacedName)
 
-	r.Log.Info("reconciled obj", req.Namespace, req.Name)
+	// TODO: pass logger via constructor
+	// logger := r.Log.WithName("controllers").WithName("beb")
+
+	subscription := &eventingv1alpha1.Subscription{}
+	ctx := context.TODO()
+
+	// Ensure the object was not deleted in the meantime
+	if err := r.Client.Get(ctx, req.NamespacedName, subscription); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Bind fields to logger
+	log := r.Log.WithValues("kind", subscription.GetObjectKind().GroupVersionKind().Kind,
+		"name", subscription.GetName(),
+		"namespace", subscription.GetNamespace(),
+		"version", subscription.GetGeneration(),
+	)
+
+	// Ensure the finalizer is set
+	if err := r.syncFinalizer(subscription, ctx, log); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Sync the BEB Subscription with the Subscription CR
+	if err := r.syncBEBSubscription(subscription, ctx, log); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	log.Info("reconciled obj", req.Namespace, req.Name)
 
 	return ctrl.Result{}, nil
+}
+
+// syncFinalizer sets the finalizer in the Subscription
+func (r *SubscriptionReconciler) syncFinalizer(subscription *eventingv1alpha1.Subscription, ctx context.Context, logger logr.Logger) error {
+	// Check if finalizer is already set
+	if r.isFinalizerSet(subscription) {
+		return nil
+	}
+
+	// Add Finalizer
+	return r.addFinalizer(subscription, ctx)
+}
+
+
+func (r *SubscriptionReconciler) syncBEBSubscription(subscription *eventingv1alpha1.Subscription, ctx context.Context, logger logr.Logger) error {
+	logger.Info("Syncing subscription with BEB")
+	// TODO: get beb credentials from secret
+	// TODO: CRUD BEB subscription
+
+	// TODO: react on finalizer
+	if r.isInDeletion(subscription) {
+		logger.Info("Deleting BEB subscription")
+		if err := r.removeFinalizer(subscription, ctx); err != nil {
+			return err
+		}
+		// TODO: delete BEB subscription
+		return nil
+	}
+
+	logger.Info("Updating BEB subscription")
+
+	return nil
 }
 
 func (r *SubscriptionReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&eventingv1alpha1.Subscription{}).
 		Complete(r)
+}
+func (r *SubscriptionReconciler) addFinalizer(subscription *eventingv1alpha1.Subscription, ctx context.Context) error {
+	subscription.ObjectMeta.Finalizers = append(subscription.ObjectMeta.Finalizers, finalizerName)
+	if err := r.Update(ctx, subscription); err != nil {
+		return errors.Wrapf(err, "error while adding Finalizer with name: %s", finalizerName)
+	}
+	return nil
+}
+
+func (r *SubscriptionReconciler) removeFinalizer(subscription *eventingv1alpha1.Subscription, ctx context.Context) error {
+	var finalizers []string
+
+	// Build finalizer list without the one the controller owns
+	for _, finalizer := range subscription.ObjectMeta.Finalizers {
+		if finalizer == finalizerName {
+			continue
+		}
+		finalizers = append(finalizers, finalizer)
+	}
+
+	subscription.ObjectMeta.Finalizers = finalizers
+	if err := r.Update(ctx, subscription); err != nil {
+		return errors.Wrapf(err, "error while removing Finalizer with name: %s", finalizerName)
+	}
+	return nil
+}
+
+// isFinalizerSet checks if a finalizer is set on the Subscription which belongs to this controller
+func (r *SubscriptionReconciler) isFinalizerSet(subscription *eventingv1alpha1.Subscription) bool {
+	// Check if finalizer is already set
+	for _, finalizer := range subscription.ObjectMeta.Finalizers {
+		if finalizer == finalizerName {
+			return true
+		}
+	}
+	return false
+}
+
+// isInDeletion checks if the Subscription shall be deleted
+func (r *SubscriptionReconciler) isInDeletion(subscription *eventingv1alpha1.Subscription) bool {
+	return !subscription.DeletionTimestamp.IsZero()
 }
