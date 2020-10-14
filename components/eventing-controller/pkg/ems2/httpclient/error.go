e69fc35a2a6f035f2cb74712a34b8fe65e0415b2
Nils Schmidt <nils.schmidt02@sap.com>
2020-10-14 20:14:36 +0200

    Fix httpclient error description

--- components/eventing-controller/pkg/ems2/httpclient/error.go
+++ components/eventing-controller/pkg/ems2/httpclient/error.go
@@ -1,5 +1,10 @@
 package httpclient
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Error struct {
 	StatusCode int
 	Message    string
@@ -16,8 +21,23 @@ func NewError(err error, opts ...ErrorOpt) *Error {
 	return e
 }
 
+// Error provides a description of the error including all fields which are initialized to a non-default value
 func (e Error) Error() string {
-	return e.Message
+	messageParts := make([]string, 0)
+
+	if e.Message != "" {
+		messageDescription := fmt.Sprintf("message: %s", e.Message)
+		messageParts = append(messageParts, messageDescription)
+	}
+	if e.StatusCode != 0 {
+		statusCodeDescription := fmt.Sprintf("status code: %d", e.StatusCode)
+		messageParts = append(messageParts, statusCodeDescription)
+	}
+	if e.Cause != nil {
+		causeDescription := fmt.Sprintf("cause: %s", e.Cause.Error())
+		messageParts = append(messageParts, causeDescription)
+	}
+	return strings.Join(messageParts, "; ")
 }
 
 func (e Error) Unwrap() error {
