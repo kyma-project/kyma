366a28d31ece53dd709d659669c9e944ba3b5d1a
Nils Schmidt <nils.schmidt02@sap.com>
2020-10-15 18:03:22 +0200

    Polishing

--- resources/application-connector/charts/application-broker/knative-broker-migration.sh
+++ resources/application-connector/charts/application-broker/knative-broker-migration.sh
@@ -5,19 +5,20 @@ set -eu -o pipefail
 readonly KNATIVE_BROKER_API_GROUP="brokers.eventing.knative.dev"
 readonly ISTIO_POLICY_API_GROUP="policies.authentication.istio.io"
 
-# this migration script ensures that Knative brokers metrics are still scrapable
+# this migration script ensures that Knative broker metrics are still scrapable
 # Application-broker creates a Istio PeerAuthentication for newly provisioned ServiceInstances
-# for existing ones, the following migartion has to be performed:
-# migrate over all Knative brokers and
-# - delete the old Istio Policy
-# - create a new Istio PeerAuthentication
+# for existing ones, the following migration has to be performed:
+# - iterate over all namespaces which have knative broker injection enabled => that means there is a broker in the namespace
+# - iterate over each broker in the found namespaces (there should only be one, but just in case)
+#   - delete the old Istio Policy
+#   - create a new Istio PeerAuthentication
 function migrate() {
      local namespaces=$(kubectl get ns --no-headers -l knative-eventing-injection | awk '{print $1}')
-     echo "found the following namespaces with broker injection enabled: ${namespaces}"
-     for namespace in "${namespaces}"; do
+     echo "found the following namespaces with broker injection enabled: \"${namespaces}\""
+     for namespace in ${namespaces}; do
         local brokers=$(kubectl get ${KNATIVE_BROKER_API_GROUP} --no-headers --namespace ${namespace} | awk '{print $1}')
-        echo "found the following Knative Brokers in namespace: ${namespace}: ${brokers}"
-        for broker in "${brokers}"; do
+        echo "found the following Knative Brokers in namespace: \"${namespace}\”: \”${brokers}\""
+        for broker in ${brokers}; do
             echo "migration of broker ${broker}/${namespace} [started]"
             delete_istio_policy "${broker}" "${namespace}"
             create_istio_peer_authentication "${namespace}"
@@ -53,7 +54,7 @@ EOF
 # deletes an Istio Policy with the given name and namespace
 function delete_istio_policy() {
     local namespace="${1}"
-  # the peerauthentication uses the namespace name + the broker suffix, see here: https://github.com/kyma-project/kyma/blob/d4be1327717a6737177b64cd730467eb17982213/components/application-broker/internal/broker/provision.go#L447
+    # the peerauthentication uses the namespace name + the broker suffix, see here: https://github.com/kyma-project/kyma/blob/d4be1327717a6737177b64cd730467eb17982213/components/application-broker/internal/broker/provision.go#L447
     local name="${namespace}-broker"
     echo "deleting istio policy ${name}/${namespace} [started]"
     # delete the policy if it exists, otherwise there is nothing todo (--ignore-not-found prevents the command from failing in this case)
