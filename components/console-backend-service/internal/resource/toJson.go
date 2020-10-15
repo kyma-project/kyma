820ac8c08d4b55ad17a1d484f11d661b555d05e6
parostatkiem <jan.sudczak@sap.com>
2020-10-15 10:02:03 +0200

    fix one more error

--- components/console-backend-service/internal/resource/toJson.go
+++ components/console-backend-service/internal/resource/toJson.go
@@ -2,19 +2,20 @@ package resource
 
 import (
 	"encoding/json"
+
 	"github.com/kyma-project/kyma/components/console-backend-service/internal/gqlschema"
 	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-func ToJson(obj runtime.Object) (gqlschema.JSON, error){
+func ToJson(obj runtime.Object) (gqlschema.JSON, error) {
 	if obj == nil {
 		return nil, nil
 	}
 
 	jsonByte, err := json.Marshal(obj)
 	if err != nil {
-		return nil, errors.Wrapf(err, "while marshalling %T `%s`", obj)
+		return nil, errors.Wrapf(err, "while marshalling %T", obj)
 	}
 
 	var jsonMap map[string]interface{}
