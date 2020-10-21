e5147435f4f62401a09e41a1a452f62006b5847c
parostatkiem <jan.sudczak@sap.com>
2020-10-21 13:45:55 +0200

    add JsonField test

--- components/console-backend-service/internal/domain/k8sNew/test/limitRange_test.go
+++ components/console-backend-service/internal/domain/k8sNew/test/limitRange_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
 	k8sMeta "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -31,26 +32,19 @@ func TestLimitRange_Query(t *testing.T) {
 	})
 }
 
-func TestLimitRange_Query(t *testing.T) {
+func TestLimitRange_JsonField(t *testing.T) {
 	const namespace = "default"
 
-	t.Run("Should return limitRanges when there are some", func(t *testing.T) {
+	t.Run("JsonField returns no error", func(t *testing.T) {
 		limit1 := createMockLimitRange("limit1", namespace)
-		limit2 := createMockLimitRange("limit2", namespace)
-		service := setupServiceWithObjects(t, limit1, limit2)
 
-		result, _ := service.LimitRangesQuery(context.Background(), namespace)
+		service := setupServiceWithObjects(t, limit1)
 
-		assert.Equal(t, 2, len(result))
+		_, err := service.LimitRangeJSONfield(context.Background(), limit1)
+
+		require.NoError(t, err)
 	})
 
-	t.Run("Should return empty array if limitRange is not found", func(t *testing.T) {
-		service := setupServiceWithObjects(t)
-
-		result, _ := service.LimitRangesQuery(context.Background(), namespace)
-
-		assert.Equal(t, 0, len(result))
-	})
 }
 
 func createMockLimitRange(name, namespace string) *v1.LimitRange {
