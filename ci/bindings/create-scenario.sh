fded68ad881df1675cb8e16c8439319f7901d9eb
Karol Jaksik <jaksikkarol@gmail.com>
2020-10-22 13:54:36 +0200

    Update rbac, add debug

--- ci/bindings/create-scenario.sh
+++ ci/bindings/create-scenario.sh
@@ -4,5 +4,5 @@ set -o errexit
 echo "Creating Scenario"
 
 kubectl apply -f ci/bindings/secret.yaml
-kubectl apply -f ci/bindings/binding-sample.yaml
 kubectl apply -f ci/bindings/deployment-sandbox.yaml
+kubectl apply -f ci/bindings/binding-sample.yaml
