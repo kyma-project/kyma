04ea6811a18884e4eb12a9ff46eb54da4399a5ee
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-10-23 08:18:39 +0200

    Restore version check for 1.6 upgrade

--- resources/istio/files/istio-upgrade-1-6.sh
+++ resources/istio/files/istio-upgrade-1-6.sh
@@ -1,12 +1,13 @@
 set -ex
+set -o pipefail
 
-#echo "Checking current Istio version"
-#
-#CURRENT_VERSION=$(kubectl -n istio-system get deployment istiod --ignore-not-found -o jsonpath='{.spec.template.spec.containers[0].image}' | awk -F: '{print $2}')
-#if [[ "${CURRENT_VERSION}" == "${TARGET_VERSION}" ]]; then
-#    echo "Istio is already in version ${TARGET_VERSION}. Exiting..."
-#    exit 0
-#fi
+echo "--> Checking current Istio version"
+
+CURRENT_VERSION=$(kubectl -n istio-system get deployment istiod --ignore-not-found -o jsonpath='{.spec.template.spec.containers[0].image}' | awk -F: '{print $2}')
+if [[ "${CURRENT_VERSION}" == "${TARGET_VERSION}" ]]; then
+   echo "Istio is already in version ${TARGET_VERSION}. Exiting..."
+   exit 0
+fi
 
 OPERATOR_FILE="/etc/istio/operator-1-6.yaml"
 
