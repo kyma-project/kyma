9338cece870060b04e54745ec9d23be710179a5a
Karol Jaksik <jaksikkarol@gmail.com>
2020-10-26 14:17:12 +0100

    Add mutating webhook

--- ci/binding/create-scenario.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-echo "Creating Scenario"
-
-kubectl apply -f ci/binding/secret.yaml
-kubectl apply -f ci/binding/deployment-sandbox.yaml
-kubectl apply -f ci/binding/binding-sample.yaml
