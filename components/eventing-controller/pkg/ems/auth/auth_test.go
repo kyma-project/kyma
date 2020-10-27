a428fec36ae7ca52fd56fdaf08558e604a15a876
Radu Fantaziu <radu.fantaziu@sap.com>
2020-10-27 17:09:43 +0100

    Eventing controller (#9744)
    * Add very rough reconciliation code
    
    * Add todos
    
    * CRD for BEB Subcription
    
    * First test according to tutorial
    
    * First working test
    
    * Current state of tests
    
    * Start manager
    
    * 2 tests working
    
    * Add schema test
    
    * Table testify schema test
    
    * Add eventrecorder
    
    * First working version having status and events
    
    * Improved status handling
    
    * Adjust tests
    
    * Add ems2 library
    
    * Remove wrongly added ems2_test file
    
    * Add BEB subscriptions handling
    
    * Integrate BEB subscriptions handling in Reconcile()
    
    * Run each test in a different namespace and dot import test packages
    
    * Rebase and extend Subscription CR Status
    
    * Move object under test into test method and add support for checking for k8s events
    
    * Fix httpclient error description
    
    * Return error when beb init fails
    
    * Do not use external cluster by default
    
    * Minor error handling changes in beb
    
    * First step in implementing BEB mock
    
    * Rebase
    
    * Solve rebasing errors
    
    * Update EMS-CE URL cause it was changed by BEB
    
    * Add and expose EMS subscription status as status
    
    * Refactor checking of EMS subscription status
    
    * Avoid an unnecessary GET request to EMS
    
    * Fix hash types (uint64 not supported by OpenAPIV3 schema)
    
    * Rename misleading method name updateStatus (it does not perform an update)
    
    * Tests working with BEB mock
    
    * Move BebMock into testing package
    
    * Fix TestAuthenticator
    
    * Trigger reconcile again if the subscription was not active, refactor for API Rule
    
    * Avoid an unnecessary GET request to EMS
    
    * Trigger reconcile again if the subscription was not active, refactor for API Rule
    
    * Update tests for the new generated events during Reconcile()
    
    * Make reconcile trigger period configurable, default 10 min
    
    * Add beb checks
    
    * Use a timestamp in subscription status for timeout at retry
    
    * Check beb calls in integration tests
    
    * Use a timestamp in subscription status for timeout at retry
    
    * Make webhookAuth not required anymore, refactor Reconcile()
    
    * Fix problem with bebmock when multiple tests are executed
    
    * Move start of beb mock to BeforeSuite block
    
    * Adjust tests to match conditions and events
    
    * Add some unit tests (not all finished yet)
    
    * Add schema tests for optional fields
    
    * Refactor ginkgo matchers to separate file
    
    * Add schema tests for optional fields
    
    * Fix unit test
    
    * Fix imports-local
    
    * Update go.mod
    
    * Add ready status and printercolumn and improve integration tests
    
    * Update go.mod to use go 1.14
    
    * Update Dockerfile
    
    * Add more testspecs
    
    * Solve some review comments
    
    * Add readiness status and more integration tests
    
    * Test that subscription update works
    
    * Remove finished todos in controller
    
    * Link issues in code
    
    * Solve review comments
    
    * Refactor ems2 package
    
    * Solve more review comments
    
    * Rename import
    
    * Solve imports-local issue
    
    * Add one more unit test
    
    Co-authored-by: Nils Schmidt <nils.schmidt02@sap.com>

---
+++ components/eventing-controller/pkg/ems/auth/auth_test.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"net/http"
+	"os"
+	"testing"
+
+	"golang.org/x/oauth2"
+)
+
+const (
+	// default value in httpTransport implementation
+	maxIdleConns        = 100
+	maxIdleConnsPerHost = 0
+)
+
+func TestAuthenticator(t *testing.T) {
+	if err := os.Setenv("CLIENT_ID", "foo"); err != nil {
+		t.Errorf("error while setting env var CLIENT_ID")
+	}
+	if err := os.Setenv("CLIENT_SECRET", "foo"); err != nil {
+		t.Errorf("error while setting env var CLIENT_SECRET")
+	}
+	if err := os.Setenv("TOKEN_ENDPOINT", "foo"); err != nil {
+		t.Errorf("error while setting env var TOKEN_ENDPOINT")
+	}
+
+	// authenticate
+	authenticator := NewAuthenticator()
+
+	httpClient := authenticator.GetClient().GetHttpClient()
+
+	secTransport, ok := httpClient.Transport.(*oauth2.Transport)
+	if !ok {
+		t.Errorf("Failed to convert to oauth2 transport")
+	}
+	httpTransport, ok := secTransport.Base.(*http.Transport)
+	if !ok {
+		t.Errorf("Failed to convert to HTTP transport")
+	}
+
+	if httpTransport.MaxIdleConns != maxIdleConns {
+		t.Errorf("HTTP Client Transport MaxIdleConns is misconfigured want: %d but got: %d", maxIdleConns, httpTransport.MaxIdleConns)
+	}
+	if httpTransport.MaxIdleConnsPerHost != maxIdleConnsPerHost {
+		t.Errorf("HTTP Client Transport MaxIdleConnsPerHost is misconfigured want: %d but got: %d", maxIdleConnsPerHost, httpTransport.MaxIdleConnsPerHost)
+	}
+}
