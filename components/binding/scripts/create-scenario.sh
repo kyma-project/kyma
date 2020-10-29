e8b6359f4bbab50e3b7aac5ef33758aa39e25d79
Karol Jaksik <jaksikkarol@gmail.com>
2020-10-29 01:40:30 +0100

    Remove pwd

--- components/binding/scripts/create-scenario.sh
+++ components/binding/scripts/create-scenario.sh
@@ -3,7 +3,6 @@ set -o errexit
 
 echo "Creating Scenario"
 
-pwd
 kubectl apply -f ./scripts/secret.yaml
 kubectl apply -f ./scripts/deployment-sandbox.yaml
 kubectl apply -f ./scripts/binding-sample.yaml
