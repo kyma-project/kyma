e68680dd4cec244d8bd903c3b3c6ccea52f58661
Julia Iskierka <48293543+colunira@users.noreply.github.com>
2020-10-30 21:20:03 +0100

    Upgrade istio to 1.6, then 1.7 (#9503)
    Co-authored-by: kubadz <jakub.dziechciewicz@sap.com>
    Co-authored-by: Tomasz Smelcerz <tomasz.smelcerz@sap.com>
    Co-authored-by: piotrmsc <piotr.mscichowski@sap.com>
    Co-authored-by: Jakub Błaszczyk <jakub.blaszczyk@sap.com>
    Co-authored-by: Nils Schmidt <nils.schmidt02@sap.com>
    Co-authored-by: Rakesh Garimella <rakesh.garimella@sap.com>
    Co-authored-by: Nils Schmidt <github@nilstschmidt.de>
    Co-authored-by: Sayan Hazra <sayan.mnit@gmail.com>
    Co-authored-by: Magda Strek <magdalena.strek@sap.com>
    Co-authored-by: Jakub Kabza <jakub.kabza@sap.com>
    Co-authored-by: Thaler, Andreas <andreas.thaler01@sap.com>
    Co-authored-by: Hisar Balik <hisar.balik@sap.com>
    Co-authored-by: I332735 <suleyman.akbas@sap.com>

---
+++ resources/istio/files/istio-upgrade-1-6.sh
@@ -0,0 +1,41 @@
+set -ex
+set -o pipefail
+
+echo "--> Checking current Istio version"
+
+CURRENT_VERSION=$(kubectl -n istio-system get deployment istiod --ignore-not-found -o jsonpath='{.spec.template.spec.containers[0].image}' | awk -F: '{print $2}')
+if [[ "${CURRENT_VERSION}" == "${TARGET_VERSION}" ]]; then
+   echo "Istio is already in version ${TARGET_VERSION}. Exiting..."
+   exit 0
+fi
+
+OPERATOR_FILE="/etc/istio/operator-1-6.yaml"
+
+echo "--> Remove deprecated resources"
+if kubectl api-versions | grep -c rbac.istio.io ; then
+    kubectl delete clusterrbacconfigs.rbac.istio.io default --ignore-not-found=true
+fi
+
+if kubectl api-versions | grep -c authentication.istio.io ; then
+    kubectl delete meshpolicies.authentication.istio.io -n istio-system default --ignore-not-found=true
+fi
+
+echo "--> Delete deprecated CRDs"
+kubectl delete customresourcedefinitions.apiextensions.k8s.io clusterrbacconfigs.rbac.istio.io --ignore-not-found
+kubectl delete customresourcedefinitions.apiextensions.k8s.io meshpolicies.authentication.istio.io --ignore-not-found
+kubectl delete customresourcedefinitions.apiextensions.k8s.io policies.authentication.istio.io --ignore-not-found
+kubectl delete customresourcedefinitions.apiextensions.k8s.io rbacconfigs.rbac.istio.io --ignore-not-found
+kubectl delete customresourcedefinitions.apiextensions.k8s.io servicerolebindings.rbac.istio.io --ignore-not-found
+kubectl delete customresourcedefinitions.apiextensions.k8s.io serviceroles.rbac.istio.io --ignore-not-found
+
+echo "--> Temporary disable ingress-gateway"
+kubectl scale deploy -n istio-system istio-ingressgateway --replicas 0
+
+echo "--> Upgrade to Istio 1.6"
+istioctl upgrade -f "${OPERATOR_FILE}" -y
+
+echo "Apply custom kyma manifests"
+kubectl apply -f /etc/manifests
+
+echo "Apply Kyma related checks and patches"
+kubectl patch MutatingWebhookConfiguration istio-sidecar-injector --type 'json' -p '[{"op":"add","path":"/webhooks/0/namespaceSelector/matchExpressions/-","value":{"key":"gardener.cloud/purpose","operator":"NotIn","values":["kube-system"]}}]'
