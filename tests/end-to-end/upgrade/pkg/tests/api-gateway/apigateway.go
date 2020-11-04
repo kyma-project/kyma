eb2526489f974027b0ae18a3d1d7a05b020c3897
kubadz <jakub.dziechciewicz@sap.com>
2020-11-04 11:28:38 +0100

    use tagged version of httpbin image

--- tests/end-to-end/upgrade/pkg/tests/api-gateway/apigateway.go
+++ tests/end-to-end/upgrade/pkg/tests/api-gateway/apigateway.go
@@ -46,7 +46,7 @@ var (
 	serviceName    = "httpbin-apigateway-test"
 	deploymentName = serviceName
 	containerName  = serviceName
-	httpbinImage   = "eu.gcr.io/kyma-project/external/kennethreitz/httpbin:latest"
+	httpbinImage   = "eu.gcr.io/kyma-project/external/kennethreitz/httpbin:20201004"
 	clientID       = "dummy_client"
 	clientSecret   = "dummy_secret"
 	scope          = "read"
