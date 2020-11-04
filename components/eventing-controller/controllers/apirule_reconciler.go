b3779c06da9f81b51bd9f68f562b4e9a5a834320
Marco Bebway <marco.bebway@sap.com>
2020-11-04 01:25:39 +0100

    Enhance reflect APIRule deletion in the subscription status

--- components/eventing-controller/controllers/apirule_reconciler.go
+++ components/eventing-controller/controllers/apirule_reconciler.go
@@ -11,7 +11,6 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	k8stypes "k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
@@ -19,7 +18,6 @@ import (
 
 	apigatewayv1alpha1 "github.com/kyma-incubator/api-gateway/api/v1alpha1"
 	eventingv1alpha1 "github.com/kyma-project/kyma/components/eventing-controller/api/v1alpha1"
-	"github.com/kyma-project/kyma/components/eventing-controller/pkg/constants"
 )
 
 const ExternalSinkScheme = "https"
@@ -56,39 +54,30 @@ func (r *APIRuleReconciler) SetupWithManager(mgr ctrl.Manager) error {
 // Reconcile TODO ...
 func (r *APIRuleReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
-	apiRule := &apigatewayv1alpha1.APIRule{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      req.Name,
-			Namespace: req.Namespace,
-		},
-	}
 
-	// handle delete APIRule
+	// prepare initial APIRule
+	apiRule := &apigatewayv1alpha1.APIRule{ObjectMeta: metav1.ObjectMeta{Name: req.Name, Namespace: req.Namespace}}
+
+	// format common logger
+	log := r.Log.WithValues("kind", "APIRule", "name", apiRule.ObjectMeta.Name, "namespace", apiRule.ObjectMeta.Namespace)
+
+	// try get the APIRule
 	if err := r.Client.Get(ctx, req.NamespacedName, apiRule); err != nil {
-		return r.handleAPIRuleDelete(ctx, apiRule)
+		log.Info("APIRule is being deleted")
 	}
 
-	// handle add/update APIRule
-	return r.handleAPIRuleAddOrUpdate(ctx, apiRule)
+	return r.syncAPIRuleSubscriptionsStatus(ctx, apiRule, log)
 }
 
-// handleAPIRuleDelete TODO ...
-func (r *APIRuleReconciler) handleAPIRuleDelete(ctx context.Context, apiRule *apigatewayv1alpha1.APIRule) (ctrl.Result, error) {
+// syncAPIRuleSubscriptionsStatus TODO ...
+func (r *APIRuleReconciler) syncAPIRuleSubscriptionsStatus(ctx context.Context, apiRule *apigatewayv1alpha1.APIRule, log logr.Logger) (ctrl.Result, error) {
 	if !isRelevantAPIRuleName(apiRule.Name) {
 		return ctrl.Result{}, nil
 	}
 
-	// format log
-	log := r.Log.WithValues(
-		"kind", "APIRule",
-		"name", apiRule.ObjectMeta.Name,
-		"namespace", apiRule.ObjectMeta.Namespace,
-		"mode", "Delete",
-	)
-
 	// list all namespace subscriptions
 	namespaceSubscriptions := &eventingv1alpha1.SubscriptionList{}
-	if err := r.Client.List(ctx, namespaceSubscriptions, client.InNamespace(apiRule.Namespace)); err != nil {
+	if err := r.Cache.List(ctx, namespaceSubscriptions, client.InNamespace(apiRule.Namespace)); err != nil {
 		log.Error(err, "Failed to list namespace Subscriptions")
 		return ctrl.Result{}, err
 	}
@@ -107,43 +96,6 @@ func (r *APIRuleReconciler) handleAPIRuleDelete(ctx context.Context, apiRule *ap
 		}
 
 		apiRuleSubscriptions = append(apiRuleSubscriptions, subscription)
-	}
-
-	return r.syncSubscriptionsStatus(ctx, apiRule, apiRuleSubscriptions, log)
-}
-
-// handleAPIRuleAddOrUpdate TODO ...
-func (r *APIRuleReconciler) handleAPIRuleAddOrUpdate(ctx context.Context, apiRule *apigatewayv1alpha1.APIRule) (ctrl.Result, error) {
-	if !isRelevantAPIRule(apiRule) {
-		return ctrl.Result{}, nil
-	}
-
-	// format log
-	log := r.Log.WithValues(
-		"kind", "APIRule",
-		"name", apiRule.ObjectMeta.Name,
-		"namespace", apiRule.ObjectMeta.Namespace,
-		"version", apiRule.GetGeneration(),
-		"mode", "Add/Update",
-	)
-
-	// get subscriptions that are relevant to the current APIRule
-	apiRuleSubscriptions := make([]eventingv1alpha1.Subscription, 0, len(apiRule.ObjectMeta.OwnerReferences))
-	for _, ownerRef := range apiRule.ObjectMeta.OwnerReferences {
-		subscription := &eventingv1alpha1.Subscription{}
-		lookupKey := k8stypes.NamespacedName{Name: ownerRef.Name, Namespace: apiRule.Namespace}
-
-		if err := r.Client.Get(ctx, lookupKey, subscription); err != nil {
-			log.Error(err, "Subscription not found", "Name", ownerRef.Name)
-			return ctrl.Result{}, err
-		}
-
-		// skip if the subscription is marked for deletion
-		if subscription.DeletionTimestamp != nil {
-			continue
-		}
-
-		apiRuleSubscriptions = append(apiRuleSubscriptions, *subscription)
 	}
 
 	return r.syncSubscriptionsStatus(ctx, apiRule, apiRuleSubscriptions, log)
@@ -192,14 +144,6 @@ func (r *APIRuleReconciler) syncSubscriptionsStatus(ctx context.Context, apiRule
 // isRelevantAPIRuleName TODO ...
 func isRelevantAPIRuleName(name string) bool {
 	return strings.HasPrefix(name, SinkURLPrefix)
-}
-
-// isRelevantAPIRule TODO ...
-func isRelevantAPIRule(apiRule *apigatewayv1alpha1.APIRule) bool {
-	if v, ok := apiRule.Labels[constants.ControllerIdentityLabelKey]; ok && v == constants.ControllerIdentityLabelValue {
-		return true
-	}
-	return false
 }
 
 // computeAPIRuleReadyStatus TODO ...
