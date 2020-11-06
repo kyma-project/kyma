fd8cb07105f7b2c8038df184cda87550a951aac9
Karol Jaksik <jaksikkarol@gmail.com>
2020-11-06 14:13:14 +0100

    Add target kind

--- components/binding/scripts/create-scenario.sh
+++ components/binding/scripts/create-scenario.sh
@@ -5,4 +5,5 @@ echo "Creating Scenario"
 
 kubectl apply -f ./scripts/scenario/secret.yaml
 kubectl apply -f ./scripts/scenario/deployment.yaml
+kubectl apply -f ./scripts/scenario/target-kind.yaml
 kubectl apply -f ./scripts/scenario/binding.yaml
