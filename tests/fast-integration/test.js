a87a6baa1c309acbb79b4707a4f480792d6efe2d
Krystian Cieslik <crabtree@users.noreply.github.com>
2020-12-07 14:29:12 +0100

    wip

--- tests/fast-integration/test.js
+++ tests/fast-integration/test.js
@@ -1,11 +1,7 @@
-const {expect} = require("chai");
+const { expect } = require("chai");
 
 describe("dummy test", () => {
-
     it("always pass", () => {
-    
         expect(true).to.equal(true);
-    
     });
-
 });
