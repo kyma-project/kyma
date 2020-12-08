bb772b48745beb94a6da8eea6c797dc4955771e1
dariadomagala <daria.domagala@sap.com>
2020-12-08 09:57:37 +0100

    Remove chai-http library

--- tests/fast-integration/test.js
+++ tests/fast-integration/test.js
@@ -1,18 +1,16 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const { expect } = chai;
-chai.use(chaiHttp);
+const { expect } = require("chai");
+const https = require("https");
 
 const config = require('./config.js');
 
 describe("Function", () => {
     it("should return Hello World", (done) => {
-        chai.request(config.functionUrl)
-        .get('/')
-        .end((_, res) => {
-            expect(res.status).to.be.equal(200);
-            expect(res.text).to.be.equal('Hello World!');
-            done();
+        https.get(config.functionUrl, (res) => {
+            expect(res.statusCode).to.be.equal(200);
+            res.on('data', (data) => {
+                expect(data.toString()).to.be.equal('Hello World!');
+                done();
+            });
         });
     });
 });
