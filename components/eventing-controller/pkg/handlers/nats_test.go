ac4277191c249e73a328878d4ce5288c4679941d
montaro <ahmed.rifaey@gmail.com>
2021-01-08 17:06:54 +0100

    Fix TestConvertMsgToCE

--- components/eventing-controller/pkg/handlers/nats_test.go
+++ components/eventing-controller/pkg/handlers/nats_test.go
@@ -1,16 +1,17 @@
 package handlers
 
 import (
-	"reflect"
+	"net/url"
 	"testing"
 
 	cev2event "github.com/cloudevents/sdk-go/v2/event"
+	"github.com/cloudevents/sdk-go/v2/types"
 	"github.com/nats-io/nats.go"
 )
 
 func TestConvertMsgToCE(t *testing.T) {
-
 	validJsonCE := `{"data":"test","datacontenttype":"application/json","id":"id","source":"source","specversion":"1.0","time":"2020-12-29T12:33:38.882056251Z","type":"sap.kyma.custom.varkes.order.created.v1"}`
+	dataContentType := "application/json"
 	testCases := []struct {
 		natsMsg            *nats.Msg
 		expectedCloudEvent *cev2event.Event
@@ -23,21 +24,21 @@ func TestConvertMsgToCE(t *testing.T) {
 			Data:    []byte(validJsonCE),
 			Sub:     nil,
 		},
-		// Context Attributes,
-        //  specversion: 1.0
-        //  type: sap.kyma.custom.varkes.order.created.v1
-        //  source: source
-        //  id: id
-        //  time: 2020-12-29T12:33:38.882056251Z
-        //  datacontenttype: application/json
-        //Data,
-        //  "test"
-		expectedCloudEvent: cev2event.Event{
-			Context:    cev2event. ,
-			DataEncoded: nil,
+		expectedCloudEvent: &cev2event.Event{
+			Context: &cev2event.EventContextV1{
+				Type: "sap.kyma.custom.varkes.order.created.v1",
+				Source: types.URIRef{
+					URL: url.URL{
+						Path: "source",
+					},
+				},
+				ID:              "id",
+				DataContentType: &dataContentType,
+			},
+			DataEncoded: []byte("\"test\""),
 			DataBase64:  false,
 			FieldErrors: nil,
-		}
+		},
 		expectedErr: nil,
 	},
 	}
@@ -47,11 +48,13 @@ func TestConvertMsgToCE(t *testing.T) {
 			t.Errorf("should not give error, got: %v", err)
 		}
 		if err != nil && tc.expectedErr.Error() != err.Error() {
-
 			t.Errorf("received wrong error, expected: %v got: %v", tc.expectedErr, err)
 		}
-
-		if !reflect.DeepEqual(gotCE, tc.expectedCloudEvent) {
+		if !(gotCE.Subject() == tc.expectedCloudEvent.Subject()) ||
+			!(gotCE.ID() == tc.expectedCloudEvent.ID()) ||
+			!(gotCE.DataContentType() == tc.expectedCloudEvent.DataContentType()) ||
+			!(gotCE.Source() == tc.expectedCloudEvent.Source()) ||
+			!(string(gotCE.Data()) == string(tc.expectedCloudEvent.Data())) {
 			t.Errorf("received wrong cloudevent, expected: %v got: %v", tc.expectedCloudEvent, gotCE)
 		}
 	}
