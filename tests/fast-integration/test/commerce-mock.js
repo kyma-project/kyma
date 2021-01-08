f1b789dd4964c3e35770f4a975642b0ac5250888
pbochynski <p.bochynski@gmail.com>
2021-01-08 14:48:57 +0100

    Refactoring of tests, prepare upgrade test

--- tests/fast-integration/test/commerce-mock.js
+++ tests/fast-integration/test/commerce-mock.js
@@ -1,278 +1,31 @@
-const k8s = require("@kubernetes/client-node");
-const {
-  commerceMockYaml,
-  serviceCatalogResources,
-  mocksNamespaceYaml,
-  lastorderFunctionYaml,
-} = require("./fixtures/commerce-mock");
-const { expect, config } = require("chai");
-config.truncateThreshold = 0; // more verbose errors
+const { ensureCommerceMockTestFixture,
+  checkAppGatewayResponse,
+  sendEventAndCheckResponse,
+  cleanMockTestFixture
+} = require('./fixtures/commerce-mock')
 
-const {
-  retryPromise,
-  expectNoK8sErr,
-  expectNoAxiosErr,
-  sleep,
-  k8sApply,
-  waitForK8sObject,
-  waitForServiceClass,
-  waitForServiceInstance,
-  waitForServiceBinding,
-  waitForVirtualService
-} = require("../utils");
+describe("CommerceMock tests", function () {
 
-const https = require("https");
-const axios = require("axios").default;
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false, // curl -k
-});
-axios.defaults.httpsAgent = httpsAgent;
-
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
-
-const k8sDynamicApi = kc.makeApiClient(k8s.KubernetesObjectApi);
-const k8sAppsApi = kc.makeApiClient(k8s.AppsV1Api);
-const watch = new k8s.Watch(kc);
-
-const commerceObjs = k8s.loadAllYaml(commerceMockYaml);
-const lastorderObjs = k8s.loadAllYaml(lastorderFunctionYaml);
-const mocksNamespaceObj = k8s.loadYaml(mocksNamespaceYaml);
-
-const tokenRequest = {
-  apiVersion: 'applicationconnector.kyma-project.io/v1alpha1',
-  kind: 'TokenRequest',
-  metadata: { name: 'commerce', namespace: 'default' }
-}
-const serviceBinding = {
-  apiVersion: 'servicecatalog.k8s.io/v1beta1',
-  kind: 'ServiceBinding',
-  metadata: { name: 'commerce-binding' },
-  spec: {
-    instanceRef: { name: 'commerce-webservices' }
-  }
-}
-
-const sbu = {
-  apiVersion: 'servicecatalog.kyma-project.io/v1alpha1',
-  kind: 'ServiceBindingUsage',
-  metadata: { name: 'commerce-lastorder-sbu' },
-  spec: {
-    serviceBindingRef: { name: 'commerce-binding' },
-    usedBy: { kind: 'serverless-function', name: 'lastorder' }
-  }
-}
-
-describe("Commerce Mock tests", function () {
-  this.timeout(10 * 60 * 1000);
+  this.timeout(3 * 60 * 1000);
   this.slow(5000);
-  let mockHost;
-  let host;
+  const testNamespace = "test"
 
-  after(async function () {
-    // return;
-    await Promise.all(
-      [
-        mocksNamespaceObj,
-        ...commerceObjs,
-        ...lastorderObjs,
-        ...k8s.loadAllYaml(serviceCatalogResources("", "")),
-        serviceBinding,
-        sbu,
-        tokenRequest
-      ].map((obj) =>
-        k8sDynamicApi.delete(
-          obj,
-          "true",
-          undefined,
-          undefined,
-          undefined,
-          "Foreground" // namespaces seem to ignore this
-        )
-      )
-    ).catch((e) => {
-      expect(e.body.reason).to.be.equal('NotFound')
-    });
-  });
-
-  it("mocks namespace should be created", async function () {
-    k8sApply(k8sDynamicApi, [mocksNamespaceObj]);
-  })
-
-  it("commerce mock and application should be created", async function () {
-    k8sApply(k8sDynamicApi, commerceObjs);
-  });
-
-  it("lastorder function should be created", async function () {
-    k8sApply(k8sDynamicApi, lastorderObjs);
-  });
-
-  it("commerce mock should be exposed with VirtualService", async function () {
-    const vs = await waitForVirtualService(watch,'mocks','commerce-mock')
-    mockHost = vs.spec.hosts[0]
-    host = mockHost.split(".").slice(1).join(".");
-  });
-
-  it("commerce-application gateway should be deployed", async function () {
-    this.retries(3);
-    const commerceApplicationGatewayDeployment = await retryPromise(
-      async () => {
-        return k8sAppsApi.readNamespacedDeployment("commerce-application-gateway", "kyma-integration");
-      },
-      10,
-      5000
-    ).catch(expectNoK8sErr);
-
-    expect(
-      commerceApplicationGatewayDeployment.body.spec.template.spec.containers[0].args[6]
-    ).to.match(/^--skipVerify/);
-    commerceApplicationGatewayDeployment.body.spec.template.spec.containers[0].args[6] =
-      "--skipVerify=true";
-
-    await k8sDynamicApi
-      .patch(commerceApplicationGatewayDeployment.body)
-      .catch(expectNoK8sErr);
-
-    const patchedDeployment = await k8sAppsApi.readNamespacedDeployment("commerce-application-gateway", "kyma-integration");
-    expect(
-      patchedDeployment.body.spec.template.spec.containers[0].args[6]).to.equal("--skipVerify=true");
-  });
-
-  it("commerce mock local apis should be available", async function () {
-    await retryPromise(
-      () => axios.get(`https://${mockHost}/local/apis`).catch(expectNoAxiosErr), 30, 3000);
-  })
-
-  it("commerce mock should connect to Kyma", async function () {
-    this.retries(4)
-    const path = `/apis/applicationconnector.kyma-project.io/v1alpha1/namespaces/default/tokenrequests`
-
-    await k8sDynamicApi.delete(tokenRequest).catch(() => { })// Ignore delete error
-    await k8sDynamicApi.create(tokenRequest)
-    const tokenObj = await waitForK8sObject(watch, path, {}, (type, apiObj, watchObj) => {
-      return (watchObj.object.status && watchObj.object.status.state == 'OK' && watchObj.object.status.url)
-    }, 5 * 1000, "Wait for TokenRequest timeout")
-
-    await axios.post(
-      `https://${mockHost}/connection`,
-      {
-        token: tokenObj.status.url,
-        baseUrl: `https://${mockHost}`,
-        insecure: true,
-      },
-      {
-        headers: {
-          "content-type": "application/json",
-        },
-      }
-    ).catch(expectNoAxiosErr);
-  });
-
-  it("Commerce mock should register Websevices API and Events API", async function () {
-    this.retries(3);
-    const remoteApis = await registerAllApis(mockHost, "default", watch, 30 * 1000);
-
-  })
-
-  it("Commerce service classes should be ready and service instances should be created", async function () {
-    const webServicesSC = await waitForServiceClass(watch, "webservices");
-    const eventsSC = await waitForServiceClass(watch, "events");
-
-    const webServicesSCExternalName = webServicesSC.spec.externalName;
-    const eventsSCExternalName = eventsSC.spec.externalName;
-    await sleep(5000); // Some issue with creating service instances when service class doesn't have service plans yet
-    const serviceCatalogObjs = k8s.loadAllYaml(
-      serviceCatalogResources(webServicesSCExternalName, eventsSCExternalName)
-    );
-    await retryPromise(() => k8sApply(k8sDynamicApi, serviceCatalogObjs, false), 5, 2000);
-  });
-
-  it("Commerce Webservices API and Events service instances should be ready", async function () {
-    await waitForServiceInstance(watch, 'commerce-webservices');
-    await waitForServiceInstance(watch, 'commerce-events');
-  });
-
-  it("service binding should be ready", async function () {
-    k8sApply(k8sDynamicApi,[serviceBinding]);
-    await waitForServiceBinding(watch, 'commerce-binding');
-  });
-
-  it("service binding usage for function should be ready", async function () {
-    k8sDynamicApi.create(sbu);
-    const path = '/apis/servicecatalog.kyma-project.io/v1alpha1/namespaces/default/servicebindingusages';
-    await waitForK8sObject(watch, path, {}, (type, apiObj, watchObj) => {
-      if (watchObj.object.metadata.name == 'commerce-lastorder-sbu' && watchObj.object.status.conditions) {
-        return watchObj.object.status.conditions.some((c) => (c.type == 'Ready' && c.status == 'True'))
-      }
-      return false;
-    }, 90 * 1000, "Waiting for ServiceBindingUsage to be ready timeout");
+  it("CommerceMock test fixture should be ready", async function () {
+    await ensureCommerceMockTestFixture("mocks", testNamespace);
   });
 
   it("function should reach Commerce mock API through app gateway", async function () {
-    let res = await retryPromise(() => axios.post(`https://lastorder.${host}`, { orderCode: "123" }), 10, 2000);
-    expect(res.data).to.have.nested.property("order.totalPriceWithTax.value", 100);
+    this.timeout(60 * 1000);
+    await checkAppGatewayResponse()
   })
 
   it("order.created.v1 event should trigger the lastorder function", async function () {
-    await sendEventAndCheckResponse(mockHost, host);
+    this.timeout(60 * 1000);
+    await sendEventAndCheckResponse();
   });
-});
 
+  it("Test namespaces should be deleted", async function () {
+    await cleanMockTestFixture("mocks", testNamespace);
+  });
 
-async function sendEventAndCheckResponse(mockHost, host) {
-  await retryPromise(
-    async () => {
-      await axios.post(
-        `https://${mockHost}/events`,
-        {
-          "event-type": "order.created",
-          "event-type-version": "v1",
-          "event-time": "2020-09-28T14:47:16.491Z",
-          data: { orderCode: "567" },
-          "event-tracing": true,
-        },
-        {
-          headers: {
-            "content-type": "application/json",
-          },
-        }
-      ).catch(e => {
-        console.dir({ status: e.response.status, data: e.response.data });
-        throw e
-      });
-
-      await sleep(500);
-
-      return axios.get(`https://lastorder.${host}`).then((res) => {
-        expect(res.data).to.have.nested.property("event.ce-type", 'order.created');
-        expect(res.data).to.have.nested.property("event.ce-source", 'commerce');
-        expect(res.data).to.have.nested.property("event.ce-eventtypeversion", 'v1');
-        expect(res.data).to.have.nested.property("event.ce-specversion", "1.0");
-        expect(res.data).to.have.nested.property("event.ce-id");
-        expect(res.data).to.have.nested.property("event.ce-time");
-        return res;
-      });
-    },
-    30,
-    2 * 1000
-  ).catch(expectNoAxiosErr);
-}
-
-async function registerAllApis(mockHost, namespace, watch, timeout = 60 * 1000) {
-  const localApis = await axios.get(`https://${mockHost}/local/apis`).catch(expectNoAxiosErr);
-  for (let api of localApis.data) {
-    await retryPromise(async () => {
-      await axios.post(`https://${mockHost}/local/apis/${api.id}/register`, {},
-        {
-          headers: {
-            "content-type": "application/json",
-            origin: `https://${mockHost}`,
-          },
-        }
-      ).catch(expectNoAxiosErr)
-    }, 3, 5000)
-  }
-  const remoteApis = await axios.get(`https://${mockHost}/remote/apis`).catch(expectNoAxiosErr);
-  expect(remoteApis.data).to.have.lengthOf.at.least(2)
-  return remoteApis;
-}
+})
--- tests/fast-integration/test/fixtures/getting-started-guides/index.js
+++ tests/fast-integration/test/fixtures/getting-started-guides/index.js
@@ -1,3 +1,4 @@
+const k8s = require("@kubernetes/client-node");
 const fs = require("fs");
 const path = require("path");
 
@@ -33,15 +34,150 @@ const serviceInstanceYaml = fs.readFileSync(
   }
 );
 
-const xfMocksYaml = fs.readFileSync(path.join(__dirname, "./xf-mocks.yaml"), {
-  encoding: "utf8",
+
+const { expect, config } = require("chai");
+config.truncateThreshold = 0; // more verbose errors
+
+const {
+  retryPromise,
+  expectNoAxiosErr,
+  waitForK8sObject,
+  waitForVirtualService,
+  waitForServiceBinding,
+  waitForServiceInstance,
+  k8sApply,
+  deleteAllK8sResources,
+  waitForServiceBindingUsage,
+  deleteNamespaces,
+} = require("../../../utils");
+
+const https = require("https");
+const axios = require("axios").default;
+
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false, // curl -k
 });
+axios.defaults.httpsAgent = httpsAgent;
+
+
+const ordersServiceNamespaceObj = k8s.loadYaml(ordersServiceNamespaceYaml);
+const ordersServiceMicroserviceObj = k8s.loadAllYaml(
+  ordersServiceMicroserviceYaml
+);
+const addonServiceBindingServiceInstanceObjs = k8s.loadAllYaml(
+  addonServiceBindingServiceInstanceYaml
+);
+const sbuObj = k8s.loadYaml(sbuYaml);
+
+const orderService = "orders-service";
+
+const order = {
+  orderCode: "762727210",
+  consignmentCode: "76272725",
+  consignmentStatus: "PICKUP_COMPLETE",
+};
+
+
+async function verifyOrderPersisted() {
+  const virtualService = await waitForVirtualService(orderService, orderService);
+  const serviceDomain = await virtualService.spec.hosts[0];
+
+  await createOrder(serviceDomain, order);
+
+  await getOrders(serviceDomain, (resp) => {
+    expect(resp).to.be.an("Array").of.length(1);
+    expect(resp[0]).to.deep.eq(order);
+  });
+
+  await deleteAllK8sResources('/api/v1/namespaces/orders-service/pods', { labelSelector: `app=${orderService}` });
+
+  await getOrders(
+    serviceDomain,
+    (resp) => {
+      expect(resp).to.be.an("Array").of.length(1);
+      expect(resp[0]).to.deep.eq(order);
+    },
+    30, // longer, because the pod has just been killed and it needs to start again
+    2000
+  );
+
+  // https://kyma-project.io/docs/root/getting-started/#getting-started-connect-an-external-application
+  // This is covered by commerce-mock.js test
+
+}
+
+
+async function getOrders(
+  serviceDomain,
+  expectFn,
+  retriesLeft = 20,
+  interval = 2000
+) {
+  return await retryPromise(
+    async () => {
+      return axios.get(`https://${serviceDomain}/orders`).then((res) => {
+        expectFn(res.data);
+        return res;
+      });
+    },
+    retriesLeft,
+    interval
+  ).catch(expectNoAxiosErr);
+}
+
+async function createOrder(serviceDomain, order) {
+  return await retryPromise(
+    async () => {
+      return axios.post(`https://${serviceDomain}/orders`, order, {
+        headers: {
+          "Cache-Control": "no-cache",
+        },
+      });
+    },
+    10,
+    3000
+  ).catch(expectNoAxiosErr);
+}
+
+async function ensureGettingStartedTestFixture() {
+  await k8sApply([ordersServiceNamespaceObj]);
+  await k8sApply(ordersServiceMicroserviceObj, orderService);
+  await k8sApply(addonServiceBindingServiceInstanceObjs, orderService);
+  const apiRulePath = `/apis/gateway.kyma-project.io/v1alpha1/namespaces/${orderService}/apirules`
+  await waitForK8sObject(apiRulePath, {}, (_type, _apiObj, watchObj) => {
+    return (watchObj.object.metadata.name == orderService && watchObj.object.status.APIRuleStatus.code == "OK")
+  }, 10 * 1000, "Waiting for APIRule to be ready timeout")
+  await waitForVirtualService(orderService, orderService);
+  await waitForServiceInstance('redis-service', orderService);
+  await waitForServiceBinding(orderService, orderService);
+  await k8sApply([sbuObj], orderService);
+  await waitForServiceBindingUsage(orderService, orderService);
+  await deleteAllK8sResources('/api/v1/namespaces/orders-service/pods', { labelSelector: `app=${orderService}` });
+
+}
+
+function getResourcePaths(namespace) {
+  return [
+    `/apis/servicecatalog.kyma-project.io/v1alpha1/namespaces/${namespace}/servicebindingusages`,
+    `/apis/servicecatalog.k8s.io/v1beta1/namespaces/${namespace}/servicebindings`,
+    `/apis/servicecatalog.k8s.io/v1beta1/namespaces/${namespace}/serviceinstances`,
+    `/apis/serverless.kyma-project.io/v1alpha1/namespaces/${namespace}/functions`,
+    `/apis/addons.kyma-project.io/v1alpha1/namespaces/${namespace}/addonsconfigurations`,
+    `/apis/gateway.kyma-project.io/v1alpha1/namespaces/${namespace}/apirules`,
+    `/apis/apps/v1/namespaces/${namespace}/deployments`,
+    `/api/v1/namespaces/${namespace}/services`,
+  ]
+}
+
+function cleanGettingStartedTestFixture() {
+  for (let path of getResourcePaths(orderService)) {
+    deleteAllK8sResources(path)
+  }
+  return deleteNamespaces([orderService]);
+}
 
 module.exports = {
-  sbuYaml,
-  ordersServiceNamespaceYaml,
-  ordersServiceMicroserviceYaml,
-  serviceInstanceYaml,
-  addonServiceBindingServiceInstanceYaml,
-  xfMocksYaml,
-};
+  ensureGettingStartedTestFixture,
+  verifyOrderPersisted,
+  cleanGettingStartedTestFixture
+}
