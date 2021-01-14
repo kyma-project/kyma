373a67276a29f751e80f49c743b0caaad5aa4639
Piotr <piotr.bochynski@sap.com>
2021-01-14 12:54:41 +0100

    Extend timeouts for fast integration tests (evaluation profile) (#10335)

--- tests/fast-integration/test/getting-started-guides.js
+++ tests/fast-integration/test/getting-started-guides.js
@@ -4,17 +4,14 @@ const { ensureGettingStartedTestFixture,
 } = require('./fixtures/getting-started-guides')
 
 describe("Getting Started Guide Tests", function () {
-
   this.timeout(3 * 60 * 1000);
   this.slow(5000);
-
 
   it("Getting started guide fixture should be ready", async function () {
     await ensureGettingStartedTestFixture()
   });
 
   it("Order should be persisted and should survive pod restarts (redis storage)", async function () {
-    this.timeout(60 * 1000);
     await verifyOrderPersisted();
   })
 
