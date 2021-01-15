e21dcb1377f5390da14cc53868a9deaec04c6348
Piotr <piotr.bochynski@sap.com>
2021-01-15 08:57:18 +0100

    Fast integration timeouts (#10347)
    * Extend commerce mock connection timeout
    
    * Increase timeouts
    
    * Remove accidental import

--- tests/fast-integration/test/fixtures/commerce-mock/index.js
+++ tests/fast-integration/test/fixtures/commerce-mock/index.js
@@ -213,10 +213,11 @@ async function ensureCommerceMockTestFixture(mockNamespace, targetNamespace) {
   const mockHost = vs.spec.hosts[0]
   await patchAppGatewayDeployment();
   await retryPromise(
-    () => axios.get(`https://${mockHost}/local/apis`).catch(expectNoAxiosErr), 30, 3000);
+    () => axios.get(`https://${mockHost}/local/apis`)
+      .catch(() => { throw new Exception("Commerce mock local API not available - timeout") }), 40, 3000);
 
-  await retryPromise(() => connectMock(mockHost, targetNamespace), 3, 1000);
-  await retryPromise(() => registerAllApis(mockHost), 3, 1000);
+  await retryPromise(() => connectMock(mockHost, targetNamespace), 10, 3000);
+  await retryPromise(() => registerAllApis(mockHost), 10, 3000);
 
   const webServicesSC = await waitForServiceClass("webservices", targetNamespace);
   const eventsSC = await waitForServiceClass("events", targetNamespace);
