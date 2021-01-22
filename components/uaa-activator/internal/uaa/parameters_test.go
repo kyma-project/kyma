d86e2692c61c6b9b403550981b0cf35817dd9522
Jasiak <piotr.jasiak@sap.com>
2021-01-22 08:51:30 +0100

    fix test

--- components/uaa-activator/internal/uaa/parameters_test.go
+++ components/uaa-activator/internal/uaa/parameters_test.go
@@ -87,7 +87,7 @@ func TestParametersBuilder_Generate(t *testing.T) {
 
 func parametersAreEqual(t *testing.T, schema Schema) {
 	roles := []string{"KymaRuntimeDeveloper", "KymaRuntimeNamespaceAdmin"}
-	rolesWithSuffix := []string{"KymaRuntimeDeveloper_uaa_test", "KymaRuntimeNamespaceAdmin_uaa_test"}
+	rolesWithSuffix := []string{"KymaRuntimeDeveloper__uaa_test", "KymaRuntimeNamespaceAdmin__uaa_test"}
 	rolesWithName := []string{"$XSAPPNAME.KymaRuntimeDeveloper", "$XSAPPNAME.KymaRuntimeNamespaceAdmin"}
 	groups := []string{"$XSAPPNAME.runtimeDeveloper", "$XSAPPNAME.runtimeNamespaceAdmin"}
 	scopesGroup := []string{"$XSAPPNAME.email", "$XSAPPNAME.runtimeDeveloper", "$XSAPPNAME.runtimeNamespaceAdmin"}
