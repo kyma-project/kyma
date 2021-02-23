e942ce9d70bafb918eb4592a234d052f26490c82
grischperl <carina.kothe@sap.com>
2021-02-23 16:42:59 +0100

    Add health and ready check for controller-nats

--- components/eventing-controller/cmd/eventing-controller-nats/main.go
+++ components/eventing-controller/cmd/eventing-controller-nats/main.go
@@ -12,6 +12,7 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	eventingv1alpha1 "github.com/kyma-project/kyma/components/eventing-controller/api/v1alpha1"
@@ -24,10 +25,12 @@ func main() {
 	setupLog := ctrl.Log.WithName("setup")
 
 	var metricsAddr string
+	var probeAddr string
 	var enableDebugLogs bool
 	var maxReconnects int
 	var reconnectWait time.Duration
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableDebugLogs, "enable-debug-logs", false, "Enable debug logs.")
 	flag.IntVar(&maxReconnects, "max-reconnects", 10, "Maximum number of reconnect attempts.")
 	flag.DurationVar(&reconnectWait, "reconnect-wait", time.Second, "Wait time between reconnect attempts.")
@@ -51,9 +54,10 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(enableDebugLogs)))
 	mgr, err := ctrl.NewManager(k8sConfig, ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		Port:               9443,
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		HealthProbeBindAddress: probeAddr,
+		Port:                   9443,
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -76,6 +80,15 @@ func main() {
 	).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to setup the NATS Subscription controller")
 		cancel()
+		os.Exit(1)
+	}
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
 		os.Exit(1)
 	}
 
