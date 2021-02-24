9d649d5e73f3e1b79d6892f9dec429ae39f6e8fc
Marco Bebway <marco.bebway@sap.com>
2021-02-24 12:47:49 +0100

    Reuse the same cloudevents client while dispatching

--- components/eventing-controller/pkg/handlers/nats.go
+++ components/eventing-controller/pkg/handlers/nats.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"strings"
 	"time"
+
+	"k8s.io/apimachinery/pkg/types"
+
+	cev2 "github.com/cloudevents/sdk-go/v2"
+	cev2event "github.com/cloudevents/sdk-go/v2/event"
 
 	"github.com/go-logr/logr"
 	"github.com/kyma-project/kyma/components/eventing-controller/pkg/env"
 	"github.com/nats-io/nats.go"
 	"github.com/pkg/errors"
-
-	cloudevents "github.com/cloudevents/sdk-go/v2"
-	ceclient "github.com/cloudevents/sdk-go/v2/client"
-	cev2event "github.com/cloudevents/sdk-go/v2/event"
-
-	"k8s.io/apimachinery/pkg/types"
 
 	eventingv1alpha1 "github.com/kyma-project/kyma/components/eventing-controller/api/v1alpha1"
 	"github.com/kyma-project/kyma/components/eventing-controller/pkg/handlers/eventtype"
@@ -32,10 +32,15 @@ type NatsInterface interface {
 }
 
 type Nats struct {
-	Connection    *nats.Conn
-	Subscriptions map[string]*nats.Subscription
-	Log           logr.Logger
-	Config        env.NatsConfig
+	config        env.NatsConfig
+	log           logr.Logger
+	client        cev2.Client
+	connection    *nats.Conn
+	subscriptions map[string]*nats.Subscription
+}
+
+func NewNats(config env.NatsConfig, log logr.Logger) *Nats {
+	return &Nats{config: config, log: log, subscriptions: make(map[string]*nats.Subscription)}
 }
 
 const (
@@ -44,24 +49,47 @@ const (
 )
 
 // Initialize creates a connection to Nats
-func (n *Nats) Initialize() error {
-	n.Log.Info("Initialize NATS connection")
-	var err error
-	if n.Connection == nil || n.Connection.Status() != nats.CONNECTED {
-		n.Connection, err = nats.Connect(n.Config.Url,
+func (n *Nats) Initialize() (err error) {
+	n.log.Info("Initialize NATS connection")
+	if n.connection == nil || n.connection.Status() != nats.CONNECTED {
+		n.connection, err = nats.Connect(n.config.Url,
 			nats.RetryOnFailedConnect(true),
-			nats.MaxReconnects(n.Config.MaxReconnects),
-			nats.ReconnectWait(n.Config.ReconnectWait))
+			nats.MaxReconnects(n.config.MaxReconnects),
+			nats.ReconnectWait(n.config.ReconnectWait))
 		if err != nil {
 			return errors.Wrapf(err, "failed to connect to Nats")
 		}
-		if n.Connection.Status() != nats.CONNECTED {
-			notConnectedErr := fmt.Errorf("not connected: status: %v", n.Connection.Status())
+		if n.connection.Status() != nats.CONNECTED {
+			notConnectedErr := fmt.Errorf("not connected: status: %v", n.connection.Status())
 			return notConnectedErr
 		}
 	}
-	n.Log.Info(fmt.Sprintf("Successfully connected to Nats: %v", n.Connection.Status()))
+	n.log.Info("Successfully connected to Nats", "status", n.connection.Status())
+
+	if n.client != nil {
+		return nil
+	}
+
+	n.log.Info("Initialize cloudevents client")
+	if n.client, err = newCloudeventClient(n.config); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func newCloudeventClient(config env.NatsConfig) (cev2.Client, error) {
+	transport := &http.Transport{
+		MaxIdleConns:        config.MaxIdleConns,
+		MaxConnsPerHost:     config.MaxConnsPerHost,
+		MaxIdleConnsPerHost: config.MaxIdleConnsPerHost,
+		IdleConnTimeout:     config.IdleConnTimeout,
+	}
+	protocol, err := cev2.NewHTTP(cev2.WithRoundTripper(transport))
+	if err != nil {
+		return nil, err
+	}
+	return cev2.NewClientObserved(protocol, cev2.WithTimeNow(), cev2.WithUUIDs(), cev2.WithTracePropagation)
 }
 
 func (n *Nats) SyncSubscription(sub *eventingv1alpha1.Subscription, cleaner eventtype.Cleaner) error {
@@ -78,18 +106,17 @@ func (n *Nats) SyncSubscription(sub *eventingv1alpha1.Subscription, cleaner even
 		eventType := strings.TrimSpace(filter.EventType.Value)
 		if len(eventType) == 0 {
 			err := nats.ErrBadSubject
-			n.Log.Error(err, "failed to create a Nats subscription")
+			n.log.Error(err, "failed to create a Nats subscription")
 			return err
 		}
 
-		callback := n.getCallback(sub)
+		callback := n.getCallback(sub.Spec.Sink)
 
-		if n.Connection.Status() != nats.CONNECTED {
-			n.Log.Info("connection to Nats", "status",
-				fmt.Sprintf("%v", n.Connection.Status()))
+		if n.connection.Status() != nats.CONNECTED {
+			n.log.Info("connection to Nats", "status", fmt.Sprintf("%v", n.connection.Status()))
 			initializeErr := n.Initialize()
 			if initializeErr != nil {
-				n.Log.Error(initializeErr, "failed to reset NATS connection")
+				n.log.Error(initializeErr, "failed to reset NATS connection")
 				return initializeErr
 			}
 		}
@@ -100,12 +127,12 @@ func (n *Nats) SyncSubscription(sub *eventingv1alpha1.Subscription, cleaner even
 			return err
 		}
 
-		sub, subscribeErr := n.Connection.Subscribe(eventType, callback)
+		sub, subscribeErr := n.connection.Subscribe(eventType, callback)
 		if subscribeErr != nil {
-			n.Log.Error(subscribeErr, "failed to create a Nats subscription")
+			n.log.Error(subscribeErr, "failed to create a Nats subscription")
 			return subscribeErr
 		}
-		n.Subscriptions[fmt.Sprintf("%s.%s", namespacedName.String(), filter.EventType.Value)] = sub
+		n.subscriptions[fmt.Sprintf("%s.%s", namespacedName.String(), filter.EventType.Value)] = sub
 	}
 	return nil
 }
@@ -115,11 +142,11 @@ func (n *Nats) DeleteSubscription(subscription *eventingv1alpha1.Subscription) e
 		Namespace: subscription.Namespace,
 		Name:      subscription.Name,
 	}
-	for k, v := range n.Subscriptions {
+	for k, v := range n.subscriptions {
 		if strings.HasPrefix(k, subNsName.String()) {
-			n.Log.Info("connection status", "status", n.Connection.Status())
+			n.log.Info("connection status", "status", n.connection.Status())
 			// Unsubscribe call to Nats is async hence checking the status of the connection is important
-			if n.Connection.Status() != nats.CONNECTED {
+			if n.connection.Status() != nats.CONNECTED {
 				initializeErr := n.Initialize()
 				if initializeErr != nil {
 					return errors.Wrapf(initializeErr, "can't connect to Nats server")
@@ -129,43 +156,40 @@ func (n *Nats) DeleteSubscription(subscription *eventingv1alpha1.Subscription) e
 			if err != nil {
 				return errors.Wrapf(err, "failed to unsubscribe")
 			}
-			delete(n.Subscriptions, k)
-			n.Log.Info("successfully unsubscribed/deleted subscription")
+			delete(n.subscriptions, k)
+			n.log.Info("successfully unsubscribed/deleted subscription")
 		}
 	}
 	return nil
 }
 
-func (n Nats) getCallback(sub *eventingv1alpha1.Subscription) func(msg *nats.Msg) {
+func (n *Nats) getCallback(sink string) nats.MsgHandler {
 	return func(msg *nats.Msg) {
 		ce, err := convertMsgToCE(msg)
 		if err != nil {
-			n.Log.Error(err, "failed to convert Nats message to CE")
+			n.log.Error(err, "failed to convert Nats message to CE")
 			return
 		}
-		ctx := context.Background()
+
 		// Creating a context with cancellable
-		ctxWithCancel, cancel := context.WithCancel(ctx)
-		// Creating a context with retries
-		ctxWithRetries := cloudevents.ContextWithRetriesExponentialBackoff(ctxWithCancel, period, maxTries)
-		// Creating a context with target
-		ctxWithCE := cloudevents.ContextWithTarget(ctxWithRetries, sub.Spec.Sink)
-		// Cancel later to clean up
+		ctxWithCancel, cancel := context.WithCancel(context.Background())
 		defer cancel()
-		cev2Client, err := ceclient.NewDefault()
-		if err != nil {
-			n.Log.Error(err, "failed to create CE client")
-			return
-		}
-		err = cev2Client.Send(ctxWithCE, *ce)
+
+		// Creating a context with retries
+		ctxWithRetries := cev2.ContextWithRetriesExponentialBackoff(ctxWithCancel, period, maxTries)
+
+		// Creating a context with target
+		ctxWithCE := cev2.ContextWithTarget(ctxWithRetries, sink)
+
+		err = n.client.Send(ctxWithCE, *ce)
 		if err != nil {
 			if !strings.Contains(err.Error(), "200") {
-				n.Log.Error(err, "failed to dispatch event")
+				n.log.Error(err, "failed to dispatch event")
 				return
 			}
 		}
-		n.Log.Info(fmt.Sprintf("Successfully dispatched event id: %s to sink: %s", ce.ID(),
-			sub.Spec.Sink))
+
+		n.log.Info(fmt.Sprintf("Successfully dispatched event id: %s to sink: %s", ce.ID(), sink))
 	}
 }
 
--- components/eventing-controller/pkg/handlers/nats_test.go
+++ components/eventing-controller/pkg/handlers/nats_test.go
@@ -94,13 +94,13 @@ func TestSubscription(t *testing.T) {
 
 	natsURL := natsServer.ClientURL()
 	natsClient := Nats{
-		Subscriptions: make(map[string]*nats.Subscription),
-		Config: env.NatsConfig{
+		subscriptions: make(map[string]*nats.Subscription),
+		config: env.NatsConfig{
 			Url:           natsURL,
 			MaxReconnects: 2,
 			ReconnectWait: time.Second,
 		},
-		Log: ctrl.Log.WithName("reconciler").WithName("Subscription"),
+		log: ctrl.Log.WithName("reconciler").WithName("Subscription"),
 	}
 	err := natsClient.Initialize()
 	if err != nil {
@@ -179,7 +179,7 @@ func SendEvent(natsClient *Nats, data string) error {
 	eventType := eventingtesting.EventType
 	eventTime := time.Now().Format(time.RFC3339)
 	sampleEvent := NewNatsMessagePayload(data, "id", eventingtesting.EventSource, eventTime, eventType)
-	return natsClient.Connection.Publish(eventType, []byte(sampleEvent))
+	return natsClient.connection.Publish(eventType, []byte(sampleEvent))
 }
 
 func NewNatsMessagePayload(data, id, source, eventTime, eventType string) string {
