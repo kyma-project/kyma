7eea4fcc78de21d8fdd2d1ac99e30d11646045eb
pbochynski <p.bochynski@gmail.com>
2021-03-01 15:14:27 +0100

    Refactoring after review

--- tests/fast-integration/installer/chart-downloader.js
+++ tests/fast-integration/installer/chart-downloader.js
@@ -19,30 +19,31 @@ async function downloadFile(url, filename) {
 }
 
 /**
- * Downloads Kyma branch into temporary folder as zip, extracts charts, and returns full path to resources folder
+ * Downloads Kyma sources into temporary folder as zip, extracts charts, and returns full path to resources folder
  * @param {Object} options Optional parameters
  * @param {string} options.repo Github repository with Kyma charts, default: kyma-project/kyma
- * @param {string} options.source Branch that should be downloaded from repository, default: master
+ * @param {string} options.source Source branch that should be downloaded from repository, default: master
  * @return {string} Path to the resources folder
  */
 async function downloadCharts(options) {
-  options = options || {};
-  const repo = options.repo || 'kyma-project/kyma'
-  const branch = options.source || 'master';
-  const dir = 'tmp-' + branch;
+  const {
+    repo = 'kyma-project/kyma',
+    source = 'master'
+  } = options;
+  const dir = 'tmp-' + source;
   const resourcesPath = join(dir, 'resources')
-  debug("Resource path:",resourcesPath)
+  debug("Resource path:", resourcesPath)
   if (fs.existsSync(resourcesPath)) {
     debug("The resources path already exists - download skipped")
     return resourcesPath;
   }
-  if (!fs.existsSync(dir)){
+  if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
 
-  const zipFile = join(dir, `${branch}.zip`);
-  const url = `https://codeload.github.com/${repo}/zip/${branch}`;
-  debug("Downloading Kyma charts from ",url);
+  const zipFile = join(dir, `${source}.zip`);
+  const url = `https://codeload.github.com/${repo}/zip/${source}`;
+  debug("Downloading Kyma charts from ", url);
   await downloadFile(url, zipFile)
   debug("Kyma charts downloaded");
   const zip = new AdmZip(zipFile);
