c682d6366c7d8bfbda092888f1287df1e13d008b
Marco Bebway <marco.bebway@sap.com>
2021-03-15 11:24:35 +0100

    Add an upgrade test from EventMesh to the new Kyma eventing (#10867)
    Co-authored-by: Sayan Hazra <sayan.hazra@sap.com>

---
+++ tests/end-to-end/upgrade/pkg/tests/eventmesh/subscription/api/v1alpha1/subscription_types.go
@@ -0,0 +1,109 @@
+// TODO remove package v1alpha1 when Kyma is fully integrated with the new Eventing solution https://github.com/kyma-project/kyma/issues/10866
+// package v1alpha1 is copied from https://github.com/kyma-project/kyma/tree/release-1.19/components/eventing-controller/api/v1alpha1
+// because we cannot upgrade Kyma version to 1.19.0 because of Kubernetes version collision with Knative 0.12.0
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Webhook defines the Webhook called by an active subscription in BEB
+type WebhookAuth struct {
+	Type         string   `json:"type,omitempty"`
+	GrantType    string   `json:"grantType"`
+	ClientId     string   `json:"clientId"`
+	ClientSecret string   `json:"clientSecret"`
+	TokenUrl     string   `json:"tokenUrl"`
+	Scope        []string `json:"scope,omitempty"`
+}
+
+// ProtocolSettings defines the CE protocol setting specification implementation
+type ProtocolSettings struct {
+	ContentMode     string       `json:"contentMode,omitempty"`
+	ExemptHandshake bool         `json:"exemptHandshake,omitempty"`
+	Qos             string       `json:"qos,omitempty"`
+	WebhookAuth     *WebhookAuth `json:"webhookAuth,omitempty"`
+}
+
+const (
+	ProtocolSettingsContentModeBinary     string = "BINARY"
+	ProtocolSettingsContentModeStructured string = "STRUCTURED"
+)
+
+// Filter defines the CE filter element
+type Filter struct {
+	Type     string `json:"type,omitempty"`
+	Property string `json:"property"`
+	Value    string `json:"value"`
+}
+
+// BebFilter defines the BEB filter element as a combination of two CE filter elements
+type BebFilter struct {
+	EventSource *Filter `json:"eventSource"`
+	EventType   *Filter `json:"eventType"`
+}
+
+// BebFilters defines the list of BEB filters
+type BebFilters struct {
+	Dialect string       `json:"dialect,omitempty"`
+	Filters []*BebFilter `json:"filters"`
+}
+
+// SubscriptionSpec defines the desired state of Subscription
+type SubscriptionSpec struct {
+	// ID is the unique identifier of Subscription, read-only.
+	ID               string            `json:"id,omitempty"`
+	Protocol         string            `json:"protocol"`
+	ProtocolSettings *ProtocolSettings `json:"protocolsettings"`
+	Sink             string            `json:"sink"`
+	Filter           *BebFilters       `json:"filter"`
+}
+
+type EmsSubscriptionStatus struct {
+	SubscriptionStatus       string `json:"subscriptionStatus,omitempty"`
+	SubscriptionStatusReason string `json:"subscriptionStatusReason,omitempty"`
+	LastSuccessfulDelivery   string `json:"lastSuccessfulDelivery,omitempty"`
+	LastFailedDelivery       string `json:"lastFailedDelivery,omitempty"`
+	LastFailedDeliveryReason string `json:"lastFailedDeliveryReason,omitempty"`
+}
+
+// +kubebuilder:subresource:status
+
+// SubscriptionStatus defines the observed state of Subscription
+type SubscriptionStatus struct {
+	Conditions            []Condition           `json:"conditions,omitempty"`
+	Ready                 bool                  `json:"ready"`
+	Ev2hash               int64                 `json:"ev2hash,omitempty"`
+	Emshash               int64                 `json:"emshash,omitempty"`
+	ExternalSink          string                `json:"externalSink,omitempty"`
+	FailedActivation      string                `json:"failedActivation,omitempty"`
+	APIRuleName           string                `json:"apiRuleName,omitempty"`
+	EmsSubscriptionStatus EmsSubscriptionStatus `json:"emsSubscriptionStatus,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:subresource:status
+
+// Subscription is the Schema for the subscriptions API
+type Subscription struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SubscriptionSpec   `json:"spec,omitempty"`
+	Status SubscriptionStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// SubscriptionList contains a list of Subscription
+type SubscriptionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Subscription `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Subscription{}, &SubscriptionList{})
+}
