91d9ce3b64070c9411d40ecdc3e823f2c7f49948
ralikio <74771103+ralikio@users.noreply.github.com>
2021-03-17 23:02:45 +0100

    Review corrections

--- resources/application-connector/charts/connector-service/files/certs-backup.sh
+++ resources/application-connector/charts/connector-service/files/certs-backup.sh
@@ -6,15 +6,15 @@ backup() {
   secretName="$2"
   backupSecretName="$secretName-backup"
 
+  echo "Removing old backup $backupSecretName"
   kubectl delete secret "$backupSecretName" -n="$namespace" --ignore-not-found
 
-  echo "Checking existence of $secretName"
   secret=$(kubectl -n "$namespace" get secret "$secretName" --ignore-not-found)
   if [ -n "$secret" ]
   then
     echo "$secretName exists, backing up to $backupSecretName"
-    kubectl get secret "$secretName" -n "$namespace" -oyaml | sed "s/name:.*$/name: $backupSecretName/" | kubectl apply -n "$namespace" -f -
-    kubectl delete secret "$backupSecretName" -n="$namespace" --ignore-not-found
+
+    kubectl get secret "$secretName" -n "$namespace" -ojson | jq "{apiVersion, data, kind, type, metadata: {name: \"$backupSecretName\", namespace: .metadata.namespace}}" | kubectl apply -n "$namespace" -f -
   fi
 }
 
--- resources/application-connector/charts/connector-service/files/certs-restore.sh
+++ resources/application-connector/charts/connector-service/files/certs-restore.sh
@@ -6,14 +6,22 @@ restore() {
   secretName="$2"
   backupSecretName="$secretName-backup"
 
+  secret=$(kubectl -n "$namespace" get secret "$secretName" --ignore-not-found)
+  if [ -z "$secret" ]
+  then
+    echo "No default secret, creating $secretName"
+    kubectl create secret generic "$secretName" -n "$namespace"
+  fi
+
   echo "Restoring $backupSecretName"
   secret=$(kubectl -n "$namespace" get secret "$backupSecretName" --ignore-not-found)
   if [ -n "$secret" ]
   then
-    echo "Removing $secretName"
-    kubectl delete secret "$secretName" -n="$namespace" --ignore-not-found
     echo "$backupSecretName exists, reverting $secretName to $backupSecretName"
-    kubectl get secret "$backupSecretName" -n "$namespace" -oyaml | sed "s/name:.*$/name: $secretName/" | kubectl apply -n "$namespace" -f -
+    kubectl patch secret -n "$namespace" "$secretName" --type merge -p "$(kubectl get secret "$backupSecretName" -n "$namespace" -ojson | jq "{data}")"
+
+    echo "Removing $backupSecretName"
+    kubectl delete secret "$backupSecretName" -n="$namespace" --ignore-not-found
   else
     echo "$backupSecretName does not exist. Aborting..."
   fi
