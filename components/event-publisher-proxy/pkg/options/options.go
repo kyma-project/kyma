8adb121067e38204b57fdd358c46accc53512150
Marco Bebway <marco.bebway@sap.com>
2021-03-18 18:13:20 +0100

    Add basic metrics for Eventing publishers

--- components/event-publisher-proxy/pkg/options/options.go
+++ components/event-publisher-proxy/pkg/options/options.go
@@ -4,13 +4,17 @@ import "flag"
 
 type Options struct {
 	MaxRequestSize int64
+	MetricsAddress string
 }
 
 func ParseArgs() *Options {
-	maxRequestSize := flag.Int64("maxRequestSize", 65536, "The maximum request size in bytes")
+	maxRequestSize := flag.Int64("max-request-Size", 65536, "The maximum request size in bytes.")
+	metricsAddress := flag.String("metrics-addr", ":9090", "The address the metric endpoint binds to.")
+
 	flag.Parse()
 
 	return &Options{
 		MaxRequestSize: *maxRequestSize,
+		MetricsAddress: *metricsAddress,
 	}
 }
