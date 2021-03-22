92015c9399f25526974a46863578c09cdf39cce3
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2021-03-22 15:03:05 +0100

    remove external-solution-test

--- tests/end-to-end/external-solution-integration/internal/scenario/send_and_check_event/state.go
+++
@@ -1,9 +0,0 @@
-package send_and_check_event
-
-import (
-	"github.com/kyma-project/kyma/tests/end-to-end/external-solution-integration/internal/scenario"
-)
-
-type state struct {
-	scenario.E2EState
-}
