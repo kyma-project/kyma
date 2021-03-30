58ba9bde16268d3d84ca9ecf2f6d28fa01ea671f
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2021-03-30 17:04:51 +0200

    remove external solution test (#10945)

--- tests/end-to-end/external-solution-integration/pkg/helpers/serverless.go
+++
@@ -1,8 +0,0 @@
-package helpers
-
-const (
-	FunctionPort             = 80
-	FunctionPayload          = "payload"
-	KymaIntegrationNamespace = "kyma-integration"
-	DefaultBrokerName        = "default"
-)
