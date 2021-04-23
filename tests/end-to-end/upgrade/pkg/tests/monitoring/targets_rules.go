c0278c39d0863647e784b87533b5107ea8066059
Rafał Jankowski <rafal.jankowski.93@gmail.com>
2021-04-23 10:08:57 +0200

    Remove node10 add node14 resources tests -resources, tests, cbs (#11121)

--- tests/end-to-end/upgrade/pkg/tests/monitoring/targets_rules.go
+++ tests/end-to-end/upgrade/pkg/tests/monitoring/targets_rules.go
@@ -226,7 +226,7 @@ func shouldIgnoreTarget(target prom.TargetLabels) bool {
 		"-testsuite-",
 		"test",
 		"nodejs12-",
-		"nodejs10-",
+		"nodejs14-",
 		"upgrade",
 		// Ignore the pods created by jobs which are executed after installation of control-plane.
 		"compass-migration",
