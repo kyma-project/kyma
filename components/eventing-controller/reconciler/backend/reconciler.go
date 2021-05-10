96c5831291e0b53b7eca36e2d8c8b0a7cebd04d4
pxsalehi <pxsalehi@gmail.com>
2021-05-10 10:53:32 +0200

    minor cleanups

--- components/eventing-controller/reconciler/backend/reconciler.go
+++ components/eventing-controller/reconciler/backend/reconciler.go
@@ -4,17 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strconv"
-
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	"github.com/kyma-project/kyma/components/eventing-controller/pkg/object"
-
 	"github.com/pkg/errors"
-
-	"k8s.io/apimachinery/pkg/util/intstr"
 
 	appsv1 "k8s.io/api/apps/v1"
 
@@ -38,32 +33,12 @@ const (
 
 	PublisherNamespace       = "kyma-system"
 	PublisherName            = "eventing-publisher-proxy"
-	ServiceAccountName       = "eventing-event-publisher-nats"
 	BackendCRLabelKey        = "kyma-project.io/eventing"
 	BackendCRLabelValue      = "backend"
 	AppLabelKey              = "app.kubernetes.io/name"
 	AppLabelValue            = PublisherName
-	InstanceLabelKey         = "app.kubernetes.io/instance"
-	InstanceLabelValue       = "eventing"
-	DashboardLabelKey        = "kyma-project.io/dashboard"
-	DashboardLabelValue      = "eventing"
-	PublisherReplicas        = 1
-	PublisherImage           = "eu.gcr.io/kyma-project/event-publisher-proxy:88360eed"
-	PublisherPortName        = "http"
-	PublisherPortNum         = int32(8080)
-	PublisherMetricsPortName = "http-metrics"
-	PublisherMetricsPortNum  = int32(9090)
-
-	LivenessInitialDelaySecs = int32(5)
-	LivenessTimeoutSecs      = int32(1)
-	LivenessPeriodSecs       = int32(2)
-	BEBNamespacePrefix       = "/"
 
 	TokenEndpointFormat = "%s?grant_type=%s&response_type=token"
-)
-
-var (
-	TerminationGracePeriodSeconds = int64(30)
 )
 
 type BackendReconciler struct {
@@ -280,7 +255,7 @@ func getSecretForPublisher(secret *v1.Secret) (*v1.Secret, error) {
 			Name:      PublisherName,
 			Namespace: PublisherNamespace,
 			Labels: map[string]string{
-				AppLabelKey: PublisherName,
+				AppLabelKey: AppLabelValue,
 			},
 		},
 	}
@@ -380,206 +355,6 @@ func (r *BackendReconciler) CreateOrUpdatePublisherProxy(ctx context.Context, ba
 	return desiredPublisher, nil
 }
 
-func newBEBPublisherDeployment() *appsv1.Deployment {
-	labels := map[string]string{
-		AppLabelKey:       PublisherName,
-		InstanceLabelKey:  InstanceLabelValue,
-		DashboardLabelKey: DashboardLabelValue,
-	}
-	return &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      PublisherName,
-			Namespace: PublisherNamespace,
-			Labels:    labels,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: intPtr(PublisherReplicas),
-			Selector: metav1.SetAsLabelSelector(labels),
-			Template: v1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:   PublisherName,
-					Labels: labels,
-				},
-				Spec: v1.PodSpec{
-					Containers: []v1.Container{
-						{
-							Name:            PublisherName,
-							Image:           PublisherImage,
-							Ports:           getContainerPorts(),
-							Env:             getBEBEnvVars(),
-							LivenessProbe:   getLivenessProbe(),
-							ReadinessProbe:  getReadinessProbe(),
-							ImagePullPolicy: v1.PullIfNotPresent,
-							SecurityContext: getSecurityContext(),
-						},
-					},
-					RestartPolicy:                 v1.RestartPolicyAlways,
-					ServiceAccountName:            ServiceAccountName,
-					TerminationGracePeriodSeconds: &TerminationGracePeriodSeconds,
-				},
-			},
-		},
-	}
-}
-
-func newNATSPublisherDeployment() *appsv1.Deployment {
-	labels := map[string]string{
-		AppLabelKey:       PublisherName,
-		InstanceLabelKey:  InstanceLabelValue,
-		DashboardLabelKey: DashboardLabelValue,
-	}
-	return &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      PublisherName,
-			Namespace: PublisherNamespace,
-			Labels:    labels,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: intPtr(PublisherReplicas),
-			Selector: metav1.SetAsLabelSelector(labels),
-			Template: v1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:   PublisherName,
-					Labels: labels,
-				},
-				Spec: v1.PodSpec{
-					Containers: []v1.Container{
-						{
-							Name:            PublisherName,
-							Image:           PublisherImage,
-							Ports:           getContainerPorts(),
-							Env:             getNATSEnvVars(),
-							LivenessProbe:   getLivenessProbe(),
-							ReadinessProbe:  getReadinessProbe(),
-							ImagePullPolicy: v1.PullIfNotPresent,
-							SecurityContext: getSecurityContext(),
-						},
-					},
-					RestartPolicy:                 v1.RestartPolicyAlways,
-					ServiceAccountName:            ServiceAccountName,
-					TerminationGracePeriodSeconds: &TerminationGracePeriodSeconds,
-				},
-			},
-		},
-		Status: appsv1.DeploymentStatus{},
-	}
-}
-
-func getSecurityContext() *v1.SecurityContext {
-	return &v1.SecurityContext{
-		Privileged:               boolPtr(false),
-		AllowPrivilegeEscalation: boolPtr(false),
-	}
-}
-
-func getReadinessProbe() *v1.Probe {
-	return &v1.Probe{
-		Handler: v1.Handler{
-			HTTPGet: &v1.HTTPGetAction{
-				Path:   "/readyz",
-				Port:   intstr.FromInt(8080),
-				Scheme: v1.URISchemeHTTP,
-			},
-		},
-		FailureThreshold: 3,
-	}
-}
-
-func getLivenessProbe() *v1.Probe {
-	return &v1.Probe{
-		Handler: v1.Handler{
-			HTTPGet: &v1.HTTPGetAction{
-				Path:   "/healthz",
-				Port:   intstr.FromInt(8080),
-				Scheme: v1.URISchemeHTTP,
-			},
-		},
-		InitialDelaySeconds: LivenessInitialDelaySecs,
-		TimeoutSeconds:      LivenessTimeoutSecs,
-		PeriodSeconds:       LivenessPeriodSecs,
-		SuccessThreshold:    1,
-		FailureThreshold:    3,
-	}
-}
-
-func getContainerPorts() []v1.ContainerPort {
-	return []v1.ContainerPort{
-		{
-			Name:          PublisherPortName,
-			ContainerPort: PublisherPortNum,
-		},
-		{
-			Name:          PublisherMetricsPortName,
-			ContainerPort: PublisherMetricsPortNum,
-		},
-	}
-}
-
-func getBEBEnvVars() []v1.EnvVar {
-	return []v1.EnvVar{
-		{Name: "BACKEND", Value: "beb"},
-		{Name: "PORT", Value: strconv.Itoa(int(PublisherPortNum))},
-		{Name: "REQUEST_TIMEOUT", Value: "5s"},
-		{Name: "EVENT_TYPE_PREFIX", Value: "sap.kyma.custom"},
-		{
-			Name: "CLIENT_ID",
-			ValueFrom: &v1.EnvVarSource{
-				SecretKeyRef: &v1.SecretKeySelector{
-					LocalObjectReference: v1.LocalObjectReference{Name: PublisherName},
-					Key:                  "client-id",
-				}},
-		},
-		{
-			Name: "CLIENT_SECRET",
-			ValueFrom: &v1.EnvVarSource{
-				SecretKeyRef: &v1.SecretKeySelector{
-					LocalObjectReference: v1.LocalObjectReference{Name: PublisherName},
-					Key:                  "client-secret",
-				}},
-		},
-		{
-			Name: "TOKEN_ENDPOINT",
-			ValueFrom: &v1.EnvVarSource{
-				SecretKeyRef: &v1.SecretKeySelector{
-					LocalObjectReference: v1.LocalObjectReference{Name: PublisherName},
-					Key:                  "token-endpoint",
-				}},
-		},
-		{
-			Name: "EMS_PUBLISH_URL",
-			ValueFrom: &v1.EnvVarSource{
-				SecretKeyRef: &v1.SecretKeySelector{
-					LocalObjectReference: v1.LocalObjectReference{Name: PublisherName},
-					Key:                  "ems-publish-url",
-				}},
-		},
-		{
-			Name: "BEB_NAMESPACE_VALUE",
-			ValueFrom: &v1.EnvVarSource{
-				SecretKeyRef: &v1.SecretKeySelector{
-					LocalObjectReference: v1.LocalObjectReference{Name: PublisherName},
-					Key:                  "beb-namespace",
-				}},
-		},
-		{
-			Name:  "BEB_NAMESPACE",
-			Value: fmt.Sprintf("%s$(BEB_NAMESPACE_VALUE)", BEBNamespacePrefix),
-		},
-	}
-}
-
-func getNATSEnvVars() []v1.EnvVar {
-	return []v1.EnvVar{
-		{Name: "BACKEND", Value: "nats"},
-		{Name: "PORT", Value: strconv.Itoa(int(PublisherPortNum))},
-		{Name: "NATS_URL", Value: "eventing-nats.kyma-system.svc.cluster.local"},
-		{Name: "REQUEST_TIMEOUT", Value: "5s"},
-		{Name: "LEGACY_NAMESPACE", Value: "kyma"},
-		{Name: "LEGACY_EVENT_TYPE_PREFIX", Value: "sap.kyma.custom"},
-		{Name: "EVENT_TYPE_PREFIX", Value: "sap.kyma.custom"},
-	}
-}
-
 func (r *BackendReconciler) CreateOrUpdateBackendCR(ctx context.Context, backend eventingv1alpha1.BackendType) (*eventingv1alpha1.EventingBackend, error) {
 	var currentBackend eventingv1alpha1.EventingBackend
 	defaultEventingBackend := types.NamespacedName{
@@ -635,14 +410,6 @@ func (r *BackendReconciler) CreateOrUpdateBackendCR(ctx context.Context, backend
 	}
 
 	return &desiredBackend, nil
-}
-
-func boolPtr(b bool) *bool {
-	return &b
-}
-
-func intPtr(i int32) *int32 {
-	return &i
 }
 
 func getDeploymentMapper() handler.EventHandler {
