3c0b4305ce8d54726574fabda2204e7e118b4988
franpog859 <franciszek.pog@gmail.com>
2021-05-28 09:07:48 +0200

    Adjust the test to the newer eventing

--- tests/fast-integration/application-connectivity-2-test/index.js
+++ tests/fast-integration/application-connectivity-2-test/index.js
@@ -1,4 +1,12 @@
 const installer = require('../installer')
+const axios = require("axios");
+const https = require("https");
+const { expect } = require("chai");
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false, // curl -k
+});
+axios.defaults.httpsAgent = httpsAgent;
+
 const {
     ensureCommerceMockLocalTestFixture,
     checkAppGatewayResponse,
@@ -8,6 +16,14 @@ const {
 const {
     printRestartReport,
     getContainerRestartsForAllNamespaces,
+    eventingSubscription,
+    waitForFunction,
+    waitForSubscription,
+    k8sApply,
+    genRandom,
+    retryPromise,
+    debug,
+    waitForVirtualService,
 } = require("../utils");
 
 
@@ -33,6 +49,37 @@ describe("Kyma Application Connectivity 2.0 tests", function () {
             console.dir(err); // first error is logged
             return ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralApplicationGateway);
         });
+    });
+
+    it("lastorder function should be ready", async function () {
+        await waitForFunction("lastorder", testNamespace);
+    });
+
+    it("In-cluster event subscription should be ready", async function () {
+        await k8sApply([eventingSubscription(
+            `sap.kyma.custom.inapp.order.received.v1`,
+            `http://lastorder.${testNamespace}.svc.cluster.local`,
+            "order-received",
+            testNamespace)]);
+        await waitForSubscription("order-received", testNamespace);
+        await waitForSubscription("order-created", testNamespace);
+    });
+
+    it("in-cluster event should be delivered", async function () {
+        const eventId = "event-"+genRandom(5);
+        const vs = await waitForVirtualService(testNamespace, "lastorder");
+        const mockHost = vs.spec.hosts[0];
+
+        // send event using function query parameter send=true
+        await retryPromise(() => axios.post(`https://${mockHost}`, { id: eventId }, {params:{send:true}}), 10, 1000)
+        // verify if event was received using function query parameter inappevent=eventId
+        await retryPromise(async () => {
+            debug("Waiting for event: ",eventId);
+            let response = await axios.get(`https://${mockHost}`, { params: { inappevent: eventId } })
+            expect(response).to.have.nested.property("data.id", eventId, "The same event id expected in the result");
+            expect(response).to.have.nested.property("data.shipped", true, "Order should have property shipped");
+        }, 10, 1000);
+
     });
 
     it("function should reach Commerce mock API through app gateway", async function () {
