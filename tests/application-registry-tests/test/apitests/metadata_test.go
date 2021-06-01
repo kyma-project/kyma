d341cfd9698407eda65d4df69406d778e5886f7f
Przemyslaw Golicz <przemyslaw.golicz@sap.com>
2021-06-01 14:13:27 +0200

    Remove redundant test case after code review.

--- tests/application-registry-tests/test/apitests/metadata_test.go
+++ tests/application-registry-tests/test/apitests/metadata_test.go
@@ -745,32 +745,6 @@ func TestApiMetadata(t *testing.T) {
 			require.True(t, strings.HasPrefix(postedService2.Identifier, identifier2))
 		})
 
-		t.Run("should return bad request 400 when adding service with name that is already used in another service", func(t *testing.T) {
-			// given
-			identifier := testkit.GenerateIdentifier()
-			initialServiceDefinition := prepareServiceDetails(identifier, map[string]string{}).WithAPI(oauthAPI)
-			identifier2 := testkit.GenerateIdentifier()
-			initialServiceDefinition2 := prepareServiceDetails(identifier2, map[string]string{}).WithAPI(oauthAPI)
-
-			postStatusCode, postResponseData, err := metadataServiceClient.CreateService(t, initialServiceDefinition)
-			require.NoError(t, err)
-			require.Equal(t, http.StatusOK, postStatusCode)
-
-			defer func() {
-				// clean up
-				statusCode1, err := metadataServiceClient.DeleteService(t, postResponseData.ID)
-				require.NoError(t, err)
-				require.Equal(t, http.StatusNoContent, statusCode1)
-			}()
-
-			//when
-			postStatusCode2, _, err := metadataServiceClient.CreateService(t, initialServiceDefinition2)
-
-			//then
-			require.NoError(t, err)
-			require.Equal(t, http.StatusBadRequest, postStatusCode2)
-		})
-
 		t.Run("should return bad request 400 when adding service with a name that will be identically normalized to a name used in another service", func(t *testing.T) {
 			// given
 			identifier := testkit.GenerateIdentifier()
