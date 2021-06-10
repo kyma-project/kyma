ec86782f7566122c7a2569e3b597c3203ed63e80
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2021-06-10 15:22:05 +0200

    Central application gateway (#11158)
    * Added sources for central-application-connector
    
    * Added charts for experimental-application-connector
    
    * central-application-gateway chart modified
    
    * Fix import paths
    
    * Compilation fix
    
    * Chart uodated and minor change in the code
    
    * requirements.yaml updated
    
    * Central Application Connector chart updated
    
    * Central Application Connector chart updated plus some cleanup
    
    * Useless files removed
    
    * Compilation fix
    
    * Added information in Application Registry to handle proxying in Central Application Gateway
    
    * Adding cache support for api bundles entries. POC for SKR kyma. Open source case not implemented yet
    
    * add support for both Management Plane mode and OS mode
    
    * modify checlm chart deploymentto support dual runtime mode
    
    * separate servers for KymaOS and Kyma SKR
    
    * Making old unit tests working with new functionality. New units tests are not added yet.
    
    * Repository changed to handle Compass and standalone use cases
    
    * Added information in Application Registry to handle proxying in Central Application Gateway
    
    * Added code for creating handler for Compass and Kyma use cases
    
    * Added tests for Proxy Factory
    
    * Missing file checked in
    
    * Changes in Central Application Gateway chart added
    
    * Unit test fixed
    
    * Unit test fixed
    
    * Imports
    
    * Chart fixed
    
    * Chart fixed
    
    * Unit tests added
    
    * Minor clean up
    
    * Refactor proxy tests
    
    * Readme.dm updated
    
    * Use normalization function (#5)
    
    * Apply suggestions from code review
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * Update components/central-application-gateway/README.md
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * Update components/central-application-gateway/README.md
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * Update components/central-application-gateway/README.md
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * Update components/central-application-gateway/README.md
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * Update components/central-application-gateway/README.md
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * Update components/central-application-gateway/README.md
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * go imports
    
    Co-authored-by: Przemyslaw Golicz <przemyslaw.golicz@sap.com>
    Co-authored-by: Michal Budzyn <michalbudzyn@gmail.com>
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>

---
+++ components/central-application-gateway/internal/proxy/proxy.go
@@ -0,0 +1,208 @@
+package proxy
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/kyma-project/kyma/components/central-application-gateway/internal/metadata/model"
+
+	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/proxyconfig"
+
+	"github.com/kyma-project/kyma/components/central-application-gateway/internal/csrf"
+
+	"github.com/kyma-project/kyma/components/central-application-gateway/internal/httperrors"
+	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/apperrors"
+	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/authorization"
+	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/httpconsts"
+)
+
+type proxy struct {
+	cache                        Cache
+	skipVerify                   bool
+	proxyTimeout                 int
+	authorizationStrategyFactory authorization.StrategyFactory
+	csrfTokenStrategyFactory     csrf.TokenStrategyFactory
+	extractPathFunc              pathExtractorFunc
+	apiExtractor                 APIExtractor
+}
+
+//go:generate mockery --name=APIExtractor
+type APIExtractor interface {
+	Get(identifier model.APIIdentifier) (*model.API, apperrors.AppError)
+}
+
+// Config stores Proxy config
+type Config struct {
+	SkipVerify    bool
+	ProxyTimeout  int
+	Application   string
+	ProxyCacheTTL int
+}
+
+func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	apiIdentifier, path, err := p.extractPath(r.URL.Path)
+	if err != nil {
+		handleErrors(w, err)
+		return
+	}
+
+	r.URL.Path = path
+
+	cacheEntry, err := p.getOrCreateCacheEntry(apiIdentifier)
+	if err != nil {
+		handleErrors(w, err)
+		return
+	}
+
+	newRequest, cancel := p.setRequestTimeout(r)
+	defer cancel()
+
+	err = p.addAuthorization(newRequest, cacheEntry)
+	if err != nil {
+		handleErrors(w, err)
+		return
+	}
+
+	if err := p.addModifyResponseHandler(newRequest, apiIdentifier, cacheEntry, p.createCacheEntry); err != nil {
+		handleErrors(w, err)
+		return
+	}
+
+	cacheEntry.Proxy.ServeHTTP(w, newRequest)
+}
+
+func (p *proxy) extractPath(path string) (model.APIIdentifier, string, apperrors.AppError) {
+	apiIdentifier, path, err := p.extractPathFunc(path)
+	if err != nil {
+		return model.APIIdentifier{}, "", apperrors.Internal("failed to extract API Identifier from path")
+	}
+
+	return apiIdentifier, path, nil
+}
+
+func (p *proxy) getOrCreateCacheEntry(apiIdentifier model.APIIdentifier) (*CacheEntry, apperrors.AppError) {
+	cacheObj, found := p.cache.Get(apiIdentifier.Application, apiIdentifier.Service, apiIdentifier.Entry)
+
+	if found {
+		return cacheObj, nil
+	}
+
+	return p.createCacheEntry(apiIdentifier)
+}
+
+func (p *proxy) createCacheEntry(apiIdentifier model.APIIdentifier) (*CacheEntry, apperrors.AppError) {
+	serviceAPI, err := p.apiExtractor.Get(apiIdentifier)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy, err := makeProxy(serviceAPI.TargetUrl, serviceAPI.RequestParameters, apiIdentifier.Service, p.skipVerify)
+	if err != nil {
+		return nil, err
+	}
+
+	authorizationStrategy := p.newAuthorizationStrategy(serviceAPI.Credentials)
+	csrfTokenStrategy := p.newCSRFTokenStrategy(authorizationStrategy, serviceAPI.Credentials)
+
+	return p.cache.Put(apiIdentifier.Application, apiIdentifier.Service, apiIdentifier.Entry, proxy, authorizationStrategy, csrfTokenStrategy), nil
+}
+
+func (p *proxy) newAuthorizationStrategy(credentials *authorization.Credentials) authorization.Strategy {
+	return p.authorizationStrategyFactory.Create(credentials)
+}
+
+func (p *proxy) newCSRFTokenStrategy(authorizationStrategy authorization.Strategy, credentials *authorization.Credentials) csrf.TokenStrategy {
+	csrfTokenEndpointURL := ""
+	if credentials != nil {
+		csrfTokenEndpointURL = credentials.CSRFTokenEndpointURL
+	}
+	return p.csrfTokenStrategyFactory.Create(authorizationStrategy, csrfTokenEndpointURL)
+}
+
+func (p *proxy) newCSRFTokenStrategyFromCSRFConfig(authorizationStrategy authorization.Strategy, csrfConfig *proxyconfig.CSRFConfig) csrf.TokenStrategy {
+	csrfTokenEndpointURL := ""
+	if csrfConfig != nil {
+		csrfTokenEndpointURL = csrfConfig.TokenURL
+	}
+	return p.csrfTokenStrategyFactory.Create(authorizationStrategy, csrfTokenEndpointURL)
+}
+
+func (p *proxy) setRequestTimeout(r *http.Request) (*http.Request, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(p.proxyTimeout)*time.Second)
+	newRequest := r.WithContext(ctx)
+
+	return newRequest, cancel
+}
+
+func (p *proxy) addAuthorization(r *http.Request, cacheEntry *CacheEntry) apperrors.AppError {
+
+	err := cacheEntry.AuthorizationStrategy.AddAuthorization(r)
+
+	if err != nil {
+		return err
+	}
+
+	return cacheEntry.CSRFTokenStrategy.AddCSRFToken(r)
+}
+
+func (p *proxy) addModifyResponseHandler(r *http.Request, apiIdentifier model.APIIdentifier, cacheEntry *CacheEntry, cacheUpdateFunc updateCacheEntryFunction) apperrors.AppError {
+	// Handle the case when credentials has been changed or OAuth token has expired
+	secondRequestBody, err := copyRequestBody(r)
+	if err != nil {
+		return err
+	}
+
+	modifyResponseFunction := func(response *http.Response) error {
+		retrier := newUnauthorizedResponseRetrier(apiIdentifier, r, secondRequestBody, p.proxyTimeout, cacheUpdateFunc)
+		return retrier.RetryIfFailedToAuthorize(response)
+	}
+
+	cacheEntry.Proxy.ModifyResponse = modifyResponseFunction
+	return nil
+}
+
+func copyRequestBody(r *http.Request) (io.ReadCloser, apperrors.AppError) {
+	if r.Body == nil {
+		return nil, nil
+	}
+
+	bodyCopy, secondRequestBody, err := drainBody(r.Body)
+	if err != nil {
+		return nil, apperrors.Internal("failed to drain request body, %s", err)
+	}
+	r.Body = bodyCopy
+
+	return secondRequestBody, nil
+}
+
+func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) {
+	if b == http.NoBody {
+		return http.NoBody, http.NoBody, nil
+	}
+	var buf bytes.Buffer
+	if _, err = buf.ReadFrom(b); err != nil {
+		return nil, b, err
+	}
+	if err = b.Close(); err != nil {
+		return nil, b, err
+	}
+	return ioutil.NopCloser(&buf), ioutil.NopCloser(bytes.NewReader(buf.Bytes())), nil
+}
+
+func handleErrors(w http.ResponseWriter, apperr apperrors.AppError) {
+	code, body := httperrors.AppErrorToResponse(apperr)
+	respondWithBody(w, code, body)
+}
+
+func respondWithBody(w http.ResponseWriter, code int, body httperrors.ErrorResponse) {
+	w.Header().Set(httpconsts.HeaderContentType, httpconsts.ContentTypeApplicationJson)
+
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(body)
+}
---
+++ components/central-application-gateway/internal/proxy/unauthorizedresponseretrier.go
@@ -0,0 +1,105 @@
+package proxy
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/kyma-project/kyma/components/central-application-gateway/internal/metadata/model"
+
+	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/apperrors"
+	log "github.com/sirupsen/logrus"
+)
+
+type retrier struct {
+	apiIdentifier            model.APIIdentifier
+	request                  *http.Request
+	requestBodyCopy          io.ReadCloser
+	retried                  bool
+	timeout                  int
+	updateCacheEntryFunction updateCacheEntryFunction
+}
+
+type updateCacheEntryFunction = func(apiIdentifier model.APIIdentifier) (*CacheEntry, apperrors.AppError)
+
+func newUnauthorizedResponseRetrier(apiIdentifier model.APIIdentifier, request *http.Request, requestBodyCopy io.ReadCloser, timeout int, updateCacheEntryFunc updateCacheEntryFunction) *retrier {
+	return &retrier{apiIdentifier: apiIdentifier, request: request, requestBodyCopy: requestBodyCopy, retried: false, timeout: timeout, updateCacheEntryFunction: updateCacheEntryFunc}
+}
+
+func (rr *retrier) RetryIfFailedToAuthorize(r *http.Response) error {
+	if rr.retried {
+		return nil
+	}
+
+	rr.retried = true
+
+	if r.StatusCode == http.StatusForbidden || r.StatusCode == http.StatusUnauthorized {
+		log.Infof("Request from service with name id %s failed with %d status, invalidating proxy and retrying.", rr.apiIdentifier.Service, r.StatusCode)
+
+		retryRes, err := rr.retry()
+		if err != nil {
+			return err
+		}
+
+		if retryRes != nil {
+			if r.Body != nil {
+				r.Body.Close()
+			}
+			*r = *retryRes
+		}
+	}
+
+	return nil
+}
+
+func (rr *retrier) retry() (*http.Response, error) {
+	request, cancel := rr.prepareRequest()
+	defer cancel()
+
+	request.Body = rr.requestBodyCopy
+
+	cacheEntry, err := rr.updateCacheEntryFunction(rr.apiIdentifier)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := rr.addAuthorization(request, cacheEntry); err != nil {
+		return nil, err
+	}
+
+	return rr.performRequest(request, cacheEntry)
+}
+
+func (rr *retrier) prepareRequest() (*http.Request, context.CancelFunc) {
+	rr.request.RequestURI = ""
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(rr.timeout)*time.Second)
+
+	return rr.request.WithContext(ctx), cancel
+}
+
+func (rr *retrier) addAuthorization(r *http.Request, cacheEntry *CacheEntry) error {
+	authorizationStrategy := cacheEntry.AuthorizationStrategy
+	authorizationStrategy.Invalidate()
+	err := cacheEntry.AuthorizationStrategy.AddAuthorization(r)
+	if err != nil {
+		return err
+	}
+
+	csrfTokenStrategy := cacheEntry.CSRFTokenStrategy
+	csrfTokenStrategy.Invalidate()
+	return csrfTokenStrategy.AddCSRFToken(r)
+}
+
+func (rr *retrier) performRequest(r *http.Request, cacheEntry *CacheEntry) (*http.Response, error) {
+	reverseProxy := cacheEntry.Proxy
+	reverseProxy.Director(r)
+
+	client := &http.Client{
+		Transport: reverseProxy.Transport,
+	}
+
+	res, err := client.Do(r)
+
+	return res, err
+}
