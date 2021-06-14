4e48668f9ec725a6c9fb6c29b22b934dbf15980f
Radu Fantaziu <radu.fantaziu@sap.com>
2021-06-14 09:38:31 +0200

    Reconnect to Nats in publisher

--- components/event-publisher-proxy/pkg/nats/connect.go
+++ components/event-publisher-proxy/pkg/nats/connect.go
@@ -7,14 +7,40 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
-// ConnectToNats returns a nats connection that is ready for use, or error if connection to the nats server failed
-func ConnectToNats(url string, retry bool, reconnects int, wait time.Duration) (*nats.Conn, error) {
-	connection, err := nats.Connect(url, nats.RetryOnFailedConnect(retry), nats.MaxReconnects(reconnects), nats.ReconnectWait(wait))
+type connectionData struct {
+	url        string
+	retry      bool
+	reconnects int
+	wait       time.Duration
+}
+
+type BackendConnection struct {
+	connectionData connectionData
+	Connection     *nats.Conn
+}
+
+// NewBackendConnection returns a new NewNatsConnection instance with the given NATS connection data.
+func NewBackendConnection(url string, retry bool, reconnects int, wait time.Duration) *BackendConnection {
+	bc := &BackendConnection{}
+	bc.connectionData = connectionData{url: url, retry: retry, reconnects: reconnects, wait: wait}
+	return bc
+}
+
+// Connect returns a nats connection that is ready for use, or error if connection to the nats server failed
+func (bc *BackendConnection) Connect() error {
+	connection, err := nats.Connect(bc.connectionData.url, nats.RetryOnFailedConnect(bc.connectionData.retry),
+		nats.MaxReconnects(bc.connectionData.reconnects), nats.ReconnectWait(bc.connectionData.wait))
 	if err != nil {
-		return nil, err
+		return err
 	}
 	if status := connection.Status(); status != nats.CONNECTED {
-		return nil, fmt.Errorf("connection status not connected: %v", status)
+		return fmt.Errorf("connection status not connected: %v", status)
 	}
-	return connection, nil
+	// OK
+	bc.Connection = connection
+	return nil
+}
+
+func (bc *BackendConnection) Reconnect() error {
+	return bc.Connect()
 }
--- components/event-publisher-proxy/pkg/nats/connect_test.go
+++ components/event-publisher-proxy/pkg/nats/connect_test.go
@@ -3,10 +3,9 @@ package nats
 import (
 	"testing"
 
+	publishertesting "github.com/kyma-project/kyma/components/event-publisher-proxy/testing"
 	"github.com/nats-io/nats.go"
 	"github.com/stretchr/testify/assert"
-
-	publishertesting "github.com/kyma-project/kyma/components/event-publisher-proxy/testing"
 )
 
 func TestConnectToNats(t *testing.T) {
@@ -15,9 +14,18 @@ func TestConnectToNats(t *testing.T) {
 	assert.NotEmpty(t, natsServer.ClientURL())
 	defer natsServer.Shutdown()
 
-	connection, err := ConnectToNats(natsServer.ClientURL(), true, 1, 3)
+	bc := NewBackendConnection(natsServer.ClientURL(), true, 1, 3)
+	err := bc.Connect()
 	assert.Nil(t, err)
-	assert.NotNil(t, connection)
-	assert.Equal(t, connection.Status(), nats.CONNECTED)
-	defer connection.Close()
+	assert.NotNil(t, bc.Connection)
+	assert.Equal(t, bc.Connection.Status(), nats.CONNECTED)
+
+	bc.Connection.Close()
+
+	err = bc.Reconnect()
+	assert.Nil(t, err)
+	assert.NotNil(t, bc.Connection)
+	assert.Equal(t, bc.Connection.Status(), nats.CONNECTED)
+
+	defer bc.Connection.Close()
 }
