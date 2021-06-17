b2d26486af9deb3eb7ee92f552973650a5dd21e1
Mostafa Shorim <mostafa.shorim@sap.com>
2021-06-17 21:08:43 +0200

    debug - freeze before querying loki

--- tests/fast-integration/logging/helpers.js
+++ tests/fast-integration/logging/helpers.js
@@ -5,6 +5,10 @@ const {
 } = require("./client");
 
 async function checkLokiLogs(startTimestamp) {
+    console.log("timestamp at which query is executed: ", new Date().toISOString());
+    while(true) {
+
+    }
     const cancelPortForward = lokiPortForward();
 
     const labels = '{app="commerce-mock", container="commerce-mock", namespace="mocks"}';
