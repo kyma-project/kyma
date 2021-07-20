679a6f5fc96f23f424371faf8d4892791020cf60
hamdymoussa <hamdymoussa1@gmail.com>
2021-07-20 10:15:30 -0400

    update secret-migration.sh
    Adding PostgreSQL replication pwd to Values.global.ory.hydra.persistence.enabled condition

--- resources/ory/files/secret-migration.sh
+++ resources/ory/files/secret-migration.sh
@@ -86,8 +86,6 @@ DATA=$(cat << EOF
   ${SECRET_COOKIE_KEY}: $(echo -n "${COOKIE}" | base64 -w 0)
   {{- if .Values.global.ory.hydra.persistence.enabled }}
   ${PASSWORD_KEY}: $(echo -n "${PASSWORD}" | base64 -w 0)
-  {{- end }}
-  {{- if .Values.postgresql.replication.enabled }}
   ${PASSWORD_R_KEY}: $(echo -n "${PASSWORDR}" | base64 -w 0)
   {{- end }}
   {{- if .Values.global.ory.hydra.persistence.gcloud.enabled }}
