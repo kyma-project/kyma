8df5d1e98ef9ed6067ac541b019cb02354ce50ca
Sayan Hazra <sayan.mnit@gmail.com>
2021-07-28 11:40:58 +0200

    Removing new eventing as it is the default

--- tests/fast-integration/installer/index.js
+++ tests/fast-integration/installer/index.js
@@ -375,7 +375,6 @@ async function uninstallKyma(options) {
  * @param {string} options.resourcesPath Path to the resources folder with Kyma charts
  * @param {string} options.istioVersion Istio version, eg. 1.8.2
  * @param {boolean} options.isUpgrade Upgrade existing installation
- * @param {boolean} options.newEventing Use new eventing
  * @param {boolean} options.withCentralApplicationGateway Install cluster-wide Application Gateway
  * @param {Array<string>} options.skipComponents List of components to not install
  * @param {Array<string>} options.components List of components to install
