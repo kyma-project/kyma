0dd7c0d053f0cfda6e947b86bef3d604e373ad21
Marcin Witalis <marcin.witalis@sap.com>
2021-07-29 11:40:41 +0200

    alternative instrumentation with b3 propagation

--- components/function-runtimes/nodejs14/kubeless/kubeless_rt/kubeless.js
+++ components/function-runtimes/nodejs14/kubeless/kubeless_rt/kubeless.js
@@ -1,12 +1,25 @@
-'use strict';
+"use strict";
 
 // Require dependencies
 const { NodeTracerProvider } = require("@opentelemetry/node");
-const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/tracing");
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
+const {
+  SimpleSpanProcessor,
+  ConsoleSpanExporter,
+} = require("@opentelemetry/tracing");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { ZipkinExporter } = require("@opentelemetry/exporter-zipkin");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const {
+  ExpressInstrumentation,
+} = require("@opentelemetry/instrumentation-express");
+const {
+  B3Propagator,
+  B3InjectEncoding,
+} = require("@opentelemetry/propagator-b3");
+const {
+  CompositePropagator,
+  HttpTraceContextPropagator,
+} = require("@opentelemetry/core");
 
 // Create a tracer provider
 const provider = new NodeTracerProvider();
@@ -14,49 +27,62 @@ const provider = new NodeTracerProvider();
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
 const zipkinExporter = new ZipkinExporter({
-    url: 'http://tracing-jaeger-collector.kyma-system.svc.cluster.local:9411/api/v2/spans',
-    serviceName: 'the-service'
+  //url: 'http://tracing-jaeger-collector.kyma-system.svc.cluster.local:9411/api/v2/spans',
+  serviceName: "the-service",
 });
 
 // The simple span processor sends spans to the exporter as soon as they are ended.
 const processor = new SimpleSpanProcessor(zipkinExporter);
 provider.addSpanProcessor(processor);
 
-// The provider must be registered in order to
-// be used by the OpenTelemetry API and instrumentations
-provider.register();
-
 // This will automatically enable all instrumentations
 registerInstrumentations({
+  tracerProvider: provider,
   instrumentations: [
-    new HttpInstrumentation(),
+    // Express instrumentation expects HTTP layer to be instrumented
+    new HttpInstrumentation({
+      requestHook: (span, request) => {
+        span.updateName("node-express-reviews-service");
+      },
+    }),
     new ExpressInstrumentation(),
   ],
 });
 
+provider.register({
+  //contextManager: new AsyncHooksContextManager(),
+  propagator: new CompositePropagator({
+    propagators: [
+      new B3Propagator({ injectEncoding: B3InjectEncoding.MULTI_HEADER }),
+      new HttpTraceContextPropagator(),
+    ],
+  }),
+});
+
 console.log("tracing initialized");
 
-const vm = require('vm');
-const path = require('path');
-const Module = require('module');
+const vm = require("vm");
+const path = require("path");
+const Module = require("module");
 
-const bodyParser = require('body-parser');
-const client = require('prom-client');
-const express = require('express');
-const helper = require('./lib/helper');
-const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const client = require("prom-client");
+const express = require("express");
+const helper = require("./lib/helper");
+const morgan = require("morgan");
 
-const bodySizeLimit = Number(process.env.REQ_MB_LIMIT || '1');
+const bodySizeLimit = Number(process.env.REQ_MB_LIMIT || "1");
 
 const app = express();
+app.locals.tracer = opentelemetry.trace.getTracer('review-node-express-tracer','0.0.1');
 
 if (process.env["KYMA_INTERNAL_LOGGER_ENABLED"]) {
-    app.use(morgan("combined"));
+  app.use(morgan("combined"));
 }
 
 const bodParserOptions = {
-    type: req => !req.is('multipart/*'),
-    limit: `${bodySizeLimit}mb`,
+  type: (req) => !req.is("multipart/*"),
+  limit: `${bodySizeLimit}mb`,
 };
 
 app.use(bodyParser.raw(bodParserOptions));
@@ -65,149 +91,157 @@ app.use(bodyParser.urlencoded({ limit: `${bodySizeLimit}mb`, extended: true }));
 
 const modName = process.env.MOD_NAME;
 const funcHandler = process.env.FUNC_HANDLER;
-const timeout = Number(process.env.FUNC_TIMEOUT || '180');
-const funcPort = Number(process.env.FUNC_PORT || '8080');
+const timeout = Number(process.env.FUNC_TIMEOUT || "180");
+const funcPort = Number(process.env.FUNC_PORT || "8080");
 
 const modKubeless = require.main.filename;
-const modRootPath = path.join(modKubeless, '..', '..', 'kubeless');
+const modRootPath = path.join(modKubeless, "..", "..", "kubeless");
 
 const modPath = path.join(modRootPath, `${modName}.js`);
-const libPath = path.join(modRootPath, 'node_modules');
-const pkgPath = path.join(modRootPath, 'package.json');
+const libPath = path.join(modRootPath, "node_modules");
+const pkgPath = path.join(modRootPath, "package.json");
 const libDeps = helper.readDependencies(pkgPath);
 
-const { timeHistogram, callsCounter, errorsCounter } = helper.prepareStatistics('method', client);
+const { timeHistogram, callsCounter, errorsCounter } = helper.prepareStatistics(
+  "method",
+  client
+);
 helper.routeLivenessProbe(app);
 helper.routeMetrics(app, client);
 
 const context = {
-    'function-name': funcHandler,
-    'timeout' : timeout,
-    'runtime': process.env.FUNC_RUNTIME,
-    'memory-limit': process.env.FUNC_MEMORY_LIMIT
+  "function-name": funcHandler,
+  timeout: timeout,
+  runtime: process.env.FUNC_RUNTIME,
+  "memory-limit": process.env.FUNC_MEMORY_LIMIT,
 };
 
-const script = new vm.Script('\nrequire(\'kubeless\')(require(\''+ modPath +'\'));\n', {
+const script = new vm.Script(
+  "\nrequire('kubeless')(require('" + modPath + "'));\n",
+  {
     filename: modPath,
     displayErrors: true,
-});
+  }
+);
 
 function modRequire(p, req, res, end) {
-    if (p === 'kubeless')
-        return (handler) => modExecute(handler, req, res, end);
-    else if (libDeps.includes(p))
-        return require(path.join(libPath, p));
-    else if (p.indexOf('./') === 0)
-        return require(path.join(path.dirname(modPath), p));
-    else
-        return require(p);
+  if (p === "kubeless") return (handler) => modExecute(handler, req, res, end);
+  else if (libDeps.includes(p)) return require(path.join(libPath, p));
+  else if (p.indexOf("./") === 0)
+    return require(path.join(path.dirname(modPath), p));
+  else return require(p);
 }
 
 function modExecute(handler, req, res, end) {
-    let func = null;
-    switch (typeof handler) {
-        case 'function':
-            func = handler;
-            break;
-        case 'object':
-            if (handler) func = handler[funcHandler];
-            break;
-    }
-    if (func === null)
-        throw new Error(`Unable to load ${handler}`);
+  let func = null;
+  switch (typeof handler) {
+    case "function":
+      func = handler;
+      break;
+    case "object":
+      if (handler) func = handler[funcHandler];
+      break;
+  }
+  if (func === null) throw new Error(`Unable to load ${handler}`);
 
-    try {
-        let data = req.body;
-        if (!req.is('multipart/*') && req.body.length > 0) {
-            if (req.is('application/json')) {
-                data = JSON.parse(req.body.toString('utf-8'))
-            } else {
-                data = req.body.toString('utf-8')
-            }
-        }
-        const event = {
-            'ce-type': req.get('ce-type'),
-            'ce-source': req.get('ce-source'),
-            'ce-eventtypeversion': req.get('ce-eventtypeversion'),
-            'ce-specversion': req.get('ce-specversion'),
-            'ce-id': req.get('ce-id'),
-            'ce-time': req.get('ce-time'),
-            data,
-            'extensions': { request: req, response: res },
-        };
-        Promise.resolve(func(event, context))
-        // Finalize
-            .then(rval => modFinalize(rval, res, end))
-            // Catch asynchronous errors
-            .catch(err => handleError(err, res, funcLabel(req), end))
-        ;
-    } catch (err) {
-        // Catch synchronous errors
-        handleError(err, res, funcLabel(req), end);
+  try {
+    let data = req.body;
+    if (!req.is("multipart/*") && req.body.length > 0) {
+      if (req.is("application/json")) {
+        data = JSON.parse(req.body.toString("utf-8"));
+      } else {
+        data = req.body.toString("utf-8");
+      }
     }
+    const event = {
+      "ce-type": req.get("ce-type"),
+      "ce-source": req.get("ce-source"),
+      "ce-eventtypeversion": req.get("ce-eventtypeversion"),
+      "ce-specversion": req.get("ce-specversion"),
+      "ce-id": req.get("ce-id"),
+      "ce-time": req.get("ce-time"),
+      data,
+      extensions: { request: req, response: res },
+    };
+    Promise.resolve(func(event, context))
+      // Finalize
+      .then((rval) => modFinalize(rval, res, end))
+      // Catch asynchronous errors
+      .catch((err) => handleError(err, res, funcLabel(req), end));
+  } catch (err) {
+    // Catch synchronous errors
+    handleError(err, res, funcLabel(req), end);
+  }
 }
 
 function modFinalize(result, res, end) {
-    if (!res.finished) switch(typeof result) {
-        case 'string':
-            res.end(result);
-            break;
-        case 'object':
-            res.json(result); // includes res.end(), null also handled
-            break;
-        case 'undefined':
-            res.end();
-            break;
-        default:
-            res.end(JSON.stringify(result));
+  if (!res.finished)
+    switch (typeof result) {
+      case "string":
+        res.end(result);
+        break;
+      case "object":
+        res.json(result); // includes res.end(), null also handled
+        break;
+      case "undefined":
+        res.end();
+        break;
+      default:
+        res.end(JSON.stringify(result));
     }
-    end();
+  end();
 }
 
 function handleError(err, res, label, end) {
-    errorsCounter.labels(label).inc();
-    res.status(500).send('Internal Server Error');
-    console.error(`Function failed to execute: ${err.stack}`);
-    end();
+  errorsCounter.labels(label).inc();
+  res.status(500).send("Internal Server Error");
+  console.error(`Function failed to execute: ${err.stack}`);
+  end();
 }
 
 function funcLabel(req) {
-    return modName + '-' + req.method;
+  return modName + "-" + req.method;
 }
 
-app.all('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    if (req.method === 'OPTIONS') {
-        // CORS preflight support (Allow any method or header requested)
-        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-        res.end();
-    } else {
-        const label = funcLabel(req);
-        const end = timeHistogram.labels(label).startTimer();
-        callsCounter.labels(label).inc();
+app.all("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    // CORS preflight support (Allow any method or header requested)
+    res.header(
+      "Access-Control-Allow-Methods",
+      req.headers["access-control-request-method"]
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      req.headers["access-control-request-headers"]
+    );
+    res.end();
+  } else {
+    const label = funcLabel(req);
+    const end = timeHistogram.labels(label).startTimer();
+    callsCounter.labels(label).inc();
 
-        const sandbox = Object.assign({}, global, {
-            __filename: modPath,
-            __dirname: modRootPath,
-            module: new Module(modPath, null),
-            require: (p) => modRequire(p, req, res, end),
-        });
+    const sandbox = Object.assign({}, global, {
+      __filename: modPath,
+      __dirname: modRootPath,
+      module: new Module(modPath, null),
+      require: (p) => modRequire(p, req, res, end),
+    });
 
-        try {
-            script.runInNewContext(sandbox, { timeout : timeout * 1000 });
-        } catch (err) {
-            if (err.toString().match('Error: Script execution timed out')) {
-                res.status(408).send(err);
-                // We cannot stop the spawned process (https://github.com/nodejs/node/issues/3020)
-                // we need to abruptly stop this process
-                console.error('CRITICAL: Unable to stop spawned process. Exiting');
-                process.exit(1);
-            } else {
-                handleError(err, res, funcLabel, end);
-            }
-        }
+    try {
+      script.runInNewContext(sandbox, { timeout: timeout * 1000 });
+    } catch (err) {
+      if (err.toString().match("Error: Script execution timed out")) {
+        res.status(408).send(err);
+        // We cannot stop the spawned process (https://github.com/nodejs/node/issues/3020)
+        // we need to abruptly stop this process
+        console.error("CRITICAL: Unable to stop spawned process. Exiting");
+        process.exit(1);
+      } else {
+        handleError(err, res, funcLabel, end);
+      }
     }
+  }
 });
 
 const server = app.listen(funcPort);
