645b500f9003238d49d9dbca04be3550943f1b0a
Benjamin Lindner <benjamin.lindner@sap.com>
2021-08-04 11:24:08 +0200

    Remove skipping for now

--- tests/fast-integration/test/1-install.js
+++ tests/fast-integration/test/1-install.js
@@ -6,11 +6,11 @@ describe("Installation", function () {
 
   it("Kyma should successfully install", async function () {
     // temporary until kyma is provided via pipeline
-    const result = await k8sCoreV1Api.listNamespace();
-    if (result && result.body.items.map((i) => i.metadata.name).includes('kyma-system')) {
-      console.log("Namespace 'kyma-system' exists. Skipping installation.");
-      return;
-    }
+    // const result = await k8sCoreV1Api.listNamespace();
+    // if (result && result.body.items.map((i) => i.metadata.name).includes('kyma-system')) {
+    //   console.log("Namespace 'kyma-system' exists. Skipping installation.");
+    //   return;
+    // }
 
     const options = {
       skipComponents: ["dex","console"],
