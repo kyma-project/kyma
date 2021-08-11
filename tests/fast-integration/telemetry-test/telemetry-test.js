395d3844493b09c521600161b15390ee12bd0342
Adrian Stobbe <adrian.stobbe@sap.com>
2021-08-11 15:52:07 +0200

    check that operator is ready

--- tests/fast-integration/telemetry-test/telemetry-test.js
+++ tests/fast-integration/telemetry-test/telemetry-test.js
@@ -2,12 +2,23 @@ const uuid = require("uuid");
 
 const { assert } = require("chai");
 
-const { waitForPodWithLabel } = require("../utils");
+const { waitForPodWithLabel, k8sCoreV1Api } = require("../utils");
 
 describe("Telemtry operator", function () {
   // check if operator installed
   it("Operator should be ready", async function () {
     let namespace = "kyma-system";
-    await waitForPodWithLabel("control-plane", "controller-manager", namespace);
+    let res = await k8sCoreV1Api.listNamespacedPod(
+      namespace,
+      "true",
+      undefined,
+      undefined,
+      undefined,
+      "control-plane=controller-manager,service.istio.io/canonical-name=telemetry-operator-controller-manager"
+    );
+    let podList = res.body.items;
+    assert.equal(podList.length, 1);
   });
+
+  it;
 });
