26523737a26bf02b1644e45a4b54651857c909fc
Marco Bebway <marco.bebway@sap.com>
2021-08-31 17:33:33 +0200

    Fix nats-operator (#11920) (#12001)

---
+++ components/nats-operator/pkg/client/natscluster/fake/client.go
@@ -0,0 +1,31 @@
+package fake
+
+import (
+	"log"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	dynamicfake "k8s.io/client-go/dynamic/fake"
+
+	"github.com/kyma-project/kyma/components/nats-operator/pkg/client/natscluster"
+	"github.com/nats-io/nats-operator/pkg/apis/nats/v1alpha2"
+)
+
+// NewClientOrDie returns a new NATS fake client.
+func NewClientOrDie(natsClusterList *v1alpha2.NatsClusterList) *natscluster.Client {
+	scheme := setupSchemeOrDie()
+	dynamicClient := dynamicfake.NewSimpleDynamicClient(scheme, natsClusterList)
+	return natscluster.NewClient(dynamicClient)
+}
+
+// setupSchemeOrDie returns a new scheme with the desired API resources.
+func setupSchemeOrDie() *runtime.Scheme {
+	scheme := runtime.NewScheme()
+	if err := corev1.AddToScheme(scheme); err != nil {
+		log.Fatalf("Failed to setup scheme with error: %v", err)
+	}
+	if err := v1alpha2.AddToScheme(scheme); err != nil {
+		log.Fatalf("Failed to setup scheme with error: %v", err)
+	}
+	return scheme
+}
