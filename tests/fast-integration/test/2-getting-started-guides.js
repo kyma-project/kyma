d67778534050b8bb3179dd6137a63a6d6385f0f1
Jeremy Harisch <jeremy.harisch@sap.com>
2021-09-03 16:41:48 +0200

    Removed commented out code

--- tests/fast-integration/test/2-getting-started-guides.js
+++ tests/fast-integration/test/2-getting-started-guides.js
@@ -1,40 +1,40 @@
-// const {
-//   ensureGettingStartedTestFixture,
-//   verifyOrderPersisted,
-//   cleanGettingStartedTestFixture,
-// } = require("./fixtures/getting-started-guides");
-// const {
-//   printRestartReport,
-//   getContainerRestartsForAllNamespaces,
-// } = require("../utils");
+const {
+  ensureGettingStartedTestFixture,
+  verifyOrderPersisted,
+  cleanGettingStartedTestFixture,
+} = require("./fixtures/getting-started-guides");
+const {
+  printRestartReport,
+  getContainerRestartsForAllNamespaces,
+} = require("../utils");
 
-// describe("Getting Started Guide Tests", function () {
-//   this.timeout(10 * 60 * 1000);
-//   this.slow(5000);
+describe("Getting Started Guide Tests", function () {
+  this.timeout(10 * 60 * 1000);
+  this.slow(5000);
 
-//   let initialRestarts = null;
+  let initialRestarts = null;
 
-//   it("Listing all pods in cluster", async function () {
-//     initialRestarts = await getContainerRestartsForAllNamespaces();
-//   });
+  it("Listing all pods in cluster", async function () {
+    initialRestarts = await getContainerRestartsForAllNamespaces();
+  });
 
-//   it("Getting started guide fixture should be ready", async function () {
-//     await ensureGettingStartedTestFixture().catch((err) => {
-//       console.dir(err); // first error is logged
-//       return ensureGettingStartedTestFixture();
-//     });
-//   });
+  it("Getting started guide fixture should be ready", async function () {
+    await ensureGettingStartedTestFixture().catch((err) => {
+      console.dir(err); // first error is logged
+      return ensureGettingStartedTestFixture();
+    });
+  });
 
-//   it("Order should be persisted and should survive pod restarts (redis storage)", async function () {
-//     await verifyOrderPersisted();
-//   });
+  it("Order should be persisted and should survive pod restarts (redis storage)", async function () {
+    await verifyOrderPersisted();
+  });
 
-//   it("Should print report of restarted containers, skipped if no crashes happened", async function () {
-//     const afterTestRestarts = await getContainerRestartsForAllNamespaces();
-//     printRestartReport(initialRestarts, afterTestRestarts);
-//   });
+  it("Should print report of restarted containers, skipped if no crashes happened", async function () {
+    const afterTestRestarts = await getContainerRestartsForAllNamespaces();
+    printRestartReport(initialRestarts, afterTestRestarts);
+  });
 
-//   it("Namespace should be deleted", async function () {
-//     await cleanGettingStartedTestFixture(false);
-//   });
-// });
+  it("Namespace should be deleted", async function () {
+    await cleanGettingStartedTestFixture(false);
+  });
+});
