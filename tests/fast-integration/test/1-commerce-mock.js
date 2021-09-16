645aace6c0c0f1b4d974a921b8326bc194cc99e1
pxsalehi <pxsalehi@gmail.com>
2021-09-16 10:25:04 +0200

    move in-cluster sub-test to fixture

--- tests/fast-integration/test/1-commerce-mock.js
+++ tests/fast-integration/test/1-commerce-mock.js
@@ -48,12 +48,8 @@ describe("CommerceMock tests", function () {
     });
   });
 
-  it("in-cluster event should be delivered (structured mode)", async function () {
-    await checkInClusterEventDelivery(testNamespace, 'structured');
-  });
-
-  it("in-cluster event should be delivered (binary mode)", async function () {
-    await checkInClusterEventDelivery(testNamespace, 'binary');
+  it("in-cluster event should be delivered (structured and binary mode)", async function () {
+    await checkInClusterEventDelivery(testNamespace);
   });
 
   it("function should be reachable through secured API Rule", async function () {
