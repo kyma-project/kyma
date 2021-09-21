a971b83200c97ec11353a3c71376be95ce1ef12a
Radu Fantaziu <radu.fantaziu@sap.com>
2021-09-21 13:33:35 +0200

    Initial commit

--- components/eventing-controller/pkg/deployment/publisher_deployment.go
+++ components/eventing-controller/pkg/deployment/publisher_deployment.go
@@ -112,6 +112,22 @@ func NewNATSPublisherDeployment(publisherConfig env.PublisherConfig) *appsv1.Dep
 					Labels: labels,
 				},
 				Spec: v1.PodSpec{
+					Affinity: &v1.Affinity{
+						PodAntiAffinity: &v1.PodAntiAffinity{
+							PreferredDuringSchedulingIgnoredDuringExecution: []v1.WeightedPodAffinityTerm{
+								{
+									Weight: 100,
+									PodAffinityTerm: v1.PodAffinityTerm{
+										LabelSelector: &metav1.LabelSelector{
+											MatchLabels: map[string]string{AppLabelKey: PublisherName},
+										},
+										Namespaces:  []string{PublisherNamespace},
+										TopologyKey: "kubernetes.io/hostname",
+									},
+								},
+							},
+						},
+					},
 					Containers: []v1.Container{
 						{
 							Name:            PublisherName,
