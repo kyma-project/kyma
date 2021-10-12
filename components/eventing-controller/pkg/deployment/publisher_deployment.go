d4aea4771badec98ad90fc03f293d7c6a704ed91
Muhammad Faizan <mfaizanse@users.noreply.github.com>
2021-10-12 11:46:30 +0200

    Made proxy requestTimeout configurable and increased timeout (#12327)
    * made proxy requestTimeout configurable, increased to 10s, updated image tag
    
    * added tests

--- components/eventing-controller/pkg/deployment/publisher_deployment.go
+++ components/eventing-controller/pkg/deployment/publisher_deployment.go
@@ -72,7 +72,7 @@ func NewBEBPublisherDeployment(publisherConfig env.PublisherConfig) *appsv1.Depl
 							Name:            PublisherName,
 							Image:           publisherConfig.Image,
 							Ports:           getContainerPorts(),
-							Env:             getBEBEnvVars(),
+							Env:             getBEBEnvVars(publisherConfig),
 							LivenessProbe:   getLivenessProbe(),
 							ReadinessProbe:  getReadinessProbe(),
 							ImagePullPolicy: getImagePullPolicy(publisherConfig.ImagePullPolicy),
@@ -134,7 +134,7 @@ func NewNATSPublisherDeployment(publisherConfig env.PublisherConfig) *appsv1.Dep
 							Name:            PublisherName,
 							Image:           publisherConfig.Image,
 							Ports:           getContainerPorts(),
-							Env:             getNATSEnvVars(),
+							Env:             getNATSEnvVars(publisherConfig),
 							LivenessProbe:   getLivenessProbe(),
 							ReadinessProbe:  getReadinessProbe(),
 							ImagePullPolicy: getImagePullPolicy(publisherConfig.ImagePullPolicy),
@@ -218,12 +218,12 @@ func getContainerPorts() []v1.ContainerPort {
 	}
 }
 
-func getBEBEnvVars() []v1.EnvVar {
+func getBEBEnvVars(publisherConfig env.PublisherConfig) []v1.EnvVar {
 	return []v1.EnvVar{
 		{Name: "BACKEND", Value: "beb"},
 		{Name: "PORT", Value: strconv.Itoa(int(publisherPortNum))},
-		{Name: "REQUEST_TIMEOUT", Value: "5s"},
 		{Name: "EVENT_TYPE_PREFIX", Value: "sap.kyma.custom"},
+		{Name: "REQUEST_TIMEOUT", Value: publisherConfig.RequestTimeout},
 		{
 			Name: "CLIENT_ID",
 			ValueFrom: &v1.EnvVarSource{
@@ -271,12 +271,12 @@ func getBEBEnvVars() []v1.EnvVar {
 	}
 }
 
-func getNATSEnvVars() []v1.EnvVar {
+func getNATSEnvVars(publisherConfig env.PublisherConfig) []v1.EnvVar {
 	return []v1.EnvVar{
 		{Name: "BACKEND", Value: "nats"},
 		{Name: "PORT", Value: strconv.Itoa(int(publisherPortNum))},
 		{Name: "NATS_URL", Value: "eventing-nats.kyma-system.svc.cluster.local"},
-		{Name: "REQUEST_TIMEOUT", Value: "5s"},
+		{Name: "REQUEST_TIMEOUT", Value: publisherConfig.RequestTimeout},
 		{Name: "LEGACY_NAMESPACE", Value: "kyma"},
 		{Name: "LEGACY_EVENT_TYPE_PREFIX", Value: "sap.kyma.custom"},
 		{Name: "EVENT_TYPE_PREFIX", Value: "sap.kyma.custom"},
