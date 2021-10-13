f034f62c0ba1fa9036d6c4d118a8407153c212e6
Marco Bebway <marco.bebway@sap.com>
2021-10-13 14:07:05 +0200

    Read eventTypePrefix from ConfigMap

--- components/eventing-controller/pkg/handlers/eventtype/build_test.go
+++ components/eventing-controller/pkg/handlers/eventtype/build_test.go
@@ -4,26 +4,27 @@ import "testing"
 
 func TestBuilder(t *testing.T) {
 	testCases := []struct {
+		name                                                        string
 		givenPrefix, givenApplicationName, givenEvent, givenVersion string
 		wantEventType                                               string
 	}{
 		{
-			givenPrefix: "sap.kyma", givenApplicationName: "testapp", givenEvent: "order.created", givenVersion: "v1",
-			wantEventType: "sap.kyma.testapp.order.created.v1",
+			name:        "prefix is empty",
+			givenPrefix: "", givenApplicationName: "test.app-1", givenEvent: "order.created", givenVersion: "v1",
+			wantEventType: "test.app-1.order.created.v1",
 		},
 		{
-			givenPrefix: "sap.kyma", givenApplicationName: "test.app", givenEvent: "order.created", givenVersion: "v1",
-			wantEventType: "sap.kyma.test.app.order.created.v1",
-		},
-		{
-			givenPrefix: "sap.kyma", givenApplicationName: "test-app", givenEvent: "order.created", givenVersion: "v1",
-			wantEventType: "sap.kyma.test-app.order.created.v1",
+			name:        "prefix is not empty",
+			givenPrefix: "prefix", givenApplicationName: "test.app-1", givenEvent: "order.created", givenVersion: "v1",
+			wantEventType: "prefix.test.app-1.order.created.v1",
 		},
 	}
 
 	for _, tc := range testCases {
-		if got := build(tc.givenPrefix, tc.givenApplicationName, tc.givenEvent, tc.givenVersion); tc.wantEventType != got {
-			t.Errorf("build failed, want [%s] but got [%s]", tc.wantEventType, got)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			if got := build(tc.givenPrefix, tc.givenApplicationName, tc.givenEvent, tc.givenVersion); tc.wantEventType != got {
+				t.Errorf("build failed, want [%s] but got [%s]", tc.wantEventType, got)
+			}
+		})
 	}
 }
