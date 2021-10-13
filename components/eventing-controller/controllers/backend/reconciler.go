e67474b8c0362cfff6d81069ef416d72734b6dca
Korbinian Stoemmer <292906+k15r@users.noreply.github.com>
2021-10-13 10:35:02 +0200

    small change to help running eventing-controller locally

--- components/eventing-controller/controllers/backend/reconciler.go
+++ components/eventing-controller/controllers/backend/reconciler.go
@@ -13,7 +13,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -725,27 +724,7 @@ func (r *Reconciler) namedLogger() *zap.SugaredLogger {
 	return r.logger.WithContext().Named(reconcilerName).With("backend", r.backendType)
 }
 
-// sets this reconciler as owner of obj
-func (r *Reconciler) setAsOwnerReference(ctx context.Context, obj metav1.Object) error {
-	controllerNamespacedName := types.NamespacedName{
-		Namespace: deployment.ControllerNamespace,
-		Name:      deployment.ControllerName,
-	}
-	var deploymentController appsv1.Deployment
-	if err := r.Cache.Get(ctx, controllerNamespacedName, &deploymentController); err != nil {
-		r.namedLogger().Errorw("get controller NamespacedName failed", "error", err)
-		return err
-	}
-	references := []metav1.OwnerReference{
-		*metav1.NewControllerRef(&deploymentController, schema.GroupVersionKind{
-			Group:   appsv1.SchemeGroupVersion.Group,
-			Version: appsv1.SchemeGroupVersion.Version,
-			Kind:    "Deployment",
-		}),
-	}
-	obj.SetOwnerReferences(references)
-	return nil
-}
+
 
 func getOAuth2ClientSecretName() string {
 	return deployment.ControllerName + BEBSecretNameSuffix
