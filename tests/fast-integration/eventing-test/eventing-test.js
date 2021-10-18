499ae4283294349d9807015ac0131a6691b6179b
pxsalehi <pxsalehi@gmail.com>
2021-10-18 17:00:34 +0200

    add metric tests

--- tests/fast-integration/eventing-test/eventing-test.js
+++ tests/fast-integration/eventing-test/eventing-test.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const https = require("https");
+const { expect, assert } = require("chai");
 const httpsAgent = new https.Agent({
   rejectUnauthorized: false, // curl -k
 });
@@ -24,6 +25,10 @@ const {
   printEventingPublisherProxyLogs,
 } = require("../utils");
 
+const {prometheusPortForward} = require("../monitoring/client")
+
+const {eventingMonitoringTest} = require("./metric-test")
+
 describe("Eventing tests", function () {
   this.timeout(10 * 60 * 1000);
   this.slow(5000);
@@ -32,6 +37,7 @@ describe("Eventing tests", function () {
   const backendK8sSecretNamespace = process.env.BACKEND_SECRET_NAMESPACE || "default";
   const eventMeshSecretFilePath = process.env.EVENTMESH_SECRET_FILE || "";
   const DEBUG = process.env.DEBUG;
+  let cancelPrometheusPortForward = null;
 
   // eventingE2ETestSuite - Runs Eventing end-to-end tests
   function eventingE2ETestSuite () {
@@ -75,6 +81,8 @@ describe("Eventing tests", function () {
       console.dir(err); // first error is logged
       return ensureCommerceMockLocalTestFixture("mocks", testNamespace);
     });
+
+    cancelPrometheusPortForward = prometheusPortForward();
   });
 
   after(async function() {
@@ -86,6 +94,8 @@ describe("Eventing tests", function () {
     if (eventMeshSecretFilePath !== "") {
       await deleteEventingBackendK8sSecret(backendK8sSecretName, backendK8sSecretNamespace);
     }
+
+    cancelPrometheusPortForward();
   });
 
   afterEach(async function() {
@@ -105,6 +115,8 @@ describe("Eventing tests", function () {
     eventingE2ETestSuite();
     // Running Eventing tracing tests
     eventingTracingTestSuite();
+    // Running Eventing Monitoring tests
+    eventingMonitoringTest('nats');
   });
 
   context('with BEB backend', function() {
@@ -120,6 +132,7 @@ describe("Eventing tests", function () {
 
     // Running Eventing end-to-end tests
     eventingE2ETestSuite();
+    eventingMonitoringTest('beb');
   });
 
   context('with Nats backend switched back from BEB', function() {
@@ -132,5 +145,7 @@ describe("Eventing tests", function () {
     eventingE2ETestSuite();
     // Running Eventing tracing tests
     eventingTracingTestSuite();
+    // Running Eventing Monitoring tests
+    eventingMonitoringTest('nats');
   });
 });
