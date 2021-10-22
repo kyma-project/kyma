bad855da2577a4396b2df9007d9e3766a255ab34
VladislavPaskar <vladislav.paskar@sap.com>
2021-10-22 09:14:11 +0200

    revert the sleep

--- tests/fast-integration/utils/index.js
+++ tests/fast-integration/utils/index.js
@@ -1447,6 +1447,7 @@ async function switchEventingBackend(secretName, namespace="default", backendTyp
     options
   );
 
+  await sleep(30 * 1000); // Putting on sleep because there may be a delay in eventing-backend status update propagation
   await waitForEventingBackendToReady(backendType)
 }
 
