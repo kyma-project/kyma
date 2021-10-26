6020e642cdcb6175a3749d659a13122c936196e5
Benjamin Lindner <benjamin.lindner@sap.com>
2021-10-26 10:02:08 +0200

    Create Dockerfile

--- components/grafana-watcher/main.go
+++ components/grafana-watcher/main.go
@@ -7,14 +7,13 @@ import (
 
 const dataSourcesPath = "etc/grafana/provsioning/datasources"
 
-type watcher struct{
+type watcher struct {
 	grafana *fsnotify.Watcher
-	path string
-
+	path    string
 	eventCount int
 }
-func main() {
 
+func main() {
 	done := make(chan bool)
 
 	watcher := watcher{path: dataSourcesPath}
@@ -22,14 +21,14 @@ func main() {
 	if err != nil {
 		// TODO Err Handle
 	}
-	<-done
 
+	<-done
 }
 
 func (w *watcher) start() error {
 	var err error
 	w.grafana, err = fsnotify.NewWatcher()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
@@ -37,17 +36,15 @@ func (w *watcher) start() error {
 		fmt.Println("ERROR", err)
 	}
 
-	go func(){
+	go func() {
 		for {
 			select {
 			case event := <-w.grafana.Events:
 				switch {
-				//case event.Op&fsnotify.Write == fsnotify.Write:
-				//	log.Printf("Write:  %s: %s", event.Op, event.Name)
 				case event.Op&fsnotify.Create == fsnotify.Create:
 					w.eventCount++
 				case event.Op&fsnotify.Remove == fsnotify.Remove:
-				 	w.eventCount++
+					w.eventCount++
 				}
 			case err := <-w.grafana.Errors:
 				fmt.Printf("Error %s", err)
