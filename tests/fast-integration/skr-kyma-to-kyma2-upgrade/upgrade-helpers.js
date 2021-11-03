b17f663a8bed3dabb83279ec95a41e3e62ae0ba4
Christoph Voigt <voigt.christoph@gmail.com>
2021-11-03 08:13:36 +0100

    avoid leaking secrets (#12523)
    * avoid leaking secrets
    
    * increase status check interval

--- tests/fast-integration/skr-kyma-to-kyma2-upgrade/upgrade-helpers.js
+++ tests/fast-integration/skr-kyma-to-kyma2-upgrade/upgrade-helpers.js
@@ -8,23 +8,23 @@ const { expect } = require("chai");
 async function kcpLogin (kcpconfigPath, kcpUser, kcpPassword) {
     debug(`Running kcpLogin...`)
     let args = []
+    // Note: dummycmd is just for output
+    let dummycmd=`kcp login --config ${kcpconfigPath} -u $KCP_TECH_USER_LOGIN -p $KCP_TECH_USER_PASSWORD`
     try {
         args = [`login`, `--config`, `${kcpconfigPath}`, `-u`, `${kcpUser}`, `-p`, `${kcpPassword}`]
         let output = await execa(`kcp`, args);
-        debug(`"kcp login --config ${kcpconfigPath} -u $KCP_TECH_USER_LOGIN -p $KCP_TECH_USER_PASSWORD" exited with code ${output.exitCode}`)
+        debug(`"${dummycmd}" exited with code ${output.exitCode}`)
         return output
     } catch (error) {
-        console.log(error)
-        debug.log(error)
         if (error.stderr === undefined) {
-            throw new Error(`failed to process output of "kcp ${args.join(' ')}": ${error}`);
+            throw new Error(`failed to process output of "${dummycmd}": ${error}`);
         }
-        throw new Error(`failed "kcp ${args.join(' ')}": ${error.stderr}`);
+        throw new Error(`failed "${dummycmd}": ${error.stderr}`);
     }
 };
 
 async function kcpUpgrade (kcpconfigPath, subaccount, kymaUpgradeVersion) {
-    debug(`Running kcpUpgrade...`)
+    debug(`Running kymaUpgrade...`)
     let args = []
     try {
         args = [`upgrade`, `kyma`, `--config`, `${kcpconfigPath}`, `--version`, `"${kymaUpgradeVersion}"`, `--target`, `subaccount=${subaccount}`]
@@ -72,7 +72,7 @@ async function ensureOrchestrationSucceeded(kcpconfigPath, orchenstrationID) {
       () => getOrchestrationStatus(kcpconfigPath, orchenstrationID),
       (res) => res && res.state && (res.state === "succeeded" || res.state === "failed"),
       1000*60*15, // 15 min
-      1000 * 5 // 5 seconds
+      1000 * 30 // 5 seconds
     );
   
     debug("KEB Orchestration Status:", res);
