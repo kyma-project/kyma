b9241d4b0015e39be0fb5ff240dd88f4b6f6f7b1
Kamil Kasperski <kamil.kasperski@sap.com>
2021-11-17 14:18:50 +0100

    Fast-integration hourly periodic for SKR nightly (#12514)
    * Fast-integration hourly periodic for SKR nightly
    
    * Refactoring tests
    
    * refactor skr-test
    
    use skr test as a module to re-use with multiple tests
    
    * test skr-nightly
    
    * fixups
    
    * tests tests
    
    * tests tests
    
    * Add mothership API url to the KCPWrapper
    
    * tests
    
    * deprovision broken skr
    
    * deprovision broken skr
    
    * testing
    
    * testing
    
    * testing
    
    * new approach for tests reuse
    
    * new approach for tests reuse

---
+++ tests/fast-integration/skr-nightly/provision/provision-skr-nightly.js
@@ -0,0 +1,77 @@
+const uuid = require("uuid");
+const {
+  provisionSKR,
+  deprovisionSKR,
+} = require("../../kyma-environment-broker");
+const {
+  unregisterKymaFromCompass,
+  addScenarioInCompass,
+  assignRuntimeToScenario
+} = require("../../compass");
+const {
+  initializeK8sClient,
+} = require("../../utils");
+
+const {
+  KCPConfig,
+  KCPWrapper,
+} = require("../../kcp/client")
+const {OIDCE2ETest, GatherOptions, WithRuntimeName, WithScenarioName, WithAppName, WithTestNS, CommerceMockTest,
+  gardener, keb, director
+} = require("../../skr-test");
+
+// Mocha root hook
+process.env.KCP_KEB_API_URL = `https://kyma-env-broker.` + keb.host;
+process.env.KCP_GARDENER_NAMESPACE = `garden-kyma-dev`;
+process.env.KCP_OIDC_ISSUER_URL = `https://kymatest.accounts400.ondemand.com`;
+process.env.KCP_MOTHERSHIP_API_URL = 'https://mothership-reconciler.cp.dev.kyma.cloud.sap/v1';
+process.env.KCP_KUBECONFIG_API_URL = 'https://kubeconfig-service.cp.dev.kyma.cloud.sap';
+const kcp = new KCPWrapper(KCPConfig.fromEnv());
+
+
+describe("SKR nightly", function () {
+  this.timeout(3600000 * 3); // 3h
+  this.slow(5000);
+  before(`Fetch last SKR and deprovision if needed`, async function () {
+    try {
+      let runtime;
+      this.options = GatherOptions(
+          WithRuntimeName('kyma-nightly'),
+          WithScenarioName('test-nightly'));
+
+      console.log('Login to KCP.');
+      await kcp.login();
+      let query = {
+        subaccount: keb.subaccountID,
+      }
+      console.log('Fetch last SKR.');
+      let runtimes = await kcp.runtimes(query);
+      if (runtimes.data) {
+        runtime = runtimes.data[0];
+      }
+      if (runtime) {
+        console.log('Deprovision last SKR.')
+        await deprovisionSKR(keb, runtime.instanceID);
+        await unregisterKymaFromCompass(director, this.options.scenarioName);
+      } else {
+        console.log("Deprovisioning not needed - no previous SKR found.");
+      }
+
+      console.log(`Provision SKR with runtime ID ${this.options.instanceID}`);
+      const customParams = {
+        oidc: this.options.oidc0,
+      };
+
+      let skr = await provisionSKR(keb, gardener, this.options.instanceID, this.options.runtimeName, null, null, customParams);
+      this.shoot = skr.shoot;
+      await addScenarioInCompass(director, this.options.scenarioName);
+      await assignRuntimeToScenario(director, this.shoot.compassID, this.options.scenarioName);
+      initializeK8sClient({ kubeconfig: this.shoot.kubeconfig });
+    }
+    catch (e) {
+      throw new Error(`before hook failed: ${e.toString()}`);
+    }
+  });
+  OIDCE2ETest();
+  CommerceMockTest();
+});
--- tests/fast-integration/test/1-commerce-mock.js
+++ tests/fast-integration/test/1-commerce-mock.js
@@ -20,56 +20,62 @@ const {
   lokiPortForward
 } = require("../logging");
 
-describe("CommerceMock tests", function () {
-  this.timeout(10 * 60 * 1000);
-  this.slow(5000);
-  const withCentralAppConnectivity = (process.env.WITH_CENTRAL_APP_CONNECTIVITY === "true");
-  const testNamespace = "test";
-  const testStartTimestamp = new Date().toISOString();
-  let initialRestarts = null;
-  let cancelPortForward = null;
+function commerceMockTests() {
+  describe("CommerceMock tests", function () {
+    this.timeout(10 * 60 * 1000);
+    this.slow(5000);
+    const withCentralAppConnectivity = (process.env.WITH_CENTRAL_APP_CONNECTIVITY === "true");
+    const testNamespace = "test";
+    const testStartTimestamp = new Date().toISOString();
+    let initialRestarts = null;
+    let cancelPortForward = null;
 
-  before(() => {
-    cancelPortForward = lokiPortForward();
-  });
+    before(() => {
+      cancelPortForward = lokiPortForward();
+    });
 
-  after(() => {
-    cancelPortForward();
-  });
+    after(() => {
+      cancelPortForward();
+    });
 
-  it("Listing all pods in cluster", async function () {
-    initialRestarts = await getContainerRestartsForAllNamespaces();
-  });
+    it("Listing all pods in cluster", async function () {
+      initialRestarts = await getContainerRestartsForAllNamespaces();
+    });
 
-  it("CommerceMock test fixture should be ready", async function () {
-    await ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralAppConnectivity).catch((err) => {
-      console.dir(err); // first error is logged
-      return ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralAppConnectivity);
+    it("CommerceMock test fixture should be ready", async function () {
+      await ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralAppConnectivity).catch((err) => {
+        console.dir(err); // first error is logged
+        return ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralAppConnectivity);
+      });
+    });
+
+    it("in-cluster event should be delivered (structured and binary mode)", async function () {
+      await checkInClusterEventDelivery(testNamespace);
+    });
+
+    it("function should be reachable through secured API Rule", async function () {
+      await checkFunctionResponse(testNamespace);
+    });
+
+    it("order.created.v1 event should trigger the lastorder function", async function () {
+      await sendEventAndCheckResponse();
+    });
+
+    it("Should print report of restarted containers, skipped if no crashes happened", async function () {
+      const afterTestRestarts = await getContainerRestartsForAllNamespaces();
+      printRestartReport(initialRestarts, afterTestRestarts);
+    });
+
+    it("Logs from commerce mock pod should be retrieved through Loki", async function() {
+      await checkLokiLogs(testStartTimestamp);
+    });
+
+    it("Test namespaces should be deleted", async function () {
+      await cleanMockTestFixture("mocks", testNamespace, true);
     });
   });
+}
 
-  it("in-cluster event should be delivered (structured and binary mode)", async function () {
-    await checkInClusterEventDelivery(testNamespace);
-  });
-
-  it("function should be reachable through secured API Rule", async function () {
-    await checkFunctionResponse(testNamespace);
-  });
-
-  it("order.created.v1 event should trigger the lastorder function", async function () {
-    await sendEventAndCheckResponse();
-  });
-
-  it("Should print report of restarted containers, skipped if no crashes happened", async function () {
-    const afterTestRestarts = await getContainerRestartsForAllNamespaces();
-    printRestartReport(initialRestarts, afterTestRestarts);
-  });
-
-  it("Logs from commerce mock pod should be retrieved through Loki", async function() {
-    await checkLokiLogs(testStartTimestamp);
-  });
-
-  it("Test namespaces should be deleted", async function () {
-    await cleanMockTestFixture("mocks", testNamespace, true);
-  });
-});
+module.exports = {
+  commerceMockTests,
+}
