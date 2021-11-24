78f895abfef3c54cc850a07a6879115cba38782a
Kamil Kasperski <kamil.kasperski@sap.com>
2021-11-24 09:54:00 +0100

    Run SKR tests on existing SKR

--- tests/fast-integration/skr-test/test-existing.js
+++ tests/fast-integration/skr-test/test-existing.js
@@ -1,8 +1,9 @@
 const {
-    gardener, GatherOptions,
+    gardener, GatherOptions, director,
 } = require('./helpers')
 const {getEnvOrDefault, initializeK8sClient} = require("../utils");
 const {CommerceMockTest} = require("./skr-test");
+const {addScenarioInCompass, assignRuntimeToScenario} = require("../compass");
 
 const shootName = getEnvOrDefault("SHOOT","something");
 
@@ -11,8 +12,15 @@ describe(`Execute tests on existing SKR`, function () {
     this.slow(5000);
     before(async function () {
         this.options = GatherOptions();
-        this.shoot = await gardener.getShoot(shootName);
-        initializeK8sClient({kubeconfig: this.shoot.kubeconfig});
+        try {
+            this.shoot = await gardener.getShoot(shootName);
+            await addScenarioInCompass(director, this.options.scenarioName);
+            await assignRuntimeToScenario(director, this.shoot.compassID, this.shoot.scenarioName);
+            initializeK8sClient({kubeconfig: this.shoot.kubeconfig});
+        } catch (e) {
+            throw e
+        }
+
     })
     CommerceMockTest();
 })
