2611ef6055046d23217146c44dee386c1c625b26
Friedrich Wilken <j.wilken@posteo.net>
2021-11-24 11:14:47 +0100

    switch from Mutex to RWMutex

--- components/eventing-controller/testing/safesubscriptions.go
+++ components/eventing-controller/testing/safesubscriptions.go
@@ -9,35 +9,35 @@ import (
 
 // SafeSubscription encapsulates Subscriptions to provide mutual exclusion.
 type SafeSubscription struct {
-	mutex         *sync.Mutex
+	mutex         *sync.RWMutex
 	subscriptions map[string]*bebtypes.Subscription
 }
 
 // NewSafeSubscription returns a new SafeSubscription.
 func NewSafeSubscription() *SafeSubscription {
 	return &SafeSubscription{
-		&sync.Mutex{}, make(map[string]*bebtypes.Subscription),
+		&sync.RWMutex{}, make(map[string]*bebtypes.Subscription),
 	}
 }
 
 // GetSubscription returns a Subscription via the corresponding the key.
 func (s *SafeSubscription) GetSubscription(key string) *bebtypes.Subscription {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return s.subscriptions[key]
 }
 
 // DeleteSubscription deletes a Subscription via the corresponding key.
 func (s *SafeSubscription) DeleteSubscription(key string) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	delete(s.subscriptions, key)
 }
 
 // DeleteSubscriptionsByName deletes all Subscriptions that contain the substring 'name' in their own name.
 func (s *SafeSubscription) DeleteSubscriptionsByName(name string) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	for k := range s.subscriptions {
 		if strings.Contains(k, name) {
 			delete(s.subscriptions, k)
@@ -47,7 +47,7 @@ func (s *SafeSubscription) DeleteSubscriptionsByName(name string) {
 
 // PutSubscription sets a Subscription of SafeSubscription via the corresponding key.
 func (s *SafeSubscription) PutSubscription(key string, subscription *bebtypes.Subscription) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	s.subscriptions[key] = subscription
 }
