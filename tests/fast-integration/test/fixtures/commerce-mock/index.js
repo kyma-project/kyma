057226a433a420a25e75579200c3c9a3f6021159
Benjamin Lindner <benjamin.lindner@sap.com>
2021-11-26 16:09:42 +0100

    Revert debugging output

--- tests/fast-integration/test/fixtures/commerce-mock/index.js
+++ tests/fast-integration/test/fixtures/commerce-mock/index.js
@@ -359,11 +359,8 @@ async function connectMockCompass(client, appName, scenarioName, mockHost, targe
 }
 
 async function connectCommerceMock(mockHost, tokenData) {
-  debug("connectCommerceMock():")
   const url = `https://${mockHost}/connection`;
-  debug(`Url: ${url}`)
   const body = tokenData;
-  debug(`HERE! - Body/Tokendata: ${body}`)
   const params = {
     headers: {
       "Content-Type": "application/json"
