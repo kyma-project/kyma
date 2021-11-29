3a5d79ed2502c791bbcb5eefab84f90a88cfa803
Pooya Salehi <pxsalehi@users.noreply.github.com>
2021-11-29 13:17:22 +0100

    Allow using different mock namespaces (#12648)
    * allow setting mock namespace
    
    * Update tests/fast-integration/README.md
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>

--- tests/fast-integration/test/fixtures/commerce-mock/index.js
+++ tests/fast-integration/test/fixtures/commerce-mock/index.js
@@ -77,7 +77,6 @@ const lastorderFunctionYaml = fs.readFileSync(
   }
 );
 
-const commerceObjs = k8s.loadAllYaml(commerceMockYaml);
 const applicationObjs = k8s.loadAllYaml(applicationMockYaml);
 const lastorderObjs = k8s.loadAllYaml(lastorderFunctionYaml);
 let eventMeshSourceNamespace = "/default/sap.kyma/tunas-prow"
@@ -103,8 +102,13 @@ function prepareLastorderObjs(type = 'standard', appName = 'commerce') {
   }
 }
 
-async function checkFunctionResponse(functionNamespace) {
-  const vs = await waitForVirtualService("mocks", "commerce-mock");
+// Allows creating Commerce Mock objects in a specific namespace
+function prepareCommerceObjs(mockNamespace) {
+  return k8s.loadAllYaml(commerceMockYaml.toString().replace(/%%MOCK_NAMESPACE%%/g, mockNamespace))
+}
+
+async function checkFunctionResponse(functionNamespace, mockNamespace = 'mocks') {
+  const vs = await waitForVirtualService(mockNamespace, "commerce-mock");
   const mockHost = vs.spec.hosts[0];
   const host = mockHost.split(".").slice(1).join(".");
 
@@ -143,8 +147,8 @@ async function checkFunctionResponse(functionNamespace) {
   expect(errorOccurred).to.be.equal(true);
 }
 
-async function sendEventAndCheckResponse() {
-  const vs = await waitForVirtualService("mocks", "commerce-mock");
+async function sendEventAndCheckResponse(mockNamespace = 'mocks') {
+  const vs = await waitForVirtualService(mockNamespace, "commerce-mock");
   const mockHost = vs.spec.hosts[0];
   const host = mockHost.split(".").slice(1).join(".");
 
@@ -196,9 +200,9 @@ async function sendEventAndCheckResponse() {
   );
 }
 
-async function sendLegacyEventAndCheckTracing() {
+async function sendLegacyEventAndCheckTracing(mockNamespace = 'mocks') {
   // Send an event and get it back from the lastorder function
-  const res = await sendEventAndCheckResponse();
+  const res = await sendEventAndCheckResponse(mockNamespace);
   expect(res.data).to.have.nested.property("event.headers.x-b3-traceid");
   expect(res.data).to.have.nested.property("podName");
 
@@ -515,7 +519,7 @@ async function ensureCommerceMockLocalTestFixture(mockNamespace, targetNamespace
 
 async function provisionCommerceMockResources(appName, mockNamespace, targetNamespace, functionObjs = lastorderObjs) {
   await k8sApply([namespaceObj(mockNamespace), namespaceObj(targetNamespace)]);
-  await k8sApply(commerceObjs);
+  await k8sApply(prepareCommerceObjs(mockNamespace));
   await k8sApply(functionObjs, targetNamespace, true);
   await k8sApply([
     eventingSubscription(
@@ -524,8 +528,8 @@ async function provisionCommerceMockResources(appName, mockNamespace, targetName
       "order-created",
       targetNamespace)
   ]);
-  await waitForDeployment("commerce-mock", "mocks", 120 * 1000);
-  const vs = await waitForVirtualService("mocks", "commerce-mock");
+  await waitForDeployment("commerce-mock", mockNamespace, 120 * 1000);
+  const vs = await waitForVirtualService(mockNamespace, "commerce-mock");
   const mockHost = vs.spec.hosts[0];
   await retryPromise(
     () =>
@@ -569,7 +573,7 @@ function cleanMockTestFixture(mockNamespace, targetNamespace, wait = true) {
   return deleteNamespaces([mockNamespace, targetNamespace], wait);
 }
 
-async function deleteMockTestFixture(targetNamespace) {
+async function deleteMockTestFixture(mockNamespace) {
   const serviceBindingUsage = {
     apiVersion: "servicecatalog.kyma-project.io/v1alpha1",
     kind: "ServiceBindingUsage",
@@ -579,7 +583,7 @@ async function deleteMockTestFixture(targetNamespace) {
       usedBy: { kind: "serverless-function", name: "lastorder" },
     },
   };
-  await k8sDelete([serviceBindingUsage], targetNamespace);
+  await k8sDelete([serviceBindingUsage], mockNamespace);
   const serviceBinding = {
     apiVersion: "servicecatalog.k8s.io/v1beta1",
     kind: "ServiceBinding",
@@ -588,9 +592,9 @@ async function deleteMockTestFixture(targetNamespace) {
       instanceRef: { name: "commerce" },
     },
   };
-  await k8sDelete([serviceBinding], targetNamespace, false);
+  await k8sDelete([serviceBinding], mockNamespace, false);
   await k8sDelete(lastorderObjs)
-  await k8sDelete(commerceObjs)
+  await k8sDelete(prepareCommerceObjs(mockNamespace))
   await k8sDelete(applicationObjs)
 }
 
