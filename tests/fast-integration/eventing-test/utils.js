24fba1bd73904c9c4e195df371e4838c4e400ba6
VladislavPaskar <vladislav.paskar@sap.com>
2021-12-23 09:28:28 +0100

    fix make targets

--- tests/fast-integration/eventing-test/utils.js
+++ tests/fast-integration/eventing-test/utils.js
@@ -1,7 +1,7 @@
 const suffix = 'eventing';
-const appName = 'app-${suffix}';
+const appName = `app-${suffix}`;
 const scenarioName = `test-${suffix}`;
-const testNamespace = `test-${suffix}`;
+const testNamespace = 'test';
 const mockNamespace = process.env.MOCK_NAMESPACE || 'mocks'
 const isSKR = process.env.KYMA_TYPE === "SKR";
 const backendK8sSecretName = process.env.BACKEND_SECRET_NAME || "eventing-backend";
