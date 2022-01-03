f9cf4d8c005f9be9b845abb50bbc75e41059bed0
Christoph Voigt <voigt.christoph@gmail.com>
2022-01-03 14:56:31 +0100

    add skr provisioning parameter info to client lib (#12971)

--- tests/fast-integration/kyma-environment-broker/client.js
+++ tests/fast-integration/kyma-environment-broker/client.js
@@ -89,7 +89,7 @@ class KEBClient {
   }
 
   async provisionSKR(name, instanceID, platformCreds, btpOperatorCreds, customParams) {
-    debug(`Provision SKR with Custom Parameters ${customParams}`)
+    debug(`Provision SKR with Custom Parameters ${JSON.stringify(customParams)}`)
     const payload = {
       service_id: KYMA_SERVICE_ID,
       plan_id: this.planID,
