7d904c7fde1cfee7d42e78ca8f8daf4a9cefe45d
Rafał Foks <r.fok74@gmail.com>
2022-01-10 15:43:51 +0100

    fix imports

--- components/compass-runtime-agent/internal/kyma/service.go
+++ components/compass-runtime-agent/internal/kyma/service.go
@@ -2,6 +2,7 @@ package kyma
 
 import (
 	"fmt"
+
 	"github.com/kyma-project/kyma/components/application-operator/pkg/apis/applicationconnector/v1alpha1"
 	log "github.com/sirupsen/logrus"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
