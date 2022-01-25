a8a9bd684ad6482b53b5fad3d1cc8d3ecc630f50
Jan Mędrek <jan.medrek@sap.com>
2022-01-25 14:43:01 +0100

    ignore app deletion error (#13167)
    * ignore app deletion error
    
    * remove ignore404 dependency

--- tests/fast-integration/test/fixtures/commerce-mock/index.js
+++ tests/fast-integration/test/fixtures/commerce-mock/index.js
@@ -12,7 +12,6 @@ axios.defaults.httpsAgent = httpsAgent;
 const {
   retryPromise,
   convertAxiosError,
-  ignore404,
   sleep,
   k8sApply,
   waitForServiceClass,
@@ -700,7 +699,7 @@ async function cleanMockTestFixture(mockNamespace, targetNamespace, wait = true)
       },
     });
   } catch (err) {
-    ignore404(err);
+    // Ignore delete error
   }
 
   debug('Deleting test namespaces');
