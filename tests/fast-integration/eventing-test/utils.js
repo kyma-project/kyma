b756dbc011285fa3865a18b5ff691861653fb2ce
VladislavPaskar <vladislav.paskar@sap.com>
2022-01-27 23:03:52 +0100

    bring back missing tracing and monitoring tests, fix compass testing

--- tests/fast-integration/eventing-test/utils.js
+++ tests/fast-integration/eventing-test/utils.js
@@ -5,24 +5,36 @@ const {
 
 const {
   debug,
-  deleteEventingBackendK8sSecret, getShootNameFromK8sServerUrl,
+  deleteEventingBackendK8sSecret,
+  getShootNameFromK8sServerUrl,
+  getEnvOrThrow,
 } = require('../utils');
 
 const {DirectorClient, DirectorConfig} = require('../compass');
 const {GardenerClient, GardenerConfig} = require('../gardener');
-const suffix = 'evnt';
+const isSKR = process.env.KYMA_TYPE === 'SKR';
+const suffix = getSuffix(isSKR);
 const appName = `app-${suffix}`;
 const scenarioName = `test-${suffix}`;
 const testNamespace = `test-${suffix}`;
 const mockNamespace = process.env.MOCK_NAMESPACE || 'mocks';
-const isSKR = process.env.KYMA_TYPE === 'SKR';
 const backendK8sSecretName = process.env.BACKEND_SECRET_NAME || 'eventing-backend';
 const backendK8sSecretNamespace = process.env.BACKEND_SECRET_NAMESPACE || 'default';
 const eventMeshSecretFilePath = process.env.EVENTMESH_SECRET_FILE || '';
 const DEBUG_MODE = process.env.DEBUG;
 const timeoutTime = 10 * 60 * 1000;
 const slowTime = 5000;
-const fatalErrCode = 'ERR_MOCHA_FATAL';
+
+function getSuffix(isSKR) {
+  let suffix;
+  if (isSKR) {
+    suffix = getEnvOrThrow('TEST_SUFFIX');
+  } else {
+    suffix = 'evnt';
+  }
+  return suffix;
+}
+
 
 async function cleanupTestingResources() {
   if (isSKR) {
@@ -62,6 +74,5 @@ module.exports = {
   DEBUG_MODE,
   timeoutTime,
   slowTime,
-  fatalErrCode,
   cleanupTestingResources,
 };
