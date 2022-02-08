d5178793ffe2f29b8865670c86445a8b8c0d5b3d
Muhammad Faizan <m.faizan@sap.com>
2022-02-08 11:24:55 +0100

    BEB reconciler restructured

--- components/eventing-controller/controllers/subscription/beb/equality.go
+++ components/eventing-controller/controllers/subscription/beb/equality.go
@@ -1,6 +1,8 @@
 package beb
 
 import (
+	"reflect"
+
 	eventingv1alpha1 "github.com/kyma-project/kyma/components/eventing-controller/api/v1alpha1"
 )
 
@@ -47,4 +49,15 @@ func isAPIRuleStatusChanged(existing, expected *eventingv1alpha1.Subscription) b
 		return true
 	}
 	return false
+}
+
+func isSubscriptionStatusEqual(oldStatus, newStatus eventingv1alpha1.SubscriptionStatus) bool {
+	oldStatusWithoutCond := oldStatus.DeepCopy()
+	newStatusWithoutCond := newStatus.DeepCopy()
+
+	// remove conditions, so that we don't compare them
+	oldStatusWithoutCond.Conditions = []eventingv1alpha1.Condition{}
+	newStatusWithoutCond.Conditions = []eventingv1alpha1.Condition{}
+
+	return reflect.DeepEqual(oldStatusWithoutCond, newStatusWithoutCond) && conditionsEquals(oldStatus.Conditions, newStatus.Conditions)
 }
