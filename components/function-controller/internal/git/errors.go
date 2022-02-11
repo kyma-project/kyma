b2b1cbfbf4ae29a2e3a52f1b5fd9fad0ae14a9b0
Damian Badura <damian.badura@sap.com>
2022-02-11 11:42:23 +0100

    remove wrong error code

--- components/function-controller/internal/git/errors.go
+++ components/function-controller/internal/git/errors.go
@@ -8,9 +8,7 @@ import (
 
 //Unrecoverable means that something is wrong with configuration of git function CR
 //and cannot be fixed without changing the function cr. For example, branch doesn't exist.
-var notRecoverableErrors = []git2go.ErrorCode{
-	git2go.ErrorCodeNotFound, git2go.ErrorCodeInvalidSpec,
-}
+var notRecoverableErrors = []git2go.ErrorCode{git2go.ErrorCodeInvalidSpec}
 
 func IsNotRecoverableError(err error) bool {
 	gitErr := getGitErr(err)
