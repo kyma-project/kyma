307553200db3661f2a6a50c7d352e10d265b6fd3
Andreas Thaler <andreas.thaler01@sap.com>
2022-02-15 14:04:49 +0100

    fix

--- resources/logging/charts/loki/files/chunkCleaner.sh
+++ resources/logging/charts/loki/files/chunkCleaner.sh
@@ -7,27 +7,30 @@ function updateCurrentSize() {
 
 trap 'EXIT=true' SIGTERM SIGINT
 
-echo "VOLUME_SIZE: ${VOLUME_SIZE}"
-VOLUME_SIZE_GB=$(echo "${VOLUME_SIZE}" | sed 's|Gi||g')
-echo "VOLUME_SIZE_GB: ${VOLUME_SIZE_GB} GB"
-VOLUME_SIZE_KB=$(( ${VOLUME_SIZE_GB} * 1024 * 1024 ))
+AVAILABLE_SIZE_KB=$(df /data | tail -n 1 | awk '{print $4}')
+echo "AVAILABLE_SIZE_KB: ${AVAILABLE_SIZE_KB} KB"
+
+updateCurrentSize
+echo "CURRENT_SIZE_KB: ${CURRENT_SIZE_KB} KB"
+
+VOLUME_SIZE_KB=$(( ${AVAILABLE_SIZE_KB} + ${CURRENT_SIZE_KB} ))
 echo "VOLUME_SIZE_KB: ${VOLUME_SIZE_KB} KB"
 
 echo "TARGET_SIZE_PCT: ${TARGET_SIZE_PCT} %"
 TARGET_SIZE_KB=$(( ${VOLUME_SIZE_KB} - (${VOLUME_SIZE_KB} * ${TARGET_SIZE_PCT} / 100) ))
 echo "TARGET_SIZE_KB: ${TARGET_SIZE_KB} KB"
 
-updateCurrentSize
-echo "CURRENT_SIZE_KB: ${CURRENT_SIZE_KB} KB"
-
 while [ -z "${EXIT}" ]
 do
     updateCurrentSize
     while [ "${TARGET_SIZE_KB}" -lt "${CURRENT_SIZE_KB}" ] && [ -z "${EXIT}" ]
     do
-        OLDEST=$(ls -t /data/loki/chunks | tail -1)
-        echo "Current size ${CURRENT_SIZE_KB}KB is bigger then ${TARGET_SIZE_KB}KB, deleting file /data/loki/chunks/${OLDEST}"
-        rm "/data/loki/chunks/${OLDEST}"
+        echo "Current size ${CURRENT_SIZE_KB}KB is bigger then ${TARGET_SIZE_KB}KB, deleting ${BATCH_SIZE} files"
+        for OLDEST in $(ls -t /data/loki/chunks | tail -${BATCH_SIZE})
+        do
+            echo "Deleting file /data/loki/chunks/${OLDEST}"
+            rm "/data/loki/chunks/${OLDEST}"
+        done
         updateCurrentSize
     done
     sleep "${SLEEP}"
