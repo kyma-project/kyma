6af9462798811f4aa9911cd89876943c9a7f927b
Friedrich Wilken <j.wilken@posteo.net>
2022-02-21 14:58:23 +0100

    renamed struct

--- components/eventing-controller/controllers/subscription/beb/equality.go
+++ components/eventing-controller/controllers/subscription/beb/equality.go
@@ -3,18 +3,18 @@ package beb
 import (
 	"reflect"
 
-	eventingv1alpha1 "github.com/kyma-project/kyma/components/eventing-controller/api/v1alpha1"
+	eventing1alpha1 "github.com/kyma-project/kyma/components/eventing-controller/api/v1alpha1"
 )
 
 // conditionsEquals checks if two list of conditions are equal.
-func conditionsEquals(existing, expected []eventingv1alpha1.Condition) bool {
+func conditionsEquals(existing, expected []eventing1alpha1.Condition) bool {
 	// not equal if length is different
 	if len(existing) != len(expected) {
 		return false
 	}
 
 	// compile map of Conditions per ConditionType
-	existingMap := make(map[eventingv1alpha1.ConditionType]eventingv1alpha1.Condition, len(existing))
+	existingMap := make(map[eventing1alpha1.ConditionType]eventing1alpha1.Condition, len(existing))
 	for _, value := range existing {
 		existingMap[value.Type] = value
 	}
@@ -29,7 +29,7 @@ func conditionsEquals(existing, expected []eventingv1alpha1.Condition) bool {
 }
 
 // conditionsEquals checks if two conditions are equal.
-func conditionEquals(existing, expected eventingv1alpha1.Condition) bool {
+func conditionEquals(existing, expected eventing1alpha1.Condition) bool {
 	isTypeEqual := existing.Type == expected.Type
 	isStatusEqual := existing.Status == expected.Status
 	isReasonEqual := existing.Reason == expected.Reason
@@ -42,13 +42,13 @@ func conditionEquals(existing, expected eventingv1alpha1.Condition) bool {
 	return true
 }
 
-func isSubscriptionStatusEqual(oldStatus, newStatus eventingv1alpha1.SubscriptionStatus) bool {
+func isSubscriptionStatusEqual(oldStatus, newStatus eventing1alpha1.SubscriptionStatus) bool {
 	oldStatusWithoutCond := oldStatus.DeepCopy()
 	newStatusWithoutCond := newStatus.DeepCopy()
 
 	// remove conditions, so that we don't compare them
-	oldStatusWithoutCond.Conditions = []eventingv1alpha1.Condition{}
-	newStatusWithoutCond.Conditions = []eventingv1alpha1.Condition{}
+	oldStatusWithoutCond.Conditions = []eventing1alpha1.Condition{}
+	newStatusWithoutCond.Conditions = []eventing1alpha1.Condition{}
 
 	return reflect.DeepEqual(oldStatusWithoutCond, newStatusWithoutCond) && conditionsEquals(oldStatus.Conditions, newStatus.Conditions)
 }
