1deeaca45f3c1bb07974ee00760049cb0fc2cbeb
Nils Seip <nils.seip@sap.com>
2022-03-08 13:29:28 +0100

    Make wants givens in TestConnect

--- components/event-publisher-proxy/pkg/nats/connect_test.go
+++ components/event-publisher-proxy/pkg/nats/connect_test.go
@@ -13,23 +13,22 @@ import (
 
 func TestConnect(t *testing.T) {
 	testCases := []struct {
-		name                     string
-		// TODO(nils): these are actually givens
-		wantRetryOnFailedConnect bool
-		wantMaxReconnect         int
-		wantReconnectWait        time.Duration
+		name                      string
+		givenRetryOnFailedConnect bool
+		givenMaxReconnect         int
+		givenReconnectWait        time.Duration
 	}{
 		{
-			name:                     "do not retry failed connections",
-			wantRetryOnFailedConnect: false,
-			wantMaxReconnect:         0,
-			wantReconnectWait:        time.Millisecond,
+			name:                      "do not retry failed connections",
+			givenRetryOnFailedConnect: false,
+			givenMaxReconnect:         0,
+			givenReconnectWait:        time.Millisecond,
 		},
 		{
-			name:                     "keep retrying failed connections",
-			wantRetryOnFailedConnect: true,
-			wantMaxReconnect:         -1,
-			wantReconnectWait:        time.Millisecond,
+			name:                      "keep retrying failed connections",
+			givenRetryOnFailedConnect: true,
+			givenMaxReconnect:         -1,
+			givenReconnectWait:        time.Millisecond,
 		},
 	}
 	for _, tc := range testCases {
@@ -42,15 +41,15 @@ func TestConnect(t *testing.T) {
 			clientURL := natsServer.ClientURL()
 			assert.NotEmpty(t, clientURL)
 
-			connection, err := pkgnats.Connect(clientURL, tc.wantRetryOnFailedConnect, tc.wantMaxReconnect, tc.wantReconnectWait)
+			connection, err := pkgnats.Connect(clientURL, tc.givenRetryOnFailedConnect, tc.givenMaxReconnect, tc.givenReconnectWait)
 			assert.Nil(t, err)
 			assert.NotNil(t, connection)
 
 			assert.Equal(t, connection.Status(), nats.CONNECTED)
 			assert.Equal(t, clientURL, connection.Opts.Servers[0])
-			assert.Equal(t, tc.wantRetryOnFailedConnect, connection.Opts.RetryOnFailedConnect)
-			assert.Equal(t, tc.wantMaxReconnect, connection.Opts.MaxReconnect)
-			assert.Equal(t, tc.wantReconnectWait, connection.Opts.ReconnectWait)
+			assert.Equal(t, tc.givenRetryOnFailedConnect, connection.Opts.RetryOnFailedConnect)
+			assert.Equal(t, tc.givenMaxReconnect, connection.Opts.MaxReconnect)
+			assert.Equal(t, tc.givenReconnectWait, connection.Opts.ReconnectWait)
 		})
 	}
 }
