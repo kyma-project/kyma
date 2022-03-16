e6ad22609ea73b320112bce6f8b791f3cb952729
Pranav Shankar <pranavshankar.work@gmail.com>
2022-03-16 09:23:40 +0100

    logging tests in upgrade tests

--- tests/fast-integration/test/test.js
+++ tests/fast-integration/test/test.js
@@ -6,13 +6,9 @@ const {
 const {
   monitoringTests,
 } = require('../monitoring');
-const {
-  loggingTests,
-} = require('../logging');
 
 describe('Executing Standard Testsuite:', function() {
   commerceMockTests();
   gettingStartedGuideTests();
   monitoringTests();
-  loggingTests();
 });
