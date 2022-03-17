2746904f5c09c3e0f4842ed68827c6366e626cb6
Damian Badura <damian.badura@sap.com>
2022-03-17 14:26:05 +0100

    bump images and add support for nodejs12

--- components/function-runtimes/nodejs12/kubeless/kubeless_rt/lib/tracer.js
+++ components/function-runtimes/nodejs12/kubeless/kubeless_rt/lib/tracer.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const axios = require('axios');
+
 const opentelemetry = require('@opentelemetry/api');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { NodeTracerProvider } = require('@opentelemetry/node');
@@ -7,8 +9,34 @@ const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { B3Propagator,B3InjectEncoding } = require("@opentelemetry/propagator-b3");
+const  {NoopTracerProvider} = require('@opentelemetry/api')
 
-module.exports = (serviceName, endpoint) => {
+const TRACER_SAMPLE_HEADER= "x-b3-sampled"
+
+class ServerlessTracerProvider {
+  constructor(serviceName, endpoint) {
+    this.noopTracerProvider = new NoopTracerProvider().getTracer(serviceName.concat('-tracer'),'0.0.1');
+    this.tracerProvider = new NoopTracerProvider().getTracer(serviceName.concat('-tracer'),'0.0.1')
+    axios(endpoint)
+        .catch((err) => {
+          // 405 is the right status code for the GET method if jaeger service exists
+          // because the only allowed method is POST and usage of other methods are not allowe
+          // https://github.com/jaegertracing/jaeger/blob/7872d1b07439c3f2d316065b1fd53e885b26a66f/cmd/collector/app/handler/http_handler.go#L60
+          if (err.response && err.response.status === 405) {
+            this.tracerProvider = getTracerProvider(serviceName, endpoint)
+          }
+        });
+  }
+
+  getTracer(req) {
+      if (req[TRACER_SAMPLE_HEADER] === "1") {
+          return this.tracerProvider
+      }
+      return this.noopTracerProvider
+  }
+}
+
+function getTracerProvider (serviceName, endpoint) {
   const provider = new NodeTracerProvider();
 
   registerInstrumentations({
@@ -38,4 +66,10 @@ module.exports = (serviceName, endpoint) => {
   });
 
   return opentelemetry.trace.getTracer(serviceName.concat('-tracer'),'0.0.1');
-};
+}
+
+// export default ServerlessTracerProvider
+
+module.exports = {
+  ServerlessTracerProvider
+}
