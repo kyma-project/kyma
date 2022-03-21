838618e3014923d96fa5d4d14b2731af86894ce7
Przemyslaw Golicz <przemyslaw.golicz@sap.com>
2022-03-21 17:02:38 +0100

    test fix for generate runtime_id when it is missing

--- components/compass-runtime-agent/cmd/main.go
+++ components/compass-runtime-agent/cmd/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"context"
 	"encoding/json"
-
-	"github.com/google/uuid"
 	"github.com/kyma-incubator/compass/components/director/pkg/correlation"
 	"github.com/kyma-incubator/compass/components/director/pkg/str"
 
+	"github.com/google/uuid"
 	"github.com/kyma-project/kyma/components/compass-runtime-agent/internal/certificates"
 	"github.com/kyma-project/kyma/components/compass-runtime-agent/internal/compass"
 	"github.com/kyma-project/kyma/components/compass-runtime-agent/internal/compass/cache"
@@ -108,7 +107,14 @@ func main() {
 	exitOnError(err, "Failed to initialize Controller")
 
 	rtmConfig, err := configProvider.GetRuntimeConfig()
-	correlationID := rtmConfig.RuntimeId + "_" + uuid.New().String()
+
+	var correlationID string
+
+	if err == nil {
+		correlationID = rtmConfig.RuntimeId + "_" + uuid.New().String()
+	} else {
+		correlationID = uuid.New().String() + "_" + uuid.New().String()
+	}
 	ctx := correlation.SaveCorrelationIDHeaderToContext(context.Background(), str.Ptr(correlation.RequestIDHeaderKey), str.Ptr(correlationID))
 
 	log.Infoln("Initializing Compass Connection CR")
