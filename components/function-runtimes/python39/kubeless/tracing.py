d8f6e84e73a91ede648db6d734ed98deb796c908
Damian Badura <45110612+dbadura@users.noreply.github.com>
2022-03-22 15:36:43 +0100

    Add support for tracing in Python runtime (#13621)
    * initial implementation
    
    * improve runtime
    
    * bump python runtime image
    
    * little improvements
    
    * remove print

--- components/function-runtimes/python39/kubeless/tracing.py
+++ components/function-runtimes/python39/kubeless/tracing.py
@@ -1,27 +1,65 @@
-import os
-import requests
-from opentelemetry import trace
-from opentelemetry.trace import context_api
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.sdk.trace import TracerProvider, _Span
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.propagate import set_global_textmap
-from opentelemetry.propagators.b3 import B3MultiFormat
+import logging
 from contextlib import contextmanager
-from opentelemetry.propagate import extract
-from opentelemetry.trace.propagation import _SPAN_KEY
 from typing import Iterator
 
-podName = os.getenv('HOSTNAME')
-serviceNamespace = os.getenv('SERVICE_NAMESPACE')
-jaegerEndpoint = os.getenv('JAEGER_SERVICE_ENDPOINT')
+import requests
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.propagate import extract
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.b3 import B3MultiFormat
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider, _Span
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import context_api
+from opentelemetry.trace.propagation import _SPAN_KEY
 
-def is_jaeger_available() -> bool:
+_TRACING_SAMPLE_HEADER = "x-b3-sampled"
+
+
+class ServerlessTracerProvider:
+    def __init__(self, jaeger_endpoint: str, service_name: str):
+        self.noop_tracer = trace.NoOpTracer()
+        if _is_jaeger_available(jaeger_endpoint):
+            self.tracer = _get_tracer(jaeger_endpoint, service_name)
+        else:
+            logging.info("jaeger is not available")
+            self.tracer = trace.NoOpTracer()
+
+    def get_tracer(self, req):
+        val = req.get_header(_TRACING_SAMPLE_HEADER)
+        if val is not None and val == "1":
+            return self.tracer
+
+        return self.noop_tracer
+
+
+def _get_tracer(jaeger_endpoint: str, service_name: str) -> trace.Tracer:
+    set_global_textmap(B3MultiFormat())
+    RequestsInstrumentor().instrument()
+
+    trace.set_tracer_provider(
+        TracerProvider(
+            resource=Resource.create({SERVICE_NAME: service_name})
+        )
+    )
+
+    jaeger_exporter = JaegerExporter(
+        collector_endpoint=jaeger_endpoint + '?format=jaeger.thrift',
+    )
+
+    span_processor = BatchSpanProcessor(jaeger_exporter)
+
+    trace.get_tracer_provider().add_span_processor(span_processor)
+
+    return trace.get_tracer(__name__)
+
+
+def _is_jaeger_available(jaegerEndpoint) -> bool:
     try:
         res = requests.get(jaegerEndpoint, timeout=2)
-    
+
         # 405 is the right status code for the GET method if jaeger service exists 
         # because the only allowed method is POST and usage of other methods are not allowe
         # https://github.com/jaegertracing/jaeger/blob/7872d1b07439c3f2d316065b1fd53e885b26a66f/cmd/collector/app/handler/http_handler.go#L60
@@ -32,29 +70,6 @@ def is_jaeger_available() -> bool:
 
     return False
 
-def get_tracer() -> trace.Tracer:
-    set_global_textmap(B3MultiFormat())
-    
-    RequestsInstrumentor().instrument()
-
-    # remove generated pods suffix ( two last sections )
-    deploymentName = '-'.join(podName.split('-')[0:podName.count('-')-1])
-
-    trace.set_tracer_provider(
-        TracerProvider(
-            resource=Resource.create({SERVICE_NAME: '.'.join([deploymentName, serviceNamespace])})
-        )
-    )
-
-    jaeger_exporter = JaegerExporter(
-        collector_endpoint = jaegerEndpoint + '?format=jaeger.thrift',
-    )
-
-    span_processor = BatchSpanProcessor(jaeger_exporter)
-
-    trace.get_tracer_provider().add_span_processor(span_processor)
-
-    return trace.get_tracer(__name__)
 
 @contextmanager  # type: ignore
 def set_req_context(req) -> Iterator[trace.Span]:
@@ -65,7 +80,7 @@ def set_req_context(req) -> Iterator[trace.Span]:
     in the separated thread we have to propagate the context manually.
     '''
     span = _Span(
-        "request-span", 
+        "request-span",
         trace.get_current_span(
             extract(req.headers)
         ).get_span_context()
