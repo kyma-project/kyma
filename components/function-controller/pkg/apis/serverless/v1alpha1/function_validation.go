147cebf43ffb406994a17328ff05c0b4a9a67910
moelsayed <m.elsayed@gmail.com>
2022-03-27 16:21:52 +0200

    review comments

--- components/function-controller/pkg/apis/serverless/v1alpha1/function_validation.go
+++ components/function-controller/pkg/apis/serverless/v1alpha1/function_validation.go
@@ -213,33 +213,9 @@ type property struct {
 	value string
 }
 
-func validateIfMissingFields(properties ...property) error {
-	var allErrs []string
-	for _, item := range properties {
-		if strings.TrimSpace(item.value) != "" {
-			continue
-		}
-		allErrs = append(allErrs, fmt.Sprintf("%s is required", item.name))
-	}
-
-	return returnAllErrs("missing required fields", allErrs)
-}
-
 func (in *Repository) validateRepository(_ *ValidationConfig) error {
 	return validateIfMissingFields([]property{
 		{name: "spec.baseDir", value: in.BaseDir},
 		{name: "spec.reference", value: in.Reference},
 	}...)
-}
-
-func returnAllErrs(msg string, allErrs []string) error {
-	if len(allErrs) == 0 {
-		return nil
-	}
-
-	if len(msg) > 0 {
-		return fmt.Errorf("%s: %v", msg, allErrs)
-	}
-
-	return fmt.Errorf("%v", allErrs)
 }
