0c01096a3580aa5729d324294f1a46837763cde2
Rafał Foks <r.fok74@gmail.com>
2022-04-13 14:45:55 +0200

    remove rafter from options

--- components/compass-runtime-agent/cmd/init.go
+++ components/compass-runtime-agent/cmd/init.go
@@ -49,7 +49,7 @@ func k8sResourceClients(k8sConfig *restclient.Config) (*k8sResourceClientSets, e
 	}, nil
 }
 
-func createKymaService(k8sResourceClients *k8sResourceClientSets, uploadServiceUrl string, integrationNamespace string, centralGatewayServiceUrl string, appTLSSkipVerify bool) (kyma.Service, error) {
+func createKymaService(k8sResourceClients *k8sResourceClientSets, integrationNamespace string, centralGatewayServiceUrl string, appTLSSkipVerify bool) (kyma.Service, error) {
 	nameResolver := k8sconsts.NewNameResolver()
 	secretsManagerConstructor := func(namespace string) secrets.Manager {
 		return k8sResourceClients.core.CoreV1().Secrets(namespace)
