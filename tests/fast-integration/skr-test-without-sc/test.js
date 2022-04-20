a8fbcb0ea770b5da55784376dc89c85a5657a05d
Till Knuesting <t.knuesting@sap.com>
2022-04-20 10:57:46 +0100

    without depro

--- tests/fast-integration/skr-test-without-sc/test.js
+++ tests/fast-integration/skr-test-without-sc/test.js
@@ -74,16 +74,16 @@ describe('Execute SKR test', function() {
   oidcE2ETest();
   commerceMockTest();
 
-  after('Deprovision SKR', async function() {
-    try {
-      await deprovisionSKR(keb, kcp, this.options.instanceID, deprovisioningTimeout);
-    } catch (e) {
-      throw new Error(`before hook failed: ${e.toString()}`);
-    } finally {
-      const runtimeStatus = await kcp.getRuntimeStatusOperations(this.options.instanceID);
-      console.log(`\nRuntime status after deprovisioning: ${runtimeStatus}`);
-      await kcp.reconcileInformationLog(runtimeStatus);
-    }
-    await unregisterKymaFromCompass(director, this.options.scenarioName);
-  });
+  // after('Deprovision SKR', async function() {
+  //   try {
+  //     await deprovisionSKR(keb, kcp, this.options.instanceID, deprovisioningTimeout);
+  //   } catch (e) {
+  //     throw new Error(`before hook failed: ${e.toString()}`);
+  //   } finally {
+  //     const runtimeStatus = await kcp.getRuntimeStatusOperations(this.options.instanceID);
+  //     console.log(`\nRuntime status after deprovisioning: ${runtimeStatus}`);
+  //     await kcp.reconcileInformationLog(runtimeStatus);
+  //   }
+  //   await unregisterKymaFromCompass(director, this.options.scenarioName);
+  // });
 });
