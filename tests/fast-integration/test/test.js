1d42384007039999faa6c38652362914b85a6778
Dennis Geiselmann <dennis.geiselmann@sap.com>
2022-04-21 11:17:05 +0200

    uncomment code

--- tests/fast-integration/test/test.js
+++ tests/fast-integration/test/test.js
@@ -1,46 +1,43 @@
-// const {
-//   commerceMockTests,
-//   gettingStartedGuideTests,
-// } = require('./');
+const {
+  commerceMockTests,
+  gettingStartedGuideTests,
+} = require('./');
 
 const {monitoringTests, resetGrafanaProxy} = require('../monitoring');
-// const {loggingTests} = require('../logging');
-// const {tracingTests} = require('../tracing');
-// const {cleanMockTestFixture} = require('./fixtures/commerce-mock');
-// const {ensureCommerceMockLocalTestFixture} = require('../test/fixtures/commerce-mock');
-// const {error} = require('../utils');
+const {loggingTests} = require('../logging');
+const {tracingTests} = require('../tracing');
+const {cleanMockTestFixture} = require('./fixtures/commerce-mock');
+const {ensureCommerceMockLocalTestFixture} = require('../test/fixtures/commerce-mock');
+const {error} = require('../utils');
 
 
 describe('Executing Standard Testsuite:', function() {
   this.timeout(10 * 60 * 1000);
   this.slow(5000);
 
-  // const withCentralAppConnectivity = (process.env.WITH_CENTRAL_APP_CONNECTIVITY === 'true');
-  // const mockNamespace = process.env.MOCK_NAMESPACE || 'mocks';
-  // const testNamespace = 'test';
+  const withCentralAppConnectivity = (process.env.WITH_CENTRAL_APP_CONNECTIVITY === 'true');
+  const mockNamespace = process.env.MOCK_NAMESPACE || 'mocks';
+  const testNamespace = 'test';
 
-  // before('CommerceMock test fixture should be ready', async function() {
-  //   await ensureCommerceMockLocalTestFixture(mockNamespace, testNamespace,
-  //       withCentralAppConnectivity).catch((err) => {
-  //     error(err);
-  //     return ensureCommerceMockLocalTestFixture(mockNamespace, testNamespace, withCentralAppConnectivity);
-  //   });
-  // });
+  before('CommerceMock test fixture should be ready', async function() {
+    await ensureCommerceMockLocalTestFixture(mockNamespace, testNamespace,
+        withCentralAppConnectivity).catch((err) => {
+      error(err);
+      return ensureCommerceMockLocalTestFixture(mockNamespace, testNamespace, withCentralAppConnectivity);
+    });
+  });
 
-  // after('Test Cleanup: Test namespaces should be deleted', async function() {
-  //   await cleanMockTestFixture(mockNamespace, testNamespace, true);
-  // });
-
-  resetGrafanaProxy();
-
+  after('Test Cleanup: Test namespaces should be deleted', async function() {
+    await cleanMockTestFixture(mockNamespace, testNamespace, true);
+  });
 
   monitoringTests();
 
-  // commerceMockTests(testNamespace);
-  // gettingStartedGuideTests();
-  //
-  // loggingTests();
-  // tracingTests(mockNamespace, testNamespace);
+  commerceMockTests(testNamespace);
+  gettingStartedGuideTests();
+
+  loggingTests();
+  tracingTests(mockNamespace, testNamespace);
 
   resetGrafanaProxy();
 });
