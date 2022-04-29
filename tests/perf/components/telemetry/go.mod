ae904eefdbf51a05769f4f70286bc94cd0c784c8
Rakesh Garimella <rakesh.garimella@sap.com>
2022-04-29 16:40:38 +0200

    add telemetry operator perf test

--- tests/perf/components/telemetry/go.mod
+++ tests/perf/components/telemetry/go.mod
@@ -1,3 +1,39 @@
 module github.com/kyma-project/kyma/tests/perf/components/telemetry-perf
 
 go 1.17
+
+require k8s.io/client-go v0.23.6
+
+require sigs.k8s.io/controller-runtime v0.10.3 // indirect
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kyma-project/kyma/components/telemetry-operator v0.0.0-20220429065724-1e866e4a0906
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
+	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/api v0.23.6 // indirect
+	k8s.io/apimachinery v0.23.6
+	k8s.io/klog/v2 v2.30.0 // indirect
+	k8s.io/utils v0.0.0-20211116205334-6203023598ed // indirect
+	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
--- tests/perf/components/telemetry/main.go
+++ tests/perf/components/telemetry/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"text/template"
+
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 type flags struct {
-	count int
+	count      int
+	kubeconfig *string
+}
+
+type logpipelineHttp struct {
+	Name         string
+	Tag          string
+	Host         string
+	StorageLimit string
 }
 
 func main() {
 	var args = flags{}
+	if home := homedir.HomeDir(); home != "" {
+		args.kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		args.kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
 	flag.IntVar(&args.count, "count", 1, "Number of log pipelines to deploy")
 	flag.Parse()
 
@@ -25,5 +44,49 @@ func main() {
 }
 
 func run(out io.Writer, f flags) error {
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *f.kubeconfig)
+	if err != nil {
+		return err
+	}
 
+	// create the clientset
+	// clientset, err := dynamic.NewForConfig(config)
+	// if err != nil {
+	// 	return err
+	// }
+
+	logPipeline, err := renderHttpLogPipeline(f.count)
+	if err != nil {
+		return err
+	}
+
+	// obj, gvk, err := scheme.Codecs.UniversalDeserializer().Decode(logPipeline, nil, nil)
+	// logPipelineRes := schema.GroupVersionResource{Group: "telemetry.kyma-project.io", Version: "v1alpha1", Resource: "LogPipeline"}
+
+	// fmt.Println("Creating deployment...")
+	// result, err := clientset.Resource(logPipelineRes).Create(context.TODO(), obj, metav1.CreateOptions{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("Created deployment %q.\n", result.GetName())
+
+	return nil
+}
+
+func renderHttpLogPipeline(count int) ([]byte, error) {
+	rendered := bytes.Buffer{}
+	values := logpipelineHttp{
+		Name:         "foo",
+		Tag:          "foo",
+		Host:         "foo",
+		StorageLimit: "5M",
+	}
+	httpTempl := template.Must(template.ParseFiles("./assets/http.yml"))
+	err := httpTempl.Execute(&rendered, values)
+
+	if err != nil {
+		return nil, err
+	}
+	return rendered.Bytes(), nil
 }
