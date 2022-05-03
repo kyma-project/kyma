fbc49d0a9860ca921bd45d278a564c6dd528398e
Stanislav Khalash <stanislav.khalash@sap.com>
2022-05-03 14:30:12 +0200

    Implement log pipeline generation

--- tests/perf/components/telemetry/main.go
+++ tests/perf/components/telemetry/main.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"text/template"
@@ -15,6 +15,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
@@ -24,7 +25,7 @@ type flags struct {
 	kubeconfig *string
 }
 
-type logpipelineHttp struct {
+type httpLogPipeline struct {
 	Name         string
 	Tag          string
 	Host         string
@@ -33,34 +34,73 @@ type logpipelineHttp struct {
 
 func main() {
 	var args = flags{}
-	if home := homedir.HomeDir(); home != "" {
+	kubeconfig := os.Getenv("KUBECONFIG")
+	if kubeconfig != "" {
+		args.kubeconfig = &kubeconfig
+	} else if home := homedir.HomeDir(); home != "" {
 		args.kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		args.kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
+
 	flag.IntVar(&args.count, "count", 1, "Number of log pipelines to deploy")
 	flag.Parse()
 
-	out := os.Stdout
-	if err := run(out, args); err != nil {
-		fmt.Fprintf(out, "Error: %v\n", err)
+	if err := run(args); err != nil {
+		fmt.Printf("Error: %v\n", err)
 		os.Exit(2)
 	}
 
 }
 
-func run(out io.Writer, f flags) error {
+func run(f flags) error {
 	config, err := clientcmd.BuildConfigFromFlags("", *f.kubeconfig)
 	if err != nil {
 		return err
 	}
 
-	dynamicClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		return err
+	for i := 0; i < f.count; i++ {
+		logPipelineYAML, err := renderHTTPLogPipeline(i)
+		if err != nil {
+			return err
+		}
+		if err := createLogPipeline(config, logPipelineYAML); err != nil {
+			return err
+		}
 	}
 
-	logPipelineYAML, err := renderHttpLogPipeline(f.count)
+	return nil
+}
+
+func renderHTTPLogPipeline(count int) ([]byte, error) {
+	rendered := bytes.Buffer{}
+	values := httpLogPipeline{
+		Name:         fmt.Sprintf("http-%d", count),
+		Tag:          randomTag(),
+		Host:         "foo",
+		StorageLimit: "5M",
+	}
+	httpTempl := template.Must(template.ParseFiles("./assets/http.yml"))
+	err := httpTempl.Execute(&rendered, values)
+
+	if err != nil {
+		return nil, err
+	}
+	return rendered.Bytes(), nil
+}
+
+func randomTag() string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
+	tagLength := 5
+	chars := make([]rune, tagLength)
+	for i := range chars {
+		chars[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(chars)
+}
+
+func createLogPipeline(config *rest.Config, logPipelineYAML []byte) error {
+	dynamicClient, err := dynamic.NewForConfig(config)
 	if err != nil {
 		return err
 	}
@@ -73,29 +113,14 @@ func run(out io.Writer, f flags) error {
 	gvr := schema.GroupVersionResource{Group: "telemetry.kyma-project.io", Version: "v1alpha1", Resource: "logpipelines"}
 	obj := unstructured.Unstructured{Object: logPipeline}
 
-	fmt.Println("Creating log pipeline...")
+	fmt.Println("Creating a log pipeline...")
+
 	result, err := dynamicClient.Resource(gvr).Create(context.TODO(), &obj, metav1.CreateOptions{})
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Created log piepline %q.\n", result.GetName())
+
+	fmt.Println("Created a log pipeline ", result.GetName())
 
 	return nil
-}
-
-func renderHttpLogPipeline(count int) ([]byte, error) {
-	rendered := bytes.Buffer{}
-	values := logpipelineHttp{
-		Name:         "foo",
-		Tag:          "foo",
-		Host:         "foo",
-		StorageLimit: "5M",
-	}
-	httpTempl := template.Must(template.ParseFiles("./assets/http.yml"))
-	err := httpTempl.Execute(&rendered, values)
-
-	if err != nil {
-		return nil, err
-	}
-	return rendered.Bytes(), nil
 }
