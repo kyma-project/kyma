d5fd94afefc9829170511c86c5625031b10c17f0
Dennis Geiselmann <dennis.geiselmann@sap.com>
2022-05-10 08:41:28 +0200

    add oidc to custom params

--- tests/fast-integration/skr-test/test.js
+++ tests/fast-integration/skr-test/test.js
@@ -38,6 +38,7 @@ describe('Execute SKR test', function() {
           `Runtime ${runtimeName}`, `Application ${this.options.appName}`, `Suffix ${suffix}`);
 
       const customParams = {
+        oidc: this.options.oidc0,
         kymaVersion: 'PR-14116',
         overridesVersion: '2.1.3',
       };
