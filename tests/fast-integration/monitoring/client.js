50d0fa8c13da7abb6a71298fb5396daac1902387
Dennis Geiselmann <dennis.geiselmann@sap.com>
2022-05-10 16:05:57 +0200

    Improve test coverage of oauth2-proxy  (#14103)
    * fix monitoring in skr fast integration test
    
    * change to it
    
    * add entry to values file
    
    * add log statement
    
    * change to trusted ips
    
    * cleanup
    
    * increase timeout
    
    * add kyma version
    
    * change values
    
    * change values
    
    * change override to 2.1.2
    
    * remove reverseProxy
    
    * remove reverseProxy
    
    * change override to 2.1
    
    * increase timeout
    
    * change back to override 1.2.1
    
    * change PR
    
    * change version
    
    * remove reverse proxy from kiali and jaeger
    
    * uncomment code
    
    * change version to 2.1.3
    
    * add check for kyma docs
    
    * add import
    
    * revert skr changes
    
    * revert skr without sc changes
    
    * add whitespace
    
    * remove reverse proxy from test

--- tests/fast-integration/monitoring/client.js
+++ tests/fast-integration/monitoring/client.js
@@ -102,12 +102,12 @@ async function checkIfGrafanaIsReachable(redirectURL, httpErrorCode) {
       return true;
     }
   } catch (err) {
-    const msg = 'Error when querying Grafana: ';
+    const msg = 'Grafana is not reachable: ';
     if (err.response) {
       if (err.response.status === httpErrorCode && err.response.data.includes(redirectURL)) {
         return true;
       }
-      error(msg + err.response.status + ' : ' + err.response.data);
+      error(`${msg} ${err.response.data} (${err.response.status})`);
     } else {
       error(`${msg}: ${err.toString()}`);
     }
