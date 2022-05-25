c528ff5a44384dd6441e766f2a8d756303220def
Badr, Nesma <>
2022-05-25 12:34:00 +0200

    Fix get shoot

--- tests/fast-integration/skr-test/helpers.js
+++ tests/fast-integration/skr-test/helpers.js
@@ -91,16 +91,12 @@ function gatherOptions(...opts) {
 
 // gets the skr config by it's instance id
 async function getSKRConfig(instanceID) {
-  let shoot;
-  try {
-    shoot = await keb.getSKR(instanceID);
-  } catch (e) {
-    throw new Error(`Cannot fetch the shoot: ${e.toString()}`);
-  }
-  const shootName = "kyma-dev--e94ca0f"; // shoot.dashboard_url.split('.')[1];
+  const runtimeStatus = await kcp.getRuntimeStatusOperations(instanceID);
+  const objRuntimeStatus = JSON.parse(runtimeStatus);
+  expect(objRuntimeStatus).to.have.nested.property('data[0].shootName').not.empty;
 
   console.log(`Fetching SKR info for shoot: ${shootName}`);
-  return await gardener.getShoot(shootName);
+  return await gardener.getShoot(objRuntimeStatus.data[0].shootName);
 }
 
 async function prepareCompassResources(shoot, options) {
