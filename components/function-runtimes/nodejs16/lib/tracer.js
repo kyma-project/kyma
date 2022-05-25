9b762caf47c184afec13399d7fdc5777edff6635
pPrecel <filip.strozik@outlook.com>
2022-05-25 23:38:18 +0200

    fix sampling and propagation

--- components/function-runtimes/nodejs16/lib/tracer.js
+++ components/function-runtimes/nodejs16/lib/tracer.js
@@ -8,12 +8,15 @@ const { SimpleSpanProcessor } = require( '@opentelemetry/sdk-trace-base');
 const { JaegerExporter } = require( '@opentelemetry/exporter-jaeger');
 const { Resource } = require( '@opentelemetry/resources');
 const { SemanticResourceAttributes } = require( '@opentelemetry/semantic-conventions');
-const { SpanAttributes } = require( "@opentelemetry/api/build/src/trace/attributes");
 const {B3Propagator, B3InjectEncoding} = require("@opentelemetry/propagator-b3");
 const { ExpressInstrumentation, ExpressLayerType } = require( '@opentelemetry/instrumentation-express');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const axios = require("axios")
 
+
+const ignoredTargets = [
+  "/healthz", "/favicon.ico", "/metrics"
+]
 
 function setupTracer(serviceName){
 
@@ -34,26 +37,23 @@ function setupTracer(serviceName){
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
     }),
 
-    sampler: new AlwaysOnSampler(),
-    // sampler: b3Sampler(new ParentBasedSampler({
-    //   root: new AlwaysOnSampler()
-    // })),
-    propagator: new CompositePropagator([new W3CTraceContextPropagator(), new B3Propagator({injectEncoding: B3InjectEncoding.MULTI_HEADER})])
+    sampler: new ParentBasedSampler({
+      root: new AlwaysOnSampler()
+    }),
   });
+
+  const propagator = new CompositePropagator({
+    propagators: [
+      new W3CTraceContextPropagator(), 
+      new B3Propagator({injectEncoding: B3InjectEncoding.MULTI_HEADER})
+    ],
+  })
 
   registerInstrumentations({
     tracerProvider: provider,
     instrumentations: [
       new HttpInstrumentation({
         ignoreIncomingPaths: ignoredTargets,
-        // headersToSpanAttributes: {
-        //   client: {
-        //     requestHeaders: ['x-b3-sampled']
-        //   },
-        //   server: {
-        //     requestHeaders: ['x-b3-sampled']
-        //   }
-        // }
       }),
       new ExpressInstrumentation({
         ignoreLayersType: [ExpressLayerType.MIDDLEWARE]
@@ -66,36 +66,14 @@ function setupTracer(serviceName){
   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
   // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
-  provider.register();
+  provider.register({
+    propagator: propagator,
+  });
 
   return opentelemetry.trace.getTracer(serviceName);
 };
 
 // type FilterFunction = (spanName: string, spanKind: SpanKind, attributes: SpanAttributes) => boolean;
-
-function b3Sampler(parent) {
-  return {
-    shouldSample(ctx, tid, spanName, spanKind, attr, links) {
-      console.log("should sample? attr", attr)
-      console.log("should sample? ctx", ctx)
-      console.log("should sample? spanname", spanName)
-      console.log("should sample? spanKind", spanKind)
-      console.log("should sample? links", links)
-      // if (!filterFn(spanName, spanKind, attr)) {
-      //   return { decision: opentelemetry.SamplingDecision.NOT_RECORD };
-      // }
-      return parent.shouldSample(ctx, tid, spanName, spanKind, attr, links);
-    },
-    toString() {
-      return `B3Sampler(${parent.toString()})`;
-    }
-  }
-}
-
-
-const ignoredTargets = [
-    "/healthz", "/favicon.ico", "/metrics"
-]
 
 module.exports = {
     setupTracer,
