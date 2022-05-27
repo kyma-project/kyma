b802d6a306da4b7ddbc696c9299f3d30bc51fa39
Marcin Witalis <marcin.witalis@sap.com>
2022-05-27 10:28:42 +0200

    fix imports

--- components/function-controller/internal/controllers/serverless/function_reconcile.go
+++ components/function-controller/internal/controllers/serverless/function_reconcile.go
@@ -11,7 +11,6 @@ import (
 	"golang.org/x/time/rate"
 	appsv1 "k8s.io/api/apps/v1"
 	autoscalingv1 "k8s.io/api/autoscaling/v1"
-	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
