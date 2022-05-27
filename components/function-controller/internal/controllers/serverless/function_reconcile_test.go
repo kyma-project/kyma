3acf798b939784a1203e596be008e9119909d037
Marcin Witalis <marcin.witalis@sap.com>
2022-05-27 11:08:03 +0200

    update tests #3

--- components/function-controller/internal/controllers/serverless/function_reconcile_test.go
+++ components/function-controller/internal/controllers/serverless/function_reconcile_test.go
@@ -1042,7 +1042,7 @@ func TestFunctionReconciler_Reconcile(t *testing.T) {
 		result, err = reconciler.Reconcile(ctx, request)
 		g.Expect(err).To(gomega.BeNil())
 		g.Expect(result.Requeue).To(gomega.BeFalse())
-		g.Expect(result.RequeueAfter).To(gomega.Equal(time.Second * 0))
+		g.Expect(result.RequeueAfter).To(gomega.Equal(time.Second * 1))
 
 		g.Expect(resourceClient.Get(context.TODO(), request.NamespacedName, function)).To(gomega.Succeed())
 		g.Expect(function.Status.Conditions).To(gomega.HaveLen(conditionLen))
