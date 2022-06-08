d314ab8e3019eba7489e74f9b30e1e99b5d31112
Mariusz Szymański <mariusz.szymanski@sap.com>
2022-06-08 15:15:04 +0200

    Implement mtls oauth token fetch (#14401)
    * Implement mtls oauth token fetch
    
    * fix build errors
    
    * imports
    
    * Add tests
    
    * imports
    
    * Remove not used function InvalidateAndRetry from OauthClient
    
    * Allign PR with values.yaml
    
    * Generate Oauth token cache key from both clientID and Authentication URL
    
    * Mock adjustments after code changes
    
    * fixing token request body experiment
    
    * remove experiment
    
    * Update Application Connector documentation with information about OAuth 2.0 Mutual-TLS support in gateway
    
    * Add OAuth 2.0 mTLS-related documentation update
    
    Co-authored-by: Przemyslaw Golicz <przemyslaw.golicz@sap.com>
    Co-authored-by: majakurcius <maja.kurcius@sap.com>

--- components/central-application-gateway/pkg/authorization/oauthcertstrategy_test.go
+++ components/central-application-gateway/pkg/authorization/oauthcertstrategy_test.go
@@ -5,20 +5,25 @@ import (
 	"testing"
 
 	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/apperrors"
+
 	oauthMocks "github.com/kyma-project/kyma/components/central-application-gateway/pkg/authorization/oauth/mocks"
 	"github.com/kyma-project/kyma/components/central-application-gateway/pkg/httpconsts"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAuthStrategy(t *testing.T) {
+func TestAuthWithCerStrategy(t *testing.T) {
 
 	t.Run("should add Authorization header", func(t *testing.T) {
 		// given
 		oauthClientMock := &oauthMocks.Client{}
-		oauthClientMock.On("GetToken", "clientId", "clientSecret", "www.example.com/token", (*map[string][]string)(nil), (*map[string][]string)(nil)).Return("token", nil)
 
-		oauthStrategy := newOAuthStrategy(oauthClientMock, "clientId", "clientSecret", "www.example.com/token", nil)
+		oauthStrategy := newOAuthWithCertStrategy(oauthClientMock, "clientId", certificate, privateKey, "www.example.com/token", nil)
+
+		prepareCertificate, err := oauthStrategy.prepareCertificate()
+		require.NoError(t, err)
+
+		oauthClientMock.On("GetTokenMTLS", "clientId", "www.example.com/token", prepareCertificate, (*map[string][]string)(nil), (*map[string][]string)(nil)).Return("token", nil)
 
 		request, err := http.NewRequest("GET", "www.example.com", nil)
 		require.NoError(t, err)
@@ -37,10 +42,10 @@ func TestAuthStrategy(t *testing.T) {
 		oauthClientMock := &oauthMocks.Client{}
 		oauthClientMock.On("InvalidateTokenCache", "clientId", "www.example.com/token").Return("token", nil).Once()
 
-		oauthStrategy := newOAuthStrategy(oauthClientMock, "clientId", "clientSecret", "www.example.com/token", nil)
+		authWithCertStrategy := newOAuthWithCertStrategy(oauthClientMock, "clientId", certificate, privateKey, "www.example.com/token", nil)
 
 		// when
-		oauthStrategy.Invalidate()
+		authWithCertStrategy.Invalidate()
 
 		// then
 		oauthClientMock.AssertExpectations(t)
@@ -49,15 +54,19 @@ func TestAuthStrategy(t *testing.T) {
 	t.Run("should not add Authorization header when getting token failed", func(t *testing.T) {
 		// given
 		oauthClientMock := &oauthMocks.Client{}
-		oauthClientMock.On("GetToken", "clientId", "clientSecret", "www.example.com/token", (*map[string][]string)(nil), (*map[string][]string)(nil)).Return("", apperrors.Internal("failed")).Once()
 
-		oauthStrategy := newOAuthStrategy(oauthClientMock, "clientId", "clientSecret", "www.example.com/token", nil)
+		authWithCertStrategy := newOAuthWithCertStrategy(oauthClientMock, "clientId", certificate, privateKey, "www.example.com/token", nil)
+
+		prepareCertificate, err := authWithCertStrategy.prepareCertificate()
+		require.NoError(t, err)
+
+		oauthClientMock.On("GetTokenMTLS", "clientId", "www.example.com/token", prepareCertificate, (*map[string][]string)(nil), (*map[string][]string)(nil)).Return("", apperrors.Internal("failed")).Once()
 
 		request, err := http.NewRequest("GET", "www.example.com", nil)
 		require.NoError(t, err)
 
 		// when
-		err = oauthStrategy.AddAuthorization(request, nil)
+		err = authWithCertStrategy.AddAuthorization(request, nil)
 
 		// then
 		require.Error(t, err)
