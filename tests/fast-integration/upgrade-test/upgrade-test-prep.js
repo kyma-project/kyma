d202856e2b918dbc5d326715d692c8982f72e76e
Till Knuesting <t.knuesting@sap.com>
2022-06-08 13:31:03 +0300

    removing helm-broker related parts (#14410)

--- tests/fast-integration/upgrade-test/upgrade-test-prep.js
+++ tests/fast-integration/upgrade-test/upgrade-test-prep.js
@@ -1,6 +1,5 @@
 const {ensureCommerceMockLocalTestFixture} = require('../test/fixtures/commerce-mock');
 
-const {ensureHelmBrokerTestFixture} = require('./fixtures/helm-broker');
 const {error} = require('../utils');
 
 describe('Upgrade test preparation', function() {
@@ -12,13 +11,6 @@ describe('Upgrade test preparation', function() {
     await ensureCommerceMockLocalTestFixture('mocks', testNamespace).catch((err) => {
       error(err);
       return ensureCommerceMockLocalTestFixture('mocks', testNamespace);
-    });
-  });
-
-  it('Helm Broker test fixture should be ready', async function() {
-    await ensureHelmBrokerTestFixture(testNamespace).catch((err) => {
-      error(err);
-      return ensureHelmBrokerTestFixture(testNamespace);
     });
   });
 });
