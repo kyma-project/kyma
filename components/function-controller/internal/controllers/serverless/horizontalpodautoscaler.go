92569a5c63450de1a3dfa9cc38d848363fa096d8
pPrecel <filip.strozik@outlook.com>
2022-06-20 22:47:46 +0200

    return right stateFn on initHpaFn

--- components/function-controller/internal/controllers/serverless/horizontalpodautoscaler.go
+++ components/function-controller/internal/controllers/serverless/horizontalpodautoscaler.go
@@ -14,7 +14,7 @@ import (
 
 func initStateFnCheckHPA(ctx context.Context, r *reconciler, s *systemState) stateFn {
 	if v, ok := s.instance.Annotations[disableHorizontalPodAutoscaler]; ok && strings.ToLower(v) == "true" {
-		return nil
+		return stateFnUpdateDeploymentStatus
 	}
 
 	return stateFnCheckHPA(ctx, r, s)
