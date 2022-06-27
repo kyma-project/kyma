a1cb4b129477aae7d79847fe8fdca3c1377cd927
Bartosz Chwila <bartosz.chwila@sap.com>
2022-06-27 11:04:37 +0200

    Debug log on fail

--- tests/components/istio/istio_installed_test.go
+++ tests/components/istio/istio_installed_test.go
@@ -131,10 +131,6 @@ func (i *istioInstallledCase) getIstioPods() error {
 	}
 	i.pilotPods = istiodPods
 
-	for _, p := range istiodPods.Items {
-		fmt.Printf("[5525] name:%v timestamp: %v\n, status:%v", p.Name, p.CreationTimestamp, p.Status)
-	}
-
 	ingressGwPods, err := listPodsIstioNamespace(metav1.ListOptions{
 		LabelSelector: "istio=ingressgateway",
 	})
@@ -199,6 +195,10 @@ func (i *istioInstallledCase) istioPodsAreAvailable() error {
 
 func (i *istioInstallledCase) thereIsPodForIngressGateway(numberOfPodsRequired int) error {
 	if len(i.ingressGwPods.Items) != numberOfPodsRequired {
+		//Log for debuging https://status.build.kyma-project.io/view/gs/kyma-prow-logs/pr-logs/pull/kyma-project_kyma/14670/pre-main-kyma-prod-istio-integration-k3d/1539914627880587264
+		for _, p := range i.ingressGwPods.Items {
+			fmt.Printf("Ingress gateway pod: name:%v timestamp: %v, status:%v\n", p.Name, p.CreationTimestamp, p.Status)
+		}
 		return fmt.Errorf("number of deployed IngressGW pods %d does not equal %d", len(i.ingressGwPods.Items), numberOfPodsRequired)
 	}
 	return nil
@@ -206,6 +206,10 @@ func (i *istioInstallledCase) thereIsPodForIngressGateway(numberOfPodsRequired i
 
 func (i *istioInstallledCase) thereIsPodForPilot(numberOfPodsRequired int) error {
 	if len(i.pilotPods.Items) != numberOfPodsRequired {
+		//Log for debuging https://status.build.kyma-project.io/view/gs/kyma-prow-logs/pr-logs/pull/kyma-project_kyma/14670/pre-main-kyma-prod-istio-integration-k3d/1539914627880587264
+		for _, p := range i.pilotPods.Items {
+			fmt.Printf("Ingress gateway pod: name:%v timestamp: %v, status:%v\n", p.Name, p.CreationTimestamp, p.Status)
+		}
 		return fmt.Errorf("number of deployed Pilot pods %d does not equal %d", len(i.pilotPods.Items), numberOfPodsRequired)
 	}
 	return nil
