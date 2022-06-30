7928d046a22c03965183dc0f402890fedae23e92
ralikio <74771103+ralikio@users.noreply.github.com>
2022-06-30 10:50:46 +0200

    Added Exception Stack Traces

--- tests/fast-integration/skr-test/provision/provision-skr.js
+++ tests/fast-integration/skr-test/provision/provision-skr.js
@@ -55,7 +55,7 @@ async function provisionSKRInstance(options, timeout) {
 
     return shoot;
   } catch (e) {
-    throw new Error(`Provisioning failed: ${e.toString()}`);
+    throw new Error(`Provisioning failed: ${e.toString(), e.stack}`);
   } finally {
     debug('Fetching runtime status...');
     const runtimeStatus = await kcp.getRuntimeStatusOperations(options.instanceID);
