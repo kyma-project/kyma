a049c0afed93544de7d874392492a93df89abc1c
ralikio <74771103+ralikio@users.noreply.github.com>
2022-07-01 09:48:52 +0200

    Removal of Catch Error

--- tests/fast-integration/skr-test/provision/provision-skr.js
+++ tests/fast-integration/skr-test/provision/provision-skr.js
@@ -54,6 +54,8 @@ async function provisionSKRInstance(options, timeout) {
     const shoot = skr.shoot;
 
     return shoot;
+  } catch (e) {
+    throw new Error(`Provisioning failed: ${e.toString(), e.stack}`);
   } finally {
     debug('Fetching runtime status...');
     const runtimeStatus = await kcp.getRuntimeStatusOperations(options.instanceID);
