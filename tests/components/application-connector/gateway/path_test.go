20e64f672e9d7687c9188da5e5242bd294dbdfd4
Rafał Foks <r.fok74@gmail.com>
2022-07-01 13:28:20 +0200

    added files

--- tests/components/application-connector/gateway/path_test.go
+++ tests/components/application-connector/gateway/path_test.go
@@ -1,1 +1,53 @@
-package main
+package main_test
+
+import (
+	"context"
+	"github.com/kyma-project/kyma/components/application-operator/pkg/apis/applicationconnector/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func callToGateway(t *testing.T, entry v1alpha1.Entry) int {
+	t.Log("Calling", entry.CentralGatewayUrl)
+	res, err := http.Get(entry.CentralGatewayUrl)
+	assert.Nil(t, err)
+	body, err := ioutil.ReadAll(res.Body)
+	if err == nil && len(body) > 0 {
+		t.Log("Response", string(body))
+	}
+	return res.StatusCode
+}
+
+func (gs *GatewaySuite) TestPath() {
+	app, err := gs.cli.ApplicationconnectorV1alpha1().Applications().Get(context.Background(), "bad-path", v1.GetOptions{})
+
+	gs.Nil(err)
+
+	for _, service := range app.Spec.Services {
+		gs.Run(service.DisplayName, func() {
+			for _, entry := range service.Entries {
+				if entry.Type != "API" {
+					gs.T().Log("Skipping event entry")
+					continue
+				}
+				var code int
+
+				switch service.Name {
+				case "missingSrvApp":
+					code = callToGateway(gs.T(), entry)
+					gs.Equal(500, code)
+				case "missingSrv":
+					code = callToGateway(gs.T(), entry)
+					gs.Equal(500, code)
+				case "badTargetURL":
+					code = callToGateway(gs.T(), entry)
+					gs.Equal(404, code)
+				}
+			}
+		})
+	}
+}
--- tests/components/application-connector/gateway/yaml_test.go
+++ tests/components/application-connector/gateway/yaml_test.go
@@ -2,66 +2,26 @@ package main_test
 
 import (
 	"context"
-	"fmt"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"strings"
 )
 
 const applicationName = "test-app"
 
-func (gs *GatewaySuite) TestApplicationName() {
+func (gs *GatewaySuite) TestYaml() {
 	app, err := gs.cli.ApplicationconnectorV1alpha1().Applications().Get(context.Background(), applicationName, v1.GetOptions{})
 	namespace := "test"
+	expectedTargetURL := "http://" + app.Name + "." + namespace + ".svc.cluster.local:8080"
 	gs.Nil(err)
 
 	for _, service := range app.Spec.Services {
 		gs.Run(service.DisplayName, func() {
 			for _, entry := range service.Entries {
 				if entry.Type == "API" {
-					fmt.Println("****************")
-					fmt.Println(entry.CentralGatewayUrl)
-					fmt.Println(service.Name)
-					fmt.Println(app.Name)
 					gs.Equal(true, strings.Contains(entry.CentralGatewayUrl, service.Name), "Service name not provided in path")
 					gs.Equal(true, strings.Contains(entry.CentralGatewayUrl, app.Name), "Application name not provided in path")
-					fmt.Println("****************")
-
-				}
-			}
-		})
-	}
-
-	for _, service := range app.Spec.Services {
-		gs.Run(service.DisplayName, func() {
-			for _, entry := range service.Entries {
-				if entry.Type == "API" {
-					fmt.Println("&&&&&&&&&&")
-					fmt.Println(entry.CentralGatewayUrl)
-					fmt.Println(service.Name)
-
-					gs.Equal(true, strings.Contains(entry.CentralGatewayUrl, service.Name), "Service name not provided in path")
-					fmt.Println("&&&&&&&&&&")
-				}
-			}
-		})
-	}
-
-	for _, service := range app.Spec.Services {
-
-		expectedTargetURL := "http://" + app.Name + "." + namespace + ".svc.cluster.local:8080"
-		gs.Run(service.DisplayName, func() {
-			for _, entry := range service.Entries {
-				if entry.Type == "API" {
-					fmt.Println("/////////////")
-					fmt.Println(entry.TargetUrl)
-					fmt.Println(service.Name)
-					fmt.Println(namespace)
-					fmt.Println(expectedTargetURL)
-
-					gs.Equal(true, strings.Contains(entry.TargetUrl, service.Name), "Service name not provided in target path")
 					gs.Equal(true, strings.Contains(entry.TargetUrl, namespace), "Namespace not provided in target path")
 					gs.Equal(true, strings.Contains(entry.TargetUrl, expectedTargetURL), "Bad targetURL path")
-					fmt.Println("///////////////")
 				}
 			}
 		})
