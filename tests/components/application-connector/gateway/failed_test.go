974012e81e8563f00d32fa723562e00a716fcf5a
Wojciech Nawa <wojciech.nawa@sap.com>
2022-07-04 15:11:24 +0200

    Parametrize failed return codes

--- tests/components/application-connector/gateway/failed_test.go
+++ tests/components/application-connector/gateway/failed_test.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strconv"
 
+	"github.com/kyma-project/kyma/components/application-operator/pkg/apis/applicationconnector/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -16,6 +19,8 @@ func (gs *GatewaySuite) TestFailedCases() {
 	gs.Nil(err)
 
 	for _, service := range app.Spec.Services {
+		code, err := codeFromService(service)
+		gs.Nil(err)
 		gs.Run(service.DisplayName, func() {
 			for _, entry := range service.Entries {
 				if entry.Type != "API" {
@@ -29,8 +34,23 @@ func (gs *GatewaySuite) TestFailedCases() {
 				if err == nil && len(body) > 0 {
 					gs.T().Log("Response", string(body))
 				}
-				gs.Equal(403, res.StatusCode) // NOTE: 403 or 401?
+				gs.Equal(code, res.StatusCode)
 			}
 		})
 	}
+}
+
+// codeFromService extracts expected response code from
+// service's description. Default to 403
+func codeFromService(service v1alpha1.Service) (code int, err error) {
+	code = 403
+	re := regexp.MustCompile(`\d+$`) // NOTE: Is this a terrible idea?
+	if codeStr := re.FindString(service.Description); len(codeStr) > 0 {
+		code, err = strconv.Atoi(codeStr)
+		if err != nil {
+			return
+		}
+	}
+
+	return
 }
