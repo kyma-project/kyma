b1870244497fa44dedfb91100c23457c04dbf47c
franpog859 <franciszek.pog@gmail.com>
2022-07-07 12:51:29 +0200

    WIP - add other auths with csrf scenarios

--- tests/components/application-connector/internal/testkit/test-api/apis.go
+++ tests/components/application-connector/internal/testkit/test-api/apis.go
@@ -18,6 +18,15 @@ func SetupRoutes(logOut io.Writer, basicAuthCredentials BasicAuthCredentials, oa
 	api := router.PathPrefix("/v1/api").Subrouter()
 	api.Use(Logger(logOut, logger.DevLoggerType))
 
+	oauth := NewOAuth(oauthCred.ClientID, oauthCred.ClientSecret)
+	csrf := NewCSRF()
+
+	{
+		api.HandleFunc("/oauth/token", oauth.Token).Methods(http.MethodPost)
+		api.HandleFunc("/mtlsoauth/token", oauth.MTLSToken).Methods(http.MethodPost)
+		api.HandleFunc("/csrf/token", csrf.Token).Methods(http.MethodGet)
+		api.HandleFunc("/csrf/bad-token", csrf.BadToken).Methods(http.MethodGet)
+	}
 	{
 		r := api.PathPrefix("/unsecure").Subrouter()
 		r.HandleFunc("/ok", alwaysOk).Methods(http.MethodGet)
@@ -30,33 +39,35 @@ func SetupRoutes(logOut io.Writer, basicAuthCredentials BasicAuthCredentials, oa
 		r.HandleFunc("/echo", echo)
 	}
 	{
-		oauth := NewOAuth(oauthCred.ClientID, oauthCred.ClientSecret)
-		api.HandleFunc("/oauth/token", oauth.Token).Methods(http.MethodPost)
-
 		r := api.PathPrefix("/oauth").Subrouter()
 		r.Use(oauth.Middleware())
 		r.HandleFunc("/ok", alwaysOk).Methods(http.MethodGet)
 		r.HandleFunc("/echo", echo)
 	}
 	{
-		oauth := NewOAuth(oauthCred.ClientID, oauthCred.ClientSecret)
-		api.HandleFunc("/mtlsoauth/token", oauth.MTLSToken).Methods(http.MethodPost)
-
 		r := api.PathPrefix("/mtlsoauth").Subrouter()
 		r.Use(oauth.Middleware())
 		r.HandleFunc("/ok", alwaysOk).Methods(http.MethodGet)
 		r.HandleFunc("/echo", echo)
 	}
 	{
-		csrf := NewCSRF()
-		api.HandleFunc("/csrf/token", csrf.Token).Methods(http.MethodGet)
-		api.HandleFunc("/csrf/bad-token", csrf.BadToken).Methods(http.MethodGet)
+		csrfBasicRouter := api.PathPrefix("/csrf-basic").Subrouter()
+		csrfBasicRouter.Use(csrf.Middleware())
+		csrfBasicRouter.Use(BasicAuth(basicAuthCredentials.User, basicAuthCredentials.Password))
+		csrfBasicRouter.HandleFunc("/ok", alwaysOk).Methods(http.MethodGet)
+		csrfBasicRouter.HandleFunc("/echo", echo)
 
-		r := api.PathPrefix("/csrf-basic").Subrouter()
-		r.Use(csrf.Middleware())
-		r.Use(BasicAuth(basicAuthCredentials.User, basicAuthCredentials.Password))
-		r.HandleFunc("/ok", alwaysOk).Methods(http.MethodGet)
-		r.HandleFunc("/echo", echo)
+		csrfOAuthRouter := api.PathPrefix("/csrf-oauth").Subrouter()
+		csrfOAuthRouter.Use(oauth.Middleware())
+		csrfOAuthRouter.Use(csrf.Middleware())
+		csrfOAuthRouter.HandleFunc("/ok", alwaysOk).Methods(http.MethodGet)
+		csrfOAuthRouter.HandleFunc("/echo", echo)
+
+		csrfMTLSOAuthRouter := api.PathPrefix("/csrf-mtlsoauth").Subrouter()
+		csrfMTLSOAuthRouter.Use(oauth.Middleware())
+		csrfMTLSOAuthRouter.Use(csrf.Middleware())
+		csrfMTLSOAuthRouter.HandleFunc("/ok", alwaysOk).Methods(http.MethodGet)
+		csrfMTLSOAuthRouter.HandleFunc("/echo", echo)
 	}
 
 	return router
