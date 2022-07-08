ca33b4160c58bfc1a71018641caf508029976605
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2022-07-08 12:35:13 +0200

    Local build script updated

--- tests/components/application-connector/scripts/local-build.sh
+++ tests/components/application-connector/scripts/local-build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-export DOCKER_TAG="PR-14743"
-export DOCKER_PUSH_REPOSITORY="agalwas"
+if [ $# -ne 2 ]; then
+  echo "Usage: local_build.sh <DOCKER_TAG> <DOCKER_PUSH_REPOSITORY>"
+  exit 1
+fi
+
+export DOCKER_TAG=$1
+export DOCKER_PUSH_REPOSITORY=$2
 make release
