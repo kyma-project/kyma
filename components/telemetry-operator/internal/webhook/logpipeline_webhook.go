eaa8d94c8fa769ff2e809ce2797e5ae68a7a0365
Rakesh Garimella <rakesh.garimella@sap.com>
2022-07-11 13:56:05 +0200

    remove warnings

--- components/telemetry-operator/internal/webhook/logpipeline_webhook.go
+++ components/telemetry-operator/internal/webhook/logpipeline_webhook.go
@@ -35,8 +35,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
-
-	"github.com/kyma-project/kyma/components/telemetry-operator/internal/secret"
 )
 
 const (
@@ -117,10 +115,6 @@ func (v *LogPipelineValidator) Handle(ctx context.Context, req admission.Request
 	}
 	var warnMsg []string
 
-	secretsFound := v.validateSecrets(ctx, logPipeline)
-	if !secretsFound {
-		warnMsg = append(warnMsg, "One or more secrets do not exist. The LogPipeline stays in pending state until the secrets are available.")
-	}
 	if v.pluginValidator.ContainsCustomPlugin(logPipeline) {
 		helpText := "https://kyma-project.io/docs/kyma/latest/01-overview/main-areas/observability/obsv-04-telemetry-in-kyma/"
 		msg := fmt.Sprintf("Logpipeline '%s' uses unsupported custom filters or outputs. Please consider changing the pipeline to use supported filters or output as described in the documentation: %s", logPipeline.Name, helpText)
@@ -247,9 +241,4 @@ func (v *LogPipelineValidator) getFluentBitConfig(ctx context.Context, currentBa
 func (v *LogPipelineValidator) InjectDecoder(d *admission.Decoder) error {
 	v.decoder = d
 	return nil
-}
-
-func (v *LogPipelineValidator) validateSecrets(ctx context.Context, logPipeline *telemetryv1alpha1.LogPipeline) bool {
-	secVal := secret.NewSecretHelper(v.Client)
-	return secVal.ValidateSecretsExist(ctx, logPipeline)
 }
