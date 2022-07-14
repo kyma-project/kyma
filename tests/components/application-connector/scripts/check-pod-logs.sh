cef6dd9e2f9c5d94bc23e9af6e4423d1bc68c20a
Rafał Foks <r.fok74@gmail.com>
2022-07-14 09:29:38 +0200

    delete unnecessary echo

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -5,9 +5,7 @@ GOPATH=$(go env GOPATH)
 podName=$(kubectl get pods -n $NAMESPACE --selector=job-name=application-gateway-test --output=jsonpath='{.items[*].metadata.name}')
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
-  echo "export"
 	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
 else
-  echo "non export"
 	kubectl -n $NAMESPACE logs $podName application-gateway-test
 fi
