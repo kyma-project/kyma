013b4165b334cf6e278c9e26ffb61ecb5dcccc9f
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2022-07-15 10:10:04 +0200

    Changed test and chart organisation

--- tests/components/application-connector/test/application-gateway/runner_test.go
+++ tests/components/application-connector/test/application-gateway/runner_test.go
@@ -5,7 +5,7 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var applications = []string{"positive-authorisation", "path-related-error-handling", "kubernetes-resources-error-handling"}
+var applications = []string{"positive-authorisation", "path-related-error-handling", "missing-resources-error-handling"}
 
 func (gs *GatewaySuite) TestGetRequest() {
 
@@ -13,25 +13,27 @@ func (gs *GatewaySuite) TestGetRequest() {
 		app, err := gs.cli.ApplicationconnectorV1alpha1().Applications().Get(context.Background(), app, v1.GetOptions{})
 		gs.Nil(err)
 
-		for _, service := range app.Spec.Services {
-			gs.Run(service.Description, func() {
-				for _, entry := range service.Entries {
-					if entry.Type != "API" {
-						gs.T().Log("Skipping event entry")
-						continue
+		gs.Run(app.Spec.Description, func() {
+			for _, service := range app.Spec.Services {
+				gs.Run(service.Description, func() {
+					for _, entry := range service.Entries {
+						if entry.Type != "API" {
+							gs.T().Log("Skipping event entry")
+							continue
+						}
+
+						expectedCode, err := getExpectedHTTPCode(service)
+						if err != nil {
+							gs.T().Log("Error during getting the error code from description -> applicationCRD")
+							gs.T().Fail()
+						}
+
+						actualCode := executeGetRequest(gs.T(), entry)
+
+						gs.Equal(expectedCode, actualCode)
 					}
-
-					expectedCode, err := getExpectedHTTPCode(service)
-					if err != nil {
-						gs.T().Log("Error during getting the error code from description -> applicationCRD")
-						gs.T().Fail()
-					}
-
-					actualCode := executeGetRequest(gs.T(), entry)
-
-					gs.Equal(expectedCode, actualCode)
-				}
-			})
-		}
+				})
+			}
+		})
 	}
 }
