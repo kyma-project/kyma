195aceba2d7048c506800c2d0a8c31507ff8839d
Rafal Foks <r.fok74@gmail.com>
2022-07-15 13:31:48 +0200

    Add the test cases to the Application Gateway test suite (#14791)
    * refactor of the test logic
    
    * added resources test
    
    * bump image
    
    * change the naming
    
    * implement waiting for job, apply review suggestions
    
    * modify kind of pod
    
    * delete unnecessary echo
    
    * clean up after tests, added debug target to makefile
    
    * Changed test and chart organisation
    
    * Makefile simplified
    
    * apply review sugestions
    
    * Fix in test configuration for case with missing request parameters secret
    
    Co-authored-by: Arkadiusz Galwas <arkadiusz.galwas@sap.com>

---
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+NAMESPACE=test
+GOPATH=$(go env GOPATH)
+
+podName=$(kubectl get pods -n $NAMESPACE --selector=job-name=application-gateway-test --output=jsonpath='{.items[*].metadata.name}')
+
+if ([[ ${EXPORT_RESULT} == true ]]); then
+	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
+else
+	kubectl -n $NAMESPACE logs $podName application-gateway-test
+fi
