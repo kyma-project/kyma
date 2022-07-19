2e7332d646200d00f27b524759dd79187c7451a5
Rakesh Garimella <rakesh.garimella@sap.com>
2022-07-19 14:19:25 +0200

    fix

--- components/telemetry-operator/internal/parsers/parser_test.go
+++ components/telemetry-operator/internal/parsers/parser_test.go
@@ -1,22 +1,35 @@
 package parsers
 
 import (
+	"context"
+	"testing"
+
 	telemetryv1alpha1 "github.com/kyma-project/kyma/components/telemetry-operator/apis/telemetry/v1alpha1"
 	"github.com/kyma-project/kyma/components/telemetry-operator/internal/sync/mocks"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/api/errors"
-	"testing"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
+
+var daemonSetConfig = FluentBitDaemonSetConfig{
+	FluentBitDaemonSetName:    types.NamespacedName{Name: "telemetry-fluent-bit", Namespace: "cm-ns"},
+	FluentBitParsersConfigMap: types.NamespacedName{Name: "telemetry-fluent-bit", Namespace: "cm-ns"},
+}
 
 func TestSyncParsersConfigMapErrorClientErrorReturnsError(t *testing.T) {
 	mockClient := &mocks.Client{}
 	badReqErr := errors.NewBadRequest("")
 	mockClient.On("Get", mock.Anything, mock.Anything, mock.Anything).Return(badReqErr)
-	sut := NewLogParserSyncer(mockClient, daemonSetConfig, pipelineConfig)
+	sut := NewLogParserSyncer(mockClient, daemonSetConfig)
 
-	lp := telemetryv1alpha1.LogPipeline{}
-	result, err := sut.syncParsersConfigMap(context.Background(), &lp)
+	lp := telemetryv1alpha1.LogParser{
+		ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "fooNs"},
+		Spec: telemetryv1alpha1.LogParserSpec{Parser: `
+Format regex`},
+	}
+	result, err := sut.SyncParsersConfigMap(context.Background(), &lp)
 
 	require.Error(t, err)
 	require.Equal(t, result, false)
