d2a23b7589fa2459bf1493b05a563b88c0677d5c
Wojciech Nawa <wojciech.nawa@sap.com>
2022-07-20 06:04:42 +0200

    Merge test files

--- tests/components/application-connector/test/application-gateway/runner_test.go
+++ tests/components/application-connector/test/application-gateway/runner_test.go
@@ -2,6 +2,7 @@ package application_gateway
 
 import (
 	"context"
+	"strconv"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -36,6 +37,36 @@ func (gs *GatewaySuite) TestGetRequest() {
 						gs.Equal(expectedCode, res.StatusCode, "Incorrect response code")
 					}
 				})
+			}
+		})
+	}
+}
+
+func (gs *GatewaySuite) TestResponseBody() {
+	app, err := gs.cli.ApplicationconnectorV1alpha1().Applications().Get(context.Background(), "proxy-cases", v1.GetOptions{})
+	gs.Nil(err)
+	for _, service := range app.Spec.Services {
+		gs.Run(service.Description, func() {
+			http := NewHttpCli(gs.T())
+
+			for _, entry := range service.Entries {
+				if entry.Type != "API" {
+					gs.T().Log("Skipping event entry")
+					continue
+				}
+
+				expectedCode, err := getExpectedHTTPCode(service)
+				if err != nil {
+					gs.T().Log("Error during getting the error code from description -> applicationCRD")
+					gs.T().Fail()
+				}
+
+				_, body, err := http.Get(entry.CentralGatewayUrl)
+				gs.Nil(err, "Request failed")
+
+				codeStr := strconv.Itoa(expectedCode)
+
+				gs.Equal(codeStr, string(body), "Incorrect body")
 			}
 		})
 	}
