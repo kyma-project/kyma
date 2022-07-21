eb17c123084e2f9436848e143325c04ba7306c79
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2022-07-21 14:30:32 +0200

    Compilation fix

--- tests/components/application-connector/tools/external-api-mock-app/config.go
+++ tests/components/application-connector/tools/external-api-mock-app/config.go
@@ -5,35 +5,34 @@ import (
 )
 
 type Config struct {
-	LogLevel          string
-	Port              int
-	MtlsPort          int
-	BasicAuthUser     string
-	BasicAuthPassword string
-	OAuthClientID     string
-	OAuthClientSecret string
+	LogLevel               string
+	Port                   int
+	MtlsPort               int
+	BasicAuthUser          string
+	BasicAuthPassword      string
+	OAuthClientID          string
+	OAuthClientSecret      string
 	RequestHeaders         map[string][]string
 	RequestQueryParameters map[string][]string
-	CaCertPath        string
-	ServerCertPath    string
-	ServerKeyPath     string
-
+	CaCertPath             string
+	ServerCertPath         string
+	ServerKeyPath          string
 }
 
 func NewConfig() *Config {
 	return &Config{
 		LogLevel:               "info",
 		Port:                   8080,
-		MtlsPort: 8090,
+		MtlsPort:               8090,
 		BasicAuthUser:          "user",
 		BasicAuthPassword:      "passwd",
 		OAuthClientID:          "clientID",
 		OAuthClientSecret:      "clientSecret",
 		RequestHeaders:         map[string][]string{"Hkey1": {"Hval1"}, "Hkey2": {"Hval21", "Hval22"}},
 		RequestQueryParameters: map[string][]string{"Qkey1": {"Qval1"}, "Qkey2": {"Qval21", "Qval22"}},
-		CaCertPath: "",
-		ServerCertPath: "",
-		ServerKeyPath: "",
+		CaCertPath:             "/etc/secret-volume/ca.crt",
+		ServerCertPath:         "/etc/secret-volume/server.crt",
+		ServerKeyPath:          "/etc/secret-volume/server.key",
 	}
 }
 
--- tests/components/application-connector/tools/external-api-mock-app/server.go
+++ tests/components/application-connector/tools/external-api-mock-app/server.go
@@ -4,14 +4,13 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
+	"github.com/kyma-project/kyma/tests/components/application-connector/internal/testkit/test-api"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"sync"
-
-	"github.com/kyma-project/kyma/tests/components/application-connector/internal/testkit/test-api"
-	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -26,14 +25,52 @@ func main() {
 	log.Infof("Starting frog auth server application")
 	log.Infof("Config: %s", cfg.String())
 
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
 	basicAuthCredentials := test_api.BasicAuthCredentials{User: cfg.BasicAuthUser, Password: cfg.BasicAuthPassword}
 	oAuthCredentials := test_api.OAuthCredentials{ClientID: cfg.OAuthClientID, ClientSecret: cfg.OAuthClientSecret}
-	expectedRequestParameters := test_api.ExpectedRequestParameters{Headers: cfg.RequestHeaders, QueryParameters: cfg.RequestQueryParameters}
 
-	router := test_api.SetupRoutes(os.Stdout, basicAuthCredentials, oAuthCredentials, expectedRequestParameters)
+	go func() {
+		expectedRequestParameters := test_api.ExpectedRequestParameters{Headers: cfg.RequestHeaders, QueryParameters: cfg.RequestQueryParameters}
 
-	address := fmt.Sprintf(":%d", cfg.Port)
-	log.Fatal(http.ListenAndServe(address, router))
+		router := test_api.SetupRoutes(os.Stdout, basicAuthCredentials, oAuthCredentials, expectedRequestParameters)
+
+		address := fmt.Sprintf(":%d", cfg.Port)
+		log.Fatal(http.ListenAndServe(address, router))
+	}()
+
+	go func() {
+		address := fmt.Sprintf(":%d", cfg.MtlsPort)
+		router := test_api.SetupMTLSRoutes(os.Stdout, oAuthCredentials)
+		mtlsServer := newMTLSServer(cfg.CaCertPath, address, router)
+		log.Fatal(mtlsServer.ListenAndServeTLS(cfg.ServerCertPath, cfg.ServerKeyPath))
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func newMTLSServer(caCertPath, address string, handler http.Handler) *http.Server {
+	// Create a CA certificate pool and add cert.pem to it
+	caCert, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Create the TLS Config with the CA pool and enable Client certificate validation
+	tlsConfig := &tls.Config{
+		ClientCAs:  caCertPool,
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+
+	return &http.Server{
+		Addr:      address,
+		Handler:   handler,
+		TLSConfig: tlsConfig,
+	}
 }
 
 func exitOnError(err error, context string) {
