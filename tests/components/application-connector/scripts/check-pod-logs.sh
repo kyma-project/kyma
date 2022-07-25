0a206c45edd4c3cdf16e413ab9b74a0a949a13b5
Wojciech Nawa <VOID404@users.noreply.github.com>
2022-07-25 13:06:49 +0200

    Implement verbs tests (#14867)
    * Implement method+body tests
    
    * Implement review suggestions
    
    * Bump images
    
    * Correct app name
    
    * Fix log collection
    
    Now script will collect logs correctly without exporting too
    
    Co-authored-by: franpog859 <franciszek.pog@gmail.com>

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -2,10 +2,8 @@
 NAMESPACE=test
 GOPATH=$(go env GOPATH)
 
-podName=$(kubectl get pods -n $NAMESPACE --selector=job-name=application-gateway-test --output=jsonpath='{.items[*].metadata.name}')
-
 if ([[ ${EXPORT_RESULT} == true ]]); then
-	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
+	kubectl -n $NAMESPACE logs -f job/application-gateway-test | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
 else
-	kubectl -n $NAMESPACE logs $podName application-gateway-test
+	kubectl -n $NAMESPACE logs -f job/application-gateway-test
 fi
