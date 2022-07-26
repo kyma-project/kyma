5f35768dd47ea6303c632dd964b94c46e98a08da
Przemyslaw Golicz <przemyslaw.golicz@sap.com>
2022-07-26 12:14:36 +0200

    remove useless comments and configuration fixes

--- tests/components/application-connector/test/application-connectivity-validator/suite_test.go
+++ tests/components/application-connector/test/application-connectivity-validator/suite_test.go
@@ -1,11 +1,7 @@
 package application_connectivity_validator
 
 import (
-	//	cli "github.com/kyma-project/kyma/components/application-operator/pkg/client/clientset/versioned"
-
-	//	cli "github.com/kyma-project/kyma/components/application-operator/pkg/client/clientset/versioned"
 	"github.com/stretchr/testify/suite"
-	//	"k8s.io/client-go/rest"
 	"net/http"
 	"testing"
 )
