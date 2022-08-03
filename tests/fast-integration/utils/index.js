51ac6f73589d322fd0ef6131dc0906238ff9193f
Damian Badura <damian.badura@sap.com>
2022-08-03 11:42:07 +0200

    add more specific log

--- tests/fast-integration/utils/index.js
+++ tests/fast-integration/utils/index.js
@@ -386,7 +386,7 @@ function waitForFunction(name, namespace = 'default', timeout = 90000) {
       `/apis/serverless.kyma-project.io/v1alpha1/namespaces/${namespace}/functions`,
       {},
       (_type, _apiObj, watchObj) => {
-        console.log(watchObj.object.status);
+        console.log(watchObj.object.status.conditions);
         return (
           watchObj.object.metadata.name === name &&
         watchObj.object.status.conditions &&
