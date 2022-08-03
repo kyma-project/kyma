c6ec12abbf977c455b65fcf818bb224a9b698c2d
Aleksei Chernyshov <aleksei.chernyshov@sap.com>
2022-08-03 18:34:58 +0200

    Added directory related unit tests

--- components/telemetry-fluentbit-sidecar/main_test.go
+++ components/telemetry-fluentbit-sidecar/main_test.go
@@ -7,24 +7,25 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestListDir(t *testing.T) {
-	err := os.Mkdir("test-data", 0700)
+func prepareMockDirectories(testDir string) string {
+	dirPath := testDir + "/test-data"
+	err := os.Mkdir(dirPath, 0700)
 	if err != nil {
 		panic(err)
 	}
 
 	files := []string{"file1", "file2", "file3"}
 	for i, file := range files {
-		err = os.Mkdir("test-data/"+file, 0700)
+		err = os.Mkdir(dirPath+"/"+file, 0700)
 		if err != nil {
 			panic(err)
 		}
 		var newFile *os.File = nil
-		newFile, err = os.Create("test-data/" + file + "/test.txt")
+		newFile, err = os.Create(dirPath + "/" + file + "/test.txt")
 		if err != nil {
 			panic(err)
 		}
-		err = os.Truncate("test-data/"+file+"/test.txt", int64(i*100))
+		err = os.Truncate(dirPath+"/"+file+"/test.txt", int64(i*100))
 		if err != nil {
 			panic(err)
 			newFile.Close()
@@ -35,21 +36,39 @@ func TestListDir(t *testing.T) {
 		}
 	}
 
-	size, errDirSize := dirSize("test-data")
-	if errDirSize != nil {
-		panic(errDirSize)
-	}
-	println(size)
+	return dirPath
+}
 
-	directories, errListDirs := listDirs("test-data")
+func TestListDir(t *testing.T) {
+	dirPath := prepareMockDirectories(t.TempDir())
+
+	expectedDirectories := []directory{
+		{name: "file1", size: int64(0)},
+		{name: "file2", size: int64(100)},
+		{name: "file3", size: int64(200)},
+	}
+
+	directories, errListDirs := listDirs(dirPath)
 	if errListDirs != nil {
 		panic(errListDirs)
 	}
-	println(directories)
 
-	err = os.RemoveAll("./test-data")
+	isTrue := (len(directories) == len(expectedDirectories))
+	for i, dir := range directories {
+		if dir != expectedDirectories[i] {
+			isTrue = false
+			break
+		}
+	}
+
+	require.True(t, isTrue)
+}
+
+func TestDirSize(t *testing.T) {
+	dirPath := prepareMockDirectories(t.TempDir())
+	size, err := dirSize(dirPath)
 	if err != nil {
 		panic(err)
 	}
-	require.Equal(t, 1, 1, "1")
+	require.Equal(t, int64(300), size)
 }
