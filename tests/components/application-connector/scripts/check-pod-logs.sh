e67879dfeb5d3caab575ed5071d0b7a259a154fc
Rafał Foks <r.fok74@gmail.com>
2022-08-04 12:50:19 +0200

    Ignore results of subtest parent tests in JUnit

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -3,7 +3,11 @@ NAMESPACE=test
 GOPATH=$(go env GOPATH)
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
+<<<<<<< HEAD
 	kubectl -n $NAMESPACE logs -f job/application-gateway-test | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
+=======
+	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -subtest-mode -set-exit-code > junit-report.xml
+>>>>>>> 8696c3a16 (Ignore results of subtest parent tests in JUnit)
 else
 	kubectl -n $NAMESPACE logs -f job/application-gateway-test
 fi
