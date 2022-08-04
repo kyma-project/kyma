93d7fdcd229c9be35150c7fcbe57847208fdaafa
Rafał Foks <r.fok74@gmail.com>
2022-08-04 14:13:07 +0200

    Change the using version of junit in makefile

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -4,10 +4,14 @@ GOPATH=$(go env GOPATH)
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
 <<<<<<< HEAD
+<<<<<<< HEAD
 	kubectl -n $NAMESPACE logs -f job/application-gateway-test | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
 =======
 	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -subtest-mode -set-exit-code > junit-report.xml
 >>>>>>> 8696c3a16 (Ignore results of subtest parent tests in JUnit)
+=======
+	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report/v2 -subtest-mode -set-exit-code > junit-report.xml
+>>>>>>> fc4b57f50 (Change the using version of junit in makefile)
 else
 	kubectl -n $NAMESPACE logs -f job/application-gateway-test
 fi
