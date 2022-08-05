38f84f01cda055cfd3c7bbbc705527efe37e24d0
Rafał Foks <r.fok74@gmail.com>
2022-08-05 14:46:47 +0200

    Make script universal

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -3,7 +3,7 @@ NAMESPACE=test
 GOPATH=$(go env GOPATH)
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
-	kubectl -n $NAMESPACE logs -f job/compass-runtime-agent-test | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
+	kubectl -n $NAMESPACE logs -f job/$1 | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
 else
-	kubectl -n $NAMESPACE logs -f job/compass-runtime-agent-test
+	kubectl -n $NAMESPACE logs -f job/$1
 fi
