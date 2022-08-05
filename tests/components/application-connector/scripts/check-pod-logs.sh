34ed92eb5a3d42f4e2f9b47114bd754b8973c444
Rafał Foks <r.fok74@gmail.com>
2022-08-05 10:18:00 +0200

    check version on pipelines

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -5,7 +5,8 @@ GOPATH=$(go env GOPATH)
 podName=$(kubectl get pods -n $NAMESPACE --selector=job-name=application-gateway-test --output=jsonpath='{.items[*].metadata.name}')
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
-	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report/v2 -subtest-mode -set-exit-code > junit-report.xml
+  $GOPATH/bin/go-junit-report -version
+	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -subtest-mode -set-exit-code > junit-report.xml
 else
 	kubectl -n $NAMESPACE logs $podName application-gateway-test
 fi
