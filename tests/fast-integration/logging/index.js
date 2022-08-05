0d035c735804cdc9bcaff5514388265daa4e8e51
Dennis Geiselmann <dennis.geiselmann@sap.com>
2022-08-05 10:48:33 +0200

    cleanup

--- tests/fast-integration/logging/index.js
+++ tests/fast-integration/logging/index.js
@@ -22,7 +22,7 @@ function istioAccessLogsTests(startTimestamp) {
 
     it('Should create the Istio Access Logs resource for Loki', async () => {
       await k8sApply(istioAccessLogsResource, namespace);
-      await sleep(10000);
+      await sleep(10000); // wait 10 seconds until resource is ready
     });
 
     it('Should query Loki and verify format of Istio Access Logs', async () => {
