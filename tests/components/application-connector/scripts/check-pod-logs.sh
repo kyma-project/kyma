f40b39405c14642e6004c5efb1597eae76a8609c
Rafał Foks <r.fok74@gmail.com>
2022-08-05 10:39:35 +0200

    change flag

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -5,8 +5,7 @@ GOPATH=$(go env GOPATH)
 podName=$(kubectl get pods -n $NAMESPACE --selector=job-name=application-gateway-test --output=jsonpath='{.items[*].metadata.name}')
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
-  $GOPATH/bin/go-junit-report -version
-	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -subtest-mode ignore-parent-results -set-exit-code > junit-report.xml
+	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -subtest-mode exclude-parents -set-exit-code > junit-report.xml
 else
 	kubectl -n $NAMESPACE logs $podName application-gateway-test
 fi
