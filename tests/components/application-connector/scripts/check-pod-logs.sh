2607f0cb148b109b0d032429d8c9af4913c6b48f
franpog859 <franciszek.pog@gmail.com>
2022-08-10 08:48:23 +0200

    Rename commands and add logs check

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 NAMESPACE=test
 GOPATH=$(go env GOPATH)
+JOB_NAME="$1"
 
-podName=$(kubectl get pods -n $NAMESPACE --selector=job-name=application-gateway-test --output=jsonpath='{.items[*].metadata.name}')
+podName=$(kubectl get pods -n $NAMESPACE --selector=job-name=$JOB_NAME --output=jsonpath='{.items[*].metadata.name}')
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
 	kubectl -n $NAMESPACE logs $podName -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
