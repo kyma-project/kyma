4abbc1eadebd134a158a32752b80a297b5970585
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2022-08-10 08:42:07 +0200

    Code review changes #2

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 NAMESPACE=test
 GOPATH=$(go env GOPATH)
+JOB_NAME=$1
+
+if [ $# -ne 1 ]; then
+  echo "Usage: check-pod-logs.sh <job name>"
+  exit 1
+fi
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
-	kubectl -n $NAMESPACE logs -f job/$1 | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
+	kubectl -n $NAMESPACE logs -f job/"$JOB_NAME" | tee /dev/stderr | $GOPATH/bin/go-junit-report -set-exit-code > junit-report.xml
 else
-	kubectl -n $NAMESPACE logs -f job/$1
+	kubectl -n $NAMESPACE logs -f job/"$JOB_NAME"
 fi
