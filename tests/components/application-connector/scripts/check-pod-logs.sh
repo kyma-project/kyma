a741a2b80301084b98089672978d7d42d22e5456
Rafał Foks <r.fok74@gmail.com>
2022-08-11 12:26:19 +0200

    Getting logs directly from Job

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -8,10 +8,8 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-POD_NAME=$(kubectl get pods -n $NAMESPACE --selector=job-name=$JOB_NAME --output=jsonpath='{.items[*].metadata.name}')
-
 if ([[ ${EXPORT_RESULT} == true ]]); then
-	kubectl -n $NAMESPACE logs $POD_NAME -f | tee /dev/stderr | $GOPATH/bin/go-junit-report -subtest-mode exclude-parents -set-exit-code > junit-report.xml
+	kubectl -n $NAMESPACE logs -f job/$JOB_NAME | tee /dev/stderr | $GOPATH/bin/go-junit-report -subtest-mode exclude-parents -set-exit-code > junit-report.xml
 else
-	kubectl -n $NAMESPACE logs $POD_NAME application-gateway-test
+	kubectl -n $NAMESPACE logs -f job/$JOB_NAME
 fi
