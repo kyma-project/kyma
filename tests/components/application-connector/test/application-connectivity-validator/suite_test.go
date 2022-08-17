a79142d6d6831fe8cd3015dc0e3c6476cead7a95
Wojciech Nawa <wojciech.nawa@sap.com>
2022-08-17 09:10:36 +0200

    Test on-cluster validation

--- tests/components/application-connector/test/application-connectivity-validator/suite_test.go
+++ tests/components/application-connector/test/application-connectivity-validator/suite_test.go
@@ -1,12 +1,12 @@
 package application_connectivity_validator
 
 import (
+	"crypto/tls"
+	_ "embed"
 	"net/http"
 	"testing"
 
-	cli "github.com/kyma-project/kyma/components/application-operator/pkg/client/clientset/versioned"
 	"github.com/stretchr/testify/suite"
-	"k8s.io/client-go/rest"
 )
 
 type ValidatorSuite struct {
@@ -14,21 +14,45 @@ type ValidatorSuite struct {
 	cli *cli.Clientset
 }
 
-func (gs *ValidatorSuite) SetupSuite() {
-	cfg, err := rest.InClusterConfig()
-	gs.Require().Nil(err)
-
-	gs.cli, err = cli.NewForConfig(cfg)
-	gs.Require().Nil(err)
+func (vs *ValidatorSuite) SetupSuite() {
 }
 
-func (gs *ValidatorSuite) TearDownSuite() {
+func (vs *ValidatorSuite) TearDownSuite() {
 	_, err := http.Post("http://localhost:15000/quitquitquit", "", nil)
-	gs.Nil(err)
+	vs.Nil(err)
 	_, err = http.Post("http://localhost:15020/quitquitquit", "", nil)
-	gs.Nil(err)
+	vs.Nil(err)
 }
 
 func TestValidatorSuite(t *testing.T) {
 	suite.Run(t, new(ValidatorSuite))
+}
+
+//go:embed events.crt
+var eventsCrt []byte
+
+//go:embed client.key
+var eventsKey []byte
+
+func (vs *ValidatorSuite) TestValidator() {
+	cert, err := tls.X509KeyPair(eventsCrt, eventsKey)
+
+	cli := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				Certificates: []tls.Certificate{cert},
+			},
+		},
+	}
+
+	// const url = "localhost:8080"
+	const url = "central-application-connectivity-validator.kyma-system.svc.cluster.local:8080"
+	req, err := http.NewRequest(http.MethodGet, "https://"+url+"/event-test/events", nil)
+	vs.Nil(err)
+
+	req.Header.Add("X-Forwarded-Client-Cert", "Subject=\"CN=event-test\"")
+
+	res, err := cli.Do(req)
+	vs.Nil(err)
+	vs.Equal(http.StatusOK, res.StatusCode)
 }
