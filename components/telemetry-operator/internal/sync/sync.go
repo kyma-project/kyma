29022cc26792d85653ee9b9647619badbe4dfef7
Aleksei Chernyshov <aleksei.chernyshov@sap.com>
2022-08-18 13:40:10 +0200

    Testing logging

--- components/telemetry-operator/internal/sync/sync.go
+++ components/telemetry-operator/internal/sync/sync.go
@@ -112,7 +112,7 @@ func (s *LogPipelineSyncer) syncSectionsConfigMap(ctx context.Context, logPipeli
 			changed = true
 		}
 		if !controllerutil.ContainsFinalizer(logPipeline, sectionsConfigMapFinalizer) {
-			log.Info("Call add finalizer")
+			log.Info("Call add finalizer +++")
 			controllerutil.AddFinalizer(logPipeline, sectionsConfigMapFinalizer)
 			changed = true
 		}
@@ -166,7 +166,7 @@ func (s *LogPipelineSyncer) syncFilesConfigMap(ctx context.Context, logPipeline 
 				changed = true
 			}
 			if !controllerutil.ContainsFinalizer(logPipeline, filesFinalizer) {
-				log.Info("Call add finalizer")
+				log.Info("Call add finalizer +++")
 				controllerutil.AddFinalizer(logPipeline, filesFinalizer)
 				changed = true
 			}
