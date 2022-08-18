54e4969b9ed1ecc8b42ed0b1f5ea0fc437cadc45
franpog859 <franciszek.pog@gmail.com>
2022-08-18 11:14:59 +0200

    Change secret namespace

--- tests/components/application-connector/test/application-connectivity-validator/simple_test.go
+++ tests/components/application-connector/test/application-connectivity-validator/simple_test.go
@@ -3,16 +3,5 @@ package application_connectivity_validator
 func (gs *ValidatorSuite) TestValidator() {
 	gs.Run("WIP - positive case", func() {
 		gs.True(true)
-
-		////TODO: Use the certificates
-		//http := NewHttpCli(gs.T())
-		//
-		//url := validatorURL("validator-test-app", "httpbin")
-		//gs.T().Log("Url:", url)
-		//
-		//// Authorize, then call endpoint
-		//res, _, err := http.Get(url)
-		//gs.Nil(err, "Request failed")
-		//gs.Equal(200, res.StatusCode, "Request failed")
 	})
 }
