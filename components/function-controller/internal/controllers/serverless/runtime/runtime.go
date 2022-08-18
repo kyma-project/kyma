b221ab750d48ca593423b2cee4152e6ed5504c61
Marcin Dobrochowski <anoip@o2.pl>
2022-08-18 13:20:33 +0200

    #15016 changed dockerfile naming convention - now we have the original runtime name (without extra dash inside)

--- components/function-controller/internal/controllers/serverless/runtime/runtime.go
+++ components/function-controller/internal/controllers/serverless/runtime/runtime.go
@@ -1,6 +1,7 @@
 package runtime
 
 import (
+	"fmt"
 	serverlessv1alpha2 "github.com/kyma-project/kyma/components/function-controller/pkg/apis/serverless/v1alpha2"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -17,63 +18,45 @@ type Config struct {
 	RuntimeEnvs             []corev1.EnvVar
 }
 
-func GetRuntimeConfig(r serverlessv1alpha2.Runtime) Config {
-	switch r {
-	case serverlessv1alpha2.NodeJs12:
-		return Config{
-			Runtime:                 serverlessv1alpha2.NodeJs12,
-			DependencyFile:          "package.json",
-			FunctionFile:            "handler.js",
-			DockerfileConfigMapName: "dockerfile-nodejs-12",
-			RuntimeEnvs: []corev1.EnvVar{
-				{Name: "NODE_PATH", Value: "$(KUBELESS_INSTALL_VOLUME)/node_modules"},
-				{Name: "FUNC_RUNTIME", Value: "nodejs12"},
-			},
-		}
-	case serverlessv1alpha2.NodeJs14:
-		return Config{
-			Runtime:                 serverlessv1alpha2.NodeJs14,
-			DependencyFile:          "package.json",
-			FunctionFile:            "handler.js",
-			DockerfileConfigMapName: "dockerfile-nodejs-14",
-			RuntimeEnvs: []corev1.EnvVar{{Name: "NODE_PATH", Value: "$(KUBELESS_INSTALL_VOLUME)/node_modules"},
-				{Name: "FUNC_RUNTIME", Value: "nodejs14"},
-			},
-		}
-	case serverlessv1alpha2.NodeJs16:
-		return Config{
-			Runtime:                 serverlessv1alpha2.NodeJs16,
-			DependencyFile:          "package.json",
-			FunctionFile:            "handler.js",
-			DockerfileConfigMapName: "dockerfile-nodejs-16",
-			RuntimeEnvs: []corev1.EnvVar{
-				{Name: "FUNC_RUNTIME", Value: "nodejs16"},
-			},
-		}
+func GetRuntimeConfig(runtime serverlessv1alpha2.Runtime) Config {
+	switch runtime {
+	case serverlessv1alpha2.NodeJs12, serverlessv1alpha2.NodeJs14, serverlessv1alpha2.NodeJs16, serverlessv1alpha2.Python39:
+		break
+	default:
+		runtime = serverlessv1alpha2.NodeJs12
+	}
+	config := Config{
+		Runtime:                 runtime,
+		DependencyFile:          "",
+		FunctionFile:            "",
+		DockerfileConfigMapName: fmt.Sprintf("dockerfile-%s", runtime),
+		RuntimeEnvs: []corev1.EnvVar{
+			{Name: "FUNC_RUNTIME", Value: string(runtime)},
+		},
+	}
+	switch runtime {
+	case serverlessv1alpha2.NodeJs12, serverlessv1alpha2.NodeJs14, serverlessv1alpha2.NodeJs16:
+		config.DependencyFile = "package.json"
+		config.FunctionFile = "handler.js"
+		break
 	case serverlessv1alpha2.Python39:
-		return Config{
-			Runtime:                 serverlessv1alpha2.Python39,
-			DependencyFile:          "requirements.txt",
-			FunctionFile:            "handler.py",
-			DockerfileConfigMapName: "dockerfile-python-39",
-			RuntimeEnvs: []corev1.EnvVar{
+		config.DependencyFile = "requirements.txt"
+		config.FunctionFile = "handler.py"
+		break
+	}
+	switch runtime {
+	case serverlessv1alpha2.NodeJs12, serverlessv1alpha2.NodeJs14:
+		config.RuntimeEnvs = append(config.RuntimeEnvs,
+			corev1.EnvVar{Name: "NODE_PATH", Value: "$(KUBELESS_INSTALL_VOLUME)/node_modules"})
+		break
+	case serverlessv1alpha2.Python39:
+		config.RuntimeEnvs = append(config.RuntimeEnvs,
+			[]corev1.EnvVar{
 				// https://github.com/kubeless/runtimes/blob/master/stable/python/python.jsonnet#L45
 				{Name: "PYTHONPATH", Value: "$(KUBELESS_INSTALL_VOLUME)/lib.python3.9/site-packages:$(KUBELESS_INSTALL_VOLUME)"},
-				{Name: "FUNC_RUNTIME", Value: "python39"},
-				{Name: "PYTHONUNBUFFERED", Value: "TRUE"}},
-		}
-	default:
-		return Config{
-			Runtime:                 serverlessv1alpha2.NodeJs12,
-			DependencyFile:          "package.json",
-			FunctionFile:            "handler.js",
-			DockerfileConfigMapName: "dockerfile-nodejs-12",
-			RuntimeEnvs: []corev1.EnvVar{
-				{Name: "NODE_PATH", Value: "$(KUBELESS_INSTALL_VOLUME)/node_modules"},
-				{Name: "FUNC_RUNTIME", Value: "nodejs12"},
-			},
-		}
+				{Name: "PYTHONUNBUFFERED", Value: "TRUE"}}...)
 	}
+	return config
 }
 
 func GetRuntime(r serverlessv1alpha2.Runtime) Runtime {
