e8822bbb73c6081a8cc53e39e91db142d8eaf0ad
Jan Wozniak <wozniak.jan@gmail.com>
2022-08-25 09:23:03 +0200

    remove binding component

--- components/binding/scripts/create-scenario.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-echo "Creating Scenario"
-
-kubectl apply -f ./scripts/scenario/secret.yaml
-kubectl apply -f ./scripts/scenario/deployment.yaml
-kubectl apply -f ./scripts/scenario/target-kind.yaml
-kubectl apply -f ./scripts/scenario/binding.yaml
