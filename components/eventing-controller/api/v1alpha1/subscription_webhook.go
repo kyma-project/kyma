02398287c0c402d8ce8cc192e51c6cce052b1d0b
Raymond <raymond.pinto@sap.com>
2022-08-31 16:06:10 +0200

    Working code

---
+++ components/eventing-controller/api/v1alpha1/subscription_webhook.go
@@ -0,0 +1,17 @@
+package v1alpha1
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// log is for logging in this package.
+var subscriptionlog = logf.Log.WithName("subscription-resource")
+
+func (r *Subscription) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
