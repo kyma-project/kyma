f10663378f175175c23071ea6512581edeb6c82b
Christoph Kleineweber <c.kleineweber@sap.com>
2022-09-02 15:29:58 +0200

    Fix typo in 2.5 to 2.6 migration script

--- docs/assets/2.5-2.6-cleanup-logging-fluent-bit.sh
+++ docs/assets/2.5-2.6-cleanup-logging-fluent-bit.sh
@@ -8,7 +8,7 @@ kubectl delete -n kyma-system configmap logging-fluent-bit
 kubectl delete -n kyma-system configmap logging-fluent-bit-grafana-dashboard
 kubectl delete -n kyma-system daemonsets.apps logging-fluent-bit
 kubectl delete -n kyma-system peerauthentications.security.istio.io logging-fluent-bit-metrics
-kubectl delete -n kyma-system podsecuritypolicys.policy 000-logging-fluent-bit
+kubectl delete -n kyma-system podsecuritypolicies.policy 000-logging-fluent-bit
 kubectl delete -n kyma-system prometheusrules.monitoring.coreos.com logging-fluent-bit-kyma-fluent-bit.rules
 kubectl delete -n kyma-system secret logging-fluent-bit-es-ca-secret
 kubectl delete -n kyma-system service logging-fluent-bit
