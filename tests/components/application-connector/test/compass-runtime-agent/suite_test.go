d48f8340e2fa62e6abaf85f43486d66c43f4f4de
Rafał Foks <r.fok74@gmail.com>
2022-09-06 15:06:30 +0200

    Secret comparer tests

--- tests/components/application-connector/test/compass-runtime-agent/suite_test.go
+++ tests/components/application-connector/test/compass-runtime-agent/suite_test.go
@@ -8,12 +8,14 @@ import (
 
 	cli "github.com/kyma-project/kyma/components/application-operator/pkg/client/clientset/versioned"
 	"github.com/stretchr/testify/suite"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
 type CompassRuntimeAgentSuite struct {
 	suite.Suite
 	cli            *cli.Clientset
+	kcli           *kubernetes.Clientset
 	directorClient director.Client
 	appComparator  applications.Comparator
 }
@@ -29,11 +31,15 @@ func (gs *CompassRuntimeAgentSuite) SetupSuite() {
 	gs.cli, err = cli.NewForConfig(cfg)
 	gs.Require().Nil(err)
 
+	kcli, err := kubernetes.NewForConfig(cfg)
+	gs.Require().Nil(err)
+
 	// TODO Pass Tenant from configuration
 	gs.directorClient, err = director.NewDirectorClient("")
 	gs.Require().Nil(err)
 
-	gs.appComparator, err = applications.NewComparator(gs.Require())
+	// TODO: Pass namespaces names
+	gs.appComparator, err = applications.NewComparator(gs.Require(), kcli, "", "")
 	gs.Require().Nil(err)
 }
 
--- tests/components/application-connector/test/compass-runtime-agent/testkit/applications/comparator.go
+++ tests/components/application-connector/test/compass-runtime-agent/testkit/applications/comparator.go
@@ -1,19 +1,47 @@
 package applications
 
-import "github.com/stretchr/testify/require"
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
 
 type Comparator interface {
 	Compare(actualApp, expectedApp string) error
 }
 
-func NewComparator(assertions *require.Assertions) (Comparator, error) {
-	return comparator{}, nil
+func NewComparator(assertions *require.Assertions, cli kubernetes.Interface, expectedNamespace, actualNamespace string) (Comparator, error) {
+	return &comparator{
+		assertions:        assertions,
+		cli:               cli,
+		expectedNamespace: expectedNamespace,
+		actualNamespace:   actualNamespace,
+	}, nil
 }
 
 type comparator struct {
-	assertions *require.Assertions
+	assertions        *require.Assertions
+	cli               kubernetes.Interface
+	expectedNamespace string
+	actualNamespace   string
 }
 
 func (c comparator) Compare(actualApp, expectedApp string) error {
+
+	expectedSecretRepo := c.cli.CoreV1().Secrets(c.expectedNamespace)
+	actualSecretRepo := c.cli.CoreV1().Secrets(c.actualNamespace)
+
+	expectedSecret, err := expectedSecretRepo.Get(context.Background(), expectedApp, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	actualSecret, err := actualSecretRepo.Get(context.Background(), actualApp, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	c.assertions.Equal(expectedSecret.Data, actualSecret.Data)
+
 	return nil
 }
