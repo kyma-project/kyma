13220efc4891810445fd8ca8b374d9a92f8fdae6
VladislavPaskar <vladislav.paskar@sap.com>
2022-09-08 17:07:31 +0200

    fix

--- components/eventing-controller/pkg/handlers/jetstream.go
+++ components/eventing-controller/pkg/handlers/jetstream.go
@@ -436,11 +436,6 @@ func (js *JetStream) createConsumer(subscription *eventingv1alpha1.Subscription,
 			}
 			log.Debug("Created consumer on JetStream")
 		}
-
-		if consumerInfo.PushBound {
-			continue
-		}
-
 		// subscribe to the given subject using the existing consumer
 		jsSubscription, err := js.jsCtx.Subscribe(
 			jsSubject,
