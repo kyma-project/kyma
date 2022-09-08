cba85dfc32f7c1896a665e0d734df1eec9e067dd
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2022-09-08 11:03:48 +0200

    Started implementing initilizer

--- tests/components/application-connector/test/compass-runtime-agent/testkit/initialization/initializer.go
+++ tests/components/application-connector/test/compass-runtime-agent/testkit/initialization/initializer.go
@@ -11,17 +11,74 @@ type DirectorClient interface {
 	UnregisterApplication(id string) error
 }
 
-func NewCompassRuntimeAgentConfigurator(directorClient DirectorClient) CompassRuntimeAgentConfigurator {
+type CompassRuntimeAgentConfig struct {
+	ConnectorUrl string
+	RuntimeID    string
+	Token        string
+	Tenant       string
+}
+
+func NewCompassRuntimeAgentConfigurator(directorClient DirectorClient, coreClientset *kubernetes.Clientset, tenant string) CompassRuntimeAgentConfigurator {
 	return compassRuntimeAgentConfigurator{
 		directorClient: directorClient,
+		coreClientset:  coreClientset,
+		tenant:         tenant,
 	}
 }
 
 type compassRuntimeAgentConfigurator struct {
 	directorClient DirectorClient
 	coreClientset  *kubernetes.Clientset
+	tenant         string
 }
 
 func (crc compassRuntimeAgentConfigurator) Configure(runtimeName string) error {
+	runtimeID, err := crc.registerRuntime(runtimeName)
+	if err != nil {
+		return err
+	}
+
+	token, compassConnectorUrl, err := crc.getTokenUrl()
+	if err != nil {
+		return err
+	}
+
+	compassRuntimeAgenConfig := CompassRuntimeAgentConfig{
+		ConnectorUrl: compassConnectorUrl,
+		RuntimeID:    runtimeID,
+		Token:        token,
+		Tenant:       crc.tenant,
+	}
+
+	err = crc.createCompassRuntimeAgentConfig(compassRuntimeAgenConfig)
+	if err != nil {
+		return err
+	}
+
+	err = crc.modifyDeployment()
+	if err != nil {
+		return err
+	}
+
+	return crc.waitForDeploymentRollout()
+}
+
+func (crc compassRuntimeAgentConfigurator) registerRuntime(runtimeName string) (string, error) {
+	return "", nil
+}
+
+func (crc compassRuntimeAgentConfigurator) getTokenUrl() (string, string, error) {
+	return "", "", nil
+}
+
+func (crc compassRuntimeAgentConfigurator) createCompassRuntimeAgentConfig(config CompassRuntimeAgentConfig) error {
+	return nil
+}
+
+func (crc compassRuntimeAgentConfigurator) modifyDeployment() error {
+	return nil
+}
+
+func (crc compassRuntimeAgentConfigurator) waitForDeploymentRollout() error {
 	return nil
 }
