27727f569d5dc01a1c4c67babaa0d2b305a5e2cc
VladislavPaskar <vladislav.paskar@sap.com>
2022-09-12 09:06:08 +0200

    test

--- tests/fast-integration/eventing-test/eventing-test-cleanup.js
+++ tests/fast-integration/eventing-test/eventing-test-cleanup.js
@@ -3,11 +3,15 @@ const {
   slowTime,
   cleanupTestingResources,
 } = require('./utils');
+const {printEventingControllerLogs} = require('../utils');
 
 describe('Eventing tests cleanup', function() {
   this.timeout(timeoutTime);
   this.slow(slowTime);
 
+  it('Printing the controller logs', async function() {
+    await printEventingControllerLogs();
+  });
   it('Cleaning: Test resources should be deleted', async function() {
     await cleanupTestingResources();
   });
