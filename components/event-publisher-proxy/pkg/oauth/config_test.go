ad1eaca9f7410b8580a50278d8f484f8d8749036
Friedrich Wilken <jens.wilken@sap.com>
2022-09-13 20:40:07 +0200

    separate tests in pkg/oauth

--- components/event-publisher-proxy/pkg/oauth/config_test.go
+++ components/event-publisher-proxy/pkg/oauth/config_test.go
@@ -1,16 +1,21 @@
-package oauth
+//go:build unit
+// +build unit
+
+package oauth_test
 
 import (
 	"testing"
 
 	"github.com/kyma-project/kyma/components/event-publisher-proxy/pkg/env"
+
+	sut "github.com/kyma-project/kyma/components/event-publisher-proxy/pkg/oauth"
 )
 
 func TestConfig(t *testing.T) {
 	t.Parallel()
 
 	cfg := &env.BebConfig{ClientID: "someID", ClientSecret: "someSecret", TokenEndpoint: "someEndpoint"}
-	conf := Config(cfg)
+	conf := sut.Config(cfg)
 
 	if cfg.ClientID != conf.ClientID {
 		t.Errorf("Client IDs do not match want:%s but got:%s", cfg.ClientID, conf.ClientID)
