af3141b1ef6f4e45d4fa7b1faf0a97f5a269a0ef
Friedrich Wilken <jens.wilken@sap.com>
2022-09-14 19:58:30 +0200

    separate tests in testing

--- components/eventing-controller/testing/bebmock_test.go
+++ components/eventing-controller/testing/bebmock_test.go
@@ -1,10 +1,15 @@
-package testing
+//go:build unit
+// +build unit
+
+package testing_test
 
 import (
 	"net/url"
 	"testing"
 
 	. "github.com/onsi/gomega"
+
+	ectesting "github.com/kyma-project/kyma/components/eventing-controller/testing"
 )
 
 func Test_GetRestAPIObject(t *testing.T) {
@@ -13,6 +18,6 @@ func Test_GetRestAPIObject(t *testing.T) {
 	urlString := "/messaging/events/subscriptions/my-subscription"
 	urlObject, err := url.Parse(urlString)
 	g.Expect(err).ShouldNot(HaveOccurred())
-	restObject := GetRestAPIObject(urlObject)
+	restObject := ectesting.GetRestAPIObject(urlObject)
 	g.Expect(restObject).To(Equal("my-subscription"))
 }
