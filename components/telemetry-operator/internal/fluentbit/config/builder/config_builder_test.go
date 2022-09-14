2cb0492c5a8594ce775b44dd9500da78a34db7cd
Christoph Kleineweber <c.kleineweber@sap.com>
2022-09-14 15:24:41 +0200

    Remove compass-system namespace by default in LogPipelines

--- components/telemetry-operator/internal/fluentbit/config/builder/config_builder_test.go
+++ components/telemetry-operator/internal/fluentbit/config/builder/config_builder_test.go
@@ -83,7 +83,7 @@ func TestMergeSectionsConfig(t *testing.T) {
     emitter_mem_buf_limit 10M
     emitter_name          foo
     emitter_storage.type  filesystem
-    rule                  $kubernetes['namespace_name'] "^(?!kyma-system$|kyma-integration$|kube-system$|istio-system$).*" foo.$TAG true
+    rule                  $kubernetes['namespace_name'] "^(?!kyma-system$|kyma-integration$|kube-system$|istio-system$|compass-system$).*" foo.$TAG true
 
 [FILTER]
     name   record_modifier
