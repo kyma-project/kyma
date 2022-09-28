d0168fcb4c2f99197a9df9a90b847ae19a4d601b
Friedrich Wilken <jens.wilken@sap.com>
2022-09-28 12:08:51 +0200

    restructure in controllers/errors/

--- components/eventing-controller/controllers/errors/skip_unit_test.go
+++ components/eventing-controller/controllers/errors/skip_unit_test.go
@@ -1,23 +1,27 @@
-package errors
+// todo unit
+
+package errors_test
 
 import (
 	"errors"
 	"fmt"
 	"testing"
+
+	ctrlserrors "github.com/kyma-project/kyma/components/eventing-controller/controllers/errors"
 )
 
 func Test_NewSkippable(t *testing.T) {
 	testCases := []struct {
 		error error
 	}{
-		{error: NewSkippable(nil)},
-		{error: NewSkippable(NewSkippable(nil))},
-		{error: NewSkippable(fmt.Errorf("some error"))},
-		{error: NewSkippable(NewSkippable(fmt.Errorf("some error")))},
+		{error: ctrlserrors.NewSkippable(nil)},
+		{error: ctrlserrors.NewSkippable(ctrlserrors.NewSkippable(nil))},
+		{error: ctrlserrors.NewSkippable(fmt.Errorf("some error"))},
+		{error: ctrlserrors.NewSkippable(ctrlserrors.NewSkippable(fmt.Errorf("some error")))},
 	}
 
 	for _, tc := range testCases {
-		skippableErr := NewSkippable(tc.error)
+		skippableErr := ctrlserrors.NewSkippable(tc.error)
 		if skippableErr == nil {
 			t.Errorf("test NewSkippable retuned nil error")
 			continue
@@ -41,7 +45,7 @@ func Test_IsSkippable(t *testing.T) {
 		},
 		{
 			name:          "skippable error, should be skipped",
-			givenError:    NewSkippable(fmt.Errorf("some errore")),
+			givenError:    ctrlserrors.NewSkippable(fmt.Errorf("some errore")),
 			wantSkippable: true,
 		},
 		{
@@ -51,14 +55,14 @@ func Test_IsSkippable(t *testing.T) {
 		},
 		{
 			name:          "not-skippable error which wraps a skippable error, should not be skipped",
-			givenError:    fmt.Errorf("some error %w", NewSkippable(fmt.Errorf("some error"))),
+			givenError:    fmt.Errorf("some error %w", ctrlserrors.NewSkippable(fmt.Errorf("some error"))),
 			wantSkippable: false,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			if gotSkippable := IsSkippable(tc.givenError); tc.wantSkippable != gotSkippable {
+			if gotSkippable := ctrlserrors.IsSkippable(tc.givenError); tc.wantSkippable != gotSkippable {
 				t.Errorf("test skippable failed, want: %v but got: %v", tc.wantSkippable, gotSkippable)
 			}
 		})
