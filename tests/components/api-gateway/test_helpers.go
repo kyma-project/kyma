6b05e55d05888c43304bd5916aa93609a1fc7e73
Vladimir Videlov <vladimir.videlov@sap.com>
2022-10-07 10:46:07 +0200

    Logs

--- tests/components/api-gateway/test_helpers.go
+++ tests/components/api-gateway/test_helpers.go
@@ -122,9 +122,16 @@ func getOAUTHToken(oauth2Cfg clientcredentials.Config) (*oauth2.Token, error) {
 	var tokenOAUTH *oauth2.Token
 	err := retry.Do(
 		func() error {
+			fmt.Printf("vladimir, clientID: %s", oauth2Cfg.ClientID)
+			fmt.Printf("vladimir, ClientSecret: %s", oauth2Cfg.ClientSecret)
+			fmt.Printf("vladimir, TokenURL: %s", oauth2Cfg.TokenURL)
+			fmt.Printf("vladimir, Scopes: %s", oauth2Cfg.Scopes[0])
 			token, err := oauth2Cfg.Token(context.Background())
 			if err != nil {
 				return fmt.Errorf("error during Token retrival: %+v", err)
+			}
+			if token.AccessToken == "" {
+				return fmt.Errorf("got empty OAuth2 token")
 			}
 			tokenOAUTH = token
 			return nil
