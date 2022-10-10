6aa89af1448b57cf46a3f455d6aad9940a2ea2db
Mohamed Elsayed <m.elsayed@gmail.com>
2022-10-10 08:49:14 +0200

    Update components/function-controller/internal/controllers/serverless/scaling.go
    Co-authored-by: Damian Badura <45110612+dbadura@users.noreply.github.com>

--- components/function-controller/internal/controllers/serverless/scaling.go
+++ components/function-controller/internal/controllers/serverless/scaling.go
@@ -56,7 +56,7 @@ func stateFnCheckHPA(ctx context.Context, r *reconciler, s *systemState) stateFn
 		return stateFnDeleteAllHorizontalPodAutoscalers
 	}
 
-	if numHpa == 1 && s.instance.Spec.ScaleConfig != nil && equalInt32Pointer(s.instance.Spec.ScaleConfig.MinReplicas, s.instance.Spec.ScaleConfig.MaxReplicas) {
+		if numHpa == 1 && !isScaleConfigEnabled(&s.instance) {
 		// this case is when we previously created HPA with maxReplicas > minReplicas, but now user changed
 		// function spec and NOW maxReplicas == minReplicas, so hpa is not needed anymore
 		return stateFnDeleteAllHorizontalPodAutoscalers
