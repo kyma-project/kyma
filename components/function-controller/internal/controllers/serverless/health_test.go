dce169625dbf8617d3d175f75b73b8b05f3346f7
Damian Badura <45110612+dbadura@users.noreply.github.com>
2022-10-27 11:21:30 +0200

    Fix health tests (#15911)
    * adjust test to real implementation
    
    * improve health package
    
    * improve API
    
    * bump image

--- components/function-controller/internal/controllers/serverless/health_test.go
+++ components/function-controller/internal/controllers/serverless/health_test.go
@@ -4,13 +4,9 @@ import (
 	"testing"
 	"time"
 
-	"go.uber.org/zap"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"github.com/kyma-project/kyma/components/function-controller/internal/controllers/serverless"
 	"github.com/stretchr/testify/require"
-	"sigs.k8s.io/controller-runtime/pkg/event"
+	"go.uber.org/zap"
 )
 
 func TestHealthChecker_Checker(t *testing.T) {
@@ -18,9 +14,7 @@ func TestHealthChecker_Checker(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		//GIVEN
 		timeout := 10 * time.Second
-		inCh := make(chan event.GenericEvent, 1)
-		outCh := make(chan bool, 1)
-		checker := serverless.NewHealthChecker(inCh, outCh, timeout, log)
+		checker, inCh, outCh := serverless.NewHealthChecker(timeout, log)
 
 		//WHEN
 		go func() {
@@ -34,12 +28,10 @@ func TestHealthChecker_Checker(t *testing.T) {
 		require.NoError(t, err)
 	})
 
-	t.Run("timeout", func(t *testing.T) {
+	t.Run("Timeout", func(t *testing.T) {
 		//GIVEN
 		timeout := time.Second
-		inCh := make(chan event.GenericEvent, 1)
-		outCh := make(chan bool, 1)
-		checker := serverless.NewHealthChecker(inCh, outCh, timeout, log)
+		checker, inCh, _ := serverless.NewHealthChecker(timeout, log)
 
 		//WHEN
 		go func() {
@@ -50,35 +42,28 @@ func TestHealthChecker_Checker(t *testing.T) {
 
 		//THEN
 		require.Error(t, err)
+		require.Contains(t, err.Error(), "reconcile didn't send confirmation")
+
 	})
 
-	t.Run("can't send check event", func(t *testing.T) {
+	t.Run("Can't send check event", func(t *testing.T) {
 		//GIVEN
 		timeout := time.Second
-		inCh := make(chan event.GenericEvent, 1)
-		outCh := make(chan bool, 1)
-		checker := serverless.NewHealthChecker(inCh, outCh, timeout, log)
+		checker, _, _ := serverless.NewHealthChecker(timeout, log)
 
-		e := event.GenericEvent{
-			Object: &corev1.Event{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "",
-				},
-			},
-		}
-		inCh <- e
 		//WHEN
 		err := checker.Checker(nil)
 
 		//THEN
 		require.Error(t, err)
+		require.Contains(t, err.Error(), "timeout when sending check event")
 	})
 }
 
 func TestHealthName(t *testing.T) {
 	//GIVEN
 	//WHEN
-	// This const is should be longer than 253 characters to avoid collisions with real k8s objects.
+	// This const is longer than 253 characters to avoid collisions with real k8s objects.
 	require.Greater(t, len(serverless.HealthEvent), 253)
 	//THEN
 }
