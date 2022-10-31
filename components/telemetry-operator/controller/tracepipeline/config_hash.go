9691c7d32011641b07180c35711c47cba0664aa9
Christoph Kleineweber <c.kleineweber@sap.com>
2022-10-31 16:45:12 +0100

    Apply review suggestions

--- components/telemetry-operator/controller/tracepipeline/config_hash.go
+++ components/telemetry-operator/controller/tracepipeline/config_hash.go
@@ -4,32 +4,42 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"hash"
+
+	corev1 "k8s.io/api/core/v1"
 )
 
 type ConfigHash struct {
 	hash hash.Hash
 }
 
-func NewConfigHash() *ConfigHash {
-	return &ConfigHash{
+func NewConfigHash(configMaps []corev1.ConfigMap, secrets []corev1.Secret) *ConfigHash {
+	configHash := ConfigHash{
 		hash: sha256.New(),
 	}
+
+	for _, cm := range configMaps {
+		configHash.addStringMap(cm.Data)
+	}
+
+	for _, secret := range secrets {
+		configHash.addByteMap(secret.Data)
+	}
+
+	return &configHash
 }
 
-func (c *ConfigHash) AddStringMap(m map[string]string) *ConfigHash {
+func (c *ConfigHash) addStringMap(m map[string]string) {
 	for k, v := range m {
 		c.hash.Write([]byte(k))
 		c.hash.Write([]byte(v))
 	}
-	return c
 }
 
-func (c *ConfigHash) AddByteMap(m map[string][]byte) *ConfigHash {
+func (c *ConfigHash) addByteMap(m map[string][]byte) {
 	for k, v := range m {
 		c.hash.Write([]byte(k))
 		c.hash.Write(v)
 	}
-	return c
 }
 
 func (c *ConfigHash) Build() string {
--- components/telemetry-operator/controller/tracepipeline/config_hash_test.go
+++ components/telemetry-operator/controller/tracepipeline/config_hash_test.go
@@ -4,34 +4,55 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 )
 
 var (
-	empty1  = map[string]string{}
-	empty2  = map[string][]byte{}
-	config1 = map[string]string{
-		"a": "b",
-		"c": "d",
+	secret1 = corev1.Secret{
+		Data: map[string][]byte{
+			"1": []byte("2"),
+			"3": []byte("4"),
+		},
 	}
-	config2 = map[string][]byte{
-		"1": []byte("2"),
-		"3": []byte("4"),
+	secret2 = corev1.Secret{
+		Data: map[string][]byte{
+			"2": []byte("1"),
+			"4": []byte("3"),
+		},
+	}
+	configMap1 = corev1.ConfigMap{
+		Data: map[string]string{
+			"a": "b",
+			"c": "d",
+		},
+	}
+	configMap2 = corev1.ConfigMap{
+		Data: map[string]string{
+			"b": "a",
+			"d": "c",
+		},
+	}
+	emptySecret = corev1.Secret{
+		Data: map[string][]byte{},
+	}
+	emptyConfigMap = corev1.ConfigMap{
+		Data: map[string]string{},
 	}
 )
 
 func TestEqualConfig(t *testing.T) {
-	hash1 := NewConfigHash().AddStringMap(config1).AddByteMap(config2).Build()
-	hash2 := NewConfigHash().AddStringMap(config1).AddByteMap(config2).Build()
+	hash1 := NewConfigHash([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1}).Build()
+	hash2 := NewConfigHash([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1}).Build()
 	require.Equal(t, hash1, hash2)
 }
 
 func TestUnequalConfig(t *testing.T) {
-	hash1 := NewConfigHash().AddStringMap(config1).AddByteMap(config2).Build()
-	hash2 := NewConfigHash().AddByteMap(config2).AddByteMap(config2).Build()
+	hash1 := NewConfigHash([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1}).Build()
+	hash2 := NewConfigHash([]corev1.ConfigMap{configMap2}, []corev1.Secret{secret2}).Build()
 	require.NotEqual(t, hash1, hash2)
 }
 
 func TestEmptyConfig(t *testing.T) {
-	hash := NewConfigHash().AddStringMap(empty1).AddByteMap(empty2).Build()
+	hash := NewConfigHash([]corev1.ConfigMap{emptyConfigMap}, []corev1.Secret{emptySecret}).Build()
 	require.NotEmpty(t, hash)
 }
