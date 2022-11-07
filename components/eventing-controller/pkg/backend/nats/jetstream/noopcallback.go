6da47bafc7de48936f600635fc4aeb9b24f5eb30
Korbinian Stoemmer <korbinian.stoemmer@sap.com>
2022-11-07 17:16:41 +0100

    add dockerfile.noop

--- components/eventing-controller/pkg/backend/nats/jetstream/noopcallback.go
+++ components/eventing-controller/pkg/backend/nats/jetstream/noopcallback.go
@@ -14,11 +14,12 @@ import (
 const streamPrefix = env.JetStreamSubjectPrefix + "."
 
 func (js *JetStream) getCallback(subKeyPrefix, subscriptionName string) nats.MsgHandler {
+	logger := js.namedLogger()
 	return func(msg *nats.Msg) {
 		et := strings.TrimPrefix(msg.Subject, streamPrefix)
 
 		if err := msg.Ack(); err != nil {
-			ceLogger.Errorw("Failed to ACK an event on JetStream")
+			logger.Errorw("Failed to ACK an event on JetStream")
 		}
 
 		js.metricsCollector.RecordDeliveryPerSubscription(subscriptionName, et, "noop-sink", http.StatusOK)
