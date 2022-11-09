079ebe0a1e4fc8a38f492031a4317bdedfca6fec
jaroslaw-pieszka <jaroslaw.pieszka@sap.com>
2022-11-09 12:53:48 +0100

    initial version

--- tests/fast-integration/skr-test/test.js
+++ tests/fast-integration/skr-test/test.js
@@ -4,6 +4,8 @@ const {
 } = require('./index');
 const {getOrProvisionSKR} = require('./provision/provision-skr');
 const {deprovisionAndUnregisterSKR} = require('./provision/deprovision-skr');
+const {getSecret} = require('../utils');
+const {expect} = require("chai");
 
 const skipProvisioning = process.env.SKIP_PROVISIONING === 'true';
 const provisioningTimeout = 1000 * 60 * 30; // 30m
@@ -32,6 +34,20 @@ describe('SKR test', function() {
     skr = await getOrProvisionSKR(options, skipProvisioning, provisioningTimeout);
     options = skr.options;
   });
+
+  // Check if the secret with BTP credentials is created properly
+  it('should check if secret exists and contains the expected data', async function() {
+    console.log(`Checking the contents of the "sap-btp-manager" secret`);
+    const secretToCheck = await getSecret('sap-btp-manager', 'kyma-system');
+    expect(secretToCheck).to.not.be.empty;
+    expect(secretToCheck.metadata.labels['app.kubernetes.io/managed-by']).to.equal('kcp-kyma-environment-broker');
+    expect(secretToCheck.data).to.have.property('clientid');
+    expect(secretToCheck.data).to.have.property('clientsecret');
+    expect(secretToCheck.data).to.have.property('sm_url');
+    expect(secretToCheck.data).to.have.property('tokenurl');
+    expect(secretToCheck.data).to.have.property('cluster_id');
+  });
+
 
   // Run the OIDC tests
   oidcE2ETest(getShootOptionsFunc, getShootInfoFunc);
