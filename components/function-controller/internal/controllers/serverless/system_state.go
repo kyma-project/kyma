7847bad7eee4e6d7af578cbe564fd8c00a23ba69
Marcin Dobrochowski <39153236+anoipm@users.noreply.github.com>
2022-11-10 14:30:06 +0100

    apply security context to the function workloads (#16014)

--- components/function-controller/internal/controllers/serverless/system_state.go
+++ components/function-controller/internal/controllers/serverless/system_state.go
@@ -2,6 +2,7 @@ package serverless
 
 import (
 	"fmt"
+	"k8s.io/utils/pointer"
 	"path"
 	"strings"
 
@@ -129,11 +130,14 @@ func (s *systemState) fnJobChanged(expectedJob batchv1.Job) bool {
 }
 
 var (
-	one          = int32(1)
-	zero         = int32(0)
-	rootUser     = int64(0)
-	functionUser = int64(1000)
-	optionalTrue = true
+	one               = int32(1)
+	zero              = int32(0)
+	rootUser          = int64(0)
+	rootUserGroup     = int64(0)
+	functionUser      = int64(10001)
+	functionUserGroup = int64(10001)
+	optionalTrue      = true
+	defaultProcMount  = corev1.DefaultProcMount
 )
 
 func (s *systemState) buildGitJob(gitOptions git.Options, cfg cfg) batchv1.Job {
@@ -146,6 +150,77 @@ func (s *systemState) buildGitJob(gitOptions git.Options, cfg cfg) batchv1.Job {
 
 	resourceRequirements := getBuildResourceRequiremenets(s)
 	rtmCfg := fnRuntime.GetRuntimeConfig(s.instance.Spec.Runtime)
+
+	templateSpec := corev1.PodSpec{
+		Volumes: []corev1.Volume{
+			{
+				Name: "credentials",
+				VolumeSource: corev1.VolumeSource{
+					Secret: &corev1.SecretVolumeSource{
+						SecretName: cfg.docker.ActiveRegistryConfigSecretName,
+						Items: []corev1.KeyToPath{
+							{
+								Key:  ".dockerconfigjson",
+								Path: ".docker/config.json",
+							},
+						},
+					},
+				},
+			},
+			{
+				Name: "runtime",
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{Name: rtmCfg.DockerfileConfigMapName},
+					},
+				},
+			},
+			{
+				Name:         "workspace",
+				VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
+			},
+			{
+				Name: "registry-config",
+				VolumeSource: corev1.VolumeSource{
+					Secret: &corev1.SecretVolumeSource{
+						SecretName: cfg.fn.PackageRegistryConfigSecretName,
+						Optional:   &optionalTrue,
+					},
+				},
+			},
+		},
+		InitContainers: []corev1.Container{
+			{
+				Name:            "repo-fetcher",
+				Image:           cfg.fn.Build.RepoFetcherImage,
+				Env:             buildRepoFetcherEnvVars(&s.instance, gitOptions),
+				ImagePullPolicy: corev1.PullAlways,
+				VolumeMounts: []corev1.VolumeMount{
+					{
+						Name:      "workspace",
+						MountPath: workspaceMountPath,
+					},
+				},
+				SecurityContext: restrictiveContainerSecurityContext(),
+			},
+		},
+		Containers: []corev1.Container{
+			{
+				Name:            "executor",
+				Image:           cfg.fn.Build.ExecutorImage,
+				Args:            args,
+				Resources:       resourceRequirements,
+				VolumeMounts:    s.getGitBuildJobVolumeMounts(rtmCfg),
+				ImagePullPolicy: corev1.PullIfNotPresent,
+				Env: []corev1.EnvVar{
+					{Name: "DOCKER_CONFIG", Value: "/docker/.docker/"},
+				},
+				SecurityContext: buildJobContainerSecurityContext(),
+			},
+		},
+		RestartPolicy: corev1.RestartPolicyNever,
+	}
+	enrichPodSpecWithSecurityContext(&templateSpec, rootUser, rootUserGroup)
 
 	return batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
@@ -163,76 +238,7 @@ func (s *systemState) buildGitJob(gitOptions git.Options, cfg cfg) batchv1.Job {
 					Labels:      s.functionLabels(),
 					Annotations: istioSidecarInjectFalse,
 				},
-				Spec: corev1.PodSpec{
-					Volumes: []corev1.Volume{
-						{
-							Name: "credentials",
-							VolumeSource: corev1.VolumeSource{
-								Secret: &corev1.SecretVolumeSource{
-									SecretName: cfg.docker.ActiveRegistryConfigSecretName,
-									Items: []corev1.KeyToPath{
-										{
-											Key:  ".dockerconfigjson",
-											Path: ".docker/config.json",
-										},
-									},
-								},
-							},
-						},
-						{
-							Name: "runtime",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{Name: rtmCfg.DockerfileConfigMapName},
-								},
-							},
-						},
-						{
-							Name:         "workspace",
-							VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
-						},
-						{
-							Name: "registry-config",
-							VolumeSource: corev1.VolumeSource{
-								Secret: &corev1.SecretVolumeSource{
-									SecretName: cfg.fn.PackageRegistryConfigSecretName,
-									Optional:   &optionalTrue,
-								},
-							},
-						},
-					},
-					InitContainers: []corev1.Container{
-						{
-							Name:            "repo-fetcher",
-							Image:           cfg.fn.Build.RepoFetcherImage,
-							Env:             buildRepoFetcherEnvVars(&s.instance, gitOptions),
-							ImagePullPolicy: corev1.PullAlways,
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "workspace",
-									MountPath: workspaceMountPath,
-								},
-							},
-						},
-					},
-					Containers: []corev1.Container{
-						{
-							Name:            "executor",
-							Image:           cfg.fn.Build.ExecutorImage,
-							Args:            args,
-							Resources:       resourceRequirements,
-							VolumeMounts:    s.getGitBuildJobVolumeMounts(rtmCfg),
-							ImagePullPolicy: corev1.PullIfNotPresent,
-							Env: []corev1.EnvVar{
-								{Name: "DOCKER_CONFIG", Value: "/docker/.docker/"},
-							},
-						},
-					},
-					RestartPolicy: corev1.RestartPolicyNever,
-					SecurityContext: &corev1.PodSecurityContext{
-						RunAsUser: &rootUser,
-					},
-				},
+				Spec: templateSpec,
 			},
 		},
 	}
@@ -248,6 +254,66 @@ func (s *systemState) buildJob(configMapName string, cfg cfg) batchv1.Job {
 
 	resourceRequirements := getBuildResourceRequiremenets(s)
 	labels := s.functionLabels()
+
+	templateSpec := corev1.PodSpec{
+		Volumes: []corev1.Volume{
+			{
+				Name: "sources",
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{Name: configMapName},
+					},
+				},
+			},
+			{
+				Name: "runtime",
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{Name: rtmCfg.DockerfileConfigMapName},
+					},
+				},
+			},
+			{
+				Name: "credentials",
+				VolumeSource: corev1.VolumeSource{
+					Secret: &corev1.SecretVolumeSource{
+						SecretName: cfg.docker.ActiveRegistryConfigSecretName,
+						Items: []corev1.KeyToPath{
+							{
+								Key:  ".dockerconfigjson",
+								Path: ".docker/config.json",
+							},
+						},
+					},
+				},
+			},
+			{
+				Name: "registry-config",
+				VolumeSource: corev1.VolumeSource{
+					Secret: &corev1.SecretVolumeSource{
+						SecretName: cfg.fn.PackageRegistryConfigSecretName,
+						Optional:   &optionalTrue,
+					},
+				},
+			},
+		},
+		Containers: []corev1.Container{
+			{
+				Name:            "executor",
+				Image:           cfg.fn.Build.ExecutorImage,
+				Args:            args,
+				Resources:       resourceRequirements,
+				VolumeMounts:    getBuildJobVolumeMounts(rtmCfg),
+				ImagePullPolicy: corev1.PullIfNotPresent,
+				Env: []corev1.EnvVar{
+					{Name: "DOCKER_CONFIG", Value: "/docker/.docker/"},
+				},
+				SecurityContext: buildJobContainerSecurityContext(),
+			},
+		},
+		RestartPolicy: corev1.RestartPolicyNever,
+	}
+	enrichPodSpecWithSecurityContext(&templateSpec, rootUser, rootUserGroup)
 
 	return batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
@@ -265,66 +331,7 @@ func (s *systemState) buildJob(configMapName string, cfg cfg) batchv1.Job {
 					Labels:      labels,
 					Annotations: istioSidecarInjectFalse,
 				},
-				Spec: corev1.PodSpec{
-					Volumes: []corev1.Volume{
-						{
-							Name: "sources",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{Name: configMapName},
-								},
-							},
-						},
-						{
-							Name: "runtime",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{Name: rtmCfg.DockerfileConfigMapName},
-								},
-							},
-						},
-						{
-							Name: "credentials",
-							VolumeSource: corev1.VolumeSource{
-								Secret: &corev1.SecretVolumeSource{
-									SecretName: cfg.docker.ActiveRegistryConfigSecretName,
-									Items: []corev1.KeyToPath{
-										{
-											Key:  ".dockerconfigjson",
-											Path: ".docker/config.json",
-										},
-									},
-								},
-							},
-						},
-						{
-							Name: "registry-config",
-							VolumeSource: corev1.VolumeSource{
-								Secret: &corev1.SecretVolumeSource{
-									SecretName: cfg.fn.PackageRegistryConfigSecretName,
-									Optional:   &optionalTrue,
-								},
-							},
-						},
-					},
-					Containers: []corev1.Container{
-						{
-							Name:            "executor",
-							Image:           cfg.fn.Build.ExecutorImage,
-							Args:            args,
-							Resources:       resourceRequirements,
-							VolumeMounts:    getBuildJobVolumeMounts(rtmCfg),
-							ImagePullPolicy: corev1.PullIfNotPresent,
-							Env: []corev1.EnvVar{
-								{Name: "DOCKER_CONFIG", Value: "/docker/.docker/"},
-							},
-						},
-					},
-					RestartPolicy:      corev1.RestartPolicyNever,
-					SecurityContext: &corev1.PodSecurityContext{
-						RunAsUser: &rootUser,
-					},
-				},
+				Spec: templateSpec,
 			},
 		},
 	}
@@ -387,6 +394,80 @@ func (s *systemState) buildDeployment(cfg buildDeploymentArgs) appsv1.Deployment
 	)
 	envs = append(envs, deploymentEnvs...)
 
+	templateSpec := corev1.PodSpec{
+		Volumes: []corev1.Volume{{
+			Name: volumeName,
+			VolumeSource: corev1.VolumeSource{
+				EmptyDir: &corev1.EmptyDirVolumeSource{
+					Medium:    corev1.StorageMediumDefault,
+					SizeLimit: &emptyDirVolumeSize,
+				},
+			},
+		}},
+		Containers: []corev1.Container{
+			{
+				Name:      functionContainerName,
+				Image:     imageName,
+				Env:       envs,
+				Resources: *s.instance.Spec.ResourceConfiguration.Function.Resources,
+				VolumeMounts: []corev1.VolumeMount{{
+					Name: volumeName,
+					/* needed in order to have python functions working:
+					python functions need writable /tmp dir, but we disable writing to root filesystem via
+					security context below. That's why we override this whole /tmp directory with emptyDir volume.
+					We've decided to add this directory to be writable by all functions, as it may come in handy
+					*/
+					MountPath: "/tmp",
+					ReadOnly:  false,
+				}},
+				/*
+					In order to mark pod as ready we need to ensure the function is actually running and ready to serve traffic.
+					We do this but first ensuring that sidecar is ready by using "proxy.istio.io/config": "{ \"holdApplicationUntilProxyStarts\": true }", annotation
+					Second thing is setting that probe which continuously
+				*/
+				StartupProbe: &corev1.Probe{
+					Handler: corev1.Handler{
+						HTTPGet: &corev1.HTTPGetAction{
+							Path: "/healthz",
+							Port: svcTargetPort,
+						},
+					},
+					InitialDelaySeconds: 0,
+					PeriodSeconds:       5,
+					SuccessThreshold:    1,
+					FailureThreshold:    30, // FailureThreshold * PeriodSeconds = 150s in this case, this should be enough for any function pod to start up
+				},
+				ReadinessProbe: &corev1.Probe{
+					Handler: corev1.Handler{
+						HTTPGet: &corev1.HTTPGetAction{
+							Path: "/healthz",
+							Port: svcTargetPort,
+						},
+					},
+					InitialDelaySeconds: 0, // startup probe exists, so delaying anything here doesn't make sense
+					FailureThreshold:    1,
+					PeriodSeconds:       5,
+					TimeoutSeconds:      2,
+				},
+				LivenessProbe: &corev1.Probe{
+					Handler: corev1.Handler{
+						HTTPGet: &corev1.HTTPGetAction{
+							Path: "/healthz",
+							Port: svcTargetPort,
+						},
+					},
+					FailureThreshold: 3,
+					PeriodSeconds:    5,
+					TimeoutSeconds:   4,
+				},
+				ImagePullPolicy: corev1.PullIfNotPresent,
+				SecurityContext: restrictiveContainerSecurityContext(),
+			},
+		},
+		ServiceAccountName: cfg.ImagePullAccountName,
+	}
+	enrichPodSpecWithSecurityContext(&templateSpec, functionUser, functionUserGroup)
+
 	return appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			GenerateName: fmt.Sprintf("%s-", s.instance.GetName()),
@@ -406,89 +487,7 @@ func (s *systemState) buildDeployment(cfg buildDeploymentArgs) appsv1.Deployment
 						"proxy.istio.io/config": "{ \"holdApplicationUntilProxyStarts\": true }",
 					},
 				},
-				Spec: corev1.PodSpec{
-					Volumes: []corev1.Volume{{
-						Name: volumeName,
-						VolumeSource: corev1.VolumeSource{
-							EmptyDir: &corev1.EmptyDirVolumeSource{
-								Medium:    corev1.StorageMediumDefault,
-								SizeLimit: &emptyDirVolumeSize,
-							},
-						},
-					}},
-					Containers: []corev1.Container{
-						{
-							Name:      functionContainerName,
-							Image:     imageName,
-							Env:       envs,
-							Resources: *s.instance.Spec.ResourceConfiguration.Function.Resources,
-							VolumeMounts: []corev1.VolumeMount{{
-								Name: volumeName,
-								/* needed in order to have python functions working:
-								python functions need writable /tmp dir, but we disable writing to root filesystem via
-								security context below. That's why we override this whole /tmp directory with emptyDir volume.
-								We've decided to add this directory to be writable by all functions, as it may come in handy
-								*/
-								MountPath: "/tmp",
-								ReadOnly:  false,
-							}},
-							/*
-								In order to mark pod as ready we need to ensure the function is actually running and ready to serve traffic.
-								We do this but first ensuring that sidecar is ready by using "proxy.istio.io/config": "{ \"holdApplicationUntilProxyStarts\": true }", annotation
-								Second thing is setting that probe which continuously
-							*/
-							StartupProbe: &corev1.Probe{
-								Handler: corev1.Handler{
-									HTTPGet: &corev1.HTTPGetAction{
-										Path: "/healthz",
-										Port: svcTargetPort,
-									},
-								},
-								InitialDelaySeconds: 0,
-								PeriodSeconds:       5,
-								SuccessThreshold:    1,
-								FailureThreshold:    30, // FailureThreshold * PeriodSeconds = 150s in this case, this should be enough for any function pod to start up
-							},
-							ReadinessProbe: &corev1.Probe{
-								Handler: corev1.Handler{
-									HTTPGet: &corev1.HTTPGetAction{
-										Path: "/healthz",
-										Port: svcTargetPort,
-									},
-								},
-								InitialDelaySeconds: 0, // startup probe exists, so delaying anything here doesn't make sense
-								FailureThreshold:    1,
-								PeriodSeconds:       5,
-								TimeoutSeconds:      2,
-							},
-							LivenessProbe: &corev1.Probe{
-								Handler: corev1.Handler{
-									HTTPGet: &corev1.HTTPGetAction{
-										Path: "/healthz",
-										Port: svcTargetPort,
-									},
-								},
-								FailureThreshold: 3,
-								PeriodSeconds:    5,
-								TimeoutSeconds:   4,
-							},
-							ImagePullPolicy: corev1.PullIfNotPresent,
-							SecurityContext: &corev1.SecurityContext{
-								Capabilities: &corev1.Capabilities{
-									Add:  []corev1.Capability{},
-									Drop: []corev1.Capability{"ALL"},
-								},
-								Privileged:               boolPtr(false),
-								RunAsUser:                &functionUser,
-								RunAsGroup:               &functionUser,
-								RunAsNonRoot:             boolPtr(true),
-								ReadOnlyRootFilesystem:   boolPtr(true),
-								AllowPrivilegeEscalation: boolPtr(false),
-							},
-						},
-					},
-					ServiceAccountName: cfg.ImagePullAccountName,
-				},
+				Spec: templateSpec,
 			},
 		},
 	}
@@ -657,4 +656,47 @@ func (s *systemState) jobFailed(p func(reason string) bool) bool {
 	}
 
 	return jobFailed(s.jobs.Items[0], p)
+}
+
+// security context is set to fulfill the baseline security profile
+// based on https://raw.githubusercontent.com/kyma-project/community/main/concepts/psp-replacement/baseline-pod-spec.yaml
+func restrictiveContainerSecurityContext() *corev1.SecurityContext {
+	return &corev1.SecurityContext{
+		Privileged: pointer.Bool(false),
+		Capabilities: &corev1.Capabilities{
+			Drop: []corev1.Capability{
+				"ALL",
+			},
+		},
+		ProcMount:              &defaultProcMount,
+		ReadOnlyRootFilesystem: pointer.Bool(true),
+	}
+}
+
+// build job requires additional permissions some than the restrictive version
+func buildJobContainerSecurityContext() *corev1.SecurityContext {
+	securityContext := restrictiveContainerSecurityContext()
+	securityContext.Capabilities.Add = []corev1.Capability{
+		"CHOWN",        // for "chown"
+		"FOWNER",       // for "chmod"
+		"SETGID",       // for "fork"
+		"DAC_OVERRIDE", // for "open"
+	}
+	securityContext.ReadOnlyRootFilesystem = pointer.Bool(false)
+	return securityContext
+}
+
+// security context is set to fulfill the baseline security profile
+// based on https://raw.githubusercontent.com/kyma-project/community/main/concepts/psp-replacement/baseline-pod-spec.yaml
+func enrichPodSpecWithSecurityContext(ps *corev1.PodSpec, user int64, userGroup int64) {
+	ps.SecurityContext = &corev1.PodSecurityContext{
+		RunAsUser:  &user,
+		RunAsGroup: &userGroup,
+		SeccompProfile: &corev1.SeccompProfile{
+			Type: corev1.SeccompProfileTypeRuntimeDefault,
+		},
+	}
+	ps.HostNetwork = false
+	ps.HostPID = false
+	ps.HostIPC = false
 }
