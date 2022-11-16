b942d66274b410ca9265e3752ac63f7ec1a8fd51
Stanislav Khalash <stanislav.khalash@sap.com>
2022-11-16 12:08:21 +0100

    Execute tests multiple times

--- components/telemetry-operator/internal/configchecksum/checksum_test.go
+++ components/telemetry-operator/internal/configchecksum/checksum_test.go
@@ -42,7 +42,7 @@ var (
 	configMap2 = corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "configMap2",
-			Namespace: "ns2",
+			Namespace: "ns1",
 		},
 		Data: map[string]string{
 			"b": "a",
@@ -57,25 +57,35 @@ var (
 	}
 )
 
+var iterations = 50
+
 func TestEqualConfig(t *testing.T) {
-	hash1 := Calculate([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1})
-	hash2 := Calculate([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1})
-	require.Equal(t, hash1, hash2)
+	for i := 0; i < iterations; i++ {
+		hash1 := Calculate([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1})
+		hash2 := Calculate([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1})
+		require.Equal(t, hash1, hash2)
+	}
 }
 
 func TestUnequalConfig(t *testing.T) {
-	hash1 := Calculate([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1})
-	hash2 := Calculate([]corev1.ConfigMap{configMap2}, []corev1.Secret{secret2})
-	require.NotEqual(t, hash1, hash2)
+	for i := 0; i < iterations; i++ {
+		hash1 := Calculate([]corev1.ConfigMap{configMap1}, []corev1.Secret{secret1})
+		hash2 := Calculate([]corev1.ConfigMap{configMap2}, []corev1.Secret{secret2})
+		require.NotEqual(t, hash1, hash2)
+	}
 }
 
 func TestEmptyConfig(t *testing.T) {
-	hash := Calculate([]corev1.ConfigMap{emptyConfigMap}, []corev1.Secret{emptySecret})
-	require.NotEmpty(t, hash)
+	for i := 0; i < iterations; i++ {
+		hash := Calculate([]corev1.ConfigMap{emptyConfigMap}, []corev1.Secret{emptySecret})
+		require.NotEmpty(t, hash)
+	}
 }
 
 func TestOrderDoesNotMatter(t *testing.T) {
-	hash1 := Calculate([]corev1.ConfigMap{configMap1, configMap2}, []corev1.Secret{secret1})
-	hash2 := Calculate([]corev1.ConfigMap{configMap2, configMap1}, []corev1.Secret{secret1})
-	require.Equal(t, hash1, hash2)
+	for i := 0; i < iterations; i++ {
+		hash1 := Calculate([]corev1.ConfigMap{configMap1, configMap2}, []corev1.Secret{secret1})
+		hash2 := Calculate([]corev1.ConfigMap{configMap2, configMap1}, []corev1.Secret{secret1})
+		require.Equal(t, hash1, hash2)
+	}
 }
