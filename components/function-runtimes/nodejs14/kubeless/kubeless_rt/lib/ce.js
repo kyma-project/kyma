752f9698229d73fab803b7ecd0613dc4025c0b1f
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2022-11-17 11:37:20 +0100

    Support structured json format of incomming cloud events in nodejs functions

--- components/function-runtimes/nodejs14/kubeless/kubeless_rt/lib/ce.js
+++ components/function-runtimes/nodejs14/kubeless/kubeless_rt/lib/ce.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const axios = require('axios');
+const { HTTP, CloudEvent } = require('cloudevents');
+const charset = 'utf-8'
 
 const publishProxyAddress = (process.env.PUBLISHER_PROXY_ADDRESS);
 
@@ -9,26 +11,30 @@ module.exports = {
 };
 
 function buildEvent(req, res, tracer) {
-    let data = req.body;
-    if (!req.is('multipart/*') && req.body.length > 0) {
-        if (req.is('application/json')) {
-            data = JSON.parse(req.body.toString('utf-8'))
-        } else {
-            data = req.body.toString('utf-8')
-        }
-    }
-
-    return Object.assign( 
-        buildCeHeaders(req), {
+    let event = {
         tracer,
-        data,
         'extensions': { request: req, response: res },
         setResponseHeader: (key, value) => setResponseHeader(res, key, value),
         setResponseContentType: (type) => setResponseContentType(res, type),
         setResponseStatus: (status) => setResponseStatus(res, status),
+        //deprecated
         publishCloudEvent: (data) => publishCloudEvent(data),
-        buildResponseCloudEvent: (id, type, data) => buildResponseCloudEvent(req, id, type, data)
-    });
+        //deprecated
+        buildResponseCloudEvent: (id, type, data) => buildResponseCloudEvent(req, id, type, data),
+        emitCloudEvent: (type, source, data, eventtypeversion) => emitCloudEvent(type, source, data, eventtypeversion),
+    };
+
+    let stringifiedReqestBody = req.body.toString(charset);
+    if (!req.is('multipart/*') && req.body.length > 0) {
+        if(isCloudEvent(req)) {
+            event = Object.assign(event,buildCloudEventAttributes(req, JSON.parse(stringifiedReqestBody)));    
+        } else if (req.is('application/json')) {
+            event = Object.assign(event,{'data':JSON.parse(stringifiedReqestBody)});
+        } else {
+            event = Object.assign(event,{'data':stringifiedReqestBody});
+        }
+    }
+    return event;
 }
 
 function setResponseHeader(res, key, value) {
@@ -44,6 +50,7 @@ function setResponseStatus(res, status) {
 }
 
 function publishCloudEvent(data) {
+    console.warn("publishCloudEvent is deprecated. Use emitCloudEvent")
     return axios({
         method: "post",
         baseURL: publishProxyAddress,
@@ -64,6 +71,7 @@ function resolvedatatype(data){
 }
 
 function buildResponseCloudEvent(req, id, type, data) {
+    console.warn("buildResponseCloudEvent is deprecated. Use emitCloudEvent")
     return {
         'type': type,
         'source': req.get('ce-source'),
@@ -75,13 +83,53 @@ function buildResponseCloudEvent(req, id, type, data) {
     };
 }
 
-function buildCeHeaders(req) {
+function isCloudEvent(req) {
+    return req.is('application/cloudevents+json') || hasCeHeaders(req);
+}
+
+function hasCeHeaders(req) {
+    return req.get('ce-type') && req.get('ce-source');
+}
+
+function buildCloudEventAttributes(req, data) {
+    const receivedEvent = HTTP.toEvent({ headers: req.headers, body: data });  
     return {
-        'ce-type': req.get('ce-type'),
-        'ce-source': req.get('ce-source'),
-        'ce-eventtypeversion': req.get('ce-eventtypeversion'),
-        'ce-specversion': req.get('ce-specversion'),
-        'ce-id': req.get('ce-id'),
-        'ce-time': req.get('ce-time'),
+        'ce-type': receivedEvent.type,
+        'ce-source': receivedEvent.source,
+        'ce-eventtypeversion': receivedEvent.eventtypeversion,
+        'ce-specversion': receivedEvent.specversion,
+        'ce-id': receivedEvent.id,
+        'ce-time': receivedEvent.time,
+        'ce-datacontenttype': receivedEvent.datacontenttype,
+        'data': receivedEvent.data
     };
+}
+
+function emitCloudEvent(type, source, data, optionalCloudEventAttributes) {
+
+    let optionalCloudEventAttributesInput = optionalCloudEventAttributes
+    if(!optionalCloudEventAttributesInput){
+        optionalCloudEventAttributesInput = {}
+    }
+
+    let cloudEventInput = {
+        type,
+        source,
+        data,
+    }
+
+    if(!optionalCloudEventAttributesInput.datacontenttype){
+        optionalCloudEventAttributesInput.datacontenttype = resolvedatatype(data);
+    }
+    
+    cloudEventInput = Object.assign(cloudEventInput, optionalCloudEventAttributesInput)
+    const ce = new CloudEvent(cloudEventInput);
+    return axios({
+        method: "post",
+        baseURL: publishProxyAddress,
+        headers: {
+            "Content-Type": "application/cloudevents+json"
+        },
+        data: ce.toJSON(),
+    });
 }
