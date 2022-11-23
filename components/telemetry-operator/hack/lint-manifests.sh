c26c573a8f7b176a059cb1da945c13172e392376
Aleksei Chernyshov <aleksei.chernyshov@sap.com>
2022-11-23 11:31:20 +0100

    Moved script to telemetry directory

--- components/telemetry-operator/hack/lint-manifests.sh
+++ components/telemetry-operator/hack/lint-manifests.sh
@@ -3,16 +3,19 @@
 readonly OPERATOR_DIR=$1
 readonly CONTROLLER_GEN=$2
 readonly CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+readonly ROOT_PATH="$( cd "${CURRENT_DIR}/../../../" && pwd )"
 
 TMP_DIR=$(mktemp -d)
 
-source "${CURRENT_DIR}/utilities.sh" || { echo 'Cannot load CI utilities.'; exit 1; }
+source "${ROOT_PATH}/hack/utilities.sh" || { echo 'Cannot load CI utilities.'; exit 1; }
 
 cleanup() {
     rm -rf "${TMP_DIR}" || true
 }
 
 trap cleanup EXIT SIGINT
+
+echo $ROOT_PATH
 
 $CONTROLLER_GEN rbac:roleName=manager-role crd webhook paths="${OPERATOR_DIR}/..." output:crd:artifacts:config="${TMP_DIR}"
 
