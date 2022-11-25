b90b7f39233f7a5029dc54188746058741a76184
VladislavPaskar <vladislav.paskar@sap.com>
2022-11-25 09:47:14 +0100

    fix

--- components/eventing-controller/pkg/backend/jetstreamv2/jetstream_integration_test.go
+++ components/eventing-controller/pkg/backend/jetstreamv2/jetstream_integration_test.go
@@ -250,7 +250,7 @@ func defaultNatsConfig(url string) env.NatsConfig {
 		MaxReconnects:           DefaultMaxReconnects,
 		ReconnectWait:           3 * time.Second,
 		JSStreamName:            DefaultStreamName,
-		JSStreamStorageType:     StorageTypeMemory,
+		JSStreamStorageType:     StorageTypeFile,
 		JSStreamRetentionPolicy: RetentionPolicyInterest,
 		JSStreamDiscardPolicy:   DiscardPolicyNew,
 	}
