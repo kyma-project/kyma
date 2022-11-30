ab03a749b74c535195d22a7930da0ffd3d82b159
ralikio <74771103+ralikio@users.noreply.github.com>
2022-11-30 13:41:55 +0100

    Own Cluster Test

---
+++ tests/fast-integration/skr-test/test-own-cluster.js
@@ -0,0 +1,69 @@
+const {gatherOptions} = require('./index');
+const {getOrProvisionSKR} = require('./provision/provision-skr');
+const {GardenerClient, GardenerConfig} = require('../gardener');
+
+const {getSecret} = require('../utils');
+
+const { expect } = require('chai');
+
+const { toBase64 } = require('../utils');
+
+const {genRandom} = require('../utils');
+
+const skipProvisioning = process.env.SKIP_PROVISIONING === 'true';
+const provisioningTimeout = 1000 * 60 * 30; // 30m
+const deprovisioningTimeout = 1000 * 60 * 95; // 95m
+let globalTimeout = 1000 * 60 * 70; // 70m
+const slowTime = 5000;
+
+const gardener = new GardenerClient(GardenerConfig.fromEnv()); 
+
+describe('SKR own_cluster test', function() {
+  if (!skipProvisioning) {
+    globalTimeout += provisioningTimeout + deprovisioningTimeout;
+  }
+  this.timeout(globalTimeout);
+  this.slow(slowTime);
+
+  let options = gatherOptions(); // with default values
+  let skr;
+
+  let shootName = genRandom(13)
+
+  before('Shoot should be ready', async function() {
+    this.timeout(provisioningTimeout);
+    console.log("Creating a shoot")
+    await gardener.createShoot(shootName)
+  });
+
+  before('Ensure SKR is provisioned', async function() {
+    const shoot = await gardener.getShoot(shootName)
+
+    this.timeout(provisioningTimeout);
+    options.customParams.kubeconfig = toBase64(shoot.kubeconfig)
+    options.customParams.shootDomain = shoot.shootDomain
+    options.customParams.shootName = shootName
+
+    skr = await getOrProvisionSKR(options,
+      skipProvisioning, provisioningTimeout);
+    options = skr.options;
+  });
+
+  // Check if the secret with BTP credentials is created properly
+  it('should check if secret exists and contains the expected data', async function() {
+      console.log(`Checking the contents of the "sap-btp-manager" secret`);
+      const secretToCheck = await getSecret('sap-btp-manager', 'kyma-system');
+      expect(secretToCheck).to.not.be.empty;
+      expect(secretToCheck.metadata.labels['app.kubernetes.io/managed-by']).to.equal('kcp-kyma-environment-broker');
+      expect(secretToCheck.data).to.have.property('clientid');
+      expect(secretToCheck.data).to.have.property('clientsecret');
+      expect(secretToCheck.data).to.have.property('sm_url');
+      expect(secretToCheck.data).to.have.property('tokenurl');
+      expect(secretToCheck.data).to.have.property('cluster_id');
+    });
+
+  after('Cleanup the resources', async function() {
+    this.timeout(deprovisioningTimeout);
+    await gardener.deleteShoot(shootName)
+  });
+});
