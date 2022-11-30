946f5a8d7929af4512bdaf57684650feed6c2f1f
Hisar Balik <hisar.balik@sap.com>
2022-11-30 19:46:50 +0100

    Tracepipeline status unit tests

---
+++ components/telemetry-operator/controller/tracepipeline/status_test.go
@@ -0,0 +1,126 @@
+package tracepipeline
+
+import (
+	"context"
+	telemetryv1alpha1 "github.com/kyma-project/kyma/components/telemetry-operator/apis/telemetry/v1alpha1"
+	"github.com/kyma-project/kyma/components/telemetry-operator/controller/tracepipeline/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
+)
+
+func TestUpdateStatus(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = clientgoscheme.AddToScheme(scheme)
+	_ = telemetryv1alpha1.AddToScheme(scheme)
+
+	t.Run("should add pending condition if OpenTelemetry Deployment is not ready", func(t *testing.T) {
+		pipelineName := "pipeline"
+		pipeline := &telemetryv1alpha1.TracePipeline{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: pipelineName,
+			},
+			Spec: telemetryv1alpha1.TracePipelineSpec{
+				Output: telemetryv1alpha1.TracePipelineOutput{
+					Otlp: &telemetryv1alpha1.OtlpOutput{
+						Endpoint: telemetryv1alpha1.ValueType{Value: "localhost"},
+					},
+				}},
+		}
+		fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(pipeline).Build()
+
+		proberStub := &mocks.DeploymentProber{}
+		proberStub.On("IsReady", mock.Anything, mock.Anything).Return(false, nil)
+
+		sut := Reconciler{
+			Client: fakeClient,
+			config: Config{Deployment: types.NamespacedName{Name: "trace-collector"}},
+			prober: proberStub,
+		}
+		err := sut.updateStatus(context.Background(), pipeline.Name)
+		require.NoError(t, err)
+
+		var updatedPipeline telemetryv1alpha1.TracePipeline
+		_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipelineName}, &updatedPipeline)
+		require.Len(t, updatedPipeline.Status.Conditions, 1)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.TracePipelinePending)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, telemetryv1alpha1.OpenTelemetryDNotReadyReason)
+	})
+
+	t.Run("should add running condition if OpenTelemetry Deployment is ready", func(t *testing.T) {
+		pipelineName := "pipeline"
+		pipeline := &telemetryv1alpha1.TracePipeline{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: pipelineName,
+			},
+			Spec: telemetryv1alpha1.TracePipelineSpec{
+				Output: telemetryv1alpha1.TracePipelineOutput{
+					Otlp: &telemetryv1alpha1.OtlpOutput{
+						Endpoint: telemetryv1alpha1.ValueType{Value: "localhost"},
+					},
+				}},
+		}
+		fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(pipeline).Build()
+
+		proberStub := &mocks.DeploymentProber{}
+		proberStub.On("IsReady", mock.Anything, mock.Anything).Return(true, nil)
+
+		sut := Reconciler{
+			Client: fakeClient,
+			config: Config{Deployment: types.NamespacedName{Name: "trace-collector"}},
+			prober: proberStub,
+		}
+		err := sut.updateStatus(context.Background(), pipeline.Name)
+		require.NoError(t, err)
+
+		var updatedPipeline telemetryv1alpha1.TracePipeline
+		_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipelineName}, &updatedPipeline)
+		require.Len(t, updatedPipeline.Status.Conditions, 1)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.TracePipelineRunning)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, telemetryv1alpha1.OpenTelemetryDReadyReason)
+	})
+
+	t.Run("should reset conditions and add pending if OpenTelemetry Deployment becomes not ready again", func(t *testing.T) {
+		pipelineName := "pipeline"
+		pipeline := &telemetryv1alpha1.TracePipeline{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: pipelineName,
+			},
+			Spec: telemetryv1alpha1.TracePipelineSpec{
+				Output: telemetryv1alpha1.TracePipelineOutput{
+					Otlp: &telemetryv1alpha1.OtlpOutput{
+						Endpoint: telemetryv1alpha1.ValueType{Value: "localhost"},
+					},
+				}},
+			Status: telemetryv1alpha1.TracePipelineStatus{
+				Conditions: []telemetryv1alpha1.TracePipelineCondition{
+					{Reason: telemetryv1alpha1.OpenTelemetryDNotReadyReason, Type: telemetryv1alpha1.TracePipelinePending},
+					{Reason: telemetryv1alpha1.OpenTelemetryDReadyReason, Type: telemetryv1alpha1.TracePipelineRunning},
+				},
+			},
+		}
+		fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(pipeline).Build()
+
+		proberStub := &mocks.DeploymentProber{}
+		proberStub.On("IsReady", mock.Anything, mock.Anything).Return(false, nil)
+
+		sut := Reconciler{
+			Client: fakeClient,
+			config: Config{Deployment: types.NamespacedName{Name: "trace-collector"}},
+			prober: proberStub,
+		}
+		err := sut.updateStatus(context.Background(), pipeline.Name)
+		require.NoError(t, err)
+
+		var updatedPipeline telemetryv1alpha1.TracePipeline
+		_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipelineName}, &updatedPipeline)
+		require.Len(t, updatedPipeline.Status.Conditions, 1)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.TracePipelinePending)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, telemetryv1alpha1.OpenTelemetryDNotReadyReason)
+	})
+}
