de2af1912adf45fcf11169aa42fea54c30e6ab07
Hisar Balik <hisar.balik@sap.com>
2022-12-02 08:14:10 +0100

    unit test for secret checks added

--- components/telemetry-operator/controller/tracepipeline/status_test.go
+++ components/telemetry-operator/controller/tracepipeline/status_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kyma-project/kyma/components/telemetry-operator/controller/tracepipeline/mocks"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -122,5 +123,103 @@ func TestUpdateStatus(t *testing.T) {
 		require.Len(t, updatedPipeline.Status.Conditions, 1)
 		require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.TracePipelinePending)
 		require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, telemetryv1alpha1.OpenTelemetryDNotReadyReason)
+	})
+
+	t.Run("should reset conditions and add pending if some referenced secret does not exist anymore", func(t *testing.T) {
+		pipelineName := "pipeline"
+		pipeline := &telemetryv1alpha1.TracePipeline{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: pipelineName,
+			},
+			Status: telemetryv1alpha1.TracePipelineStatus{
+				Conditions: []telemetryv1alpha1.TracePipelineCondition{
+					{Reason: telemetryv1alpha1.OpenTelemetryDNotReadyReason, Type: telemetryv1alpha1.TracePipelinePending},
+					{Reason: telemetryv1alpha1.OpenTelemetryDReadyReason, Type: telemetryv1alpha1.TracePipelineRunning},
+				},
+			},
+			Spec: telemetryv1alpha1.TracePipelineSpec{
+				Output: telemetryv1alpha1.TracePipelineOutput{
+					Otlp: &telemetryv1alpha1.OtlpOutput{
+						Endpoint: telemetryv1alpha1.ValueType{
+							ValueFrom: &telemetryv1alpha1.ValueFromSource{
+								SecretKeyRef: &telemetryv1alpha1.SecretKeyRef{
+									Name:      "some-secret",
+									Namespace: "some-namespace",
+									Key:       "host",
+								},
+							},
+						},
+					},
+				}},
+		}
+
+		proberStub := &mocks.DeploymentProber{}
+		proberStub.On("IsReady", mock.Anything, mock.Anything).Return(true, nil)
+		fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(pipeline).Build()
+
+		sut := Reconciler{
+			Client: fakeClient,
+			config: Config{Deployment: types.NamespacedName{Name: "trace-collector"}},
+			prober: proberStub,
+		}
+
+		err := sut.updateStatus(context.Background(), pipeline.Name)
+		require.NoError(t, err)
+
+		var updatedPipeline telemetryv1alpha1.TracePipeline
+		_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipelineName}, &updatedPipeline)
+		require.Len(t, updatedPipeline.Status.Conditions, 1)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.TracePipelinePending)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, telemetryv1alpha1.OTReferencedSecretMissingReason)
+	})
+
+	t.Run("should add running condition if referenced secret exists and OpenTelemetry Deployment is ready", func(t *testing.T) {
+		pipelineName := "pipeline"
+		pipeline := &telemetryv1alpha1.TracePipeline{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: pipelineName,
+			},
+			Spec: telemetryv1alpha1.TracePipelineSpec{
+				Output: telemetryv1alpha1.TracePipelineOutput{
+					Otlp: &telemetryv1alpha1.OtlpOutput{
+						Endpoint: telemetryv1alpha1.ValueType{
+							ValueFrom: &telemetryv1alpha1.ValueFromSource{
+								SecretKeyRef: &telemetryv1alpha1.SecretKeyRef{
+									Name:      "some-secret",
+									Namespace: "some-namespace",
+									Key:       "host",
+								},
+							},
+						},
+					},
+				}},
+		}
+		secret := &corev1.Secret{
+			TypeMeta: metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "some-secret",
+				Namespace: "some-namespace",
+			},
+			Data: map[string][]byte{"host": nil},
+		}
+		fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(pipeline, secret).Build()
+
+		proberStub := &mocks.DeploymentProber{}
+		proberStub.On("IsReady", mock.Anything, mock.Anything).Return(true, nil)
+
+		sut := Reconciler{
+			Client: fakeClient,
+			config: Config{Deployment: types.NamespacedName{Name: "trace-collector"}},
+			prober: proberStub,
+		}
+
+		err := sut.updateStatus(context.Background(), pipeline.Name)
+		require.NoError(t, err)
+
+		var updatedPipeline telemetryv1alpha1.TracePipeline
+		_ = fakeClient.Get(context.Background(), types.NamespacedName{Name: pipelineName}, &updatedPipeline)
+		require.Len(t, updatedPipeline.Status.Conditions, 1)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Type, telemetryv1alpha1.TracePipelineRunning)
+		require.Equal(t, updatedPipeline.Status.Conditions[0].Reason, telemetryv1alpha1.OpenTelemetryDReadyReason)
 	})
 }
