3ac8f63ea29fdcd86be5f239252b63382926a88d
Marcin Dobrochowski <anoip@o2.pl>
2022-12-04 18:49:28 +0100

    add function crd generator (under development)

--- components/function-controller/config/crd/bases/table-gen.go
+++ components/function-controller/config/crd/bases/table-gen.go
@@ -2,26 +2,150 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v3"
-	"io/ioutil"
-	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"os"
+	"regexp"
+	"sigs.k8s.io/yaml"
+	"strings"
 )
 
-type Config map[string]string
+type IndentType int
+
+const (
+	IndentSpace IndentType = iota
+	IndentNbsp
+	IndentParentPath
+)
+const indentType = IndentSpace
+
+const InputCRDFilename = `/Users/I567085/src/2022jul/kyma/components/function-controller/config/crd/bases/serverless.kyma-project.io_functions.yaml`
+const APIVersion = "v1alpha2"
+
+const OutputMDFilename = `/Users/I567085/src/2022jul/kyma/docs/05-technical-reference/00-custom-resources/svls-01-function.md`
+const REPatternToReplace = `<!--\s*FUNCTION-CRD-PARAMETERS-TABLE\(START\).*<<!--\s*FUNCTION-CRD-PARAMETERS-TABLE\(START\)[^\n]*`
 
 func main() {
-	filename := "/Users/I567085/src/2022jul/kyma/components/function-controller/config/crd/bases/serverless.kyma-project.io_functions.yaml"
-	stream, err := ioutil.ReadFile(filename)
+	newDoc := generateDocFromCRD()
+	replaceDocInMD(newDoc)
+}
+
+func replaceDocInMD(doc string) {
+	inDoc, err := os.ReadFile(OutputMDFilename)
 	if err != nil {
 		panic(err)
 	}
 
-	var obj v1.CustomResourceDefinition
-	//var obj interface{}
-	err = yaml.Unmarshal(stream, &obj)
+	re, err := regexp.Compile(REPatternToReplace)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Value: %#v\n", obj)
+	outDoc := re.ReplaceAll(inDoc, []byte(doc))
+
+	outFile, err := os.OpenFile(OutputMDFilename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Close()
+	outFile.Write(outDoc)
+}
+
+func generateDocFromCRD() string {
+	input, err := os.ReadFile(InputCRDFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	var obj interface{}
+	if err := yaml.Unmarshal(input, &obj); err != nil {
+		panic(err)
+	}
+
+	var doc []string
+
+	versions := getElement(obj, "spec", "versions")
+	for _, version := range versions.([]interface{}) {
+		name := getElement(version, "name")
+		if name.(string) != APIVersion {
+			continue
+		}
+		functionSpec := getElement(version, "schema", "openAPIV3Schema", "properties", "spec")
+		doc = append(doc, generateElementDoc(functionSpec, "spec", true, 0, "")...)
+	}
+	return strings.Join(doc, "\n")
+}
+
+func getElement(obj interface{}, path ...string) interface{} {
+	elem := obj
+	for _, p := range path {
+		elem = elem.(map[string]interface{})[p]
+	}
+	return elem
+}
+
+func generateElementDoc(obj interface{}, name string, required bool, indent int, parentPath string) []string {
+	var result []string
+	element := obj.(map[string]interface{})
+	elementType := element["type"].(string)
+	description := ""
+	if d := element["description"]; d != nil {
+		description = d.(string)
+	}
+	result = append(result,
+		fmt.Sprintf("| %s%s | %s | %s |",
+			getIndent(indent, parentPath), name,
+			yesNo(required), description))
+
+	if elementType == "object" {
+		result = append(result,
+			generateObjectDoc(element, name, indent, parentPath)...)
+	}
+	return result
+}
+
+func generateObjectDoc(element map[string]interface{}, name string, indent int, parentPath string) []string {
+	var result []string
+	properties := getElement(element, "properties")
+	if properties == nil {
+		return result
+	}
+	var requiredChildren []interface{}
+	if rc := getElement(element, "required"); rc != nil {
+		requiredChildren = rc.([]interface{})
+	}
+	for propName, propVal := range properties.(map[string]interface{}) {
+		propRequired := contains(requiredChildren, name)
+		result = append(result,
+			generateElementDoc(propVal, propName, propRequired,
+				indent+1, parentPath+name+".")...)
+	}
+	return result
+}
+
+func getIndent(indent int, path string) string {
+	switch indentType {
+	case IndentSpace:
+		return strings.Repeat(" ", indent*2)
+	case IndentNbsp:
+		return strings.Repeat("&nbsp", indent*2)
+	case IndentParentPath:
+		return path
+	default:
+		panic("Unexpected indent type!")
+	}
+}
+
+func yesNo(b bool) string {
+	if b {
+		return "Yes"
+	}
+	return "No"
+}
+
+func contains(s []interface{}, e string) bool {
+	for _, a := range s {
+		if a.(string) == e {
+			return true
+		}
+	}
+	return false
 }
--- docs/05-technical-reference/00-custom-resources/svls-01-function.md
+++ docs/05-technical-reference/00-custom-resources/svls-01-function.md
@@ -113,7 +113,7 @@ This table lists all the possible parameters of a given resource together with t
 
 <!-- The table bellow is generated automatically don't touch them. When you want to change description, do it in components/function-controller/pkg/apis/serverless/v1alpha2/function_types.go -->
 
-<!-- FUNCTION-CRD-PARAMETERS-TABLE-START -->
+<!-- FUNCTION-CRD-PARAMETERS-TABLE(START) -->
 | Parameter         |    Required    | Description                                   |
 | ---------------------------------------- | :------------: | ---------|
 | **metadata.name**              |      Yes       | Specifies the name of the CR.                 |
@@ -160,7 +160,7 @@ This table lists all the possible parameters of a given resource together with t
 | **status.conditions.reason**             | Not applicable | Provides information on the Function CR processing success or failure. See the [**Reasons**](#status-reasons) section for the full list of possible status reasons and their descriptions. All status reasons are in camelCase.   |
 | **status.conditions.status**             | Not applicable | Describes the status of processing the Function CR by the Function Controller. It can be `True` for success, `False` for failure, or `Unknown` if the CR processing is still in progress. If the status of all conditions is `True`, the overall status of the Function CR is ready.     |
 | **status.conditions.type**               | Not applicable | Describes a substage of the Function CR processing. There are three condition types that a Function has to meet to be ready: `ConfigurationReady`, `BuildReady`, and `Running`. When displaying the Function status in the terminal, these types are shown under `CONFIGURED`, `BUILT`, and `RUNNING` columns respectively. All condition types can change asynchronously depending on the type of Function modification, but all three need to be in the `True` status for the Function to be considered successfully processed. |
-<!-- FUNCTION-CRD-PARAMETERS-TABLE-END -->
+<!-- FUNCTION-CRD-PARAMETERS-TABLE(END) -->
 
 ### Status reasons
 
