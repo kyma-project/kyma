e2469560107154e3da5611def96c2f81a93d3147
Benjamin Lindner <benjamin.lindner@sap.com>
2022-12-06 09:16:28 +0100

    Wrap up daemonset config

--- components/telemetry-operator/controller/logpipeline/resources.go
+++ components/telemetry-operator/controller/logpipeline/resources.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -15,35 +16,239 @@ type resourceConfig struct {
 }
 
 func makeDaemonSet(config resourceConfig) *appsv1.DaemonSet {
+	resourcesFluentBit := corev1.ResourceRequirements{
+		Requests: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceCPU:    resource.MustParse("10m"),
+			corev1.ResourceMemory: resource.MustParse("50Mi"),
+		},
+		Limits: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceCPU:    resource.MustParse("400m"),
+			corev1.ResourceMemory: resource.MustParse("256Mi"),
+		},
+	}
+
+	resourcesExporter := corev1.ResourceRequirements{
+		Requests: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceMemory: resource.MustParse("5Mi"),
+		},
+		Limits: map[corev1.ResourceName]resource.Quantity{
+			corev1.ResourceCPU:    resource.MustParse("100m"),
+			corev1.ResourceMemory: resource.MustParse("50Mi"),
+		},
+	}
+
 	return &appsv1.DaemonSet{
 		TypeMeta: metav1.TypeMeta{},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: name(config),
+			Name:   name(config),
 			Labels: labels(config),
 		},
-		Spec:   appsv1.DaemonSetSpec{
+		Spec: appsv1.DaemonSetSpec{
 			Selector: &metav1.LabelSelector{
 				MatchLabels: labels(config),
 			},
-			Template:             corev1.PodTemplateSpec{
+			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
-					Labels:                     labels(config),
-					Annotations:                map[string]string{
-						"checksum/config" : "5c928f5bb13aabb32f086b320b200c0df16c9ca8a553a856bad3993cd3582143",
+					Labels: labels(config),
+					Annotations: map[string]string{
+						"checksum/config":     "5c928f5bb13aabb32f086b320b200c0df16c9ca8a553a856bad3993cd3582143",
 						"checksum/luascripts": "d538085e16b852526ec9e36ce06000a0bb6907347d083db72c15ce0952bda559",
 					},
 				},
-				Spec:       corev1.PodSpec{
-					ServiceAccountName:            name(config),
-					PriorityClassName:             "kyma-system-priority",
-					SecurityContext:               &corev1.PodSecurityContext{
-						RunAsNonRoot:        nil, //TODO
-						SeccompProfile:      nil, //TODO
+				Spec: corev1.PodSpec{
+					ServiceAccountName: name(config),
+					PriorityClassName:  "kyma-system-priority",
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsNonRoot:   setFalse(),
+						SeccompProfile: &corev1.SeccompProfile{Type: "RuntimeDefault"},
 					},
-					HostNetwork:                   false,
-					InitContainers:                nil, //TODO
-					Containers:                    nil, //TODO
-					Volumes:                       nil, //TODO
+					DNSPolicy:   "ClusterFirst",
+					HostNetwork: false,
+					InitContainers: []corev1.Container{
+						{
+							Name:  "prep-fluent-bit-config",
+							Image: "eu.gcr.io/kyma-project/external/busybox:1.34.1",
+							Command: []string{
+								"sh", "-c",
+								"cp /main/* /fluent-bit/etc/ && mkdir -p /fluent-bit/etc/dynamic/ && cp /dynamic/* /fluent-bit/etc/dynamic && mkdir -p /fluent-bit/etc/dynamic-parsers/ && cp /dynamic-parsers/* /fluent-bit/etc/dynamic-parsers || touch /fluent-bit/etc/dynamic/empty.conf",
+							},
+							SecurityContext: &corev1.SecurityContext{
+								AllowPrivilegeEscalation: setFalse(),
+								Privileged:               setFalse(),
+								ReadOnlyRootFilesystem:   setTrue(),
+								Capabilities: &corev1.Capabilities{
+									Drop: []corev1.Capability{"ALL"},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{Name: "shared-fluent-bit-config", MountPath: "/fluent-bit/etc/"},
+								{Name: "config", MountPath: "/main/fluent-bit.conf", SubPath: "fluent-bit.conf"},
+								{Name: "dynamic-config", MountPath: "/dynamic"},
+								{Name: "dynamic-parsers-config", MountPath: "/dynamic-parsers"},
+							},
+						},
+					},
+					Containers: []corev1.Container{
+						{
+							Name: "fluent-bit",
+							SecurityContext: &corev1.SecurityContext{
+								AllowPrivilegeEscalation: setFalse(),
+								Capabilities: &corev1.Capabilities{
+									Add:  []corev1.Capability{"FOWNER"},
+									Drop: []corev1.Capability{"ALL"},
+								},
+								Privileged:             setFalse(),
+								ReadOnlyRootFilesystem: setTrue(),
+							},
+							Image:           "eu.gcr.io/kyma-project/tpi/fluent-bit:1.9.9-cf0a130c",
+							ImagePullPolicy: "IfNotPresent",
+							EnvFrom: []corev1.EnvFromSource{
+								{
+									SecretRef: &corev1.SecretEnvSource{
+										LocalObjectReference: corev1.LocalObjectReference{Name: "telemetry-fluent-bit-env"},
+										Optional:             setTrue(),
+									},
+								},
+							},
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									ContainerPort: 2020,
+									Protocol:      "TCP",
+								},
+								{
+									Name:          "http-metrics",
+									ContainerPort: 2021,
+									Protocol:      "TCP",
+								},
+							},
+							LivenessProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/",
+										Port: intstr.IntOrString{StrVal: "http"},
+									},
+								},
+							},
+							ReadinessProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/api/v1/health",
+										Port: intstr.IntOrString{StrVal: "http"},
+									},
+								},
+							},
+							Resources: resourcesFluentBit,
+							VolumeMounts: []corev1.VolumeMount{
+								{MountPath: "/fluent-bit/etc", Name: "shared-fluent-bit-config"},
+								{MountPath: "/fluent-bit/etc/fluent-bit.conf", Name: "config", SubPath: "fluent-bit.conf"},
+								{MountPath: "/fluent-bit/etc/custom_parsers.conf", Name: "config", SubPath: "custom_parsers.conf"},
+								{MountPath: "/fluent-bit/etc/loki-labelmap.json", Name: "config", SubPath: "loki-labelmap.json"},
+								{MountPath: "/fluent-bit/scripts/filter-script.lua", Name: "luascripts", SubPath: "filter-script.lua"},
+								{MountPath: "/var/log", Name: "varlog"},
+								{MountPath: "/var/lib/docker/containers", Name: "varlibdockercontainers", ReadOnly: true},
+								{MountPath: "/data", Name: "varfluentbit"},
+								{MountPath: "/files", Name: "dynamic-files"},
+							},
+						},
+						{
+							Name:      "exporter",
+							Image:     "eu.gcr.io/kyma-project/directory-size-exporter:v20221020-e314a071",
+							Resources: resourcesExporter,
+							Args: []string{
+								"--storage-path=/data/flb-storage/",
+								"--metric-name=telemetry_fsbuffer_usage_bytes",
+							},
+							WorkingDir: "",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http-metrics",
+									ContainerPort: 2021,
+									Protocol:      "TCP",
+								},
+							},
+							SecurityContext: &corev1.SecurityContext{
+								AllowPrivilegeEscalation: setFalse(),
+								Privileged:               setFalse(),
+								ReadOnlyRootFilesystem:   setTrue(),
+								Capabilities: &corev1.Capabilities{
+									Drop: []corev1.Capability{"ALL"},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{Name: "varfluentbit", MountPath: "/data"},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{Name: "telemetry-fluent-bit"},
+								},
+							},
+						},
+						{
+							Name: "luascripts",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{Name: "telemetry-fluent-bit-luascripts"},
+								},
+							},
+						},
+						{
+							Name: "varlog",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{Path: "/var/log"},
+							},
+						},
+						{
+							Name: "varlibdockercontainers",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{Path: "/var/lib/docker/containers"},
+							},
+						},
+						{
+							Name: "shared-fluent-bit-config",
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+						{
+							Name: "dynamic-config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{Name: "telemetry-fluent-bit-sections"},
+									Optional:             setTrue(),
+								},
+							},
+						},
+						{
+							Name: "dynamic-parsers-config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{Name: "telemetry-fluent-bit-parsers"},
+									Optional:             setTrue(),
+								},
+							},
+						},
+						{
+							Name: "dynamic-files",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{Name: "telemetry-fluent-bit-files"},
+									Optional:             setTrue(),
+								},
+							},
+						},
+						{
+							Name: "varfluentbit",
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{Path: "/var/telemetry-fluent-bit"},
+							},
+						},
+					},
 				},
 			},
 		},
@@ -55,17 +260,19 @@ func makeService(config resourceConfig) *corev1.Service {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name(config),
 			Namespace: config.Name.Namespace,
-			Labels: labels(config),
+			Labels:    labels(config),
 		},
 		Spec: corev1.ServiceSpec{
 			Ports: []corev1.ServicePort{
 				{
 					Name:       "http",
+					Protocol:   "TCP",
 					Port:       2020,
 					TargetPort: intstr.IntOrString{StrVal: "http"},
 				},
 				{
 					Name:       "http-metrics",
+					Protocol:   "TCP",
 					Port:       2021,
 					TargetPort: intstr.IntOrString{StrVal: "http-metrics"},
 				},
@@ -154,7 +361,7 @@ func makeConfigMap(config resourceConfig) *corev1.ConfigMap {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name(config),
 			Namespace: config.Name.Namespace,
-			Labels: labels(config),
+			Labels:    labels(config),
 		},
 		Data: map[string]string{
 			"custom_parsers.conf": parserConfig,
@@ -200,7 +407,7 @@ end
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      fmt.Sprintf("%s-%s-luascripts", config.Component, config.Name.Name),
 			Namespace: config.Name.Namespace,
-			Labels: labels(config),
+			Labels:    labels(config),
 		},
 		Data: map[string]string{"filter-script.lua": luaFilter},
 	}
@@ -215,4 +422,14 @@ func labels(config resourceConfig) map[string]string {
 
 func name(config resourceConfig) string {
 	return fmt.Sprintf("%s-%s", config.Component, config.Name.Name)
+}
+
+func setTrue() *bool {
+	b := true
+	return &b
+}
+
+func setFalse() *bool {
+	b := false
+	return &b
 }
