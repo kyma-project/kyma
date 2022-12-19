57c31cecce15ddb8d523223f66ee772418fd59b1
Benjamin Lindner <benjamin.lindner@sap.com>
2022-12-19 14:01:37 +0100

    Add tests for annontation merging

--- components/telemetry-operator/internal/kubernetes/utils_test.go
+++ components/telemetry-operator/internal/kubernetes/utils_test.go
@@ -2,6 +2,8 @@ package kubernetes
 
 import (
 	"context"
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 
 	"github.com/stretchr/testify/mock"
@@ -75,4 +77,84 @@ func TestGetOrCreateSecretSuccess(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "some-secret", secret.Name)
 	require.Equal(t, "secret-ns", secret.Namespace)
+}
+
+func TestMergeKubectlAnnotations(t *testing.T) {
+	existing := v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "some-deployment",
+			Annotations: map[string]string{
+				"kubectl.kubernetes.io/1": "1",
+				"kubectl.kubernetes.io/2": "2",
+				"kubectl.kubernetes.io/3": "3",
+			},
+		},
+		Spec:   v1.DeploymentSpec{},
+		Status: v1.DeploymentStatus{},
+	}
+
+	desired := v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "some-deployment",
+			Annotations: map[string]string{
+				"kubectl.kubernetes.io/2": "b",
+				"kubectl.kubernetes.io/3": "3",
+				"kubectl.kubernetes.io/4": "4",
+			},
+		},
+		Spec:   v1.DeploymentSpec{},
+		Status: v1.DeploymentStatus{},
+	}
+
+	mergeKubectlAnnotations(&desired.ObjectMeta, existing.ObjectMeta)
+
+	require.Equal(t, len(desired.Annotations), 4)
+	require.Contains(t, desired.Annotations, "kubectl.kubernetes.io/1")
+	require.Contains(t, desired.Annotations, "kubectl.kubernetes.io/2")
+	require.Contains(t, desired.Annotations, "kubectl.kubernetes.io/3")
+	require.Contains(t, desired.Annotations, "kubectl.kubernetes.io/4")
+	require.Equal(t, desired.Annotations["kubectl.kubernetes.io/1"],  "1")
+	require.Equal(t, desired.Annotations["kubectl.kubernetes.io/2"],  "b")
+	require.Equal(t, desired.Annotations["kubectl.kubernetes.io/3"],  "3")
+	require.Equal(t, desired.Annotations["kubectl.kubernetes.io/4"],  "4")
+}
+
+func TestMergeChecksumAnnotations(t *testing.T) {
+	existing := v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "some-deployment",
+			Annotations: map[string]string{
+				"checksum/1": "1",
+				"checksum/2": "2",
+				"checksum/3": "3",
+			},
+		},
+		Spec:   v1.DeploymentSpec{},
+		Status: v1.DeploymentStatus{},
+	}
+
+	desired := v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "some-deployment",
+			Annotations: map[string]string{
+				"checksum/2": "b",
+				"checksum/3": "3",
+				"checksum/4": "4",
+			},
+		},
+		Spec:   v1.DeploymentSpec{},
+		Status: v1.DeploymentStatus{},
+	}
+
+	mergeChecksumAnnotations(&desired.ObjectMeta, existing.ObjectMeta)
+
+	require.Equal(t, len(desired.Annotations), 4)
+	require.Contains(t, desired.Annotations, "checksum/1")
+	require.Contains(t, desired.Annotations, "checksum/2")
+	require.Contains(t, desired.Annotations, "checksum/3")
+	require.Contains(t, desired.Annotations, "checksum/4")
+	require.Equal(t, desired.Annotations["checksum/1"],  "1")
+	require.Equal(t, desired.Annotations["checksum/2"],  "b")
+	require.Equal(t, desired.Annotations["checksum/3"],  "3")
+	require.Equal(t, desired.Annotations["checksum/4"],  "4")
 }
