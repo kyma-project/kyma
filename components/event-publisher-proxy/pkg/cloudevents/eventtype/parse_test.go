cf5b58c48142c9397d8e470261032e4c2895c34b
Muhammad Faizan <mfaizanse@users.noreply.github.com>
2022-12-23 10:17:05 +0100

    Support publishing cloud-events for Subscritpion v1alpha2 CRD in EPP (#16401)
    * Support publihsing events for Subscritpion v1alpha2 CRD in EPP
    
    * updated tests
    
    * updated tests
    
    * addressed review comments

--- components/event-publisher-proxy/pkg/cloudevents/eventtype/parse_test.go
+++ components/event-publisher-proxy/pkg/cloudevents/eventtype/parse_test.go
@@ -77,30 +77,3 @@ func TestParser(t *testing.T) {
 		})
 	}
 }
-
-func Test_checkForEmptySegments(t *testing.T) {
-	t.Parallel()
-	testCases := []struct {
-		name          string
-		givenSegments []string
-		wantResult    bool
-	}{
-		{
-			name:          "should pass if all segments are non-empty",
-			givenSegments: []string{"one", "two", "three"},
-			wantResult:    false,
-		},
-		{
-			name:          "should fail if any segment is empty",
-			givenSegments: []string{"one", "", "three"},
-			wantResult:    true,
-		},
-	}
-	for _, tc := range testCases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			require.Equal(t, tc.wantResult, checkForEmptySegments(tc.givenSegments))
-		})
-	}
-}
