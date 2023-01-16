bff0f4d013d07458bdf3b25eade5f5a497534d19
Christoph Kleineweber <c.kleineweber@sap.com>
2023-01-16 17:28:34 +0100

    Remove Fluent Bit dummy pipeline creation (#16573)
    * Remove Fluent Bit dummy pipeline creation
    
    * Bump telemetry images

--- components/telemetry-operator/internal/resources/logpipeline/resources.go
+++ components/telemetry-operator/internal/resources/logpipeline/resources.go
@@ -2,6 +2,7 @@ package logpipeline
 
 import (
 	"fmt"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -309,12 +310,6 @@ func MakeConfigMap(name types.NamespacedName) *corev1.ConfigMap {
     DB /data/flb_kube.db
     storage.type  filesystem
 
-[INPUT]
-    Name tail
-    Path /null.log
-    Tag null.*
-    Alias null-tail
-
 [FILTER]
     Name kubernetes
     Match tele.*
@@ -322,11 +317,6 @@ func MakeConfigMap(name types.NamespacedName) *corev1.ConfigMap {
     K8S-Logging.Parser On
     K8S-Logging.Exclude On
     Buffer_Size 1MB
-
-[OUTPUT]
-    Name null
-    Match null.*
-    Alias null-null
 
 @INCLUDE dynamic/*.conf
 `
