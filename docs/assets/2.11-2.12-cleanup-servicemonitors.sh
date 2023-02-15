177eee7cb9869b41890176b5380a48d4889b2656
Stanislav Khalash <stanislav.khalash@sap.com>
2023-02-15 15:18:30 +0100

    Convert loki

--- docs/assets/2.11-2.12-cleanup-servicemonitors.sh
+++ docs/assets/2.11-2.12-cleanup-servicemonitors.sh
@@ -4,6 +4,7 @@ kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com eventing-controller
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com eventing-nats
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com eventing-publisher-proxy
+kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com logging-loki
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com ory-hydra-maester
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com ory-oathkeeper-maester
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com serverless
