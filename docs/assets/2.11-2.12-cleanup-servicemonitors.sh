87e6a4bb22d1838fd05e81bc3a331917d51da38e
Stanislav Khalash <stanislav.khalash@sap.com>
2023-02-15 14:10:57 +0100

    Convert serverless to annotation-based Prometheus scraping (#16810)
    * Convert service monitor to annotations
    
    * Fix chart
    
    * Update migration script

--- docs/assets/2.11-2.12-cleanup-servicemonitors.sh
+++ docs/assets/2.11-2.12-cleanup-servicemonitors.sh
@@ -6,5 +6,6 @@ kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com eventing-publisher-proxy
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com ory-hydra-maester
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com ory-oathkeeper-maester
+kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com serverless
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com telemetry-fluent-bit
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com telemetry-operator-metrics
