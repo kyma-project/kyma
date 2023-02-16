86632f75e3557d3c11764736ea10f43d4b1355f5
Stanislav Khalash <stanislav.khalash@sap.com>
2023-02-16 11:14:59 +0100

    Convert logging, tracing to annotation-based Prometheus scraping (#16813)
    * Convert loki
    
    * Convert jaeger
    
    * Fix port
    
    * Rename script and add missing resources to delete
    
    * Rename script back

--- docs/assets/2.11-2.12-cleanup-servicemonitors.sh
+++ docs/assets/2.11-2.12-cleanup-servicemonitors.sh
@@ -4,8 +4,15 @@ kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com eventing-controller
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com eventing-nats
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com eventing-publisher-proxy
+kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com logging-loki
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com ory-hydra-maester
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com ory-oathkeeper-maester
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com serverless
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com telemetry-fluent-bit
 kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com telemetry-operator-metrics
+kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com telemetry-operator-metrics
+kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com tracing-jaeger
+kubectl delete -n kyma-system --ignore-not-found=true servicemonitors.monitoring.coreos.com tracing-jaeger-operator
+kubectl delete -n istio-system --ignore-not-found=true servicemonitors.monitoring.coreos.com istio-component-monitor
+
+kubectl delete -n kyma-system --ignore-not-found=true prometheusrules.monitoring.coreos.com logging-loki-kyma-loki.rules
