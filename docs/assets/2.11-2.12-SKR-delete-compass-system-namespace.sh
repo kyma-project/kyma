1a35b3a7e0f55be80e414e85fe3dfc4cc974a4d6
Rafał Foks <r.fok74@gmail.com>
2023-02-22 12:10:54 +0100

    Refreshing credentials in CR

--- docs/assets/2.11-2.12-SKR-delete-compass-system-namespace.sh
+++ docs/assets/2.11-2.12-SKR-delete-compass-system-namespace.sh
@@ -2,3 +2,5 @@
 
 echo "Deleting the compass-system namespace"
 kubectl delete namespace compass-system
+echo "Refreshing credential for Compass Connection"
+kubectl patch compassconnection compass-connection --type=merge -p '{"spec":{"refreshCredentialsNow":true}}'
