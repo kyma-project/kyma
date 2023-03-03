a28ab6bdf10987bfdf462c4945b95d4c08086255
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2023-03-03 15:52:07 +0100

    Upgrade OTEL SDK for Python runtime
    Co-authored-by: Filip Strózik <filip.strozik@outlook.com>

--- components/function-runtimes/python39/kubeless/tracing.py
+++ components/function-runtimes/python39/kubeless/tracing.py
@@ -1,57 +1,45 @@
-import logging
 from contextlib import contextmanager
 from typing import Iterator
 
-import requests
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.propagate import extract
-from opentelemetry.propagate import set_global_textmap
-from opentelemetry.propagators.b3 import B3MultiFormat
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider, _Span
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.propagate import extract
+from opentelemetry.sdk.resources import (
+    SERVICE_NAME,
+    Resource
+)
+from opentelemetry.sdk.trace.export import (
+    SimpleSpanProcessor,
+)
+from opentelemetry.sdk.trace.sampling import ( 
+    DEFAULT_ON,
+)
+
 from opentelemetry.trace import context_api
 from opentelemetry.trace.propagation import _SPAN_KEY
 
-_TRACING_SAMPLE_HEADER = "x-b3-sampled"
+import os
 
+# Tracing propagators are configured based on OTEL_PROPAGATORS env variable set in deockerfile
+# https://opentelemetry.io/docs/instrumentation/python/manual/#using-environment-variables
+def _setup_tracer(service_name: str) -> trace.Tracer:
 
-class ServerlessTracerProvider:
-    def __init__(self, tracecollector_endpoint: str, service_name: str):
-        self.noop_tracer = trace.NoOpTracer()
-        if not tracecollector_endpoint:
-            self.tracer = trace.NoOpTracer()
-        else:
-            self.tracer = _get_tracer(tracecollector_endpoint, service_name)
-
-    def get_tracer(self, req):
-        val = req.get_header(_TRACING_SAMPLE_HEADER)
-        if val is not None and val == "1":
-            return self.tracer
-
-        return self.noop_tracer
-
-
-def _get_tracer(tracecollector_endpoint: str, service_name: str) -> trace.Tracer:
-    set_global_textmap(B3MultiFormat())
-    RequestsInstrumentor().instrument()
-
-    trace.set_tracer_provider(
-        TracerProvider(
-            resource=Resource.create({SERVICE_NAME: service_name})
-        )
+    provider = TracerProvider(
+        resource=Resource.create({SERVICE_NAME: service_name}),
+        sampler=DEFAULT_ON,
     )
+   
+    tracecollector_endpoint = os.getenv('TRACE_COLLECTOR_ENDPOINT')
 
-    otlp_exporter = OTLPSpanExporter(
-        endpoint=tracecollector_endpoint,
-    )
+    if tracecollector_endpoint:
+        span_processor = SimpleSpanProcessor(OTLPSpanExporter(endpoint=tracecollector_endpoint))
+        provider.add_span_processor(span_processor)
+ 
+    # Sets the global default tracer provider
+    trace.set_tracer_provider(provider)
 
-    span_processor = BatchSpanProcessor(otlp_exporter)
-
-    trace.get_tracer_provider().add_span_processor(span_processor)
-
+    # Creates a tracer from the global tracer provider
     return trace.get_tracer(__name__)
 
 
@@ -75,3 +63,4 @@ def set_req_context(req) -> Iterator[trace.Span]:
         yield span
     finally:
         context_api.detach(token)
+        
