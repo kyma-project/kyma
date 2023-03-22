aa59e07d50ee3039834593cc90571d6c4adff0e4
void404 <wojciech.nawa@gmail.com>
2023-03-22 12:30:17 +0100

    Add tests script

---
+++ tests/components/application-connector/scripts/run-tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+service docker start
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+curl -Lo kyma.tar.gz "https://github.com/kyma-project/cli/releases/download/$(curl -s https://api.github.com/repos/kyma-project/cli/releases/latest | grep tag_name | cut -d '"' -f 4)/kyma_Linux_x86_64.tar.gz" && mkdir kyma-release && tar -C kyma-release -zxvf kyma.tar.gz && chmod +x kyma-release/kyma && rm -rf kyma.tar.gz
+kyma-release/kyma provision k3d
+kubectl cluster-info
+kyma-release/kyma deploy --ci --components-file tests/components/application-connector/resources/installation-config/mini-kyma-skr.yaml --source local --workspace $PWD
+cd tests/components/application-connector
+make -f Makefile.test-compass-runtime-agent test-compass-runtime-agent
+failed=$?
+k3d cluster delete kyma
+exit $failed
