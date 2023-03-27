b500c79d88b122085b462c22b23db48866e1f73a
Damian Badura <45110612+dbadura@users.noreply.github.com>
2023-03-27 14:12:28 +0200

    Change v1alpha1 to v1alpha2 serverless fast integration (#17180)
    * fix fast integration
    
    * change function version

--- tests/fast-integration/utils/index.js
+++ tests/fast-integration/utils/index.js
@@ -245,7 +245,7 @@ async function getSecret(name, namespace) {
 }
 
 async function getFunction(name, namespace) {
-  const path = `/apis/serverless.kyma-project.io/v1alpha1/namespaces/${namespace}/functions/${name}`;
+  const path = `/apis/serverless.kyma-project.io/v1alpha2/namespaces/${namespace}/functions/${name}`;
   const response = await k8sDynamicApi.requestPromise({
     url: k8sDynamicApi.basePath + path,
   });
@@ -396,7 +396,7 @@ function waitForEndpoint(name, namespace = 'default', timeout = 300_000) {
 
 function waitForFunction(name, namespace = 'default', timeout = 90_000) {
   return waitForK8sObject(
-      `/apis/serverless.kyma-project.io/v1alpha1/namespaces/${namespace}/functions`,
+      `/apis/serverless.kyma-project.io/v1alpha2/namespaces/${namespace}/functions`,
       {},
       (_type, _apiObj, watchObj) => {
         return (
