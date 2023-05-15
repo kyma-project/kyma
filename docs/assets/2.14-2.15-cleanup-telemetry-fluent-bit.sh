f06fb0ed74b74c44502d76fe02bd58e5bf4f2f39
Hisar Balik <hisar.balik@sap.com>
2023-05-15 07:47:13 +0200

    updated

--- docs/assets/2.14-2.15-cleanup-telemetry-fluent-bit.sh
+++ docs/assets/2.14-2.15-cleanup-telemetry-fluent-bit.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 kubectl delete -n kyma-system peerauthentications.security.istio.io telemetry-fluent-bit-metrics --ignore-not-found
-kubectl delete -n kyma-system configmaps telemetry-otel-collector-grafana-dashboard --ignore-not-found
-kubectl delete -n kyma-system networkpolicies.networking.k8s.io telemetry-operator-pprof-deny-ingress --ignore-not-found
+kubectl delete -n kube-public configmaps logparsers.telemetry.kyma-project.io --ignore-not-found
+kubectl delete -n kube-public configmaps logpipelines.telemetry.kyma-project.io --ignore-not-found
+kubectl delete -n kube-public configmaps tracepipelines.telemetry.kyma-project.io --ignore-not-found
