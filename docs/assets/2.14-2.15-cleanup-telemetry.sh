315e8738659a23545d1065a08b2eaca6d31c1fd3
nataliasitko <natalia.sitko@sap.com>
2023-05-19 10:10:04 +0200

    Revert "Merge branch 'crd-auto-generation' of https://github.com/nataliasitko/kyma into crd-auto-generation"
    This reverts commit 53096e119e02b904812941eeb6714c7154c9458a, reversing
    changes made to a1f44421dd8827215fe25bf1f3d537794503c1f9.

--- docs/assets/2.14-2.15-cleanup-telemetry.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl delete -n kyma-system peerauthentications.security.istio.io telemetry-fluent-bit-metrics --ignore-not-found
-kubectl delete -n kube-public configmaps logparsers.telemetry.kyma-project.io --ignore-not-found
-kubectl delete -n kube-public configmaps logpipelines.telemetry.kyma-project.io --ignore-not-found
-kubectl delete -n kube-public configmaps tracepipelines.telemetry.kyma-project.io --ignore-not-found
