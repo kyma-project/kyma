22abd40c0463482ffae8fed3b62634eb65c10565
Hisar Balik <hisar.balik@sap.com>
2023-05-25 12:37:22 +0200

    retry added

--- tests/fast-integration/upgrade-test/upgrade-test-prep.js
+++ tests/fast-integration/upgrade-test/upgrade-test-prep.js
@@ -1,15 +1,27 @@
 const {ensureCommerceMockLocalTestFixture} = require('../test/fixtures/commerce-mock');
 const {error} = require('../utils');
-
+const defaultRetryDelayMs = 1000;
+const defaultRetries = 5;
+const retryWithDelay = (operation, delay, retries) => new Promise((resolve, reject) => {
+  return operation()
+      .then(resolve)
+      .catch((reason) => {
+        if (retries > 0) {
+          return wait(delay)
+              .then(retryWithDelay.bind(null, operation, delay, retries - 1))
+              .then(resolve)
+              .catch(reject);
+        }
+        return reject(reason);
+      });
+});
 describe('Upgrade test preparation', function() {
   this.timeout(10 * 60 * 1000);
   this.slow(5000);
   const testNamespace = 'test';
 
   it('CommerceMock test fixture should be ready', async function() {
-    await ensureCommerceMockLocalTestFixture('mocks', testNamespace).catch((err) => {
-      error(err);
-      return ensureCommerceMockLocalTestFixture('mocks', testNamespace);
-    });
+    await retryWithDelay( (r) => ensureCommerceMockLocalTestFixture('mocks', testNamespace),
+        defaultRetryDelayMs, defaultRetries);
   });
 });
