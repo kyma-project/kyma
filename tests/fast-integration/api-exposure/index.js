0a30f491ccc2de075fadeeada89eb1e9d914fe10
Hisar Balik <hisar.balik@sap.com>
2023-05-25 12:51:38 +0200

    add retry

--- tests/fast-integration/api-exposure/index.js
+++ tests/fast-integration/api-exposure/index.js
@@ -6,16 +6,29 @@ const {
   cleanApiExposureFixture,
 } = require('./fixtures');
 
+const defaultRetryDelayMs = 1000;
+const defaultRetries = 5;
+const retryWithDelay = (operation, delay, retries) => new Promise((resolve, reject) => {
+  return operation()
+      .then(resolve)
+      .catch((reason) => {
+        if (retries > 0) {
+          return wait(delay)
+              .then(retryWithDelay.bind(null, operation, delay, retries - 1))
+              .then(resolve)
+              .catch(reject);
+        }
+        return reject(reason);
+      });
+});
+
 function apiExposureTests() {
   describe('API Exposure Tests:', function() {
     this.timeout(10 * 60 * 1000);
     this.slow(5000);
 
     it('Httpbin deployment should be ready', async function() {
-      await ensureApiExposureFixture().catch((err) => {
-        console.dir(err); // first error is logged
-        return ensureApiExposureFixture();
-      });
+      await retryWithDelay((r)=> ensureApiExposureFixture(), defaultRetryDelayMs, defaultRetries);
     });
 
     it('Testing unsecured httpbin API Rule', async function() {
