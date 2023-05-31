4d2c7e67a6d67be44e5fddaf83b6ddc6f9ffed99
Marcin Szwed <marcin.szwed@sap.com>
2023-05-31 13:46:59 +0200

    Define new BTP Manager Secret test file

--- tests/fast-integration/skr-test/btp-manager-secret/index.js
+++ tests/fast-integration/skr-test/btp-manager-secret/index.js
@@ -1,15 +1,7 @@
-const os = require('os');
 const {expect} = require('chai');
-
-const {
-    initializeK8sClient,
-} = require('../../utils');
 
 function btpManagerSecretTest() {
     describe('BTP Manager Secret Test', function () {
-        before('REMOVE LATER: Initialize kubeconfig', async function(){
-            await initializeK8sClient({kubeconfigPath: `${os.homedir()}/k3d_kubeconfig`});
-        });
     });
 }
 
