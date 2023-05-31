d7a5b4a3ac6188966db671a72064bb100a9305c9
Marcin Szwed <marcin.szwed@sap.com>
2023-05-31 13:47:43 +0200

    init k8s client based on local kubeconfig

--- tests/fast-integration/skr-test/btp-manager-secret/index.js
+++ tests/fast-integration/skr-test/btp-manager-secret/index.js
@@ -1,7 +1,15 @@
+const os = require('os');
 const {expect} = require('chai');
+
+const {
+    initializeK8sClient,
+} = require('../../utils');
 
 function btpManagerSecretTest() {
     describe('BTP Manager Secret Test', function () {
+        before('REMOVE LATER: Initialize kubeconfig', async function(){
+            await initializeK8sClient({kubeconfigPath: `${os.homedir()}/k3d_kubeconfig`});
+        });
     });
 }
 
