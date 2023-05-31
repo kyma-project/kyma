730b5494d5434afee68f138aa63cd2770ec5c85d
Marcin Szwed <marcin.szwed@sap.com>
2023-05-31 13:50:15 +0200

    Move btp-manager Secret existence check to new file

--- tests/fast-integration/skr-test/btp-manager-secret/index.js
+++ tests/fast-integration/skr-test/btp-manager-secret/index.js
@@ -1,14 +1,32 @@
 const os = require('os');
 const {expect} = require('chai');
-
 const {
     initializeK8sClient,
+    getSecret,
 } = require('../../utils');
+
+const secretName = 'sap-btp-manager';
+const ns = 'kyma-system';
+
+let expectedSecret, modifiedSecret;
 
 function btpManagerSecretTest() {
     describe('BTP Manager Secret Test', function () {
         before('REMOVE LATER: Initialize kubeconfig', async function(){
             await initializeK8sClient({kubeconfigPath: `${os.homedir()}/k3d_kubeconfig`});
+        });
+        // Check if BTP Manager Secret with BTP Operator credentials is created properly
+        it('should check if Secret exists and contains the expected data keys', async function() {
+            console.log(`Checking the data keys of the "sap-btp-manager" Secret`);
+            expectedSecret = await getSecret(secretName, ns);
+            expect(expectedSecret).to.not.be.empty;
+            expect(expectedSecret.metadata.labels['app.kubernetes.io/managed-by']).to.equal('kcp-kyma-environment-broker');
+            expect(expectedSecret.data).to.have.property('clientid');
+            expect(expectedSecret.data).to.have.property('clientsecret');
+            expect(expectedSecret.data).to.have.property('sm_url');
+            expect(expectedSecret.data).to.have.property('tokenurl');
+            expect(expectedSecret.data).to.have.property('cluster_id');
+            modifiedSecret = JSON.parse(JSON.stringify(expectedSecret))
         });
     });
 }
--- tests/fast-integration/skr-test/test.js
+++ tests/fast-integration/skr-test/test.js
@@ -36,20 +36,6 @@ describe('SKR test', function() {
     options = skr.options;
   });
 
-  // Check if the secret with BTP credentials is created properly
-  it('should check if secret exists and contains the expected data', async function() {
-    console.log(`Checking the contents of the "sap-btp-manager" secret`);
-    const secretToCheck = await getSecret('sap-btp-manager', 'kyma-system');
-    expect(secretToCheck).to.not.be.empty;
-    expect(secretToCheck.metadata.labels['app.kubernetes.io/managed-by']).to.equal('kcp-kyma-environment-broker');
-    expect(secretToCheck.data).to.have.property('clientid');
-    expect(secretToCheck.data).to.have.property('clientsecret');
-    expect(secretToCheck.data).to.have.property('sm_url');
-    expect(secretToCheck.data).to.have.property('tokenurl');
-    expect(secretToCheck.data).to.have.property('cluster_id');
-  });
-
-
   // Run the OIDC and machine type tests
   oidcE2ETest(getShootOptionsFunc, getShootInfoFunc);
   machineTypeE2ETest(getShootOptionsFunc, getShootInfoFunc);
