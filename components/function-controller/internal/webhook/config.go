f4271d6365d9ae0d8f33a3cf52f068e2d04d7329
Damian Badura <damian.badura@sap.com>
2023-08-31 04:28:34 +0200

    fix test and remove old function

--- components/function-controller/internal/webhook/config.go
+++ components/function-controller/internal/webhook/config.go
@@ -1,11 +1,5 @@
 package webhook
 
-import (
-	serverlessv1alpha2 "github.com/kyma-project/kyma/components/function-controller/pkg/apis/serverless/v1alpha2"
-	"github.com/pkg/errors"
-	"github.com/vrischmann/envconfig"
-)
-
 type Config struct {
 	SystemNamespace string `envconfig:"default=kyma-system"`
 	ServiceName     string `envconfig:"default=serverless-webhook"`
@@ -13,12 +7,4 @@ type Config struct {
 	Port            int    `envconfig:"default=8443"`
 	LogConfigPath   string `envconfig:"default=/appdata/log_config.yaml"`
 	ConfigPath      string `envconfig:"default=/appdata/config.yaml"`
-}
-
-func ReadValidationConfigV1Alpha2OrDie() *serverlessv1alpha2.ValidationConfig {
-	validationCfg := &serverlessv1alpha2.ValidationConfig{}
-	if err := envconfig.InitWithPrefix(validationCfg, "WEBHOOK_VALIDATION"); err != nil {
-		panic(errors.Wrap(err, "while reading env defaulting variables"))
-	}
-	return validationCfg
 }
