6cafb5cbed2710348d42dc2e1a72c3512df6ca04
void404 <wojciech.nawa@sap.com>
2023-09-25 11:26:41 +0200

    Fix app-con test timeout

--- tests/components/application-connector/scripts/check-pod-logs.sh
+++ tests/components/application-connector/scripts/check-pod-logs.sh
@@ -9,16 +9,20 @@ if [ $# -ne 1 ]; then
 fi
 
 
+time_so_far=0 # we've already waited that many seconds
+sleep_sec=5 # wait between checks
+wait_timeout=900 # 15min -> 900sec
 retval_complete=1
 retval_failed=1
-while [[ $retval_complete -ne 0 ]] && [[ $retval_failed -ne 0 ]]; do
-  sleep 5
+while [[ $retval_complete -ne 0 ]] && [[ $retval_failed -ne 0 ]] && [[ $time_so_far -le $wait_timeout ]]; do
+  sleep $sleep_sec
+  time_so_far=$((time_so_far+sleep_sec))
+
   output=$(kubectl wait --for=condition=failed -n $NAMESPACE job/$JOB_NAME --timeout=0 2>&1)
   retval_failed=$?
   output=$(kubectl wait --for=condition=complete -n $NAMESPACE job/$JOB_NAME --timeout=0 2>&1)
   retval_complete=$?
 done
-
 
 if ([[ ${EXPORT_RESULT} == true ]]); then
 	kubectl -n $NAMESPACE logs -f job/$JOB_NAME | tee /dev/stderr | $GOPATH/bin/go-junit-report -subtest-mode exclude-parents -set-exit-code > junit-report.xml
