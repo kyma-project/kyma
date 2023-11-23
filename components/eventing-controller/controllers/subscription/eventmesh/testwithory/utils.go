4f1d6526acba0ec1a15214650a0e375f0b88629c
Mansur Uralov <m.uralov@sap.com>
2023-11-23 17:48:32 +0100

    Delete external CRD folder

--- components/eventing-controller/controllers/subscription/eventmesh/testwithory/utils.go
+++ components/eventing-controller/controllers/subscription/eventmesh/testwithory/utils.go
@@ -202,7 +202,6 @@ func startTestEnv() (*rest.Config, error) {
 	emTestEnsemble.testEnv = &envtest.Environment{
 		CRDDirectoryPaths: []string{
 			filepath.Join("../../../../", "config", "crd", "bases"),
-			filepath.Join("../../../../", "config", "crd", "external"),
 		},
 		AttachControlPlaneOutput: attachControlPlaneOutput,
 		UseExistingCluster:       &useExistingCluster,
