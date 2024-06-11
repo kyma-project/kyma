44e3fd9b57998b9ac80551386cf403f49b61c12c
Wojciech Nawa <wojciech.nawa@sap.com>
2024-06-11 08:54:23 +0200

    Remove moved code

--- tests/components/application-connector/test/application-connectivity-validator/tools.go
+++
@@ -1,5 +0,0 @@
-package application_connectivity_validator
-
-func validatorURL(app, path string) string {
-	return "http://central-application-connectivity-validator.kyma-system:8080/" + app + "/" + path
-}
