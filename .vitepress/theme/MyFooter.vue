fe0ab2717de1792ee4834c48e1b014c204acd74c
pbochynski <p.bochynski@gmail.com>
2025-09-05 14:54:50 +0200

    Remove redundant tags from footer

--- .vitepress/theme/MyFooter.vue
+++ .vitepress/theme/MyFooter.vue
@@ -22,5 +22,3 @@ export default {
   color: var(--vp-c-text-2);
 }
 </style>
-<p style="text-align: center;"><a href="https://www.sap.com/corporate/en/legal/terms-of-use.html" target="_blank" rel="noopener noreferrer">Terms of Use</a> | <a href="https://www.sap.com/corporate/en/legal/privacy.html" target="_blank" rel="noopener noreferrer">Privacy Statement</a> | <a href="https://www.sap.com/about/legal/impressum.html" target="_blank" rel="noopener noreferrer">Legal Disclosure</a></p>
-<p style="text-align: center;">Copyright © 2018-2025 The Kyma project authors.</p>
