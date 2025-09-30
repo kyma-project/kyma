6bbbecab4e6c001682a5c23c889502b425cbfcbf
Marco Porru <marco.porru@sap.com>
2025-09-30 16:31:33 +0200

    Update move_to_public.sh
    Co-authored-by: Mateusz Wisniewski <46759954+mrCherry97@users.noreply.github.com>

--- move_to_public.sh
+++ move_to_public.sh
@@ -28,7 +28,6 @@ find "$ROOT_DIR" -type f \
   ! -name "*.scss" \
   ! -name "*.html" \
   ! -name "config.*" \
-  ! -name ".DS_Store" \
   ! -name "$SCRIPT_NAME" \
   | while read -r file; do
     # Skip files in root directory unless they are inside /assets/
