9d5e3d91ee885713a8bf662ecfc6a87f1b27a6e2
eternasparta <eternasparta@live.it>
2025-10-16 13:37:37 +0200

    favicon and formatter

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -160,11 +160,6 @@ export default defineConfig({
       },
       { text: 'Glossary', link: '/glossary' }
     ],
-    head: [[
-      'link',
-      {rel: 'icon', type: 'image/svg+xml', href: '/assets/logo.svg'}
-    ],['link', { rel: 'icon', href: '/assets/favicon.ico' }]
-    ],
     socialLinks: [{
         icon: {
           svg: '<svg aria-hidden="true" class="svg-icon iconLogoGlyphMd native native" width="32" height="37" viewBox="0 0 32 37"><path d="M26 33v-9h4v13H0V24h4v9h22Z" fill="#BCBBBB"></path><path d="m21.5 0-2.7 2 9.9 13.3 2.7-2L21.5 0ZM26 18.4 13.3 7.8l2.1-2.5 12.7 10.6-2.1 2.5ZM9.1 15.2l15 7 1.4-3-15-7-1.4 3Zm14 10.79.68-2.95-16.1-3.35L7 23l16.1 2.99ZM23 30H7v-3h16v3Z" fill="#F48024"></path></svg>'
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -11,5 +11,35 @@ export default {
     app.component('Tabs', Tabs)
     app.component('Tab', Tab)
     // You can register global components here if needed
+    if (typeof window !== 'undefined') {
+      const stylesheets = [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: './assets/favicon-16x16.png',
+          sizes: '16x16',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: './assets/favicon-32x32.png',
+          sizes: '32x32',
+        },
+        { rel: 'shortcut icon', href: './assets/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: './assets/apple-touch-icon.png',
+        },
+      ];
+
+      stylesheets.forEach((attrs) => {
+        const link = document.createElement('link');
+        Object.entries(attrs).forEach(([key, value]) => {
+          link.setAttribute(key, value);
+        });
+        document.head.appendChild(link);
+      });
+    }
   }
 }
