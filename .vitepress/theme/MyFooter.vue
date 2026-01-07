814043de9576f083a705797f224668de90c5fb04
Małgorzata Świeca <malgorzata.swieca@sap.com>
2026-01-07 13:52:25 +0100

    Update copyright year in MyFooter.vue

--- .vitepress/theme/MyFooter.vue
+++ .vitepress/theme/MyFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="my-footer">
-    <p style="text-align: center;">Copyright © 2018-2025 The Kyma project authors.</p>
+    <p style="text-align: center;">Copyright © 2018-2026 The Kyma project authors.</p>
     <p style="text-align: center;">
       <a href="https://www.sap.com/corporate/en/legal/terms-of-use.html"  target="_blank" rel="noopener noreferrer">Terms of Use</a> | <a href="https://www.sap.com/corporate/en/legal/privacy.html" target="_blank" rel="noopener noreferrer">Privacy Statement</a> | <a href="https://www.sap.com/about/legal/impressum.html" target="_blank" rel="noopener noreferrer">Legal Disclosure</a>
     </p>
